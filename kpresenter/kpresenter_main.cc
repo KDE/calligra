aacdb38e7518b79fa527b9d90d41587230c34032
Reginald Stadlbauer <reggie@kde.org>
1998-05-05 12:21:30 +0000

    other changes
    svn path=/trunk/koffice/; revision=7313

--- kpresenter/kpresenter_main.cc
+++ kpresenter/kpresenter_main.cc
@@ -14,6 +14,13 @@
 
 #include "kpresenter_main.h"
 #include "kpresenter_main.moc"
+#include <string.h>
+#include <factory_impl.h>
+
+bool g_bWithGUI = true;
+
+FACTORY(KPresenterDocument_impl,KPresenter::Factory_skel,KPresenterFactory)
+typedef AutoLoader<KPresenterFactory> KPresenterAutoLoader;
 
 /******************************************************************/
 /* class KPresenterApp - KPresenter Application                   */
@@ -34,13 +41,16 @@ KPresenterApp::~KPresenterApp()
 /*=================== start application ==========================*/
 void KPresenterApp::start()
 {
+  if ( g_bWithGUI )
+    {
+      KPresenterShell_impl* m_pShell;
+      m_pShell = new KPresenterShell_impl;
+      m_pShell->enableMenuBar();
+      m_pShell->PartShell_impl::enableStatusBar();
+      m_pShell->enableToolBars();
+      m_pShell->show();
+    }
   printf("Started\n");
-  
-  m_pShell = new KPresenterShell_impl;
-  m_pShell->enableMenuBar();
-  m_pShell->PartShell_impl::enableStatusBar();
-  m_pShell->enableToolBars();
-  m_pShell->show();
 }
 
 /*======================== main ==================================*/
@@ -49,8 +59,17 @@ int main(int argc,char **argv)
   FormatManager *formatMngr;
   formatMngr = new FormatManager();
 
+  for(int i = 1;i < argc;i++)
+  {
+    if (strcmp(argv[i],"-s") == 0 || strcmp(argv[i],"--server") == 0)
+      g_bWithGUI = false;
+  }
+  
+  KPresenterAutoLoader loader("IDL:KPresenter/Factory:1.0");
+
   KPresenterApp app(argc,argv);
   app.exec();
 
   return 0;
 }
+
