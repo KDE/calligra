721c1663044929a86410576b588b24eb591ea5ad
Stephan Kulow <coolo@kde.org>
1998-10-08 22:02:59 +0000

    more indentation
    svn path=/trunk/koffice/; revision=11197

--- kformula/FormulaClass.h
+++ kformula/FormulaClass.h
@@ -17,28 +17,28 @@ class BasicElement;
 
 class FormulaClass 
 {
-public:
- FormulaClass(QPainter *p=0L);
- void setPainter(QPainter* p) { thePainter=p; }
- void setActiveElement(BasicElement* c); 
- void setCursor(QRect r) { theCursor=r; }
- void setFont(QFont f) { theFont=f;}
- void setColor(QColor c) { theColor=c;}
- void setFirstElement(BasicElement* c);
- QPainter * painter() { return thePainter; } 
- BasicElement *activeElement() {return theActiveElement; } 
- QRect cursor() {return theCursor;}
- QFont generalFont() {return theFont; }
- QColor generalColor() {return theColor; }
+ public:
+    FormulaClass(QPainter *p=0L);
+    void setPainter(QPainter* p) { thePainter=p; }
+    void setActiveElement(BasicElement* c); 
+    void setCursor(QRect r) { theCursor=r; }
+    void setFont(QFont f) { theFont=f;}
+    void setColor(QColor c) { theColor=c;}
+    void setFirstElement(BasicElement* c);
+    QPainter * painter() { return thePainter; } 
+    BasicElement *activeElement() {return theActiveElement; } 
+    QRect cursor() {return theCursor;}
+    QFont generalFont() {return theFont; }
+    QColor generalColor() {return theColor; }
 
-protected:
-// QPoint theClickPoint;
- QPainter *thePainter;
- BasicElement *theActiveElement;
- BasicElement *theFirstElement;
- QRect theCursor;
- QFont theFont;
- QColor theColor;
+ protected:
+    // QPoint theClickPoint;
+    QPainter *thePainter;
+    BasicElement *theActiveElement;
+    BasicElement *theFirstElement;
+    QRect theCursor;
+    QFont theFont;
+    QColor theColor;
 };
 
 #endif		     
