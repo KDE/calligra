9f879194876e5a92ee0de5ec6e900bcc03c281de
Reginald Stadlbauer <reggie@kde.org>
1999-02-19 15:08:16 +0000

    set correct background
    svn path=/trunk/koffice/; revision=17084

--- kdiagramm/kdiagramm_view.cc
+++ kdiagramm/kdiagramm_view.cc
@@ -1,21 +1,21 @@
 /* This file is part of the KDE project
    Copyright (C) 1998, 1999 Torben Weis <weis@kde.org>
- 
+
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License as published by the Free Software Foundation; either
    version 2 of the License, or (at your option) any later version.
- 
+
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Library General Public License for more details.
- 
+
    You should have received a copy of the GNU Library General Public License
    along with this library; see the file COPYING.LIB.  If not, write to
    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.
-*/     
+*/
 
 #include <qprinter.h>
 #include "kdiagramm_view.h"
@@ -49,11 +49,13 @@ KDiagrammView::KDiagrammView( QWidget *_parent, const char *_name, KDiagrammDoc*
 {
   setWidget( this );
 
-  OPPartIf::setFocusPolicy( OpenParts::Part::ClickFocus ); 
+  OPPartIf::setFocusPolicy( OpenParts::Part::ClickFocus );
 
   m_pDoc = _doc;
 
   QObject::connect( m_pDoc, SIGNAL( sig_updateView() ), this, SLOT( slotUpdateView() ) );
+
+  setBackgroundColor(white);
 }
 
 void KDiagrammView::init()
@@ -63,7 +65,7 @@ void KDiagrammView::init()
    ******************************************************/
 
   cerr << "Registering menu as " << id() << endl;
-  
+
   OpenParts::MenuBarManager_var menu_bar_manager = m_vMainWindow->menuBarManager();
   if ( !CORBA::is_nil( menu_bar_manager ) )
     menu_bar_manager->registerClient( id(), this );
@@ -78,7 +80,7 @@ void KDiagrammView::init()
   if ( !CORBA::is_nil( tool_bar_manager ) )
     tool_bar_manager->registerClient( id(), this );
   else
-    cerr << "Did not get a tool bar manager" << endl;  
+    cerr << "Did not get a tool bar manager" << endl;
 
   // Paint something
   slotUpdateView();
@@ -94,12 +96,12 @@ KDiagrammView::~KDiagrammView()
 void KDiagrammView::cleanUp()
 {
   cerr << "void KDiagrammView::cleanUp() " << endl;
-  
+
   if ( m_bIsClean )
     return;
 
   cerr << "1b) Unregistering menu and toolbar" << endl;
-  
+
   OpenParts::MenuBarManager_var menu_bar_manager = m_vMainWindow->menuBarManager();
   if ( !CORBA::is_nil( menu_bar_manager ) )
     menu_bar_manager->unregisterClient( id() );
@@ -107,7 +109,7 @@ void KDiagrammView::cleanUp()
   OpenParts::ToolBarManager_var tool_bar_manager = m_vMainWindow->toolBarManager();
   if ( !CORBA::is_nil( tool_bar_manager ) )
     tool_bar_manager->unregisterClient( id() );
-  
+
   m_pDoc->removeView( this );
 
   KoViewIf::cleanUp();
@@ -124,7 +126,7 @@ bool KDiagrammView::event( const char* _event, const CORBA::Any& _value )
 
 
   END_EVENT_MAPPER;
-  
+
   return false;
 }
 
@@ -152,7 +154,7 @@ bool KDiagrammView::mappingEventConfigured( KDiagramm::View::EventConfigured _ev
 bool KDiagrammView::mappingCreateToolbar( OpenPartsUI::ToolBarFactory_ptr _factory )
 {
   cerr << "bool KDiagrammView::mappingCreateToolbar( OpenPartsUI::ToolBarFactory_ptr _factory )" << endl;
-  
+
   if ( CORBA::is_nil( _factory ) )
   {
     cerr << "Setting to nil" << endl;
@@ -182,7 +184,7 @@ bool KDiagrammView::mappingCreateToolbar( OpenPartsUI::ToolBarFactory_ptr _facto
   tmp += "/kdiagramm/pics/cakes.xpm";
   pix = OPUIUtils::loadPixmap( tmp );
   m_idButtonEdit_Cakes = m_vToolBarEdit->insertButton2( pix , 3, SIGNAL( clicked() ), this, "modeCakes", true, i18n( "Cakes" ), -1 );
-  
+
   m_vToolBarEdit->enable( OpenPartsUI::Show );
 
   m_vToolBarEdit->enable( OpenPartsUI::Hide );
@@ -201,7 +203,7 @@ bool KDiagrammView::mappingCreateMenubar( OpenPartsUI::MenuBar_ptr _menubar )
     return true;
   }
 
-  // Edit  
+  // Edit
   _menubar->insertMenu( i18n( "&Edit" ), m_vMenuEdit, -1, -1 );
 
   QString path = kapp->kde_datadir().copy();
@@ -218,30 +220,30 @@ bool KDiagrammView::mappingCreateMenubar( OpenPartsUI::MenuBar_ptr _menubar )
   path += "/kdiagramm/pics/bars.xpm";
   pix = OPUIUtils::loadPixmap( path );
   m_idMenuEdit_Bars = m_vMenuEdit->insertItem6( pix, i18n("&Bars"), this, "modeBars", CTRL + Key_B, -1, -1 );
-  
+
   path = kapp->kde_datadir().copy();
   path += "/kdiagramm/pics/cakes.xpm";
   pix = OPUIUtils::loadPixmap( path );
   m_idMenuEdit_Cakes = m_vMenuEdit->insertItem6( pix, i18n("&Cakes"), this, "modeCakes", CTRL + Key_C, -1, -1 );
 
   m_vMenuEdit->insertSeparator( -1 );
-  
+
   m_idMenuEdit_Data = m_vMenuEdit->insertItem( i18n("&Edit Data..."), this, "editData", CTRL + Key_E );
 
   m_vMenuEdit->insertSeparator( -1 );
-  
+
   m_idMenuEdit_Page = m_vMenuEdit->insertItem( i18n("&Page Layout"), this, "pageLayout", CTRL + Key_L );
 
   m_idMenuConfig_Chart = m_vMenuEdit->insertItem( i18n( "C&onfigure Chart..." ), this, "configChart", CTRL + Key_O );
 
   return true;
-}      
+}
 
 void KDiagrammView::configChart()
 {
        m_pDoc->configChart();
 }
-  
+
 
 
 void KDiagrammView::editData()
@@ -258,10 +260,10 @@ CORBA::Boolean KDiagrammView::printDlg()
 {
   QPrinter prt;
   if ( QPrintDialog::getPrinterSetup( &prt ) )
-  {    
+  {
     m_pDoc->print( &prt );
   }
-  
+
   return true;
 }
 
@@ -269,7 +271,7 @@ void KDiagrammView::pageLayout()
 {
   m_pDoc->paperLayoutDlg();
 }
-  
+
 void KDiagrammView::newView()
 {
   assert( (m_pDoc != 0L) );
@@ -283,7 +285,7 @@ void KDiagrammView::modeLines()
 {
   KDiagramm::View::EventConfigured event;
   event.mode = KDiagramm::LINES;
-  
+
   EMIT_EVENT( this, KDiagramm::View::eventConfigured, event );
 }
 
@@ -291,7 +293,7 @@ void KDiagrammView::modeAreas()
 {
   KDiagramm::View::EventConfigured event;
   event.mode = KDiagramm::AREAS;
-  
+
   EMIT_EVENT( this, KDiagramm::View::eventConfigured, event );
 }
 
@@ -299,7 +301,7 @@ void KDiagrammView::modeBars()
 {
   KDiagramm::View::EventConfigured event;
   event.mode = KDiagramm::BARS;
-  
+
   EMIT_EVENT( this, KDiagramm::View::eventConfigured, event );
 }
 
@@ -307,7 +309,7 @@ void KDiagrammView::modeCakes()
 {
   KDiagramm::View::EventConfigured event;
   event.mode = KDiagramm::CAKES;
-  
+
   EMIT_EVENT( this, KDiagramm::View::eventConfigured, event );
 }
 
