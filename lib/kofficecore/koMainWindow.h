7eb5597220ca42859631f7e7072a93382742fb15
Reginald Stadlbauer <reggie@kde.org>
1999-03-09 22:47:26 +0000

    some stuff for fixing focus problems CVS: CVS:
    svn path=/trunk/koffice/; revision=17823

--- lib/kofficecore/koMainWindow.h
+++ lib/kofficecore/koMainWindow.h
@@ -1,21 +1,21 @@
 /* This file is part of the KDE project
    Copyright (C) 1998, 1999 Torben Weis <weis@kde.org>
- 
+
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License as published by the Free Software Foundation; either
    version 2 of the License, or (at your option) any later version.
- 
+
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Library General Public License for more details.
- 
+
    You should have received a copy of the GNU Library General Public License
    along with this library; see the file COPYING.LIB.  If not, write to
    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.
-*/     
+*/
 
 #ifndef __ko_main_window_h__
 #define __ko_main_window_h__
@@ -46,14 +46,16 @@ public:
   virtual void createHelpMenu( OPMenuBar* );
 
   virtual void setRootPart( unsigned long _part_id );
-  
+
   virtual void cleanUp();
 
   virtual KOffice::Document_ptr document() = 0L;
   virtual KOffice::View_ptr view() = 0L;
 
+  KoFrame *getFrame() { return m_pFrame; }
+  
 protected slots:
-  virtual void slotActivePartChanged( unsigned long _new_part_id, unsigned long _old_opart_id );  
+  virtual void slotActivePartChanged( unsigned long _new_part_id, unsigned long _old_opart_id );
 
   virtual void slotFileNew();
   virtual void slotFileOpen();
@@ -63,11 +65,11 @@ protected slots:
   virtual void slotFileClose();
   virtual void slotFileQuit();
   virtual void slotHelpAbout();
-  
+
 protected:
   OPMenu* m_pFileMenu;
   OPMenu* m_pHelpMenu;
-  
+
   int m_idMenuFile_New;
   int m_idMenuFile_Open;
   int m_idMenuFile_Save;
@@ -76,8 +78,8 @@ protected:
   int m_idMenuFile_Close;
   int m_idMenuFile_Quit;
   int m_idMenuHelp_About;
-  
-  enum { TOOLBAR_NEW, TOOLBAR_OPEN, TOOLBAR_SAVE, TOOLBAR_PRINT }; 
+
+  enum { TOOLBAR_NEW, TOOLBAR_OPEN, TOOLBAR_SAVE, TOOLBAR_PRINT };
 
   KoFrame* m_pFrame;
   KoMainWindowIf* m_pKoInterface;
@@ -98,7 +100,7 @@ public:
   
 protected:
   void unmarkPart();
-  
+
   KoMainWindow* m_pKoMainWindow;
 
   OpenParts::Id m_iMarkedPart;
--- lib/kofficecore/koView.h
+++ lib/kofficecore/koView.h
@@ -1,21 +1,21 @@
 /* This file is part of the KDE project
    Copyright (C) 1998, 1999 Torben Weis <weis@kde.org>
- 
+
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License as published by the Free Software Foundation; either
    version 2 of the License, or (at your option) any later version.
- 
+
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Library General Public License for more details.
- 
+
    You should have received a copy of the GNU Library General Public License
    along with this library; see the file COPYING.LIB.  If not, write to
    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.
-*/     
+*/
 
 #ifndef __ko_view_h__
 #define __ko_view_h__
@@ -31,7 +31,7 @@ class KoViewIf : virtual public OPViewIf,
 public:
   KoViewIf( KoDocument* _doc );
   ~KoViewIf();
-  
+
   // IDL
   virtual KOffice::View::Mode mode();
   virtual void setMode( KOffice::View::Mode _mode );
@@ -43,7 +43,7 @@ public:
   // IDL
   virtual CORBA::Boolean printDlg();
   virtual void setMainWindow( OpenParts::MainWindow_ptr _main );
-  
+
   // IDL
   void KoViewIf::setFocus( CORBA::Boolean _focus );
 
@@ -52,10 +52,10 @@ public:
   virtual CORBA::ULong rightGUISize() { return 0; }
   virtual CORBA::ULong topGUISize() { return 0; }
   virtual CORBA::ULong bottomGUISize() { return 0; }
-  
+
 protected:
   CORBA::Boolean m_bMarked;
-  
+
   KOffice::View::Mode m_eMode;
   KOffice::MainWindow_var m_vKoMainWindow;
 };
