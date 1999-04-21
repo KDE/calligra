352a6c9b427997f840f089bfd62ce162151430b3
Reginald Stadlbauer <reggie@kde.org>
1999-04-21 18:38:48 +0000

    make it compile
    svn path=/trunk/koffice/; revision=20134

--- kdiagramm/kdiagramm_view.cc
+++ kdiagramm/kdiagramm_view.cc
@@ -1,22 +1,3 @@
-/* This file is part of the KDE project
-   Copyright (C) 1998, 1999 Torben Weis <weis@kde.org>
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.
-*/
-
 #include <qprinter.h>
 #include "kdiagramm_view.h"
 
@@ -37,6 +18,7 @@
 
 #include "kdiagramm_doc.h"
 #include "kdiagramm_shell.h"
+
 #include <klocale.h>
 
 /*****************************************************************************
@@ -55,8 +37,6 @@ KDiagrammView::KDiagrammView( QWidget *_parent, const char *_name, KDiagrammDoc*
   m_pDoc = _doc;
 
   QObject::connect( m_pDoc, SIGNAL( sig_updateView() ), this, SLOT( slotUpdateView() ) );
-
-  setBackgroundColor(white);
 }
 
 void KDiagrammView::init()
@@ -122,34 +102,10 @@ bool KDiagrammView::event( const char* _event, const CORBA::Any& _value )
 
   MAPPING( OpenPartsUI::eventCreateMenuBar, OpenPartsUI::typeCreateMenuBar_var, mappingCreateMenubar );
   MAPPING( OpenPartsUI::eventCreateToolBar, OpenPartsUI::typeCreateToolBar_var, mappingCreateToolbar );
-  MAPPING( KDiagramm::View::eventConfigured, KDiagramm::View::EventConfigured,
-	   mappingEventConfigured );
-
 
   END_EVENT_MAPPER;
 
   return false;
-}
-
-bool KDiagrammView::mappingEventConfigured( KDiagramm::View::EventConfigured _event )
-{
-  switch ( _event.mode )
-  {
-  case KDiagramm::LINES:
-    m_pDoc->setDiaType( KoDiagramm::DT_LINIEN );
-    break;
-  case KDiagramm::AREAS:
-    m_pDoc->setDiaType( KoDiagramm::DT_AREA );
-    break;
-  case KDiagramm::BARS:
-    m_pDoc->setDiaType( KoDiagramm::DT_SAEULEN );
-    break;
-  case KDiagramm::CAKES:
-    m_pDoc->setDiaType( KoDiagramm::DT_KREIS );
-    break;
-  }
-
-  return true;
 }
 
 bool KDiagrammView::mappingCreateToolbar( OpenPartsUI::ToolBarFactory_ptr _factory )
@@ -188,11 +144,6 @@ bool KDiagrammView::mappingCreateToolbar( OpenPartsUI::ToolBarFactory_ptr _facto
 
   m_vToolBarEdit->enable( OpenPartsUI::Show );
 
-  m_vToolBarEdit->enable( OpenPartsUI::Hide );
-  m_vToolBarEdit->setBarPos(OpenPartsUI::Floating);
-  m_vToolBarEdit->setBarPos(OpenPartsUI::Top);
-  m_vToolBarEdit->enable( OpenPartsUI::Show );
-
   return true;
 }
 
@@ -204,53 +155,63 @@ bool KDiagrammView::mappingCreateMenubar( OpenPartsUI::MenuBar_ptr _menubar )
     return true;
   }
 
+  CORBA::WString_var text;
+
   // Edit
-  _menubar->insertMenu( i18n( "&Edit" ), m_vMenuEdit, -1, -1 );
+  text = Q2C( i18n( "&Edit" ) );
+  _menubar->insertMenu( text, m_vMenuEdit, -1, -1 );
 
   QString path = kapp->kde_datadir().copy();
   path += "/kdiagramm/pics/lines.xpm";
   OpenPartsUI::Pixmap_var pix = OPUIUtils::loadPixmap( path );
-  m_idMenuEdit_Lines = m_vMenuEdit->insertItem6( pix, i18n("&Lines"), this, "modeLines", CTRL + Key_L, -1, -1 );
+  text = Q2C( i18n("&Lines") );
+  m_idMenuEdit_Lines = m_vMenuEdit->insertItem6( pix, text, this, "modeLines", CTRL + Key_L, -1, -1 );
 
   path = kapp->kde_datadir().copy();
   path += "/kdiagramm/pics/areas.xpm";
   pix = OPUIUtils::loadPixmap( path );
-  m_idMenuEdit_Areas = m_vMenuEdit->insertItem6( pix, i18n("&Areas"), this, "modeAreas", CTRL + Key_A, -1, -1 );
+  text = Q2C( i18n("&Areas") );
+  m_idMenuEdit_Areas = m_vMenuEdit->insertItem6( pix, text, this, "modeAreas", CTRL + Key_A, -1, -1 );
 
   path = kapp->kde_datadir().copy();
   path += "/kdiagramm/pics/bars.xpm";
   pix = OPUIUtils::loadPixmap( path );
-  m_idMenuEdit_Bars = m_vMenuEdit->insertItem6( pix, i18n("&Bars"), this, "modeBars", CTRL + Key_B, -1, -1 );
+  text = Q2C( i18n("&Bars") );
+  m_idMenuEdit_Bars = m_vMenuEdit->insertItem6( pix, text, this, "modeBars", CTRL + Key_B, -1, -1 );
 
   path = kapp->kde_datadir().copy();
   path += "/kdiagramm/pics/cakes.xpm";
   pix = OPUIUtils::loadPixmap( path );
-  m_idMenuEdit_Cakes = m_vMenuEdit->insertItem6( pix, i18n("&Cakes"), this, "modeCakes", CTRL + Key_C, -1, -1 );
+  text = Q2C( i18n("&Cakes") );
+  m_idMenuEdit_Cakes = m_vMenuEdit->insertItem6( pix, text, this, "modeCakes", CTRL + Key_C, -1, -1 );
 
   m_vMenuEdit->insertSeparator( -1 );
 
-  m_idMenuEdit_Data = m_vMenuEdit->insertItem( i18n("&Edit Data..."), this, "editData", CTRL + Key_E );
+  text = Q2C( i18n("&Edit Data...") );
+  m_idMenuEdit_Data = m_vMenuEdit->insertItem( text, this, "editData", CTRL + Key_E );
+
+  text = Q2C( i18n(" C&onfigure Chart..." ) );
+  m_idMenuConfig_Chart = m_vMenuEdit->insertItem( text, this, "configChart", CTRL + Key_O );
 
   m_vMenuEdit->insertSeparator( -1 );
 
-  m_idMenuEdit_Page = m_vMenuEdit->insertItem( i18n("&Page Layout"), this, "pageLayout", CTRL + Key_L );
-
-  m_idMenuConfig_Chart = m_vMenuEdit->insertItem( i18n( "C&onfigure Chart..." ), this, "configChart", CTRL + Key_O );
+  text = Q2C( i18n("&Page Layout") );
+  m_idMenuEdit_Page = m_vMenuEdit->insertItem( text, this, "pageLayout", CTRL + Key_L );
 
   return true;
 }
-
-void KDiagrammView::configChart()
-{
-       m_pDoc->configChart();
-}
-
-
 
 void KDiagrammView::editData()
 {
   m_pDoc->editData();
 }
+
+
+void KDiagrammView::configChart()
+{
+	m_pDoc->configChart();
+}
+
 
 void KDiagrammView::helpUsing()
 {
@@ -284,34 +245,22 @@ void KDiagrammView::newView()
 
 void KDiagrammView::modeLines()
 {
-  KDiagramm::View::EventConfigured event;
-  event.mode = KDiagramm::LINES;
-
-  EMIT_EVENT( this, KDiagramm::View::eventConfigured, event );
+  m_pDoc->setDiaType( KoDiagramm::DT_LINIEN );
 }
 
 void KDiagrammView::modeAreas()
 {
-  KDiagramm::View::EventConfigured event;
-  event.mode = KDiagramm::AREAS;
-
-  EMIT_EVENT( this, KDiagramm::View::eventConfigured, event );
+  m_pDoc->setDiaType( KoDiagramm::DT_AREA );
 }
 
 void KDiagrammView::modeBars()
 {
-  KDiagramm::View::EventConfigured event;
-  event.mode = KDiagramm::BARS;
-
-  EMIT_EVENT( this, KDiagramm::View::eventConfigured, event );
+  m_pDoc->setDiaType( KoDiagramm::DT_SAEULEN );
 }
 
 void KDiagrammView::modeCakes()
 {
-  KDiagramm::View::EventConfigured event;
-  event.mode = KDiagramm::CAKES;
-
-  EMIT_EVENT( this, KDiagramm::View::eventConfigured, event );
+  m_pDoc->setDiaType( KoDiagramm::DT_KREIS );
 }
 
 void KDiagrammView::slotUpdateView()
