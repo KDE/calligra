610a2a5b61718b8fe1ab9f45b5d2926ea5f1561c
Michael Koch <koch@kde.org>
1999-06-09 14:41:16 +0000

    make it compilable
    svn path=/trunk/koffice/; revision=23129

--- kimage/kimage_view.cc
+++ kimage/kimage_view.cc
@@ -44,7 +44,6 @@
 #include "kimage_doc.h"
 #include "kimage_view.h"
 #include "kimage_shell.h"
-#include "macros.h"
 
 #define CHECK_DOCUMENT \
 	if( m_pDoc->isEmpty() ) \
@@ -163,16 +162,25 @@ bool KImageView::event( const char* _event, const CORBA::Any& _value )
   return false;
 }
 
+/**
+ *  Allgemeine Makros
+ */
 #define TEXT( text ) Q2C( i18n( text ) )
 #define PIX( pix ) *OPUIUtils::convertPixmap( ICON( pix ) )
- 
+
+/**
+ *  Makros zur Menubarerzeugung
+ */
 #define MENU( menu, text ) _menubar->insertMenu( TEXT( text ), menu, -1, -1 );
 #define ITEM1( id, menu, text, func ) id = menu->insertItem( TEXT( text ), this, func, 0 );
 #define ITEM2( id, menu, text, func, key ) id = menu->insertItem4( TEXT( text ), this, func, key, -1, -1 );
 #define ITEM3( id, menu, pix, text, func ) id = menu->insertItem6( PIX( pix ), TEXT( text ), this, func, 0, -1, -1 );
 #define ITEM4( id, menu, pix, text, func, key ) id = menu->insertItem6( PIX( pix ), TEXT( text ), this, func, key, -1, -1 );
 #define MN_SEPARATOR( menu ) menu->insertSeparator( -1 );
- 
+
+/**
+ *  Makros zur Toolbarerzeugung
+ */
 #define TOOLBAR( bar ) bar = _factory->create( OpenPartsUI::ToolBarFactory::Transient );
 #define BUTTON1( id, bar, pix, num, func, text ) id = bar->insertButton2( PIX( pix ), num, SIGNAL( clicked() ), this, func, true, TEXT( text ), -1 );
 #define TB_SEPARATOR( bar ) bar->insertSeparator( -1 );                                             
