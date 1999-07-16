786942fcfc2110f2ca3d9d348de687acf9ed3bec
Werner Trobin <trobin@kde.org>
1999-07-16 08:38:47 +0000

    these files moved
    svn path=/trunk/koffice/; revision=25650

--- kword/filters/winword97/asumy.h
+++
@@ -1,9 +0,0 @@
-// ASUMY, AutoSummary Analysis
-
-#ifndef ASUMY_H
-#define ASUMY_H
-
-struct ASUMY {
-    long lLevel;
-};
-#endif // ASUMY_H
--- kword/filters/winword97/frd.h
+++
@@ -1,9 +0,0 @@
-// FRD, Footnote Reference Descriptor
-
-#ifndef FRD_H
-#define FRD_H
-
-struct FRD {
-    short nAuto;
-};
-#endif // FRD_H
