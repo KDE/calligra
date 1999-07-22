6f1762c4fb244c2e3902e2bbe1585f4d499ef6b0
Michael Koch <koch@kde.org>
1999-07-22 21:29:39 +0000

    fixed QColor::name() problem
    copied code from QT 2.1.
    
    svn path=/trunk/koffice/; revision=26105

--- kword/format.cc
+++ kword/format.cc
@@ -172,7 +172,14 @@ QDomElement KWFormat::save( QDomDocument &doc, int id )
     QDomElement format = doc.createElement( "FORMAT" );
     if ( id != -1 )
 	format.setAttribute( "id", id );
-    format.setAttribute( "color", color.name() );
+
+    QString c("#%1%2%3");
+    c = c.arg( color.red(), 2, 16 ).arg( color.green(), 2, 16 ).arg( color.blue(), 2, 16 );
+    for( uint i = 0; i < c.length(); ++i )
+      if ( c[i].isSpace() )
+        c[i] = '0';
+
+    format.setAttribute( "color", c );
     format.setAttribute( "font", userFont->getFontName() );
     format.setAttribute( "size", ptFontSize );
     format.setAttribute( "weight", weight );
