464d4c52a85a4ac1a219a180b9712984969943d5
Michael Koch <koch@kde.org>
1999-07-29 01:32:48 +0000

    kde_datadir -> KStandardDirs
    I hope I made it the right way. ;-)
    
    svn path=/trunk/koffice/; revision=26613

--- kdiagramm/kdiagramm_view.cc
+++ kdiagramm/kdiagramm_view.cc
@@ -19,6 +19,8 @@
 #include "kdiagramm_shell.h"
 
 #include <klocale.h>
+#include <kglobal.h>
+#include <kstddirs.h>
 
 /*****************************************************************************
  *
@@ -126,26 +128,23 @@ bool KDiagrammView::mappingCreateToolbar( OpenPartsUI::ToolBarFactory_ptr _facto
 
   m_vToolBarEdit = _factory->create( OpenPartsUI::ToolBarFactory::Transient );
 
-  QString tmp = kapp->kde_datadir().copy();
-  tmp += "/kdiagramm/pics/lines.xpm";
+  QString dir = KGlobal::dirs()->findResourceDir( "appdata", "pics/lines.xpm" ) + "pics/";
+  QString tmp = dir + "lines.xpm";
   OpenPartsUI::Pixmap_var pix = OPUIUtils::loadPixmap( tmp );
   toolTip = Q2C( i18n( "Lines" ) );
   m_idButtonEdit_Lines = m_vToolBarEdit->insertButton2( pix, 1, SIGNAL( clicked() ), this, "modeLines", true, toolTip, -1 );
 
-  tmp = kapp->kde_datadir().copy();
-  tmp += "/kdiagramm/pics/areas.xpm";
+  tmp = dir + "areas.xpm";
   pix = OPUIUtils::loadPixmap( tmp );
   toolTip = Q2C( i18n( "Areas" ) );
   m_idButtonEdit_Areas = m_vToolBarEdit->insertButton2( pix, 2, SIGNAL( clicked() ), this, "modeAreas", true, toolTip, -1 );
 
-  tmp = kapp->kde_datadir().copy();
-  tmp += "/kdiagramm/pics/bars.xpm";
+  tmp = dir + "bars.xpm";
   pix = OPUIUtils::loadPixmap( tmp );
   toolTip = Q2C( i18n( "3D-Bars" ) );
   m_idButtonEdit_Bars = m_vToolBarEdit->insertButton2( pix , 3, SIGNAL( clicked() ), this, "modeBars", true, toolTip, -1 );
 
-  tmp = kapp->kde_datadir().copy();
-  tmp += "/kdiagramm/pics/cakes.xpm";
+  tmp = dir + "cakes.xpm";
   pix = OPUIUtils::loadPixmap( tmp );
   toolTip = Q2C( i18n( "Cakes" ) );
   m_idButtonEdit_Cakes = m_vToolBarEdit->insertButton2( pix , 3, SIGNAL( clicked() ), this, "modeCakes", true, toolTip, -1 );
@@ -174,26 +173,23 @@ bool KDiagrammView::mappingCreateMenubar( OpenPartsUI::MenuBar_ptr _menubar )
   text = Q2C( i18n( "&Edit" ) );
   _menubar->insertMenu( text, m_vMenuEdit, -1, -1 );
 
-  QString path = kapp->kde_datadir().copy();
-  path += "/kdiagramm/pics/lines.xpm";
+  QString dir = KGlobal::dirs()->findResourceDir( "appdata", "pics/lines.xpm" ) + "pics/";
+  QString path = dir + "lines.xpm";
   OpenPartsUI::Pixmap_var pix = OPUIUtils::loadPixmap( path );
   text = Q2C( i18n("&Lines") );
   m_idMenuEdit_Lines = m_vMenuEdit->insertItem6( pix, text, this, "modeLines", CTRL + Key_L, -1, -1 );
 
-  path = kapp->kde_datadir().copy();
-  path += "/kdiagramm/pics/areas.xpm";
+  path = dir + "areas.xpm";
   pix = OPUIUtils::loadPixmap( path );
   text = Q2C( i18n("&Areas") );
   m_idMenuEdit_Areas = m_vMenuEdit->insertItem6( pix, text, this, "modeAreas", CTRL + Key_A, -1, -1 );
 
-  path = kapp->kde_datadir().copy();
-  path += "/kdiagramm/pics/bars.xpm";
+  path = dir + "bars.xpm";
   pix = OPUIUtils::loadPixmap( path );
   text = Q2C( i18n("&Bars") );
   m_idMenuEdit_Bars = m_vMenuEdit->insertItem6( pix, text, this, "modeBars", CTRL + Key_B, -1, -1 );
 
-  path = kapp->kde_datadir().copy();
-  path += "/kdiagramm/pics/cakes.xpm";
+  path = dir + "cakes.xpm";
   pix = OPUIUtils::loadPixmap( path );
   text = Q2C( i18n("&Cakes") );
   m_idMenuEdit_Cakes = m_vMenuEdit->insertItem6( pix, text, this, "modeCakes", CTRL + Key_C, -1, -1 );
