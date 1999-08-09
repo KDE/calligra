db23f311a286328a7c16c482bf6b2d316ad30c1c
Matthias Elter <m.elter@kde.org>
1999-08-09 16:21:55 +0000

    zoomtool
    svn path=/trunk/koffice/; revision=27375

---
+++ kimageshop/zoomtool.cc
@@ -0,0 +1,45 @@
+/*
+ *  zoomtool.cc - part of KImageShop
+ *
+ *  Copyright (c) 1999 The KImageShop team (see file AUTHORS)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include "zoomtool.h"
+#include "kimageshop_doc.h"
+
+ZoomTool::ZoomTool(KImageShopView *view) : Tool(0L, view){}
+ZoomTool::~ZoomTool() {}
+
+void ZoomTool::mousePress(const KImageShop::MouseEvent& e)
+{
+  if (!e.leftButton)
+    return;
+
+  float zf = m_pView->zoomFactor();
+
+  if (zf == 0) zf = 1;
+
+  if (e.shiftButton)
+    zf/=2;
+  else
+    zf*=2;
+  
+  m_pView->slotSetZoomFactor(zf);
+}
+
+void ZoomTool::mouseMove(const KImageShop::MouseEvent& e){}
+void ZoomTool::mouseRelease(const KImageShop::MouseEvent& e){}
---
+++ kimageshop/zoomtool.h
@@ -0,0 +1,40 @@
+/*
+ *  zoomtool.h - part of KImageShop
+ *
+ *  Copyright (c) 1999 The KImageShop team (see file AUTHORS)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef __zoomtool_h__
+#define __zoomtool_h__
+
+#include <qpoint.h>
+
+#include "tool.h"
+
+class ZoomTool : public Tool
+{
+ public:
+  ZoomTool(KImageShopView *view);
+  ~ZoomTool();
+
+  virtual char* toolName() { return CORBA::string_dup("ZoomTool"); }
+  virtual void mousePress(const KImageShop::MouseEvent& e);
+  virtual void mouseMove(const KImageShop::MouseEvent& e);
+  virtual void mouseRelease(const KImageShop::MouseEvent& e);
+};
+
+#endif //__zoomtool_h__
