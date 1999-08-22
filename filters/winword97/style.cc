f8afe6eb3c973810c84d9e89d1fd0f9dc203c2cf
Werner Trobin <trobin@kde.org>
1999-08-22 20:11:32 +0000

    some work on the new CStyle class
    svn path=/trunk/koffice/; revision=28043

--- filters/winword97/style.cc
+++ filters/winword97/style.cc
@@ -1,7 +1,30 @@
 #include <style.h>
 
-CStyle::CStyle() {
+CStyle::CStyle(unsigned short styleID, unsigned char id) {
+
+    if(id>0 && id<6)
+        _id=id;
+    else
+        _id=1;
+
+    stID=styleID;
     formatDirty=true;
+    _pos=0;
+    _len=0;
+
+    // just very basic init! more to come...
+    if(_id==1)
+        data.text.fontname=0L;
+    else if(_id==2)
+        data.image.filename=0L;
+    else if(_id==4)
+        data.variable.fontname=0L;
+    else if(_id==5) {
+        data.footnote.before=0L;
+        data.footnote.after=0L;
+        data.footnote.fontname=0L;
+        data.footnote.ref=0L;
+    }
 }
 
 CStyle::CStyle(CStyle &rhs) {
@@ -9,5 +32,63 @@ CStyle::CStyle(CStyle &rhs) {
     if(!rhs.dirty()) {
         //theFormat=rhs.format();
         formatDirty=false;
+    }
+}
+
+CStyle::~CStyle() {
+
+    if(_id==1) {
+        if(data.text.fontname) {
+            delete [] data.text.fontname;
+            data.text.fontname=0L;
+        }
+    }
+    else if(_id==2) {
+        if(data.image.filename) {
+            delete [] data.image.filename;
+            data.image.filename=0L;
+        }
+    }
+    else if(_id==4) {
+        if(data.variable.fontname) {
+            delete [] data.variable.fontname;
+            data.variable.fontname=0L;
+        }
+    }
+    else if(_id==5) {
+        if(data.footnote.fontname) {
+            delete [] data.footnote.fontname;
+            data.footnote.fontname=0L;
+        }
+        if(data.footnote.before) {
+            delete [] data.footnote.before;
+            data.footnote.before=0L;
+        }
+        if(data.footnote.after) {
+            delete [] data.footnote.after;
+            data.footnote.after=0L;
+        }
+        if(data.footnote.ref) {
+            delete [] data.footnote.ref;
+            data.footnote.ref=0L;
+        }
+    }
+}
+
+void CStyle::textFontname(const QString name) {
+    if(_id==1) {
+        int len=name.length();
+        data.text.fontname=new char[len+1];
+        strncpy(data.text.fontname, (const char*)name, len);
+        data.text.fontname[len]='\0';
+    }
+}
+
+void CStyle::imageFilename(const QString name) {
+    if(_id==2) {
+        int len=name.length();
+        data.image.filename=new char[len+1];
+        strncpy(data.image.filename, (const char*)name, len);
+        data.image.filename[len]='\0';
     }
 }
--- filters/winword97/style.h
+++ filters/winword97/style.h
@@ -7,22 +7,102 @@
 class CStyle {
 
 public:
-    CStyle();
+    CStyle(unsigned short styleID, unsigned char id=1);
     CStyle(CStyle &rhs);
-    ~CStyle() {}
+    ~CStyle();
 
     const QString format();   // get the <FROMAT> stuff
     const bool dirty() const { return formatDirty; }
 
     const unsigned short styleID() const { return stID; }
 
+    // general stuff
+    const unsigned char id() const { return _id; }
+
+    const unsigned long pos() const { return _pos; }
+    void pos(const unsigned long p) { _pos=p; }
+
+    const unsigned long len() const { return _len; }
+    void len(const unsigned long l) { _len=l; }
+
+    // id=1, "normal" text
+    const unsigned char textRed() const { return _id==1 ? data.text.red : 0; }
+    const unsigned char textGreen() const { return _id==1 ? data.text.green : 0; }
+    const unsigned char textBlue() const { return _id==1 ? data.text.blue : 0; }
+    const QString textFontname() const { return _id==1 ? QString(data.text.fontname) : QString(0); }
+    const unsigned char textSize() const { return _id==1 ? data.text.size : 0; }
+    const unsigned char textWeight() const { return _id==1 ? data.text.weight : 0; }
+    const unsigned char textUnderline() const { return _id==1 ? data.text.underline : 0; }
+    const unsigned char textVertalign() const { return _id==1 ? data.text.vertalign : 0; }
+
+    void textRed(const unsigned char r) { if(_id==1) data.text.red=r; }
+    void textGreen(const unsigned char g) { if(_id==1) data.text.green=g; }
+    void textBlue(const unsigned char b) { if(_id==1) data.text.blue=b; }
+    void textFontname(const QString name);
+    void textSize(const unsigned char s) { if(_id==1 && s<101) data.text.size=s; }     // limit?
+    void textWeight(const unsigned char w) { if(_id==1 && w<101) data.text.weight=w; } // limit?
+    void textUnderline(const unsigned char u) { if(_id==1 && u<2) data.text.underline=u; }
+    void textVertalign(const unsigned char v) { if(_id==1 && v<3) data.text.vertalign=v; }
+
+    // id=2, pictures
+    const QString imageFilename() const { return _id==2 ? QString(data.image.filename) : QString(0); }
+    void imageFilename(const QString name);
+
+    // id=4, variables
+    // id=5, footnotes
+
 private:
     QString theFormat;
     bool formatDirty;
     unsigned short stID;
-    bool pStyle;
+
+    // KWord data
+    unsigned char _id;   // 1=text, 2=image, 3=tab, 4=variable, 5=footnote
+    unsigned long _pos;
+    unsigned long _len;
+
+    union {
+        struct {
+            unsigned char red, green, blue;
+            char *fontname;
+            unsigned char size, weight;
+            unsigned char underline;
+            unsigned char vertalign;
+        } text;
+        struct {
+            char *filename;
+        } image;
+        struct {
+            unsigned char type;
+            unsigned char frameSet, frame, pageNum;
+            unsigned short year;
+            unsigned char month, day, fix;
+            unsigned char hour, minute, second;
+            unsigned short msecond;
+
+            unsigned char red, green, blue;
+            char *fontname;
+            unsigned char size, weight;
+            unsigned char underline;
+            unsigned char vertalign;
+        } variable;
+        struct {
+            unsigned short from, to;
+            char space;
+            unsigned short start, end;
+            char *before, *after;
+            char *ref;
+
+            unsigned char red, green, blue;
+            char *fontname;
+            unsigned char size, weight;
+            unsigned char underline;
+            unsigned char vertalign;
+        } footnote;
+    } data;
 };
 
+// I'll have to change this :)
 /*
 class PStyle {
 
