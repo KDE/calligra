54ba5ecb05f9c3d3bed962882a57461ce6c4a55d
David Faure <faure@kde.org>
1999-09-15 19:50:41 +0000

    Adaptations for the new saveDocument method I would have saved time writing a script for this :)
    svn path=/trunk/koffice/; revision=29209

--- katabase/kformviewer/kformviewer_shell.h
+++ katabase/kformviewer/kformviewer_shell.h
@@ -37,7 +37,7 @@ public:
   void setDocument( KformViewerDoc* _doc );
   virtual bool newDocument();
   virtual bool openDocument( const char* _filename, const char* _format );
-  virtual bool saveDocument( const char* _file, const char* _format );
+  virtual bool saveDocument();
   virtual bool closeDocument();
   virtual bool closeAllDocuments();
 
