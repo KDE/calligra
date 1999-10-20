6e67a992161d5dea20da4adb5748ccd1d5394292
Stephan Kulow <coolo@kde.org>
1999-10-20 10:54:54 +0000

    sync with canossa - hopefully complete now
    svn path=/trunk/koffice/; revision=31315

--- tools/spell/main.cc
+++ tools/spell/main.cc
@@ -1,122 +1,101 @@
 /* This file is part of the KDE project
    Copyright (C) 1998, 1999 Torben Weis <weis@kde.org>
- 
+
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License as published by the Free Software Foundation; either
    version 2 of the License, or (at your option) any later version.
- 
+
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Library General Public License for more details.
- 
+
    You should have received a copy of the GNU Library General Public License
    along with this library; see the file COPYING.LIB.  If not, write to
    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.
-*/     
+*/
 
 #include "main.h"
 
-#include <iostream>
+#include <qmessagebox.h>
 
-#include <komAutoLoader.h>
-
-#include <qmsgbox.h>
 #include <klocale.h>
+#include <kspell.h>
 
-typedef KOMAutoLoader<MyTool> MyAutoLoader;
+/***************************************************
+ *
+ * Factory
+ *
+ ***************************************************/
 
-MyApplication::MyApplication( int &argc, char **argv ) : KOMApplication( argc, argv , "spell_tool")
+extern "C"
+{
+    void* init_libkspelltool()
+    {
+	return new SpellCheckerFactory;
+    }
+};
+
+SpellCheckerFactory::SpellCheckerFactory( QObject* parent, const char* name )
+    : KLibFactory( parent, name )
 {
 }
 
-void MyApplication::start()
+SpellCheckerFactory::~SpellCheckerFactory()
 {
 }
 
-MyTool::MyTool( const CORBA::ORB::ObjectTag &_tag ) : DataTools::Tool_skel( _tag )
+QObject* SpellCheckerFactory::create( QObject* parent, const char* name, const char* classname )
+{
+    return new SpellChecker( parent, name );
+}
+
+/***************************************************
+ *
+ * Spellchecker
+ *
+ ***************************************************/
+
+SpellChecker::SpellChecker( QObject* parent, const char* name )
+    : KoDataTool( parent, name )
 {
 }
 
-MyTool::MyTool( CORBA::Object_ptr _obj ) : DataTools::Tool_skel( _obj )
+bool SpellChecker::run( const QString& command, void* data, const QString& datatype, const QString& mimetype )
 {
-}
+    if ( command != "spellcheck" )
+    {
+	qDebug("SpellChecker does only accept the command 'spellcheck'");
+	qDebug("   The commands %s is not accepted", command.latin1() );
+	return FALSE;
+    }
 
-void MyTool::run( const QCString & _command, KOM::Base_ptr _sender, const CORBA::Any& _value, const CORBA::Any& _id )
-{
-  if ( strcmp( _command.data(), "spellcheck" ) != 0L )
-  {
-    DataTools::UnknownCommand exc;
-    exc.command = CORBA::string_dup( _command );
-    mico_throw( exc );
-  }
- 
-  CORBA::String_var str;
-  _value >>= CORBA::Any::to_string( str, 0 );
-  
-  (void)new SpellChecker( str.in(), _sender, _id );
-  
-  cerr << "I am called" << endl;
-}
+    // Check wether we can accept the data
+    if ( datatype != "QString" )
+    {
+	qDebug("SpellChecker does only accept datatype QString");
+	return FALSE;
+    }
+    if ( mimetype != "text/plain" )
+    {
+	qDebug("SpellChecker does only accept mimetype text/plain");
+	return FALSE;
+    }
 
-void MyTool::cancel( CORBA::Long )
-{
-}
+    // Get data
+    QString buffer = *((QString*)data);
+    buffer = buffer.stripWhiteSpace();
 
-SpellChecker::SpellChecker( const QCString &_buffer, KOM::Base_ptr _sender, const CORBA::Any& _id )
-{
-  m_id = _id;
-  m_vSender = KOM::Base::_duplicate( _sender );
-  m_strBuffer = _buffer;
-  m_strBuffer = m_strBuffer.stripWhiteSpace();
-  m_pSpell = new KSpell( 0L, i18n("Spell Checker"), this, SLOT( slotReady() ) );
-}
+    // #### handle errors
+    // Call the spell checker
+    KSpell::modalCheck( buffer );
 
-void SpellChecker::slotReady()
-{
-  connect( m_pSpell, SIGNAL( done( char* ) ), this, SLOT( slotDone( char* ) ) );
-  m_pSpell->check( (char*)m_strBuffer.data() );
-}
+    // Set data
+    *((QString*)data) = buffer;
 
-void SpellChecker::slotDone( char *_buffer )
-{
-  QString str1 = (const char*)_buffer;
-  QString str2 = str1.stripWhiteSpace();
- 
-  cerr << "The result is ´" << str2 << "´" << endl;
- 
-  if ( str2 == m_strBuffer )
-  {
-    QMessageBox::information( 0L, i18n("Spell Checker"), i18n("The text is spelled correctly") );
-  }
-  else
-  {
-    CORBA::Any any;
-    DataTools::Answer a;
-    a.id = m_id;
-    a.value <<= CORBA::Any::from_string( (char*)str2.data(), 0 );
-    any <<= a;
-    m_vSender->receive( DataTools::eventDone, any );
-  }
-  
-  m_pSpell->cleanUp();
-  delete m_pSpell;
-  
-  delete this;
-  komapp_orb->shutdown( TRUE );
-}
-
-int main( int argc, char **argv )
-{
-  MyApplication app( argc, argv );
-
-  MyAutoLoader loader( "IDL:DataTools/Tool:1.0", "SpellTool" );
-  
-  app.exec();
-
-  return 0;
+    return TRUE;
 }
 
 #include "main.moc"
--- tools/spell/main.h
+++ tools/spell/main.h
@@ -1,66 +1,50 @@
 /* This file is part of the KDE project
    Copyright (C) 1998, 1999 Torben Weis <weis@kde.org>
- 
+
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License as published by the Free Software Foundation; either
    version 2 of the License, or (at your option) any later version.
- 
+
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Library General Public License for more details.
- 
+
    You should have received a copy of the GNU Library General Public License
    along with this library; see the file COPYING.LIB.  If not, write to
    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.
-*/     
+*/
 
 #ifndef __main_h__
 #define __main_h__
 
-#include <komApplication.h>
-#include <komComponent.h>
+class KSpell;
 
-#include "datatools.h"
-
-#include <kspell.h>
 #include <qobject.h>
 
-class SpellChecker : public QObject
-{
-  Q_OBJECT
-public:
-  SpellChecker( const QCString &_buffer, KOM::Base_ptr _sender, const CORBA::Any& _id );
+#include <koDataTool.h>
 
-public slots:
-  void slotReady();
-  void slotDone( char* );
-  
-protected:
-  QString m_strBuffer;
-  KSpell* m_pSpell;
-  KOM::Base_var m_vSender;
-  CORBA::Any m_id;
+#include <klibloader.h>
+
+class SpellChecker : public KoDataTool
+{
+    Q_OBJECT
+public:
+    SpellChecker( QObject* parent = 0, const char* name = 0 );
+
+    virtual bool run( const QString& command, void* data, const QString& datatype, const QString& mimetype );
 };
 
-class MyTool : virtual public DataTools::Tool_skel
+class SpellCheckerFactory : public KLibFactory
 {
+    Q_OBJECT
 public:
-  MyTool( const CORBA::ORB::ObjectTag &_tag );
-  MyTool( CORBA::Object_ptr _obj );
-  
-  virtual void run( const QCString & _command, KOM::Base_ptr _sender, const CORBA::Any& _value, const CORBA::Any& _id );
-  virtual void cancel( CORBA::Long _id );
-};
+    SpellCheckerFactory( QObject* parent = 0, const char* name = 0 );
+    ~SpellCheckerFactory();
 
-class MyApplication : public KOMApplication
-{
-public:
-  MyApplication( int &argc, char **argv );
-  
-  void start();
+    virtual QObject* create( QObject* parent = 0, const char* name = 0, const char* classname = "QObject" );
 };
 
 #endif
