9adf49cef28a7cfb9cf436342a3e1ef21efba143
Lotzi Boloni <lboloni@cpe.ucf.edu>
1999-10-25 13:47:42 +0000

    sorry
    svn path=/trunk/koffice/; revision=31796

---
+++ kchart/enginedraw.h
@@ -0,0 +1,32 @@
+void draw_3d_bar( QPainter* p,
+	     int x1,
+	     int x2,
+	     int y0,
+	     int yhigh,
+	     int xdepth,
+	     int ydepth,
+	     QColor clr,
+	     QColor clrshd );
+
+void draw_3d_line( QPainter*	p,
+		   int y0,
+		   int x1,
+		   int x2,
+		   int y1[],
+		   int y2[],
+		   int xdepth,
+		   int ydepth,
+		   int num_sets,
+		   QColor clr[],
+		   QColor clrshd[] );
+
+void draw_3d_area( QPainter* p,
+			  int x1,
+			  int x2,
+			  int y0,			// drawn from 0
+			  int y1,
+			  int y2,
+			  int xdepth,
+			  int ydepth,
+			  QColor clr,
+		   QColor clrshd );
