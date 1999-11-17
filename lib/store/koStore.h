7858b80c48c2a7124bbc72581ba35660039b9a43
David Faure <faure@kde.org>
1999-11-17 20:52:50 +0000

    Heaps of docu. Something I should have done long ago :)
    svn path=/trunk/koffice/; revision=34165

--- lib/store/koStore.h
+++ lib/store/koStore.h
@@ -25,28 +25,63 @@
 
 /**
  * Base class for @ref KoTarStore and @ref KoBinaryStore
+ * We call a "store" the file on the hard disk (the one the users sees)
+ * and call a "file" a file inside the store.
  */
 class KoStore
 {
 protected:
     /**
      * This is an abstract class.
+     * See KoTarStore and KoBinaryStore for how to construct a store
      */
     KoStore() {}
     
 public:
     enum Mode { Read, Write };
     
+    /**
+     * Destroys the store (i.e. closes the file on the hard disk)
+     */
     virtual ~KoStore() {}
 
-    virtual bool write( const char* _data, unsigned long _len ) = 0;
-    virtual long read( char *_buffer, unsigned long _len ) = 0;
+    /**
+     * Open a new file inside the store
+     * @param name the filename, internal representation ("root", "tar:0"... )
+     * @mimetype deprecated, don't use
+     */
+    virtual bool open( const QString & name, const QCString & mimetype ) = 0;
+    /**
+     * Close the file inside the store
+     */
+    virtual void close() = 0;
+
+    /**
+     * @return true if an error occured
+     */
     virtual bool bad() = 0;
 
-    virtual bool open( const QString & name, const QCString & mimetype ) = 0;
-    virtual void close() = 0;
-    virtual QByteArray read( unsigned long max ) = 0;
+    /**
+     * Write data into the currently opened file. You can also use the streams
+     * for this.
+     */
+    virtual bool write( const char* _data, unsigned long _len ) = 0;
+    /**
+     * Read data from the currently opened file. You can also use the streams
+     * for this.
+     */
+    virtual long read( char *_buffer, unsigned long _len ) = 0;
+
+    /**
+     * Write data into the currently opened file. You can also use the streams
+     * for this.
+     */
     virtual bool write( const QByteArray& data ) = 0;
+    /**
+     * Read data from the currently opened file. You can also use the streams
+     * for this.
+     */
+    virtual QByteArray read( unsigned long max ) = 0;
 };
 
 #endif
