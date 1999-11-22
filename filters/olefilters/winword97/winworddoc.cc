1f2267a7ddbc36692a2dbe94a6eba77a920f82cb
Werner Trobin <trobin@kde.org>
1999-11-22 15:28:51 +0000

    fixes
    svn path=/trunk/koffice/; revision=34704

--- filters/olefilters/winword97/winworddoc.cc
+++ filters/olefilters/winword97/winworddoc.cc
@@ -85,7 +85,7 @@ const QDomDocument * const WinWordDoc::part() {
     if(m_ready && m_success)
         return &m_part;
     else {
-        m_part.setContent(QString("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+        m_part.setContent(QString("<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE DOC >\n"
             "<DOC author=\"Reginald Stadlbauer and Torben Weis\" email=\"reggie@kde.org and weis@kde.org\" editor=\"KWord\" mime=\"application/x-kword\">\n"
             " <PAPER format=\"1\" ptWidth=\"595\" ptHeight=\"841\" mmWidth =\"210\" mmHeight=\"297\" inchWidth =\"8.26772\" inchHeight=\"11.6929\" orientation=\"0\" columns=\"1\" ptColumnspc=\"2\" mmColumnspc=\"1\" inchColumnspc=\"0.0393701\" hType=\"0\" fType=\"0\" ptHeadBody=\"9\" ptFootBody=\"9\" mmHeadBody=\"3.5\" mmFootBody=\"3.5\" inchHeadBody=\"0.137795\" inchFootBody=\"0.137795\">\n"
             "  <PAPERBORDERS mmLeft=\"10\" mmTop=\"15\" mmRight=\"10\" mmBottom=\"15\" ptLeft=\"28\" ptTop=\"42\" ptRight=\"28\" ptBottom=\"42\" inchLeft=\"0.393701\" inchTop=\"0.590551\" inchRight=\"0.393701\" inchBottom=\"0.590551\"/>\n"
