8006c52745d69b50fa6b3dcd493564afd5bd6b12
Michael Koch <koch@kde.org>
1999-11-24 13:29:31 +0000

    some API docu work
    svn path=/trunk/koffice/; revision=34890

--- lib/koparts/part.h
+++ lib/koparts/part.h
@@ -17,80 +17,156 @@ class QPainter;
 
 class Shell;
 
+/**
+ *  Contains the data and can paint it.
+ */
 class Part : public QObject
 {
     Q_OBJECT
+
 public:
+
+    /**
+     *  Constructor.
+     *
+     *  @param parent Pointer to parent object, may be NULL.
+     *  @param name   Name of the part, may be NULL.
+     */
     Part( QObject* parent = 0, const char* name = 0 );
+    
+    /**
+     *  Destructor.
+     */
     ~Part();
 
     /**
-     * @return a pointer to the parent part. If there is no parent
-     *         or if the parent is not a Part, then 0 is returned.
+     *  @return a pointer to the parent part. If there is no parent
+     *          or if the parent is not a Part, then 0 is returned.
      */
     Part* parentPart();
 
     /**
-     * Create a new view for the part.
+     *  Create a new view for the part.
+     *
+     *  You need to overload this method to create a view of your desired type.
+     *
+     *  @param parent Parent widget of the view.
+     *  @param name   Name of the view.
+     *
+     *  @see createShell
      */
     virtual View* createView( QWidget* parent = 0, const char* name = 0 ) = 0;
-    /*
-     * Create a new toplevel shell for the part. This in turn will create
-     * a new view.
+    
+    /**
+     *  Create a new toplevel shell for the part. This in turn will create
+     *  a new view.
+     *
+     *  You have to overload this method to return a shell of your desired type.
+     *
+     *  @see createView
      */
     virtual Shell* createShell() = 0;
 
     /**
-     * @return the XML GUI configuration of the part.
-     *         This is used by the @ref Shell to construct menus and
-     *         toolbars if the Part is activated.
+     *  @return the XML GUI configuration of the part.
+     *          This is used by the @ref Shell to construct menus and
+     *          toolbars if the Part is activated.
      *
-     * This functionality is in the Part and not in the View since
-     * a part may be activated and have menus etc. without having a view.
-     * That is the case if the part is directly drawn on some widget.
+     *  This functionality is in the Part and not in the View since
+     *  a part may be activated and have menus etc. without having a view.
+     *  That is the case if the part is directly drawn on some widget.
      */
     QString config();
 
     /**
-     * A convenience function
+     *  A convenience function
      *
-     * @param p is in the coordinate system of this part.
+     *  @param p is in the coordinate system of this part.
      */
     Part* hitTest( const QPoint& p );
+    
     /**
-     * Find the most nested child part which contains the
-     * questionable point. The point is in the coordinate system
-     * of this part. If no child part contains this point, then
-     * a pointer to this part is returned.
+     *  Find the most nested child part which contains the
+     *  questionable point. The point is in the coordinate system
+     *  of this part. If no child part contains this point, then
+     *  a pointer to this part is returned.
      *
-     * This function has to be overloaded if the part features child parts.
+     *  This function has to be overloaded if the part features child parts.
      *
-     * @param matrix transforms points from the parts coordinate system
-     *        to the coordinate system of the questionable point.
-     * @param p is in some unknown coordinate system, but the matrix can
-     *        be used to transform a point of this parts coordinate system
-     *        to the coordinate system of p.
+     *  @param matrix transforms points from the parts coordinate system
+     *         to the coordinate system of the questionable point.
+     *  @param p is in some unknown coordinate system, but the matrix can
+     *         be used to transform a point of this parts coordinate system
+     *         to the coordinate system of p.
+     *
+     *  @return Pointer to the part, that was hit.
      */
     virtual Part* hitTest( const QPoint& p, const QWMatrix& matrix );
 
+    /**
+     *  Retrieves an action with a given name
+     *
+     *  @param name Name of the desired action.
+     *
+     *  @return The action, may be NULL if no action with the desired name found.
+     */
     QAction* action( const char* name );
+
+    /**
+     *  Retrives all actions of this part.
+     *
+     *  @return An action collection with all actions.
+     */
     QActionCollection* actionCollection();
 
+    /**
+     *  Paints the whole part into the given painter object.
+     *
+     *  @param painter     The painter object into that should be drawn.
+     *  @param rect        The rect that should be used in the painter object.
+     *  @param transparent .
+     *  @param view        .
+     */
     virtual void paintEverything( QPainter& painter, const QRect& rect,
 				  bool transparent = FALSE, View* view = 0 ) = 0;
 
 protected:
-    virtual QString configFile() const = 0;
-    virtual QString readConfigFile( const QString& filenae ) const;
 
+    /**
+     *  Retrieves the resource filename of the part.
+     *
+     *  You have tp overload it to return the right filename.
+     */
+    virtual QString configFile() const = 0;
+
+    virtual QString readConfigFile( const QString& filename ) const;
+
+    /**
+     *  Adds a view  to the part.
+     */
     virtual void addView( View* );
+
+    /**
+     *  Retrieves the first view of the part.
+     *
+     *  @see nextView
+     */
     virtual View* firstView();
+    
+    /**
+     *  Retrieves the next view of the part after you called @ref firstView
+     *  or nestView itself.
+     *
+     *  @see firstView
+     */
     virtual View* nextView();
 
 protected slots:
+
     void slotViewDestroyed();
 
 private:
+
     QList<View> m_views;
     QString m_config;
     QActionCollection m_collection;
@@ -99,16 +175,20 @@ private:
 class PartChild : public QObject
 {
     Q_OBJECT
+
 public:
+
     enum Gadget { NoGadget, TopLeft, TopMid, TopRight, MidLeft, MidRight,
 		  BottomLeft, BottomMid, BottomRight, Move };
 
     PartChild( Part* parent, Part* part, const QRect& geometry );
+
     /**
      * When using this function you must call @ref setPart before
      * you can call any other function of this class.
      */
     PartChild( Part* parent );
+    
     ~PartChild();
 
     /**
@@ -117,7 +197,11 @@ public:
      */
     virtual void setPart( Part* part, const QRect& geometry );
 
+    /**
+     *  Sets a new geometry for this child part.
+     */
     void setGeometry( const QRect& rect );
+
     /**
      * @return the rectangle that would be used to display this
      *         child part if the child is not rotated or
@@ -127,9 +211,13 @@ public:
      * @see #setGeometry
      */
     QRect geometry() const;
-    Part* part();
 
     /**
+     *  Retrieves the part object.
+     */
+    Part* part();
+
+    /*
      * Set a different matrix. This is useful for shearing, scaling
      * and rotating the child part.
      *
@@ -137,37 +225,44 @@ public:
      */
     // void setMatrix( const QWMatrix& );
     // QWMatrix matrix() const;
+    
     /**
      * @return the region of this child part relative to the
      *         coordinate system of the parent.
      */
     QRegion region() const;
+    
     /**
      * A convenience function. The region is transformed with the passed
      * matrix.
      */
     QRegion region( const QWMatrix& ) const;
+    
     /**
      * @return the polygon which surrounds the child part. The points
      *         are in coordinates of the parent.
      */
     QPointArray pointArray() const;
+    
     /**
      * A convenience function. The points are transformed with the
      * passed matrix.
      */
     QPointArray pointArray( const QWMatrix& ) const;
+    
     /**
      * Tests wether the part contains a certain point. The point is
      * in tghe corrdinate system of the parent.
      */
     bool contains( const QPoint& ) const;
+    
     /**
      * @return the effective bounding rect after all transformations.
      *         The coordinates of the rectangle are in the coordinate system
      *         of the parent.
      */
     QRect boundingRect() const;
+    
     QRect boundingRect( const QWMatrix& ) const;
 
     /**
@@ -175,17 +270,23 @@ public:
      * affect the size of the child part.
      */
     void setScaling( double, double );
+    
     double xScaling() const;
+    
     double yScaling() const;
 
     void setShearing( double, double );
+    
     double xShearing() const;
+    
     double yShearing() const;
 
     void setRotation( double );
+    
     double rotation() const;
 
     void setRotationPoint( const QPoint& pos );
+    
     QPoint rotationPoint() const;
 
     /**
@@ -203,27 +304,31 @@ public:
      *        the childs region is set.
      */
     void setClipRegion( QPainter& painter, bool combine = TRUE );
+    
     /**
      * Transforms the painter (its worldmatrix and the clipping)
      * in such a way, that the painter can be passed to the child part
      * for drawing.
      */
     void transform( QPainter& painter );
+    
     /**
      * @return the contents rectangle that is visible.
      *         This value depends on the scaling and the
      *         geometry.
      *
-     * @see #scaling
-     * @see #geometry
+     * @see scaling geomtry
      */
     QRect contentRect() const;
 
     QRegion frameRegion( const QWMatrix& matrix, bool solid = FALSE ) const;
+    
     QPointArray framePointArray() const;
+    
     QPointArray framePointArray( const QWMatrix& ) const;
 
     Part* hitTest( const QPoint& p );
+     
     Part* hitTest( const QPoint& p, const QWMatrix& matrix );
 
     Gadget gadgetHitTest( const QPoint& p, const QWMatrix& matrix );
@@ -231,21 +336,27 @@ public:
     QWMatrix matrix() const;
 
     void lock();
+     
     void unlock();
 
     QPointArray oldPointArray( const QWMatrix& );
 
     void setTransparent( bool );
+    
     bool isTransparent();
 
 signals:
+     
     void changed( PartChild* child );
 
 protected:
+     
     QPointArray pointArray( const QRect& r, const QWMatrix& matrix ) const;
+     
     void updateMatrix();
 
 private:
+     
     Part* m_part;
     QRect m_geometry;
     double m_rotation;
--- lib/koparts/plugin.h
+++ lib/koparts/plugin.h
@@ -4,17 +4,47 @@
 #include <qobject.h>
 #include <qaction.h>
 
+/**
+ *  A KParts plugin.
+ *
+ *  @short generic plugin class for KParts.
+ */
 class Plugin : public QObject
 {
     Q_OBJECT
+
 public:
+
+    /**
+     *  Initializes a plugin.
+     */
     Plugin( QObject* parent = 0, const char* name = 0 );
+
+    /**
+     *  Destructor.
+     */
     ~Plugin();
     
+    /**
+     *  Retrieves an action with a given name.
+     *
+     *  @param name Name of the action.
+     *
+     *  @return The action, may be NULL.
+     *
+     *  @see actionCollection
+     */
     QAction* action( const char* name );
+
+    /**
+     *  Retrieves the action collection of this plugin.
+     *
+     *  @see action
+     */
     QActionCollection* actionCollection();
     
 private:
+
     QActionCollection m_collection;
 };
 
