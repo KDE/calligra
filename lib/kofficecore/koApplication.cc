43d36b0c3d309b28e124609b4d37896407caa9db
David Faure <faure@kde.org>
1999-11-27 20:15:54 +0000

    Do better error checking (when no args). This fixes the core dump from e.g. kword if pressing "Cancel" in the initial dialog
    svn path=/trunk/koffice/; revision=35246

--- lib/kofficecore/koApplication.cc
+++ lib/kofficecore/koApplication.cc
@@ -43,23 +43,26 @@ KoApplication::KoApplication(int &argc, char **argv, const QCString& rAppName, c
 void KoApplication::start()
 {
     KoDocumentEntry entry = KoDocumentEntry::queryByMimeType( m_nativeMimeType );
-        
+
     ASSERT( !entry.isEmpty() );
-        
+
     QStringList open;
     // Parse command line parameters
     for( uint i = 0; i < m_params.count(); i++ )
         if( m_params.get( i ).left( 1 ) != "-" )
-            open.append( m_params.get( i ) ); 
-        
+            open.append( m_params.get( i ) );
+
     // No argument
     if ( open.isEmpty() ) {
         KoDocument* doc = entry.createDoc( 0, "Document" );
-        doc->initDoc();
-            
-        Shell* shell = doc->createShell();
-        shell->show();
-        // setMainWidget( shell ); // probably bad idea, says Torben...
+        if ( doc && ( doc->initDoc() ) )
+        {
+          Shell* shell = doc->createShell();
+          shell->show();
+          // setMainWidget( shell ); // probably bad idea, says Torben...
+        }
+        else
+          ::exit(1);
     } else {
         // Loop through arguments
         QStringList::Iterator it = open.begin();
