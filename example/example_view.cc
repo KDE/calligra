5d526317a3642ff18e77eedb1db39b8c0b161c36
Werner Trobin <trobin@kde.org>
2000-03-25 10:50:09 +0000

    fix - make it compile
    svn path=/trunk/koffice/; revision=44610

--- example/example_view.cc
+++ example/example_view.cc
@@ -3,14 +3,16 @@
 #include "example_part.h"
 
 #include <qpainter.h>
+#include <qiconset.h>
 #include <kaction.h>
+#include <klocale.h>
 
 ExampleView::ExampleView( ExamplePart* part, QWidget* parent, const char* name )
     : KoView( part, parent, name )
 {
     setInstance( ExampleFactory::global() );
     setXMLFile( "example.rc" );
-    m_cut = new KAction( tr("&Cut"), ExampleBarIcon("editcut"), 0, this, SLOT( cut() ),
+    m_cut = new KAction( i18n("&Cut"), QIconSet(ExampleBarIcon("editcut")), 0, this, SLOT( cut() ),
 			 actionCollection(), "cut");
 }
 
