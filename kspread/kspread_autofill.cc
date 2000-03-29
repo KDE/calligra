a86e66e1646fadc8c41ba620102ff79ebde30ea6
Torben Weis <weis@kde.org>
2000-03-29 13:37:41 +0000

    Added docu and fixed auto completion for floating point values.
    svn path=/trunk/koffice/; revision=45055

--- kspread/kspread_autofill.cc
+++ kspread/kspread_autofill.cc
@@ -1,21 +1,21 @@
 /* This file is part of the KDE project
    Copyright (C) 1998, 1999 Torben Weis <weis@kde.org>
- 
+
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License as published by the Free Software Foundation; either
    version 2 of the License, or (at your option) any later version.
- 
+
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Library General Public License for more details.
- 
+
    You should have received a copy of the GNU Library General Public License
    along with this library; see the file COPYING.LIB.  If not, write to
    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.
-*/     
+*/
 
 #include "kspread_autofill.h"
 #include "kspread_cell.h"
@@ -38,15 +38,15 @@ AutoFillDeltaSequence::AutoFillDeltaSequence( AutoFillSequence *_first, AutoFill
 {
   ok = TRUE;
   sequence = 0L;
-  
+
   if ( _first->count() != _next->count() )
   {
     ok = FALSE;
     return;
   }
-  
+
   sequence = new QArray<double> ( _first->count() );
-  
+
   AutoFillSequenceItem *item = _first->getFirst();
   AutoFillSequenceItem *item2 = _next->getFirst();
   int i = 0;
@@ -79,17 +79,17 @@ bool AutoFillDeltaSequence::equals( AutoFillDeltaSequence *_delta )
     return FALSE;
   if ( sequence->size() != _delta->getSequence()->size() )
     return FALSE;
-  
+
   for ( unsigned int i = 0; i < sequence->size(); i++ )
   {
     if ( sequence->at( i ) != _delta->getSequence()->at( i ) )
       return FALSE;
   }
-    
+
   return TRUE;
 }
 
-double AutoFillDeltaSequence::getItemDelta( int _pos ) 
+double AutoFillDeltaSequence::getItemDelta( int _pos )
 {
   if ( sequence == 0L )
     return 0.0;
@@ -118,7 +118,7 @@ AutoFillSequenceItem::AutoFillSequenceItem( const char *_str )
 {
     string = _str;
     type = STRING;
-    
+
     if ( month == 0L )
     {
 	month = new QStrList();
@@ -135,7 +135,7 @@ AutoFillSequenceItem::AutoFillSequenceItem( const char *_str )
 	month->append( i18n("November") );
 	month->append( i18n("December") );
     }
-    
+
     if ( day == 0L )
     {
 	day = new QStrList();
@@ -147,13 +147,13 @@ AutoFillSequenceItem::AutoFillSequenceItem( const char *_str )
 	day->append( i18n("Saturday") );
 	day->append( i18n("Sunday") );
     }
-    
+
     if ( month->find( _str ) != -1 )
     {
 	type = MONTH;
 	return;
     }
-    
+
     if ( day->find( _str ) != -1 )
     {
 	type = DAY;
@@ -198,7 +198,7 @@ bool AutoFillSequenceItem::getDelta( AutoFillSequenceItem *seq, double &_delta )
 		_delta = ( double )( k - i );
 	    return TRUE;
 	}
-    
+
     case DAY:
 	{
 	    int i = day->find( string );
@@ -220,14 +220,14 @@ bool AutoFillSequenceItem::getDelta( AutoFillSequenceItem *seq, double &_delta )
 QString AutoFillSequenceItem::getSuccessor( int _no, double _delta )
 {
     QString erg;
-   
+
     switch( type )
     {
     case INTEGER:
 	erg.sprintf("%i", ivalue + _no * (int)_delta );
 	break;
     case FLOAT:
-	erg.sprintf("%f", ivalue + (float)_no * _delta );
+	erg.sprintf("%f", dvalue + (double)_no * _delta );
 	break;
     case FORMULAR:
     case STRING:
@@ -262,7 +262,7 @@ QString AutoFillSequenceItem::getSuccessor( int _no, double _delta )
 AutoFillSequence::AutoFillSequence( KSpreadCell *_cell )
 {
     sequence.setAutoDelete( TRUE );
-    
+
     if ( _cell->isFormular() )
     {
 	QString d = _cell->encodeFormular().data();
@@ -286,7 +286,7 @@ bool AutoFillSequence::matches( AutoFillSequence* _seq, AutoFillDeltaSequence *_
     AutoFillDeltaSequence delta( this, _seq );
     if ( !delta.isOk() )
 	return FALSE;
-    
+
     if ( delta.equals( _delta ) )
 	 return TRUE;
 
@@ -305,7 +305,7 @@ void AutoFillSequence::fillCell( KSpreadCell *src, KSpreadCell *dest, AutoFillDe
 	dest->copyLayout( src );
 	return;
     }
-    
+
     AutoFillSequenceItem *item;
     int i = 0;
     for ( item = sequence.first(); item != 0L; item = sequence.next() )
@@ -323,6 +323,7 @@ void AutoFillSequence::fillCell( KSpreadCell *src, KSpreadCell *dest, AutoFillDe
 
 void KSpreadTable::autofill( QRect &src, QRect &dest )
 {
+    // Fill from left to right
     if ( src.left() == dest.left() && src.right() < dest.right() )
     {
 	for ( int y = src.top(); y <= src.bottom(); y++ )
@@ -342,6 +343,7 @@ void KSpreadTable::autofill( QRect &src, QRect &dest )
 	}
     }
 
+    // Fill from top to bottom
     if ( src.top() == dest.top() && src.bottom() < dest.bottom() )
     {
 	for ( int x = src.left(); x <= dest.right(); x++ )
@@ -360,29 +362,53 @@ void KSpreadTable::autofill( QRect &src, QRect &dest )
 	    fillSequence( srcList, destList, seqList );
 	}
     }
-    
+
 }
 
-void KSpreadTable::fillSequence( QList<KSpreadCell>& _srcList, QList<KSpreadCell>& _destList, 
+void KSpreadTable::fillSequence( QList<KSpreadCell>& _srcList, QList<KSpreadCell>& _destList,
 				 QList<AutoFillSequence>& _seqList )
 {
     QList<AutoFillDeltaSequence> deltaList;
     deltaList.setAutoDelete( TRUE );
 
+    // What is the interval (block)? If your table looks like this:
+    // 1 3 5 7 9
+    // then the interval has the length 1 and the delta list is [2].
+    // 2 200 3 300 4 400
+    // Here the interval has length 2 and the delta list is [1,100]
+    
+    
+    // How big is the interval. It is in the range from [2...n/2].
+    // The case of an interval of length n is handled below.
+    //
+    // We try to find the shortest interval. 
     for ( unsigned int step = 1; step <= _seqList.count() / 2; step++ )
     {
+	// If the interval is of length 'step' then the _seqList size must
+	// be a multiple of 'step'
 	if ( _seqList.count() % step == 0 )
 	{
+	    // Be optimistic
 	    bool ok = TRUE;
 	    deltaList.clear();
+
+	    // Gueess the delta by looking at cells 0...2*step-1
+	    //
+	    // Since the interval may be of length 'step' we calculate the delta
+	    // between cells 0 and step, 1 and step+1, ...., step-1 and 2*step-1
 	    for ( unsigned int t = 0; t < step; t++ )
 	    {
 		deltaList.append( new AutoFillDeltaSequence( _seqList.at(t), _seqList.at(t+step) ) );
 		if ( !deltaList.getLast()->isOk() )
 		    ok = FALSE;
 	    }
-	    
-	    // Testphase
+	
+	    // Verify the delta by looking at cells step..._seqList.count()
+	    //
+	    // We only looked at the cells 0 ... '2*step-1'.
+	    // Now test wether the cells from "(tst-1) * step + s" share the same delta
+	    // with the cell "tst * step + s" for all test=1..._seqList.count()/step and
+	    // for all s=0...step-1.
 	    for ( unsigned int tst = 1; ok && ( tst * step < _seqList.count() ); tst++ )
 	    {
 		for ( unsigned int s = 0; ok && ( s < step ); s++ )
@@ -393,19 +419,27 @@ void KSpreadTable::fillSequence( QList<KSpreadCell>& _srcList, QList<KSpreadCell
 		}
 	    }
 
+	    // Did we find a valid interval ?
 	    if ( ok )
 	    {
+		// Start iterating with the first cell
 		KSpreadCell *cell = _destList.first();
 		unsigned int s = 0;
+		// Amount of intervals (blocks)
 		int block = _seqList.count() / step;
+		// Loop over all destination cells
 		while ( cell )
 		{
+		    // End of block? -> start again from beginning
 		    if ( s == step )
 		    {
 			block++;
 			s = 0;
 		    }
+		    // Set the value of 'cell' by adding 'block' times the delta tp the value
+		    // of cell 's'.
 		    _seqList.at( s )->fillCell( _srcList.at( s ), cell, deltaList.at( s ), block );
+		    // Next cell
 		    cell = _destList.next();
 		    s++;
 		}
@@ -415,6 +449,8 @@ void KSpreadTable::fillSequence( QList<KSpreadCell>& _srcList, QList<KSpreadCell
 	}
     }
 
+    // We did not find any valid interval. So just copy over the marked
+    // area.
     KSpreadCell *cell = _destList.first();
     unsigned int s = 0;
     while ( cell )
