40348b2d664f96f6b674972e6d4fde842e11985f
Torben Weis <weis@kde.org>
2000-07-09 21:43:34 +0000

    Make it compile. The header files changed.
    svn path=/trunk/koffice/; revision=55887

--- killustrator/share/ColorComboBox.h
+++ killustrator/share/ColorComboBox.h
@@ -6,7 +6,7 @@
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU Library General Public License as
-  published by  
+  published by
   the Free Software Foundation; either version 2 of the License, or
   (at your option) any later version.
 
@@ -14,7 +14,7 @@
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU Library General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -24,7 +24,7 @@
 #ifndef ColorComboBox_h_
 #define ColorComboBox_h_
 
-#include <kcolordlg.h>
+#include <kcolorcombo.h>
 
 class ColorComboBox : public KColorCombo {
   Q_OBJECT
