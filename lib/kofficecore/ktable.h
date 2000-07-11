26eb81bbee472cb1f9fe620f0b1b385b572e3a0e
Werner Trobin <trobin@kde.org>
2000-07-11 21:31:48 +0000

    added license here too
    Coolo: I just used cvs log, sorry :)
    
    svn path=/trunk/koffice/; revision=56194

--- lib/kofficecore/ktable.h
+++ lib/kofficecore/ktable.h
@@ -1,3 +1,22 @@
+/* This file is part of the KDE project
+   Copyright (C) 1998, 1999, 2000 Torben Weis <weis@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
 /**
  * $Id$
  */
@@ -178,7 +197,7 @@ public:
   ~KTable() { if ( sh->deref() ) delete sh; }
 
   KTable& operator=( const KTable& t ) { if ( sh->deref() ) delete sh; sh = t.sh; sh->ref(); return *this; }
-    
+
   Iterator begin() { return sh->matrix.begin(); }
   ConstIterator begin() const { return sh->matrix.begin(); }
   Iterator end() { return sh->matrix.end(); }
