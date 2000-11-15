e754182d245e0e41741509bc02e31e4d00af6157
David Faure <faure@kde.org>
2000-11-15 19:20:32 +0000

    setHelpMenuEnabled(false) to prevent KMainWindow from creating a KHelpMenu (which led to double actions in the configure keys dialog). Reported by Tinkl Lukas, of course.
    svn path=/trunk/koffice/; revision=70953

--- lib/kofficecore/koMainWindow.cc
+++ lib/kofficecore/koMainWindow.cc
@@ -168,6 +168,8 @@ KoMainWindow::KoMainWindow( KInstance *instance, const char* name )
     d->m_manager->setAllowNestedParts( true );
     d->m_manager->setIgnoreScrollBars( true );
 
+   setHelpMenuEnabled(false); // we have our own
+
     connect( d->m_manager, SIGNAL( activePartChanged( KParts::Part * ) ),
              this, SLOT( slotActivePartChanged( KParts::Part * ) ) );
 
