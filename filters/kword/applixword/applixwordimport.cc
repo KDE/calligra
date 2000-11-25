46335aed7514c3ec2254ff311b412d86de99a213
Werner Trobin <trobin@kde.org>
2000-11-25 08:25:02 +0000

    First version of an Applixword import filter by Enno Bartels <ebartels@nwn.de>
    svn path=/trunk/koffice/; revision=72067

---
+++ filters/kword/applixword/applixwordimport.cc
@@ -0,0 +1,167 @@
+/* This file is part of the KDE project
+   Copyright (C) 2000 Enno Bartels <ebartels@nwn.de>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include <config.h>
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
+#include <qtextstream.h>
+#include <applixwordimport.h>
+#include <applixwordimport.moc>
+#include <kdebug.h>
+
+APPLIXWORDImport::APPLIXWORDImport(KoFilter *parent, const char *name) :
+                     KoFilter(parent, name) {
+}
+
+const bool APPLIXWORDImport::filter(const QString &fileIn, const QString &fileOut,
+                               const QString& from, const QString& to,
+                               const QString &) {
+
+    if(to!="application/x-kword" || from!="application/x-applix")
+        return false;
+
+    QFile in(fileIn);
+    if(!in.open(IO_ReadOnly)) {
+        kdError(30502) << "Unable to open input file!" << endl;
+        in.close();
+        return false;
+    }
+
+    QString str;
+
+    str += "<?xml version=\"1.0\"?>\n";
+    str += "<DOC  author=\"Reginald Stadlbauer and Torben Weis\" email=\"reggie@kde.org and weis@kde.org\" editor=\"KWord\" mime=\"application/x-kword\">\n";
+    str += "<PAPER format=\"1\" ptWidth=\"595\" ptHeight=\"841\" mmWidth =\"210\" mmHeight=\"297\" inchWidth =\"8.26772\" inchHeight=\"11.6929\" orientation=\"0\" columns=\"1\" ptColumnspc=\"2\" mmColumnspc=\"1\" inchColumnspc=\"0.0393701\" hType=\"0\" fType=\"0\" ptHeadBody=\"9\" ptFootBody=\"9\" mmHeadBody=\"3.5\" mmFootBody=\"3.5\" inchHeadBody=\"0.137795\" inchFootBody=\"0.137795\">\n";
+    str += "<PAPERBORDERS mmLeft=\"10\" mmTop=\"15\" mmRight=\"10\" mmBottom=\"15\" ptLeft=\"28\" ptTop=\"42\" ptRight=\"28\" ptBottom=\"42\" inchLeft=\"0.393701\" inchTop=\"0.590551\" inchRight=\"0.393701\" inchBottom=\"0.590551\"/>\n";
+    str += "</PAPER>\n";
+    str += "<ATTRIBUTES processing=\"0\" standardpage=\"1\" hasHeader=\"0\" hasFooter=\"0\" unit=\"mm\"/>\n";
+    str += "<FRAMESETS>\n";
+    str += "<FRAMESET frameType=\"1\" autoCreateNewFrame=\"1\" frameInfo=\"0\" removeable=\"0\">\n";
+    str += "<FRAME left=\"28\" top=\"42\" right=\"566\" bottom=\"798\" runaround=\"1\" runaGapPT=\"2\" runaGapMM=\"1\" runaGapINCH=\"0.0393701\"  lWidth=\"1\" lRed=\"255\" lGreen=\"255\" lBlue=\"255\" lStyle=\"0\"  rWidth=\"1\" rRed=\"255\" rGreen=\"255\" rBlue=\"255\" rStyle=\"0\"  tWidth=\"1\" tRed=\"255\" tGreen=\"255\" tBlue=\"255\" tStyle=\"0\"  bWidth=\"1\" bRed=\"255\" bGreen=\"255\" bBlue=\"255\" bStyle=\"0\" bkRed=\"255\" bkGreen=\"255\" bkBlue=\"255\" bleftpt=\"0\" bleftmm=\"0\" bleftinch=\"0\" brightpt=\"0\" brightmm=\"0\" brightinch=\"0\" btoppt=\"0\" btopmm=\"0\" btopinch=\"0\" bbottompt=\"0\" bbottommm=\"0\" bbottominch=\"0\"/>\n";
+
+    str += "<PARAGRAPH>\n";
+    str += "<TEXT>";
+   
+    int merker=0;
+    QTextStream stream(&in);
+    int step=in.size()/50;
+    int value=0;
+    int i=0;
+    while(!stream.atEnd())
+    {
+        ++i;
+        QChar c, c1, c2;
+        stream >> c;
+        if ( (merker == 4) && (c == QChar ('\n')) ) 
+        {
+          merker = 3;
+	  /*   str += "</TEXT>\n";
+            str += "</PARAGRAPH>\n";
+            str += "<PARAGRAPH>\n";
+            str += "<TEXT>";*/
+        }
+        if ( (merker == 4) && (c == QChar ('\\')) ) 
+        {
+	  merker = 3;
+        }
+        else if (c == QChar ('<'))  
+        {
+          stream >> c1; 
+          // search for text
+          if (c1 == QChar ('T'))
+	  {
+            merker = 3;
+            stream >> c2;
+            stream >> c2;
+	  }
+          //else  str = str + "&lt; " + c1;
+	}
+        else if ( (merker == 4) && (c == QChar ('"') ) ) 
+        {
+          merker = 0;            
+          str += "</TEXT>\n";
+          str += "</PARAGRAPH>\n";
+          str += "<PARAGRAPH>\n";
+          str += "<TEXT>";
+	}
+        else if ( c == QChar ('&') ) str += "&amp;";
+        else if ( c == QChar ('^') )
+	{
+	  //str += "XX ";
+          stream >> c1;
+          stream >> c2;
+          // printf  ("C1 %c  C2 %c\n", (char ) c1, (char ) c2);
+
+          if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('m') ) )
+            str += "ue"; //"&uuml;";
+          if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('m') ) )
+            str += "Ue"; //"&Uuml;";
+
+          if ( ( c1 == QChar ('o') )  &&  ( c2 == QChar ('e') ) )
+            {str += "ae"; /*"&auml;";*/ printf ("habe ä gefunden\n");}
+
+          if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('g') ) )
+            str += "oe"; //"&ouml;";
+
+          if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('p') ) )
+            str += "sz"; //"&szlig;";
+          
+	  //str += " XX";
+          merker = 3;
+	}
+        //else str += c;
+        
+        if (merker == 4) 
+          str += c;
+
+        if (merker == 3) merker = 4;
+
+        if (i>step) 
+        {
+            i=0;
+            value+=2;
+            emit sigProgress(value);
+        }
+    }
+    emit sigProgress(100);
+
+    str += "</TEXT>\n";
+    str += "</PARAGRAPH>\n";
+    str += "</FRAMESET>\n";
+    str += "</FRAMESETS>\n";
+    str += "</DOC>\n";
+    printf ("Text %s\n", (const char *) str.utf8());
+    KoStore out=KoStore(QString(fileOut), KoStore::Write);
+    if (!out.open("root")) 
+    {
+        kdError(30502) << "Unable to open output file!" << endl;
+        in.close();
+        out.close();
+        return false;
+    }
+    QCString cstring=str.utf8();
+    out.write((const char*)cstring, cstring.length());
+    out.close();
+    in.close();
+    return true;
+}
+
---
+++ filters/kword/applixword/applixwordimport_factory.cc
@@ -0,0 +1,64 @@
+/* This file is part of the KDE project
+   Copyright (C) 2000 Enno Bartels <ebartels@nwn.de>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include "applixwordimport_factory.h"
+#include "applixwordimport_factory.moc"
+#include "applixwordimport.h"
+
+#include <kinstance.h>
+#include <kdebug.h>
+
+extern "C"
+{
+    void* init_libapplixwordimport()
+    {
+        return new APPLIXWORDImportFactory;
+    }
+};
+
+KInstance* APPLIXWORDImportFactory::s_global = 0;
+
+APPLIXWORDImportFactory::APPLIXWORDImportFactory( QObject* parent, const char* name )
+    : KLibFactory( parent, name )
+{
+    s_global = new KInstance( "applixwordimport" );
+}
+
+APPLIXWORDImportFactory::~APPLIXWORDImportFactory()
+{
+    delete s_global;
+}
+
+QObject* APPLIXWORDImportFactory::create( QObject* parent, const char* name, const char*, const QStringList & )
+{
+    if ( parent && !parent->inherits("KoFilter") )
+    {
+	    kdDebug(30502) << "APPLIXWORDImportFactory: parent does not inherit KoFilter" << endl;
+	    return 0L;
+    }
+    APPLIXWORDImport *f = new APPLIXWORDImport( (KoFilter*)parent, name );
+    emit objectCreated(f);
+    return f;
+}
+
+KInstance* APPLIXWORDImportFactory::global()
+{
+    return s_global;
+}
+
