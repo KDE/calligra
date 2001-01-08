2ee2db97a6e5884a27938c5849fc1681c9eccdf6
David Faure <faure@kde.org>
2001-01-08 12:46:55 +0000

    Better use const_cast than a C cast
    svn path=/trunk/koffice/; revision=76919

--- kimageshop/tools/kis_tool_stamp.cc
+++ kimageshop/tools/kis_tool_stamp.cc
@@ -49,7 +49,7 @@ StampTool::~StampTool() {}
 
 void StampTool::setPattern(const KisPattern *pattern)
 {
-    m_pPattern = (KisPattern*)pattern;    
+    m_pPattern = const_cast<KisPattern*>(pattern);    
     
     // use this to establish pattern size and the
     // "hot spot" in center of image, will be the
