f4fdb99c296e3e0b6c28cdf8352018d07563f96a
Lukáš Tinkl <lukas@kde.org>
2001-01-09 15:30:04 +0000

    missing i18n()
    Hmm, same here (and at other places too). I'm not going to fix them all,
    file by file...
    
    svn path=/trunk/koffice/; revision=77123

--- kimageshop/ui/opts_line_dlg.cc
+++ kimageshop/ui/opts_line_dlg.cc
@@ -25,28 +25,28 @@
 #include <qlabel.h>
 #include <qspinbox.h>
 
+#include <klocale.h>
+
 #include "opts_line_dlg.h"
-
-
 
 LineOptionsDialog::LineOptionsDialog( QWidget *parent, const char *name )
     : KDialog( parent, name, true )
 {
-    setCaption( "Current Tool Options" );
+    setCaption( i18n("Current Tool Options") );
 
     QVBoxLayout* layout = new QVBoxLayout( this, 3 );
     QGridLayout* grid = new QGridLayout( layout, 2, 2);
 
     m_thickness = new QSpinBox( 1, 16, 10, this );
     m_thickness->setValue( 1 );
-    QLabel* tlabel = new QLabel( m_thickness, "T&hickness", this );
+    QLabel* tlabel = new QLabel( m_thickness, i18n("T&hickness"), this );
 
     grid->addWidget( tlabel, 0, 0 );
     grid->addWidget( m_thickness, 0, 1 );
 
     m_opacity = new QSpinBox( 0, 255, 10, this );
     m_opacity->setValue( 255 );
-    QLabel* olabel = new QLabel( m_opacity, "&Opacity", this );
+    QLabel* olabel = new QLabel( m_opacity, i18n("&Opacity"), this );
 
     grid->addWidget( olabel, 1, 0 );
     grid->addWidget( m_opacity, 1, 1 );
@@ -55,18 +55,18 @@ LineOptionsDialog::LineOptionsDialog( QWidget *parent, const char *name )
     buttons->addStretch( 3 );
 
     QPushButton *ok, *cancel, *save;
-    ok = new QPushButton( "&OK", this );
+    ok = new QPushButton( i18n("&OK"), this );
     ok->setDefault( true );
     ok->setMinimumSize( ok->sizeHint() );
     connect( ok, SIGNAL(clicked()), SLOT(accept()) );
     buttons->addWidget( ok );
 
-    cancel = new QPushButton( "&Cancel", this );
+    cancel = new QPushButton( i18n("&Cancel"), this );
     cancel->setMinimumSize( cancel->sizeHint() );
     connect( cancel, SIGNAL(clicked()), SLOT(reject()) );
     buttons->addWidget( cancel );
 
-    save = new QPushButton( "&Save", this );
+    save = new QPushButton( i18n("&Save"), this );
     save->setMinimumSize( save->sizeHint() );
     //connect( save, SIGNAL(clicked()), SLOT(reject()) );
     buttons->addWidget( save );
