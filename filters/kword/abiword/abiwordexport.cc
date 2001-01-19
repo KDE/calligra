99fe84502a8910f5f7dd05a52c91c2774521c674
Nicolas Goutte <nicolasg@snafu.de>
2001-01-19 11:27:31 +0000

    Adapting AbiWord filter to the new files processors.cc and processors.h
    svn path=/trunk/koffice/; revision=79007

--- filters/kword/abiword/abiwordexport.cc
+++ filters/kword/abiword/abiwordexport.cc
@@ -1,3 +1,5 @@
+/* $Header$ */
+
 /*
    This file is part of the KDE project
    Copyright (C) 2001 Nicolas GOUTTE <nicog@snafu.de>
@@ -19,7 +21,7 @@
 
 /*
    This file is based on the old file:
-    koffice/filters/kword/ascii/asciiexport.cc
+    /home/kde/koffice/filters/kword/ascii/asciiexport.cc
 
    The old file was copyrighted by 
     Copyright (C) 1998, 1999 Reginald Stadlbauer <reggie@kde.org>
@@ -30,188 +32,16 @@
    License version 2.
 */
 
-/* $Header$ */
-
 #include <abiwordexport.h>
 #include <abiwordexport.moc>
 #include <kdebug.h>
 #include <qdom.h>
 #include <zlib.h>
+#include "processors.h"
 
 ABIWORDExport::ABIWORDExport(KoFilter *parent, const char *name) :
                      KoFilter(parent, name) {
 }
-
-
-// The class TagProcessing and the two functions ProcessSubtags () and
-// AllowNoSubtags () allow for easing parsing of subtags in the
-// current tag. If don't expect any subtags you call AllowNoSubtags ().
-// Otherwise you create a list of TagProcessing elements and pass that
-// to ProcessSubtags () which will go through all subtags it can find,
-// call the corresponding processing function, and do all the
-// necessary error handling.
-
-
-class TagProcessing
-{
-    public:
-        TagProcessing ()
-        {}
-
-        TagProcessing (QString  n,
-                       void     (*p)(QDomNode, void *, QString &),
-                       void    *d) : name (n), processor (p), data (d)
-        {}
-
-        QString  name;
-        void     (*processor)(QDomNode, void *, QString &);
-        void    *data;
-};
-
-
-static void ProcessSubtags ( QDomNode parentNode, QValueList<TagProcessing>  &tagProcessingList, QString &outputText )
-{
-    QDomNode childNode;
-
-    for ( childNode = parentNode.firstChild (); !childNode.isNull (); childNode = childNode.nextSibling () )
-    {
-//      if ( childNode.isElement () )   // doesn't work!!!
-        if ( childNode.nodeType () == QDomNode::ElementNode )
-        {
-            bool found = false;
-
-            QValueList<TagProcessing>::Iterator  tagProcessingIt;
-
-            for ( tagProcessingIt = tagProcessingList.begin ();
-                  !found && tagProcessingIt != tagProcessingList.end ();
-                  tagProcessingIt++ )
-            {
-                if ( childNode.nodeName () == (*tagProcessingIt).name )
-                {
-                    found = true;
-
-                    if ( (*tagProcessingIt).processor != NULL )
-                    {
-                        ((*tagProcessingIt).processor) ( childNode, (*tagProcessingIt).data, outputText );
-                    }
-                    else
-                    {
-//                      kdError () << "<para>ignoring " << childNode.nodeName ()
-//                                      << " tag in " << parentNode.nodeName () << "!</para>" << endl;
-                    }
-                }
-            }
-
-            if ( !found )
-            {
-                kdError(30502) << "Unexpected tag " << childNode.nodeName ()
-                               << " in " << parentNode.nodeName () << "!" << endl;
-            }
-        }
-    }
-}
-
-
-static void AllowNoSubtags ( QDomNode  myNode )
-{
-    QString outputText;
-    QValueList<TagProcessing> tagProcessingList;
-    ProcessSubtags (myNode, tagProcessingList, outputText);
-}
-
-
-// The class AttrProcessing and the two functions ProcessAttributes ()
-// and AllowNoSubtags () allow for easing parsing of the current tag's
-// attributes. If don't expect any attributes you call AllowNoAttributes ().
-// Otherwise you create a list of AttrProcessing elements and pass
-// that to ProcessAttributes () which will go through all attributes
-// it can find, retreive the value in the datatype defined, and do all
-// the necessary error handling.
-
-
-class AttrProcessing
-{
-    public:
-        AttrProcessing ()
-        {}
-
-        AttrProcessing ( QString   n,
-                         QString   t,
-                         void     *d  ) : name (n), type (t), data (d)
-        {}
-
-        QString   name;
-        QString   type;
-        void     *data;
-};
-
-
-static void ProcessAttributes ( QDomNode myNode, QValueList<AttrProcessing> &attrProcessingList )
-{
-    QDomNamedNodeMap myAttribs ( myNode.toElement ().attributes () );
-
-    int i, n;
-    n = myAttribs.length ();
-
-    for ( i = 0; i < n; i++ )
-    {
-        QDomAttr myAttrib ( myAttribs.item (i).toAttr () );
-
-        if ( !myAttrib.isNull () )
-        {
-            bool found = false;
-
-            QValueList<AttrProcessing>::Iterator attrProcessingIt;
-
-            for ( attrProcessingIt = attrProcessingList.begin ();
-                  !found && attrProcessingIt != attrProcessingList.end ();
-                  attrProcessingIt++ )
-            {
-                if ( myAttrib.name () == (*attrProcessingIt).name )
-                {
-                    found = true;
-
-                    if ( (*attrProcessingIt).data != NULL )
-                    {
-                        if ( (*attrProcessingIt).type == "QString" )
-                        {
-                            *((QString *) (*attrProcessingIt).data) = myAttrib.value ();
-                        }
-                        else if ( (*attrProcessingIt).type == "int" )
-                        {
-                            *((int *) (*attrProcessingIt).data) = myAttrib.value ().toInt ();
-                        }
-                        else
-                        {
-                            kdError() << "Unexpected data type " << (*attrProcessingIt).type << " in " <<
-                                           myNode.nodeName () << " attribute " << (*attrProcessingIt).name
-                                           << "!" << endl;
-                        }
-                    }
-                    else
-                    {
-//                      kdError() << "<para>ignoring " << myNode.tagName << " attribute "
-//                                     << (*attrProcessingIt).name << "!</para>" << endl;
-                    }
-                }
-            }
-
-            if ( !found )
-            {
-                kdError() << "Unexpected attribute " << myAttrib.name () << " in " <<
-                                  myNode.nodeName () << "!" << endl;
-            }
-        }
-    }
-}
-
-
-static void AllowNoAttributes ( QDomNode  myNode )
-{
-    QValueList<AttrProcessing> attrProcessingList;
-    ProcessAttributes (myNode, attrProcessingList);
-}
-
 
 // Every tag has its own processing function. All of those functions
 // have the same parameters since the functions are passed to
@@ -634,7 +464,7 @@ static bool writeOutputFileUncompressed(const QString& filename, const QCString&
     {
         fileOut.close ();
 
-        kdError(30502) << "Unable to open output file!" << endl;
+        kdError() << "Unable to open output file!" << endl;
         return false;
     }
 
@@ -669,7 +499,7 @@ static bool writeOutputFileGZipped(const QString& filename, const QCString& strO
     if (num != int(strOut.length()))
     { //We have a problem
     	gzclose (fileOut); //Close the file
-        kdError() << "Unable to write gzipped output file!" << num << "/" << strOut.length() << endl;
+        kdError() << "Unable to write gzipped output file! " << num << " instead of " << strOut.length() << endl;
     	return false;
     }
 
