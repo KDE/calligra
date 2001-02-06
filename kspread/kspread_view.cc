98e138debbe04c4e5fecb1a7cb8fd62548e1d1e5
Laurent Montel <montel@kde.org>
2001-02-06 14:13:11 +0000

    Don't overwritting print parameter store in kspread file
    svn path=/trunk/koffice/; revision=81558

--- kspread/kspread_view.cc
+++ kspread/kspread_view.cc
@@ -494,10 +494,14 @@ if( config->hasGroup("Parameters" ))
 	m_pDoc->changeDefaultGridPenColor(_col);
 	m_pDoc->setShowMessageError(config->readBoolEntry( "Msg error" ,false) );
 	
+	if( m_pDoc->isEmpty())
+	  {
+	    m_pDoc->setPaperFormat((KoFormat)config->readNumEntry("Default size page",0));
+	    
+	    m_pDoc->setPaperOrientation((KoOrientation)config->readNumEntry("Default orientation page",0));
+	    
+	  }
 	m_pDoc->setShowCommentIndicator(config->readBoolEntry("Comment Indicator",true));
-
-	m_pDoc->setPaperFormat((KoFormat)config->readNumEntry("Default size page",1));
-	m_pDoc->setPaperOrientation((KoOrientation)config->readNumEntry("Default orientation page",0));
 	
 	m_pDoc->setShowFormularBar(config->readBoolEntry("Formula bar",true));
 	}
