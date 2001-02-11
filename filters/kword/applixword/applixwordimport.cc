f3fe6f6580f19d87fe00392c225d593de4ce1c59
Enno Bartels <ebartels@nwn.de>
2001-02-11 14:12:37 +0000

    Now the importfilter can convert all special characters
    svn path=/trunk/koffice/; revision=82367

--- filters/kword/applixword/applixwordimport.cc
+++ filters/kword/applixword/applixwordimport.cc
@@ -29,13 +29,16 @@
 #include <kdebug.h>
 
 APPLIXWORDImport::APPLIXWORDImport(KoFilter *parent, const char *name) :
-                     KoFilter(parent, name) {
+                     KoFilter(parent, name) 
+{
 }
 
 const bool APPLIXWORDImport::filter(const QString &fileIn, const QString &fileOut,
                                const QString& from, const QString& to,
-                               const QString &) {
+                               const QString &) 
+{
 
+//  if(to!="application/x-kword" || from!="application/x-applixword")
     if(to!="application/x-kword" || from!="application/x-applix")
         return false;
 
@@ -63,27 +66,25 @@ const bool APPLIXWORDImport::filter(const QString &fileIn, const QString &fileOu
    
     int merker=0;
     QTextStream stream(&in);
-    int step=in.size()/50;
-    int value=0;
-    int i=0;
-    while(!stream.atEnd())
+    int step = in.size()/50;
+    int value= 0;
+    int i = 0;
+    while (!stream.atEnd())
     {
         ++i;
         QChar c, c1, c2;
         stream >> c;
+
         if ( (merker == 4) && (c == QChar ('\n')) ) 
         {
           merker = 3;
-	  /*   str += "</TEXT>\n";
-            str += "</PARAGRAPH>\n";
-            str += "<PARAGRAPH>\n";
-            str += "<TEXT>";*/
         }
+
         if ( (merker == 4) && (c == QChar ('\\')) ) 
         {
 	  merker = 3;
         }
-        else if (c == QChar ('<'))  
+        else if ((merker == 0) && c == QChar ('<'))  
         {
           stream >> c1; 
           // search for text
@@ -93,7 +94,16 @@ const bool APPLIXWORDImport::filter(const QString &fileIn, const QString &fileOu
             stream >> c2;
             stream >> c2;
 	  }
-          //else  str = str + "&lt; " + c1;
+	}
+        else if ((merker == 4) && c == QChar ('<'))  
+        {
+          str = str + "&lt; ";
+          merker = 3;
+	}
+        else if ((merker == 4) && c == QChar ('>'))  
+        {
+          str = str + "&gt; ";
+          merker = 3;
 	}
         else if ( (merker == 4) && (c == QChar ('"') ) ) 
         {
@@ -106,29 +116,179 @@ const bool APPLIXWORDImport::filter(const QString &fileIn, const QString &fileOu
         else if ( c == QChar ('&') ) str += "&amp;";
         else if ( c == QChar ('^') )
 	{
-	  //str += "XX ";
           stream >> c1;
           stream >> c2;
-          // printf  ("C1 %c  C2 %c\n", (char ) c1, (char ) c2);
 
-          if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('m') ) )
-            str += "ue"; //"&uuml;";
-          if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('m') ) )
-            str += "Ue"; //"&Uuml;";
+          if      ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('p') ) ) str += "ß"; 
 
-          if ( ( c1 == QChar ('o') )  &&  ( c2 == QChar ('e') ) )
-            {str += "ae"; /*"&auml;";*/ printf ("habe ä gefunden\n");}
 
-          if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('g') ) )
-            str += "oe"; //"&ouml;";
+          else if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('c') ) ) str += "Ò"; 
+          else if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('c') ) ) str += "ò"; 
 
-          if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('p') ) )
-            str += "sz"; //"&szlig;";
-          
-	  //str += " XX";
+          else if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('d') ) ) str += "Ó"; 
+          else if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('d') ) ) str += "ó"; 
+
+          else if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('e') ) ) str += "Ô"; 
+          else if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('e') ) ) str += "ô"; 
+
+          else if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('f') ) ) str += "Õ"; 
+          else if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('f') ) ) str += "õ"; 
+
+          else if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('g') ) ) str += "ö"; 
+          else if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('g') ) ) str += "Ö"; 
+
+
+
+          else if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('j') ) ) str += "Ù"; 
+          else if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('j') ) ) str += "ù"; 
+
+          else if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('k') ) ) str += "Ú"; 
+          else if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('k') ) ) str += "ú"; 
+
+          else if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('l') ) ) str += "Û"; 
+          else if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('l') ) ) str += "û"; 
+
+          else if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('m') ) ) str += "ü"; 
+          else if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('m') ) ) str += "Ü";
+
+
+
+          else if ( ( c1 == QChar ('m') )  &&  ( c2 == QChar ('a') ) ) str += "À"; 
+          else if ( ( c1 == QChar ('o') )  &&  ( c2 == QChar ('a') ) ) str += "à"; 
+
+          else if ( ( c1 == QChar ('m') )  &&  ( c2 == QChar ('b') ) ) str += "Á"; 
+          else if ( ( c1 == QChar ('o') )  &&  ( c2 == QChar ('b') ) ) str += "á"; 
+
+          else if ( ( c1 == QChar ('m') )  &&  ( c2 == QChar ('c') ) ) str += "Â"; 
+          else if ( ( c1 == QChar ('o') )  &&  ( c2 == QChar ('c') ) ) str += "â"; 
+
+          else if ( ( c1 == QChar ('m') )  &&  ( c2 == QChar ('d') ) ) str += "Ã"; 
+          else if ( ( c1 == QChar ('o') )  &&  ( c2 == QChar ('d') ) ) str += "ã"; 
+
+          else if ( ( c1 == QChar ('m') )  &&  ( c2 == QChar ('e') ) ) str += "Ä"; 
+          else if ( ( c1 == QChar ('o') )  &&  ( c2 == QChar ('e') ) ) str += "ä"; 
+
+          else if ( ( c1 == QChar ('m') )  &&  ( c2 == QChar ('f') ) ) str += "Å"; 
+          else if ( ( c1 == QChar ('o') )  &&  ( c2 == QChar ('f') ) ) str += "å"; 
+
+          else if ( ( c1 == QChar ('m') )  &&  ( c2 == QChar ('g') ) ) str += "Æ"; 
+          else if ( ( c1 == QChar ('o') )  &&  ( c2 == QChar ('g') ) ) str += "æ"; 
+
+
+
+          else if ( ( c1 == QChar ('m') )  &&  ( c2 == QChar ('i') ) ) str += "È"; 
+          else if ( ( c1 == QChar ('o') )  &&  ( c2 == QChar ('i') ) ) str += "è"; 
+
+          else if ( ( c1 == QChar ('m') )  &&  ( c2 == QChar ('j') ) ) str += "É"; 
+          else if ( ( c1 == QChar ('o') )  &&  ( c2 == QChar ('j') ) ) str += "é"; 
+
+          else if ( ( c1 == QChar ('m') )  &&  ( c2 == QChar ('k') ) ) str += "Ê"; 
+          else if ( ( c1 == QChar ('o') )  &&  ( c2 == QChar ('k') ) ) str += "ê";
+ 
+          else if ( ( c1 == QChar ('m') )  &&  ( c2 == QChar ('l') ) ) str += "Ë"; 
+          else if ( ( c1 == QChar ('o') )  &&  ( c2 == QChar ('l') ) ) str += "ë"; 
+
+
+
+
+
+
+          else if ( ( c1 == QChar ('m') )  &&  ( c2 == QChar ('m') ) ) str += "Ì"; 
+          else if ( ( c1 == QChar ('o') )  &&  ( c2 == QChar ('m') ) ) str += "ì"; 
+
+          else if ( ( c1 == QChar ('m') )  &&  ( c2 == QChar ('n') ) ) str += "Í"; 
+          else if ( ( c1 == QChar ('o') )  &&  ( c2 == QChar ('n') ) ) str += "í"; 
+
+          else if ( ( c1 == QChar ('m') )  &&  ( c2 == QChar ('o') ) ) str += "Î"; 
+          else if ( ( c1 == QChar ('o') )  &&  ( c2 == QChar ('o') ) ) str += "î"; 
+
+          else if ( ( c1 == QChar ('m') )  &&  ( c2 == QChar ('p') ) ) str += "Ï"; 
+          else if ( ( c1 == QChar ('o') )  &&  ( c2 == QChar ('p') ) ) str += "ï"; 
+
+
+          else if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('b') ) ) str += "Ñ"; 
+          else if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('b') ) ) str += "ñ"; 
+
+
+          else if ( ( c1 == QChar ('k') )  &&  ( c2 == QChar ('c') ) ) str += "¢"; 
+          else if ( ( c1 == QChar ('k') )  &&  ( c2 == QChar ('j') ) ) str += "©"; 
+          else if ( ( c1 == QChar ('l') )  &&  ( c2 == QChar ('f') ) ) str += "µ"; 
+          else if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('i') ) ) str += "Ø"; 
+          else if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('i') ) ) str += "ø"; 
+
+          else if ( ( c1 == QChar ('l') )  &&  ( c2 == QChar ('j') ) ) str += "¹"; 
+          else if ( ( c1 == QChar ('l') )  &&  ( c2 == QChar ('c') ) ) str += "²"; 
+          else if ( ( c1 == QChar ('l') )  &&  ( c2 == QChar ('d') ) ) str += "³"; 
+
+          else if ( ( c1 == QChar ('l') )  &&  ( c2 == QChar ('m') ) ) str += "¼"; 
+          else if ( ( c1 == QChar ('l') )  &&  ( c2 == QChar ('n') ) ) str += "½"; 
+          else if ( ( c1 == QChar ('l') )  &&  ( c2 == QChar ('o') ) ) str += "¾"; 
+
+          else if ( ( c1 == QChar ('l') )  &&  ( c2 == QChar ('a') ) ) str += "°"; 
+
+          else if ( ( c1 == QChar ('k') )  &&  ( c2 == QChar ('o') ) ) str += "®"; 
+          else if ( ( c1 == QChar ('k') )  &&  ( c2 == QChar ('h') ) ) str += "§"; 
+          else if ( ( c1 == QChar ('k') )  &&  ( c2 == QChar ('d') ) ) str += "£"; 
+
+          else if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('a') ) ) str += "ð"; 
+          else if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('a') ) ) str += "Ð"; 
+
+          else if ( ( c1 == QChar ('l') )  &&  ( c2 == QChar ('l') ) ) str += "»"; 
+          else if ( ( c1 == QChar ('k') )  &&  ( c2 == QChar ('l') ) ) str += "«"; 
+
+          else if ( ( c1 == QChar ('l') )  &&  ( c2 == QChar ('k') ) ) str += "º"; 
+
+          else if ( ( c1 == QChar ('l') )  &&  ( c2 == QChar ('h') ) ) str += "·"; 
+
+          else if ( ( c1 == QChar ('k') )  &&  ( c2 == QChar ('b') ) ) str += "¡"; 
+
+          else if ( ( c1 == QChar ('k') )  &&  ( c2 == QChar ('e') ) ) str += "¤"; 
+
+          else if ( ( c1 == QChar ('l') )  &&  ( c2 == QChar ('b') ) ) str += "±"; 
+
+          else if ( ( c1 == QChar ('l') )  &&  ( c2 == QChar ('p') ) ) str += "¿"; 
+
+          else if ( ( c1 == QChar ('k') )  &&  ( c2 == QChar ('f') ) ) str += "¥"; 
+
+          else if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('o') ) ) str += "þ";
+          else if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('o') ) ) str += "Þ";
+
+          else if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('n') ) ) str += "Ý"; 
+          else if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('n') ) ) str += "ý"; 
+          else if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('p') ) ) str += "ÿ"; 
+
+          else if ( ( c1 == QChar ('k') )  &&  ( c2 == QChar ('k') ) ) str += "ª"; 
+
+          else if ( ( c1 == QChar ('k') )  &&  ( c2 == QChar ('m') ) ) str += "¬"; 
+          else if ( ( c1 == QChar ('p') )  &&  ( c2 == QChar ('h') ) ) str += "÷";
+ 
+          else if ( ( c1 == QChar ('k') )  &&  ( c2 == QChar ('g') ) ) str += "|"; 
+
+          else if ( ( c1 == QChar ('l') )  &&  ( c2 == QChar ('e') ) ) str += "'"; 
+
+          else if ( ( c1 == QChar ('k') )  &&  ( c2 == QChar ('i') ) ) str += "¨"; 
+
+          else if ( ( c1 == QChar ('k') )  &&  ( c2 == QChar ('n') ) ) str += "­"; 
+
+          else if ( ( c1 == QChar ('k') )  &&  ( c2 == QChar ('p') ) ) str += "¯"; 
+
+          else if ( ( c1 == QChar ('l') )  &&  ( c2 == QChar ('g') ) ) str += "¶"; 
+
+          else if ( ( c1 == QChar ('l') )  &&  ( c2 == QChar ('i') ) ) str += "¸"; 
+
+          else if ( ( c1 == QChar ('m') )  &&  ( c2 == QChar ('h') ) ) str += "Ç"; 
+          else if ( ( c1 == QChar ('o') )  &&  ( c2 == QChar ('h') ) ) str += "ç"; 
+
+          else if ( ( c1 == QChar ('n') )  &&  ( c2 == QChar ('h') ) ) str += "×"; 
+
+          else if ( ( c1 == QChar ('k') )  &&  ( c2 == QChar ('a') ) ) str += " "; 
+
+          else if ( ( c1 == QChar ('a') )  &&  ( c2 == QChar ('j') ) ) str += "  &lt;TAB&gt;  "; 
+
+          else  str = str + "&lt; unknown_applixword_special_character: ^" + c1 + c2 + " &gt;";
+              
           merker = 3;
 	}
-        //else str += c;
         
         if (merker == 4) 
           str += c;
@@ -150,18 +310,21 @@ const bool APPLIXWORDImport::filter(const QString &fileIn, const QString &fileOu
     str += "</FRAMESETS>\n";
     str += "</DOC>\n";
     printf ("Text %s\n", (const char *) str.utf8());
-    KoStore out=KoStore(QString(fileOut), KoStore::Write);
-    if (!out.open("root")) 
+    KoStore out = KoStore (QString(fileOut), KoStore::Write);
+    if (!out.open ("root")) 
     {
         kdError(30502) << "Unable to open output file!" << endl;
-        in.close();
-        out.close();
+        in.close  ();
+        out.close ();
         return false;
     }
-    QCString cstring=str.utf8();
-    out.write((const char*)cstring, cstring.length());
-    out.close();
-    in.close();
+
+    QCString cstring = str.utf8 ();
+
+    out.write ((const char*)cstring, cstring.length());
+
+    out.close ();
+    in.close ();
     return true;
 }
 
