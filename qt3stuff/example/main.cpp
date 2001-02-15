a750df1e67d4de31710178a23e48f4402c6e272c
David Faure <faure@kde.org>
2001-02-15 23:55:49 +0000

    Example copied here so that it compiles against the qt3stuff. However, it segfaults on startup, in QScrollView (!?!?!?!).
    svn path=/trunk/koffice/; revision=83042

---
+++ qt3stuff/example/main.cpp
@@ -0,0 +1,25 @@
+/****************************************************************************
+** $Id$
+**
+** Copyright (C) 1992-2000 Trolltech AS.  All rights reserved.
+**
+** This file is part of an example program for Qt.  This example
+** program may be used, distributed and modified without limitation.
+**
+*****************************************************************************/
+
+#include "richtext.h"
+#include <qapplication.h>
+
+int main( int argc, char **argv )
+{
+    QApplication a( argc, argv );
+
+    MyRichText richtext;
+    richtext.resize( 450, 350 );
+    richtext.setCaption( "Fortune" );
+    a.setMainWidget( &richtext );
+    richtext.show();
+
+    return a.exec();
+}
---
+++ qt3stuff/example/richtext.h
@@ -0,0 +1,37 @@
+/****************************************************************************
+** $Id$
+**
+** Copyright (C) 1992-2000 Trolltech AS.  All rights reserved.
+**
+** This file is part of an example program for Qt.  This example
+** program may be used, distributed and modified without limitation.
+**
+*****************************************************************************/
+
+#ifndef RICHTEXT_H
+#define RICHTEXT_H
+
+#include <qvbox.h>
+
+class QTextView;
+class QPushButton;
+
+class MyRichText : public QVBox
+{
+    Q_OBJECT
+
+public:
+    MyRichText( QWidget *parent = 0, const char *name = 0 );
+
+protected:
+    QTextView *view;
+    QPushButton *bClose, *bNext, *bPrev;
+    int num;
+
+protected slots:
+    void prev();
+    void next();
+
+};
+
+#endif
