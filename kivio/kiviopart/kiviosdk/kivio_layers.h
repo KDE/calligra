1357be2a9db1443c8533304aa2d01836948f9f35
Thomas Leitner <tom@radar.tu-graz.ac.at>
2001-03-13 14:42:48 +0000

    fixed compiler warning for Compaq C++
    svn path=/trunk/koffice/; revision=86766

--- kivio/kiviopart/kiviosdk/kivio_layers.h
+++ kivio/kiviopart/kiviosdk/kivio_layers.h
@@ -29,7 +29,7 @@
   void insertLayer( int i, KivioLayer *l ) { page->insertLayer(i,l); }
   KivioLayer *layerAt( int i ) { return page->layerAt(i); }
 
-  bool addStencil( KivioStencil *s ) { page->addStencil(s); }
+  bool addStencil( KivioStencil *s ) { page->addStencil(s); return TRUE; }
 
   void deleteSelectedStencils() { page->deleteSelectedStencils(); }
   void groupSelectedStencils() { page->groupSelectedStencils(); }
