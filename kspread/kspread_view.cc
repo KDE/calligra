4c7118580a3094ccb8df10db4434685e5411fc4e
Laurent Montel <montel@kde.org>
2001-03-26 09:33:36 +0000

    Update dtd
    svn path=/trunk/koffice/; revision=88718

--- kspread/kspread_view.cc
+++ kspread/kspread_view.cc
@@ -164,14 +164,10 @@ KSpreadView::KSpreadView( QWidget *_parent, const char *_name, KSpreadDoc* doc )
 
     // Edit Bar
     m_pToolWidget = new QFrame( this );
-    // m_pToolWidget->setFrameStyle( 49 );
 
     QHBoxLayout* hbox = new QHBoxLayout( m_pToolWidget );
     hbox->addSpacing( 2 );
 
-    //m_pPosWidget = new QLabel( m_pToolWidget );
-    //m_pPosWidget->setAlignment( AlignCenter );
-    //m_pPosWidget->setFrameStyle( QFrame::WinPanel|QFrame::Sunken );
     m_pPosWidget = new KSpreadLocationEditWidget( m_pToolWidget, this );
 
     m_pPosWidget->setMinimumWidth( 100 );
