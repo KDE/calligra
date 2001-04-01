9f9058a91b420c6bbd7b26fd758687c2794bfd7f
David Faure <faure@kde.org>
2001-04-01 02:47:25 +0000

    The best way to prevent the ksciplot-fiasco (3rd-party app that includes koView.h can't link) is to #error if the presence of kdeprint wasn't tested by configure. At least it will give a better hint to koffice-app-developers than a mysterious link error. Enough for today :)
    svn path=/trunk/koffice/; revision=89702

--- lib/kofficecore/koprinter.h
+++ lib/kofficecore/koprinter.h
@@ -3,6 +3,10 @@
 
 #include <config.h>
 
+#ifndef HAVE_KDEPRINT_TESTED
+#error "The presence of kdeprint wasn't tested by configure. If you are compiling KOffice itself, rerun configure. If you are compiling another KOffice application, add a test for it (see koffice/lib/configure.in.in)."
+#endif
+
 #ifdef HAVE_KDEPRINT
 #include <kprinter.h>
 #else
