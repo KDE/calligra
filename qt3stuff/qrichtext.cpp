bd715670ba46e3cbe8eaa6c49e1213e12ea01d1c
David Faure <faure@kde.org>
2001-04-02 18:18:14 +0000

    Sick of this $Id$ tag that forces a recompilation after each commit
    svn path=/trunk/koffice/; revision=89979

--- qt3stuff/qrichtext.cpp
+++ qt3stuff/qrichtext.cpp
@@ -1,6 +1,4 @@
 /****************************************************************************
-** $Id$
-**
 ** Implementation of the internal Qt classes dealing with rich text
 **
 ** Created : 990101
