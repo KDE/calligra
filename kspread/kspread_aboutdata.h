10205a96c5fe5e33690e94e859f802a44303611c
David Faure <faure@kde.org>
2001-04-19 13:43:15 +0000

    1.1 (beta1), laurent said ok
    svn path=/trunk/koffice/; revision=92875

--- kspread/kspread_aboutdata.h
+++ kspread/kspread_aboutdata.h
@@ -24,7 +24,7 @@
 #include <klocale.h>
 
 static const char* description=I18N_NOOP("KOffice Spreadsheet Application");
-static const char* version="1.1";
+static const char* version="1.1 (beta1)";
 
 KAboutData * newKSpreadAboutData()
 {
