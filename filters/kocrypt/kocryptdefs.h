ebf93b6bc796aea0383f27cad2b9b03f7cfe6fb3
George Staikos <staikos@kde.org>
2001-04-20 06:32:33 +0000

    I guess i need this one too since I use it in the .cc file.
    svn path=/trunk/koffice/; revision=92996

---
+++ filters/kocrypt/kocryptdefs.h
@@ -0,0 +1,42 @@
+/* This file is part of the KDE project
+   Copyright (C) 2001 George Staikos <staikos@kde.org>
+ 
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+ 
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+ 
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+
+#ifndef __kocryptdefs_h
+#define __kocryptdefs_h
+
+// These _never_ change!
+#define FID_FIRST   0x0d 
+#define FID_SECOND  0x1a 
+#define FID_THIRD   0x27
+
+// The original version of the file format
+#define FID_FVER    0x00
+
+#define 
+// don't define an APPID 0
+#define APPID_KWORD         1
+#define APPID_KSPREAD       2
+#define APPID_KPRESENTER    3
+
+
+
+
+
+#endif
