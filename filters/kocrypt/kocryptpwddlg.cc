b229d927cedc73bcdfc3505c21d0913601590032
George Staikos <staikos@kde.org>
2001-04-23 21:11:41 +0000

    These can go into cvs now.  The dlg is still not done but the factory is.
    svn path=/trunk/koffice/; revision=93648

---
+++ filters/kocrypt/kocryptpwddlg.cc
@@ -0,0 +1,50 @@
+/* This file is part of the KDE project
+   Copyright (C) 2001 George Staikos <staikos@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include <config.h>
+#include <qstring.h>
+
+#include "kocryptpwddlg.h"
+
+
+
+KoCryptPwdDlg::KoCryptPwdDlg(QWidget *parent, const char *name) 
+: KoFilterDialog(parent, name) {
+   _password = "";
+}
+
+
+KoCryptPwdDlg::~KoCryptPwdDlg() {
+
+}
+
+
+QString KoCryptPwdDlg::password() {
+   return _password;
+}
+
+
+QString KoCryptPwdDlg::state() {
+return "";
+}
+
+
+
+#include "kocryptpwddlg.moc"
+
