910b250caabb19f3e5054331fa1736396b57dcc0
David Faure <faure@kde.org>
2001-05-07 10:16:10 +0000

    Thanks to Thomas Leitner's CXX for that one :)
    svn path=/trunk/koffice/; revision=95599

--- qt3stuff/qstylesheet.cpp
+++ qt3stuff/qstylesheet.cpp
@@ -577,7 +577,7 @@ void QStyleSheetItem::setMargin(Margin m, int v)
 	d->margin[1] = v;
 	d->margin[2] = v;
 	d->margin[3] = v;
-	d->margin[5] = v;
+	d->margin[4] = v;
     } else if (m == MarginVertical ) {
 	d->margin[MarginTop] = v;
 	d->margin[MarginBottom] = v;
