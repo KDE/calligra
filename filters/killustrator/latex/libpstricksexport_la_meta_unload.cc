86ffe6f71777ceda14e70c4f81dbe7dead0bc79a
Laurent Montel <montel@kde.org>
2001-07-14 14:51:25 +0000

    remove these files (not necessary in cvs).
    svn path=/trunk/koffice/; revision=106080

--- filters/killustrator/latex/libpstricksexport_la_meta_unload.cc
+++
@@ -1,6 +0,0 @@
-static const char * _metalist_libpstricksexport_la[] = {
-"PSTRICKSExport",
-"PSTRICKSExportFactory",
-0};
-#include <kunload.h>
-_UNLOAD(libpstricksexport_la)
--- filters/killustrator/latex/libtexgraphexport_la_meta_unload.cc
+++
@@ -1,6 +0,0 @@
-static const char * _metalist_libtexgraphexport_la[] = {
-"TEXGRAPHExport",
-"TEXGRAPHExportFactory",
-0};
-#include <kunload.h>
-_UNLOAD(libtexgraphexport_la)
