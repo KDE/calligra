4d5673c569aaf2f2d958d405e45aff4e2b5f1166
Laurent Montel <montel@kde.org>
2001-08-13 07:29:24 +0000

    --- kspread_aboutdata.h 2001/07/19 12:25:40     1.8 +++ kspread_aboutdata.h 2001/08/13 07:29:08 @@ -24,7 +24,7 @@  #include <klocale.h>
     static const char* description=I18N_NOOP("KOffice Spreadsheet
    Application");
    -static const char* version="1.1 (RC1)";
    +static const char* version="1.1";
    
    svn path=/branches/KOFFICE_1_1_BRANCH/koffice/; revision=110248

---
+++ kspread/kspread_aboutdata.h
@@ -0,0 +1,41 @@
+/* This file is part of the KDE project
+   Copyright (C) 1998, 1999 Torben Weis <weis@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef KSPREAD_ABOUTDATA
+#define KSPREAD_ABOUTDATA
+
+#include <kaboutdata.h>
+#include <klocale.h>
+
+static const char* description=I18N_NOOP("KOffice Spreadsheet Application");
+static const char* version="1.1";
+
+KAboutData * newKSpreadAboutData()
+{
+    KAboutData * aboutData = new KAboutData( "kspread", I18N_NOOP("KSpread"),
+                                             version, description, KAboutData::License_GPL,
+                                             I18N_NOOP("(c) 1998-2001, The KSpread Team"), 0,
+					     "http://www.koffice.org/kspread/");
+    aboutData->addAuthor("Torben Weis",0, "weis@kde.org");
+    aboutData->addAuthor("Laurent Montel",0, "lmontel@mandrakesoft.com");
+    aboutData->addAuthor("David Faure",0, "faure@kde.org");
+    return aboutData;
+}
+
+#endif
