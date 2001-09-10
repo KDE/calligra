a154507eb0a6c48bbb00f26bd173927725b4cd90
Ulrich Kuettler <ulrich.kuettler@gmx.de>
2001-09-10 20:45:29 +0000

    The missing files. (sorry)
    svn path=/trunk/koffice/; revision=113959

---
+++ kformula/kfconfig.cc
@@ -0,0 +1,62 @@
+/* This file is part of the KDE project
+   Copyright (C) 2001 Andrea Rizzi <rizzi@kde.org>
+	              Ulrich Kuettler <ulrich.kuettler@mailbox.tu-dresden.de>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include <kiconloader.h>
+#include <kinstance.h>
+#include <klocale.h>
+
+#include "kfconfig.h"
+#include "kformula_factory.h"
+#include "kformula_doc.h"
+#include "kformula_view.h"
+
+
+KFConfig::KFConfig( KFormulaPartView* parent )
+    : KDialogBase( KDialogBase::IconList, i18n( "Configure KFormula" ),
+                   KDialogBase::Ok | KDialogBase::Cancel | KDialogBase::Default,
+                   KDialogBase::Ok )
+{
+    QVBox* page = addVBoxPage( i18n( "Formula" ), i18n( "Formula settings" ),
+                               BarIcon( "settings", KIcon::SizeMedium ) );
+    _page = new KFormula::ConfigurePage( parent->document()->getDocument(),
+                                         KFormulaFactory::global()->config(),
+                                         page );
+
+    connect( this, SIGNAL( okClicked() ), this, SLOT( slotApply() ) );
+}
+
+void KFConfig::slotApply()
+{
+    _page->apply();
+}
+
+void KFConfig::slotDefault()
+{
+    switch(activePageIndex())
+    {
+    case 0:
+        _page->slotDefault();
+        break;
+    default:
+        break;
+    }
+}
+
+#include "kfconfig.moc"
