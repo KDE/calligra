3be95c8c63119b485672dce48720bf816dd5fcbd
Nicolas Goutte <nicolasg@snafu.de>
2001-09-15 21:02:17 +0000

    Change copyright holder
    svn path=/trunk/koffice/; revision=114392

--- filters/kword/html/import/htmlimport.cpp
+++ filters/kword/html/import/htmlimport.cpp
@@ -1,6 +1,6 @@
 /* This file is part of the KDE project
    Copyright (C) 1998, 1999 Reginald Stadlbauer <reggie@kde.org>
-   Copyright (C) 2001 Nicolas GOUTTE <nicog@snafu.de>
+   Copyright (C) 2001 Frank Dekervel <Frank.Dekervel@student.kuleuven.ac.be>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
