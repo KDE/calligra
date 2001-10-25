89fd0c3d33155c6c543fa5feca5d89e7b647c19e
Nicolas Goutte <nicolasg@snafu.de>
2001-10-25 21:01:44 +0000

    "New" simplified HTML export filter for KWord: - compared to the previous one, it has just one mode   (the former "Full Power" one.) - can save not only in UTF8 but also in the local encoding.
    Reasons for giving up the modes:
    - users were unaware which mode they should take (no real description anywhere)
    - the filter has reached its limit for what can be described only in HTML,
      future enhancements need CSS2 too.
    
    svn path=/trunk/koffice/; revision=119395

---
+++ filters/kword/html/export/ExportFilter.h
@@ -0,0 +1,57 @@
+// $Header$
+
+/*
+   This file is part of the KDE project
+   Copyright (C) 2001 Nicolas GOUTTE <nicog@snafu.de>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef EXPORTFILTERFULLPOWER_H
+#define EXPORTFILTERFULLPOWER_H
+
+class ClassExportFilterHtml : public KWEFBaseClass
+{
+    public:
+        ClassExportFilterHtml (void) {}
+        virtual ~ClassExportFilterHtml (void) {}
+    public:
+        virtual bool isXML  (void) const { return m_xml; } // TODO: make it inline when not defined anymore in KWEFBaseClass
+        inline bool isUTF8 (void) const { return m_utf8; }
+        inline void setXML (const bool flag ) { m_xml=flag; }
+        inline void setUTF8 (const bool flag ) { m_utf8=flag; }
+    public: //virtual
+        virtual QString escapeText(const QString& str) const;
+        virtual QString getHtmlOpeningTagExtraAttributes(void) const;
+        virtual bool filter(const QString  &filenameIn, const QString  &filenameOut);
+        virtual QString getDocType(void) const;
+        virtual QString getBodyOpeningTagExtraAttributes(void) const;
+        virtual void ProcessParagraphData ( QString &paraText, ValueListFormatData &paraFormatDataList, QString &outputText);
+        virtual QString processDocTagStylesOnly(QDomElement myNode);
+        virtual QString getStartOfListOpeningTag(const CounterData::Style typeList, bool& ordered);
+        virtual QString getParagraphElement(const QString& strTag, const QString& strParagraphText, LayoutData& layout);
+        virtual void processStyleTag (QDomNode myNode, void * tagData, QString &strStyles);
+    protected:
+        QString escapeCssIdentifier(const QString& strText) const;
+        QString layoutToCss(LayoutData& layout) const;
+    protected:
+        virtual void helpStyleProcessing(QDomNode myNode,LayoutData* layout);
+    protected:
+        bool m_utf8;
+        bool m_xml;
+};
+
+#endif /* EXPORTFILTERFULLPOWER_H */
