0aa905a184978cd2b6b6ca59b8c87ef899101e2d
Daniel Naber <daniel.naber@t-online.de>
2001-10-31 21:22:40 +0000

    a frontend to Wordnet (a kind of very powerful thesaurus)
    svn path=/trunk/koffice/; revision=120152

---
+++ tools/thesaurus/main.cc
@@ -0,0 +1,304 @@
+/* This file is part of the KDE project
+   Copyright (C) 2001 Daniel Naber <daniel.naber@t-online.de>
+*/
+/***************************************************************************
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ ***************************************************************************/
+
+/*
+FIXME:
+-no trash at end of output!
+-see the fixme's in the source below
+-function words (like "if" etc) are not part of wordnet. There  are not *that*
+ many of them, so maybe we should add them. See search tools' stop word list for
+ function words.
+*/
+
+/* QA:
+-example for an multi-part-word "mental faculty"
+*/
+
+#include "main.h"
+
+#include <stdio.h>
+
+#include <qwidget.h>
+#include <qlayout.h>
+#include <qstring.h>
+#include <qlistbox.h>
+#include <qcombobox.h>
+#include <qvbox.h>
+#include <qregexp.h>
+
+#include <kdatatool.h>
+#include <kdialogbase.h>
+#include <kmessagebox.h>
+#include <klocale.h>
+#include <kdebug.h>
+#include <kinstance.h>
+#include <kconfig.h>
+#include <kcombobox.h>
+#include <kprocess.h>
+
+/***************************************************
+ *
+ * Factory
+ *
+ ***************************************************/
+
+extern "C"
+{
+    void* init_libthesaurustool()
+    {
+      return new ThesaurusFactory;
+    }
+};
+
+ThesaurusFactory::ThesaurusFactory( QObject* parent, const char* name )
+    : KLibFactory( parent, name )
+{
+}
+
+ThesaurusFactory::~ThesaurusFactory()
+{
+}
+
+QObject* ThesaurusFactory::createObject(QObject* parent, const char* name, const char* /*classname*/, const QStringList &)
+{
+    Thesaurus *wn = new Thesaurus(parent, name);
+    // TODO: where to delete this?
+    emit objectCreated(wn);
+    return wn;
+}
+
+/***************************************************
+ *
+ * Thesaurus *
+ ***************************************************/
+
+Thesaurus::Thesaurus(QObject* parent, const char* name)
+    : KDataTool( parent, name )
+{
+    dialog = new KDialogBase("Thesaurus", KDialogBase::User1|KDialogBase::Cancel,
+        KDialogBase::Yes, KDialogBase::Cancel,
+        0, "thesaurus", true, false, QString::null, i18n("Replace"));
+    dialog->setInitialSize(QSize(350, 400));
+
+    layout = dialog->makeVBoxMainWidget();
+    // fixme: selecting one item from the history should immediately go there
+
+    combobox = new QComboBox(layout);
+    combobox->setEditable(false);
+    // warning: order matters:
+    combobox->insertItem(i18n("Synonyms"));
+    combobox->insertItem(i18n("Antonyms"));
+    combobox->insertItem(i18n("Hyponyms - ...is a (kind of) X"));
+    combobox->insertItem(i18n("Meroyms - X has a..."));
+    // TODO: add all the other relations
+    connect(combobox, SIGNAL(activated(int)), this, SLOT(slotFindTerm()));
+
+    edit = new KHistoryCombo(layout);
+    KCompletion *comp = edit->completionObject();
+
+    // fixme: doesn't work?:
+    connect(edit, SIGNAL(returnPressed(const QString&)), comp, SLOT(addItem(const QString&)));
+    connect(edit, SIGNAL(returnPressed(const QString&)), this, SLOT(slotFindTerm(const QString&)));
+    // fixme: doesn't work?:
+    connect(edit, SIGNAL(activated(const QString &)), this, SLOT(slotFindTerm(const QString &)));
+
+    indent = "   ";
+    thesaurusproc = new KProcess;
+    connect(thesaurusproc, SIGNAL(processExited(KProcess*)),
+        this, SLOT(thesaurusExited(KProcess*)));
+    connect(thesaurusproc, SIGNAL(receivedStdout(KProcess*,char*,int)),
+        this, SLOT(receivedStdout(KProcess*, char*, int)));
+    connect(thesaurusproc, SIGNAL(receivedStderr(KProcess*,char*,int)),
+        this, SLOT(receivedStderr(KProcess*, char*, int)));
+
+    listbox = new QListBox(layout);
+    connect(listbox, SIGNAL(selected(int)), this, SLOT(slotFindTerm(int)));
+}
+
+Thesaurus::~Thesaurus()
+{
+    if( dialog ) {
+        delete dialog;
+    }
+    if( thesaurusproc ) {
+        delete thesaurusproc;
+    }
+    // TODO: can we ignore the others because they are children of layout?
+}
+
+bool Thesaurus::run(const QString& command, void* data, const QString& datatype, const QString& mimetype)
+{
+    if ( command != "thesaurus" )
+    {
+        kdDebug(31000) << "Thesaurus does only accept the command 'thesaurus'" << endl;
+        kdDebug(31000) << "   The command " << command << " is not accepted" << endl;
+        return FALSE;
+    }
+    // Check wether we can accept the data
+    if ( datatype != "QString" )
+    {
+        kdDebug(31000) << "Thesaurus only accepts datatype QString" << endl;
+        return FALSE;
+    }
+    if ( mimetype != "text/plain" )
+    {
+        kdDebug(31000) << "Thesaurus only accepts mimetype text/plain" << endl;
+        return FALSE;
+    }
+
+    // Get data and clean it up:
+    QString buffer = *((QString *)data);
+    buffer = buffer.stripWhiteSpace();
+    QRegExp re("[.,;!?\"'()\\[\\]]");
+    buffer.replace(re, "");
+    edit->insertItem(buffer, 0);
+
+    procresult_stdout = "";
+    procresult_stderr = "";
+
+    if( ! slotFindTerm(buffer) ) {
+        return FALSE;
+    }
+
+    if( dialog->exec() == KDialogBase::User1 ) {    // Replace
+        *((QString*)data) = listbox->currentText().stripWhiteSpace();
+    }
+                
+    return TRUE;
+}
+
+
+bool Thesaurus::slotFindTerm(int index)
+{
+    kdDebug(31000) << "##KWWordInfo::slotFindTerm(" << index << ")" << endl;
+    if( listbox->currentItem() != -1 && listbox->text(index).left(indent.length()) != indent ) {
+        return false;
+    }
+    QString term = listbox->text(index).stripWhiteSpace();
+    edit->insertItem(term, 0);
+    edit->setCurrentItem(0);
+    return slotFindTerm(term);
+}
+
+
+bool Thesaurus::slotFindTerm()
+{
+    return findTerm(edit->currentText());
+}
+
+
+bool Thesaurus::slotFindTerm(const QString &term)
+{
+    return findTerm(term);
+}
+
+
+bool Thesaurus::findTerm(const QString &term)
+{
+    // TODO: show waiting mouse pointer
+
+    procresult_stdout = "";
+    procresult_stderr = "";
+    listbox->clear();
+
+    thesaurusproc->clearArguments();
+    *thesaurusproc << "wn";
+    *thesaurusproc << term;
+    // get all results: nouns, verbs, adjectives, adverbs:
+    if( combobox->currentItem() == 0 ) {
+        *thesaurusproc << "-synsn" << "-synsv" << "-synsa" << "-synsr";
+    } else if( combobox->currentItem() == 1 ) {
+        *thesaurusproc << "-antsn" << "-antsv" << "-antsa" << "-antsr";
+    } else if( combobox->currentItem() == 2 ) {
+        *thesaurusproc << "-hypon" << "-hypov";
+    } else if( combobox->currentItem() == 3 ) {
+        *thesaurusproc << "-meron";
+    }
+
+    if( thesaurusproc->isRunning() ) {
+        // should never happen
+        kdDebug(31000) << "Warning: findTerm(): process is already running?!" << endl;
+        return false;
+    }
+
+    if( !thesaurusproc->start(KProcess::NotifyOnExit, KProcess::AllOutput) ) {
+        KMessageBox::error(0, i18n("Failed to execute thesaurus program 'wn' (wordnet)"));	// TODO: add tips
+        return false;
+    }
+    return true;
+}
+
+
+void Thesaurus::thesaurusExited(KProcess *proc)
+{
+    if( !procresult_stderr.isEmpty() ) {
+      KMessageBox::error(0, i18n("Failed to execute thesaurus program 'wn' (wordnet)."
+        "Output:\n%1").arg(procresult_stderr));
+      return;
+    }
+
+    if( procresult_stdout.isEmpty() ) {
+        listbox->insertItem(i18n("No match for '%1'.").arg(edit->currentText()));
+    } else {
+        // fixme:
+        QStringList list = list.split(QRegExp("\n"), procresult_stdout, true);
+        // improve output:
+        for( QStringList::Iterator it = list.begin(); it != list.end(); ++it ) {
+            if( (*it).find(" senses of ") != -1 ) {	// fixme: make it more safe
+                // not interesting
+                //it = list.remove(it);
+            } else if( (*it).find("Sense ") == 0 ) {
+                // fixme: merge with next line
+            } else if( (*it).contains(" => ") > 0 ) {
+                QString line = (*it);
+                line.replace( QRegExp("=>"), "");
+                // fixme: starting with "--":
+                // fixme: manual linebreaks
+                QStringList sublist = sublist.split(QRegExp(","), line, true);
+                for( QStringList::Iterator sub_it = sublist.begin(); sub_it != sublist.end(); ++sub_it ) {
+                    QString syn = *(sub_it);
+                    syn = indent + syn.stripWhiteSpace();
+                    list.insert(it, syn);
+                }
+                it = list.remove(it);
+            }
+        }
+        listbox->insertStringList(list);
+    }
+}
+
+
+void Thesaurus::receivedStdout(KProcess *proc, char *result, int len)
+{
+    char result_tmp[len+1];
+    strncpy(result_tmp, result, len);
+    procresult_stdout += QString(result_tmp);
+    //printf("##### %s\n", result);
+    //printf("##### %s\n", result_tmp);
+    //kdDebug(31000) << "#### stdout: " << QString(result_tmp) << endl;
+}
+
+
+void Thesaurus::receivedStderr(KProcess *proc, char *result, int len)
+{
+    procresult_stderr.replace(procresult_stderr.length(), len, result);
+    //kdDebug(31000) << "#### stderr: " << QString(procresult_stderr) << endl;
+}
+
+#include "main.moc"
---
+++ tools/thesaurus/main.h
@@ -0,0 +1,73 @@
+/* This file is part of the KDE project
+   Copyright (C) 2001 Daniel Naber <daniel.naber@t-online.de>
+*/
+/***************************************************************************
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ ***************************************************************************/
+
+#ifndef __main_h__
+#define __main_h__
+
+#include <qobject.h>
+#include <qvbox.h>
+
+#include <kdialogbase.h>
+#include <kdatatool.h>
+#include <klibloader.h>
+#include <kprocess.h>
+#include <kcombobox.h>
+
+class Thesaurus : public KDataTool
+{
+    Q_OBJECT
+
+public:
+    Thesaurus(QObject* parent = 0, const char* name = 0);
+    ~Thesaurus();
+    virtual bool run( const QString& command, void* data, const QString& datatype, const QString& mimetype);
+
+protected slots:
+    bool slotFindTerm();
+    bool slotFindTerm(int index);
+    bool slotFindTerm(const QString &term);
+    void thesaurusExited(KProcess *proc);
+    void receivedStdout(KProcess *proc, char *result, int len);
+    void receivedStderr(KProcess *proc, char *result, int len);
+protected:
+    bool findTerm(const QString &term);
+    KProcess *thesaurusproc;
+    QString procresult_stdout;
+    QString procresult_stderr;
+    QString indent;
+    QVBox *layout;
+    KHistoryCombo *edit;
+    QListBox *listbox;
+    QComboBox *combobox;
+    KDialogBase *dialog;
+    void* data_access;
+};
+
+class ThesaurusFactory : public KLibFactory
+{
+    Q_OBJECT
+
+public:
+    ThesaurusFactory( QObject* parent = 0, const char* name = 0 );
+    ~ThesaurusFactory();
+    virtual QObject* createObject( QObject* parent = 0, const char* name = 0, 
+        const char* classname = "QObject", const QStringList &args = QStringList() );
+};
+
+#endif
