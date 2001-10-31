625e837386a09be54315777d62f50ef1127f8069
Laurent Montel <montel@kde.org>
2001-10-31 09:09:20 +0000

    Minor fix
    svn path=/trunk/koffice/; revision=120053

--- kword/kwcanvas.cc
+++ kword/kwcanvas.cc
@@ -2025,8 +2025,11 @@ bool KWCanvas::eventFilter( QObject *o, QEvent *e )
 
     if ( o == this || o == viewport() ) {
 
-        // Pass event to auto-hide-cursor code (see kcursor.h for details)
-        KCursor::autoHideEventFilter( o, e );
+        if(m_currentFrameSetEdit)
+        {
+            // Pass event to auto-hide-cursor code (see kcursor.h for details)
+            KCursor::autoHideEventFilter( o, e );
+        }
 
         switch ( e->type() ) {
             case QEvent::FocusIn:
