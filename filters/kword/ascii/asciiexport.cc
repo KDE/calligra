c7478554e0c55f6ee800dcf6c95ad73af000cb89
Nicolas Goutte <nicolasg@snafu.de>
2001-11-11 20:45:07 +0000

    Make the layout of paragraph independant of the layout/style name
    svn path=/trunk/koffice/; revision=121904

--- filters/kword/ascii/asciiexport.cc
+++ filters/kword/ascii/asciiexport.cc
@@ -35,7 +35,8 @@
 class ASCIIWorker : public KWEFBaseWorker
 {
 public:
-    ASCIIWorker(void) : m_ioDevice(NULL), m_streamOut(NULL) { }
+    ASCIIWorker(void) : m_ioDevice(NULL), m_streamOut(NULL), m_eol("\n"),
+        m_inList(false) { }
     virtual ~ASCIIWorker(void) { }
 public:
     virtual bool doOpenFile(const QString& filenameOut, const QString& to);
@@ -48,6 +49,11 @@ private:
 private:
     QIODevice* m_ioDevice;
     QTextStream* m_streamOut;
+    QString m_eol; // End of line character(s)
+    CounterData::Style m_typeList; // What is the style of the current list (undefined, if we are not in a list)
+    bool m_inList; // Are we currently in a list?
+    bool m_orderedList; // Is the current list ordered or not (undefined, if we are not in a list)
+    int  m_counterList; // Counter for te lists
 };
 
 bool ASCIIWorker::doOpenFile(const QString& filenameOut, const QString& to)
@@ -117,55 +123,134 @@ void ASCIIWorker::ProcessParagraphData (const QString& paraText,
         }
     }
 
-    *m_streamOut << "\n";
+    *m_streamOut << m_eol; // Write end of line
 }
 
 bool ASCIIWorker::doFullParagraph(QString& paraText, LayoutData& layout, ValueListFormatData& paraFormatDataList)
 {
     kdDebug(30503) << "Entering ASCIIWorker::doFullParagraph" << endl;
-    QString paraLayout=layout.styleName;
+    
+    // As KWord has only one depth of lists, we can process lists very simply.
+    if ( layout.counter.numbering == CounterData::NUM_LIST )
+    {
+        // Are we still in a list of the right type?
+        if (!m_inList || (layout.counter.style!=m_typeList))
+        {
+            // We are not yet part of a list
+            m_inList=true;
+            m_counterList=1; // Start numbering
+            m_typeList=layout.counter.style;
+        }
 
-    if ( paraLayout == "Head 1" )
-    {
-        *m_streamOut << "###################################\n";
-        *m_streamOut << "# ";
-        ProcessParagraphData ( paraText, paraFormatDataList);
-        *m_streamOut << "###################################\n";
-    }
-    else if ( paraLayout == "Head 2" )
-    {
-        *m_streamOut << "#### ";
-        ProcessParagraphData ( paraText, paraFormatDataList);
-    }
-    else if ( paraLayout == "Head 3" )
-    {
-        *m_streamOut << "## ";
-        ProcessParagraphData ( paraText, paraFormatDataList);
-    }
-    else if ( paraLayout == "Bullet List" )
-    {
-        *m_streamOut << "o ";
-        ProcessParagraphData ( paraText, paraFormatDataList);
-    }
-    else if ( paraLayout == "Enumerated List" )
-    {
-        *m_streamOut << "1. ";   // less than perfect
-        ProcessParagraphData ( paraText, paraFormatDataList);
-    }
-    else if ( paraLayout == "Alphabetical List" )
-    {
-        *m_streamOut << "a) ";   // less than perfect
-        ProcessParagraphData ( paraText, paraFormatDataList);
+        switch (m_typeList)
+        // TODO: when we would be able to save to UTF-8,
+        //   use correct symbols
+        {
+        case CounterData::STYLE_CUSTOMBULLET: // We cannot keep the custom type/style
+        default:
+            {
+                m_orderedList=false;
+                *m_streamOut << "- ";
+                break;
+            }
+        case CounterData::STYLE_NONE:
+            {
+                m_orderedList=false;
+                break;
+            }
+        case CounterData::STYLE_CIRCLEBULLET:
+            {
+                m_orderedList=false;
+                *m_streamOut << "o ";
+                break;
+            }
+        case CounterData::STYLE_SQUAREBULLET:
+            {
+                m_orderedList=false;
+                *m_streamOut << "~ "; // Not much a square
+                break;
+            }
+        case CounterData::STYLE_DISCBULLET:
+            {
+                m_orderedList=false;
+                *m_streamOut << "* "; // Not much a disc
+                break;
+            }
+        case CounterData::STYLE_NUM:
+        case CounterData::STYLE_CUSTOM:
+            {
+                m_orderedList=true;
+                *m_streamOut << QString::number(m_counterList,10);
+                break;
+            }
+        case CounterData::STYLE_ALPHAB_L:
+            {
+                m_orderedList=true;
+                QString strTemp;
+                for (int i=m_counterList;i>0;i/=26)
+                     strTemp=QChar(0x40+i%26)+strTemp; // Lower alpha
+                *m_streamOut << strTemp;
+                break;
+        }
+        case CounterData::STYLE_ALPHAB_U:
+            {
+                m_orderedList=true;
+                QString strTemp;
+                for (int i=m_counterList;i>0;i/=26)
+                     strTemp=QChar(0x40+i%26)+strTemp; // Lower alpha
+                *m_streamOut << strTemp;
+                break;
+            }
+        case CounterData::STYLE_ROM_NUM_L:
+            {
+                // For now, we do not support lower-case Roman numbering (TODO)
+                m_orderedList=true;
+                *m_streamOut << QString::number(m_counterList,10);
+                break;
+            }
+        case CounterData::STYLE_ROM_NUM_U:
+            {
+                // For now, we do not support upper-case Roman numbering (TODO)
+                m_orderedList=true;
+                *m_streamOut << QString::number(m_counterList,10);
+                break;
+            }
+        }
+        m_counterList++; // Increment the list counter
     }
     else
     {
-        if ( paraLayout != "Standard" )
+        m_inList=false; // Close an eventual list
+        if ( layout.counter.numbering == CounterData::NUM_CHAPTER )
         {
-            kdError(30502) << "Unknown layout " + paraLayout + "!" << endl;
+            if (!layout.counter.depth)
+            {   // HEAD 1
+                *m_streamOut << "###################################" << m_eol;
+                *m_streamOut << "# ";
+                ProcessParagraphData ( paraText, paraFormatDataList);
+                *m_streamOut << "###################################" << m_eol;
+            }
+            else if (layout.counter.depth==1)
+            {   // HEAD 2
+                *m_streamOut << "#### ";
+                ProcessParagraphData ( paraText, paraFormatDataList);
+            }
+            else if (layout.counter.depth==2)
+            {   // HEAD 3
+                *m_streamOut << "## ";
+                ProcessParagraphData ( paraText, paraFormatDataList);
+            }
+            else
+            {
+                ProcessParagraphData ( paraText, paraFormatDataList);
+            }
         }
-
-        ProcessParagraphData ( paraText, paraFormatDataList);
+        else
+        {
+            ProcessParagraphData ( paraText, paraFormatDataList);
+        }
     }
+
     kdDebug(30503) << "Exiting ASCIIWorker::doFullParagraph" << endl;
     return true;
 }
