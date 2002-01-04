75967289dc36663a5ee94a060ef8a53ab9eef40b
Stephan Binner <binner@kde.org>
2002-01-04 20:48:18 +0000

    CVS_SILENT Fixed capitalisation.
    svn path=/trunk/koffice/; revision=129902

--- kivio/kiviopart/kivio_dlg_pageshow.cpp
+++ kivio/kiviopart/kivio_dlg_pageshow.cpp
@@ -43,7 +43,7 @@ KivioPageShow::KivioPageShow( KivioView* parent, const char* name )
   list=new QListBox(this);
   lay1->addWidget( list );
 
-  setCaption( i18n("Hidden pages") );
+  setCaption( i18n("Hidden Pages") );
 
   KButtonBox *bb = new KButtonBox( this );
   bb->addStretch();
