7ecc9787a54baa1bccf65c9ce555ba0dea8faa95
Bernhard Rosenkraenzer <bero@arklinux.org>
2002-01-07 23:50:03 +0000

    Fix build with gcc 3.1
    svn path=/trunk/koffice/; revision=130476

--- kspread/kspread_interpreter.cc
+++ kspread/kspread_interpreter.cc
@@ -2965,10 +2965,10 @@ static bool kspreadfunc_roundup( KSContext& context )
         }
   // This is not correct solution for problem with floating point numbers and probably
   // will fail in platforms where float and double lenghts are same.
-  if (approx_equal(floor(args[0]->doubleValue()*pow(10,digits)), args[0]->doubleValue()*pow(10,digits)))
+  if (approx_equal(floor(args[0]->doubleValue()*::pow(10,digits)), args[0]->doubleValue()*::pow(10,digits)))
       result = args[0]->doubleValue();
   else
-      result=floor(args[0]->doubleValue()*pow(10,digits)+1)/pow(10,digits);
+      result=floor(args[0]->doubleValue()*::pow(10,digits)+1)/::pow(10,digits);
   context.setValue( new KSValue( result) );
 
   return true;
@@ -3822,7 +3822,7 @@ static bool kspreadfunc_impower( KSContext& context )
         return false;
         }
 
-  double arg=pow(sqrt(pow(imag,2)+pow(real,2)),args[1]->intValue());
+  double arg=::pow(sqrt(pow(imag,2)+pow(real,2)),args[1]->intValue());
   double angle=atan(imag/real);
 
   double real_res=arg*cos(angle*args[1]->intValue());
