f61fd2fd64596e2d5371cf5e5dfcdd044923121e
John Dailey <dailey@vt.edu>
2002-02-02 21:56:20 +0000

    oops...forgot to cvs add the kspread_depend files in the last commit.
    svn path=/trunk/koffice/; revision=135078

---
+++ kspread/kspread_depend.cc
@@ -0,0 +1,71 @@
+/* This file is part of the KDE project
+   Copyright (C) 1998, 1999 Torben Weis <weis@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+
+#include "kspread_depend.h"
+
+#include <qglobal.h>
+
+
+KSpreadDependancy::KSpreadDependancy(int col, int row, KSpreadTable* table)
+{
+  Q_ASSERT(col > 0 && row > 0);
+
+  m_left = m_right = col;
+  m_top = m_bottom = row;
+
+  m_table = table;
+}
+
+KSpreadDependancy::KSpreadDependancy(int left, int top, int right, int bottom, KSpreadTable* table)
+{
+  Q_ASSERT(left > 0 && top > 0 && right > 0 && bottom > 0);
+
+  m_left = left;
+  m_right = right;
+  m_top = top;
+  m_bottom = bottom;
+
+  m_table = table;
+}
+
+int KSpreadDependancy::Left()
+{
+  return m_left;
+}
+
+int KSpreadDependancy::Right()
+{
+  return m_right;
+}
+
+int KSpreadDependancy::Top()
+{
+  return m_top;
+}
+
+int KSpreadDependancy::Bottom()
+{
+  return m_bottom;
+}
+  
+KSpreadTable* KSpreadDependancy::Table()
+{
+  return m_table;
+}
---
+++ kspread/kspread_depend.h
@@ -0,0 +1,48 @@
+/* This file is part of the KDE project
+   Copyright (C) 1998, 1999 Torben Weis <weis@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef __kspread_depend_h__
+#define __kspread_depend_h__
+
+class KSpreadTable;
+
+class KSpreadDependancy
+{
+ public:
+  KSpreadDependancy(int col, int row, KSpreadTable* table);
+  KSpreadDependancy(int left, int top, int right, int bottom, KSpreadTable* table);
+
+  int Left();
+  int Right();
+  int Top();
+  int Bottom();
+  
+  KSpreadTable* Table();
+
+ private:
+  int m_left;
+  int m_right;
+  int m_top;
+  int m_bottom;
+  KSpreadTable *m_table;
+
+
+};
+
+#endif /* defined __kspread_depend_h__ */
