2f21e93c096051d1979b961aac5ec8dd22077427
Tomislav Lukman <tomislav.lukman@ck.tel.hr>
2002-02-03 10:40:19 +0000

    Solved the button problem. Now creating signals and slots to make them work...
    svn path=/trunk/koffice/; revision=135144

--- karbon/ui/vtoolcontainer.cc
+++ karbon/ui/vtoolcontainer.cc
@@ -7,6 +7,7 @@
 #include <qlayout.h>
 #include <klocale.h>
 #include <kpixmap.h>
+#include <kstandarddirs.h>
 
 #include "vtoolcontainer.h"
 
@@ -18,63 +19,65 @@ VToolContainer::VToolContainer( QObject* parent, const char* name )
 	setCaption(i18n("Tools"));
 	btngroup = new QButtonGroup( 2, Horizontal, this );
 	button = new QPushButton(btngroup);
-	QPixmap pixmap("select");
+	QPixmap pixmap(locate("appdata", "icons/hicolor/22x22/actions/select.png"));
 	button->setPixmap(pixmap);
 	btngroup->insert( button, Select );
 
 	button = new QPushButton(btngroup);
-	pixmap = QPixmap("scale");
+	pixmap = QPixmap(locate("appdata", "icons/hicolor/16x16/actions/scale.png"));
 	button->setPixmap(pixmap);
 	btngroup->insert( button, Scale );
 
 	button = new QPushButton(btngroup);
-	pixmap = QPixmap("rotate");
+	pixmap = QPixmap(locate("appdata", "icons/hicolor/22x22/actions/rotate.png"));
 	button->setPixmap(pixmap);
 	btngroup->insert( button, Rotate );
 	
 	button = new QPushButton(btngroup);
-	pixmap = QPixmap("shear");
+	pixmap = QPixmap(locate("appdata", "icons/hicolor/16x16/actions/shear.png"));
 	button->setPixmap(pixmap);
 	btngroup->insert( button, Shear );
 	
 	button = new QPushButton(btngroup);
-	pixmap = QPixmap("ellipse");
+	pixmap = QPixmap(locate("appdata", "icons/hicolor/22x22/actions/ellipse.png"));
 	button->setPixmap(pixmap);
 	btngroup->insert( button, Ellipse );
 	
 	button = new QPushButton(btngroup);
-	pixmap = QPixmap("rectangle");
+	pixmap = QPixmap(locate("appdata", "icons/hicolor/22x22/actions/rectangle.png"));
 	button->setPixmap(pixmap);
 	btngroup->insert( button, Rectangle );
 
 	button = new QPushButton(btngroup);
-	pixmap = QPixmap("roundrect");
+	pixmap = QPixmap(locate("appdata", "icons/hicolor/22x22/actions/roundrect.png"));
 	button->setPixmap(pixmap);
 	btngroup->insert( button, Roundrect );
 	
 	button = new QPushButton(btngroup);
-	pixmap = QPixmap("polygon");
+	pixmap = QPixmap(locate("appdata", "icons/hicolor/22x22/actions/polygon.png"));
 	button->setPixmap(pixmap);
 	btngroup->insert( button, Polygon );
 	
 	button = new QPushButton(btngroup);
-	pixmap = QPixmap("star");
+	pixmap = QPixmap(locate("appdata", "icons/hicolor/22x22/actions/star.png"));
 	button->setPixmap(pixmap);
 	btngroup->insert( button, Star );
 	
 	button = new QPushButton(btngroup);
-	pixmap = QPixmap("sinus");
+	pixmap = QPixmap(locate("appdata", "icons/hicolor/22x22/actions/sinus.png"));
 	button->setPixmap(pixmap);
 	btngroup->insert( button, Sinus );
-	
+
 	button = new QPushButton(btngroup);
-	pixmap = QPixmap("spiral");
+	pixmap = QPixmap(locate("appdata", "icons/hicolor/22x22/actions/spiral.png"));
 	button->setPixmap(pixmap);
+	btngroup->setInsideSpacing(0);
+	btngroup->setInsideMargin(0);
 	btngroup->insert( button, Spiral );
 
 	connect(btngroup, SIGNAL(clicked(int)), this, SLOT(slotButton(int)));
 	layout->addWidget(btngroup);
-	//resize(100,100);
+	setFixedSize(baseSize());
 	layout->activate();
 }
 
