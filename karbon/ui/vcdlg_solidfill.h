3be292294cca684b8f3f2a6a27f522105467a13e
Tomislav Lukman <tomislav.lukman@ck.tel.hr>
2002-02-11 21:50:07 +0000

    Started working on the new Solid Fill dialog
    svn path=/trunk/koffice/; revision=136039

---
+++ karbon/ui/vcdlg_solidfill.h
@@ -0,0 +1,18 @@
+/* This file is part of the KDE project
+   Copyright (C) 2001, The Karbon Developers
+   Copyright (C) 2002, The Karbon Developers
+*/
+
+#ifndef __VCDLGSOLIDFILL_H__
+#define __VCDLGSOLIDFILL_H__
+
+#include <kdialogbase.h>
+
+class VCDlgSolidFill : public KDialogBase
+{
+	Q_OBJECT
+public:
+	VCDlgSolidFill();
+};
+
+#endif
