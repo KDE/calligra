e1201e271d1c1329ed3fdbe3ed461bba7bf8c33d
Tomislav Lukman <tomislav.lukman@ck.tel.hr>
2002-02-12 15:37:37 +0000

    Renamed the dialog by Lenny's request
    svn path=/trunk/koffice/; revision=136164

---
+++ karbon/ui/vmdlg_solidfill.cc
@@ -0,0 +1,14 @@
+/* This file is part of the KDE project
+   Copyright (C) 2001, The Karbon Developers
+   Copyright (C) 2002, The Karbon Developers
+*/
+
+#include <klocale.h>
+
+#include "vmdlg_solidfill.h"
+
+VMDlgSolidFill::VMDlgSolidFill() : KDialogBase ( 0L, 0, true, i18n( "Solid Fill" ))
+{
+}
+
+#include "vmdlg_solidfill.moc"
---
+++ karbon/ui/vmdlg_solidfill.h
@@ -0,0 +1,18 @@
+/* This file is part of the KDE project
+   Copyright (C) 2001, The Karbon Developers
+   Copyright (C) 2002, The Karbon Developers
+*/
+
+#ifndef __VMDLGSOLIDFILL_H__
+#define __VMDLGSOLIDFILL_H__
+
+#include <kdialogbase.h>
+
+class VMDlgSolidFill : public KDialogBase
+{
+	Q_OBJECT
+public:
+	VMDlgSolidFill();
+};
+
+#endif
