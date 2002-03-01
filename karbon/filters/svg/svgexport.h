c44968d83a947e9c5391b8b81478214528d05cd2
Lars Siebold <khandha5@gmx.net>
2002-03-01 23:33:39 +0000

    here it goes
    svn path=/trunk/koffice/; revision=140021

---
+++ karbon/filters/svg/svgexport.h
@@ -0,0 +1,37 @@
+/* This file is part of the KDE project
+   Copyright (C) 2002, The Karbon Developers
+*/
+
+#ifndef __SVGEXPORT_H__
+#define __SVGEXPORT_H__
+
+#include <qobject.h>
+
+#include <koFilter.h>
+
+class QDomElement;
+class QTextStream;
+
+class SvgExport : public KoFilter
+{
+	Q_OBJECT
+
+public:
+	SvgExport( KoFilter* parent, const char* name, const QStringList& );
+	virtual ~SvgExport() {}
+
+	virtual KoFilter::ConversionStatus convert( const QCString& from, const QCString& to );
+
+private:
+	void exportDocument( QTextStream& s, const QDomElement& node );
+	void exportLayer( QTextStream& s, const QDomElement& node );
+	void exportPath( QTextStream& s, const QDomElement& node );
+	void exportSegments( QTextStream& s, const QDomElement& node );
+	void exportStroke( QTextStream& s, const QDomElement& node );
+	void exportFill( QTextStream& s, const QDomElement& node);
+	void getHexColor( QTextStream& s, const QDomElement& node);
+	int fill_rule;
+};
+
+#endif
+
