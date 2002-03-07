b84ed132ae6b5e67a067b7e3995a7e2e71c9a3af
Werner Trobin <trobin@kde.org>
2002-03-07 06:45:06 +0000

    This is all quite broken, but for now just make it compile. Thanks to Michael Matz :-)
    svn path=/trunk/koffice/; revision=141478

--- filters/kword/latex/xmlparser.cc
+++ filters/kword/latex/xmlparser.cc
@@ -54,7 +54,7 @@ XmlParser::XmlParser(QByteArray in)
 
 XmlParser::XmlParser(const KoStore& in)
 {
-        _in = &in;
+        _in = const_cast<KoStore*>(&in);
 	if(!_in->open("root"))
 	{
 	        kdError(30503) << "Unable to open input file!" << endl;
