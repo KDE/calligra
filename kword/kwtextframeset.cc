11bb7df5433e13063c7de0099a0c797304325808
Laurent Montel <montel@kde.org>
2002-03-17 13:13:50 +0000

    Now variable has specific menu otherwise menu will be too big.
    svn path=/trunk/koffice/; revision=143386

--- kword/kwtextframeset.cc
+++ kword/kwtextframeset.cc
@@ -2798,24 +2798,34 @@ void KWTextFrameSetEdit::showPopup( KWFrame * /*frame*/, KWView *view, const QPo
         variableList = doc->getVariableCollection()->variableActionList();
     }
 
-    kdDebug() << "KWView::openPopupMenuInsideFrame plugging actionlist with " << actionList.count() << " actions" << endl;
-    if(refLink().isNull())
+    if( variableList.count()>0)
     {
-        view->plugActionList( "datatools", actionList );
         view->plugActionList( "variable_action", variableList );
-        QPopupMenu * popup = view->popupMenu("text_popup");
+        QPopupMenu * popup = view->popupMenu("variable_popup");
         Q_ASSERT(popup);
         if (popup)
             popup->popup( point ); // using exec() here breaks the spellcheck tool (event loop pb)
+
     }
     else
     {
-        view->plugActionList( "datatools_link", actionList );
-        view->plugActionList( "variable_action", variableList );
-        QPopupMenu * popup = view->popupMenu("text_popup_link");
-        Q_ASSERT(popup);
-        if (popup)
-            popup->popup( point ); // using exec() here breaks the spellcheck tool (event loop pb)
+        kdDebug() << "KWView::openPopupMenuInsideFrame plugging actionlist with " << actionList.count() << " actions" << endl;
+        if(refLink().isNull())
+        {
+            view->plugActionList( "datatools", actionList );
+            QPopupMenu * popup = view->popupMenu("text_popup");
+            Q_ASSERT(popup);
+            if (popup)
+                popup->popup( point ); // using exec() here breaks the spellcheck tool (event loop pb)
+        }
+        else
+        {
+            view->plugActionList( "datatools_link", actionList );
+            QPopupMenu * popup = view->popupMenu("text_popup_link");
+            Q_ASSERT(popup);
+            if (popup)
+                popup->popup( point ); // using exec() here breaks the spellcheck tool (event loop pb)
+        }
     }
 }
 
