e6a9ef68703a7648de494e57a8c61f35c94bdea5
David Faure <faure@kde.org>
2002-03-18 16:02:28 +0000

    setFormatCommand API cleanup
    svn path=/trunk/koffice/; revision=143666

--- kword/kwtextframeset.cc
+++ kword/kwtextframeset.cc
@@ -2075,8 +2075,7 @@ KoTextFormat *KWTextFrameSet::currentFormat() const
 KCommand *KWTextFrameSet::setFormatCommand( KoTextFormat * newFormat, int flags, bool zoomFont )
 {
     m_textobj->textDocument()->selectAll( KoTextDocument::Temp );
-    KoTextFormat* current = currentFormat();
-    KCommand *cmd = m_textobj->setFormatCommand( 0L, current, newFormat, flags, zoomFont, KoTextDocument::Temp );
+    KCommand *cmd = m_textobj->setFormatCommand( 0L, 0L, newFormat, flags, zoomFont, KoTextDocument::Temp );
     m_textobj->textDocument()->removeSelection( KoTextDocument::Temp );
     return cmd;
 }
