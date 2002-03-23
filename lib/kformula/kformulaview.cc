dd0e675b613d3c17052a40c3a143107669546ac4
Ulrich Kuettler <ulrich.kuettler@gmx.de>
2002-03-23 13:52:03 +0000

    the kword crashing bug has finally been found (by ellis)
    svn path=/trunk/koffice/; revision=144673

--- lib/kformula/kformulaview.cc
+++ lib/kformula/kformulaview.cc
@@ -56,6 +56,9 @@ struct View::View_Impl {
 
     ~View_Impl()
     {
+        if ( document->activeCursor() == cursor ) {
+            document->setActiveCursor( 0 );
+        }
         delete cursor;
     }
 
