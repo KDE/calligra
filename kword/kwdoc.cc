543900183199d2bdfc05d0adf776985a581d4764
David Faure <faure@kde.org>
2002-03-29 14:11:16 +0000

    Disabling all hitTest hacks in KWord (i.e. using standard koffice stuff again) fixes part activation+deactivation. RMB activates the part now (this is what the custom hitTest was for), but this has to be fixed a better way.
    Simon, any hints? :}  I can only think of KoPartManager storing the mouse button
    number of the last click (from an eventFilter reimplementation)...
    
    svn path=/trunk/koffice/; revision=145830

--- kword/kwdoc.cc
+++ kword/kwdoc.cc
@@ -77,10 +77,12 @@ KWChild::~KWChild()
 {
 }
 
-KoDocument *KWChild::hitTest( const QPoint &, const QWMatrix & )
+KoDocument *KWChild::hitTest( const QPoint &p, const QWMatrix &m )
 {
-  // Disable part-manager magic for part activation, KWord does it itself.
-  return 0L;
+  // This was meant to disable part activation on RMB ... but it breaks
+  // part activation with LMB
+  //return 0L;
+  return KoDocumentChild::hitTest( p, m );
 }
 
 /******************************************************************/
@@ -340,8 +342,7 @@ void KWDocument::setZoomAndResolution( int zoom, int dpiX, int dpiY )
 
 KoDocument *KWDocument::hitTest( const QPoint &pos, const QWMatrix &matrix )
 {
-    // Disable part-manager magic for part activation, KWord does it itself.
-    return 0L;
+    return KoDocument::hitTest( pos, matrix );
 }
 
 KWTextFrameSet * KWDocument::textFrameSet ( unsigned int _num ) const
