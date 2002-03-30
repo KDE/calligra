3c885beb30689354646c5de766fc34f6819a6f74
Philipp Müller <philipp.mueller@gmx.de>
2002-03-30 10:24:24 +0000

    Grml, how in the world did I think that my last commit was right. Here is the right fix for the BR.
    svn path=/trunk/koffice/; revision=145994

--- kspread/kspread_cell.cc
+++ kspread/kspread_cell.cc
@@ -458,23 +458,12 @@ QString KSpreadCell::encodeFormula( int _col, int _row )
                         if ( fix1 )
                             erg += QString( "$%1" ).arg( col );
                         else
-                            if ( col - _col > 1 )
-                                erg += QString( "#%1" ).arg( col - _col );
-                            else // the new col is out of range -> exit with reference error message
-                            {
-                                erg = "=\"" + i18n("REFERENCE IS OUT OF RANGE") + "\"";
-                                return erg;
-                            }
+                            erg += QString( "#%1" ).arg( col - _col );
+
                         if ( fix2 )
                             erg += QString( "$%1#").arg( row );
                         else
-                            if ( row - _row > 1 )
-                                erg += QString( "#%1#" ).arg( row - _row );
-                            else // the new row is out of range -> return with reference error message
-                            {
-                                erg = "=\"" + i18n("REFERENCE IS OUT OF RANGE") + "\"";
-                                return erg;
-                            }
+                            erg += QString( "#%1#" ).arg( row - _row );
                     }
                 }
                 else
@@ -552,8 +541,9 @@ QString KSpreadCell::decodeFormula( const QString &_text, int _col, int _row )
             ++pos;
             if ( row < 1 || col < 1 || row > KS_rowMax || col > KS_colMax )
             {
-                kdError(36001) << "KSpreadCell::decodeFormula: row or column out of range" << endl;
-                return _text;
+                kdDebug(36001) << "KSpreadCell::decodeFormula: row or column out of range (col: " << col << " | row: " << row << ")" << endl;
+                erg = "=\"#### " + i18n("REFERENCE TO COLUMN OR ROW IS OUT OF RANGE") + "\"";
+                return erg;
             }
             if ( fix1 )
                 erg += "$";
