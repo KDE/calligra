18c902a6ce175fb730c6c3703986eff313b0b2f9
Lennart Kudling <kudling@kde.org>
2002-04-17 18:53:29 +0000

    hmm
    svn path=/trunk/koffice/; revision=150100

--- filters/karbon/eps/epsimport.cc
+++ filters/karbon/eps/epsimport.cc
@@ -3,13 +3,11 @@
 */
 
 #include <qcstring.h>
-#include <qfile.h>
 #include <qstring.h>
 
 #include <kgenericfactory.h>
 #include <koFilter.h>
 #include <koFilterChain.h>
-#include <koStore.h>
 #include <kprocio.h>
 
 #include <kdebug.h>
@@ -43,63 +41,26 @@ EpsImport::~EpsImport()
 }
 
 KoFilter::ConversionStatus
-EpsImport::convert( const QCString& from, const QCString& /*to*/ )
+EpsImport::convert( const QCString& from, const QCString& to )
 {
-kdDebug() << "****0" << endl;
-
-	if( from != "image/x-eps" )
-//	if( to != "application/illustrator" || from != "image/x-eps" )
+	if( to != "application/illustrator" || from != "image/x-eps" )
 	{
 		return KoFilter::NotImplemented;
 	}
 
-	KoStore* storeIn = KoStore::createStore( m_chain->inputFile(), KoStore::Read );
-	if( !storeIn->open( "root" ) )
-	{
-		delete storeIn;
-		return KoFilter::StupidError;
-	}
-
-	QFile fileOut( m_chain->outputFile() );
-	if( !fileOut.open( IO_WriteOnly ) ) {
-		delete storeIn;
-		return KoFilter::StupidError;
-	}
-
-	QByteArray byteArrayIn = storeIn->read( storeIn->size() );
-	storeIn->close();
-
-kdDebug() << "****1" << endl;
-
 	// initialize ghostscript:
 	KProcIO* proc = new KProcIO();
-	*proc << "gs - -q -dNOPAUSE -dSAFER -dNODISPLAY ps2ai.ps";
-
-kdDebug() << "****2" << endl;
+	*proc
+		<< "/usr/bin/gs -q -dNOPAUSE -dSAFER -dNODISPLAY "
+		<< " ps2ai.ps " << m_chain->inputFile()
+		<< " >" << m_chain->outputFile();
 
 	// start ghostscript:
-	proc->start( KProcess::DontCare, KProcess::All );
-
-kdDebug() << "****3" << endl;
-
-	// write data to ghostscript:
-	proc->fputs( byteArrayIn );
-	proc->closeWhenDone();
-
-	QString line;
-
-kdDebug() << "****4" << endl;
-
-	while( proc->readln( line, true ) )
-	{
-kdDebug() << line << endl;
-	}
+	proc->start( KProcess::Block );
 
 	// stop ghostview:
 	delete( proc );
 
-	fileOut.close();
-	delete storeIn;
 	return KoFilter::OK;
 }
 
