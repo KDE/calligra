25233a0cda1880cd8cc0b6f1818063e6712bbcf8
Rob Buis <buis@kde.org>
2002-04-24 19:46:59 +0000

    First stab at documentation.
    svn path=/trunk/koffice/; revision=151714

--- karbon/core/vobject.h
+++ karbon/core/vobject.h
@@ -14,10 +14,10 @@
 
 enum VState
 {
-	state_normal   = 0,
-	state_selected = 1,
-	state_edit     = 2,
-	state_deleted  = 3
+	state_normal   = 0,	/// visible, not active
+	state_selected = 1, /// visible, active and can be manipulated by tools
+	state_edit     = 2, /// visible, active and can be manipulated by edit tools
+	state_deleted  = 3  /// not visible
 };
 
 
@@ -26,8 +26,13 @@ class QWMatrix;
 class VPainter;
 class VVisitor;
 
-// The base class for all karbon objects.
-
+/**
+ * The base class for all karbon objects. Every object should
+ * have the ability to draw itself using a painter, manage
+ * stroke/fill properties, be state aware, perform hit detection,
+ * transform on demand, clone and load/save itself.
+ * Also an extension mechanism is provided.
+ */
 class VObject
 {
 public:
@@ -40,17 +45,59 @@ public:
 	}
 	virtual ~VObject() {}
 
+	/**
+	 * Draw the object 
+	 *
+	 * @param painter abstraction that is used to render to a painting device.
+	 * @param rect represents the visible rectangular area. If this object doesnt
+	 *             intersect with this area it is not drawn.
+	 * @param zoomFactor zooming factor to take into account when drawing.
+	 */
 	virtual void draw( VPainter* painter, const KoRect& rect,
 		const double zoomFactor = 1.0  ) = 0;
 
+	/**
+	 * Transform the object according to the given matrix.
+	 * Note that this operation triggers an immediate object
+	 * transformation and may be slow for complex objects.
+	 *
+	 * @param m matrix to use for the transformation.
+	 */
 	virtual void transform( const QWMatrix& m ) = 0;
 
+	/**
+	 * Calculates the tightest bounding box around the object.
+	 *
+	 * @param zoomFactor the zoom factor to take into account when calculating the bbox.
+	 * @return the bounding box.
+	 */
 	virtual KoRect boundingBox( const double /*zoomFactor*/ ) const
 		{ return KoRect(); }
+	/**
+	 * Tests whether this object intersects the given rectangle.
+	 * Default is false, each VObject derivative has to implement
+	 * this method.
+	 *
+	 * @param rect the rectangle to test against.
+	 * @param zoomFactor 
+	 * @return true indicates one or more segments intersect with rect, false otherwise.
+	 */
 	virtual bool intersects( const KoRect& /*rect*/, const double /*zoomFactor*/ ) const
 		{ return false; };
 
+	/**
+	 * Get the state the object is in.
+	 *
+	 * @return the object state at time of calling.
+	 */
 	VState state() const { return m_state; }
+	/**
+	 * Sets the state to a specified new state.
+	 * Note that this will not have any effect until draw() is
+	 * called on this object.
+	 *
+	 * @param state the new state.
+	 */
 	virtual void setState( const VState state ) { m_state = state; }
 
 	const VFill& fill() const { return m_fill; }
@@ -59,9 +106,19 @@ public:
 	virtual void setFill( const VFill& fill ) { m_fill = fill; }
 	virtual void setStroke( const VStroke& stroke ) { m_stroke = stroke; }
 
+	/**
+	 * Create an exact copy of this object.
+	 */
 	virtual VObject* clone() = 0;
 
+	/**
+	 * Save this object's state to xml.
+	 */
 	virtual void save( QDomElement& element ) const;
+	/**
+	 * Load this object's state from xml and initialize
+	 * this object accordingly.
+	 */
 	virtual void load( const QDomElement& element );
 
 	/// Accept a VVisitor.
