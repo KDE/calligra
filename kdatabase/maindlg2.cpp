44b8d38b87cb08ac42f89522eb205dc9434b15ce
Chris Machemer <machemc@wyeth.com>
2002-04-28 19:26:12 +0000

    Version 2 of Main Dialog.  Not generated by QT Designer.
    svn path=/trunk/koffice/; revision=152550

---
+++ kdatabase/maindlg2.cpp
@@ -0,0 +1,138 @@
+/***************************************************************************
+                          maindlg2.cpp  -  description
+                             -------------------
+    begin                : Sat Apr 27 2002
+    copyright            : (C) 2002 by root
+    email                : machey@ceinetworks.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License.         *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "maindlg2.h"
+#include "kdebug.h"
+#include "kdatabase_struct.h"
+#include <klistview.h>
+#include "ktabctl.h"
+
+MainDlg2::MainDlg2(QWidget *parentWidget, const char * widgetName, bool isModal):KDialog(parentWidget,widgetName,isModal){
+   tabWidget = new KTabCtl(this, "tabWidget");
+   tabWidget->resize(525,320);
+   KListView1 = new QListView(tabWidget, "KListView1");
+   KListView2 = new QListView(tabWidget, "KListView2");
+   KListView3 = new QListView(tabWidget, "KListView3");
+   KListView4 = new QListView(tabWidget, "KListView4");
+   KListView1->addColumn("Table");
+   KListView1->addColumn("Description");
+   KListView2->addColumn("View");
+   KListView2->addColumn("Description");
+   KListView3->addColumn("Form");
+   KListView3->addColumn("Description");
+
+   QListViewItem tblAdd = new QListViewItem(KListView1,"Add Table", "Click here to add a new table");
+   QListViewItem vwAdd = new QListViewItem(KListView2,"Add View", "Click here to add a new view");
+   QListViewItem frmAdd = new QListViewItem(KListView3,"Add Form", "Click here to add a new form");
+   //tblAdd.setText(1, "Click here to add a new table");
+   tblAdd.repaint();
+   tabWidget->addTab(KListView1,"Tables");
+   tabWidget->addTab(KListView2, "Views");
+   tabWidget->addTab(KListView3, "Forms");
+   tabWidget->addTab(KListView4, "All");
+
+	connect(KListView1,SIGNAL(clicked(QListViewItem *)),this,SLOT(slotTblItemClicked(QListViewItem *)));
+	connect(KListView2,SIGNAL(clicked(QListViewItem *)),this,SLOT(slotViewItemClicked(QListViewItem *)));
+	connect(KListView3,SIGNAL(clicked(QListViewItem *)),this,SLOT(slotFormItemClicked(QListViewItem *)));
+
+
+   resize(525,360);
+   }
+
+MainDlg2::~MainDlg2(){
+}
+
+void MainDlg2::slotTblItemClicked(QListViewItem *itemClicked){
+
+     if(!(itemClicked == NULL)) {
+        kdDebug() << "KDatabase:MainDlg tbl Item Clicked - " << itemClicked->text(0) << endl;
+        myTblDesigner=new clsTblDesigner();
+        myTblDesigner->populateTblDesigner(itemClicked->text(0));
+        myTblDesigner->show();
+        }
+}
+
+void MainDlg2::slotViewItemClicked(QListViewItem *itemClicked){
+
+     if(!(itemClicked == NULL)) {
+        kdDebug() << "KDatabase:MainDlg view Item Clicked - " << itemClicked->text(0) << endl;
+        myQueryBuilder=new QBuildDlg();
+        myQueryBuilder->show();
+        }
+}
+
+void MainDlg2::slotFormItemClicked(QListViewItem *itemClicked){
+     kdDebug() << "KDatabase:MainDlg form Item Clicked" << endl;
+}
+
+void MainDlg2::slotAllItemClicked(QListViewItem *itemClicked){
+     kdDebug() << "KDatabase:MainDlg all Item Clicked" << endl;
+}
+
+
+bool MainDlg2::initStruct(QDomDocument *kdbFile){
+
+    myKDBFile = kdbFile;
+    myStruct = new KDBStruct(kdbFile);
+    refreshStructView();
+    return(true);
+}		
+
+bool MainDlg2::refreshStructView(){
+
+    kdDebug() << "KDatabase:MainDlg refreshStructView" << endl;
+    QDomElement mainElement=myKDBFile->documentElement();
+    QDomNode structureSection=mainElement.namedItem("STRUCTURE");
+    refreshTableView(structureSection.namedItem("TABLES"));
+    refreshViewView(structureSection.namedItem("VIEWS"));
+    refreshFormView(structureSection.namedItem("FORMS"));
+
+return(true);
+}
+
+bool MainDlg2::refreshTableView(QDomNode tableSection){
+
+    QPtrList<QString> myList;
+    QString myItemName;
+
+    kdDebug() << "KDatabase:MainDlg refreshTableView" << endl;
+    myList = myStruct->getTables();
+    QPtrListIterator<QString> myIterator(myList);
+    if(!(myIterator.isEmpty())) {
+       while(!(myIterator==NULL)) {
+//         KListViewItem myItem=new KListViewItem(this->tabWidget->QWidget8->);
+         QListViewItem myItem=new QListViewItem(KListView1,myIterator.current()->latin1());
+//         myItemName= myIterator.current()->latin1();
+//         myItem.setText(0,myItemName);
+         ++myIterator;
+         }
+      }
+    return(true);
+}
+
+bool MainDlg2::refreshViewView(QDomNode viewSection){
+
+    kdDebug() << "KDatabase:MainDlg refreshViewView" << endl;
+    return(true);
+}
+
+bool MainDlg2::refreshFormView(QDomNode formSection){
+
+    kdDebug() << "KDatabase:MainDlg refreshFormView" << endl;
+    return(true);
+}
+
+#include "maindlg2.moc"
---
+++ kdatabase/maindlg2.h
@@ -0,0 +1,63 @@
+/***************************************************************************
+                          maindlg2.h  -  description
+                             -------------------
+    begin                : Sat Apr 27 2002
+    copyright            : (C) 2002 by root
+    email                : machey@ceinetworks.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License.         *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef MAINDLG2_H
+#define MAINDLG2_H
+
+#include <kdialog.h>
+#include <clstbldesigner.h>
+#include <qbuilddlg.h>
+#include <qpaintdevice.h>
+#include "kdatabase_struct.h"
+#include <klistview.h>
+#include <qlistview.h>
+#include "ktabctl.h"
+
+/**
+  *@author root
+  */
+
+class MainDlg2 : public KDialog  {
+       Q_OBJECT
+
+public:
+	MainDlg2(QWidget *parentWidget=0, const char * widgetName=0, bool isModal=false);
+	~MainDlg2(void);
+   bool initStruct(QDomDocument *kdbFile);
+public slots:
+   virtual void slotTblItemClicked(QListViewItem *itemClicked);
+   virtual void slotViewItemClicked(QListViewItem *itemClicked);
+   virtual void slotFormItemClicked(QListViewItem *itemClicked);
+   virtual void slotAllItemClicked(QListViewItem *itemClicked);
+private:
+   bool refreshStructView();
+   bool refreshTableView(QDomNode tableSection);
+   bool refreshViewView(QDomNode viewSection);
+   bool refreshFormView(QDomNode formSection);
+
+    clsTblDesigner* myTblDesigner;
+    QBuildDlg* myQueryBuilder;
+    KDBStruct* myStruct;
+
+    KTabCtl *tabWidget;
+    QDomDocument* myKDBFile;
+    QListView* KListView1;
+    QListView* KListView2;
+    QListView* KListView3;
+    QListView* KListView4;
+};
+
+#endif
