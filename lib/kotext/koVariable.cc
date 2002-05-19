45e9c6053c85064f9c7966e80649232405c03b4c
Laurent Montel <montel@kde.org>
2002-05-19 15:42:11 +0000

    Don't draw twice strikeout.
    svn path=/trunk/koffice/; revision=156354

--- lib/kotext/koVariable.cc
+++ lib/kotext/koVariable.cc
@@ -509,10 +509,10 @@ void KoVariable::drawCustomItemHelper( QPainter* p, int x, int y, const QColorGr
         p->setPen( QPen( textColor ) );
     }
 
-    KoTextParag::drawUnderlineDoubleUnderline( p, fmt, zh, font, textColor, x, bl, zh->layoutUnitToPixelX( width ), y, h );
-
     p->setFont( font ); // already done by KoTextCustomItem::draw but someone might
                         // change the font passed to drawCustomItemHelper (e.g. KoLinkVariable)
+
+    KoTextParag::drawUnderlineDoubleUnderline( p, fmt, zh, font, textColor, x, bl, zh->layoutUnitToPixelX( width ), y, h );
 
     //kdDebug() << "KoVariable::draw bl=" << bl << << endl;
     p->drawText( x, y + bl + offset, text() );
