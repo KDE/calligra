cd47b28648983c39d3f0291324fea65c5aaaedf2
Alexander Dymo <adymo@kdevelop.org>
2002-06-04 21:29:00 +0000

    The first import to CVS
    svn path=/trunk/koffice/; revision=159384

---
+++ kugar/lib/kreportviewer.h
@@ -0,0 +1,34 @@
+/***************************************************************************
+              kreportviewer.h  -  Kugar KDE report viewer widget
+              -------------------
+    begin     : Tue Jun 04 2002
+    copyright : (C) 2002 Alexander Dymo
+    email     : cloudtemple@mksat.net
+ ***************************************************************************/
+
+#ifndef KREPORTVIEWER_H
+#define KREPORTVIEWER_H
+
+#include "mreportviewer.h"
+
+class KPrinter;
+
+/**Kugar KDE report viewer widget
+  *@author Alexander Dymo
+  */
+
+class KReportViewer : public MReportViewer
+{
+	Q_OBJECT
+
+public:
+	KReportViewer(QWidget *parent=0, const char *name=0):
+		MReportViewer(parent, name) {}
+
+	void printReport();
+
+private:
+	KPrinter *printer;
+};
+
+#endif
