10344d0663fd183a720acb5e8849690264d22af1
Tomislav Lukman <tomislav.lukman@ck.tel.hr>
2002-06-05 15:34:06 +0000

    Reserved space for the Reference Point Widget
    svn path=/trunk/koffice/; revision=159485

---
+++ karbon/widgets/vreference.cc
@@ -0,0 +1,15 @@
+/* This file is part of the KDE project
+   Made by Tomislav Lukman (tomislav.lukman@ck.tel.hr)
+   Copyright (C) 2002, The Karbon Developers
+*/
+
+/* vreference.cc */
+
+#include "vreference.h"
+
+VReference::VReference( QWidget* parent )
+{
+}
+
+#include "vreference.moc"
+
---
+++ karbon/widgets/vreference.h
@@ -0,0 +1,22 @@
+/* This file is part of the KDE project
+   Made by Tomislav Lukman (tomislav.lukman@ck.tel.hr)
+   Copyright (C) 2002, The Karbon Developers
+*/
+
+/* This is the color slider widget that is used to select color or color components */
+
+/* vreference.h */
+#ifndef VREFERENCE_H
+#define VREFERENCE_H
+
+#include <qwidget.h>
+
+class VReference : public QWidget
+{
+	Q_OBJECT
+public:
+	VReference( QWidget* parent = 0L );
+};
+
+#endif
+
