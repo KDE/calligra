b229f90154839e25048fabb80b692c167dd66ed8
Rob Buis <buis@kde.org>
2002-06-07 17:11:46 +0000

    Compile. I noticed a crash, investigating now.
    svn path=/trunk/koffice/; revision=159789

--- karbon/shapes/vroundrect.cc
+++ karbon/shapes/vroundrect.cc
@@ -3,6 +3,7 @@
    Copyright (C) 2002, The Karbon Developers
 */
 
+#define QMIN(a, b)      ((a) < (b) ? (a) : (b))
 
 #include <klocale.h>
 
@@ -19,7 +20,7 @@ VRoundRect::VRoundRect( VObject* parent,
 	// catch case, when radius is larger than width or height:
 	double minimum;
 
-	if( edgeRadius  > ( minimum = QMIN( width, height * 0.5 ) )
+	if( edgeRadius  > ( minimum = QMIN( width, height * 0.5 ) ) )
 	{
  		edgeRadius = minimum;
 	}
