922e5787b83b9a72e512bee0372ae56903f0a1f9
Daniel Naber <daniel.naber@t-online.de>
2002-06-09 01:07:38 +0000

    update
    svn path=/trunk/koffice/; revision=159930

--- tools/kthesaurus/main.cc
+++ tools/kthesaurus/main.cc
@@ -1,10 +1,11 @@
 /***************************************************************************
-                       main.cc - using the thesaurus KDataTool stand alone
+                       main.cc - using the language KDataTool 
+					   stand alone (for testing purposes only)
                        -------------------
-    begin            : 2001-12-22
-    copyright        : (C) 2001 by Daniel Naber
+    begin            : 2002-02-19
+    copyright        : (C) 2002 by Daniel Naber
     email            : daniel.naber@t-online.de
-	$Id$
+	$Id: $
  ***************************************************************************/
 
 /***************************************************************************
@@ -16,6 +17,7 @@
  *                                                                         *
  ***************************************************************************/
 
+#include <qfile.h>
 #include <qstring.h>
 
 #include <kaboutdata.h>
@@ -28,61 +30,60 @@
 
 static KCmdLineOptions options[] =
 {
-	{ "+[term]",   I18N_NOOP("Term to search for when starting up"), 0 },
+	{ "+[filename]",   I18N_NOOP("File that should be checked"), 0 },
 	{ 0, 0, 0 }
 };
 
 int main(int argc, char **argv)
 {
 
-	KAboutData aboutData("kthesaurus", I18N_NOOP("KThesaurus"), "1.0",
-		I18N_NOOP( "KThesaurus - List synonyms" ), KAboutData::License_GPL,
-		I18N_NOOP( "(c) 2001 Daniel Naber" ) );
+	KAboutData aboutData("klanguage", I18N_NOOP("KLanguage"), "1.0",
+		I18N_NOOP( "KLanguageTool - Style and Grammar Tool" ), KAboutData::License_GPL,
+		I18N_NOOP( "(c) 2002 Daniel Naber" ));
 
 	KCmdLineArgs::init(argc, argv, &aboutData);
 	KCmdLineArgs::addCmdLineOptions(options);
 	// "unused var" warning, but we need it because kdatatool needs an instance:
 	KApplication *a = new KApplication();
 
-	// TODO: take term from command line!
-
-	KService::Ptr service = KService::serviceByDesktopName("thesaurustool");
+	KService::Ptr service = KService::serviceByDesktopName("languagetool");
 	if( ! service ) {
-		kdWarning() << "Could not find Service/KDataTool 'thesaurustool'!" << endl;
+		kdWarning() << "Could not find Service/KDataTool 'languagetool'!" << endl;
 		return 1;
 	}
 
 	KDataToolInfo *info = new KDataToolInfo(service, 0);
 	KDataTool *tool = info->createTool();
 	if ( !tool ) {
-		kdWarning() << "Could not create tool 'thesaurustool'!" << endl;
+		kdWarning() << "Could not create tool 'languagetool'!" << endl;
 		return 2;
 	}
 
-/* TODO: get selection(), not only clipboard!
-	QClipboard *cb = QApplication::clipboard();
-	QString text = cb->text();
-	if( text == QString::null || text.length() > 50 ) {
-		// long texts are probably not supposed to be searched for
-		text = "";
-	}
-*/
+/* TODO: get selection(), not just clipboard! see kthesaurus */
+
 	QString text = "";
 	KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
 	if ( args->count() > 0 ) {
-		text = args->arg(0);
+		QString filename = args->arg(0);
+		QFile f(filename);
+		if( f.open(IO_ReadOnly) ) {
+			QTextStream t( &f );
+			text = t.read();
+		} else {
+			kdWarning() << "Could not open file '" << filename << "'" << endl;
+		}
+		f.close();
+	} else {
+		kdError() << "No file specified." << endl;
 	}
+	// fixme: else? can we do anything useful?
 
-	QString command = "thesaurus_standalone";	// 'standalone' will give us different buttons
+	QString command = "language_standalone";	// 'standalone' will give us different buttons
 	QString mimetype = "text/plain";
 	QString datatype = "QString";
-
-	//kdDebug() << "KThesaurus command=" << command
-	//		<< " dataType=" << info->dataType() << endl;
-
 	tool->run(command, &text, datatype, mimetype);
+	// TODO??: save the text again if modified and "Ok" was clicked
 
 	delete tool;
-
 	return 0;
 }
