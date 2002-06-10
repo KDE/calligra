a3db5fd09a66a9b2ca14ae4ecca9752dffc730ba
Laurent Montel <montel@kde.org>
2002-06-10 09:43:19 +0000

    Start to implement import style from file
    svn path=/trunk/koffice/; revision=160125

---
+++ lib/kotext/koimportstyledia.cc
@@ -0,0 +1,121 @@
+/* This file is part of the KDE project
+   Copyright (C)  2002 Montel Laurent <lmontel@mandrakesoft.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include <klocale.h>
+#include <qvbox.h>
+#include <qlayout.h>
+#include <qlineedit.h>
+#include <qpushbutton.h>
+#include <qlistbox.h>
+#include "koimportstyledia.h"
+
+#include <kdebug.h>
+#include <qlabel.h>
+
+KoImportStyleDia::KoImportStyleDia( const QStringList &_list, QWidget *parent, const char *name )
+    : KDialogBase( parent, name , true, "", Ok|Cancel|User1, Ok, true )
+{
+    setCaption( i18n("Import Style") );
+    m_list =_list;
+    QVBox *page = makeVBoxMainWidget();
+    new QLabel(i18n("Select Style to import:"), page);
+    m_listStyleName = new QListBox( page );
+    m_listStyleName->setSelectionMode( QListBox::Multi );
+    enableButtonOK( (m_listStyleName->count()!=0) );
+    setButtonText( KDialogBase::User1, i18n("Load") );
+    connect( this, SIGNAL( user1Clicked() ), this, SLOT(slotLoadFile()));
+    resize (300, 400);
+}
+
+KoImportStyleDia::~KoImportStyleDia()
+{
+}
+
+void KoImportStyleDia::slotLoadFile()
+{
+    loadFile();
+}
+
+QString KoImportStyleDia::generateStyleName( const QString & templateName )
+{
+    QString name;
+    int num = 1;
+    bool exists;
+    do {
+        name = templateName.arg( num );
+        exists = (m_list.findIndex( name )!=-1);
+        ++num;
+    } while ( exists );
+    return name;
+}
+
+
+void KoImportStyleDia::loadFile()
+{
+    m_styleList.setAutoDelete(true);
+    m_styleList.clear();
+}
+
+void KoImportStyleDia::initList()
+{
+    QStringList lst;
+    for ( KoStyle * p = m_styleList.first(); p != 0L; p = m_styleList.next() )
+    {
+        lst<<p->translatedName();
+    }
+    m_listStyleName->insertStringList(lst);
+}
+
+void KoImportStyleDia::slotOk()
+{
+#if 0
+    for (uint i = 0; i< m_listStyleName->count();i++)
+    {
+        if ( !m_listStyleName->isSelected( i ))
+        {
+            if ( m_typeStyle ==frameStyle )
+            {
+                //remove this style from list
+                QPtrListIterator<KWFrameStyle> styleIt( m_frameStyleList );
+                for ( ; styleIt.current(); ++styleIt )
+                {
+                    if ( styleIt.current()->name() == m_listStyleName->text(i ) )
+                    {
+                        m_frameStyleList.remove(styleIt.current());
+                    }
+                }
+            }
+            else
+            {
+                //remove this style from list
+                QPtrListIterator<KWTableStyle> styleIt( m_tableStyleList );
+                for ( ; styleIt.current(); ++styleIt )
+                {
+                    if ( styleIt.current()->name() == m_listStyleName->text(i ) )
+                    {
+                        m_tableStyleList.remove(styleIt.current());
+                    }
+                }
+            }
+        }
+    }
+#endif
+    KDialogBase::slotOk();
+}
+#include "koimportstyledia.moc"
---
+++ lib/kotext/koimportstyledia.h
@@ -0,0 +1,54 @@
+/* This file is part of the KDE project
+   Copyright (C)  2002 Montel Laurent <lmontel@mandrakesoft.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef __KoImportStyleDia__
+#define __KoImportStyleDia__
+
+#include <kdialogbase.h>
+#include <qstringlist.h>
+#include <kostyle.h>
+class QLineEdit;
+class QListBox;
+class QPushButton;
+
+class KoImportStyleDia : public KDialogBase
+{
+    Q_OBJECT
+public:
+    KoImportStyleDia( const QStringList & _list, QWidget *parent, const char *name );
+    ~KoImportStyleDia();
+
+    QPtrList<KoStyle> listOfStyleImported()const { return m_styleList;}
+protected slots:
+    virtual void slotOk();
+    void slotLoadFile();
+
+protected:
+    QString generateStyleName( const QString & templateName );
+    virtual void loadFile()=0;
+
+    void initList();
+
+    QListBox *m_listStyleName;
+    QPtrList<KoStyle> m_styleList;
+
+    QStringList m_list;
+};
+
+#endif
