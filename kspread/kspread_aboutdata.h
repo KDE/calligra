5506694e0815895e142aab4573ca41a3c332db40
Laurent Montel <montel@kde.org>
2002-06-16 07:42:40 +0000

    beta2
    svn path=/trunk/koffice/; revision=161318

--- kspread/kspread_aboutdata.h
+++ kspread/kspread_aboutdata.h
@@ -24,7 +24,7 @@
 #include <klocale.h>
 
 static const char* description=I18N_NOOP("KOffice Spreadsheet Application");
-static const char* version="1.2 Beta1";
+static const char* version="1.2 Beta2";
 
 KAboutData * newKSpreadAboutData()
 {
