892b97705bb8958602fa7665ee8749a5fe8002c7
Laurent Montel <montel@kde.org>
2002-06-16 07:50:33 +0000

    Beta2
    svn path=/trunk/koffice/; revision=161321

--- kivio/kiviopart/kivio_aboutdata.h
+++ kivio/kiviopart/kivio_aboutdata.h
@@ -24,7 +24,7 @@
 #include <klocale.h>
 
 static const char* description=I18N_NOOP("KOffice Kivio Application");
-static const char* version="1.2 Beta1";
+static const char* version="1.2 Beta2";
 
 KAboutData * newKivioAboutData()
 {
