0310469c6fad91354567e5c860f0cd60bb338ff3
Malcolm Hunter <malcolm.hunter@gmx.co.uk>
2002-06-23 02:26:36 +0000

    Corrected typographical errors
    svn path=/trunk/koffice/; revision=162562

--- lib/kotext/koAutoFormatDia.cc
+++ lib/kotext/koAutoFormatDia.cc
@@ -499,7 +499,7 @@ void KoAutoFormatDia::setupTab3()
     connect(autoFormatLanguage, SIGNAL(highlighted ( const QString & )), this, SLOT(changeAutoformatLanguage(const QString & )));
 
     grid->addMultiCellWidget( autoFormatLanguage, 0, 0, 4, 6 );
-    QLabel *lblAutoFormatLanguage = new QLabel( i18n("Remplacement and exception for language"), tab3);
+    QLabel *lblAutoFormatLanguage = new QLabel( i18n("Replacement and exception for language"), tab3);
     grid->addMultiCellWidget( lblAutoFormatLanguage, 0, 0, 0, 3 );
 
     cbAdvancedAutoCorrection = new QCheckBox( tab3 );
