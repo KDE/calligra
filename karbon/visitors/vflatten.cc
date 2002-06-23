7900ef0a6bd3640e3f0c24547ff01b33161c2d86
Lennart Kudling <kudling@kde.org>
2002-06-23 12:08:57 +0000

    - add hints about ghostscript's flatten algo
    svn path=/trunk/koffice/; revision=162636

--- karbon/visitors/vflatten.cc
+++ karbon/visitors/vflatten.cc
@@ -8,6 +8,35 @@
 #include "vsegment.h"
 #include "vsegmentlist.h"
 
+
+// TODO: Think about if we want to adapt this:
+
+/*
+ * <cite from GNU ghostscript's gxpflat.c>
+ *
+ * To calculate how many points to sample along a path in order to
+ * approximate it to the desired degree of flatness, we define
+ *      dist((x,y)) = abs(x) + abs(y);
+ * then the number of points we need is
+ *      N = 1 + sqrt(3/4 * D / flatness),
+ * where
+ *      D = max(dist(p0 - 2*p1 + p2), dist(p1 - 2*p2 + p3)).
+ * Since we are going to use a power of 2 for the number of intervals,
+ * we can avoid the square root by letting
+ *      N = 1 + 2^(ceiling(log2(3/4 * D / flatness) / 2)).
+ * (Reference: DEC Paris Research Laboratory report #1, May 1989.)
+ *
+ * We treat two cases specially.  First, if the curve is very
+ * short, we halve the flatness, to avoid turning short shallow curves
+ * into short straight lines.  Second, if the curve forms part of a
+ * character (indicated by flatness = 0), we let
+ *      N = 1 + 2 * max(abs(x3-x0), abs(y3-y0)).
+ * This is probably too conservative, but it produces good results.
+ *
+ * </cite from GNU ghostscript's gxpflat.c>
+ */
+
+
 void
 VFlatten::setFlatness( double flatness )
 {
