d5905edb38343c27287d6ef773595a26a03793c1
Laurent Montel <montel@kde.org>
2002-06-24 16:26:36 +0000

    Rename protectcontent to protectContent as asked by David.
    svn path=/trunk/koffice/; revision=162904

--- kword/kwtextframeset.cc
+++ kword/kwtextframeset.cc
@@ -1655,7 +1655,7 @@ QDomElement KWTextFrameSet::saveInternal( QDomElement &parentElem, bool saveFram
         framesetElem.setAttribute( "removable", static_cast<int>( m_removeableHeader ) );
     }
     if ( protectContent() )
-        framesetElem.setAttribute( "protectcontent", static_cast<int>(protectContent()));
+        framesetElem.setAttribute( "protectContent", static_cast<int>(protectContent()));
 
     KWFrameSet::saveCommon( framesetElem, saveFrames );
 
@@ -1673,8 +1673,8 @@ QDomElement KWTextFrameSet::saveInternal( QDomElement &parentElem, bool saveFram
 void KWTextFrameSet::load( QDomElement &attributes, bool loadFrames )
 {
     KWFrameSet::load( attributes, loadFrames );
-    if ( attributes.hasAttribute( "protectcontent"))
-        setProtectContent((bool)attributes.attribute( "protectcontent" ).toInt());
+    if ( attributes.hasAttribute( "protectContent"))
+        setProtectContent((bool)attributes.attribute( "protectContent" ).toInt());
 
     textDocument()->clear(false); // Get rid of dummy paragraph (and more if any)
     m_textobj->setLastFormattedParag( 0L ); // no more parags, avoid UMR in next setLastFormattedParag call
