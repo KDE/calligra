bb0d61e12855cc47e5797b78fc695e137efe706d
David Faure <faure@kde.org>
2002-06-28 16:21:37 +0000

    Fix crect too small by one pixel (rounding problems + qrect semantics)... This fixes the "white lines" on inline items (particularly images) CCMAIL: 44226-done@bugs.kde.org, 44214-done@bugs.kde.org
    svn path=/trunk/koffice/; revision=163664

--- kword/kwanchor.cc
+++ kword/kwanchor.cc
@@ -116,6 +116,9 @@ void KWAnchor::draw( QPainter* p, int x, int y, int cx, int cy, int cw, int ch, 
 
     // Convert crect to view coords
     QRect crect = fs->currentViewMode()->normalToView( zh->zoomRect( crectPt ) );
+    // and add 1 to right and bottom, to avoid rounding errors (and due to qrect semantics)
+    crect.rBottom() += 1;
+    crect.rRight() += 1;
 #ifdef DEBUG_DRAWING
     kdDebug() << "               crect in view coordinates (pixel) : " << DEBUGRECT( crect ) << endl;
 #endif
