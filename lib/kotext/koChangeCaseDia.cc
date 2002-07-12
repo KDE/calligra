f0a1b6d5cd49f5ff444d99a7c3fad8323d42c76e
Lukáš Tinkl <lukas@kde.org>
2002-07-12 16:44:26 +0000

    fix QWhatsThis
    svn path=/trunk/koffice/; revision=166269

--- lib/kotext/koChangeCaseDia.cc
+++ lib/kotext/koChangeCaseDia.cc
@@ -44,7 +44,7 @@ KoChangeCaseDia::KoChangeCaseDia( QWidget *parent, const char *name )
 
     m_toggleCase=new QRadioButton( i18n("&Toggle case"), grp );
     m_sentenceCase=new QRadioButton( i18n("Sentence case"), grp );
-    QWhatsThis::add( m_sentenceCase, i18n("Convert &first letter of a sentence to &uppercase."));
+    QWhatsThis::add( m_sentenceCase, i18n("Convert first letter of a sentence to uppercase."));
 
     m_upperCase->setChecked(true);
     grp->setRadioButtonExclusive( TRUE );
