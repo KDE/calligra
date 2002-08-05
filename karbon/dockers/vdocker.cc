826354925e71093dcb9f6424099b87e0074a0c08
Tomislav Lukman <tomislav.lukman@ck.tel.hr>
2002-08-05 18:06:08 +0000

    Added VDocker baseclass for dockers
    svn path=/trunk/koffice/; revision=170558

---
+++ karbon/dockers/vdocker.cc
@@ -0,0 +1,36 @@
+/* This file is part of the KDE project
+   Made by Tomislav Lukman (tomislav.lukman@ck.tel.hr)
+   Copyright (C) 2002, The Karbon Developers
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include <qfont.h>
+
+#include <koMainWindow.h>
+#include <koView.h>
+
+#include "vdocker.h"
+
+VDocker::VDocker( KoView* parent, const char* /*name*/ )
+	: QDockWindow( QDockWindow::OutsideDock, parent->shell() )
+{
+	setCloseMode( QDockWindow::Always );
+	setFont( QFont( "helvetica" , 8 ) );
+}
+
+#include "vdocker.moc"
+
