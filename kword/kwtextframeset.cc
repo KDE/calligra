2342ee9869530dfa50bbc1285879c15f8af33bc5
David Faure <faure@kde.org>
2002-09-20 11:09:59 +0000

    Fixed crash (e.g. on saving, in #48073), moving m_doc->recalcFrames(); down because it can delete 'theFrame'.
    svn path=/trunk/koffice/; revision=179130

--- kword/kwtextframeset.cc
+++ kword/kwtextframeset.cc
@@ -2163,9 +2163,6 @@ void KWTextFrameSet::frameResized( KWFrame *theFrame, bool invalidateLayout )
     theFrame->frameSet()->updateFrames(); // update e.g. available height
     m_doc->updateFramesOnTopOrBelow( theFrame->pageNum() );
 
-    if ( theFrame->frameSet()->frameSetInfo() != KWFrameSet::FI_BODY )
-        m_doc->recalcFrames();
-
     KWTableFrameSet *table = theFrame->frameSet()->getGroupManager();
     if ( table )
     {
@@ -2173,13 +2170,16 @@ void KWTextFrameSet::frameResized( KWFrame *theFrame, bool invalidateLayout )
         table->recalcCols(cell->m_col,cell->m_row);
         table->recalcRows(cell->m_col,cell->m_row);
     }
+    theFrame->updateRulerHandles();
+
+    if ( theFrame->frameSet()->frameSetInfo() != KWFrameSet::FI_BODY )
+        m_doc->recalcFrames(); // warning this can delete theFrame!
 
     // m_doc->frameChanged( theFrame );
     // Warning, can't call layout() (frameChanged calls it)
     // from here, since it calls formatMore() !
     if ( invalidateLayout )
         m_doc->invalidate(this);
-    theFrame->updateRulerHandles();
 
     // Can't repaint directly, we might be in a paint event already
     m_doc->delayedRepaintAllViews();
