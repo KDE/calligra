61e1a4003a9f13dd92afc5305ca3d2795a9f93a0
Rob Buis <buis@kde.org>
2002-09-28 07:33:25 +0000

    i18n fixes.
    svn path=/trunk/koffice/; revision=180819

--- karbon/tools/vcliparttool.cc
+++ karbon/tools/vcliparttool.cc
@@ -104,13 +104,13 @@ VClipartWidget::VClipartWidget( QPtrList<VClipartIconItem>* clipartItems, Karbon
 	m_buttonGroup->insert( m_deleteClipartButton = new QToolButton( m_buttonGroup ) );
 	m_clipartChooser->setFixedSize( 180, 120 );
 	m_addClipartButton->setIconSet( QPixmap( il.iconPath( "14_clipart_add.png", KIcon::Small ) ) );
-	m_addClipartButton->setTextLabel( "Add" );
+	m_addClipartButton->setTextLabel( i18n( "Add" ) );
 	m_addClipartButton->setTextPosition( QToolButton::Under );
 	m_importClipartButton->setIconSet( QPixmap( il.iconPath( "14_clipart_import.png", KIcon::Small ) ) );
-	m_importClipartButton->setTextLabel( "Import" );
+	m_importClipartButton->setTextLabel( i18n( "Import" ) );
 	m_importClipartButton->setTextPosition( QToolButton::Under );
 	m_deleteClipartButton->setIconSet( QPixmap( il.iconPath( "14_clipart_delete.png", KIcon::Small ) ) );
-	m_deleteClipartButton->setTextLabel( "Delete" );
+	m_deleteClipartButton->setTextLabel( i18n( "Delete" ) );
 	m_deleteClipartButton->setTextPosition( QToolButton::Under );
 
 	m_buttonGroup->setInsideMargin( 3 );
