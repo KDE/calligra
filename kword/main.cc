d95a0da9379ed2495d7038dc5fefd411dc03133c
David Faure <faure@kde.org>
2002-10-29 16:39:34 +0000

    +    KoApplication::addCmdLineOptions();
    svn path=/trunk/koffice/; revision=186399

--- kword/main.cc
+++ kword/main.cc
@@ -32,6 +32,7 @@ int main( int argc, char **argv )
 {
     KCmdLineArgs::init( argc, argv, newKWordAboutData());
     KCmdLineArgs::addCmdLineOptions( options );
+    KoApplication::addCmdLineOptions();
 
     KoApplication app;
 
