fc335e21e9abd44f70fae0e459c3ed6c1fa44f0c
David Faure <faure@kde.org>
2002-11-04 00:00:52 +0000

    cater for multiple prefixes ; look for the kword binary too
    svn path=/trunk/koffice/; revision=187283

--- tools/scripts/check_msword_import.sh
+++ tools/scripts/check_msword_import.sh
@@ -3,16 +3,29 @@ if ! kde-config ; then
   echo 'kde-config not found ! Aborting. You need a more recent KDE, or to fix your $PATH.'
   exit 1
 fi
+IFS=:
+
+echo -n "KDE prefixes: "
+kde-config --prefix
+
+# Check for the binary
+found=0
+for dir in `kde-config --path exe`; do
+  if [ -f "$dir/kword" ]; then
+    echo "kword found in $dir"
+    found=1
+  fi
+done
+
+if [ $found -eq 0 ]; then
+    echo "ERROR: kword not found"
+fi
 
 mimelnks=`kde-config --path mime`
-lastmimepath=""
-found=0
 # Relevant existing mimetypes
 mimes=""
 
-IFS=:
 for dir in $mimelnks; do
-  lastmimepath=$dir
   filename="application/msword.desktop"
   if [ -f "$dir$filename" ]; then
       echo -n "Found: $dir$filename... "
@@ -51,18 +64,25 @@ IFS=" "
 mimes=`echo $mimes | sed -e 's/^://g'`
 echo "Relevant mimetypes found: $mimes"
 
-magic=$lastmimepath/magic
-if [ -f "$magic" ]; then
-    echo "magic file says: "
+IFS=:
+foundmagicfile=0
+for dir in $mimelnks; do
+  magic=$dir/magic
+  if [ -f "$magic" ]; then
+    echo "$magic says: "
     grep 'Microsoft\\ Word' $magic
-else
+    foundmagicfile=1
+  fi
+done
+
+if [ $foundmagicfile -eq 0 ]; then
     echo "ERROR: Magic file not found $magic"
 fi
 
-IFS=:
 for dir in `kde-config --path services`; do
   echo Services dir $dir
   for mime in $mimes; do
      grep "Import=.*$mime" $dir/*.desktop
   done
 done
+
