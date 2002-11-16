9abc0561a82f421d579840aeca70dccbe28de20a
Philipp Müller <philipp.mueller@gmx.de>
2002-11-16 10:12:59 +0000

    Fixes for: #48515: KSpread and KChart - can't change text of items and series This is mostly a hack, as the real fix must be done in KDChart, but this is to hard at the moment.
    #42998: when embedding into kword x-axis from one instance overwrites one from the other
    This only moves the QStringList of x-achsis texts from a static QStringList in kchartDataEditor.cc (eeek!) to "normal" QStringList in kchart_part.cc. IMHO this should in principle be handled as virtual entries as well, but this would be BIC.
    
    I contacted KHZ if I should apply this to head as well or if he would like to work on a clean/complete fix, especially in KDChart.
    
    OTH, I couldn't fix it in KDChart, due to not able to test all functionality which KDChart supports (e.g. multiple x-Achses), but we don't use yet.
    
    svn path=/branches/KOFFICE_1_2_BRANCH/koffice/; revision=189212

---
+++ kchart/kchart_part.h
@@ -0,0 +1,73 @@
+/**
+ * Kalle Dalheimer <kalle@kde.org>
+ */
+
+#ifndef KCHART_PART_H
+#define KCHART_PART_H
+
+#include <koChart.h>
+#include <kconfig.h>
+
+class KChartParams;
+
+class KChartPart : public KoChart::Part
+{
+  Q_OBJECT
+    public:
+  KChartPart( QWidget *parentWidget = 0, const char *widgetName = 0, QObject* parent = 0, const char* name = 0, bool singleViewMode = false );
+  ~KChartPart();
+
+  virtual void paintContent( QPainter& painter, const QRect& rect, bool transparent = false, double zoomX = 1.0, double zoomY = 1.0 );
+
+  virtual bool initDoc();
+
+  virtual void setData( const KoChart::Data& data );
+
+  QStringList *axisLabelTextLong() { return &longLabels; }
+  QStringList *axisLabelTextShort() { return &shortLabels; }
+  
+  bool showWizard();
+  void initLabelAndLegend();
+  void loadConfig(KConfig *conf);
+  void saveConfig(KConfig *conf);
+  void defaultConfig();
+  KoChart::Data *data() {return &currentData; };
+  KChartParams* params() const { return _params; };
+  // save and load
+  virtual QDomDocument saveXML();
+  virtual bool loadXML( QIODevice *, const QDomDocument& doc );
+  bool m_bLoading; // Kalle: get rid of that one :)
+  bool isLoading() {
+    return m_bLoading;
+  }
+    bool canChangeValue() const { return m_bCanChangeValue;}
+    virtual void setCanChangeValue(bool b ) { m_bCanChangeValue = b;}
+ signals:
+  void docChanged();
+
+ protected:
+  void initRandomData();
+  virtual KoView* createViewInstance( QWidget* parent, const char* name );
+  bool loadOldXML( const QDomDocument& doc );
+  bool loadData( const QDomDocument& doc, KoChart::Data& currentData );
+
+ private:
+  QDomElement createElement(const QString &tagName, const QFont &font, QDomDocument &doc) const;
+  QFont toFont(QDomElement &element) const;
+  KoChart::Data currentData;
+  QStringList longLabels, shortLabels;
+  KChartParams* _params;
+  QWidget* _parentWidget;
+  bool m_bCanChangeValue;
+};
+
+class WizardExt : public KoChart::WizardExtension
+{
+public:
+    WizardExt( KoChart::Part *part )
+        : KoChart::WizardExtension( part ) {};
+
+    virtual bool show() { return static_cast<KChartPart *>( part() )->showWizard(); }
+};
+
+#endif
