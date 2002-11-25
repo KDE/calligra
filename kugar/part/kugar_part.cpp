85f0816c2f9a3cff39677fe6929ffc2bea4f6eaa
Joseph Wenninger <kde@jowenn.at>
2002-11-25 21:08:11 +0000

    dont't build the old shell at the moment
    svn path=/trunk/koffice/; revision=190476

--- kugar/part/kugar_part.cpp
+++ kugar/part/kugar_part.cpp
@@ -15,6 +15,10 @@
 #include <qfile.h>
 #include <kglobal.h>
 #include <kiconloader.h>
+#include <qwidget.h>
+#include <kfiledialog.h>
+#include <kdebug.h>
+#include <kmessagebox.h>
 
 #if defined(HAVE_CONFIG_H)
 #include "config.h"
@@ -23,224 +27,78 @@
 #include "kugar.h"
 
 #include "kugar_part.h"
+#include "kugar_view.h"
+#include "kugar_factory.h"
 
-
-KInstance *KugarFactory::s_instance = 0L;
-KAboutData *KugarFactory::s_aboutdata = 0L;
-
-
-// The part's entry point.
-
-extern "C" void *init_libkugarpart()
+KugarPart::KugarPart( QWidget *parentWidget, const char *widgetName, QObject* parent ,
+                const char* name , bool singleViewMode):KoDocument(parentWidget,widgetName,parent,name,singleViewMode)
 {
-    KGlobal::locale()->insertCatalogue("kugar");
-    return new KugarFactory;
+	setInstance(KugarFactory::global(),false);
 }
-
-
-// The factory ctor.
-
-KugarFactory::KugarFactory(QObject *parent, const char* name):KoFactory(parent,name)
-{
-	global();	
-}
-
-KInstance* KugarFactory::global()
-{
-    if ( !s_instance )
-    {
-        s_instance = new KInstance(aboutData());
-        s_instance->iconLoader()->addAppDir("koffice");
-        s_instance->iconLoader()->addAppDir("kugar");
-
-    }
-    return s_instance;
-}
-
-// The factory dtor.
-
-KugarFactory::~KugarFactory()
-{
-	delete s_instance;
-	s_instance=0;
-	delete s_aboutdata;
-	s_aboutdata=0;
-}
-
-KAboutData *KugarFactory::aboutData()
-{
-	if (!s_aboutdata)
-	s_aboutdata=new KAboutData("kugar",
-                                                   I18N_NOOP("Kugar"),
-                   	                               VERSION);
-	return s_aboutdata;
-}
-
-// Create a new part.
-
-KParts::Part* KugarFactory::createPartObject( QWidget *parentWidget, const char *,
-        QObject* parent, const char* name, const char*, const QStringList & data)
-{
-	QString forcedUserTemplate;
-	for (QStringList::const_iterator it=data.begin();it!=data.end();++it)
-	{
-		QString tmp=(*it);
-		if (tmp.startsWith("template="))
-			forcedUserTemplate=tmp.right(tmp.length()-9);
-	}
-	return ( new KugarPart(parentWidget,name,forcedUserTemplate));
-}
-
-
-// The view ctor.
-
-KugarPart::KugarPart(QWidget *parent,const char *name,const QString &forcedUserTemplate)
-	: KParts::ReadOnlyPart(parent,name)
-{
-	m_forcedUserTemplate=forcedUserTemplate;
-	setInstance(KugarFactory::global());
-
-	view = new MReportViewer(parent);
-
-	connect(view,SIGNAL(preferedTemplate(const QString &)),
-		     SLOT(slotPreferedTemplate(const QString &)));
-
-	view -> setFocusPolicy(QWidget::ClickFocus);
-	view -> show();
-
-	setWidget(view);
-
-	m_extension = new KugarBrowserExtension(this);
-
-
-	// Define the actions.
-
-	KStdAction::prior(view,SLOT(slotPrevPage()),actionCollection(),"kuPrevPage");
-	KStdAction::next(view,SLOT(slotNextPage()),actionCollection(),"kuNextPage");
-	KStdAction::firstPage(view,SLOT(slotFirstPage()),actionCollection(),"kuFirstPage");
-	KStdAction::lastPage(view,SLOT(slotLastPage()),actionCollection(),"kuLastPage");
-
-	setXMLFile("kugarpart.rc");
-
-}
-
-
-// The view dtor.
 
 KugarPart::~KugarPart()
 {
-	closeURL();
+//	closeURL();
 }
 
-
-// Open a data file.
-
-bool KugarPart::openFile()
+bool KugarPart::loadXML( QIODevice *file, const QDomDocument & )
 {
-	bool ok = false;
-	QFile f(m_file);
-
-	if (f.open(IO_ReadOnly))
-	{
-		if (view -> setReportData(&f))
+	bool ok=true;
+        if (file)
+        {
+		file->reset();
+		m_reportData=QString(file->readAll());
+		if (m_reportData.length()!=0)
 		{
-			if (view -> renderReport())
-				ok = true;
+			kdDebug()<<m_reportData<<endl;
+			QPtrList<KoView> vs= views();
+			for (KoView *v=vs.first();v;v=vs.next())
+			{
+				if (!(static_cast<KugarView*>(v->qt_cast("KugarView"))->renderReport(m_reportData)))
+				{
+					ok=false;
+					break;
+				}
+			}		
+                        
+			if (!ok) KMessageBox::sorry(0,i18n("Invalid data file: %1").arg(m_file));
 		}
 		else
-			KMessageBox::sorry(widget(),i18n("Invalid data file: %1").arg(m_file));
-
-		f.close();
-	}
-	else
-		KMessageBox::sorry(widget(),i18n("Unable to open data file: %1").arg(m_file));
-
-	return ok;
-}
-
-
-// Close the data file.
-
-bool KugarPart::closeURL()
-{
-	// Nothing to do.
-
-	return true;
-}
-
-
-// Print the report.
-
-void KugarPart::print()
-{
-	view -> printReport();
-}
-
-
-// Handle a prefered template in the data.  This is actually the only way to
-// set the template when used as a browser extension.
-
-void KugarPart::slotPreferedTemplate(const QString &tpl)
-{
-	KURL url(m_forcedUserTemplate.isEmpty()?tpl:m_forcedUserTemplate);
-	QString localtpl;
-	bool isTemp = false;
-
-	if (url.isMalformed())
-	{
-		if (tpl.find('/') >= 0)
-			localtpl = tpl;
-		else
-			localtpl = kapp -> dirs() -> findResource("data","kugar/templates/" + tpl);
-	}
-	else
-	{
-		if (KIO::NetAccess::download(url,localtpl))
-			isTemp = true;
-		else
-			KMessageBox::sorry(widget(),i18n("Unable to download template file: %1").arg(url.prettyURL()));
-	}
-
-	if (!localtpl.isNull())
-	{
-		QFile f(localtpl);
-
-		if (f.open(IO_ReadOnly))
 		{
-			if (!view -> setReportTemplate(&f))
-				KMessageBox::sorry(widget(),i18n("Invalid template file: %1").arg(localtpl));
-
-			f.close();
+			ok=false;
+			KMessageBox::sorry(0,i18n("The zero sized data file %1  can't be rendered").arg(m_file));
 		}
-		else
-			KMessageBox::sorry(widget(),i18n("Unable to open template file: %1").arg(localtpl));
 
-		if (isTemp)
-			KIO::NetAccess::removeTempFile(localtpl);
+        }
+        else
+	{
+		ok=false;
+                KMessageBox::sorry(0,i18n("Unable to open data file: %1").arg(m_file));
 	}
+
+        return ok;	
 }
 
-
-// The browser extension ctor.
-
-KugarBrowserExtension::KugarBrowserExtension(KugarPart *parent)
-	: KParts::BrowserExtension(parent,"KugarBrowserExtension")
+bool KugarPart::initDoc()
 {
-    emit enableAction( "print", true );
+        QString filename;
+	bool ok=false;
+	KURL url=KFileDialog::getOpenURL(QString::null,
+                           "*.kud|Kugar Data File");
+	if (url.isValid())
+	{
+		ok=openURL(url);
+	}
+        return ok;
+
+    // If nothing is loaded, do initialize here
+    return TRUE;
 }
 
-
-// The browser extension dtor.
-
-KugarBrowserExtension::~KugarBrowserExtension()
+KoView* KugarPart::createViewInstance( QWidget* parent, const char* name )
 {
+    KugarView *v=new KugarView( this, parent, name );
+    v->renderReport(m_reportData);
+    return v;
 }
 
-
-// This is the browser hook to print the report.
-
-void KugarBrowserExtension::print()
-{
-	((KugarPart *)parent()) -> print();
-}
-#include "kugar_part.moc"
--- kugar/part/kugar_part.h
+++ kugar/part/kugar_part.h
@@ -6,10 +6,8 @@
 #ifndef _KUGAR_PART_H
 #define _KUGAR_PART_H
 
-#include <kparts/browserextension.h>
-#include <klibloader.h>
 
-#include <koFactory.h>
+#include <koDocument.h>
 
 #include "kugar.h"
 
@@ -17,69 +15,32 @@
 class KInstance;
 class KugarBrowserExtension;
 
-
-class KugarFactory : public KoFactory
-{
-    Q_OBJECT
-public:
-    KugarFactory( QObject* parent = 0, const char* name = 0 );
-    ~KugarFactory();
-
-    virtual KParts::Part *createPartObject( QWidget *parentWidget = 0, const char *widgetName = 0,
-                QObject *parent = 0, const char *name = 0, const char *classname = "KoDocument",
-                const QStringList &args = QStringList() );
-
-    static KInstance* global();
-
-    // _Creates_ a KAboutData but doesn't keep ownership
-    static KAboutData* aboutData();
-private:
-    static KInstance* s_instance;
-    static KAboutData* s_aboutdata;
-};
-
-
-class KugarPart: public KParts::ReadOnlyPart
+class KugarPart: public KoDocument
 {
 	Q_OBJECT
 
 public:
-	KugarPart(QWidget *parent,const char *name,const QString& forcedUserTemplate);
+        KugarPart( QWidget *parentWidget = 0, const char *widgetName = 0, QObject* parent = 0,
+                const char* name = 0, bool singleViewMode = false);
 	virtual ~KugarPart();
 
-	void print();
-	
-	bool setReportTemplate(const QString &data)
-	{
-		return view -> setReportTemplate(data);
-	}
+	virtual bool initDoc();
+
+	virtual bool loadXML( QIODevice *, const QDomDocument & );
+//	virtual QDomDocument saveXML();
+
+	virtual void paintContent( QPainter& painter, const QRect& rect,
+		bool transparent = FALSE, double zoomX = 1.0, double zoomY = 1.0 ){;}
+private:
+	QString m_reportData;
 
 protected:
-	virtual bool openFile();
-	virtual bool closeURL();
-
-private slots:
-	void slotPreferedTemplate(const QString &);
-
-private:
-	MReportViewer *view;
-	KugarBrowserExtension *m_extension;
-	QString m_forcedUserTemplate;
-};
-
-
-class KugarBrowserExtension : public KParts::BrowserExtension
-{
-	Q_OBJECT
-
-	friend class KugarPart;
-
-public:
-	KugarBrowserExtension(KugarPart *parent);
-	virtual ~KugarBrowserExtension();
+	virtual KoView* createViewInstance( QWidget* parent, const char* name );
 
 public slots:
-	void print();
+	void setForcedUserTemplate(const QString &name){;}
+
 };
+
 
 #endif 
