3eb976a1fef4dc1c22efbfe88ce01da1058d6978
Joseph Wenninger <kde@jowenn.at>
2002-12-01 21:51:08 +0000

    half ready relation handler
    svn path=/trunk/koffice/; revision=191511

---
+++ kexi/plugins/relations/kexirelationhandlerproxy.h
@@ -0,0 +1,47 @@
+/* This file is part of the KDE project
+   Copyright (C) 2002   Joseph Wenninger <jowenn@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef KEXIRELATIONHANDLERPROXY_H
+#define KEXIRELATIONHANDLERPROXY_H
+
+
+#include "kexirelationhandler.h"
+#include "kexiprojecthandlerproxy.h"
+#include <kxmlguiclient.h>
+
+class KexiView;
+
+class KexiRelationHandlerProxy : public KexiProjectPartProxy, public KXMLGUIClient
+{
+	Q_OBJECT
+
+	public:
+		KexiRelationHandlerProxy(KexiRelationHandler *part, KexiView*view);
+
+                virtual KexiPartPopupMenu       *groupContext();
+                virtual KexiPartPopupMenu       *itemContext(const QString&);
+
+		virtual void executeItem(const QString&);
+
+	protected slots:
+		void slotShowRelationEditor();
+	private:
+};
+
+#endif
