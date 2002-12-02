520c8edd1ac6c8f1051a15a19cf31f8499f4d2bc
David Faure <faure@kde.org>
2002-12-02 01:50:58 +0000

    This should solve the problem of constantly having to update so many files :) +#include <config.h> -static const char* KWORD_VERSION="1.2"; +static const char* KWORD_VERSION=VERSION;
    svn path=/trunk/koffice/; revision=191537

--- kword/kwaboutdata.h
+++ kword/kwaboutdata.h
@@ -22,13 +22,15 @@
 
 #include <kaboutdata.h>
 #include <klocale.h>
+#include <config.h> // for VERSION
 
 static const char* KWORD_DESCRIPTION=I18N_NOOP("KOffice Word Processor");
 // First official public release Oct 2000 version 0.8
 // KOffice-1.1-beta1: version 0.9
 // KOffice-1.1-beta2: version 1.1 (to remove confusion)
 // KOffice-1.2: version 1.2
-static const char* KWORD_VERSION="1.2";
+// From then on: always the same as the KOffice version
+static const char* KWORD_VERSION=VERSION;
 
 KAboutData * newKWordAboutData()
 {
