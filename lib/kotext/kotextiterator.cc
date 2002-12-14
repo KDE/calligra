3f9967149165a8c49ea5046cd80ac98fd44950c9
David Faure <faure@kde.org>
2002-12-14 01:12:51 +0000

    * A convenient way to iterate over paragraphs, possibly in multiple textobjects, * with many options (from cursor, backwards, in selection). To be used in find/replace, but also in background spellchecking, etc.
    svn path=/trunk/koffice/; revision=193842

---
+++ lib/kotext/kotextiterator.cc
@@ -0,0 +1,206 @@
+/* This file is part of the KDE project
+   Copyright (C) 2002 David Faure <faure@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include "kotextiterator.h"
+#include "kotextview.h"
+#include <kfinddialog.h>
+#include <kdebug.h>
+
+//// TODO connect to kotextobject's destroyed() signal (QObject)
+//// and same with the paragraph pointers (signal from kotextobject)
+// QGuardedPtr sets to 0 but doesn't allow skipping properly.
+
+void KoTextIterator::init( const QValueList<KoTextObject *> & lstObjects, KoTextView* textView, int options )
+{
+    Q_ASSERT( !lstObjects.isEmpty() );
+    if ( textView )
+        // Hmm textView->textObject() should be first in lstObjects!
+        // by contract? or should we change order over here?
+        Q_ASSERT( lstObjects.first() == textView->textObject() );
+
+    m_lstObjects.clear();
+    m_firstParag = 0;
+    m_firstIndex = 0;
+    m_options = options;
+
+    // 'From Cursor' option
+    if ( options & KFindDialog::FromCursor )
+    {
+        if ( textView ) {
+            m_firstParag = textView->cursor()->parag();
+            m_firstIndex = textView->cursor()->index();
+        } else {
+            // !? FromCursor option can't work
+            m_options &= ~KFindDialog::FromCursor;
+            kdWarning(32500) << "FromCursor specified, but no textview?" << endl;
+        }
+    } // no else here !
+
+    bool forw = ! ( options & KFindDialog::FindBackwards );
+
+    // 'Selected Text' option
+    if ( textView && ( options & KFindDialog::SelectedText ) )
+    {
+        KoTextObject* textObj = lstObjects.first();
+        KoTextCursor c1 = textObj->textDocument()->selectionStartCursor( KoTextDocument::Standard );
+        KoTextCursor c2 = textObj->textDocument()->selectionEndCursor( KoTextDocument::Standard );
+        if ( !m_firstParag ) // not from cursor
+        {
+            m_firstParag = forw ? c1.parag() : c2.parag();
+            m_firstIndex = forw ? c1.index() : c2.index();
+        }
+        m_lastParag = forw ? c2.parag() : c1.parag();
+        m_lastIndex = forw ? c2.index() : c1.index();
+        // Find in the selection only -> only one textobject
+        m_lstObjects.append( textObj );
+        m_currentTextObj = m_lstObjects.begin();
+    }
+    else
+    {
+        // Not "selected text" -> loop through all textobjects
+        m_lstObjects = lstObjects;
+        KoTextParag* firstParag = m_lstObjects.first()->textDocument()->firstParag();
+        int firstIndex = 0;
+        KoTextParag* lastParag = m_lstObjects.last()->textDocument()->lastParag();
+        int lastIndex = lastParag->length()-1;
+        if ( m_firstParag )
+        {
+            // from cursor. So we have to start from the textview's object, even when going backwards
+            m_currentTextObj = m_lstObjects.begin();
+            // so we end at the last textobject's end - which end depends on the direction
+            if ( forw ) {
+                m_lastParag = lastParag;
+                m_lastIndex = lastIndex;
+            } else {
+                m_lastParag = m_lstObjects.last()->textDocument()->firstParag();
+                m_lastIndex = 0;
+            }
+        }
+        else // not from cursor.
+        {
+            m_firstParag = forw ? firstParag : lastParag;
+            m_lastIndex = forw ? firstIndex : lastIndex;
+            m_currentTextObj = forw ? m_lstObjects.begin() : m_lstObjects.fromLast();
+        }
+    }
+
+    assert( *m_currentTextObj ); // all branches set it
+    assert( m_firstParag );
+    assert( m_lastParag );
+    m_currentParag = m_firstParag;
+}
+
+void KoTextIterator::restart()
+{
+    m_currentParag = m_firstParag;
+    bool forw = ! ( m_options & KFindDialog::FindBackwards );
+    if ( (m_options & KFindDialog::FromCursor) || forw )
+        m_currentTextObj = m_lstObjects.begin();
+    else
+        m_currentTextObj = m_lstObjects.fromLast();
+}
+
+// Go to next paragraph that we must iterate over
+void KoTextIterator::operator++()
+{
+    if ( !m_currentParag ) {
+        kdDebug() << k_funcinfo << " called past the end" << endl;
+        return;
+    }
+    bool forw = ! ( m_options & KFindDialog::FindBackwards );
+    KoTextParag* parag = forw ? m_currentParag->next() : m_currentParag->prev();
+    if ( parag )
+    {
+        m_currentParag = parag;
+    }
+    else
+    {
+        // We need to go to the next textobject
+        if ( forw ) {
+            ++m_currentTextObj;
+            if ( m_currentTextObj == m_lstObjects.end() )
+                m_currentParag = 0L; // done
+            else
+                m_currentParag = (*m_currentTextObj)->textDocument()->firstParag();
+        } else {
+            if ( m_currentTextObj == m_lstObjects.begin() )
+            {
+                --m_currentTextObj;
+                m_currentParag = (*m_currentTextObj)->textDocument()->lastParag();
+            } else
+                m_currentParag = 0L; // done
+        }
+    }
+}
+
+bool KoTextIterator::atEnd() const
+{
+    // operator++ sets m_currentParag to 0 when it's done
+    return m_currentParag == 0L;
+}
+
+int KoTextIterator::currentStartIndex() const
+{
+    return currentTextAndIndex().first;
+}
+
+QString KoTextIterator::currentText() const
+{
+    return currentTextAndIndex().second;
+}
+
+QPair<int, QString> KoTextIterator::currentTextAndIndex() const
+{
+    QString str = m_currentParag->string()->toString();
+    str.truncate( str.length() - 1 ); // damn trailing space
+    bool forw = ! ( m_options & KFindDialog::FindBackwards );
+    if ( m_currentParag == m_firstParag )
+    {
+        if ( m_firstParag == m_lastParag ) // special case, needs truncating at both ends
+            return qMakePair( m_firstIndex, str.mid( m_firstIndex, m_lastIndex - m_firstIndex ) );
+        else
+            return forw ? qMakePair( m_firstIndex, str.mid( m_firstIndex ) )
+                        : qMakePair( 0, str.left( m_firstIndex ) );
+    }
+    if ( m_currentParag == m_lastParag )
+    {
+        return forw ? qMakePair( 0, str.left( m_lastIndex ) )
+                    : qMakePair( m_lastIndex, str.mid( m_lastIndex ) );
+    }
+    // Not the first parag, nor the last, so we return it all
+    return qMakePair( 0, str );
+}
+
+bool KoTextIterator::hasText() const
+{
+    // Same logic as currentTextAndIndex, but w/o calling it, to avoid all the string copying
+    bool forw = ! ( m_options & KFindDialog::FindBackwards );
+    int strLength = m_currentParag->string()->length() - 1;
+    if ( m_currentParag == m_firstParag )
+    {
+        if ( m_firstParag == m_lastParag )
+            return m_firstIndex < m_lastIndex;
+        else
+            return forw ? m_firstIndex < strLength
+                        : m_firstIndex > 0;
+    }
+    if ( m_currentParag == m_lastParag )
+        return forw ? m_lastIndex > 0
+                    : m_lastIndex < strLength;
+    return strLength > 0;
+}
---
+++ lib/kotext/kotextiterator.h
@@ -0,0 +1,122 @@
+/* This file is part of the KDE project
+   Copyright (C) 2002 David Faure <faure@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef KOTEXTITERATOR_H
+#define KOTEXTITERATOR_H
+
+#include <qvaluelist.h>
+#include <qstring.h>
+#include <qpair.h>
+class KoTextParag;
+class KoTextObject;
+class KoTextView;
+
+/**
+ * A convenient way to iterate over paragraphs, possibly in multiple textobjects,
+ * with many options (from cursor, backwards, in selection).
+ * @short General purpose paragraph iterator
+ */
+class KoTextIterator
+{
+public:
+    /**
+     * @param options see KFindDialog
+     */
+    KoTextIterator( const QValueList<KoTextObject *> & lstObjects, KoTextView* textView, int options ) {
+        init( lstObjects, textView, options );
+    }
+    void init( const QValueList<KoTextObject *> & lstObjects, KoTextView* textView, int options );
+
+    /**
+     * Restart from the beginning - assumes same parameters given to init
+     */
+    void restart();
+
+    /**
+     * Change options during iteration. ## Not sure how if all cases will be handled :}
+     * At least this is useful for the "Replace All" button during replacing.
+     */
+    void setOptions( int options ) { m_options = options; }
+
+    /**
+     * Go to next paragraph that we must iterate over
+     */
+    void operator++();
+
+    /**
+     * @return true if we have iterated over all paragraphs
+     */
+    bool atEnd() const;
+
+    /**
+     * @return true if currentText() isn't empty. The implementation is
+     * faster than calling currentText().isEmpty() though.
+     */
+    bool hasText() const;
+
+    /**
+     * @return the string at the current position of the iterator
+     */
+    QString currentText() const;
+
+    /**
+     * @return the string at the current position of the iterator
+     */
+    KoTextParag* currentParag() const { return m_currentParag; }
+
+    /**
+     * @return the text object in which @ref currentParag() is.
+     */
+    KoTextObject* currentTextObject() const { return *m_currentTextObj; }
+
+    /**
+     * Where in @ref currentParag() does @ref currentText() start?
+     */
+    int currentStartIndex() const;
+
+    /**
+     * @return the string at the current position of the iterator,
+     * as well as the index in the current paragraph.
+     * Use this instead of separate calls to currentText and currentStartIndex,
+     * for performance reasons.
+     */
+    QPair<int, QString> currentTextAndIndex() const;
+
+private:
+    // The reason we use a QValueList of pointers instead of QPtrList
+    // is that having a QPtrListIterator member var can't work, one can't
+    // initialize it afterwards.
+    QValueList<KoTextObject *> m_lstObjects;
+    int m_options;
+
+    // This is relative to the first textobject in m_lstObjects
+    // We always start from this paragraph (even when going backwards)
+    KoTextParag* m_firstParag;
+    int m_firstIndex;
+
+    // This is relative to the last textobject in m_lstObjects
+    // We're done when we hit this paragraph
+    KoTextParag* m_lastParag;
+    int m_lastIndex;
+
+    // Our current position
+    QValueList<KoTextObject *>::Iterator m_currentTextObj;
+    KoTextParag* m_currentParag;
+};
+
+#endif
