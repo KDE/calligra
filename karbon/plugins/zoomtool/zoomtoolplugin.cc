4510280e74e4909316c6d36c07b1aace6bea8838
Rob Buis <buis@kde.org>
2002-12-20 16:30:45 +0000

    Should be obvious, a plugin tool for zooming :)
    svn path=/trunk/koffice/; revision=195037

---
+++ karbon/plugins/zoomtool/zoomtoolplugin.cc
@@ -0,0 +1,125 @@
+/* This file is part of the KDE project
+   Copyright (C) 2002, The Karbon Developers
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+
+*/
+
+#include <qevent.h>
+
+#include <klocale.h>
+
+#include "zoomtoolplugin.h"
+#include <karbon_part_base.h>
+#include <karbon_part.h>
+#include <karbon_view_base.h>
+#include <karbon_view.h>
+#include <core/vcanvas.h>
+#include <render/vpainter.h>
+#include <render/vpainterfactory.h>
+#include <kgenericfactory.h>
+
+typedef KGenericFactory<VZoomTool, KarbonViewBase> ZoomToolPluginFactory;
+K_EXPORT_COMPONENT_FACTORY( karbon_zoomtoolplugin, ZoomToolPluginFactory( "karbonzoomtoolplugin" ) );
+
+VZoomTool::VZoomTool( KarbonViewBase* view, const char *name, const QStringList & )
+	: VTool( (KarbonView *)view, name ), VKarbonPlugin( view, name )
+{
+	registerTool( this );
+}
+
+VZoomTool::~VZoomTool()
+{
+}
+
+QString
+VZoomTool::contextHelp()
+{
+	QString s = i18n( "<qt><b>Zoom tool:</b><br>" );
+	return s;
+}
+
+void
+VZoomTool::activate()
+{
+	//view()->statusMessage()->setText( i18n( "Zoom Tool" ) );
+	//view()->canvasWidget()->viewport()->setCursor( QCursor( Qt::crossCursor ) );
+}
+
+void
+VZoomTool::deactivate()
+{
+}
+
+void
+VZoomTool::draw()
+{
+	VPainter *painter = view()->painterFactory()->editpainter();
+	painter->setRasterOp( Qt::NotROP );
+
+	if( isDragging() )
+	{
+		painter->setPen( Qt::DotLine );
+		painter->newPath();
+		painter->moveTo( KoPoint( first().x(), first().y() ) );
+		painter->lineTo( KoPoint( m_current.x(), first().y() ) );
+		painter->lineTo( KoPoint( m_current.x(), m_current.y() ) );
+		painter->lineTo( KoPoint( first().x(), m_current.y() ) );
+		painter->lineTo( KoPoint( first().x(), first().y() ) );
+		painter->strokePath();
+	}
+}
+
+void
+VZoomTool::mouseButtonPress()
+{
+	m_current = first();
+
+	recalc();
+
+	draw();
+}
+
+void
+VZoomTool::mouseButtonRelease()
+{
+}
+
+void
+VZoomTool::mouseDrag()
+{
+	draw();
+
+	recalc();
+
+	draw();
+}
+
+void
+VZoomTool::mouseDragRelease()
+{
+	KoPoint fp = first();
+	KoPoint lp = last();
+	view()->canvasWidget()->setContentsRect( KoRect( fp.x(), fp.y(), lp.x() - fp.x(), lp.y() - fp.y() ).normalize() );
+	view()->part()->repaintAllViews( KoRect( fp.x(), fp.y(), lp.x() - fp.x(), lp.y() - fp.y() ).normalize() );
+}
+
+void
+VZoomTool::recalc()
+{
+	m_current = last();
+}
+
---
+++ karbon/plugins/zoomtool/zoomtoolplugin.h
@@ -0,0 +1,62 @@
+/* This file is part of the KDE project
+   Copyright (C) 2002, The Karbon Developers
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+
+*/
+
+#ifndef __VZOOMTOOL_H__
+#define __VZOOMTOOL_H__
+
+#include "koPoint.h"
+#include <qstring.h>
+
+#include <tools/vtool.h>
+#include <core/vkarbonplugin.h>
+
+class KarbonViewBase;
+/**
+ */
+
+class VZoomTool : public VTool, public VKarbonPlugin
+{
+public:
+	VZoomTool( KarbonViewBase *view, const char *, const QStringList & );
+	~VZoomTool(); 
+
+	virtual void activate();
+	virtual void deactivate();
+
+	virtual QString name() { return i18n( "Zoom Tool" ); }
+	virtual QString contextHelp();
+	virtual QString icon() { return "zoom"; }
+
+protected:
+	void draw();
+
+	virtual void mouseButtonPress();
+	virtual void mouseButtonRelease();
+	virtual void mouseDrag();
+	virtual void mouseDragRelease();
+
+	void recalc();
+
+protected:
+	KoPoint m_current;
+};
+
+#endif
+
