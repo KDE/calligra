9bddaffbe5c064a93f41cfef2aa801ce8c792b8e
Lukáš Tinkl <lukas@kde.org>
2003-02-27 13:40:24 +0000

    sync the param, fix the docu
    svn path=/trunk/koffice/; revision=209992

--- lib/kofficecore/koMainWindow.h
+++ lib/kofficecore/koMainWindow.h
@@ -259,13 +259,13 @@ protected:
     /**
      * Saves the document, asking for a filename if necessary.
      *
-     * @param _saveas if set to TRUE the user is always prompted for a filename
+     * @param saveas if set to TRUE the user is always prompted for a filename
      *
-     * @return TRUE on success or on cancel, false on error
+     * @return TRUE on success, false on error or cancel
      *         (don't display anything in this case, the error dialog box is also implemented here
      *         but restore the original URL in slotFileSaveAs)
      */
-    virtual bool saveDocument( bool _saveas = false );
+    virtual bool saveDocument( bool saveas = false );
 
     virtual void closeEvent( QCloseEvent * e );
     virtual void resizeEvent( QResizeEvent * e );
