b15d3d72ce62b39cbabb5af4a1dfc79468aa42c0
Joseph Wenninger <kde@jowenn.at>
2003-02-27 23:39:48 +0000

    Some time ago we decided to make core LGPL. I've eventually done the license header changes. Better now than later, before new contributers commit code
    svn path=/trunk/koffice/; revision=210168

--- kexi/core/kexi_aboutdata.h
+++ kexi/core/kexi_aboutdata.h
@@ -1,19 +1,22 @@
-/***************************************************************************
-                          kexi_aboutdata.h  -  description
-                             -------------------
-    begin                : Sun Jun  9 12:15:11 CEST 2002
-    copyright            : (C) 2002 by lucijan busch, Joseph Wenninger
-    email                : lucijan@gmx.at, jowenn@kde.org
- ***************************************************************************/
+/* This file is part of the KDE project
+   Copyright (C) 2002, 2003 Lucijan Busch <lucijan@gmx.at>
+   Copyright (C) 2002, 2003 Joseph Wenninger <jowenn@kde.org>
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
 
 #ifndef _KEXI_ABOU_DATA_
 #define _KEXI_ABOU_DATA_
@@ -30,7 +33,7 @@ static const char *version=VERSION;
 KAboutData *newKexiAboutData()
 {
 	KAboutData *aboutData=new KAboutData( "kexi", I18N_NOOP("Kexi"),
-		version, description, KAboutData::License_GPL,
+		version, description, KAboutData::License_LGPL,
 		I18N_NOOP("(c) 2002, Kexi Team"), 0, "http://luci.bux.at/projects/kexi", "lucijan@gmx.at");
 	aboutData->addAuthor("Lucijan Busch",I18N_NOOP("Developer & Maintainer"), "lucijan@gmx.at");
 	aboutData->addAuthor("Peter Simonsson",I18N_NOOP("Developer"),"psn@linux.se");
--- kexi/core/kexiview.cpp
+++ kexi/core/kexiview.cpp
@@ -1,20 +1,22 @@
-/***************************************************************************
-                          kexiview.cpp  -  description
-                             -------------------
-    begin                : Sun Jun 9 2002
-    copyright            : (C) 2002 by lucijan busch
-			   (C) 2002 by Joseph Wenninger <jowenn@kde.org>
-    email                : lucijan@gmx.at
- ***************************************************************************/
+/* This file is part of the KDE project
+   Copyright (C) 2002, 2003 Lucijan Busch <lucijan@gmx.at>
+   Copyright (C) 2002, 2003 Joseph Wenninger <jowenn@kde.org>
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
 
 #include <qsqlquery.h>
 #include <qsqldatabase.h>
