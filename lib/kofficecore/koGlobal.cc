006df860af99f7957f2bf6e41200fc456a5d3c82
Nicolas Goutte <nicolasg@snafu.de>
2003-03-05 19:57:29 +0000

    Change the code for page formats: - centralize it in a single table - rename the "DIN" to "ISO" (as KDE's default language is en_US)
    svn path=/trunk/koffice/; revision=211563

--- lib/kofficecore/koGlobal.cc
+++ lib/kofficecore/koGlobal.cc
@@ -1,5 +1,6 @@
 /* This file is part of the KDE project
    Copyright (C) 2001 David Faure <faure@kde.org>
+   Copyright 2003 Nicolas GOUTTE <goutte@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -28,119 +29,67 @@
 #include <ksimpleconfig.h>
 #include <kstandarddirs.h>
 
+struct PageFormatInfo
+{
+    KoFormat format;
+    KPrinter::PageSize kprinter;
+    const char* shortName;
+    const char* descriptiveName;
+    double width; // in mm
+    double height; // in mm
+};
+
+const PageFormatInfo pageFormatInfo[]=
+{
+    { PG_DIN_A3,        KPrinter::A3,           "A3",           I18N_NOOP("ISO A3"),       297.0,  420.0 },
+    { PG_DIN_A4,        KPrinter::A4,           "A4",           I18N_NOOP("ISO A4"),       210.0,  297.0 },
+    { PG_DIN_A5,        KPrinter::A5,           "A5",           I18N_NOOP("ISO A5"),       148.0,  210.0 },
+    { PG_US_LETTER,     KPrinter::Letter,       "Letter",       I18N_NOOP("US Letter"),    216.0,  279.0 }, // should be 215.9 mm x 279.4 mm
+    { PG_US_LEGAL,      KPrinter::Legal,        "Legal",        I18N_NOOP("US Legal"),     216.0,  356.0 }, // should be 215.9 mm x 335.6 mm
+    { PG_SCREEN,        KPrinter::A4,           "Screen",       I18N_NOOP("Screen"), PG_A4_HEIGHT, PG_A4_WIDTH }, // Custom so fall back to A4
+    { PG_CUSTOM,        KPrinter::A4,           "Custom",       I18N_NOOP("Custom"), PG_A4_WIDTH, PG_A4_HEIGHT }, // Custom so fall back to A4
+    { PG_DIN_B5,        KPrinter::B5,           "B5",           I18N_NOOP("ISO B5"),       182.0,  257.0 },
+    { PG_US_EXECUTIVE,  KPrinter::Executive,    "Executive",    I18N_NOOP("US Executive"), 191.0,  254.0 }, // should be 190.5 mm x 254.0 mm
+    { PG_DIN_A0,        KPrinter::A0,           "A0",           I18N_NOOP("ISO A0"),       841.0, 1189.0 },
+    { PG_DIN_A1,        KPrinter::A1,           "A1",           I18N_NOOP("ISO A1"),       594.0,  841.0 },
+    { PG_DIN_A2,        KPrinter::A2,           "A2",           I18N_NOOP("ISO A2"),       420.0,  594.0 },
+    { PG_DIN_A6,        KPrinter::A6,           "A6",           I18N_NOOP("ISO A6"),       105.0,  148.0 },
+    { PG_DIN_A7,        KPrinter::A7,           "A7",           I18N_NOOP("ISO A7"),        74.0,  105.0 },
+    { PG_DIN_A8,        KPrinter::A8,           "A8",           I18N_NOOP("ISO A8"),        52.0,   74.0 },
+    { PG_DIN_A9,        KPrinter::A9,           "A9",           I18N_NOOP("ISO A9"),        37.0,   52.0 },
+    { PG_DIN_B0,        KPrinter::B0,           "B0",           I18N_NOOP("ISO B0"),      1030.0, 1456.0 },
+    { PG_DIN_B1,        KPrinter::B1,           "B1",           I18N_NOOP("ISO B1"),       728.0, 1030.0 },
+    { PG_DIN_B10,       KPrinter::B10,          "B10",          I18N_NOOP("ISO B10"),       32.0,   45.0 },
+    { PG_DIN_B2,        KPrinter::B2,           "B2",           I18N_NOOP("ISO B2"),       515.0,  728.0 },
+    { PG_DIN_B3,        KPrinter::B3,           "B3",           I18N_NOOP("ISO B3"),       364.0,  515.0 },
+    { PG_DIN_B4,        KPrinter::B4,           "B4",           I18N_NOOP("ISO B4"),       257.0,  364.0 },
+    { PG_DIN_B6,        KPrinter::B6,           "B6",           I18N_NOOP("ISO B6"),       128.0,  182.0 }
+};
+
 int KoPageFormat::printerPageSize( KoFormat format )
 {
-    switch ( format ) {
-        case PG_DIN_A3:
-            return KPrinter::A3;
-        case PG_DIN_A4:
-            return KPrinter::A4;
-        case PG_DIN_A5:
-            return KPrinter::A5;
-        case PG_US_LETTER:
-            return KPrinter::Letter;
-        case PG_US_LEGAL:
-            return KPrinter::Legal;
-        case PG_SCREEN:
+    if ( format == PG_SCREEN )
+    {
             kdWarning() << "You use the page layout SCREEN. Printing in DIN A4 LANDSCAPE." << endl;
             return KPrinter::A4;
-        case PG_CUSTOM:
+    }
+    else if ( format == PG_CUSTOM )
+    {
             kdWarning() << "The used page layout (CUSTOM) is not supported by KPrinter. Printing in A4." << endl;
             return KPrinter::A4;
-        case PG_DIN_B5:
-            return KPrinter::B5;
-        case PG_US_EXECUTIVE:
-            return KPrinter::Executive;
-        case PG_DIN_A0:
-            return KPrinter::A0;
-        case PG_DIN_A1:
-            return KPrinter::A1;
-        case PG_DIN_A2:
-            return KPrinter::A2;
-        case PG_DIN_A6:
-            return KPrinter::A6;
-        case PG_DIN_A7:
-            return KPrinter::A7;
-        case PG_DIN_A8:
-            return KPrinter::A8;
-        case PG_DIN_A9:
-            return KPrinter::A9;
-        case PG_DIN_B0:
-            return KPrinter::B0;
-        case PG_DIN_B1:
-            return KPrinter::B1;
-        case PG_DIN_B10:
-            return KPrinter::B10;
-        case PG_DIN_B2:
-            return KPrinter::B2;
-        case PG_DIN_B3:
-            return KPrinter::B3;
-        case PG_DIN_B4:
-            return KPrinter::B4;
-        case PG_DIN_B6:
-            return KPrinter::B6;
     }
-    return KPrinter::A4;   // let's make Tru64's cxx happy
+    else if ( format <= PG_LAST_FORMAT )
+        return pageFormatInfo[ format ].kprinter;
+    else 
+        return KPrinter::A4;
 }
-
-static const double s_widths[ PG_LAST_FORMAT+1 ] = {
-    PG_A3_WIDTH,
-    PG_A4_WIDTH,
-    PG_A5_WIDTH,
-    PG_US_LETTER_WIDTH,
-    PG_US_LEGAL_WIDTH,
-    PG_A4_HEIGHT,  // since we fallback on A4 landscape
-    0, // custom
-    PG_B5_WIDTH,
-    PG_US_EXECUTIVE_WIDTH,
-    841.0,
-    594.0,
-    420.0,
-    105.0,
-    74.0,
-    52.0,
-    37.0,
-    1030.0, // B0
-    728.0,
-    32.0,
-    515.0,
-    364.0,
-    257.0,
-    128.0 // B6
-};
-
-static const double s_heights[ PG_LAST_FORMAT+1 ] = {
-    PG_A3_HEIGHT,
-    PG_A4_HEIGHT,
-    PG_A5_HEIGHT,
-    PG_US_LETTER_HEIGHT,
-    PG_US_LEGAL_HEIGHT,
-    PG_A4_WIDTH, // since we fallback on A4 landscape
-    0, // custom
-    PG_B5_HEIGHT,
-    PG_US_EXECUTIVE_HEIGHT,
-    1189.0, // A0
-    841.0,
-    594.0,
-    148.0,
-    105.0,
-    74.0,
-    52.0,
-    1456.0, // B0
-    1030.0,
-    45.0,
-    728.0,
-    515.0,
-    364.0,
-    182.0 // B6
-};
 
 double KoPageFormat::width( KoFormat format, KoOrientation orientation )
 {
     if ( orientation == PG_LANDSCAPE )
         return height( format, PG_PORTRAIT );
     if ( format <= PG_LAST_FORMAT )
-        return s_widths[ format ];
+        return pageFormatInfo[ format ].width;
     return PG_A4_WIDTH;   // should never happen
 }
 
@@ -149,7 +98,7 @@ double KoPageFormat::height( KoFormat format, KoOrientation orientation )
     if ( orientation == PG_LANDSCAPE )
         return width( format, PG_PORTRAIT );
     if ( format <= PG_LAST_FORMAT )
-        return s_heights[ format ];
+        return pageFormatInfo[ format ].height;
     return PG_A4_HEIGHT;
 }
 
@@ -160,8 +109,8 @@ KoFormat KoPageFormat::guessFormat( double width, double height )
         // We have some tolerance. 1pt is a third of a mm, this is
         // barely noticeable for a page size.
         if ( i != PG_CUSTOM
-             && QABS( width - s_widths[i] ) < 1
-             && QABS( height - s_heights[i] ) < 1 )
+             && kAbs( width - pageFormatInfo[i].width ) < 1
+             && kAbs( height - pageFormatInfo[i].height ) < 1 )
             return static_cast<KoFormat>(i);
     }
     return PG_CUSTOM;
@@ -169,189 +118,36 @@ KoFormat KoPageFormat::guessFormat( double width, double height )
 
 QString KoPageFormat::formatString( KoFormat format )
 {
-    switch( format )
-    {
-        case PG_DIN_A3:
-            return QString::fromLatin1( "A3" );
-        case PG_DIN_A4:
-            return QString::fromLatin1( "A4" );
-        case PG_DIN_A5:
-            return QString::fromLatin1( "A5" );
-        case PG_US_LETTER:
-            return QString::fromLatin1( "Letter" );
-        case PG_US_LEGAL:
-            return QString::fromLatin1( "Legal" );
-        case PG_SCREEN:
-            return QString::fromLatin1( "Screen" );
-        case PG_CUSTOM:
-            return QString::fromLatin1( "Custom" );
-        case PG_DIN_B5:
-            return QString::fromLatin1( "B5" );
-        case PG_US_EXECUTIVE:
-            return QString::fromLatin1( "Executive" );
-        case PG_DIN_A0:
-            return QString::fromLatin1( "A0" );
-        case PG_DIN_A1:
-            return QString::fromLatin1( "A1" );
-        case PG_DIN_A2:
-            return QString::fromLatin1( "A2" );
-        case PG_DIN_A6:
-            return QString::fromLatin1( "A6" );
-        case PG_DIN_A7:
-            return QString::fromLatin1( "A7" );
-        case PG_DIN_A8:
-            return QString::fromLatin1( "A8" );
-        case PG_DIN_A9:
-            return QString::fromLatin1( "A9" );
-        case PG_DIN_B0:
-            return QString::fromLatin1( "B0" );
-        case PG_DIN_B1:
-            return QString::fromLatin1( "B1" );
-        case PG_DIN_B10:
-            return QString::fromLatin1( "B10" );
-        case PG_DIN_B2:
-            return QString::fromLatin1( "B2" );
-        case PG_DIN_B3:
-            return QString::fromLatin1( "B3" );
-        case PG_DIN_B4:
-            return QString::fromLatin1( "B4" );
-        case PG_DIN_B6:
-            return QString::fromLatin1( "B6" );
-    }
-    return QString::fromLatin1( "A4" );   // let's make Tru64's cxx happy
+    if ( format <= PG_LAST_FORMAT )
+        return QString::fromLatin1( pageFormatInfo[ format ].shortName );
+    return QString::fromLatin1( "A4" );
 }
 
 KoFormat KoPageFormat::formatFromString( const QString & string )
 {
-    if ( string == "A3" )
-        return PG_DIN_A3;
-    if ( string == "A4" )
-        return PG_DIN_A4;
-    if ( string == "A5" )
-        return PG_DIN_A5;
-    if ( string == "Letter" )
-        return PG_US_LETTER;
-    if ( string == "Legal" )
-        return PG_US_LEGAL;
-    if ( string == "Screen" )
-        return PG_SCREEN;
-    if ( string == "Custom" )
-        return PG_CUSTOM;
-    if ( string == "B5" )
-        return PG_DIN_B5;
-    if ( string == "Executive" )
-        return PG_US_EXECUTIVE;
-    if ( string == "A0" )
-        return PG_DIN_A0;
-    if ( string == "A1" )
-        return PG_DIN_A1;
-    if ( string == "A2" )
-        return PG_DIN_A2;
-    if ( string == "A6" )
-        return PG_DIN_A6;
-    if ( string == "A7" )
-        return PG_DIN_A7;
-    if ( string == "A8" )
-        return PG_DIN_A8;
-    if ( string == "A9" )
-        return PG_DIN_A9;
-    if ( string == "B0" )
-        return PG_DIN_B0;
-    if ( string == "B1" )
-        return PG_DIN_B1;
-    if ( string == "B10" )
-        return PG_DIN_B10;
-    if ( string == "B2" )
-        return PG_DIN_B2;
-    if ( string == "B3" )
-        return PG_DIN_B3;
-    if ( string == "B4" )
-        return PG_DIN_B4;
-    if ( string == "B6" )
-        return PG_DIN_B6;
+    for ( int i = 0 ; i <= PG_LAST_FORMAT ; ++i )
+    {
+        if (string == QString::fromLatin1( pageFormatInfo[ i ].shortName ))
+            return pageFormatInfo[ i ].format;
+    }
     // We do not know the format name, so we have a custom format
     return PG_CUSTOM;
 }
 
 QString KoPageFormat::name( KoFormat format )
 {
-    switch( format ) {
-        case PG_DIN_A3:
-            return i18n("DIN A3");
-        case PG_DIN_A4:
-            return i18n("DIN A4");
-        case PG_DIN_A5:
-            return i18n("DIN A5");
-        case PG_US_LETTER:
-            return i18n("US Letter");
-        case PG_US_LEGAL:
-            return i18n("US Legal");
-        case PG_SCREEN:
-            return i18n("Screen");
-        case PG_CUSTOM:
-            return i18n("Custom");
-        case PG_DIN_B5:
-            return i18n("DIN B5");
-        case PG_US_EXECUTIVE:
-            return i18n("US Executive");
-        case PG_DIN_A0:
-            return i18n( "DIN A0" );
-        case PG_DIN_A1:
-            return i18n( "DIN A1" );
-        case PG_DIN_A2:
-            return i18n( "DIN A2" );
-        case PG_DIN_A6:
-            return i18n( "DIN A6" );
-        case PG_DIN_A7:
-            return i18n( "DIN A7" );
-        case PG_DIN_A8:
-            return i18n( "DIN A8" );
-        case PG_DIN_A9:
-            return i18n( "DIN A9" );
-        case PG_DIN_B0:
-            return i18n( "DIN B0" );
-        case PG_DIN_B1:
-            return i18n( "DIN B1" );
-        case PG_DIN_B10:
-            return i18n( "DIN B10" );
-        case PG_DIN_B2:
-            return i18n( "DIN B2" );
-        case PG_DIN_B3:
-            return i18n( "DIN B3" );
-        case PG_DIN_B4:
-            return i18n( "DIN B4" );
-        case PG_DIN_B6:
-            return i18n( "DIN B6" );
-    }
-    return i18n("DIN A4");   // let's make Tru64's cxx happy
+    if ( format <= PG_LAST_FORMAT )
+        return i18n( pageFormatInfo[ format ].descriptiveName );
+    return i18n( pageFormatInfo[ PG_DIN_A4 ].descriptiveName );
 }
 
 QStringList KoPageFormat::allFormats()
 {
     QStringList lst;
-    lst += i18n( "DIN A3" );
-    lst += i18n( "DIN A4" );
-    lst += i18n( "DIN A5" );
-    lst += i18n( "US Letter" );
-    lst += i18n( "US Legal" );
-    lst += i18n( "Screen" );
-    lst += i18n( "Custom" );
-    lst += i18n( "DIN B5" );
-    lst += i18n( "US Executive" );
-    lst += i18n( "DIN A0" );
-    lst += i18n( "DIN A1" );
-    lst += i18n( "DIN A2" );
-    lst += i18n( "DIN A6" );
-    lst += i18n( "DIN A7" );
-    lst += i18n( "DIN A8" );
-    lst += i18n( "DIN A9" );
-    lst += i18n( "DIN B0" );
-    lst += i18n( "DIN B1" );
-    lst += i18n( "DIN B10" );
-    lst += i18n( "DIN B2" );
-    lst += i18n( "DIN B3" );
-    lst += i18n( "DIN B4" );
-    lst += i18n( "DIN B6" );
+    for ( int i = 0 ; i <= PG_LAST_FORMAT ; ++i )
+    {
+        lst << i18n( pageFormatInfo[ i ].descriptiveName );
+    }
     return lst;
 }
 
