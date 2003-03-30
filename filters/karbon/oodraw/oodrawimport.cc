ad7eaf7ec4de58624dfe1ad3e90ebdcf020975b0
Rob Buis <buis@kde.org>
2003-03-30 10:23:39 +0000

    Cleanup.
    svn path=/trunk/koffice/; revision=216999

--- filters/karbon/oodraw/oodrawimport.cc
+++ filters/karbon/oodraw/oodrawimport.cc
@@ -270,37 +270,10 @@ OoDrawImport::convert()
 				}
 				VEllipse *ellipse = new VEllipse( &m_document, KoPoint( x, y ), w / 2.0, h / 2.0, type, start, end );
 				appendPen( *ellipse );
+				// arc has no brush
+				if( kind != "arc" )
+					appendBrush( *ellipse );
 				obj = ellipse;
-
-/*if( o.hasAttribute( "draw:kind" ) ) // pie, chord or arc
-{
-e.setAttribute( "type", 8 );
-appendPie( doc, e, o );
-QDomElement type = doc.createElement( "PIETYPE" );
-
-QString kind = o.attribute( "draw:kind" );
-if( kind == "section" )
-{
-appendBrush( doc, e );
-type.setAttribute( "value", 0 );
-}
-else if( kind == "cut" )
-{
-appendBrush( doc, e );
-type.setAttribute( "value", 2 );
-}
-else if( kind == "arc" )
-{
-// arc has no brush
-type.setAttribute( "value", 1 );
-}
-e.appendChild( type );
-}
-else  // circle or ellipse
-{
-e.setAttribute( "type", 3 );
-appendBrush( doc, e );
-}*/
             }
 			else if( name == "draw:line" ) // line
 			{
@@ -534,24 +507,6 @@ brush.setAttribute( "color", draw->attribute( "draw:color" ) );
 e.appendChild( brush );
 }*/
 }
-
-/*
-void OoDrawImport::appendPie( QDomDocument& doc, QDomElement& e, const QDomElement& object )
-{
-    QDomElement angle = doc.createElement( "PIEANGLE" );
-    int start = (int) ( object.attribute( "draw:start-angle" ).toDouble() );
-    angle.setAttribute( "value",  start * 16 );
-    e.appendChild( angle );
-
-    QDomElement length = doc.createElement( "PIELENGTH" );
-    int end = (int) ( object.attribute( "draw:end-angle" ).toDouble() );
-    if( end < start )
-        length.setAttribute( "value",  ( 360 - start + end ) * 16 );
-    else
-        length.setAttribute( "value",  ( end - start ) * 16 );
-    e.appendChild( length );
-}
-*/
 
 void OoDrawImport::createStyleMap( QDomDocument &docstyles )
 {
