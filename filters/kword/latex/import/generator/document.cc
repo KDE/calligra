137d3179d1df330485fc2cf9d488429c2bc407c0
Robert Jacolin <rjacolin@ifrance.com>
2003-04-13 13:04:58 +0000

    Second stage for the latex importer: the kword generator. The generator create a kword file empty with only a maindoc.xml file which contains doc markup.
    svn path=/trunk/koffice/; revision=219977

---
+++ filters/kword/latex/import/generator/document.cc
@@ -0,0 +1,38 @@
+#include <qdom.h>
+#include <qtextstream.h>
+
+#include "kdebug.h"
+#include <koStore.h>
+#include "document.h"
+
+
+bool Document::analyse(QPtrList<Element>* root)
+{
+	return true;
+}
+
+bool Document::generate(KoStore* store)
+{
+	QDomDocument doc("KWORD");
+	doc->appendChild(doc->createProcessingInstruction("xml", 
+				"version=\"1.0\" encoding=\"UTF-8\""));
+  QDomElement root = doc.createElement( "DOC" );
+	root.setAttribute("editor", "LaTex Import Filter");
+	root.setAttribute("mime", "application/x-kword");
+	root.setAttribute("syntaxVersion", "1");
+  doc.appendChild(root);
+	kdDebug() << "serializer" << endl;
+	serialize(store, doc);
+	return true;
+}
+
+void Document::serialize(KoStore* store, QDomDocument doc)
+{
+	QCString str = doc.toCString();
+	qWarning(str);
+	if(store->open("root"))
+	{
+		store->write((const char *)str, str.length());
+		store->close();
+	}
+}
