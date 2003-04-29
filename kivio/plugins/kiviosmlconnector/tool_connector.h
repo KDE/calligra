910f10bca403385f8d42973f40d4f926a6ea8b39
Ian Reinhart Geiser <geiseri@kde.org>
2003-04-29 02:13:14 +0000

    Added the start of the SML based connection tool.  Currently wont load for some unknown reason.  On the off chance someone figures out why, it will use a stencil from a SML path.  Works with strait line, and as soon as I figure out how to store the IDs correctly we can support more XML based connectors.
    svn path=/trunk/koffice/; revision=222685

---
+++ kivio/plugins/kiviosmlconnector/tool_connector.h
@@ -0,0 +1,81 @@
+/*
+ * Kivio - Visual Modelling and Flowcharting
+ * Copyright (C) 2000-2001 theKompany.com & Dave Marotti
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+#ifndef SML_TOOL_CONNECTOR_H
+#define SML_TOOL_CONNECTOR_H
+
+#include "tool.h"
+#include <koPoint.h>
+
+class QMouseEvent;
+class QCursor;
+
+class KivioView;
+class KivioPage;
+class KivioSMLConnector;
+class KivioCustomDragData;
+
+class SMLConnector : public Tool
+{ Q_OBJECT
+public:
+  SMLConnector( KivioView* view );
+  ~SMLConnector();
+
+  virtual void processEvent( QEvent* );
+  virtual void activate();
+  virtual void deactivate();
+  virtual void configure();
+
+  void connector(QRect);
+
+signals:
+  void operationDone();
+
+protected:
+  void mousePress(QMouseEvent *);
+  void mouseMove(QMouseEvent *);
+  void mouseRelease(QMouseEvent *);
+
+  bool startRubberBanding(QMouseEvent*);
+  void continueRubberBanding(QMouseEvent *);
+  void endRubberBanding(QMouseEvent *);
+
+
+
+  QPoint m_startPoint, m_releasePoint;
+
+  // Connector Tool Mode
+  enum
+  {
+      stmNone,
+      stmDrawRubber
+  };
+
+private:
+  // Flag to indicate that we are drawing a rubber band
+  int m_mode;
+  QCursor* m_pConnectorCursor1;
+  QCursor* m_pConnectorCursor2;
+  KivioSMLConnector* m_pStencil;
+  KoPoint startPoint;
+  KivioCustomDragData* m_pDragData;
+};
+
+#endif
+
+
