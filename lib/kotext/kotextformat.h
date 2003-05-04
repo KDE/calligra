57c452195e34ecb7b815e323b21473e7a1f700cb
David Faure <faure@kde.org>
2003-05-04 08:15:52 +0000

    System headers that use #define, suck CCMAIL: 58065-done@bugs.kde.org
    svn path=/trunk/koffice/; revision=223740

--- lib/kotext/kotextformat.h
+++ lib/kotext/kotextformat.h
@@ -23,6 +23,9 @@
    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.
 */
+
+#undef S_NONE // Solaris defines it in sys/signal.h
+
 class KoCharStyle;
 class Q_EXPORT KoTextFormat
 {
