fdc413a7fe0f6e6960111560a691cdde3ef9979e
Rob Buis <buis@kde.org>
2003-05-08 17:00:05 +0000

    Small cleanup.
    svn path=/trunk/koffice/; revision=224687

--- karbon/core/vsegment.cc
+++ karbon/core/vsegment.cc
@@ -1,6 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 2001, The Karbon Developers
-   Copyright (C) 2002, The Karbon Developers
+   Copyright (C) 2001, 2002, 2003 The Karbon Developers
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -22,13 +21,11 @@
 
 #include <qdom.h>
 
-#include "vglobal.h"
 #include "vpainter.h"
 #include "vpath.h"
 #include "vsegment.h"
 
 #include <kdebug.h>
-
 
 VSegment::VSegment( unsigned short deg )
 {
