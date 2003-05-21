6975c4f8d21bbd7057d2722716cb9ea591f733a9
David Faure <faure@kde.org>
2003-05-21 22:09:47 +0000

    Oh boy, did I mess things up here. There was a korichtext.h already.... Folded its contents into kotextdocument.h (two small classes).
    svn path=/trunk/koffice/; revision=227155

--- lib/kotext/kotextdocument.h
+++ lib/kotext/kotextdocument.h
@@ -37,6 +37,7 @@ class KoTextFormatCollection;
 class KoParagVisitor;
 class KoTextFormatter;
 class KoTextParag;
+class CustomItemsMap;
 
 class Q_EXPORT KoTextDocument : public QObject
 {
@@ -589,5 +590,38 @@ inline void KoTextDocument::takeFlow()
 {
     flow_ = 0L;
 }
+
+/**
+ * Base class for "visitors". Visitors are a well-designed way to
+ * apply a given operation to all the paragraphs in a selection, or
+ * in a document. The visitor needs to inherit KoParagVisitor, and implement visit().
+ */
+class KoParagVisitor
+{
+protected:
+    /** protected since this is an abstract base class */
+    KoParagVisitor() {}
+    virtual ~KoParagVisitor() {}
+public:
+    /** Visit the paragraph @p parag, from index @p start to index @p end */
+    virtual bool visit( KoTextParag *parag, int start, int end ) = 0;
+};
+
+class KCommand;
+class QDomElement;
+class KMacroCommand;
+
+/** A CustomItemsMap associates a custom item to an index
+ * Used in the undo/redo info for insert/delete text. */
+class CustomItemsMap : public QMap<int, KoTextCustomItem *>
+{
+public:
+
+    /** Insert all the items from the map, into the existing text */
+    void insertItems( const KoTextCursor & startCursor, int size );
+
+    /** Delete all the items from the map, adding their commands into macroCmd */
+    void deleteAll( KMacroCommand *macroCmd );
+};
 
 #endif
