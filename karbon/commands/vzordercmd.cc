ad20cc45196833c3b6f6c614be84ef7025c11e60
Rob Buis <buis@kde.org>
2003-06-07 13:25:46 +0000

    Set correctly for undo/redo history.
    svn path=/trunk/koffice/; revision=230158

--- karbon/commands/vzordercmd.cc
+++ karbon/commands/vzordercmd.cc
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 2002, The Karbon Developers
+   Copyright (C) 2002, 2003 The Karbon Developers
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -135,10 +135,12 @@ VZOrderCmd::execute()
 			}
 		}
 	}
+	setSuccess( true );
 }
 
 void
 VZOrderCmd::unexecute()
 {
+	setSuccess( false );
 }
 
