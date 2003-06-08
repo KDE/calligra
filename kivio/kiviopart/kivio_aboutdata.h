770408abe4a15ab83feb549bd5e87991825248ce
Peter Simonsson <peter.simonsson@gmail.com>
2003-06-08 20:45:37 +0000

    Add Frauke
    svn path=/trunk/koffice/; revision=230462

--- kivio/kiviopart/kivio_aboutdata.h
+++ kivio/kiviopart/kivio_aboutdata.h
@@ -35,6 +35,7 @@ KAboutData * newKivioAboutData()
     aboutData->addAuthor("Peter Simonsson", I18N_NOOP("Maintainer"), "psn@linux.se");
     aboutData->addAuthor("Ian Reinhart Geiser", I18N_NOOP("Development"), "geiseri@kde.org");
     aboutData->addAuthor("Laurent Montel", I18N_NOOP("Development"), "montel@kde.org");
+    aboutData->addAuthor("Frauke Oster", I18N_NOOP("Development/Documentation"), "frauke@frsv.de");
     aboutData->addAuthor("Dave Marotti", I18N_NOOP("Initial idea, development and basic stencil sets."),"lndshark@speakeasy.net");
     aboutData->addAuthor("The Kompany.com - \"Where Open Source is at Home\"(TM)", I18N_NOOP("Development and additional stencil sets."), "info@thekompany.com");
     return aboutData;
