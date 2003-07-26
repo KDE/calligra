b47f650fed659dbf577a64d6eb47c2ede3e12a4a
Jarosław Staniek <staniek@kde.org>
2003-07-26 18:10:52 +0000

    private data for KexiTableView (that can be conveniently shared with subclasses/friends :)
    svn path=/trunk/koffice/; revision=239293

---
+++ kexi/tableview/kexitableview_p.cpp
@@ -0,0 +1,50 @@
+/* This file is part of the KDE project
+   Copyright (C) 2002 Till Busch <till@bux.at>
+   Lucijan Busch <lucijan@gmx.at>
+   Daniel Molkentin <molkentin@kde.org>
+   Copyright (C) 2003 Joseph Wenninger <jowenn@kde.org>
+   Copyright (C) 2003 Jaroslaw Staniek <js@iidea.pl>
+
+   This program is free software; you can redistribute it and,or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+
+   Original Author:  Till Busch <till@bux.at>
+   Original Project: buX (www.bux.at)
+*/
+
+#include "kexitableview_p.h"
+
+KexiTableViewPrivate::KexiTableViewPrivate()
+{
+	clearVariables();
+
+	numCols = 0;
+	editOnDoubleClick = true;
+	pBufferPm = 0;
+	deletionPolicy = KexiTableView::NoDelete;
+}
+void KexiTableViewPrivate::clearVariables() {
+	// Initialize variables
+	pEditor = 0;
+	numRows = 0;
+	curRow = -1;
+	curCol = -1;
+	pCurrentItem=0;
+	pInsertItem = 0;
+	sortedColumn = -1;
+	sortOrder = true;
+	recordIndicator = false;
+}
+
---
+++ kexi/tableview/kexitableview_p.h
@@ -0,0 +1,90 @@
+/* This file is part of the KDE project
+   Copyright (C) 2002 Till Busch <till@bux.at>
+   Lucijan Busch <lucijan@gmx.at>
+   Daniel Molkentin <molkentin@kde.org>
+   Copyright (C) 2003 Joseph Wenninger <jowenn@kde.org>
+   Copyright (C) 2003 Jaroslaw Staniek <js@iidea.pl>
+
+   This program is free software; you can redistribute it and,or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+
+   Original Author:  Till Busch <till@bux.at>
+   Original Project: buX (www.bux.at)
+*/
+
+#ifndef KEXITABLEVIEW_P_H
+#define KEXITABLEVIEW_P_H
+
+#include "kexitableview.h"
+#include "kexitableheader.h"
+
+class KexiTableItem;
+class KexiTableRM;
+class KexiTableEdit;
+
+class KexiTableViewPrivate 
+{
+	public:
+
+	KexiTableViewPrivate();
+	void clearVariables();
+
+	bool editOnDoubleClick : 1;
+	bool recordIndicator : 1;
+
+	// cursor position
+	int			curRow;
+	int			curCol;
+	KexiTableItem	*pCurrentItem;
+
+	// foreign widgets
+	QHeader			*pTopHeader;
+	KexiTableRM	*pVerticalHeader;
+	KexiTableRM		*pRecordMarker;
+	KexiTableEdit	*pEditor;
+
+	int numRows;
+	int numCols;
+	int rowHeight;
+	int sortedColumn;
+	bool sortOrder;
+
+	KexiTableView::AdditionPolicy additionPolicy;
+	KexiTableView::DeletionPolicy deletionPolicy;
+
+	QPixmap			*pBufferPm;
+	QTimer			*pUpdateTimer;
+	QPopupMenu		*pContextMenu;
+	int menu_id_addRecord;
+	int menu_id_removeRecord;
+
+	QMemArray<QVariant::Type> pColumnTypes;
+	QMemArray<int> pColumnModes;
+	QPtrList<QVariant> pColumnDefaults;
+
+	bool			needAutoScroll;
+	QTimer			*scrollTimer;
+	KexiTableItem	*pInsertItem;
+
+	QStringList		dropFilters;
+
+	KexiTableView::ScrollDirection scrollDirection;
+
+	bool			bgAltering;
+
+	QColor emptyAreaColor;
+};
+
+#endif
