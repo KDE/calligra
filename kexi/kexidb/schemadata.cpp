54807a8589c99f502a5bfd53ce6f3f11d31d8d7b
Jarosław Staniek <staniek@kde.org>
2003-10-01 19:10:15 +0000

    KexiDB::SchemaData introduced.
    svn path=/trunk/koffice/; revision=255460

---
+++ kexi/kexidb/schemadata.cpp
@@ -0,0 +1,79 @@
+/* This file is part of the KDE project
+   Copyright (C) 2003 Jaroslaw Staniek <js@iidea.pl>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include <kexidb/schemadata.h>
+#include <kexidb/connection.h>
+
+#include <kdebug.h>
+
+using namespace KexiDB;
+
+SchemaData::SchemaData()
+	: m_type(KexiDB::UnknownObjectType)
+	, m_id(-1)
+{
+}
+
+SchemaData::~SchemaData()
+{
+}
+
+const int SchemaData::type() const
+{
+	return m_type;
+}
+
+const int SchemaData::id() const
+{
+	return m_id;
+}
+
+const QString& SchemaData::name() const
+{
+	return m_name;
+}
+
+const QString& SchemaData::caption() const
+{
+	return m_caption;
+}
+
+const QString& SchemaData::helpText() const
+{
+	return m_helpText;
+}
+
+void SchemaData::clear()
+{
+	m_id = -1;
+	m_name = QString::null;
+	m_caption = QString::null;
+	m_helpText = QString::null;
+}
+
+QString SchemaData::schemaDataDebugString()
+{
+	QString helpText = m_helpText;
+	if (helpText.length()>40) {
+		helpText.truncate(40);
+		helpText+="...";
+	}
+	return QString("id=%1 name='%2' caption='%3' helpText='%4'")
+		.arg(m_id).arg(m_name).arg(m_caption).arg(helpText);
+}
---
+++ kexi/kexidb/schemadata.h
@@ -0,0 +1,68 @@
+/* This file is part of the KDE project
+   Copyright (C) 2003 Jaroslaw Staniek <js@iidea.pl>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef KEXIDB_SCHEMADATA_H
+#define KEXIDB_SCHEMADATA_H
+
+#include <qvaluelist.h>
+#include <qstring.h>
+
+#include <kexidb/field.h>
+
+namespace KexiDB {
+
+class Connection;
+
+/*! Container class that stores common kexi object schema's properties like
+ id, name, caption, help text.
+ By kexi object we mean in-db storable object like table schema or query schema.
+*/
+
+class KEXI_DB_EXPORT SchemaData
+{
+	public:
+		const int type() const;
+		const int id() const;
+		const QString& name() const;
+//		void setName(const QString& name);
+		const QString& caption() const;
+		const QString& helpText() const;
+
+		//! returns debug string useful for debugging
+		virtual QString schemaDataDebugString();
+	protected:
+		SchemaData();
+		virtual ~SchemaData();
+
+		//! Clears all properties excpet 'type'.
+		void clear();
+
+	//js	QStringList m_primaryKeys;
+		int m_type;
+		int m_id;
+		QString m_name;
+		QString m_caption;
+		QString m_helpText;
+
+	friend class Connection;
+};
+
+} //namespace KexiDB
+
+#endif
