32a162896f7139358f187bfb20347bf705c96796
Jarosław Staniek <staniek@kde.org>
2003-10-10 22:22:36 +0000

    testing tables creation added
    svn path=/trunk/koffice/; revision=257874

---
+++ kexi/kexidb/tests/newapi/tables_test.h
@@ -0,0 +1,48 @@
+	if (argc<=3) {
+		kdDebug() << prgname << ": name for db?" << endl;
+		usage();
+		return 0;
+	}
+	QCString db_name = QCString(argv[3]);
+	conn_data.setFileName( db_name );
+
+	conn = driver->createConnection(conn_data);
+	if (driver->error()) {
+		driver->debugError();
+		return 1;
+	}
+	if (!conn->connect()) {
+		conn->debugError();
+		return 1;
+	}
+	if (!conn->useDatabase( /*default*/ )) {
+		conn->debugError();
+		return 1;
+	}
+
+	//now: lets create table:
+	
+	KexiDB::TableSchema *t_persons = new KexiDB::TableSchema("persons");
+	t_persons->setCaption("Persons group in our factory");
+	t_persons->addField( new KexiDB::Field("id", KexiDB::Field::Integer, KexiDB::Field::PrimaryKey | KexiDB::Field::AutoInc, KexiDB::Field::Unsigned) )
+	.addField( new KexiDB::Field("age", KexiDB::Field::Integer, 0, KexiDB::Field::Unsigned) )
+	.addField( new KexiDB::Field("name", KexiDB::Field::Text) )
+	.addField( new KexiDB::Field("surname", KexiDB::Field::Text) );
+	if (!conn->createTable( t_persons )) {
+		conn->debugError();
+		return 1;
+	}
+	kdDebug() << "-- PERSONS created --" << endl;
+	
+	KexiDB::TableSchema *t_cars = new KexiDB::TableSchema("cars");
+	t_cars->setCaption("Cars owned by persons");
+	t_cars->addField( new KexiDB::Field("owner", KexiDB::Field::Integer, 0, KexiDB::Field::Unsigned) )
+	.addField( new KexiDB::Field("model", KexiDB::Field::Text) );
+	if (!conn->createTable( t_cars )) {
+		conn->debugError();
+		return 1;
+	}
+	kdDebug() << "-- CARS created --" << endl;
+	
+
+
