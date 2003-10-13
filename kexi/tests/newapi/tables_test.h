122477c2a279ccfce014b8f09c5695ad934f4a65
Jarosław Staniek <staniek@kde.org>
2003-10-13 20:53:26 +0000

    some data for tests added
    svn path=/trunk/koffice/; revision=258660

--- kexi/tests/newapi/tables_test.h
+++ kexi/tests/newapi/tables_test.h
@@ -8,30 +8,70 @@ int tablesTest()
 		return 1;
 	}
 
-	//now: lets create table:
-	
+	conn->setAutoCommit(false);
+	KexiDB::Transaction t = conn->beginTransaction();
+	if (conn->error()) {
+		conn->debugError();
+		return 1;
+	}
+
+	//now: lets create tables:
+	KexiDB::Field *f;
 	KexiDB::TableSchema *t_persons = new KexiDB::TableSchema("persons");
-	t_persons->setCaption("Persons group in our factory");
-	t_persons->addField( new KexiDB::Field("id", KexiDB::Field::Integer, KexiDB::Field::PrimaryKey | KexiDB::Field::AutoInc, KexiDB::Field::Unsigned) )
-	.addField( new KexiDB::Field("age", KexiDB::Field::Integer, 0, KexiDB::Field::Unsigned) )
-	.addField( new KexiDB::Field("name", KexiDB::Field::Text) )
-	.addField( new KexiDB::Field("surname", KexiDB::Field::Text) );
+	t_persons->setCaption("Persons in our factory");
+	t_persons->addField( f=new KexiDB::Field("id", KexiDB::Field::Integer, KexiDB::Field::PrimaryKey | KexiDB::Field::AutoInc, KexiDB::Field::Unsigned) );
+	f->setCaption("ID");
+	t_persons->addField( f=new KexiDB::Field("age", KexiDB::Field::Integer, 0, KexiDB::Field::Unsigned) );
+	f->setCaption("Age");
+	t_persons->addField( f=new KexiDB::Field("name", KexiDB::Field::Text) );
+	f->setCaption("Name");
+	t_persons->addField( f=new KexiDB::Field("surname", KexiDB::Field::Text) );
+	f->setCaption("Surname");
 	if (!conn->createTable( t_persons )) {
 		conn->debugError();
 		return 1;
 	}
 	kdDebug() << "-- PERSONS created --" << endl;
-	
+
+	if (!conn->insertRecord(*t_persons, QVariant(1), QVariant(27), QVariant("Jaroslaw"), QVariant("Staniek"))
+	  ||!conn->insertRecord(*t_persons, QVariant(2), QVariant(60), QVariant("Lech"), QVariant("Walesa"))
+	  ||!conn->insertRecord(*t_persons, QVariant(3), QVariant(45), QVariant("Bill"), QVariant("Gates"))
+	  ||!conn->insertRecord(*t_persons, QVariant(4), QVariant(35), QVariant("John"), QVariant("Smith"))
+	   )
+	{
+		kdDebug() << "-- PERSONS data err. --" << endl;
+		return 1;
+	}
+	kdDebug() << "-- PERSONS data created --" << endl;
+
+
 	KexiDB::TableSchema *t_cars = new KexiDB::TableSchema("cars");
 	t_cars->setCaption("Cars owned by persons");
-	t_cars->addField( new KexiDB::Field("owner", KexiDB::Field::Integer, 0, KexiDB::Field::Unsigned) )
-	.addField( new KexiDB::Field("model", KexiDB::Field::Text) );
+	t_cars->addField( f=new KexiDB::Field("owner", KexiDB::Field::Integer, 0, KexiDB::Field::Unsigned) );
+	f->setCaption("Car owner");
+	t_cars->addField( f=new KexiDB::Field("model", KexiDB::Field::Text) );
+	f->setCaption("Car model");
 	if (!conn->createTable( t_cars )) {
 		conn->debugError();
 		return 1;
 	}
 	kdDebug() << "-- CARS created --" << endl;
-	
+	if (!conn->insertRecord(*t_cars, QVariant(1), QVariant("Fiat"))
+		||!conn->insertRecord(*t_cars, QVariant(2), QVariant("Syrena"))
+		||!conn->insertRecord(*t_cars, QVariant(3), QVariant("Chrysler"))
+		||!conn->insertRecord(*t_cars, QVariant(3), QVariant("BMW"))
+		||!conn->insertRecord(*t_cars, QVariant(4), QVariant("Volvo"))
+		)
+	{
+		kdDebug() << "-- CARS data err. --" << endl;
+		return 1;
+	}
+	kdDebug() << "-- CARS data created --" << endl;
+
+	if (!conn->commitTransaction(t)) {
+		conn->debugError();
+		return 1;
+	}
 
 	return 0;
 }
