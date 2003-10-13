f4e88d605c84dfa56ef05f9e6d9ca91e32e5fbce
Jarosław Staniek <staniek@kde.org>
2003-10-13 18:38:27 +0000

    now reuses tables test
    svn path=/trunk/koffice/; revision=258620

--- kexi/tests/newapi/cursors_test.h
+++ kexi/tests/newapi/cursors_test.h
@@ -1,31 +1,34 @@
+int tablesTest();
+
 int cursorsTest()
 {
-	if (!conn->databaseExists( "mydb" )) {
-		if (!conn->createDatabase( "mydb" )) {
-			conn->debugError();
+	if (!conn->databaseExists( db_name )) {
+		if (tablesTest()!=0)
 			return 1;
-		}
 		kdDebug() << "DB created"<< endl;
 	}
-	if (!conn->useDatabase( "mydb" )) {
+
+/*	if (!conn->useDatabase( db_name )) {
 		conn->debugError();
 		return 1;
-	}
-	KexiDB::Cursor *cursor = conn->executeQuery( "select * from osoby", KexiDB::Cursor::Buffered );
+	}*/
+	KexiDB::Cursor *cursor = conn->executeQuery( "select * from persons", KexiDB::Cursor::Buffered );
 	kdDebug()<<"executeQuery() = "<<!!cursor<<endl;
-	if (cursor) {
-		kdDebug()<<"Cursor::moveLast() ---------------------" << endl;
-		kdDebug()<<"-- Cursor::moveLast() == " << cursor->moveLast() << endl;
-		cursor->moveLast();
-		kdDebug()<<"Cursor::moveFirst() ---------------------" << endl;
-		kdDebug()<<"-- Cursor::moveFirst() == " << cursor->moveFirst() << endl;
+	if (!cursor)
+		return 1;
+
+	kdDebug()<<"Cursor::moveLast() ---------------------" << endl;
+	kdDebug()<<"-- Cursor::moveLast() == " << cursor->moveLast() << endl;
+	cursor->moveLast();
+	kdDebug()<<"Cursor::moveFirst() ---------------------" << endl;
+	kdDebug()<<"-- Cursor::moveFirst() == " << cursor->moveFirst() << endl;
 
 /*		kdDebug()<<"Cursor::moveNext() == "<<cursor->moveNext()<<endl;
-		kdDebug()<<"Cursor::moveNext() == "<<cursor->moveNext()<<endl;
-		kdDebug()<<"Cursor::moveNext() == "<<cursor->moveNext()<<endl;
-		kdDebug()<<"Cursor::moveNext() == "<<cursor->moveNext()<<endl;
-		kdDebug()<<"Cursor::eof() == "<<cursor->eof()<<endl;*/
-		conn->deleteCursor(cursor);
-	}
+	kdDebug()<<"Cursor::moveNext() == "<<cursor->moveNext()<<endl;
+	kdDebug()<<"Cursor::moveNext() == "<<cursor->moveNext()<<endl;
+	kdDebug()<<"Cursor::moveNext() == "<<cursor->moveNext()<<endl;
+	kdDebug()<<"Cursor::eof() == "<<cursor->eof()<<endl;*/
+	conn->deleteCursor(cursor);
+
 	return 0;
 }
