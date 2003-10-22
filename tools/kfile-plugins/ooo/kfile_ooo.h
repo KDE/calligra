58450257d0423639f33d63d3de8ba3ddeb551550
Pascal Létourneau <pascal.letourneau@gmail.com>
2003-10-22 19:57:41 +0000

    Search & Replace gone mad?? s/softbmpre/software/ etc... in license
    svn path=/trunk/koffice/; revision=261129

--- tools/kfile-plugins/ooo/kfile_ooo.h
+++ tools/kfile-plugins/ooo/kfile_ooo.h
@@ -1,18 +1,18 @@
 /* This file is part of the KDE project
  * Copyright (C) 2003 Pierre Souchay <pierre@souchay.net>
  *
- * This program is free softbmpre; you can redistribute it and/or
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
- * License as published by the Free Softbmpre Foundation version 2.
+ * License as published by the Free Software Foundation version 2.
  *
  * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied bmprranty of
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; see the file COPYING.  If not, write to
- * the Free Softbmpre Foundation, Inc., 59 Temple Place - Suite 330,
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  *
  */
