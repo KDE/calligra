9b73601323faf4cc3cbf44f817df5712052eefa1
Jarosław Staniek <staniek@kde.org>
2003-11-28 21:27:59 +0000

    license info, added; minor fixes
    svn path=/trunk/koffice/; revision=270127

--- kexi/plugins/tables/kexitablepart.cpp
+++ kexi/plugins/tables/kexitablepart.cpp
@@ -1,3 +1,25 @@
+/* This file is part of the KDE project
+   Copyright (C) 2003 Lucijan Busch <lucijan@kde.org>
+   Copyright (C) 2002, 2003 Joseph Wenninger <jowenn@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include "kexitablepart.h"
+
 #include <kdebug.h>
 #include <kgenericfactory.h>
 
@@ -7,14 +29,13 @@
 #include "widget/kexidatatable.h"
 #include "keximainwindow.h"
 #include "kexiproject.h"
-#include "kexitablepart.h"
 #include <kxmlguiclient.h>
 
 
 class KexiTablePartGUIClient: public QObject, public KXMLGUIClient {
 public:
 	KexiTablePartGUIClient(KexiMainWindow *parent): QObject(parent), KXMLGUIClient(parent) {
-	        new KAction(i18n("Table ..."), "createTable", 0, this, SLOT(create()), actionCollection(), "tablepart_create");
+	        new KAction(i18n("Table..."), "createTable", 0, this, SLOT(create()), actionCollection(), "tablepart_create");
         	setXMLFile("kexitablepartui.rc");
 		parent->guiFactory()->addClient(this);
 	}
