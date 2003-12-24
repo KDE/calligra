00d21c02ce191c0353b08972013651a2790e48c2
Nicolas Goutte <nicolasg@snafu.de>
2003-12-24 01:34:07 +0000

    I18N: untranslatable strings
    Add the strings of the shape files of the SML type (in Kivio.)
    
    The strings are prefixed with "SML", as some of the strings seems pretty
    common. So I supposed that there may be I18N teams that would need some
    different strings.
    
    Note: this way of I18N extraction is mainly, as it is urgent to have
    these user visible strings extracted. (I am not sure if it would be the long
    term solution.)
    
    I am sorry for the inconveniences
    
    CCMAIL:kde-i18n-doc@kde.org
    CCMAIL:koffice-devel@kde.org
    
    svn path=/trunk/koffice/; revision=274631

---
+++ kivio/kivio-sml-i18n.cpp
@@ -0,0 +1,198 @@
+i18n("SML","Direct Data") // Title
+i18n("SML","Basic flowcharting direct data shape") // Description
+i18n("SML","Card") // Title
+i18n("SML","Basic flowcharting card shape") // Description
+i18n("SML","Data") // Title
+i18n("SML","Data shape for flowcharting") // Description
+i18n("SML","Decision") // Title
+i18n("SML","Basic flowcharting decision shape") // Description
+i18n("SML","Internal Storage") // Title
+i18n("SML","Internal storage shape") // Description
+i18n("SML","Display") // Title
+i18n("SML","Basic flowcharting display shape") // Description
+i18n("SML","Document") // Title
+i18n("SML","Flowcharting Document Stencil") // Description
+i18n("SML","Predefined Process") // Title
+i18n("SML","Predefined process shape") // Description
+i18n("SML","Loop Limit") // Title
+i18n("SML","Basic flowcharting loop limit shape") // Description
+i18n("SML","Manual Input") // Title
+i18n("SML","Basic flowcharting manual input shape") // Description
+i18n("SML","Manual Operation") // Title
+i18n("SML","Basic flowcharting manual operation shape") // Description
+i18n("SML","Off-Page Reference") // Title
+i18n("SML","Off-Page Reference") // Description
+i18n("SML","On-Page Reference") // Title
+i18n("SML","On-Page Reference") // Description
+i18n("SML","Paper Tape") // Title
+i18n("SML","Flowcharting Paper Tape Stencil") // Description
+i18n("SML","Preperation") // Title
+i18n("SML","Basic flowcharting preperation shape") // Description
+i18n("SML","Process") // Title
+i18n("SML","Basic flowcharting process shape") // Description
+i18n("SML","Sequential Data") // Title
+i18n("SML","Sequential Data") // Description
+i18n("SML","Stored Data") // Title
+i18n("SML","Basic flowcharting stored data shape") // Description
+i18n("SML","Terminator") // Title
+i18n("SML","Terminator shape") // Description
+i18n("SML","Connector") // Title
+i18n("SML","Logical Connection Point") // Description
+i18n("SML","And") // Title
+i18n("SML","Logical And Gate") // Description
+i18n("SML","Buffer") // Title
+i18n("SML","Logical Buffer Gate") // Description
+i18n("SML","Inverter") // Title
+i18n("SML","Logical Inverter Gate") // Description
+i18n("SML","Nand") // Title
+i18n("SML","Logical Nand Gate") // Description
+i18n("SML","Nor") // Title
+i18n("SML","Logical Nor Gate") // Description
+i18n("SML","Not") // Title
+i18n("SML","Logical Not Gate") // Description
+i18n("SML","Or") // Title
+i18n("SML","Logical Or Gate") // Description
+i18n("SML","Xor") // Title
+i18n("SML","Logical Xor Gate") // Description
+i18n("SML","Data Store") // Title
+i18n("SML","Data Store shape") // Description
+i18n("SML","Database") // Title
+i18n("SML","Database shape") // Description
+i18n("SML","Process") // Title
+i18n("SML","Process shape") // Description
+i18n("SML","Source or Sink") // Title
+i18n("SML","Source or Sink shape") // Description
+i18n("SML","Austria") // Title
+i18n("SML","Austria") // Description
+i18n("SML","Belgium") // Title
+i18n("SML","Belgium") // Description
+i18n("SML","Finland") // Title
+i18n("SML","Finland") // Description
+i18n("SML","France") // Title
+i18n("SML","France") // Description
+i18n("SML","Germany") // Title
+i18n("SML","Germany") // Description
+i18n("SML","Great Britain") // Title
+i18n("SML","Great Britain") // Description
+i18n("SML","Greece") // Title
+i18n("SML","Greece") // Description
+i18n("SML","Iceland") // Title
+i18n("SML","Iceland") // Description
+i18n("SML","Ireland") // Title
+i18n("SML","Ireland") // Description
+i18n("SML","Italy") // Title
+i18n("SML","Italy") // Description
+i18n("SML","Japan") // Title
+i18n("SML","Japan") // Description
+i18n("SML","Luxemburg") // Title
+i18n("SML","Luxemburg") // Description
+i18n("SML","Netherlands") // Title
+i18n("SML","Netherlands") // Description
+i18n("SML","Norway") // Title
+i18n("SML","Norway") // Description
+i18n("SML","Poland") // Title
+i18n("SML","Poland") // Description
+i18n("SML","Sweden") // Title
+i18n("SML","Sweden") // Description
+i18n("SML","Switzerland") // Title
+i18n("SML","Switzerland") // Description
+i18n("SML","United States of America") // Title
+i18n("SML","United States of America") // Description
+i18n("SML","Belgium") // Title
+i18n("SML","Map of Belgium") // Description
+i18n("SML","Europe") // Title
+i18n("SML","Map of Europe") // Description
+i18n("SML","Floppy Disk") // Title
+i18n("SML","Floppy Disk shape") // Description
+i18n("SML","Desktop PC 2D") // Title
+i18n("SML","Desktop PC 2D") // Description
+i18n("SML","Desktop PC 3D") // Title
+i18n("SML","Desktop PC 3D") // Description
+i18n("SML","HDD") // Title
+i18n("SML","HDD") // Description
+i18n("SML","Mini-tower Desktop PC 2D") // Title
+i18n("SML","Mini-tower Desktop PC 2D") // Description
+i18n("SML","Mini-tower Desktop PC 3D") // Title
+i18n("SML","Mini-tower Desktop PC 3D") // Description
+i18n("SML","Notebook") // Title
+i18n("SML","Notebook") // Description
+i18n("SML","PCI card") // Title
+i18n("SML","PCI card") // Description
+i18n("SML","Rack") // Title
+i18n("SML","Rack") // Description
+i18n("SML","Terminal") // Title
+i18n("SML","Terminal") // Description
+i18n("SML","Workstation 3D") // Title
+i18n("SML","Workstation 3D") // Description
+i18n("SML","X Terminal") // Title
+i18n("SML","Terminal 2D") // Description
+i18n("SML","Keyboard") // Title
+i18n("SML","Keyboard") // Description
+i18n("SML","Printer 1") // Title
+i18n("SML","Printer 1") // Description
+i18n("SML","Printer 2") // Title
+i18n("SML","Printer 2") // Description
+i18n("SML","Proxy") // Title
+i18n("SML","Proxy") // Description
+i18n("SML","Cell Phone") // Title
+i18n("SML","Cell Phone") // Description
+i18n("SML","CD-ROM") // Title
+i18n("SML","CD-ROM") // Description
+i18n("SML","Key") // Title
+i18n("SML","Key shape") // Description
+i18n("SML","Arrow") // Title
+i18n("SML","Arrow shape") // Description
+i18n("SML","Multiple Arrow 1") // Title
+i18n("SML","Multiple Arrow shape 1") // Description
+i18n("SML","Multiple Arrow 2") // Title
+i18n("SML","Multiple Arrow shape 2") // Description
+i18n("SML","Multiple Arrow 3") // Title
+i18n("SML","Multiple Arrow shape 3") // Description
+i18n("SML","Sun Arrow") // Title
+i18n("SML","Sun Arrow shape") // Description
+i18n("SML","Factory") // Title
+i18n("SML","Factory shape") // Description
+i18n("SML","House") // Title
+i18n("SML","House shape") // Description
+i18n("SML","Large office building") // Title
+i18n("SML","Large office building") // Description
+i18n("SML","Bad") // Title
+i18n("SML","Bad shape") // Description
+i18n("SML","Blue Collar Worker") // Title
+i18n("SML","Blue Collar Worker") // Description
+i18n("SML","Good") // Title
+i18n("SML","Good shape") // Description
+i18n("SML","Group") // Title
+i18n("SML","Group") // Description
+i18n("SML","Man") // Title
+i18n("SML","Man shape") // Description
+i18n("SML","White Collar Worker") // Title
+i18n("SML","White Collar Worker") // Description
+i18n("SML","Woman") // Title
+i18n("SML","Woman shape") // Description
+i18n("SML","Woman Shape") // Title
+i18n("SML","Woman shape") // Description
+i18n("SML","Truck") // Title
+i18n("SML","Truck shape") // Description
+i18n("SML","Activity") // Title
+i18n("SML","Activity") // Description
+i18n("SML","Branch & Merge") // Title
+i18n("SML","Branch and Merge") // Description
+i18n("SML","End") // Title
+i18n("SML","End") // Description
+i18n("SML","Fork & Join") // Title
+i18n("SML","Fork and Join") // Description
+i18n("SML","Start") // Title
+i18n("SML","Start") // Description
+i18n("SML","Class") // Title
+i18n("SML","Class") // Description
+i18n("SML","Class") // Title
+i18n("SML","ClassConceptual") // Description
+i18n("SML","Interface") // Title
+i18n("SML","Interface") // Description
+i18n("SML","Note") // Title
+i18n("SML","Note") // Description
+i18n("SML","Template Class") // Title
+i18n("SML","TemplateClass") // Description
+i18n("SML","Text") // Title
+i18n("SML","Text") // Description
