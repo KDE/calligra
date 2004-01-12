d5a2c7e514ea48dbcd040dfce65f33405f0ddb78
Nicolas Goutte <nicolasg@snafu.de>
2004-01-12 16:27:54 +0000

    Generate footnotes more like KWord (This does not solve bug #72140)
    svn path=/trunk/koffice/; revision=279094

--- filters/kword/rtf/import/rtfimport.cpp
+++ filters/kword/rtf/import/rtfimport.cpp
@@ -598,7 +598,7 @@ KoFilter::ConversionStatus RTFImport::convert( const QCString& from, const QCStr
 	    num++;
 	    mainDoc.addFrameSet( str, 1, 7 );
 	      mainDoc.addFrame( leftMargin, paperHeight - bottomMargin-80,
-			(paperWidth - rightMargin), paperHeight-bottomMargin, 0, 1, 0 );
+			(paperWidth - rightMargin), paperHeight-bottomMargin, 0, 2, 0 );
 	      mainDoc.closeNode( "FRAME" );
 	      mainDoc.appendNode( i->node );
 	    mainDoc.closeNode( "FRAMESET" );
@@ -1809,6 +1809,7 @@ void RTFImport::parseFootNote( RTFProperty * property)
             node.setAttribute("numberingtype", "auto");
             node.setAttribute("notetype", "footnote");
             node.setAttribute("frameset", str);
+            node.setAttribute("value", fnnum);
             node.closeNode("FOOTNOTE");
 	addVariable(node, 11, "STRING");
     }
