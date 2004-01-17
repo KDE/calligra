2bbbeba773db3943cc55cae380a04401bbc9b2e0
Nicolas Goutte <nicolasg@snafu.de>
2004-01-17 20:50:33 +0000

    Filter out control characters that are not allowed in XML (At least some KWord 0.8 seems to have some.)
    svn path=/trunk/koffice/; revision=280576

--- filters/kword/kword1.3/import/ExportFilter.cc
+++ filters/kword/kword1.3/import/ExportFilter.cc
@@ -90,6 +90,7 @@ QString OOWriterWorker::escapeOOSpan(const QString& strText) const
             spaceNumber=-1;
         }
 
+        // ### TODO: would be switch/case or if/elseif the best?
         switch (ch.unicode())
         {
         case 9: // Tab
@@ -142,9 +143,19 @@ QString OOWriterWorker::escapeOOSpan(const QString& strText) const
             }
         case 1: // (Non-XML-compatible) replacement character from KWord 0.8
             {
-                strReturn+="#"; //use KWord 1.[123] replacement character instead
+                strReturn += '#'; //use KWord 1.[123] replacement character instead
                 break;
             }
+        // Following characters are not allowed in XML (but some files from KWord 0.8 have some of them.)
+        case  0: case  2: case  3: case  4: case  5: case  6: case  7: case  8: case 11: case 12:
+        case 14: case 15: case 16: case 17: case 18: case 19: case 20: case 21: case 22: case 23:
+        case 24: case 25: case 26: case 27: case 28: case 29: case 30: case 31:
+            {
+                kdWarning(30518) << "Not allowed XML character: " << ch.unicode() << endl;
+                strReturn += '?';
+                break;
+            }
+        case 13: // ### TODO: what to do with it?
         default:
             {
                 strReturn+=ch;
