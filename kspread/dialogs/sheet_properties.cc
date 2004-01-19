e3bb732a539ec7b327577ef3feda9e6f9c886557
Ariya Hidayat <ariya@kde.org>
2004-01-19 15:37:11 +0000

    Sheet Properties dialog
    svn path=/trunk/koffice/; revision=281060

---
+++ kspread/dialogs/sheet_properties.cc
@@ -0,0 +1,138 @@
+/*
+   This file is part of the KDE project
+   Copyright (C) 2004 Ariya Hidayat <ariya@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include "sheet_properties.h"
+#include "sheet_properties_base.h"
+
+#include <qcheckbox.h>
+#include <qvbox.h>
+
+#include <kdialogbase.h>
+#include <klocale.h>
+
+SheetPropertiesDialog::SheetPropertiesDialog( QWidget* parent ):
+  KDialogBase( parent )
+{
+  QVBox* mainWidget = makeVBoxMainWidget();
+  d = new SheetPropertiesBase( mainWidget );
+  
+  showButtonOK( true );
+  showButtonCancel( true );
+  showButtonApply( false );
+  
+  setCaption( i18n("Sheet Properties") );
+  enableButtonSeparator( true );
+}
+
+SheetPropertiesDialog::~SheetPropertiesDialog()
+{
+  delete d;
+}
+
+bool SheetPropertiesDialog::autoCalc() const
+{
+  return d->autoCalcCheckBox->isChecked();
+}
+
+void SheetPropertiesDialog::setAutoCalc( bool b )
+{
+  d->autoCalcCheckBox->setChecked( b );
+}
+
+bool SheetPropertiesDialog::showGrid() const
+{
+  return d->showGridCheckBox->isChecked();
+}
+
+void SheetPropertiesDialog::setShowGrid( bool b )
+{
+  d->showGridCheckBox->setChecked( b );
+}
+
+bool SheetPropertiesDialog::showPageBorders() const
+{
+  return d->showPageBordersCheckBox->isChecked();
+}
+    
+void SheetPropertiesDialog::setShowPageBorders( bool b )
+{
+  d->showPageBordersCheckBox->setChecked( b );
+}   
+
+bool SheetPropertiesDialog::showFormula() const
+{
+  return d->showFormulaCheckBox->isChecked();
+}
+    
+void SheetPropertiesDialog::setShowFormula( bool b )
+{
+  d->showFormulaCheckBox->setChecked( b );
+}
+    
+bool SheetPropertiesDialog::hideZero() const
+{
+  return d->hideZeroCheckBox->isChecked();
+}
+    
+void SheetPropertiesDialog::setHideZero( bool b )
+{
+  d->hideZeroCheckBox->setChecked( b );
+}
+    
+bool SheetPropertiesDialog::showFormulaIndicator() const
+{
+  return d->showFormulaIndicatorCheckBox->isChecked();
+}
+    
+void SheetPropertiesDialog::setShowFormulaIndicator( bool b )
+{
+  d->showFormulaIndicatorCheckBox->setChecked( b );
+}
+    
+bool SheetPropertiesDialog::columnAsNumber() const
+{
+  return d->showColumnAsNumbersCheckBox->isChecked();
+}
+    
+void SheetPropertiesDialog::setColumnAsNumber( bool b )
+{
+  d->showColumnAsNumbersCheckBox->setChecked( b );
+}
+    
+bool SheetPropertiesDialog::lcMode() const
+{
+  return d->useLCModeCheckBox->isChecked();
+}
+    
+void SheetPropertiesDialog::setLcMode( bool b )
+{
+  d->useLCModeCheckBox->setChecked( b );
+}    
+    
+bool SheetPropertiesDialog::capitalizeFirstLetter() const
+{
+  return d->capitalizeFirstLetterCheckBox->isChecked();
+}
+    
+void SheetPropertiesDialog::setCapitalizeFirstLetter( bool b )
+{
+  d->capitalizeFirstLetterCheckBox->setChecked( b );
+}    
+    
---
+++ kspread/dialogs/sheet_properties.h
@@ -0,0 +1,80 @@
+/*
+   This file is part of the KDE project
+   Copyright (C) 2004 Ariya Hidayat <ariya@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef SHEET_PROPERTIES_DIALOG
+#define SHEET_PROPERTIES_DIALOG
+
+#include <qwidget.h>
+#include <kdialogbase.h>
+
+class SheetPropertiesBase;
+
+
+class SheetPropertiesDialog : public KDialogBase
+{
+    Q_OBJECT
+    
+public:
+
+    SheetPropertiesDialog( QWidget* parent = 0 );
+    
+    ~SheetPropertiesDialog();
+    
+    bool autoCalc() const;
+    
+    void setAutoCalc( bool b );
+    
+    bool showGrid() const;
+    
+    void setShowGrid( bool b );
+    
+    bool showPageBorders() const;
+    
+    void setShowPageBorders( bool b );
+    
+    bool showFormula() const;
+    
+    void setShowFormula( bool b );
+    
+    bool hideZero() const;
+    
+    void setHideZero( bool b );
+    
+    bool showFormulaIndicator() const;
+    
+    void setShowFormulaIndicator( bool b );
+    
+    bool columnAsNumber() const;
+    
+    void setColumnAsNumber( bool b );
+    
+    bool lcMode() const;
+    
+    void setLcMode( bool b );
+    
+    bool capitalizeFirstLetter() const;
+    
+    void setCapitalizeFirstLetter( bool b );
+    
+private:
+    SheetPropertiesBase* d;
+};
+
+#endif /* SHEET_PROPERTIES_DIALOG */
