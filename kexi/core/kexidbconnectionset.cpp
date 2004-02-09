5916becec832ed1b372f43fcaac58c83235fa954
Jarosław Staniek <staniek@kde.org>
2004-02-09 10:26:27 +0000

    projectset and connectionset moved from startup/
    svn path=/trunk/koffice/; revision=286390

---
+++ kexi/core/kexidbconnectionset.cpp
@@ -0,0 +1,60 @@
+/* This file is part of the KDE project
+   Copyright (C) 2003 Jaroslaw Staniek <js@iidea.pl>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+ */
+
+#include "kexidbconnectionset.h"
+
+#include <kdebug.h>
+
+class KexiDBConnectionSetPrivate
+{
+public:
+	KexiDBConnectionSetPrivate()
+	{
+		list.setAutoDelete(true);
+		maxid=-1;
+	}
+	KexiDB::ConnectionData::List list;
+	int maxid;
+};
+
+KexiDBConnectionSet::KexiDBConnectionSet()
+: d(new KexiDBConnectionSetPrivate())
+{
+}
+
+KexiDBConnectionSet::~KexiDBConnectionSet()
+{
+	delete d;
+}
+
+void KexiDBConnectionSet::addConnectionData(KexiDB::ConnectionData *data)
+{
+	if (data->id<0)
+		data->id = d->maxid+1;
+	//TODO: 	check for id-duplicates
+	
+	d->maxid = QMAX(d->maxid,data->id);
+	d->list.append(data);
+}
+
+KexiDB::ConnectionData::List KexiDBConnectionSet::list() const
+{
+	return d->list;
+}
+
---
+++ kexi/core/kexiprojectset.h
@@ -0,0 +1,63 @@
+/* This file is part of the KDE project
+   Copyright (C) 2003 Jaroslaw Staniek <js@iidea.pl>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+ */
+
+#ifndef KEXIPROJECTSET_H
+#define KEXIPROJECTSET_H
+
+#include <kexidb/connectiondata.h>
+#include <kexidb/object.h>
+
+#include "kexiprojectdata.h"
+
+class KexiProjectSetPrivate;
+
+/*! Stores information about multiple kexi project-data items
+*/
+
+class KEXICORE_EXPORT KexiProjectSet : public KexiDB::Object
+{
+public:
+
+	/*! Creates empty project set */
+    KexiProjectSet();
+    
+	/*! Creates project set filled with all projects found using \a conndata. 
+	 There may be error during project list retrieving - use appropriate 
+	 KexiDB::Object::error(), and similar methods to get error message. */
+    KexiProjectSet(KexiDB::ConnectionData &conndata);
+	
+	~KexiProjectSet();
+
+	/*! Adds \a data as connection data. 
+	 \a will be owned by a KexiDBConnectionSet object. */
+	void addProjectData(KexiProjectData *data);
+	
+	//! return list object
+	KexiProjectData::List list() const;
+
+	//! finds (case insensitive) 
+	//! and returns project data for databased \a dbName or NULL if not found
+	KexiProjectData* findProject(const QString &dbName) const;
+
+private:
+	KexiProjectSetPrivate *d;
+};
+
+#endif // KEXINEWDBCONNDIALOG_H
+
