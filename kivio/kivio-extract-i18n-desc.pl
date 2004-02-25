66472cd6f88f4f4faba31588f143fd26cd59d687
Nicolas Goutte <nicolasg@snafu.de>
2004-02-25 16:25:20 +0000

    Extract also the .sml stencils (with a common script also for the desc files)
    svn path=/trunk/koffice/; revision=291342

--- kivio/kivio-extract-i18n-desc.pl
+++ kivio/kivio-extract-i18n-desc.pl
@@ -5,14 +5,32 @@
 # License: LGPL 2.0
 #
 
+sub writei18n
+{
+    local $_ = @_[1];
+    if ( $_ )
+    {
+        s/&lt;/</g;
+        s/&gt;/>/g;
+        s/&apos;/\'/g;
+        s/&quot;/\"/g;
+        s/&amp;/&/g;
+        s/\"/\\\"/g;
+        print "i18n( \"". @_[0] . "\", \"" . $_ . "\" );";
+        if ( @_[2] )
+        {
+            print " // " . @_[2];
+        }
+        print "\n";
+    }
+}
+
 if ( m/<Title +data=\"(.+)\"/ )
 {
-    $_ = $1;
-    s/&lt;/</g;
-    s/&gt;/>/g;
-    s/&apos;/\'/g;
-    s/&quot;/\"/g;
-    s/&amp;/&/g;
-    s/\"/\\\"/g;
-    print "i18n( \"Stencils\", \"" . $_ . "\" );\n";
+    writei18n( "Stencils", $1 , "Title" );
+}
+
+if ( m/<Description +data=\"(.+)\"/ )
+{
+    writei18n( "Stencils", $1 , "Description" );
 }
