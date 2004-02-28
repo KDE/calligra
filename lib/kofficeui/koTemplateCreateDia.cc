5152a29e4c77dc9745fea8cf68fc25d8cc0e1a06
Nicolas Goutte <nicolasg@snafu.de>
2004-02-28 20:01:45 +0000

    CVS_SILENT: add copyright (backport)
    svn path=/branches/KOFFICE_1_3_BRANCH/koffice/; revision=292332

--- lib/kofficeui/koTemplateCreateDia.cc
+++ lib/kofficeui/koTemplateCreateDia.cc
@@ -2,6 +2,7 @@
    This file is part of the KDE project
    Copyright (C) 1998, 1999 Reginald Stadlbauer <reggie@kde.org>
                  2000 Werner Trobin <trobin@kde.org>
+   Copyright (C) 2004 Nicolas GOUTTE <goutte@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
