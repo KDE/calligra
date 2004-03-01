de4548f02d44f6a2555e8ce5b86e96fe6b57658c
Nicolas Goutte <nicolasg@snafu.de>
2004-03-01 14:54:24 +0000

    Remain consistannt with the OOCalc filter (Malcom Hunter has changed the text in the OOCalc filter.)
    svn path=/trunk/koffice/; revision=292835

--- filters/kword/oowriter/oowriterimport.cc
+++ filters/kword/oowriter/oowriterimport.cc
@@ -600,7 +600,7 @@ bool OoWriterImport::createStyleMap( const QDomDocument & styles, QDomDocument& 
       kdDebug(30518) << "OpenWriter version: " << d << endl;
       if ( d > 1.0 )
       {
-        QString message( i18n("This document was created with the OpenOffice.org version '%1'. This filter was written for version for 1.0. Reading this file could cause strange behavior, crashes or incorrect display of the data. Do you want to continue converting the document?") );
+        QString message( i18n("This document was created with OpenOffice.org version '%1'. This filter was written for version for 1.0. Reading this file could cause strange behavior, crashes or incorrect display of the data. Do you want to continue converting the document?") );
         message = message.arg( docElement.attribute( "office:version" ) );
         if ( KMessageBox::warningYesNo( 0, message, i18n( "Unsupported document version" ) ) == KMessageBox::No )
           return false;
