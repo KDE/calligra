f68048b5b58592dcdb419872b1144f6a86288919
Nicolas Goutte <nicolasg@snafu.de>
2004-03-04 00:35:42 +0000

    Fallback to previously used encoding if the asked encoding is not known (Backport)
    svn path=/branches/KOFFICE_1_3_BRANCH/koffice/; revision=293472

--- filters/kword/rtf/import/rtfimport.cpp
+++ filters/kword/rtf/import/rtfimport.cpp
@@ -705,11 +705,14 @@ void RTFImport::ignoreKeyword( RTFProperty * )
  */
 void RTFImport::setCodepage( RTFProperty * )
 {
+    QTextCodec* oldCodec = textCodec;
     QCString cp;
     cp.setNum( token.value );
     cp.prepend("CP");
     textCodec=QTextCodec::codecForName(cp);
     kdDebug(30515) << "\\ansicpg: asked: " << cp << " given: " << (textCodec?textCodec->name():QString("-none-")) << endl;
+    if ( ! textCodec )
+        textCodec = oldCodec;
 }
 
 /**
@@ -717,8 +720,11 @@ void RTFImport::setCodepage( RTFProperty * )
  */
 void RTFImport::setMacCodepage( RTFProperty * )
 {
+    QTextCodec* oldCodec = textCodec;
     textCodec=QTextCodec::codecForName("Apple Roman");
     kdDebug(30515) << "\\mac " << (textCodec?textCodec->name():QString("-none-")) << endl;
+    if ( ! textCodec )
+        textCodec = oldCodec;
 }
 
 /**
@@ -727,8 +733,11 @@ void RTFImport::setMacCodepage( RTFProperty * )
  */
 void RTFImport::setAnsiCodepage( RTFProperty * )
 {
+    QTextCodec* oldCodec = textCodec;
     textCodec=QTextCodec::codecForName("CP1252");
     kdDebug(30515) << "\\ansi " << (textCodec?textCodec->name():QString("-none-")) << endl;
+    if ( ! textCodec )
+        textCodec = oldCodec;
 }
 
 /**
@@ -736,8 +745,11 @@ void RTFImport::setAnsiCodepage( RTFProperty * )
  */
 void RTFImport::setPcaCodepage( RTFProperty * )
 {
+    QTextCodec* oldCodec = textCodec;
     textCodec=QTextCodec::codecForName("IBM 850"); // Qt writes the name with a space
     kdDebug(30515) << "\\pca " << (textCodec?textCodec->name():QString("-none-")) << endl;
+    if ( ! textCodec )
+        textCodec = oldCodec;
 }
 
 /**
@@ -745,8 +757,11 @@ void RTFImport::setPcaCodepage( RTFProperty * )
  */
 void RTFImport::setPcCodepage( RTFProperty * )
 {
+    QTextCodec* oldCodec = textCodec;
     textCodec=QTextCodec::codecForName("IBM 850"); // This is an approximation
     kdDebug(30515) << "\\pc (approximation) " << (textCodec?textCodec->name():QString("-none-")) << endl;
+    if ( ! textCodec )
+        textCodec = oldCodec;
 }
 
 /**
