1709589e2d3ac291907884e30f446480996760a9
David Faure <faure@kde.org>
2004-03-08 09:45:36 +0000

    Improvements in the delete rows dialog's text. CCMAIL: 73606-done@bugs.kde.org
    svn path=/trunk/koffice/; revision=294354

--- kword/deldia.cc
+++ kword/deldia.cc
@@ -47,11 +47,7 @@ KWDeleteDia::KWDeleteDia( QWidget *parent, const char *name, KWTableFrameSet *_t
     m_toRemove.clear();
 
     setupTab1();
-#if KDE_IS_VERSION(3, 1, 90)
     setButtonOK( KGuiItem( 
-#else
-    setButtonOKText( (
-#endif
         i18n("&Delete"), type == ROW ?
     	i18n("Delete the row from the table.") :
     	i18n("Delete the column from the table.")) );
@@ -63,8 +59,8 @@ void KWDeleteDia::setupTab1()
 {
     tab1 = plainPage();
     grid1 = new QGridLayout( tab1, 4, 1, 0, spacingHint() );
-    QString message =type == ROW ? i18n( "Delete rows:" ) : i18n( "Delete columns:" );
-    bool firstSelectedCell = true; // used to know whether to add a ", " to the message string.
+    QString rowList;
+    bool firstSelectedCell = true; // used to know whether to add a ", " to the rowList string.
 
     uint max = (type == ROW) ? table->getRows() : table->getCols(); // max row/col to loop up to
     for (uint i=0;i<max; i++)
@@ -73,8 +69,8 @@ void KWDeleteDia::setupTab1()
                 ( (type == COL) && table->isColSelected(i) ) )
         {
             if (!firstSelectedCell)
-                message += ", "; // i18n??
-            message += QString::number(i +1);
+                rowList += ", "; // i18n??
+            rowList += QString::number(i+1);
             m_toRemove.push_back(i);
             firstSelectedCell = false;
        }
@@ -83,19 +79,27 @@ void KWDeleteDia::setupTab1()
     {
         int val = type == ROW ? canvas->currentTableRow() : canvas->currentTableCol();
         Q_ASSERT( val != -1 );
-        message += QString::number(val+1);
+        rowList += QString::number(val+1);
         m_toRemove.push_back(val);
     }
-    Q_ASSERT(m_toRemove.count() > 0);
+    int count = m_toRemove.count();
+    Q_ASSERT(count > 0);
 
-    if (m_toRemove.count() == ( (type == ROW) ? table->getRows() : table->getCols() ) )
+    QString message;
+    if ( count == ( (type == ROW) ? table->getRows() : table->getCols() ) )
         // all the columns are selected and the user asked to remove columns or the same with rows
         // => we want to delete the whole table
-        message = i18n("Delete the whole table");
-
+        message = i18n("Delete the whole table?");
+    else if ( count > 10 )
     // do not display hugely long dialogs if many rows/cells are selected
-    if (m_toRemove.count() > 10)
-        message = ROW ? i18n("Delete all selected rows") : i18n("Delete all selected cells");
+        message = ROW ? i18n("Delete all selected rows?") : i18n("Delete all selected cells?");
+    else if ( count == 1 ) {
+        message = type == ROW ? i18n( "Delete row number %1?" ) : i18n( "Delete column number %1?" );
+        message = message.arg( m_toRemove.first() );
+    } else {
+        message = type == ROW ? i18n( "Delete rows: %1 ?" ) : i18n( "Delete columns: %1 ?" );
+        message = message.arg( rowList );
+    }
 
     rc = new QLabel( message , tab1 );
     rc->resize( rc->sizeHint() );
