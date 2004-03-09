f148b4aedccdffaa2df45369b2e3e605421fee1a
Peter Simonsson <peter.simonsson@gmail.com>
2004-03-09 17:17:47 +0000

    kivio_settings.kcfgc have moved into it's own dir... this should make it compile... It adds an unfinnished class, but it wouldn't compile only kivio_settings so I added it anyway
    svn path=/trunk/koffice/; revision=294730

---
+++ kivio/kiviopart/config/kivio_config.cpp
@@ -0,0 +1,26 @@
+//
+// C++ Implementation: kivio_config
+//
+// Description: 
+//
+//
+// Author: Peter Simonsson <psn@linux.se>, (C) 2004
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "kivio_config.h"
+
+namespace Kivio {
+
+Config::Config()
+{
+}
+
+
+Config::~Config()
+{
+}
+
+
+};
---
+++ kivio/kiviopart/config/kivio_config.h
@@ -0,0 +1,30 @@
+//
+// C++ Interface: kivio_config
+//
+// Description: 
+//
+//
+// Author: Peter Simonsson <psn@linux.se>, (C) 2004
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef KIVIOKIVIO_CONFIG_H
+#define KIVIOKIVIO_CONFIG_H
+
+namespace Kivio {
+
+/**
+@author Peter Simonsson
+*/
+class Config{
+public:
+    Config();
+
+    ~Config();
+
+};
+
+};
+
+#endif
