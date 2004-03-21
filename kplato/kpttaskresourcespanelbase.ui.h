683e61d84b8ae251666d595fa507921cfd221281
Shaheed Haque <srhaque@theiet.org>
2004-03-21 15:56:46 +0000

    Resource property pane of task dialog.
    svn path=/trunk/koffice/; revision=297594

---
+++ kplato/kpttaskresourcespanelbase.ui.h
@@ -0,0 +1,14 @@
+/****************************************************************************
+** ui.h extension file, included from the uic-generated form implementation.
+**
+** If you wish to add, delete or rename functions or slots use
+** Qt Designer which will update this file, preserving your code. Create an
+** init() function in place of a constructor, and a destroy() function in
+** place of a destructor.
+*****************************************************************************/
+
+void KPTTaskResourcesPanelBase::init()
+{
+    effort = new KPTDurationWidget( groupBox1, "effort" );
+    layout1->addWidget( effort, 0, 1 );
+}
