41365699eb04f6940f35e8d6dcd276668575ed58
David Faure <faure@kde.org>
2004-03-25 19:45:31 +0000

    Applying patch by Raphael Langerhorst to fix [Bug 77574] koshell sets the wrong URL (the temporary converted file!!) for imported documents
    CCMAIL: 77574-done@bugs.kde.org
    
    svn path=/branches/KOFFICE_1_3_BRANCH/koffice/; revision=298587

--- lib/kofficecore/koDocument.cc
+++ lib/kofficecore/koDocument.cc
@@ -461,6 +461,11 @@ QCString KoDocument::mimeType() const
     return d->mimeType;
 }
 
+void KoDocument::setMimeType( const QCString & mimeType )
+{
+    d->mimeType = mimeType;
+}
+
 void KoDocument::setOutputMimeType( const QCString & mimeType, int specialOutputFlag )
 {
     d->outputMimeType = mimeType;
