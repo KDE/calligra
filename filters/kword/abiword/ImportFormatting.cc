4069b154a50ea80426c9451f45ae87f019230f9e
Nicolas Goutte <nicolasg@snafu.de>
2004-03-27 18:51:45 +0000

    KWord does not seem to like compatibility entries in case of an "at-least" line spacing.
    svn path=/trunk/koffice/; revision=299093

--- filters/kword/abiword/ImportFormatting.cc
+++ filters/kword/abiword/ImportFormatting.cc
@@ -1,7 +1,7 @@
-// $Header$
+//
 
 /* This file is part of the KDE project
-   Copyright (C) 2001, 2002 Nicolas GOUTTE <goutte@kde.org>
+   Copyright (C) 2001, 2002, 2004 Nicolas GOUTTE <goutte@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -344,7 +344,7 @@ void AddLayout(const QString& strStyleName, QDomElement& layoutElement,
                 }
 
                 // We have a meaningful value, so use it!
-                element.setAttribute( "value", lineHeight );
+                //element.setAttribute( "value", lineHeight );
                 element.setAttribute( "spacingvalue", lineHeight );
             }
         }
