f1f77b0d37fc47667e76cafbe96927d9bb85f67c
Marc Heyvaert <marc_heyvaert@yahoo.com>
2004-03-28 20:22:25 +0000

    CVS_SILENT Copyright fix
    svn path=/trunk/koffice/; revision=299328

--- kspread/dialogs/kspread_dlg_styles.cc
+++ kspread/dialogs/kspread_dlg_styles.cc
@@ -1,5 +1,6 @@
 /* This file is part of the KDE project
-   Copyright (C) 2003 Norbert Andres, nandres@web.de
+   Copyright (C) 2003 Laurent Montel <montel@kde.org>
+             (C) 2003 Norbert Andres <nandres@web.de>  
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
