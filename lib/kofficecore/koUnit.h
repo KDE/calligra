3c13104f6fa12c8e8a620971ff6d7d8cc451fe27
Nicolas Goutte <nicolasg@snafu.de>
2004-04-02 22:47:10 +0000

    CVS_SILENT: I forgot the copyrights
    svn path=/trunk/koffice/; revision=300866

--- lib/kofficecore/koUnit.h
+++ lib/kofficecore/koUnit.h
@@ -1,5 +1,6 @@
 /* This file is part of the KDE project
    Copyright (C) 1998, 1999 Reginald Stadlbauer <reggie@kde.org>, Torben Weis <weis@kde.org>
+   Copyright (C) 2004, Nicolas GOUTTE <goutte@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
