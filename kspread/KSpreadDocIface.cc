25ee90e06447eccb383a9b7f0adeba392ba39696
Marc Heyvaert <marc_heyvaert@yahoo.com>
2004-04-04 17:22:43 +0000

    CVS_SILENT Copyright fix
    svn path=/trunk/koffice/; revision=301288

--- kspread/KSpreadDocIface.cc
+++ kspread/KSpreadDocIface.cc
@@ -1,3 +1,30 @@
+/* This file is part of the KDE project
+   
+   Copyright 2004 Ariya Hidayat <ariya@kde.org>
+   Copyright 2003 Norbert Andres <nandres@web.de>
+   Copyright 2002 Laurent Montel <montel@kde.org>
+   Copyright 2002 Simon Hausmann <hausmann@kde.org>
+   Copyright 2001-2002 Philipp Mueller <philipp.mueller@gmx.de>
+   Copyright 2000 Stephan Kulow <coolo@kde.org>
+   Copyright 1999-2000 David Faure <faure@kde.org>
+   Copyright 1999 Torben Weis <weis@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
 #include "KSpreadDocIface.h"
 #include <KoDocumentIface.h>
 
