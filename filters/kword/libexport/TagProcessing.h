71bc44891988c39d5754d1fcb5dd8fe0024fd707
Nicolas Goutte <nicolasg@snafu.de>
2004-04-11 14:45:18 +0000

    Replace the TagProcessing files by the better versions of the KWord 1.3 import filter
    svn path=/trunk/koffice/; revision=302828

--- filters/kword/libexport/TagProcessing.h
+++ filters/kword/libexport/TagProcessing.h
@@ -1,8 +1,8 @@
-// $Header$
+//
 
 /*
    This file is part of the KDE project
-   Copyright (C) 2001, 2002 Nicolas GOUTTE <goutte@kde.org>
+   Copyright (C) 2001, 2002, 2004 Nicolas GOUTTE <goutte@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -43,13 +43,15 @@
 
 class KWEFKWordLeader;
 
-// The class TagProcessing and the two functions ProcessSubtags () and
-// AllowNoSubtags () allow for easing parsing of subtags in the
-// current tag. If don't expect any subtags you call AllowNoSubtags ().
-// Otherwise you create a list of TagProcessing elements and pass that
-// to ProcessSubtags () which will go through all subtags it can find,
-// call the corresponding processing function, and do all the
-// necessary error handling.
+/**
+ * The class TagProcessing and the two functions ProcessSubtags () and
+ * AllowNoSubtags () allow for easing parsing of subtags in the
+ * current tag. If don't expect any subtags you call AllowNoSubtags ().
+ * Otherwise you create a list of TagProcessing elements and pass that
+ * to ProcessSubtags () which will go through all subtags it can find,
+ * call the corresponding processing function, and do all the
+ * necessary error handling.
+ */
 
 class TagProcessing
 {
@@ -57,9 +59,16 @@ class TagProcessing
         TagProcessing ()
         {}
 
-        TagProcessing (QString  n,
-                       void     (*p)(QDomNode, void *, KWEFKWordLeader*),
-                       void    *d) : name (n), processor (p), data (d)
+        TagProcessing (const QString& n,
+                       void (*p)(QDomNode, void *, KWEFKWordLeader*),
+                       void *d) : name (n), processor (p), data (d)
+        {}
+
+        /**
+         * No-op constructor
+         * Avoids a warning that a tag is unknown.
+         */
+        TagProcessing (const QString& _name) : name(_name), processor(0), data(0)
         {}
 
         QString  name;
@@ -67,40 +76,87 @@ class TagProcessing
         void    *data;
 };
 
-void ProcessSubtags     ( QDomNode                   parentNode,
+void ProcessSubtags     ( const QDomNode             &parentNode,
                           QValueList<TagProcessing>  &tagProcessingList,
                           KWEFKWordLeader            *leader);
 
-void AllowNoSubtags ( QDomNode myNode, KWEFKWordLeader *leader );
+void AllowNoSubtags ( const QDomNode& myNode, KWEFKWordLeader *leader );
 
-// The class AttrProcessing and the two functions ProcessAttributes ()
-// and AllowNoSubtags () allow for easing parsing of the current tag's
-// attributes. If don't expect any attributes you call AllowNoAttributes ().
-// Otherwise you create a list of AttrProcessing elements and pass
-// that to ProcessAttributes () which will go through all attributes
-// it can find, retrieve the value in the datatype defined, and do all
-// the necessary error handling.
 
+/**
+ * The class AttrProcessing and the two functions ProcessAttributes ()
+ * and AllowNoSubtags () allow for easing parsing of the current tag's
+ * attributes. If don't expect any attributes you call AllowNoAttributes ().
+ * Otherwise you create a list of AttrProcessing elements and pass
+ * that to ProcessAttributes () which will go through all attributes
+ * it can find, retrieve the value in the datatype defined, and do all
+ * the necessary error handling.
+ */
 
 class AttrProcessing
 {
-    public:
-        AttrProcessing ()
-        {}
+public:
 
-        AttrProcessing ( QString   n,
-                         QString   t,
-                         void     *d  ) : name (n), type (t), data (d)
-        {}
+    enum AttrType
+        { AttrNull = 0, AttrInt, AttrDouble, AttrBool, AttrQString };
 
-        QString   name;
-        QString   type;
-        void     *data;
+    /**
+     * Default constructor
+     */
+    AttrProcessing () : type( AttrNull ), data(0) {}
+
+    /**
+     * Old constructor (without type checking)
+     * Deprecated, do not use for new code
+     */
+    AttrProcessing ( const QString& n, const QString& t, void *d );
+
+    /**
+     * No-op constructor
+     * This just avoids that a warning is triggered that an attribute is not known
+     */
+    AttrProcessing ( const QString& _name )
+        : name( _name ), type( AttrNull ), data( 0 ) {}
+
+    /**
+     * Integer constructor
+     * Get the attribute value as an integer
+     */
+    AttrProcessing ( const QString& _name, int& i )
+        : name( _name ), type( AttrInt ), data( &i ) {}
+
+    /**
+     * Double constructor
+     * Get the attribute value as a double
+     */
+    AttrProcessing ( const QString& _name, double& d )
+        : name( _name ), type( AttrDouble ), data( &d ) {}
+
+    /**
+     * Boolean constructor
+     * Get the attribute value as a bool
+     * (The values "yes,", "no", "true", "false", "0" and "1" are supported)
+     */
+    AttrProcessing ( const QString& _name, bool& flag )
+        : name( _name ), type( AttrBool ), data( &flag ) {}
+
+    /**
+     * String constructor
+     * Get the attribute value as a QString
+     */
+    AttrProcessing ( const QString& _name, QString& str )
+        : name( _name ), type( AttrQString ), data( &str ) {}
+
+
+public:
+    QString   name;
+    AttrType  type;
+    void     *data;
 };
 
-void ProcessAttributes ( QDomNode                     myNode,
+void ProcessAttributes ( const QDomNode              &myNode,
                          QValueList<AttrProcessing>  &attrProcessingList );
 
-void AllowNoAttributes ( QDomNode  myNode );
+void AllowNoAttributes ( const QDomNode& myNode );
 
 #endif
