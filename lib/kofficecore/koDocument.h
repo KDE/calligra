9862fb07688f6a99b21ad0701c1861a98daf52fe
David Faure <faure@kde.org>
2004-05-03 10:00:04 +0000

    CVS_SILENT copyright and license update
    svn path=/trunk/koffice/; revision=308232

--- lib/kofficecore/koDocument.h
+++ lib/kofficecore/koDocument.h
@@ -1,7 +1,7 @@
 // -*- c-basic-offset: 4 -*-
 /* This file is part of the KDE project
    Copyright (C) 1998, 1999 Torben Weis <weis@kde.org>
-   Copyright (C) 2000, 2001 David Faure <david@mandrakesoft.com>
+   Copyright (C) 2000-2004 David Faure <faure@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
