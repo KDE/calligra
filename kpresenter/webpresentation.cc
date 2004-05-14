9e802b18624fd0eb4fd96607b1bc6f6b18bc246a
Laurent Montel <montel@kde.org>
2004-05-14 15:22:56 +0000

    Use NetAccess::file_move to fix pb "tmp file was not removed", and otherwrite file if it exists (Thanks David)
    svn path=/trunk/koffice/; revision=311418

--- kpresenter/webpresentation.cc
+++ kpresenter/webpresentation.cc
@@ -282,8 +282,7 @@ void KPWebPresentation::initCreation( KProgress *progressBar )
         srcurl.setPath( locate( "slideshow", filename, KPresenterFactory::global() ) );
         desturl = path;
         desturl.addPath( "/pics/" + filename );
-        KIO::NetAccess::del( desturl ); // Copy does not remove existing destination file
-        KIO::NetAccess::copy( srcurl, desturl );
+        KIO::NetAccess::file_move( srcurl, desturl, -1, true /*overwrite*/);
         p = progressBar->progress();
         progressBar->setProgress( ++p );
         kapp->processEvents();
@@ -305,8 +304,7 @@ void KPWebPresentation::createSlidesPictures( KProgress *progressBar )
         KTempFile tmp;
         pix.save( tmp.name(), "PNG" );
 
-        KIO::NetAccess::del( filename ); // Copy does not remove existing destination file
-        KIO::NetAccess::copy( tmp.name(), filename );
+        KIO::NetAccess::file_move( tmp.name(), filename, -1, true /*overwrite*/);
 
         p = progressBar->progress();
         progressBar->setProgress( ++p );
@@ -490,8 +488,8 @@ void KPWebPresentation::createSlidesHTML( KProgress *progressBar )
 
         file.close();
 
-        KIO::NetAccess::del( dest ); // Copy does not remove existing destination file
-        KIO::NetAccess::copy( tmp.name(), dest );
+        KIO::NetAccess::file_move( tmp.name(), dest, -1, true /*overwrite*/);
+
         int p = progressBar->progress();
         progressBar->setProgress( ++p );
         kapp->processEvents();
@@ -537,8 +535,8 @@ void KPWebPresentation::createMainPage( KProgress *progressBar )
     streamOut << "</body>\n</html>\n";
     file.close();
 
-    KIO::NetAccess::del( dest ); // Copy does not remove existing destination file
-    KIO::NetAccess::copy( tmp.name(), dest );
+    KIO::NetAccess::file_move( tmp.name(), dest, -1, true /*overwrite*/);
+
 
     progressBar->setProgress( progressBar->totalSteps() );
     kapp->processEvents();
