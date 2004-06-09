4a68d94e61c9d530c3262e1c01adeb19941633a7
Brad Hards <bradh@frogmouth.net>
2004-06-09 08:07:06 +0000

    Remove deprecated KIO::NetAccess::del() and copy() calls.
    svn path=/trunk/koffice/; revision=318989

--- kpresenter/kpresenter_doc.cc
+++ kpresenter/kpresenter_doc.cc
@@ -2846,8 +2846,10 @@ QString KPresenterDoc::templateFileName( bool chooseTemplate, const QString &the
         kdDebug(33001) << "Copying template  (in KPresenterDoc::templateFileName)" << endl
                        << "  from: " << src.prettyURL() << endl
                        << "  to: " << dest.prettyURL() << endl;
-        KIO::NetAccess::del( dest ); // copy does *not* delete, so have to do it explicitly!
-        KIO::NetAccess::copy( src, dest );
+        KIO::NetAccess::file_copy( src,
+				   dest,
+				   -1, /* default permissions */
+				   true /* overwrite */ );
     }
     return fileName;
 }
