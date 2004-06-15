f7a237d42fdaf86f8267ccd48bd3891f24da8f23
Nicolas Goutte <nicolasg@snafu.de>
2004-06-15 17:05:13 +0000

    CVS_SILENT: add copyright and header #ifdef (The 2001-2004 copyright dates are due that the parser comes from KWord's Abiword import filter.)
    svn path=/trunk/koffice/; revision=320940

--- filters/kword/kword1.3/import/kword13parser.h
+++ filters/kword/kword1.3/import/kword13parser.h
@@ -1,3 +1,27 @@
+//
+
+/*
+   This file is part of the KDE project
+   Copyright (C) 2004 Nicolas GOUTTE <goutte@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#ifndef KWORD_1_3_PARSER_H
+#define KWORD_1_3_PARSER_H
+
 #include <qxml.h>
 #include <qptrstack.h>
 
@@ -77,3 +101,5 @@ protected:
     KWordParagraph* m_currentParagraph; ///< Current paragraph
     KWord13Layout* m_currentLayout; ///< Current layout (or style)
 };
+
+#endif // KWORD_1_3_PARSER_H
