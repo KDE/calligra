b116c035ded56e6daa39df76d1fe27df1d041ace
Nicolas Goutte <nicolasg@snafu.de>
2004-06-18 07:50:08 +0000

    Create a new factory and a new main class for the filter
    svn path=/trunk/koffice/; revision=321627

---
+++ filters/kword/kword1.3/import/kword13import.h
@@ -0,0 +1,41 @@
+//
+
+/*
+   This file is part of the KDE project
+   Copyright (C) 2004 Nicolas GOUTTE <goutte@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#ifndef KWORD_1_3_IMPORT_H
+#define KWORD_1_3_IMPORT_H
+
+#include <qstring.h>
+#include <qcstring.h>
+
+#include <koFilter.h>
+
+
+class KWord13Import : public KoFilter {
+
+    Q_OBJECT
+
+public:
+    KWord13Import(KoFilter *parent, const char *name, const QStringList &);
+    virtual ~KWord13Import() {}
+
+    virtual KoFilter::ConversionStatus convert( const QCString& from, const QCString& to );
+};
+#endif // KWORD_1_3_IMPORT_H
