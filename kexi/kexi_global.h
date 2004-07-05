4a092e541569715898781c8c2032f6c0eb92ecc5
Jarosław Staniek <staniek@kde.org>
2004-07-05 21:42:48 +0000

    Beta 4 approaching....
    svn path=/trunk/koffice/; revision=326152

--- kexi/kexi_global.h
+++ kexi/kexi_global.h
@@ -30,12 +30,12 @@
 #endif
 
 #ifndef KEXI_VERSION_STRING
-# define KEXI_VERSION_STRING "0.1 beta 3"
+# define KEXI_VERSION_STRING "0.1 beta 4"
 #endif
 
 #define KEXI_VERSION_MAJOR 0
 #define KEXI_VERSION_MINOR 0 /* 0.1 beta */
-#define KEXI_VERSION_RELEASE 192
+#define KEXI_VERSION_RELEASE 193
 
 #define KEXI_MAKE_VERSION( a,b,c ) (((a) << 16) | ((b) << 8) | (c))
 
