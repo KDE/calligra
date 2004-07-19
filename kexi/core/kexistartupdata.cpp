0574d97bec0007bc6be6a984250014e0914bdc0d
Jarosław Staniek <staniek@kde.org>
2004-07-19 20:44:51 +0000

    Core KexiStartupHandler introduced: handles startup actions, thet are new performed independently from Main Window's stuff.
    svn path=/trunk/koffice/; revision=331007

---
+++ kexi/core/kexistartupdata.cpp
@@ -0,0 +1,68 @@
+/* This file is part of the KDE project
+   Copyright (C) 2004 Jaroslaw Staniek <js@iidea.pl>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include "kexistartupdata.h"
+#include "kexi.h"
+
+#include <kexidb/driver.h>
+#include <kexidb/drivermanager.h>
+
+#include <kdebug.h>
+#include <klocale.h>
+#include <kmimetype.h>
+#include <kmessagebox.h>
+#include <kcmdlineargs.h>
+#include <kdeversion.h>
+
+#include <qfileinfo.h>
+#include <qcstring.h>
+
+
+KexiStartupData::KexiStartupData()
+	: m_projectData(0)
+	, m_action(KexiStartupData::DoNothing)
+	, m_forcedFinalMode(false)
+	, m_forcedDesignMode(false)
+{
+}
+
+KexiStartupData::~KexiStartupData()
+{
+}
+
+KexiProjectData *KexiStartupData::projectData() const
+{
+	return m_projectData;
+}
+
+KexiStartupData::Action KexiStartupData::action() const
+{
+	return m_action;
+}
+
+bool KexiStartupData::forcedFinalMode() const
+{
+	return m_forcedFinalMode;
+}
+
+bool KexiStartupData::forcedDesignMode() const
+{
+	return m_forcedDesignMode;
+}
+
---
+++ kexi/core/kexistartupdata.h
@@ -0,0 +1,64 @@
+/* This file is part of the KDE project
+   Copyright (C) 2004 Jaroslaw Staniek <js@iidea.pl>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef KEXI_STARTUPDATA_H
+#define KEXI_STARTUPDATA_H
+
+#include <qstring.h>
+
+class KexiProjectData;
+
+class KEXICORE_EXPORT KexiStartupData
+{
+	public:
+		typedef enum Action {
+			DoNothing = 0,
+			CreateBlankProject = 1,
+			UseTemplate = 2,
+			OpenProject = 3
+			
+		};
+		
+		KexiStartupData();
+		virtual ~KexiStartupData();
+		
+		virtual bool init() { return true; };
+
+		Action action() const;
+
+		KexiProjectData *projectData() const;
+
+		/*! \return true is the Design Mode is forced for this project. 
+			Used on startup (by --design-mode comman line switch). */
+		bool forcedDesignMode() const;
+	
+		/*! \return true is the Final Mode is forced for this project. 
+			Used on startup (by --final-mode comman line switch). 
+			By default this is false. */
+		bool forcedFinalMode() const;
+		
+	protected:
+		KexiProjectData *m_projectData;
+		Action m_action;
+		bool m_forcedFinalMode : 1;
+		bool m_forcedDesignMode : 1;
+};
+
+#endif
+
