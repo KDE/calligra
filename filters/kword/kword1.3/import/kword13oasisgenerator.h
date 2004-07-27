171006f9a8844c4b50acc4c148aa050eea100eea
Nicolas Goutte <nicolasg@snafu.de>
2004-07-27 21:02:49 +0000

    Remove the obsolete code coming from the old filter
    svn path=/trunk/koffice/; revision=333353

--- filters/kword/kword1.3/import/kword13oasisgenerator.h
+++ filters/kword/kword1.3/import/kword13oasisgenerator.h
@@ -42,20 +42,7 @@ public:
     
     bool prepare( KWord13Document& kwordDocument );
     bool generate( const QString& fileName, KWord13Document& kwordDocument );
-    
-protected: //deprecated
-    QString escapeOOText(const QString& strText) const;
-    QString escapeOOSpan(const QString& strText) const;
-protected: // deprecated
-    bool zipPrepareWriting(const QString& name);
-    bool zipDoneWriting(void);
-    bool zipWriteData(const char* str);
-    bool zipWriteData(const QByteArray& array);
-    bool zipWriteData(const QCString& cstr);
-    bool zipWriteData(const QString& str); ///< Assumes UTF-8
 protected: // Generating phase
-    /// \deprecated
-    void writeStartOfFile( const QString& type );
     /// @param main Is it the main frameset?
     void generateTextFrameset( KoXmlWriter& writer, KWordTextFrameset* frameset, bool main );
     void writeStylesXml( void );
@@ -76,11 +63,7 @@ protected: // Preparing phase
     void fillGenStyleWithLayout( const KWord13Layout& layout, KoGenStyle& gs, const bool style ) const;
 protected:
     KWord13Document* m_kwordDocument;
-    KZip* m_zip; ///< Output OOWriter file
     KoStore* m_store;
-    uint m_size; ///< Size of ZIP entry
-    QTextStream* m_streamOut;
-    QByteArray m_contentBody; ///< office:body element of content.xml
     QString m_contentAutomaticStyles; ///< Automatic styles for content.xml (in OO format)
     KoGenStyles m_oasisGenStyles; ///< OASIS styles
     KoXmlWriter* m_manifestWriter; ///< Manifest file writer
