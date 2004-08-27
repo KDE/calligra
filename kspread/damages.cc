e44e948c296d63e760d97e52691b74a490f565c4
Ariya Hidayat <ariya@kde.org>
2004-08-27 07:49:12 +0000

    /me sucks, forgot to commit them
    svn path=/trunk/koffice/; revision=341713

---
+++ kspread/damages.cc
@@ -0,0 +1,61 @@
+/* This file is part of the KDE project
+   Copyright 2004 Ariya Hidayat <ariya@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+
+#include "damages.h"
+
+
+#include "kspread_sheet.h"
+
+namespace KSpread
+{
+
+class SheetDamage::Private
+{
+public:
+  KSpreadSheet* sheet;
+  int action;
+};
+
+};
+
+
+using namespace KSpread;
+
+SheetDamage::SheetDamage( KSpreadSheet* sheet, int action )
+{
+  d = new Private;
+  d->sheet = sheet;
+  d->action = action;
+}
+
+SheetDamage::~SheetDamage()
+{
+  delete d;
+}
+
+KSpreadSheet* SheetDamage::sheet() const
+{
+  return d->sheet;
+}
+
+int SheetDamage::action() const
+{
+  return d->action;
+}
---
+++ kspread/damages.h
@@ -0,0 +1,78 @@
+/* This file is part of the KDE project
+   Copyright 2004 Ariya Hidayat <ariya@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef KSPREAD_DAMAGES
+#define KSPREAD_DAMAGES
+
+#include <qvaluelist.h>
+
+class KSpreadSheet;
+
+namespace KSpread
+{
+
+class Damage
+{
+  public:
+  
+    typedef enum
+    {
+      Nothing = 0,
+      Document,
+      Workbook,
+      Sheet,
+      Range,
+      Cell
+    } Type;
+
+    virtual Type type() const { return Nothing; }
+};
+
+class SheetDamage : public Damage
+{
+  public:
+  
+    enum
+    {
+      None = 0,
+      ContentChanged,
+      PropertiesChanged,
+      Hidden,
+      Shown
+    };
+    
+    SheetDamage( KSpreadSheet* sheet, int action );
+    
+    virtual ~SheetDamage();
+  
+    virtual Type type() const { return Sheet; }
+    
+    KSpreadSheet* sheet() const;
+    
+    int action() const;
+    
+  private:
+    class Private;
+    Private *d;
+};
+
+
+}; // namespace KSpread
+
+#endif // KSPREAD_DAMAGES
