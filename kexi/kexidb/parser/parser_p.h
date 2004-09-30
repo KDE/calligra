021ce895f4c4a8b7bd12a3cfc32992181f28a709
Jarosław Staniek <staniek@kde.org>
2004-09-30 17:51:40 +0000

    private data
    svn path=/trunk/koffice/; revision=350574

---
+++ kexi/kexidb/parser/parser_p.h
@@ -0,0 +1,46 @@
+/* This file is part of the KDE project
+   Copyright (C) 2004 Jaroslaw Staniek <js@iidea.pl>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef KEXIDB_PARSER_P_H
+#define KEXIDB_PARSER_P_H
+
+#include <qvaluelist.h>
+#include <qdict.h>
+
+#include <kexidb/queryschema.h>
+
+namespace KexiDB {
+
+/*! Data used on parsing. @internal */
+class ParseInfo
+{
+	public:
+		ParseInfo(QuerySchema *query);
+		~ParseInfo();
+
+		//! collects positions of tables/aliases with the same names
+		QDict< QValueList<int> > repeatedTablesAndAliases;
+
+		QString errMsg, errDescr; //helpers
+		QuerySchema *querySchema;
+};
+
+}
+
+#endif
