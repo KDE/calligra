8f4ff90262b6acdcb903292681f7b2a1de89fe75
Laurent Montel <montel@kde.org>
2004-10-05 12:33:56 +0000

    Change my copyright.
    svn path=/trunk/koffice/; revision=352071

--- kchart/KChartParamsIface.cc
+++ kchart/KChartParamsIface.cc
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 2001 Laurent Montel <lmontel@mandrakesoft.com>
+   Copyright (C) 2001,2002,2003,2004 Laurent Montel <montel@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
