5bb84ac6593c7d718aaaadf24e381a70e4fbd13b
Tomas Mecir <mecirt@gmail.com>
2004-10-07 08:28:08 +0000

    Created two new classes that will hold conversion methods, which are currently spread around multiple files. Just the classes, no actual functionality as of now.
    svn path=/trunk/koffice/; revision=352571

---
+++ kspread/convert.h
@@ -0,0 +1,56 @@
+/* This file is part of the KDE project
+   Copyright 2004 Tomas Mecir <mecirt@gmail.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef KSPREAD_CONVERT
+#define KSPREAD_CONVERT
+
+/*
+This file contains two classes, the former of them can convert between
+various data types of KSpreadValue, the latter can generate a textual
+representation of data in KSpreadValue with a given formatting.
+
+Methods contained here were originally scattered around kspread_cell.*,
+kspread_util.*, formula.* and probably other places as well
+
+*/
+
+// KSpread namespace
+namespace KSpread {
+
+/**
+The Convert class contains various methods used to convert between
+various data types of KSpreadValue. Things like number to string,
+string to bool, and so on */
+class Convert {
+
+};
+
+
+/** The OutTextConvert class generates a textual representation of
+data stored in a KSpreadValue, with a given formatting */
+
+class OutTextConvert {
+
+};
+
+
+};  //end of KSpread namespace
+
+#endif //KSPREAD_CONVERT
+
