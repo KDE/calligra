631c91d715b28a77910c83536e691b6bc81c0d41
Jarosław Staniek <staniek@kde.org>
2004-10-24 18:34:56 +0000

    CVS_SILENT: compile fix...
    svn path=/trunk/koffice/; revision=357379

--- kexi/tableview/kexidatetimeeditor_p.h
+++ kexi/tableview/kexidatetimeeditor_p.h
@@ -18,89 +18,97 @@ class QDateTimeEditorPrivate;
 
 class QDateTimeEditor : public QWidget
 {
-    Q_OBJECT
+	Q_OBJECT
 public:
-    uint QDateTimeEditor::sectionCount() const;
-    int  focusSection() const;
-    bool setFocusSection( int s );
+	uint QDateTimeEditor::sectionCount() const;
+	int  focusSection() const;
+	bool setFocusSection( int s );
 
 private:
-    QDateTimeEditBase* cw;
-    QDateTimeEditorPrivate* d;
+	QDateTimeEditBase* cw;
+	QDateTimeEditorPrivate* d;
 };
 
 
 class Q_EXPORT QNumberSection
 {
 public:
-    int selectionStart() const;
-    int selectionEnd() const;
-    Q_DUMMY_COMPARISON_OPERATOR( QNumberSection )
+	int selectionStart() const;
+	int selectionEnd() const;
+	Q_DUMMY_COMPARISON_OPERATOR( QNumberSection )
 private:
-    int selstart :12;
-    int selend	 :12;
-    int act	 :7;
-    bool sep	 :1;
+	int selstart :12;
+	int selend	 :12;
+	int act	 :7;
+	bool sep	 :1;
 };
 
 //! needed for focusSection() and setFocusSection()
 class QDateTimeEditorPrivate
 {
 public:
-    uint sectionCount() const { return (uint)sections.count(); }
-    int focusSection() const { return focusSec; }
-    bool setFocusSection( int idx )
-    {
-	if ( idx > (int)sections.count()-1 || idx < 0 )
-	    return FALSE;
-	if ( idx != focusSec ) {
-	    focusSec = idx;
-	    applyFocusSelection();
-	    return TRUE;
-	}
-	return FALSE;
-    }
+	uint sectionCount() const;
+	int focusSection() const;
+	bool setFocusSection( int idx );
 
 protected:
-    void applyFocusSelection()
-    {
-	if ( focusSec > -1 ) {
-	    int selstart = sections[ focusSec ].selectionStart();
-	    int selend = sections[ focusSec ].selectionEnd();
-	    parag->setSelection( QTextDocument::Standard, selstart, selend );
-	    parag->format();
-	    if ( parag->at( selstart )->x < offset ||
-		 parag->at( selend )->x + parag->string()->width( selend ) > offset + sz.width() ) {
-		offset = parag->at( selstart )->x;
-	    }
-	}
-    }
+	void applyFocusSelection();
+
 private:
-    bool frm;
-    QTextParagraph *parag;
-    QTextCursor *cursor;
-    QSize sz;
-    int focusSec;
-    QValueList< QNumberSection > sections;
-    QString sep;
-    int offset;
+	bool frm;
+	QTextParagraph *parag;
+	QTextCursor *cursor;
+	QSize sz;
+	int focusSec;
+	QValueList< QNumberSection > sections;
+	QString sep;
+	int offset;
 };
 
+uint QDateTimeEditorPrivate::sectionCount() const { return (uint)sections.count(); }
+
+int QDateTimeEditorPrivate::focusSection() const { return focusSec; }
+
+bool QDateTimeEditorPrivate::setFocusSection( int idx )
+{
+	if ( idx > (int)sections.count()-1 || idx < 0 )
+		return FALSE;
+	if ( idx != focusSec ) {
+		focusSec = idx;
+		applyFocusSelection();
+		return TRUE;
+	}
+	return FALSE;
+}
+
+void QDateTimeEditorPrivate::applyFocusSelection()
+{
+	if ( focusSec > -1 ) {
+		int selstart = sections[ focusSec ].selectionStart();
+		int selend = sections[ focusSec ].selectionEnd();
+		parag->setSelection( QTextDocument::Standard, selstart, selend );
+		parag->format();
+		if ( parag->at( selstart )->x < offset ||
+			parag->at( selend )->x + parag->string()->width( selend ) > offset + sz.width() ) {
+			offset = parag->at( selstart )->x;
+		}
+	}
+}
 
 int QDateTimeEditor::focusSection() const
 {
-    return d->focusSection();
+	return d->focusSection();
 }
 
 bool QDateTimeEditor::setFocusSection( int sec )
 {
-    return d->setFocusSection( sec );
+	return d->setFocusSection( sec );
 }
 
 //added
 uint QDateTimeEditor::sectionCount() const
 {
-    return d->sectionCount();
+	return d->sectionCount();
 }
 
 #endif
