4dd553ac3910913706595bb8de3d53380a82bf91
Cédric Pasteur <cedric.pasteur@free.fr>
2004-10-27 16:29:02 +0000

    Missing files
    svn path=/trunk/koffice/; revision=358243

---
+++ kexi/formeditor/utils.cpp
@@ -0,0 +1,94 @@
+/* This file is part of the KDE project
+   Copyright (C) 2004 Cedric Pasteur <cedric.pasteur@free.fr>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include <qcursor.h>
+#include <qobjectlist.h>
+#include <kdebug.h>
+
+#include "form.h"
+#include "objecttree.h"
+#include "utils.h"
+
+namespace KFormDesigner {
+
+/// Helper function to clear a list (by removing all children)
+void removeChildrenFromList(WidgetList &list)
+{
+	for(WidgetListIterator it(list); it.current() != 0; ++it)  {
+		QWidget *w = it.current();
+
+		// If any widget in the list is a child of this widget, we remove it from the list
+		for(WidgetListIterator it2(list); it2.current() != 0; ++it2) {
+			QWidget *widg = it2.current();
+			if((w != widg) && (w->child(widg->name())))
+			{
+				kdDebug() << "Removing the widget " << widg->name() << "which is a child of " << w->name() << endl;
+				list.remove(widg);
+			}
+		}
+	}
+}
+
+//// Helper functions for event filtering on composed widgets
+void installRecursiveEventFilter(QObject *object, QObject *container)
+{
+	if(!object->isWidgetType())
+		return;
+
+	object->installEventFilter(container);
+	if(((QWidget*)object)->ownCursor())
+		((QWidget*)object)->setCursor(QCursor(Qt::ArrowCursor));
+
+	if(!object->children())
+		return;
+
+	QObjectList list = *(object->children());
+	for(QObject *obj = list.first(); obj; obj = list.next())
+		installRecursiveEventFilter(obj, container);
+}
+
+void removeRecursiveEventFilter(QObject *object, QObject *container)
+{
+	object->removeEventFilter(container);
+	if(!object->isWidgetType())
+		return;
+	if(!object->children())
+		return;
+
+	QObjectList list = *(object->children());
+	for(QObject *obj = list.first(); obj; obj = list.next())
+		removeRecursiveEventFilter(obj, container);
+}
+
+void setRecursiveCursor(QWidget *w, Form *form)
+{
+	ObjectTreeItem *tree = form->objectTree()->lookup(w->name());
+	if(tree && ((tree->modifiedProperties()->contains("cursor")) || !tree->children()->isEmpty()) )
+			return; // if the user has set a cursor for this widget or this is a container, don't change it
+
+	if(w->ownCursor())
+		w->setCursor(Qt::ArrowCursor);
+
+	QObjectList *l = w->queryList( "QWidget" );
+	for(QObject *o = l->first(); o; o = l->next())
+		((QWidget*)o)->setCursor(Qt::ArrowCursor);
+	delete l;
+}
+
+}
---
+++ kexi/formeditor/utils.h
@@ -0,0 +1,84 @@
+/* This file is part of the KDE project
+   Copyright (C) 2004 Cedric Pasteur <cedric.pasteur@free.fr>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef FORMEDITORUTILS_H
+#define FORMEDITORUTILS_H
+
+#include <qptrlist.h>
+#include <qwidget.h>
+
+namespace KFormDesigner {
+
+class Form;
+typedef QPtrList<QWidget> WidgetList;
+typedef QPtrListIterator<QWidget> WidgetListIterator;
+
+// Helper classes for sorting widgets
+class HorWidgetList : public WidgetList
+{
+	public:
+	HorWidgetList() {;}
+	virtual int compareItems(QPtrCollection::Item item1, QPtrCollection::Item item2)
+	{
+		QWidget *w1 = static_cast<QWidget*>(item1);
+		QWidget *w2 = static_cast<QWidget*>(item2);
+
+		if(w1->x() < w2->x())
+			return -1;
+		if(w1->x() > w2->x())
+			return 1;
+		return 0; // item1 == item2
+	}
+};
+
+class VerWidgetList : public WidgetList
+{
+	public:
+	VerWidgetList() {;}
+	virtual int compareItems(QPtrCollection::Item item1, QPtrCollection::Item item2)
+	{
+		QWidget *w1 = static_cast<QWidget*>(item1);
+		QWidget *w2 = static_cast<QWidget*>(item2);
+
+		if(w1->y() < w2->y())
+			return -10;
+		if(w1->y() > w2->y())
+			return 1;
+		return 0; // item1 == item2
+	}
+};
+
+/*! This function is used to remove all the child widgets from a list, and
+  keep only the "toplevel" ones. */
+void KFORMEDITOR_EXPORT removeChildrenFromList(WidgetList &list);
+
+/*! This helper function install an event filter on \a object and all of its children, directed to \a container.
+  This is necessary to filter events for composed widgets. */
+void KFORMEDITOR_EXPORT installRecursiveEventFilter(QObject *object, QObject *container);
+
+/*! This helper function removes an event filter installed before on \a object and all of its children.
+  This is necessary to filter events for composed widgets. */
+void KFORMEDITOR_EXPORT removeRecursiveEventFilter(QObject *object, QObject *container);
+
+void KFORMEDITOR_EXPORT setRecursiveCursor(QWidget *w, Form *form);
+
+}
+
+#endif
+
