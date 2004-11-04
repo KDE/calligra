97a5a125d027f14881e43c75d1be799128a950d6
Dag Andersen <danders@get2net.dk>
2004-11-04 11:26:37 +0000

    Add copyright header for now. Hope these can be converted to ui later.
    svn path=/trunk/koffice/; revision=360326

--- kplato/kptcalendarlistdialogbase.h
+++ kplato/kptcalendarlistdialogbase.h
@@ -1,11 +1,22 @@
-/****************************************************************************
-** Form interface generated from reading ui file './kptcalendarsdialogbase.ui'
-**
-** Created: Fri Jan 16 09:28:56 2004
-**      by: The User Interface Compiler ($Id$)
-**
-** WARNING! All changes made in this file will be lost!
-****************************************************************************/
+/* This file is part of the KDE project
+   Copyright (C) 2004 Dag Andersen <danders@get2net.dk>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
 
 #ifndef KPTCALENDARLISTDIALOGBASE_H
 #define KPTCALENDARLISTDIALOGBASE_H
