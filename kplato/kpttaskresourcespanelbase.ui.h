768b6e4bacee3befd61b3f799b8bf4540ed42784
Dag Andersen <danders@get2net.dk>
2004-11-19 12:56:55 +0000

    Add license to ui.h files
    svn path=/trunk/koffice/; revision=364231

--- kplato/kpttaskresourcespanelbase.ui.h
+++ kplato/kpttaskresourcespanelbase.ui.h
@@ -7,6 +7,25 @@
 ** place of a destructor.
 *****************************************************************************/
 
+/* This file is part of the KDE project
+   Copyright (C) 2004 Dag Andersen <danders@get2net.dk>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation;
+   version 2 of the License.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
 using namespace KPlato;
 
 void KPTTaskResourcesPanelBase::init()
