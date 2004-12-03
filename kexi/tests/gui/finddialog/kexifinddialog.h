4299750e3f4ae33e93e730eb8c2c0e4c4cd4ae2e
Jarosław Staniek <staniek@kde.org>
2004-12-03 17:36:44 +0000

    finddialog prototype test added
    svn path=/trunk/koffice/; revision=368145

---
+++ kexi/tests/gui/finddialog/kexifinddialog.h
@@ -0,0 +1,62 @@
+/* This file is part of the KDE project
+   Copyright (C) 2004 Jaroslaw Staniek <js@iidea.pl>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef KEXIFINDDIALOG_H
+#define KEXIFINDDIALOG_H
+
+#include "kexifinddialogbase.h"
+
+/*! @brief A Kexi-specific "Find text" dialog.
+
+	Also used for replace.
+*/
+class KexiFindDialog : public KexiFindDialogBase
+{
+	Q_OBJECT
+	public:
+		KexiFindDialog( QWidget* parent = 0, const char* name = 0, bool modal = FALSE );
+		virtual ~KexiFindDialog();
+
+#if 0
+TODO TODO TODO TODO TODO TODO 
+	/*! Sets \a columnNames list for 'look in column' combo box. 
+	 "(All columns)" item is also prepended. */
+	void setLookInColumnList(const QStringList& columnNames);
+
+	/*! \return column name selected in 'look in column' combo box.
+	 If "(All columns)" item is selected, "*" is returned. */
+	QString lookInColumn() const;
+
+	/*! Selecte \a columnName to be selected 'look in column'.
+	 By default "(All columns)" item is selected. To select this item, pass "*". */
+	void setLookInColumn(const QString& columnName);
+
+#endif
+
+	public slots:
+		/*! Sets or clears replace mode. 
+		 For replace mode 'prompt or replace' option is visible.
+		*/
+		void setReplaceMode(bool set);
+
+	protected:
+		bool m_replaceMode : 1;
+};
+
+#endif
---
+++ kexi/tests/gui/finddialog/main.cpp
@@ -0,0 +1,36 @@
+/* This file is part of the KDE project
+   Copyright (C) 2004 Jaroslaw Staniek <js@iidea.pl>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include <kapplication.h>
+#include <klocale.h>
+#include <kaboutdata.h>
+#include <kcmdlineargs.h>
+
+#include "kexifinddialog.h"
+
+int main( int argc, char ** argv )
+{
+  KAboutData aboutData( "test", I18N_NOOP("KFind"), "0", "", KAboutData::License_LGPL );
+  KCmdLineArgs::init( argc, argv, &aboutData );
+  KApplication app;
+
+  KexiFindDialog dlg(0, "dialog");
+
+  return dlg.exec();
+}
