aef5dfc923b1dd3602f4a53f73970889e86c2a99
Jarosław Staniek <staniek@kde.org>
2005-01-15 13:27:31 +0000

    Forms - KexiDataItemChangesListener added to allow signaling changes   from widgets implementing KexiDataItemInterface
    svn path=/trunk/koffice/; revision=378727

--- kexi/core/kexidataiteminterface.h
+++ kexi/core/kexidataiteminterface.h
@@ -22,6 +22,20 @@
 
 #include <qstring.h>
 #include <qvariant.h>
+#include <qguardedptr.h>
+
+class KexiDataItemInterface;
+
+class KEXICORE_EXPORT KexiDataItemChangesListener
+{
+	public:
+		KexiDataItemChangesListener();
+		~KexiDataItemChangesListener();
+
+		//! Implement this to react for change of \a item.
+		//! Called by KexiDataItemInterface::valueChanged()
+		virtual void valueChanged(KexiDataItemInterface* item) = 0;
+};
 
 //! An interface for declaring widgets to be data-aware.
 class KEXICORE_EXPORT KexiDataItemInterface
@@ -36,12 +50,31 @@ class KEXICORE_EXPORT KexiDataItemInterface
 
 		//! Sets the name of the data source for this widget
 		//! Data source usually means here a table or query or field name name.
-		void setDataSource(const QString &ds) { m_dataSource = ds; }
+		inline void setDataSource(const QString &ds) { m_dataSource = ds; }
 
-		virtual void setValue(const QVariant& value) = 0;
+		//! Sets listener 
+		void installListener(KexiDataItemChangesListener* listener);
+
+		//! Sets value \a value for a widget. 
+		//! Just calls setValueInternal(), but also blocks valueChanged() 
+		//! as you probably don't want to react on your own change
+		void setValue(const QVariant& value);
+
+		//! \return value saved in a widget item.
+		virtual QVariant value() = 0;
 
 	protected:
+		//! Sets value \a value for a widget. 
+		//! Implement this method to allow setting value for this widget item.
+		virtual void setValueInternal(const QVariant& value) = 0;
+
+		//! Call this in your inplementation when value changes, 
+		//! so installed listener can react on this change.
+		void valueChanged();
+
 		QString m_dataSource;
+		KexiDataItemChangesListener* m_listener;
+		bool m_disable_valueChanged : 1;
 };
 
 #endif
