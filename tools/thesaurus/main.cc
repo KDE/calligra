fdbd018c42c15155dfa7169dbabb7b5bf803a2d0
Stephan Binner <binner@kde.org>
2005-02-06 20:35:55 +0000

    iconset for disabled icons
    svn path=/trunk/koffice/; revision=386871

--- tools/thesaurus/main.cc
+++ tools/thesaurus/main.cc
@@ -96,11 +96,11 @@ Thesaurus::Thesaurus(QObject* parent, const char* name, const QStringList &)
     row1->addWidget(m_edit, 1);
     row1->addWidget(m_search, 0);
     m_back = new QToolButton(m_page);
-    m_back->setPixmap(BarIcon(QString::fromLatin1("back")));
+    m_back->setIconSet(BarIconSet(QString::fromLatin1("back")));
     QToolTip::add(m_back, i18n("Back"));
     row1->addWidget(m_back, 0);
     m_forward = new QToolButton(m_page);
-    m_forward->setPixmap(BarIcon(QString::fromLatin1("forward")));
+    m_forward->setIconSet(BarIconSet(QString::fromLatin1("forward")));
     QToolTip::add(m_forward, i18n("Forward"));
     row1->addWidget(m_forward, 0);
     m_lang = new KPushButton(i18n("Change Language..."), m_page);
