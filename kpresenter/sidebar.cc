cb3d91b36142e658980a1397300f682d189265cc
Laurent Montel <montel@kde.org>
2005-02-13 13:00:41 +0000

    Add item "Copy Slide" as requested by David at "Solution Linux 2005"
    svn path=/trunk/koffice/; revision=388957

--- kpresenter/sidebar.cc
+++ kpresenter/sidebar.cc
@@ -3,6 +3,7 @@
    Copyright (C) 1998, 1999 Reginald Stadlbauer <reggie@kde.org>
    Copyright (C) 2001 Lukas Tinkl <lukas@kde.org>
    Copyright (C) 2002 Ariya Hidayat <ariya@kde.org>
+   Copyright (C) 2005 Laurent Montel <montel@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
