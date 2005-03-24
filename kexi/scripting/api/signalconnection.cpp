c74e706a49a62526feaa5da2bb83902ab14618ce
Sebastian Sauer <mail@dipe.org>
2005-03-24 16:14:36 +0000

    We've all needed slots in the SignalConnection class in place now.
    svn path=/trunk/koffice/; revision=400262

--- kexi/scripting/api/signalconnection.cpp
+++ kexi/scripting/api/signalconnection.cpp
@@ -25,6 +25,8 @@
 #include "../main/scriptcontainer.h"
 #include "signalhandler.h"
 
+#include <qvaluelist.h>
+
 using namespace Kross::Api;
 
 SignalConnection::SignalConnection(SignalHandler* signalhandler)
@@ -33,19 +35,64 @@ SignalConnection::SignalConnection(SignalHandler* signalhandler)
 {
 }
 
-void SignalConnection::callback() { callback_variant(QVariant()); }
-void SignalConnection::callback_short(short s) { callback_variant(QVariant(s)); }
-void SignalConnection::callback_int(int i) { callback_variant(QVariant(i)); }
-void SignalConnection::callback_uint(uint i) { callback_variant(QVariant(i)); }
-void SignalConnection::callback_long(long l) { callback_variant(QVariant((Q_LLONG)l)); }
-void SignalConnection::callback_ulong(ulong l) { callback_variant(QVariant((Q_ULLONG)l)); }
-void SignalConnection::callback_llong(Q_LLONG l) { callback_variant(QVariant(l)); }
-void SignalConnection::callback_ullong(Q_ULLONG l) { callback_variant(QVariant(l)); }
-void SignalConnection::callback_double(double d) { callback_variant(QVariant(d)); }
-void SignalConnection::callback_char(const char* c) { callback_variant(QVariant(c)); }
-void SignalConnection::callback_bool(bool b) { callback_variant(QVariant(b)); }
-void SignalConnection::callback_string(const QString& s) { callback_variant(QVariant(s)); }
-void SignalConnection::callback_stringlist(const QStringList& sl) { callback_variant(QVariant(sl)); }
+void SignalConnection::callback() {
+    callback_variant(QVariant()); }
+void SignalConnection::callback_short(short s) {
+    callback_variant(QVariant(s)); }
+void SignalConnection::callback_int(int i) {
+    callback_variant(QVariant(i)); }
+void SignalConnection::callback_int(int i1, int i2) {
+    callback_variant(QVariant( QValueList<QVariant>() << i1 << i2 )); }
+void SignalConnection::callback_intintint(int i1, int i2, int i3) {
+    callback_variant(QVariant( QValueList<QVariant>() << i1 << i2 << i3 )); }
+void SignalConnection::callback_intintintint(int i1, int i2, int i3, int i4) {
+    callback_variant(QVariant( QValueList<QVariant>() << i1 << i2 << i3 << i4 )); }
+void SignalConnection::callback_intintintintint(int i1, int i2, int i3, int i4, int i5) {
+    callback_variant(QVariant( QValueList<QVariant>() << i1 << i2 << i3 << i4 << i5 )); }
+void SignalConnection::callback_intintintintbool(int i1, int i2, int i3, int i4, bool b) {
+    callback_variant(QVariant( QValueList<QVariant>() << i1 << i2 << i3 << i4 << b )); }
+void SignalConnection::callback_intbool(int i1, bool b) {
+    callback_variant(QVariant( QValueList<QVariant>() << i1 << b )); }
+void SignalConnection::callback_intintbool(int i1, int i2, bool b) {
+    callback_variant(QVariant( QValueList<QVariant>() << i1 << i2 << b )); }
+void SignalConnection::callback_intintstring(int i1, int i2, const QString& s) {
+    callback_variant(QVariant( QValueList<QVariant>() << i1 << i2 << s )); }
+void SignalConnection::callback_uint(uint i) {
+    callback_variant(QVariant(i)); }
+void SignalConnection::callback_long(long l) {
+    callback_variant(QVariant((Q_LLONG)l)); }
+void SignalConnection::callback_ulong(ulong l) {
+    callback_variant(QVariant((Q_ULLONG)l)); }
+void SignalConnection::callback_llong(Q_LLONG l) {
+    callback_variant(QVariant(l)); }
+void SignalConnection::callback_ullong(Q_ULLONG l) {
+    callback_variant(QVariant(l)); }
+void SignalConnection::callback_double(double d) {
+    callback_variant(QVariant(d)); }
+void SignalConnection::callback_char(const char* c) {
+    callback_variant(QVariant(c)); }
+void SignalConnection::callback_bool(bool b) {
+    callback_variant(QVariant(b)); }
+void SignalConnection::callback_string(const QString& s) {
+    callback_variant(QVariant(s)); }
+void SignalConnection::callback_stringint(const QString& s, int i) {
+    callback_variant(QVariant( QValueList<QVariant>() << s << i )); }
+void SignalConnection::callback_stringintint(const QString& s, int i1, int i2) {
+    callback_variant(QVariant( QValueList<QVariant>() << s << i1 << i2 )); }
+void SignalConnection::callback_stringuint(const QString& s, uint i) {
+    callback_variant(QVariant( QValueList<QVariant>() << s << i )); }
+void SignalConnection::callback_stringbool(const QString& s, bool b) {
+    callback_variant(QVariant( QValueList<QVariant>() << s << b )); }
+void SignalConnection::callback_stringboolbool(const QString& s, bool b1, bool b2) {
+    callback_variant(QVariant( QValueList<QVariant>() << s << b1 << b2 )); }
+void SignalConnection::callback_stringboolint(const QString& s, bool b, int i) {
+    callback_variant(QVariant( QValueList<QVariant>() << s << b << i )); }
+void SignalConnection::callback_stringstring(const QString& s1, const QString& s2) {
+    callback_variant(QVariant( QValueList<QVariant>() << s1 << s2 )); }
+void SignalConnection::callback_stringstringstring(const QString& s1, const QString& s2, const QString& s3) {
+    callback_variant(QVariant( QValueList<QVariant>() << s1 << s2 << s3 )); }
+void SignalConnection::callback_stringlist(const QStringList& sl) {
+    callback_variant(QVariant(sl)); }
 
 void SignalConnection::callback_variant(const QVariant& variant)
 {
