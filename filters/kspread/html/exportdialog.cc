fba3954ab899371fe25326c77c903a84592102fa
Bram Schoenmakers <bramschoenmakers@kde.nl>
2005-04-05 16:54:21 +0000

    CVS_SILENT: Copy-paste error
    svn path=/trunk/koffice/; revision=403347

--- filters/kspread/html/exportdialog.cc
+++ filters/kspread/html/exportdialog.cc
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 2001 Eva Brucherseifer <eva@kde.org>
+   Copyright (C) 2005 Bram Schoenmakers <bramschoenmakers@kde.nl>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
