3814dbf43d5bfd59569ac5cb463e463e8449604e
Inge Wallin <inge@lysator.liu.se>
2005-04-08 20:51:18 +0000

    Make the filter actually work.
    - Read data and create a picture
    - Link with the KChart document library.
    - Update the status.html info page
    
    Left to do:
     - Give the user a dialog to select the size of the image.
    
    svn path=/trunk/koffice/; revision=404151

--- filters/kchart/png/pngexport.cpp
+++ filters/kchart/png/pngexport.cpp
@@ -1,10 +1,19 @@
 // Please add a license header here and change the copyright
 // to your name.
 
+#include <qpixmap.h>
+#include <qpainter.h>
+
+#include <kmessagebox.h>
+
 #include <koFilterChain.h>
+#include <koStore.h>
+//#include <koStoreDevice.h>
 #include <kgenericfactory.h>
 
-#include <pngexport.h>
+#include "kchart_part.h"
+
+#include "pngexport.h"
 
 
 typedef KGenericFactory<PngExport, KoFilter> PngExportFactory;
@@ -19,20 +28,48 @@ PngExport::~PngExport()
 {
 }
 
+
 KoFilter::ConversionStatus
 PngExport::convert(const QCString& from, const QCString& to)
 {
-    // check for proper conversion
+    // Check for proper conversion.
     if ( from != "application/x-kchart" || to != "image/png" )
         return KoFilter::NotImplemented;
 
-	
+    // Read the contents of the KChart file
+    KoStoreDevice* storeIn = m_chain->storageFile( "root", KoStore::Read );
+    if ( !storeIn ) {
+	KMessageBox::error( 0, i18n("Failed to read data." ), 
+			    i18n( "PNG Export Error" ) );
+	return KoFilter::FileNotFound;
+    }
 
-    // Do the conversion stuff here. For notes how to get the input/output
-    // locations please refer to koffice/lib/kofficecore/koFilterChain.h
+    // Get the XML tree.
+    QDomDocument  domIn;
+    domIn.setContent( storeIn );
+    QDomElement   docNode = domIn.documentElement();
 
-    return KoFilter::NotImplemented; // Change to KoFilter::OK if the conversion
-                                     // was successfull
+    // Read the document from the XML tree.
+    KChart::KChartPart  kchartDoc;
+    if ( !kchartDoc.loadXML(0, domIn) ) {
+        KMessageBox::error( 0, i18n( "Malformed XML data." ), 
+			    i18n( "PNG Export Error" ) );
+        return KoFilter::WrongFormat;
+    }
+
+    // Draw the actual bitmap.
+    QPixmap   pixmap(500, 300);
+    QPainter  painter(&pixmap);
+    kchartDoc.paintContent(painter, pixmap.rect(), false);
+
+    // Save the image.
+    if ( !pixmap.save( m_chain->outputFile(), "PNG" ) ) {
+        KMessageBox::error( 0, i18n( "Failed to write file." ), 
+			    i18n( "PNG Export Error" ) );
+    }
+
+    return KoFilter::OK;
 }
+
 
 #include <pngexport.moc>
