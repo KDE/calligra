67003d8d1ca17563c1b163e6bc7f83af77bc04e5
David Faure <faure@kde.org>
2005-06-05 18:09:40 +0000

    Add i18n comment to distinguish from other uses of "Outline", mail by Federico Zenith on ... 20/04/2005 :(
    svn path=/trunk/koffice/; revision=422520

--- kpresenter/sidebar.cc
+++ kpresenter/sidebar.cc
@@ -125,7 +125,7 @@ SideBar::SideBar(QWidget *parent, KPresenterDoc *d, KPresenterView *v)
     setTabShape(QTabWidget::Triangular);
 
     m_outline = new Outline(this, m_doc, m_view);
-    addTab(m_outline, i18n("Outline"));
+    addTab(m_outline, i18n("Structure of the presentation", "Outline"));
 
     m_thb = new ThumbBar(this, m_doc, m_view);
     addTab(m_thb,i18n("Preview"));
