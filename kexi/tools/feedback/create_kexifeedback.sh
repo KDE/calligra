2b51563bfb0965a1cfa19f21fbcb588ec97ff676
Jarosław Staniek <staniek@kde.org>
2005-06-12 20:50:38 +0000

    SVN_SILENT fix for win32
    svn path=/trunk/koffice/; revision=424720

--- kexi/tools/feedback/create_kexifeedback.sh
+++ kexi/tools/feedback/create_kexifeedback.sh
@@ -15,8 +15,10 @@ cd kfeedbackwizard
 rm -rf .svn
 rm -rf po/.svn
 rm -rf lib/.svn
-cat configure.in.in | sed -e s/kfeedbackwizard/kexifeedbackwizard/ >configure.in.in
-cat Makefile.am | sed -e "s/SUBDIRS = po lib src/SUBDIRS = po lib/" >Makefile.am
+cat configure.in.in | sed -e s/kfeedbackwizard/kexifeedbackwizard/ >configure.in.in.tmp
+mv configure.in.in.tmp configure.in.in
+cat Makefile.am | sed -e "s/SUBDIRS = po lib src/SUBDIRS = po lib/" >Makefile.am.tmp
+mv Makefile.am.tmp Makefile.am
 rm po/kfeedbackwizard.pot
 rm -rf src
 rm -rf templates
@@ -26,9 +28,11 @@ rm INSTALL
 rm NEWS
 cd lib
 for i in `ls -1 kfeedback*`; do
- cat ${i} | sed -e s/KFeedback/KexiFeedback/g | sed -e s/KFEEDBACK/KEXIFEEDBACK/g | sed -e s/kfeedback/kexifeedback/g >$(echo ${i} | sed -e s/kfeedback/kexifeedback/)
+ cat ${i} | sed -e "s/KFeedback/KexiFeedback/g;s/KFEEDBACK/KEXIFEEDBACK/g;s/kfeedback/kexifeedback/g" \
+  >$(echo ${i} | sed -e s/kfeedback/kexifeedback/)
  rm ${i}
 done
-cat Makefile.am | sed -e s/kfeedback/kexifeedback/g >Makefile.am
+cat Makefile.am | sed -e s/kfeedback/kexifeedback/g >Makefile.am.tmp
+mv Makefile.am.tmp Makefile.am
 cd ../..
 mv kfeedbackwizard kexifeedbackwizard
