ded547721a0a2c219f89b04e43e0d686d20506a2
Bram Schoenmakers <bramschoenmakers@kde.nl>
2005-06-19 09:35:47 +0000

    Forward: Add license
    svn path=/trunk/koffice/; revision=427037

--- lib/kofficeui/tkcoloractions.cpp
+++ lib/kofficeui/tkcoloractions.cpp
@@ -1,3 +1,23 @@
+/*
+ * Kivio - Visual Modelling and Flowcharting
+ * Copyright (C) 2000 theKompany.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
 #include "tkcoloractions.h"
 #include "tktoolbarbutton.h"
 
@@ -146,8 +166,8 @@ void TKSelectColorAction::updatePixmap()
     }
     else if(w->inherits("QPopupMenu") ) {
         QPixmap pix =iconSet(KIcon::Small).pixmap(QIconSet::Automatic,QIconSet::Active);
-	if ( pix.isNull() )
-	    return;
+  if ( pix.isNull() )
+      return;
         QPainter p(&pix);
         switch (m_type) {
             case TextColor:
