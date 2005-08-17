eebf59a82ab477359639399f07630504b6f0e5eb
Adrian Page <adrian@pagenet.plus.com>
2005-08-17 22:04:49 +0000

    Enable saving in OpenEXR format.
    svn path=/trunk/koffice/; revision=450321

--- filters/krita/openexr/kis_openexr_export.cpp
+++ filters/krita/openexr/kis_openexr_export.cpp
@@ -16,6 +16,13 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+#include <qfile.h>
+
+#include <kmessagebox.h>
+
+#include <half.h>
+#include <ImfRgbaFile.h>
+
 #include <kgenericfactory.h>
 #include <koDocument.h>
 #include <koFilterChain.h>
@@ -25,8 +32,17 @@
 #include "kis_layer.h"
 #include "kis_annotation.h"
 #include "kis_types.h"
+#include "kis_colorspace_registry.h"
+#include "kis_iterators_pixel.h"
+#include "kis_abstract_colorspace.h"
+#include "kis_paint_device.h"
+#include "kis_rgb_f32_colorspace.h"
 
 #include "kis_openexr_export.h"
+
+using namespace std;
+using namespace Imf;
+using namespace Imath;
 
 typedef KGenericFactory<KisOpenEXRExport, KoFilter> KisOpenEXRExportFactory;
 K_EXPORT_COMPONENT_FACTORY(libkrita_openexr_export, KisOpenEXRExportFactory("kofficefilters"))
@@ -45,45 +61,88 @@ KoFilter::ConversionStatus KisOpenEXRExport::convert(const QCString& from, const
         return KoFilter::NotImplemented;
     }
 
-    return KoFilter::NotImplemented;
-
     kdDebug() << "Krita exporting to OpenEXR\n";
 
     // XXX: Add dialog about flattening layers here
 
-    KisDoc *output = dynamic_cast<KisDoc*>(m_chain->inputDocument());
-    QString filename = m_chain->outputFile();
+    KisDoc *output = dynamic_cast<KisDoc*>(m_chain -> inputDocument());
+    QString filename = m_chain -> outputFile();
     
-    if (!output)
+    if (!output) {
         return KoFilter::CreationError;
+    }
     
-    
-    if (filename.isEmpty()) return KoFilter::FileNotFound;
+    if (filename.isEmpty()) {
+        return KoFilter::FileNotFound;
+    }
 
-    KURL url(filename);
-
-    KisLayerSP dst;
-
-    KisImageSP img = new KisImage(*output->currentImage());
+    KisImageSP img = new KisImage(*output -> currentImage());
     Q_CHECK_PTR(img);
 
     // Don't store this information in the document's undo adapter
-    bool undo = output->undoAdapter()->undo();
-    output->undoAdapter()->setUndo(false);
+    bool undo = output -> undoAdapter() -> undo();
+    output -> undoAdapter() -> setUndo(false);
 
-    img->flatten();
+    img -> flatten();
 
-    dst = img->layer(0);
-    Q_ASSERT(dst);
+    KisLayerSP layer = img -> layer(0);
+    Q_ASSERT(layer);
     
-    output->undoAdapter()->setUndo(undo);
+    output -> undoAdapter() -> setUndo(undo);
 
-    //vKisAnnotationSP_it beginIt = img->beginAnnotations();
-    //vKisAnnotationSP_it endIt = img->endAnnotations();
-    if (1) {
-        return KoFilter::OK;
+    //KisF32RgbColorSpace * cs = static_cast<KisF32RgbColorSpace *>((KisColorSpaceRegistry::instance() -> get(KisID("RGBAF32", ""))));
+    KisF32RgbColorSpace *cs = dynamic_cast<KisF32RgbColorSpace *>(layer -> colorStrategy());
+
+    if (cs == 0) {
+        // We could convert automatically, but the conversion wants to be done with
+        // selectable profiles and rendering intent.
+        KMessageBox::information(0, i18n("The image is using an unsupported color space. "
+                                         "Please convert to 32-bit floating point RGB/Alpha "
+                                         "before saving in the OpenEXR format."));
+        return KoFilter::WrongFormat;
     }
-    return KoFilter::InternalError;
+
+    Box2i displayWindow(V2i(0, 0), V2i(img -> width() - 1, img -> height() - 1));
+
+    QRect dataExtent = layer -> exactBounds();
+    int dataWidth = dataExtent.width();
+    int dataHeight = dataExtent.height();
+
+    Box2i dataWindow(V2i(dataExtent.left(), dataExtent.top()), V2i(dataExtent.right(), dataExtent.bottom()));
+
+    RgbaOutputFile file(QFile::encodeName(filename), displayWindow, dataWindow, WRITE_RGBA);
+
+    QMemArray<Rgba> pixels(dataWidth);
+
+    for (int y = 0; y < dataHeight; ++y) {
+
+        file.setFrameBuffer(pixels.data() - dataWindow.min.x - (dataWindow.min.y + y) * dataWidth, 1, dataWidth);
+
+        KisHLineIterator it = layer -> createHLineIterator(dataWindow.min.x, dataWindow.min.y + y, dataWidth, false);
+        Rgba *rgba = pixels.data();
+
+        while (!it.isDone()) {
+
+            // XXX: Currently we use unmultiplied data so premult it.
+            float unmultipliedRed;
+            float unmultipliedGreen;
+            float unmultipliedBlue;
+            float alpha;
+
+            cs -> getPixel(it.rawData(), &unmultipliedRed, &unmultipliedGreen, &unmultipliedBlue, &alpha);
+            rgba -> r = unmultipliedRed * alpha;
+            rgba -> g = unmultipliedGreen * alpha;
+            rgba -> b = unmultipliedBlue * alpha;
+            rgba -> a = alpha;
+            ++it;
+            ++rgba;
+        }
+        file.writePixels();
+    }
+
+    //vKisAnnotationSP_it beginIt = img -> beginAnnotations();
+    //vKisAnnotationSP_it endIt = img -> endAnnotations();
+    return KoFilter::OK;
 }
 
 #include "kis_openexr_export.moc"
