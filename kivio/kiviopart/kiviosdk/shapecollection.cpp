122ec75d9ed36cde96f6849210f0a589a1e5df42
Peter Simonsson <peter.simonsson@gmail.com>
2005-08-22 11:23:53 +0000

    argh missed some more
    svn path=/trunk/koffice/; revision=451983

---
+++ kivio/kiviopart/kiviosdk/shapecollection.cpp
@@ -0,0 +1,51 @@
+/*
+ * Kivio - Visual Modelling and Flowcharting
+ * Copyright (C) 2005 Peter Simonsson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+#include "shapecollection.h"
+
+#include "object.h"
+
+namespace Kivio {
+
+ShapeCollection::ShapeCollection()
+{
+}
+
+ShapeCollection::~ShapeCollection()
+{
+  QValueList<Object*>::iterator it = m_shapeList.begin();
+  QValueList<Object*>::iterator itEnd = m_shapeList.end();
+  Object* object;
+
+  while(it != itEnd) {
+    object = *it;
+    it = m_shapeList.remove(it);
+    delete object;
+  }
+}
+
+void ShapeCollection::addShape(Object* shape)
+{
+  if(!shape) {
+    return;
+  }
+
+  m_shapeList.append(shape);
+}
+
+}
---
+++ kivio/kiviopart/kiviosdk/shapecollection.h
@@ -0,0 +1,54 @@
+/*
+ * Kivio - Visual Modelling and Flowcharting
+ * Copyright (C) 2005 Peter Simonsson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+#ifndef KIVIOSHAPECOLLECTION_H
+#define KIVIOSHAPECOLLECTION_H
+
+#include <qstring.h>
+#include <qvaluelist.h>
+
+namespace Kivio {
+
+class Object;
+
+/**
+ * Class the contains a collection of premade shapes
+ */
+class ShapeCollection
+{
+  public:
+    ShapeCollection();
+    ~ShapeCollection();
+
+    void setId(const QString& newId) { m_id = newId; }
+    QString id() const { return m_id; }
+    void setName(const QString& newName) { m_name = newName; }
+    QString name() const { return m_name; }
+
+    void addShape(Object* shape);
+
+  private:
+    QString m_id;
+    QString m_name;
+
+    QValueList<Object*> m_shapeList;
+};
+
+}
+
+#endif
