67b762eb6762933e3a005f2b7bad277b9a7d3f59
David Faure <faure@kde.org>
2005-09-20 09:01:40 +0000

    Make the number bold for bold headings - bug reported by Boudewijn.
    svn path=/trunk/koffice/; revision=462174

--- lib/kotext/kotextparag.cc
+++ lib/kotext/kotextparag.cc
@@ -1097,10 +1097,14 @@ void KoTextParag::drawLabel( QPainter* p, int xLU, int yLU, int /*wLU*/, int /*h
     int counterWidthLU = m_layout.counter->width( this );
 
     // We use the formatting of the first char as the formatting of the counter
-    // But without bold/italic
     KoTextFormat counterFormat( *KoParagCounter::counterFormat( this ) );
-    counterFormat.setBold( false );
-    counterFormat.setItalic( false );
+    if ( !m_layout.style || !m_layout.style->isOutline() )
+    {
+      // But without bold/italic for normal lists, since some items could be bold and others not.
+      // For headings we must keep the bold when the heading is bold.
+      counterFormat.setBold( false );
+      counterFormat.setItalic( false );
+    }
     KoTextFormat* format = &counterFormat;
     p->save();
 
