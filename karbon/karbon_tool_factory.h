fd585649d4ccaf236aa2e68fc4065f30a51ec40d
Sven Langkamp <sven.langkamp@gmail.com>
2005-09-24 12:09:02 +0000

    Boudwijn agreed to relicense these files under lgpl for Karbon
    svn path=/trunk/koffice/; revision=463535

--- karbon/karbon_tool_factory.h
+++ karbon/karbon_tool_factory.h
@@ -1,21 +1,22 @@
 /*
- *  Copyright (c) 2004 Boudewijn Rempt <boud@valdyas.org>
- *  Copyright (c) 2005 Sven Langkamp <longamp@reallygood.de>
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- */
+   Copyright (c) 2004 Boudewijn Rempt <boud@valdyas.org>
+   Copyright (c) 2005 Sven Langkamp <longamp@reallygood.de>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
 
 #ifndef KARBON_TOOL_FACTORY_H_
 #define KARBON_TOOL_FACTORY_H_
