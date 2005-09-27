822c7550c0e7de8f2c8f9d0f120ed28979f67782
Sebastian Sauer <mail@dipe.org>
2005-09-27 23:28:49 +0000

    s/spaces/tabs/g; SVN_SILENT
    svn path=/trunk/koffice/; revision=464627

--- kexi/core/kexiscripting.cpp
+++ kexi/core/kexiscripting.cpp
@@ -40,173 +40,173 @@
 /// \internal
 class KexiScriptContainerPrivate
 {
-    public:
+	public:
 #ifdef KEXI_KROSS_SUPPORT
-        /// The \a Kross::Api::Manager singelton.
-        Kross::Api::Manager* manager;
-        /// The wrapped \a Kross::Api::ScriptContainer instance.
-        Kross::Api::ScriptContainer::Ptr scriptcontainer;
+		/// The \a Kross::Api::Manager singelton.
+		Kross::Api::Manager* manager;
+		/// The wrapped \a Kross::Api::ScriptContainer instance.
+		Kross::Api::ScriptContainer::Ptr scriptcontainer;
 #else
-        /// The name this container has.
-        QString name;
-        /// The name of the interpreter.
-        QString interpretername;
-        /// The scripting code.
-        QString code;
+		/// The name this container has.
+		QString name;
+		/// The name of the interpreter.
+		QString interpretername;
+		/// The scripting code.
+		QString code;
 #endif
-        /// The last error.
-        QString lasterr;
-        /// Remembered stdout and stderr output.
-        QStringList output;
+		/// The last error.
+		QString lasterr;
+		/// Remembered stdout and stderr output.
+		QStringList output;
 };
 
 KexiScriptContainer::KexiScriptContainer(KexiScriptManager* manager, const QString& name)
-    : QObject(manager, "KexiScriptContainer")
-    , d(new KexiScriptContainerPrivate())
+	: QObject(manager, "KexiScriptContainer")
+	, d(new KexiScriptContainerPrivate())
 {
 #ifdef KEXI_KROSS_SUPPORT
-    d->manager = Kross::Api::Manager::scriptManager();
-    d->scriptcontainer = d->manager->getScriptContainer(name);
-    d->scriptcontainer->setInterpreterName("python"); // set default interpreter
+	d->manager = Kross::Api::Manager::scriptManager();
+	d->scriptcontainer = d->manager->getScriptContainer(name);
+	d->scriptcontainer->setInterpreterName("python"); // set default interpreter
 
-    // Redirect stdout and stderr.
-    //FIXME remember previous stdout+stderr and restore them in the dtor?
-    d->scriptcontainer->addSlot("stdout", this, SLOT(addStdOut(const QString&)));
-    d->scriptcontainer->addSlot("stderr", this, SLOT(addStdErr(const QString&)));
+	// Redirect stdout and stderr.
+	//FIXME remember previous stdout+stderr and restore them in the dtor?
+	d->scriptcontainer->addSlot("stdout", this, SLOT(addStdOut(const QString&)));
+	d->scriptcontainer->addSlot("stderr", this, SLOT(addStdErr(const QString&)));
 #else
-    d->name = name;
+	d->name = name;
 #endif
 }
 
 KexiScriptContainer::~KexiScriptContainer()
 {
 #ifdef KEXI_KROSS_SUPPORT
-    //TODO Don't redirect stdout and stderr any longer.
-    //d->scriptcontainer->removeSignal("stdout");
-    //d->scriptcontainer->removeSignal("stderr");
+	//TODO Don't redirect stdout and stderr any longer.
+	//d->scriptcontainer->removeSignal("stdout");
+	//d->scriptcontainer->removeSignal("stderr");
 #endif
-    delete d;
+	delete d;
 }
 
 QString KexiScriptContainer::getInterpreterName()
 {
 #ifdef KEXI_KROSS_SUPPORT
-    return d->scriptcontainer->getInterpreterName();
+	return d->scriptcontainer->getInterpreterName();
 #else
-    return d->interpretername;
+	return d->interpretername;
 #endif
 }
 
 void KexiScriptContainer::setInterpreterName(const QString& interpretername)
 {
 #ifdef KEXI_KROSS_SUPPORT
-    d->scriptcontainer->setInterpreterName(interpretername);
+	d->scriptcontainer->setInterpreterName(interpretername);
 #else
-    d->interpretername = interpretername;
+	d->interpretername = interpretername;
 #endif
 }
 
 QString KexiScriptContainer::getCode()
 {
 #ifdef KEXI_KROSS_SUPPORT
-    return d->scriptcontainer->getCode();
+	return d->scriptcontainer->getCode();
 #else
-    return d->code;
+	return d->code;
 #endif
 }
 
 void KexiScriptContainer::setCode(const QString& code)
 {
 #ifdef KEXI_KROSS_SUPPORT
-    d->scriptcontainer->setCode(code);
+	d->scriptcontainer->setCode(code);
 #else
-    d->code = code;
+	d->code = code;
 #endif
 }
 
 QMap<QString, KexiScriptContainer::Option*> KexiScriptContainer::getOptions()
 {
-    QMap<QString, KexiScriptContainer::Option*> options;
+	QMap<QString, KexiScriptContainer::Option*> options;
 #ifdef KEXI_KROSS_SUPPORT
-    Kross::Api::InterpreterInfo* info =
-        Kross::Api::Manager::scriptManager()->getInterpreterInfo( d->scriptcontainer->getInterpreterName() );
-    if(info) {
-        Kross::Api::InterpreterInfo::Option::Map optionmap = info->getOptions();
-        Kross::Api::InterpreterInfo::Option::Map::Iterator it( optionmap.begin() );
-        for(; it != optionmap.end(); ++it) {
-            KexiScriptContainer::Option* o = new KexiScriptContainer::Option();
-            o->name = it.data()->name;
-            o->comment = it.data()->comment;
-            o->value = it.data()->value;
-            options.replace(it.key(), o);
-        }
-    } else kdWarning() << "KexiScriptContainer::getOptions(): No such interpreterinfo" << endl;
+	Kross::Api::InterpreterInfo* info =
+		Kross::Api::Manager::scriptManager()->getInterpreterInfo( d->scriptcontainer->getInterpreterName() );
+	if(info) {
+		Kross::Api::InterpreterInfo::Option::Map optionmap = info->getOptions();
+		Kross::Api::InterpreterInfo::Option::Map::Iterator it( optionmap.begin() );
+		for(; it != optionmap.end(); ++it) {
+			KexiScriptContainer::Option* o = new KexiScriptContainer::Option();
+			o->name = it.data()->name;
+			o->comment = it.data()->comment;
+			o->value = it.data()->value;
+			options.replace(it.key(), o);
+		}
+	} else kdWarning() << "KexiScriptContainer::getOptions(): No such interpreterinfo" << endl;
 #endif
-    return options;
+	return options;
 }
 
 bool KexiScriptContainer::setOption(const QString& key, QVariant value)
 {
 #ifdef KEXI_KROSS_SUPPORT
-    Kross::Api::InterpreterInfo* info =
-        Kross::Api::Manager::scriptManager()->getInterpreterInfo( d->scriptcontainer->getInterpreterName() );
-    if(info) {
-        if(info->hasOption(key)) {
-            d->scriptcontainer->setOption(key, value);
-            return true;
-        } else kdWarning() << QString("KexiScriptContainer::setOption(%1, %2): No such option").arg(key).arg(value.toString()) << endl;
-    } else kdWarning() << QString("KexiScriptContainer::setOption(%1, %2): No such interpreterinfo").arg(key).arg(value.toString()) << endl;
+	Kross::Api::InterpreterInfo* info =
+		Kross::Api::Manager::scriptManager()->getInterpreterInfo( d->scriptcontainer->getInterpreterName() );
+	if(info) {
+		if(info->hasOption(key)) {
+			d->scriptcontainer->setOption(key, value);
+			return true;
+		} else kdWarning() << QString("KexiScriptContainer::setOption(%1, %2): No such option").arg(key).arg(value.toString()) << endl;
+	} else kdWarning() << QString("KexiScriptContainer::setOption(%1, %2): No such interpreterinfo").arg(key).arg(value.toString()) << endl;
 #endif
-    return false;
+	return false;
 }
 
 QString KexiScriptContainer::getLastError()
 {
-    return d->lasterr;
+	return d->lasterr;
 }
 
 QStringList KexiScriptContainer::getOutput()
 {
-    return d->output;
+	return d->output;
 }
 
 bool KexiScriptContainer::execute()
 {
-    d->output.clear(); // clear previous output
-    emit clearOutput();
+	d->output.clear(); // clear previous output
+	emit clearOutput();
 
 #ifdef KEXI_KROSS_SUPPORT
-    QApplication::setOverrideCursor(QCursor(Qt::WaitCursor));
-    Kross::Api::Object::Ptr result = d->scriptcontainer->execute();
-    bool ok = true;
-    if( d->scriptcontainer->hadException() ) {
-        Kross::Api::Exception::Ptr exception = d->scriptcontainer->getException();
-        d->lasterr = exception->toString();
-        addStdErr(d->lasterr);
-        long line = exception->getLineNo();
-        if(line >= 0)
-            emit lineNo(line);
-        ok = false;
-    }
-    QApplication::restoreOverrideCursor();
-    return ok;
+	QApplication::setOverrideCursor(QCursor(Qt::WaitCursor));
+	Kross::Api::Object::Ptr result = d->scriptcontainer->execute();
+	bool ok = true;
+	if( d->scriptcontainer->hadException() ) {
+		Kross::Api::Exception::Ptr exception = d->scriptcontainer->getException();
+		d->lasterr = exception->toString();
+		addStdErr(d->lasterr);
+		long line = exception->getLineNo();
+		if(line >= 0)
+			emit lineNo(line);
+		ok = false;
+	}
+	QApplication::restoreOverrideCursor();
+	return ok;
 #else
-    addStdErr( "KexiScriptManager::execute() called, but Kexi is compiled without Kross scripting support." );
-    return false;
+	addStdErr( "KexiScriptManager::execute() called, but Kexi is compiled without Kross scripting support." );
+	return false;
 #endif
 }
 
 void KexiScriptContainer::addStdOut(const QString& s)
 {
-    d->output.append(s);
-    emit addOutput(s);
+	d->output.append(s);
+	emit addOutput(s);
 }
 
 void KexiScriptContainer::addStdErr(const QString& s)
 {
-    QString t = QString("<b>%1</b>").arg(s);
-    d->output.append(t);
-    emit addOutput(t);
+	QString t = QString("<b>%1</b>").arg(s);
+	d->output.append(t);
+	emit addOutput(t);
 }
 
 /****************************************************************
@@ -216,82 +216,82 @@ void KexiScriptContainer::addStdErr(const QString& s)
 /// \internal
 class KexiScriptManagerPrivate
 {
-    public:
-        /// The \a KexiMainWindow singelton instance.
-        KexiMainWindow* mainwindow;
-        /// Map of \a KexiScriptContainer children this \a KexiScriptManager
-        QMap<QString, KexiScriptContainer*> scriptcontainers;
+	public:
+		/// The \a KexiMainWindow singelton instance.
+		KexiMainWindow* mainwindow;
+		/// Map of \a KexiScriptContainer children this \a KexiScriptManager
+		QMap<QString, KexiScriptContainer*> scriptcontainers;
 };
 
 KexiScriptManager* KexiScriptManager::self(KexiMainWindow* mainwin)
 {
-    static KexiScriptManager* manager = 0;
-    if(! manager)
-        manager = new KexiScriptManager(mainwin);
-    return manager;
+	static KexiScriptManager* manager = 0;
+	if(! manager)
+		manager = new KexiScriptManager(mainwin);
+	return manager;
 }
 
 KexiScriptManager::KexiScriptManager(KexiMainWindow* mainwindow)
-    : QObject(mainwindow, "KexiScriptManager")
-    , d(new KexiScriptManagerPrivate())
+	: QObject(mainwindow, "KexiScriptManager")
+	, d(new KexiScriptManagerPrivate())
 {
-    d->mainwindow = mainwindow;
+	d->mainwindow = mainwindow;
 
 #ifdef KEXI_KROSS_SUPPORT
-    // Publish the KexiMainWindow singelton instance. At least the KexiApp 
-    // scripting-plugin depends on this instance and loading the plugin will 
-    // fail if it's not avaiable.
-    Kross::Api::Manager::scriptManager()->addQObject(mainwindow, "KexiMainWindow");
+	// Publish the KexiMainWindow singelton instance. At least the KexiApp 
+	// scripting-plugin depends on this instance and loading the plugin will 
+	// fail if it's not avaiable.
+	Kross::Api::Manager::scriptManager()->addQObject(mainwindow, "KexiMainWindow");
 #endif
 }
 
 KexiScriptManager::~KexiScriptManager()
 {
-    delete d;
+	delete d;
 }
 
 bool KexiScriptManager::hasScriptContainer(const QString& name)
 {
-    return d->scriptcontainers.contains(name);
+	return d->scriptcontainers.contains(name);
 }
 
 KexiScriptContainer* KexiScriptManager::getScriptContainer(const QString& name, bool create)
 {
-    KexiScriptContainer* sc = d->scriptcontainers[name];
-    if((! sc) && create) {
-        sc = new KexiScriptContainer(this, name);
-        d->scriptcontainers.replace(name, sc); // remember them
-    }
-    return sc;
+	KexiScriptContainer* sc = d->scriptcontainers[name];
+	if((! sc) && create) {
+		sc = new KexiScriptContainer(this, name);
+		d->scriptcontainers.replace(name, sc); // remember them
+	}
+	return sc;
 }
 
 const QStringList KexiScriptManager::getInterpreters()
 {
 #ifdef KEXI_KROSS_SUPPORT
-    return Kross::Api::Manager::scriptManager()->getInterpreters();
+	return Kross::Api::Manager::scriptManager()->getInterpreters();
 #else
-    return QStringList();
+	return QStringList();
 #endif
 }
 
 bool KexiScriptManager::executeFile(const QString& file, QString& error)
 {
-    QFile f(file);
-    if(! f.open(IO_ReadOnly)) {
-        error = i18n("<qt>Could not load script file <nobr>\"%1\"</nobr></qt>").arg(file);
-        return false;
-    }
-    QString code = f.readAll();
-    f.close();
+	QFile f(file);
+	if(! f.open(IO_ReadOnly)) {
+		error = i18n("<qt>Could not load script file <nobr>\"%1\"</nobr></qt>").arg(file);
+		return false;
+	}
+	QString code = f.readAll();
+	f.close();
 
-    KexiScriptContainer* sc = new KexiScriptContainer(this, file);
-    sc->setInterpreterName("python");
-    sc->setCode(code);
-    bool ok = sc->execute();
-    if(! ok)
-        error = sc->getLastError();
-    delete sc;
-    return ok;
+	KexiScriptContainer* sc = new KexiScriptContainer(this, file);
+	sc->setInterpreterName("python");
+	sc->setCode(code);
+	bool ok = sc->execute();
+	if(! ok)
+		error = sc->getLastError();
+	delete sc;
+	return ok;
 }
 
 #include "kexiscripting.moc"
--- kexi/scriptingcore/python/scripts/ExportHTML.py
+++ kexi/scriptingcore/python/scripts/ExportHTML.py
@@ -1,200 +1,201 @@
-""" Export to HTML
+""" 
+Export to HTML
 
-    Description:
-    This script exports a KexiDB table or query to a HTML file.
+Description:
+This script exports a KexiDB table or query to a HTML file.
 
-    Author:
-    Sebastian Sauer <mail@dipe.org>
+Author:
+Sebastian Sauer <mail@dipe.org>
 
-    Copyright:
-    Published as-is without any warranties.
+Copyright:
+Published as-is without any warranties.
 """
 
 class Datasource:
-    def __init__(self):
-        try:
-            import krosskexiapp
-            keximainwindow = krosskexiapp.get("KexiAppMainWindow")
-        except:
-            raise "Import of the Kross KexiApp module failed."
+	def __init__(self):
+		try:
+			import krosskexiapp
+			keximainwindow = krosskexiapp.get("KexiAppMainWindow")
+		except:
+			raise "Import of the Kross KexiApp module failed."
 
-        try:
-            self.connection = keximainwindow.getConnection()
-        except:
-            raise "No connection established. Please open a project before."
+		try:
+			self.connection = keximainwindow.getConnection()
+		except:
+			raise "No connection established. Please open a project before."
 
-        self.queryschema = None
+		self.queryschema = None
 
-    def getSources(self):
-        sources = [""]
-        for table in self.connection.tableNames():
-            sources.append("Tables/%s" % table)
-        for query in self.connection.queryNames():
-            sources.append("Queries/%s" % query)
-        sources.sort()
-        return sources
+	def getSources(self):
+		sources = [""]
+		for table in self.connection.tableNames():
+			sources.append("Tables/%s" % table)
+		for query in self.connection.queryNames():
+			sources.append("Queries/%s" % query)
+		sources.sort()
+		return sources
 
-    def setSource(self, source):
-        s = source.split("/",1)
-        if s[0] == "Tables":
-            self.queryschema = self.connection.tableSchema( s[1] )
-            if self.queryschema != None: # we need a queryschema not a tableschema
-                self.queryschema = self.queryschema.query() # "select * from table"-query
-        elif s[0] == "Queries":
-            self.queryschema = self.connection.querySchema( s[1] )
-        self.cursor = None
-        return self.queryschema != None
+	def setSource(self, source):
+		s = source.split("/",1)
+		if s[0] == "Tables":
+			self.queryschema = self.connection.tableSchema( s[1] )
+			if self.queryschema != None: # we need a queryschema not a tableschema
+				self.queryschema = self.queryschema.query() # "select * from table"-query
+		elif s[0] == "Queries":
+			self.queryschema = self.connection.querySchema( s[1] )
+		self.cursor = None
+		return self.queryschema != None
 
-    def name(self):
-        return self.queryschema.name()
+	def name(self):
+		return self.queryschema.name()
 
-    def caption(self):
-        return self.queryschema.caption()
+	def caption(self):
+		return self.queryschema.caption()
 
-    def description(self):
-        return self.queryschema.description()
+	def description(self):
+		return self.queryschema.description()
 
-    def header(self):
-        h = []
-        for field in self.queryschema.fieldlist().fields():
-            h.append( field.name() ) #field.caption()
-        return h
+	def header(self):
+		h = []
+		for field in self.queryschema.fieldlist().fields():
+			h.append( field.name() ) #field.caption()
+		return h
 
-    def getNext(self):
-        if not self.cursor:
-            self.cursor = self.connection.executeQuerySchema( self.queryschema )
-            if not self.cursor:
-                raise "Failed to execute queryschema."
-            if not self.cursor.moveFirst():
-                raise "Failed to move cursor to first record."
-        if self.cursor.eof():
-            self.cursor = None
-            return None
-        items = []
-        for i in range( self.cursor.fieldCount() ):
-            items.append( self.cursor.value(i) )
-        self.cursor.moveNext()
-        return items
+	def getNext(self):
+		if not self.cursor:
+			self.cursor = self.connection.executeQuerySchema( self.queryschema )
+			if not self.cursor:
+				raise "Failed to execute queryschema."
+			if not self.cursor.moveFirst():
+				raise "Failed to move cursor to first record."
+		if self.cursor.eof():
+			self.cursor = None
+			return None
+		items = []
+		for i in range( self.cursor.fieldCount() ):
+			items.append( self.cursor.value(i) )
+		self.cursor.moveNext()
+		return items
 
 class HtmlExporter:
-    def __init__(self, datasource):
-        self.datasource = datasource
+	def __init__(self, datasource):
+		self.datasource = datasource
 
-    def write(self, output):
-        name = self.datasource.name()
-        output.write("<html><head><title>%s</title></head><body>" % name)
-        output.write("<h1>%s</h1>" % name)
+	def write(self, output):
+		name = self.datasource.name()
+		output.write("<html><head><title>%s</title></head><body>" % name)
+		output.write("<h1>%s</h1>" % name)
 
-        caption = self.datasource.caption()
-        if caption and caption != name:
-            output.write("caption: %s<br />" % caption)
+		caption = self.datasource.caption()
+		if caption and caption != name:
+			output.write("caption: %s<br />" % caption)
 
-        description = self.datasource.description()
-        if description:
-            output.write("description: %s<br />" % description)
+		description = self.datasource.description()
+		if description:
+			output.write("description: %s<br />" % description)
 
-        #import datetime
-        #output.write("date: %s<br />" % datetime.datetime.now())
+		#import datetime
+		#output.write("date: %s<br />" % datetime.datetime.now())
 
-        output.write("<table border='1'>")
+		output.write("<table border='1'>")
 
-        #output.write("<tr>")
-        #for h in self.datasource.header():
-        #    output.write("<th>%s</th>" % h)
-        #output.write("</tr>")
+		#output.write("<tr>")
+		#for h in self.datasource.header():
+		#	output.write("<th>%s</th>" % h)
+		#output.write("</tr>")
 
-        while 1 == 1:
-            items = self.datasource.getNext()
-            if items == None: break
-            output.write("<tr>")
-            for item in items:
-                output.write("<td>%s</td>" % item)
-            output.write("</tr>")
-        output.write("</table>")
-        output.write("</body></html>")
+		while 1 == 1:
+			items = self.datasource.getNext()
+			if items == None: break
+			output.write("<tr>")
+			for item in items:
+				output.write("<td>%s</td>" % item)
+			output.write("</tr>")
+		output.write("</table>")
+		output.write("</body></html>")
 
 class GuiApp:
-    def __init__(self, datasource):
-        self.datasource = datasource
+	def __init__(self, datasource):
+		self.datasource = datasource
 
-        try:
-            import Tkinter
-        except:
-            raise "Import of the python TKinter module failed."
+		try:
+			import Tkinter
+		except:
+			raise "Import of the python TKinter module failed."
 
-        self.root = Tkinter.Tk()
-        self.root.title("Export to HTML")
+		self.root = Tkinter.Tk()
+		self.root.title("Export to HTML")
 
-        frame = Tkinter.Frame(self.root)
-        frame.pack()
+		frame = Tkinter.Frame(self.root)
+		frame.pack()
 
-        queryframe = Tkinter.Frame(frame)
-        queryframe.pack()
-        Tkinter.Label(queryframe, text="Table or query to export:").pack(side=Tkinter.LEFT)
-        self.querycontent = Tkinter.StringVar()
-        self.query = apply(Tkinter.OptionMenu,
-                          (queryframe, self.querycontent) + tuple( self.datasource.getSources() ))
-        self.query.pack(side=Tkinter.LEFT)
+		queryframe = Tkinter.Frame(frame)
+		queryframe.pack()
+		Tkinter.Label(queryframe, text="Table or query to export:").pack(side=Tkinter.LEFT)
+		self.querycontent = Tkinter.StringVar()
+		self.query = apply(Tkinter.OptionMenu,
+						  (queryframe, self.querycontent) + tuple( self.datasource.getSources() ))
+		self.query.pack(side=Tkinter.LEFT)
 
-        fileframe = Tkinter.Frame(frame)
-        fileframe.pack()
-        Tkinter.Label(fileframe, text="Export to file:").pack(side=Tkinter.LEFT)
-        self.filecontent = Tkinter.StringVar()
-        file = Tkinter.Entry(fileframe, width=36, textvariable=self.filecontent)
-        self.filecontent.set(self.getHome() + "/output.html")
-        file.pack(side=Tkinter.LEFT)
-        Tkinter.Button(fileframe, text="...",
-                       command=self.doBrowse).pack(side=Tkinter.LEFT)
+		fileframe = Tkinter.Frame(frame)
+		fileframe.pack()
+		Tkinter.Label(fileframe, text="Export to file:").pack(side=Tkinter.LEFT)
+		self.filecontent = Tkinter.StringVar()
+		file = Tkinter.Entry(fileframe, width=36, textvariable=self.filecontent)
+		self.filecontent.set(self.getHome() + "/output.html")
+		file.pack(side=Tkinter.LEFT)
+		Tkinter.Button(fileframe, text="...",
+					   command=self.doBrowse).pack(side=Tkinter.LEFT)
 
-        btnframe = Tkinter.Frame(frame)
-        btnframe.pack()
-        Tkinter.Button(btnframe, text="Export",
-                       command=self.doExport).pack(side=Tkinter.LEFT)
-        Tkinter.Button(btnframe, text="Cancel",
-                       command=self.root.destroy).pack(side=Tkinter.LEFT)
+		btnframe = Tkinter.Frame(frame)
+		btnframe.pack()
+		Tkinter.Button(btnframe, text="Export",
+					   command=self.doExport).pack(side=Tkinter.LEFT)
+		Tkinter.Button(btnframe, text="Cancel",
+					   command=self.root.destroy).pack(side=Tkinter.LEFT)
 
-        self.root.mainloop()
+		self.root.mainloop()
 
-    def getHome(self):
-        import os
-        try:
-            home = os.getenv("HOME")
-            if not home:
-                import pwd
-                user = os.getenv("USER") or os.getenv("LOGNAME")
-                if not user:
-                    pwent = pwd.getpwuid(os.getuid())
-                else:
-                    pwent = pwd.getpwnam(user)
-                home = pwent[6]
-            return home
-        except (KeyError, ImportError):
-            return os.curdir
+	def getHome(self):
+		import os
+		try:
+			home = os.getenv("HOME")
+			if not home:
+				import pwd
+				user = os.getenv("USER") or os.getenv("LOGNAME")
+				if not user:
+					pwent = pwd.getpwuid(os.getuid())
+				else:
+					pwent = pwd.getpwnam(user)
+				home = pwent[6]
+			return home
+		except (KeyError, ImportError):
+			return os.curdir
 
-    def doBrowse(self):
-        import tkFileDialog
-        file = tkFileDialog.asksaveasfilename(initialdir=self.getHome(),
-            initialfile='output.html', defaultextension='.html',
-            filetypes=(('HTML files', '*.html'),('All files', '*')))
-        if file: self.filecontent.set( file )
+	def doBrowse(self):
+		import tkFileDialog
+		file = tkFileDialog.asksaveasfilename(initialdir=self.getHome(),
+			initialfile='output.html', defaultextension='.html',
+			filetypes=(('HTML files', '*.html'),('All files', '*')))
+		if file: self.filecontent.set( file )
 
-    def doExport(self):
-        query = self.querycontent.get()
-        if not self.datasource.setSource(query):
-            import tkMessageBox
-            tkMessageBox.showerror("Error", "Please select a valid table or query.")
-            return
+	def doExport(self):
+		query = self.querycontent.get()
+		if not self.datasource.setSource(query):
+			import tkMessageBox
+			tkMessageBox.showerror("Error", "Please select a valid table or query.")
+			return
 
-        file = self.filecontent.get()
-        print "Exporting '%s' to file '%s' ..." % (query,file)
+		file = self.filecontent.get()
+		print "Exporting '%s' to file '%s' ..." % (query,file)
 
-        f = open(file, "w")
-        global HtmlExporter
-        exporter = HtmlExporter(self.datasource)
-        exporter.write(f)
-        f.close()
+		f = open(file, "w")
+		global HtmlExporter
+		exporter = HtmlExporter(self.datasource)
+		exporter.write(f)
+		f.close()
 
-        print "Successfully exported '%s' to file %s" % (query,file)
-        self.root.destroy()
+		print "Successfully exported '%s' to file %s" % (query,file)
+		self.root.destroy()
 
 GuiApp( Datasource() )
