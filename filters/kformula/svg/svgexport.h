0140778812759b1aa8abe71c01a2b6bd32e72122
Fredrik Edemar <f_edemar@linux.se>
2005-09-29 22:25:40 +0000

    Add a license header
    svn path=/trunk/koffice/; revision=465485

--- filters/kformula/svg/svgexport.h
+++ filters/kformula/svg/svgexport.h
@@ -1,8 +1,22 @@
-// Please add a license header here and change the copyright
-// to your name.
-//
-// Copyright Inge Wallin
-// Copyright Fredrik Edemar
+/* This file is part of the KDE project
+   Copyright (C) 2005 Inge Wallin <inge@lysator.liu.se>
+   Copyright (C) 2005 Fredrik Edemar <f_edemar@linux.se>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA  02110-1301 USA.
+*/
 
 #ifndef __SVGEXPORT_H__
 #define __SVGEXPORT_H__
