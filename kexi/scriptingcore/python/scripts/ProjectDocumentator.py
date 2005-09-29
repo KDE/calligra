dbb70c0e4de0b5726c8a144918d65179735ac0fe
Sebastian Sauer <mail@dipe.org>
2005-09-29 22:33:57 +0000

    Added the ProjectDocumentator.py extension to generate a HTML file that contains some infos about the loaded project.
    svn path=/trunk/koffice/; revision=465488

---
+++ kexi/scriptingcore/python/scripts/ProjectDocumentator.py
@@ -0,0 +1,142 @@
+""" 
+Project Documentator
+
+Description:
+This script collects various informations about a Kexi project
+and exports them to a HTML file.
+
+Author:
+Sebastian Sauer <mail@dipe.org>
+
+Copyright:
+Published as-is without any warranties.
+"""
+
+class DataProvider:
+    def __init__(self):
+        try:
+            import krosskexiapp
+            keximainwindow = krosskexiapp.get("KexiAppMainWindow")
+        except:
+            raise "Import of the Kross KexiApp module failed."
+
+        try:
+            self.connection = keximainwindow.getConnection()
+        except:
+            raise "No connection established. Please open the project that should be documentated before."
+
+    def printConnection(self):
+        condata = self.connection.data()
+        infos = []
+        for item in ("caption", "description", "driverName", "hostName", "port", "userName", "fileName", "dbPath", "localSocketFileName", "serverInfoString"):
+            result = getattr(condata, item)()
+            if result != None and result != "" and (item != "port" or result != 0):
+                infos.append( (item, result) )
+        return infos
+
+    def printDriver(self):
+        driver = self.connection.driver()
+        result = [ ("Version", "%s.%s" % (driver.versionMajor(),driver.versionMinor())) ]
+        conlist = driver.connectionList()
+        if len(conlist) > 0:
+            result.append( ("Connections",str(conlist)) )
+        return result
+
+    def printDatabases(self):
+        result = [ ("Current database", self.connection.currentDatabase()) ]
+        dbnames = self.connection.databaseNames()
+        if len(dbnames) > 0:
+            result.append( ("Databases",str(dbnames)) )
+        return result
+
+    def printTables(self):
+        result = []
+        for t in self.connection.tableNames():
+            tableschema = self.connection.tableSchema(t)
+            ti = []
+            for i in ("name", "caption", "description"):
+                v = getattr(tableschema,i)()
+                if v != None and v != "":
+                    ti.append( (i,v) )
+            if len(ti) > 0:
+                result.append( (t, ti) )
+        return result
+
+    def printQueries(self):
+        result = []
+        for q in self.connection.queryNames():
+            queryschema = self.connection.querySchema(q)
+            qi = []
+            for i in ("name", "caption", "description"):
+                v = getattr(queryschema,i)()
+                if v != None and v != "":
+                    qi.append( (i,v) )
+            if len(qi) > 0:
+                result.append( (q, qi) )
+        return result
+
+class GuiApp:
+    def __init__(self, dataprovider):
+        self.dataprovider = dataprovider
+
+        try:
+            import gui
+        except:
+            raise "Import of the Kross GUI module failed."
+
+        self.dialog = gui.Dialog("Project Documentator")
+        self.dialog.addLabel(self.dialog.addFrame(self.dialog), "Export informations about the opened project.")
+        #self.dialog.addCheckbox(self.dialog.addFrame(self.dialog), "Projectdetails")
+        #self.dialog.addCheckbox(self.dialog.addFrame(self.dialog), "Tableschemas")
+        #self.dialog.addCheckbox(self.dialog.addFrame(self.dialog), "Queryschemas")
+        #self.dialog.addCheckbox(self.dialog.addFrame(self.dialog), "Tablestatistics")
+        #self.dialog.addCheckbox(self.dialog.addFrame(self.dialog), "Relationships")
+
+        self.file = self.dialog.addFileChooser(self.dialog,
+            "Save as:",
+            gui.getHome() + "/output.html",
+            (('HTML files', '*.html'),('All files', '*')))
+
+        btnframe = self.dialog.addFrame(self.dialog)
+        self.dialog.addButton(btnframe, "Save", self.doSave)
+        self.dialog.addButton(btnframe, "Cancel", self.dialog.quitDialog)
+
+        self.dialog.show()
+
+    def doSave(self):
+        from types import *
+
+        file = self.file.get()
+        print "Try to save project documentation to file: %s" % file
+
+        f = open(file, "w")
+
+        f.write("<html><head><title>Projectinformations</title></head><body>")
+        f.write("<h1>Projectinformations</h1>")
+
+        for d in dir(self.dataprovider):
+            if d.startswith("print"):
+                result = getattr(self.dataprovider,d)()
+                if result != None and len(result) > 0:
+                    f.write("<h2>%s</h2>" % d[5:])
+                    f.write("<ul>")
+                    for (key,value) in result:
+                        if isinstance(value,ListType):
+                            f.write("<li>%s:<br />" % key)
+                            for v in value:
+                                if isinstance(v,TupleType):
+                                    f.write("%s: %s<br />" % tuple(v))
+                                else:
+                                    f.write("%s<br />" % v)
+                            f.write("</li>")
+                        else:
+                            f.write("<li>%s: %s</li>" % (key,value))
+                    f.write("</ul>")
+
+        f.close()
+
+        print "Successfully saved project documentation to file: %s" % file
+        self.dialog.quitDialog()
+
+GuiApp( DataProvider() )
+
