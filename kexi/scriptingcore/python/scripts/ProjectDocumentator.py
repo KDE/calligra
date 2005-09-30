acc89ba57cde28f75da9516866ac7f9830f683cb
Sebastian Sauer <mail@dipe.org>
2005-09-30 13:57:23 +0000

    s/spaces/tabs/g; again SVN_SILENT
    svn path=/trunk/koffice/; revision=465687

--- kexi/scriptingcore/python/scripts/ProjectDocumentator.py
+++ kexi/scriptingcore/python/scripts/ProjectDocumentator.py
@@ -13,159 +13,159 @@ Published as-is without any warranties.
 """
 
 class DataProvider:
-    def __init__(self):
-        try:
-            import krosskexiapp
-            keximainwindow = krosskexiapp.get("KexiAppMainWindow")
-        except:
-            raise "Import of the Kross KexiApp module failed."
+	def __init__(self):
+		try:
+			import krosskexiapp
+			keximainwindow = krosskexiapp.get("KexiAppMainWindow")
+		except:
+			raise "Import of the Kross KexiApp module failed."
 
-        try:
-            self.connection = keximainwindow.getConnection()
-        except:
-            raise "No connection established. Please open the project that should be documentated before."
+		try:
+			self.connection = keximainwindow.getConnection()
+		except:
+			raise "No connection established. Please open the project that should be documentated before."
 
-    def printConnection(self):
-        condata = self.connection.data()
-        infos = []
-        for item in ("caption", "description", "driverName", "hostName", "port", "userName", "fileName", "dbPath", "localSocketFileName", "serverInfoString"):
-            result = getattr(condata, item)()
-            if result != None and result != "" and (item != "port" or result != 0):
-                infos.append( (item, result) )
-        return infos
+	def printConnection(self):
+		condata = self.connection.data()
+		infos = []
+		for item in ("caption", "description", "driverName", "hostName", "port", "userName", "fileName", "dbPath", "localSocketFileName", "serverInfoString"):
+			result = getattr(condata, item)()
+			if result != None and result != "" and (item != "port" or result != 0):
+				infos.append( (item, result) )
+		return infos
 
-    def printDriver(self):
-        driver = self.connection.driver()
-        result = [ ("Version", "%s.%s" % (driver.versionMajor(),driver.versionMinor())) ]
-        conlist = driver.connectionList()
-        if len(conlist) > 0:
-            result.append( ("Connections",str(conlist)) )
-        return result
+	def printDriver(self):
+		driver = self.connection.driver()
+		result = [ ("Version", "%s.%s" % (driver.versionMajor(),driver.versionMinor())) ]
+		conlist = driver.connectionList()
+		if len(conlist) > 0:
+			result.append( ("Connections",str(conlist)) )
+		return result
 
-    def printDatabases(self):
-        result = [ ("Current database", self.connection.currentDatabase()) ]
-        dbnames = self.connection.databaseNames()
-        if len(dbnames) > 0:
-            result.append( ("Databases",str(dbnames)) )
-        return result
+	def printDatabases(self):
+		result = [ ("Current database", self.connection.currentDatabase()) ]
+		dbnames = self.connection.databaseNames()
+		if len(dbnames) > 0:
+			result.append( ("Databases",str(dbnames)) )
+		return result
 
-    def printTables(self):
-        result = []
-        for t in self.connection.tableNames():
-            tableschema = self.connection.tableSchema(t)
-            ti = []
-            for i in ("name", "caption", "description"):
-                v = getattr(tableschema,i)()
-                if v != None and v != "":
-                    ti.append( (i,v) )
-            tf = []
-            for field in tableschema.fieldlist().fields():
-                tfi = []
-                for n in ("caption","description","type","subType","typeGroup","length","defaultValue"):
-                    v = getattr(field,n)()
-                    if v != None and v != "":
-                        tfi.append( (n,v) )
-                props = []
-                for n in ("PrimaryKey","ForeignKey","AutoInc","UniqueKey","NotNull", "NotEmpty","Indexed","Unsigned"):
-                    v = getattr(field,"is%s" % n)()
-                    if v != None and v != "" and v != False and v != 0:
-                        props.append( "%s " % n )
-                if len(props) > 0:
-                    tfi.append( ("properties",props) )
+	def printTables(self):
+		result = []
+		for t in self.connection.tableNames():
+			tableschema = self.connection.tableSchema(t)
+			ti = []
+			for i in ("name", "caption", "description"):
+				v = getattr(tableschema,i)()
+				if v != None and v != "":
+					ti.append( (i,v) )
+			tf = []
+			for field in tableschema.fieldlist().fields():
+				tfi = []
+				for n in ("caption","description","type","subType","typeGroup","length","defaultValue"):
+					v = getattr(field,n)()
+					if v != None and v != "":
+						tfi.append( (n,v) )
+				props = []
+				for n in ("PrimaryKey","ForeignKey","AutoInc","UniqueKey","NotNull", "NotEmpty","Indexed","Unsigned"):
+					v = getattr(field,"is%s" % n)()
+					if v != None and v != "" and v != False and v != 0:
+						props.append( "%s " % n )
+				if len(props) > 0:
+					tfi.append( ("properties",props) )
 
-                tf.append( (field.name(), tfi) )
-            ti.append( ("fields", tf) )
-            if len(ti) > 0:
-                result.append( (t, ti) )
-        return result
+				tf.append( (field.name(), tfi) )
+			ti.append( ("fields", tf) )
+			if len(ti) > 0:
+				result.append( (t, ti) )
+		return result
 
-    def printQueries(self):
-        result = []
-        for q in self.connection.queryNames():
-            queryschema = self.connection.querySchema(q)
-            qi = []
-            for i in ("name", "caption", "description", "statement"):
-                v = getattr(queryschema,i)()
-                if v != None and v != "":
-                    qi.append( (i,v) )
-            if len(qi) > 0:
-                result.append( (q, qi) )
-        return result
+	def printQueries(self):
+		result = []
+		for q in self.connection.queryNames():
+			queryschema = self.connection.querySchema(q)
+			qi = []
+			for i in ("name", "caption", "description", "statement"):
+				v = getattr(queryschema,i)()
+				if v != None and v != "":
+					qi.append( (i,v) )
+			if len(qi) > 0:
+				result.append( (q, qi) )
+		return result
 
 class GuiApp:
-    def __init__(self, dataprovider):
-        self.dataprovider = dataprovider
+	def __init__(self, dataprovider):
+		self.dataprovider = dataprovider
 
-        try:
-            import gui
-        except:
-            raise "Import of the Kross GUI module failed."
+		try:
+			import gui
+		except:
+			raise "Import of the Kross GUI module failed."
 
-        self.dialog = gui.Dialog("Project Documentator")
-        self.dialog.addLabel(self.dialog.addFrame(self.dialog), "Export informations about the opened project.")
-        #self.dialog.addCheckbox(self.dialog.addFrame(self.dialog), "Projectdetails")
-        #self.dialog.addCheckbox(self.dialog.addFrame(self.dialog), "Tableschemas")
-        #self.dialog.addCheckbox(self.dialog.addFrame(self.dialog), "Queryschemas")
-        #self.dialog.addCheckbox(self.dialog.addFrame(self.dialog), "Tablestatistics")
-        #self.dialog.addCheckbox(self.dialog.addFrame(self.dialog), "Relationships")
+		self.dialog = gui.Dialog("Project Documentator")
+		self.dialog.addLabel(self.dialog.addFrame(self.dialog), "Export informations about the opened project.")
+		#self.dialog.addCheckbox(self.dialog.addFrame(self.dialog), "Projectdetails")
+		#self.dialog.addCheckbox(self.dialog.addFrame(self.dialog), "Tableschemas")
+		#self.dialog.addCheckbox(self.dialog.addFrame(self.dialog), "Queryschemas")
+		#self.dialog.addCheckbox(self.dialog.addFrame(self.dialog), "Tablestatistics")
+		#self.dialog.addCheckbox(self.dialog.addFrame(self.dialog), "Relationships")
 
-        self.file = self.dialog.addFileChooser(self.dialog,
-            "Save as:",
-            gui.getHome() + "/output.html",
-            (('HTML files', '*.html'),('All files', '*')))
+		self.file = self.dialog.addFileChooser(self.dialog,
+			"Save as:",
+			gui.getHome() + "/output.html",
+			(('HTML files', '*.html'),('All files', '*')))
 
-        btnframe = self.dialog.addFrame(self.dialog)
-        self.dialog.addButton(btnframe, "Save", self.doSave)
-        self.dialog.addButton(btnframe, "Cancel", self.dialog.quitDialog)
+		btnframe = self.dialog.addFrame(self.dialog)
+		self.dialog.addButton(btnframe, "Save", self.doSave)
+		self.dialog.addButton(btnframe, "Cancel", self.dialog.quitDialog)
 
-        self.dialog.show()
+		self.dialog.show()
 
-    def toHTML(self, value):
-        import types
-        result = ""
-        if isinstance(value, types.TupleType):
-            result += "<ul>"
-            if len(value) == 1:
-                result += "<li>%s</li>" % value
-            elif len(value) == 2:
-                result += "<li>%s: %s</li>" % (value[0], self.toHTML(value[1]))
-            elif len(value) > 2:
-                for item in value:
-                    i = self.toHTML(item)
-                    if i != "":
-                        result += "<li>%s</li>" % i
-            result += "</ul>"
-        elif isinstance(value, types.ListType):
-            for item in value:
-                i = self.toHTML(item)
-                if i != "":
-                    result += "%s" % i
-        else:
-            result += "%s" % value
-        return result
+	def toHTML(self, value):
+		import types
+		result = ""
+		if isinstance(value, types.TupleType):
+			result += "<ul>"
+			if len(value) == 1:
+				result += "<li>%s</li>" % value
+			elif len(value) == 2:
+				result += "<li>%s: %s</li>" % (value[0], self.toHTML(value[1]))
+			elif len(value) > 2:
+				for item in value:
+					i = self.toHTML(item)
+					if i != "":
+						result += "<li>%s</li>" % i
+			result += "</ul>"
+		elif isinstance(value, types.ListType):
+			for item in value:
+				i = self.toHTML(item)
+				if i != "":
+					result += "%s" % i
+		else:
+			result += "%s" % value
+		return result
 
-    def doSave(self):
-        import types
+	def doSave(self):
+		import types
 
-        file = self.file.get()
-        print "Try to save project documentation to file: %s" % file
+		file = self.file.get()
+		print "Try to save project documentation to file: %s" % file
 
-        f = open(file, "w")
+		f = open(file, "w")
 
-        f.write("<html><head><title>Projectinformations</title></head><body>")
-        f.write("<h1>Projectinformations</h1>")
+		f.write("<html><head><title>Projectinformations</title></head><body>")
+		f.write("<h1>Projectinformations</h1>")
 
-        for d in dir(self.dataprovider):
-            if d.startswith("print"):
-                value = getattr(self.dataprovider,d)()
-                if value != None and len(value) > 0:
-                    f.write("<h2>%s</h2>" % d[5:])
-                    f.write( self.toHTML(value) )
+		for d in dir(self.dataprovider):
+			if d.startswith("print"):
+				value = getattr(self.dataprovider,d)()
+				if value != None and len(value) > 0:
+					f.write("<h2>%s</h2>" % d[5:])
+					f.write( self.toHTML(value) )
 
-        f.close()
+		f.close()
 
-        print "Successfully saved project documentation to file: %s" % file
-        self.dialog.quitDialog()
+		print "Successfully saved project documentation to file: %s" % file
+		self.dialog.quitDialog()
 
 GuiApp( DataProvider() )
 
--- kexi/scriptingcore/python/scripts/kross/gui.py
+++ kexi/scriptingcore/python/scripts/kross/gui.py
@@ -14,179 +14,179 @@ Published as-is without any warranties.
 """
 
 def getHome():
-    """ Return the homedirectory. """
-    import os
-    try:
-        home = os.getenv("HOME")
-        if not home:
-            import pwd
-            user = os.getenv("USER") or os.getenv("LOGNAME")
-            if not user:
-                pwent = pwd.getpwuid(os.getuid())
-            else:
-                pwent = pwd.getpwnam(user)
-            home = pwent[6]
-        return home
-    except (KeyError, ImportError):
-        return os.curdir
+	""" Return the homedirectory. """
+	import os
+	try:
+		home = os.getenv("HOME")
+		if not home:
+			import pwd
+			user = os.getenv("USER") or os.getenv("LOGNAME")
+			if not user:
+				pwent = pwd.getpwuid(os.getuid())
+			else:
+				pwent = pwd.getpwnam(user)
+			home = pwent[6]
+		return home
+	except (KeyError, ImportError):
+		return os.curdir
 
 class TkDialog:
-    """ This class is used to wrap Tkinter into a more abstract interface."""
+	""" This class is used to wrap Tkinter into a more abstract interface."""
 
-    def __init__(self, title):
-        import Tkinter
-        self.root = Tkinter.Tk()
-        self.root.title(title)
-        self.root.deiconify()
+	def __init__(self, title):
+		import Tkinter
+		self.root = Tkinter.Tk()
+		self.root.title(title)
+		self.root.deiconify()
 
-        mainframe = self.Frame(self, self.root)
-        self.widget = mainframe.widget
+		mainframe = self.Frame(self, self.root)
+		self.widget = mainframe.widget
 
-    class Widget:
-        def __init__(self, dialog, parent):
-            self.dialog = dialog
-            self.parent = parent
-        #def setVisible(self, visibled): pass
-        #def setEnabled(self, enabled): pass
+	class Widget:
+		def __init__(self, dialog, parent):
+			self.dialog = dialog
+			self.parent = parent
+		#def setVisible(self, visibled): pass
+		#def setEnabled(self, enabled): pass
 
-    class Frame(Widget):
-        def __init__(self, dialog, parent):
-            #TkDialog.Widget.__init__(self, dialog, parent)
-            import Tkinter
-            self.widget = Tkinter.Frame(parent)
-            self.widget.pack()
+	class Frame(Widget):
+		def __init__(self, dialog, parent):
+			#TkDialog.Widget.__init__(self, dialog, parent)
+			import Tkinter
+			self.widget = Tkinter.Frame(parent)
+			self.widget.pack()
 
-    class Label(Widget):
-        def __init__(self, dialog, parent, caption):
-            #TkDialog.Widget.__init__(self, dialog, parent)
-            import Tkinter
-            self.widget = Tkinter.Label(parent, text=caption)
-            self.widget.pack(side=Tkinter.LEFT)
+	class Label(Widget):
+		def __init__(self, dialog, parent, caption):
+			#TkDialog.Widget.__init__(self, dialog, parent)
+			import Tkinter
+			self.widget = Tkinter.Label(parent, text=caption)
+			self.widget.pack(side=Tkinter.LEFT)
 
-    class Checkbox(Widget):
-        def __init__(self, dialog, parent, caption):
-            #TkDialog.Widget.__init__(self, dialog, parent)
-            import Tkinter
-            self.widget = Tkinter.Checkbutton(parent, text=caption)
-            self.widget.pack(side=Tkinter.LEFT)
+	class Checkbox(Widget):
+		def __init__(self, dialog, parent, caption):
+			#TkDialog.Widget.__init__(self, dialog, parent)
+			import Tkinter
+			self.widget = Tkinter.Checkbutton(parent, text=caption)
+			self.widget.pack(side=Tkinter.LEFT)
 
-    class List(Widget):
-        def __init__(self, dialog, parent, caption, items):
-            #TkDialog.Widget.__init__(self, dialog, parent)
-            import Tkinter
+	class List(Widget):
+		def __init__(self, dialog, parent, caption, items):
+			#TkDialog.Widget.__init__(self, dialog, parent)
+			import Tkinter
 
-            listframe = Tkinter.Frame(parent)
-            listframe.pack()
+			listframe = Tkinter.Frame(parent)
+			listframe.pack()
 
-            Tkinter.Label(listframe, text=caption).pack(side=Tkinter.LEFT)
+			Tkinter.Label(listframe, text=caption).pack(side=Tkinter.LEFT)
 
-            self.items = Tkinter.StringVar()
-            itemlist = apply(Tkinter.OptionMenu, (listframe, self.items) + tuple( items ))
-            itemlist.pack(side=Tkinter.LEFT)
+			self.items = Tkinter.StringVar()
+			itemlist = apply(Tkinter.OptionMenu, (listframe, self.items) + tuple( items ))
+			itemlist.pack(side=Tkinter.LEFT)
 
-    class Button(Widget):
-        def __init__(self, dialog, parent, caption, commandmethod):
-            #TkDialog.Widget.__init__(self, dialog, parent)
-            import Tkinter
-            self.widget = Tkinter.Button(parent, text=caption, command=self.doCommand)
-            self.commandmethod = commandmethod
-            self.widget.pack(side=Tkinter.LEFT)
-        def doCommand(self):
-            try:
-                self.commandmethod()
-            except:
-                #TODO why the heck we arn't able to redirect exceptions?
-                import traceback
-                import StringIO
-                fp = StringIO.StringIO()
-                traceback.print_exc(file=fp)
-                import tkMessageBox
-                tkMessageBox.showerror("Exception", fp.getvalue())
-                #self.dialog.root.destroy()
+	class Button(Widget):
+		def __init__(self, dialog, parent, caption, commandmethod):
+			#TkDialog.Widget.__init__(self, dialog, parent)
+			import Tkinter
+			self.widget = Tkinter.Button(parent, text=caption, command=self.doCommand)
+			self.commandmethod = commandmethod
+			self.widget.pack(side=Tkinter.LEFT)
+		def doCommand(self):
+			try:
+				self.commandmethod()
+			except:
+				#TODO why the heck we arn't able to redirect exceptions?
+				import traceback
+				import StringIO
+				fp = StringIO.StringIO()
+				traceback.print_exc(file=fp)
+				import tkMessageBox
+				tkMessageBox.showerror("Exception", fp.getvalue())
+				#self.dialog.root.destroy()
 
-    class Entry(Widget):
-        def __init__(self, dialog, parent, caption):
-            #TkDialog.Widget.__init__(self, dialog, parent)
-            import Tkinter
+	class Entry(Widget):
+		def __init__(self, dialog, parent, caption):
+			#TkDialog.Widget.__init__(self, dialog, parent)
+			import Tkinter
 
-            self.widget = Tkinter.Frame(parent)
-            self.widget.pack()
+			self.widget = Tkinter.Frame(parent)
+			self.widget.pack()
 
-            label = Tkinter.Label(self.widget, text=caption)
-            label.pack(side=Tkinter.LEFT)
+			label = Tkinter.Label(self.widget, text=caption)
+			label.pack(side=Tkinter.LEFT)
 
-            self.entrytext = Tkinter.StringVar()
-            self.entry = Tkinter.Entry(self.widget, width=36, textvariable=self.entrytext)
-            self.entry.pack(side=Tkinter.LEFT)
-        def get(self):
-            return self.entrytext.get()
+			self.entrytext = Tkinter.StringVar()
+			self.entry = Tkinter.Entry(self.widget, width=36, textvariable=self.entrytext)
+			self.entry.pack(side=Tkinter.LEFT)
+		def get(self):
+			return self.entrytext.get()
 
-    class FileChooser(Entry):
-        def __init__(self, dialog, parent, caption, initialfile = None, filetypes = None):
-            TkDialog.Entry.__init__(self, dialog, parent, caption)
-            import Tkinter
+	class FileChooser(Entry):
+		def __init__(self, dialog, parent, caption, initialfile = None, filetypes = None):
+			TkDialog.Entry.__init__(self, dialog, parent, caption)
+			import Tkinter
 
-            self.initialfile = initialfile
-            self.entrytext.set(initialfile)
+			self.initialfile = initialfile
+			self.entrytext.set(initialfile)
 
-            btn = Tkinter.Button(self.widget, text="...", command=self.browse)
-            btn.pack(side=Tkinter.LEFT)
+			btn = Tkinter.Button(self.widget, text="...", command=self.browse)
+			btn.pack(side=Tkinter.LEFT)
 
-            if filetypes:
-                self.filetypes = filetypes
-            else:
-                self.filetypes = (('All files', '*'),)
+			if filetypes:
+				self.filetypes = filetypes
+			else:
+				self.filetypes = (('All files', '*'),)
 
-        def browse(self):
-            import os
-            text = self.entrytext.get()
-            d = os.path.dirname(text) or os.path.dirname(self.initialfile)
-            f = os.path.basename(text) or os.path.basename(self.initialfile)
+		def browse(self):
+			import os
+			text = self.entrytext.get()
+			d = os.path.dirname(text) or os.path.dirname(self.initialfile)
+			f = os.path.basename(text) or os.path.basename(self.initialfile)
 
-            import tkFileDialog
-            file = tkFileDialog.asksaveasfilename(
-                       initialdir=d,
-                       initialfile=f,
-                       #defaultextension='.html',
-                       filetypes=self.filetypes
-            )
-            if file:
-                self.entrytext.set( file )
+			import tkFileDialog
+			file = tkFileDialog.asksaveasfilename(
+					   initialdir=d,
+					   initialfile=f,
+					   #defaultextension='.html',
+					   filetypes=self.filetypes
+			)
+			if file:
+				self.entrytext.set( file )
 
-    def show(self):
-        self.root.mainloop()
+	def show(self):
+		self.root.mainloop()
 
 class Dialog:
-    """ Central class that provides abstract GUI-access to the outer world. """
+	""" Central class that provides abstract GUI-access to the outer world. """
 
-    def __init__(self, title):
-        self.dialog = TkDialog(title)
-        self.widget = self.dialog.widget
+	def __init__(self, title):
+		self.dialog = TkDialog(title)
+		self.widget = self.dialog.widget
 
-    def show(self):
-        self.dialog.show()
+	def show(self):
+		self.dialog.show()
 
-    def quitDialog(self):
-        self.dialog.root.destroy()
+	def quitDialog(self):
+		self.dialog.root.destroy()
 
-    def addFrame(self, parentwidget):
-        return self.dialog.Frame(self.dialog, parentwidget.widget)
+	def addFrame(self, parentwidget):
+		return self.dialog.Frame(self.dialog, parentwidget.widget)
 
-    def addLabel(self, parentwidget, caption):
-        return self.dialog.Label(self.dialog, parentwidget.widget, caption)
+	def addLabel(self, parentwidget, caption):
+		return self.dialog.Label(self.dialog, parentwidget.widget, caption)
 
-    def addCheckbox(self, parentwidget, caption):
-        return self.dialog.Checkbox(self.dialog, parentwidget.widget, caption)
+	def addCheckbox(self, parentwidget, caption):
+		return self.dialog.Checkbox(self.dialog, parentwidget.widget, caption)
 
-    def addButton(self, parentwidget, caption, commandmethod):
-        return self.dialog.Button(self.dialog, parentwidget.widget, caption, commandmethod)
+	def addButton(self, parentwidget, caption, commandmethod):
+		return self.dialog.Button(self.dialog, parentwidget.widget, caption, commandmethod)
 
-    def addEntry(self, parentwidget, caption):
-        return self.dialog.Entry(self.dialog, parentwidget.widget, caption)
+	def addEntry(self, parentwidget, caption):
+		return self.dialog.Entry(self.dialog, parentwidget.widget, caption)
 
-    def addFileChooser(self, parentwidget, caption, initialfile = None, filetypes = None):
-        return self.dialog.FileChooser(self.dialog, parentwidget.widget, caption, initialfile, filetypes)
+	def addFileChooser(self, parentwidget, caption, initialfile = None, filetypes = None):
+		return self.dialog.FileChooser(self.dialog, parentwidget.widget, caption, initialfile, filetypes)
 
-    def addList(self, parentwidget, caption, items):
-        return self.dialog.List(self.dialog, parentwidget.widget, caption, items)
+	def addList(self, parentwidget, caption, items):
+		return self.dialog.List(self.dialog, parentwidget.widget, caption, items)
 
