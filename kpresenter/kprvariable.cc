38db1f0ddaf63891d146d2c9fea7b8c3f1144259
Laurent Montel <montel@kde.org>
2005-10-15 20:06:38 +0000

    Backport: fix crash when we load statistic variable.
    Koffice-1.4.3 ? :)
    
    svn path=/branches/koffice/1.4/koffice/; revision=470961

--- kpresenter/kprvariable.cc
+++ kpresenter/kprvariable.cc
@@ -129,6 +129,8 @@ void KPrStatisticVariable::recalc()
                        m_subtype == VST_STATISTIC_NB_LINES ||
                        m_subtype == VST_STATISTIC_NB_CHARACTERE);
     KPrPage *page = m_doc->activePage();
+    if( !page) //when we load file active page is null
+	    return;
     QPtrListIterator<KPObject> objIt( page->objectList() );
 
     for ( objIt.toFirst(); objIt.current(); ++objIt )
