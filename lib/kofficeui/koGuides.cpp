cd11789f7b722d55907e562d6d30b6e2db07f902
Thorsten Zachmann <t.zachmann@zagge.de>
2005-10-17 06:21:24 +0000

    o Added a new class to handle guide lines.
    svn path=/trunk/koffice/; revision=471292

---
+++ lib/kofficeui/koGuides.cpp
@@ -0,0 +1,500 @@
+// -*- Mode: c++; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 4; -*-
+/* This file is part of the KDE project
+   Copyright (C) 2005 Thorsten Zachmann <zachmann@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "koGuides.h"
+
+#include <qpainter.h>
+#include <qpixmap.h>
+
+#include <koDocument.h>
+#include <koPoint.h>
+#include <koView.h>
+#include <kozoomhandler.h>
+
+KoGuides::KoGuides( KoView *view, KoZoomHandler *zoomHandler )
+: m_view( view )
+, m_zoomHandler( zoomHandler )    
+{
+}
+
+
+KoGuides::~KoGuides()
+{
+}
+
+
+void KoGuides::paintGuides( QPainter &painter )
+{
+    //painter.setRasterOp( NotROP );
+    painter.setPen( QPen( blue, 0, DotLine ) );
+    const KoPageLayout& pl = m_view->koDocument()->pageLayout();
+    int width = QMAX( m_view->canvas()->width(), m_zoomHandler->zoomItX( pl.ptWidth ) );
+    int height = QMAX( m_view->canvas()->height(), m_zoomHandler->zoomItY( pl.ptHeight ) );
+
+    QValueList<KoGuideLine *>::iterator it = m_guideLines.begin();
+    for ( ; it != m_guideLines.end(); ++it )
+    {
+        painter.save();
+        if ( ( *it )->orientation == Qt::Vertical )
+        {
+            painter.translate( m_zoomHandler->zoomItX( ( *it )->position ), 0 );
+            painter.drawLine( 0, 0, 0, height );
+        }
+        else
+        {
+            painter.translate( 0, m_zoomHandler->zoomItY( ( *it )->position ) );
+            painter.drawLine( 0, 0, width, 0 );
+        }
+        painter.restore();
+    }
+
+    painter.setPen( QPen( red, 0, DotLine ) );
+    it = m_selectedGuideLines.begin();
+    for ( ; it != m_selectedGuideLines.end(); ++it )
+    {
+        painter.save();
+        if ( ( *it )->orientation == Qt::Vertical )
+        {
+            painter.translate( m_zoomHandler->zoomItX( ( *it )->position ), 0 );
+            painter.drawLine( 0, 0, 0, height );
+        }
+        else
+        {
+            painter.translate( 0, m_zoomHandler->zoomItY( ( *it )->position ) );
+            painter.drawLine( 0, 0, width, 0 );
+        }
+        painter.restore();
+    }
+}
+
+bool KoGuides::mousePressEvent( QMouseEvent *e )
+{
+    bool eventProcessed = true;
+    bool changed = false;
+    m_mouseSelected = false;
+    
+    KoPoint p( mapFromScreen( e->pos() ) );
+    KoGuideLine * guideLine = find( p, m_zoomHandler->unzoomItY( 2 ) );
+    if ( guideLine )
+    {
+        m_lastPoint = e->pos();
+        if ( e->button() == Qt::LeftButton )
+        {
+            m_mouseSelected = true;
+            if ( e->state() & Qt::ControlButton )
+            {
+                if ( guideLine->selected )
+                {
+                    unselect( guideLine );
+                    m_mouseSelected = false;
+                }
+                else
+                {
+                    select( guideLine );
+                }
+                changed = true;
+            }
+            else if ( ! guideLine->selected )
+            {
+                unselectAll();
+                select( guideLine );
+                changed = true;
+            }
+        }
+    }
+    else 
+    {   
+        if ( !( e->state() && Qt::ControlButton ) )
+        {
+            changed = unselectAll();
+        }
+        eventProcessed = false;
+    }
+    
+    if ( changed || hasSelected() )
+    {
+        emit moveGuides( true );
+    }
+
+    if ( changed )
+    {
+        paint();
+    }
+    
+    return eventProcessed;
+}
+
+
+bool KoGuides::mouseMoveEvent( QMouseEvent *e )
+{
+    //kdDebug(33001) << "KoGuideLines::mouseMoveEvent" << endl;
+    bool eventProcessed = false;
+    if ( m_mouseSelected )
+    {
+        QPoint p( e->pos() );
+        p -= m_lastPoint;
+        m_lastPoint = e->pos();
+        moveSelectedBy( p );
+        paint();
+        emit guideLinesChanged( m_view );
+        eventProcessed = true;
+    }
+    else if ( e->button() == Qt::NoButton )
+    {
+        KoPoint p( mapFromScreen( e->pos() ) );
+        KoGuideLine * guideLine = find( p, m_zoomHandler->unzoomItY( 2 ) );
+        if ( guideLine )
+        {
+            m_view->canvas()->setCursor( guideLine->orientation == Qt::Vertical ? Qt::sizeHorCursor : Qt::sizeVerCursor );
+            eventProcessed = true;
+        }
+    }
+    return eventProcessed;
+}
+
+
+bool KoGuides::mouseReleaseEvent( QMouseEvent *e )
+{
+    bool eventProcessed = false;
+    if ( m_mouseSelected )
+    {
+        KoPoint p( mapFromScreen( e->pos() ) );
+        if ( m_selectedGuideLines.count() == 1 )
+        {
+            int x1, y1, x2, y2;
+            m_view->canvas()->rect().coords( &x1, &y1, &x2, &y2 );
+            QPoint gp( m_view->canvas()->mapFromGlobal( e->globalPos() ) );
+            if ( m_selectedGuideLines.first()->orientation == Qt::Vertical )
+            {
+                if ( gp.x() < x1 || gp.x() > x2 )
+                    removeSelected();
+            }
+            else
+            {
+                if ( gp.y() < y1 || gp.y() > y2 )
+                    removeSelected();
+            }
+        }
+        KoGuideLine * guideLine = find( p, m_zoomHandler->unzoomItY( 2 ) );
+        if ( guideLine )
+        {
+            m_view->canvas()->setCursor( guideLine->orientation == Qt::Vertical ? Qt::sizeHorCursor : Qt::sizeVerCursor );
+        }
+        m_mouseSelected = false;
+        eventProcessed = true;
+        emit guideLinesChanged( m_view );
+    }
+        emit moveGuides( false );
+    return eventProcessed;
+}
+
+
+bool KoGuides::keyPressEvent( QKeyEvent *e )
+{
+    //kdDebug( 33001 ) << "KoGuideLines::keyPressEvent" << endl;
+    bool eventProcessed = false;
+    switch( e->key() )
+    {
+        case Qt::Key_Delete: 
+            if ( hasSelected() )
+            {
+                removeSelected();
+                paint();
+                emit guideLinesChanged( m_view );
+                eventProcessed = true;
+            }
+            break;
+        default:
+            break;
+    }
+    return eventProcessed;
+}
+
+
+void KoGuides::setGuideLines( const QValueList<double> &horizontalPos, const QValueList<double> &verticalPos )
+{
+    removeSelected();
+
+    QValueList<KoGuideLine *>::iterator it = m_guideLines.begin();
+    for ( ; it != m_guideLines.end(); ++it )
+    {
+        delete ( *it );
+    }
+    m_guideLines.clear();
+
+    QValueList<double>::ConstIterator posIt = horizontalPos.begin();
+    for ( ; posIt != horizontalPos.end(); ++posIt )
+    {
+        KoGuideLine *guideLine = new KoGuideLine( Qt::Horizontal, *posIt );
+        m_guideLines.append( guideLine );
+    }
+    posIt = verticalPos.begin();
+    for ( ; posIt != verticalPos.end(); ++posIt )
+    {
+        KoGuideLine *guideLine = new KoGuideLine( Qt::Vertical, *posIt );
+        m_guideLines.append( guideLine );
+    }
+    paint();
+}
+
+
+void KoGuides::getGuideLines( QValueList<double> &horizontalPos, QValueList<double> &verticalPos ) const
+{
+    horizontalPos.clear();
+    verticalPos.clear();
+
+    QValueList<KoGuideLine *>::const_iterator it = m_guideLines.begin();
+    for ( ; it != m_guideLines.end(); ++it )
+    {
+        if ( ( *it )->orientation == Qt::Horizontal )
+        {
+            horizontalPos.append( ( *it )->position );
+        }
+        else
+        {
+            verticalPos.append( ( *it )->position );
+        }
+    }
+    it = m_selectedGuideLines.begin();
+    for ( ; it != m_selectedGuideLines.end(); ++it )
+    {
+        if ( ( *it )->orientation == Qt::Horizontal )
+        {
+            horizontalPos.append( ( *it )->position );
+        }
+        else
+        {
+            verticalPos.append( ( *it )->position );
+        }
+    }
+}
+
+
+void KoGuides::moveGuide( const QPoint &pos, bool horizontal, int rulerWidth )
+{
+    int x = pos.x() - rulerWidth;
+    int y = pos.y() - rulerWidth;
+    QPoint p( x, y );
+    if ( !m_insertGuide )
+    {
+        if ( ! horizontal && x > 0 )
+        {
+            m_insertGuide = true;
+            add( Qt::Vertical, p );
+        }
+        else if ( horizontal && y > 0 )
+        {
+            m_insertGuide = true;
+            add( Qt::Horizontal, p );
+        }
+        if ( m_insertGuide )
+        {
+            QMouseEvent e( QEvent::MouseButtonPress, p, Qt::LeftButton, Qt::LeftButton );
+            mousePressEvent( &e );
+        }
+    }
+    else
+    {
+        QMouseEvent e( QEvent::MouseMove, p, Qt::NoButton, Qt::LeftButton );
+        mouseMoveEvent( &e );
+    }
+}
+
+
+void KoGuides::addGuide( const QPoint &pos, bool /* horizontal */, int rulerWidth )
+{
+    int x = pos.x() - rulerWidth;
+    int y = pos.y() - rulerWidth;
+    QPoint p( x, y );
+    m_insertGuide = false;
+    QMouseEvent e( QEvent::MouseButtonRelease, p, Qt::LeftButton, Qt::LeftButton );
+    mouseReleaseEvent( &e );
+}
+
+
+void KoGuides::paint()
+{
+    m_view->canvas()->repaint( false );
+}
+
+
+void KoGuides::add( Qt::Orientation o, QPoint &pos )
+{
+    KoPoint p( mapFromScreen( pos ) );
+    KoGuideLine *guideLine = new KoGuideLine( o, o == Qt::Vertical ? p.x(): p.y() );
+    m_guideLines.append( guideLine );
+}
+
+
+void KoGuides::select( KoGuideLine *guideLine )
+{
+    guideLine->selected = true;
+    if ( m_guideLines.remove( guideLine ) == 1 )
+    {
+        m_selectedGuideLines.append( guideLine );
+    }
+}
+
+
+void KoGuides::unselect( KoGuideLine *guideLine )
+{
+    guideLine->selected = false;
+    if ( m_selectedGuideLines.remove( guideLine ) == 1 )
+    {
+        m_guideLines.append( guideLine );
+    }
+}
+
+
+bool KoGuides::unselectAll()
+{
+    bool selected = m_selectedGuideLines.empty() == false;
+    
+    QValueList<KoGuideLine *>::iterator it = m_selectedGuideLines.begin();
+    for ( ; it != m_selectedGuideLines.end(); ++it )
+    {
+        ( *it )->selected = false;
+        m_guideLines.append( *it );
+    }
+    m_selectedGuideLines.clear();
+    
+    return selected;
+}
+
+
+void KoGuides::removeSelected()
+{
+    QValueList<KoGuideLine *>::iterator it = m_selectedGuideLines.begin();
+    for ( ; it != m_selectedGuideLines.end(); ++it )
+    {
+        delete ( *it );
+    }
+    m_selectedGuideLines.clear();
+}
+
+
+bool KoGuides::hasSelected()
+{
+    return m_selectedGuideLines.empty() == false;
+}
+
+
+KoGuides::KoGuideLine * KoGuides::find( KoPoint &p, double diff )
+{
+    QValueList<KoGuideLine *>::iterator it = m_selectedGuideLines.begin();
+    for ( ; it != m_selectedGuideLines.end(); ++it )
+    {
+        if ( ( *it )->orientation == Qt::Vertical && QABS( ( *it )->position - p.x() ) < diff )
+        {
+            return *it;
+        }
+        if ( ( *it )->orientation == Qt::Horizontal && QABS( ( *it )->position - p.y() ) < diff )
+        {
+            return *it;
+        }
+    }
+
+    it = m_guideLines.begin();
+    for ( ; it != m_guideLines.end(); ++it )
+    {
+        if ( ( *it )->orientation == Qt::Vertical && QABS( ( *it )->position - p.x() ) < diff )
+        {
+            return *it;
+        }
+        if ( ( *it )->orientation == Qt::Horizontal && QABS( ( *it )->position - p.y() ) < diff )
+        {
+            return *it;
+        }
+    }
+    return 0;
+}
+
+
+void KoGuides::moveSelectedBy( QPoint &p )
+{
+    KoPoint point( m_zoomHandler->unzoomPoint( p ) );
+    if ( m_selectedGuideLines.count() > 1 )
+    {
+        const KoPageLayout& pl = m_view->koDocument()->pageLayout();
+        double right = QMAX( pl.ptWidth, m_zoomHandler->unzoomItX( m_view->canvas()->width() + m_view->canvasXOffset() - 1 ) );
+        double bottom = QMAX( pl.ptHeight, m_zoomHandler->unzoomItY( m_view->canvas()->height() + m_view->canvasYOffset() - 1 ) );
+
+        QValueList<KoGuideLine *>::iterator it = m_selectedGuideLines.begin();
+        for ( ; it != m_selectedGuideLines.end(); ++it )
+        {
+            if ( ( *it )->orientation == Qt::Vertical )
+            {
+                double tmp = ( *it )->position + point.x();
+                if ( tmp < 0 )
+                {
+                    point.setX( point.x() - tmp );
+                }
+                else if ( tmp > right )
+                {
+                    point.setX( point.x() - ( tmp - right ) );
+                }
+            }
+            else
+            {
+                double tmp = ( *it )->position + point.y();
+                if ( tmp < 0 )
+                {
+                    point.setY( point.y() - tmp );
+                }
+                else if ( tmp > bottom )
+                {
+                    point.setY( point.y() - ( tmp - bottom ) );
+                }
+            }
+        }
+    }
+    QValueList<KoGuideLine *>::iterator it = m_selectedGuideLines.begin();
+    for ( ; it != m_selectedGuideLines.end(); ++it )
+    {
+        if ( ( *it )->orientation == Qt::Vertical && p.x() != 0 )
+        {
+            ( *it )->position = ( *it )->position + point.x();
+        }
+        else if ( ( *it )->orientation == Qt::Horizontal && p.y() != 0 )
+        {
+            ( *it )->position = ( *it )->position + point.y();
+        }
+    }
+}
+
+
+KoPoint KoGuides::mapFromScreen( const QPoint & pos )
+{
+    int x = pos.x() + m_view->canvasXOffset();
+    int y = pos.y() + m_view->canvasYOffset();
+    double xf = m_zoomHandler->unzoomItX( x );
+    double yf = m_zoomHandler->unzoomItY( y );
+    return KoPoint( xf, yf );
+}
+
+
+QPoint KoGuides::mapToScreen( const KoPoint & pos )
+{
+    int x = m_zoomHandler->zoomItX( pos.x() ) - m_view->canvasXOffset();
+    int y = m_zoomHandler->zoomItY( pos.y() ) - m_view->canvasYOffset();
+    return QPoint( x, y );
+}
+
+#include "koGuides.moc"
---
+++ lib/kofficeui/koGuides.h
@@ -0,0 +1,295 @@
+// -*- Mode: c++; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 4; -*-
+/* This file is part of the KDE project
+   Copyright (C) 2005 Thorsten Zachmann <zachmann@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KOGUIDES_H
+#define KOGUIDES_H
+
+#include <qevent.h>
+#include <qobject.h>
+
+#include <koffice_export.h>
+
+class QPaintDevice;
+class KoPoint;
+class KoView;
+class KoZoomHandler;
+
+class KOFFICEUI_EXPORT KoGuides : public QObject
+{
+    Q_OBJECT
+public:        
+    /**
+     * @brief Constructor
+     *
+     * @param view The view in which the guides will be shown
+     * @param zoomHandler The zoom handler of the view
+     */
+    KoGuides( KoView *view, KoZoomHandler *zoomHandler );
+
+    /**
+     * @brief Destructor
+     */
+    ~KoGuides();
+        
+    /**
+     * @brief Paint the guides
+     *
+     * @param painter with which the guides are painted
+     */
+    void paintGuides( QPainter &painter );
+
+    /**
+     * @brief Handle mousePressEvent
+     *
+     * This checks if a mousePressEvent would affect a guide line.
+     * If the mouse is pressed over a guide line it gets selected.
+     * Guide lines which were select get unselect.
+     * If also the Ctrl Key is pressed the selection of the guide 
+     * gets toggled.
+     * If no guide is under the position all guides get deselected.
+     *
+     * @param e QMouseEvent
+     *
+     * @return true if the event was handled
+     * @return false otherwise
+     *         The event counts a not handled when only guides where
+     *         deselected.
+     */
+    bool mousePressEvent( QMouseEvent *e );
+
+    /**
+     * @brief Handle mouseMoveEvent
+     *
+     * The mouse button is pressed and a guide was selected it moves the 
+     * selected guides.
+     * If the mouse is moved over a guide line the cursor gets updated.
+     *
+     * @param e QMouseEvent
+     *
+     * @return true if the event was handled (guide moved, cursor changed as
+     *         guide lies below)    
+     * @return false otherwise
+     */
+    bool mouseMoveEvent( QMouseEvent *e );
+    
+    /**
+     *
+     * @param e QMouseEvent
+     *
+     * @return true if the event was handled
+     * @return false otherwise
+     */
+    bool mouseReleaseEvent( QMouseEvent *e );
+    
+    /**
+     *
+     * @param e QKeyEvent
+     *
+     * @return true if the event was handled
+     * @return false otherwise
+     */
+    bool keyPressEvent( QKeyEvent *e );
+
+    /**
+     * @brief Set the guide lines.
+     *
+     * This removes all existing guides and set up ne ones at the positions given.
+     *
+     * @param horizontalPos A list of the position of the horizontal guide lines.
+     * @param verticalPos A list of the position of the vertical guide lines.
+     */
+    void setGuideLines( const QValueList<double> &horizontalPos, const QValueList<double> &verticalPos );
+
+    /**
+     * @brief Get the position of the guide lines
+     *
+     * This filles the passed lists with the positions of the guide lines. 
+     * The lists will be emptied before any positions are added.
+     *
+     * @param horizontalPos A list of the position of the horizontal guide lines.
+     * @param verticalPos A list of the position of the vertical guide lines.
+     */
+    void getGuideLines( QValueList<double> &horizontalPos, QValueList<double> &verticalPos ) const;
+
+public slots:
+    /**
+     * @brief Move Guide
+     *
+     * This slot can be connected to void KoRuler::moveGuide( const QPoint &, bool, int );
+     * It will add a new guide when you move from the ruler to the canvas. After that it
+     * moves the guide.
+     *
+     * @param pos The pos of the mouse 
+     * @param horizontal true if the guide is horizontal, false if vertical
+     * @param rulerWidth The witdth of the ruler as the pos is seen from the ruler widget.
+     */
+    void moveGuide( const QPoint &pos, bool horizontal, int rulerWidth );
+
+    /**
+     * @brief Add Guide
+     *
+     * This slot can be connected to void KoRuler::addGuide( const QPoint &, bool, int );
+     * It will finish the inserting of a guide from moveGuide().
+     *
+     * @param pos The pos of the mouse 
+     * @param horizontal true if the guide is horizontal, false if vertical
+     * @param rulerWidth The witdth of the ruler as the pos is seen from the ruler widget.
+     */
+    void addGuide( const QPoint &pos, bool horizontal, int rulerWidth );
+
+signals:
+    /**
+     * @brief Signal that shows that the guide lines are changed
+     *
+     * This signal is emmited when the guide lines are changed ( moved / deleted )
+     *
+     * @param view The view in which the guide lines are changed.
+     */
+    void guideLinesChanged( KoView * view );
+
+    /**
+     * @brief This signal is emitted when guides start/stop moving.
+     *
+     * @param state true when starting moving guides, false when stopping.
+     */
+    void moveGuides( bool state );
+
+private:
+    /// Strukt holding the data of a guide line
+    struct KoGuideLine 
+    {
+        KoGuideLine( Qt::Orientation o, double pos )
+        : orientation( o )
+        , position( pos )
+        , selected( false )
+        {}
+        Qt::Orientation orientation;
+        double position;
+        bool selected;
+    };
+
+    /**
+     * @brief Paint the canvas
+     */ 
+    void paint();
+
+    /**
+     * @brief Add a guide line with the orientation o at the position pos
+     *
+     * @param pos where to insert the guide
+     * @param o orientation of the guide line
+     */
+    void add( Qt::Orientation o, QPoint &pos );
+    
+    /**
+     * @brief Select a guide
+     *
+     * @param gd guide to select
+     */
+    void select( KoGuideLine *guideLine );
+
+    /**
+     * @brief Unselect a guide
+     *
+     * @param gd guide to unselect
+     */
+    void unselect( KoGuideLine *guideLine );
+        
+    /**
+     * @brief Unselect all selected KoGuideLineData
+     *
+     * @return true, when selection was changed
+     * @return false otherwise
+     */
+    bool unselectAll();
+
+    /**
+     * @brief remove all selected guides
+     */
+    void removeSelected();
+
+    /**
+     * @brief Check if at least one guide is selected
+     *
+     * @return true if at least on guide is seleted
+     * @return false otherwise
+     */
+    bool hasSelected();
+
+    /**
+     * @brief Find a guide
+     *
+     * This function looks for a guide at x or y pos. The position can differ by
+     * diff.
+     *
+     * @param x x position to look for a guide
+     * @param y y position to look for a guide
+     * @param diff how far next to a guide sould it also be found
+     *
+     * @return the fould guide
+     * @return 0 if none is found
+     */
+    KoGuideLine * find( KoPoint &p, double diff );
+
+    /**
+     * @brief Move selected guides.
+     *
+     * This moves all selected guides around. If more than one guide is selected it makes
+     * sure the guides are not moved of the canvas.
+     *
+     * @param pos position of the mouse
+     */
+    void moveSelectedBy( QPoint &p );
+
+    /**
+     * @brief Map pos from screen
+     * 
+     * @param pos on screen
+     *
+     * @return pos in document
+     */
+    KoPoint mapFromScreen( const QPoint & pos );
+
+    /**
+     * @brief Map pos to screen
+     * 
+     * @param pos in document
+     *
+     * @return pos on screen
+     */
+    QPoint mapToScreen( const KoPoint & pos );
+
+    /// view
+    KoView * m_view;
+    /// zoom handler of the view
+    KoZoomHandler * m_zoomHandler;
+    /// list of the position of unselected guide lines
+    QValueList<KoGuideLine *> m_guideLines;
+    /// list of the position of selected guide lines
+    QValueList<KoGuideLine *> m_selectedGuideLines;
+    /// used to save the last mouse position
+    QPoint m_lastPoint;
+    /// true if a guide is selected at the moment
+    bool m_mouseSelected;
+    /// true if a guide is inserted at the moment
+    bool m_insertGuide;
+};
+
+#endif /* KOGUIDES_H */
