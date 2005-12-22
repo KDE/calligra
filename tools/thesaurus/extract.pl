6e1705ba797e11e991700b1b3315266de6ed1db8
Script Kiddy <scripty@kde.org>
2005-12-22 12:32:17 +0000

    Fix FSF address (except in kexi/3rdparty) (Mainly old FSF address) (goutte)
    svn path=/trunk/koffice/; revision=490579

--- tools/thesaurus/extract.pl
+++ tools/thesaurus/extract.pl
@@ -15,7 +15,7 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 # Extract information from WordNet data files - only useful for development.
 # cat together all of WordNet's data.* files and call this script. As a second
