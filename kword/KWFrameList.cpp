6dd4f7cbd6e67c10eebba44df8fe5b1a33e90c37
Gary Cramblitt <garycramblitt@comcast.net>
2006-02-14 02:59:28 +0000

    Fix compile
    svn path=/trunk/koffice/; revision=509241

--- kword/KWFrameList.cpp
+++ kword/KWFrameList.cpp
@@ -15,6 +15,7 @@
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
  */
+#include <algorithm>
 #include "KWFrameList.h"
 #include "KWFrame.h"
 #include "KWFrameSet.h"
