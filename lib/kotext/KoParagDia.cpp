d1dc3f4fba27b2ce38923d34a8c6e0c7b21502b5
Thomas Zander <zander@kde.org>
2006-04-02 23:37:38 +0000

    * Fix infinite loop on showing the widget if a big indent-style was used
    svn path=/branches/koffice/1.5/koffice/; revision=525830

--- lib/kotext/KoParagDia.cpp
+++ lib/kotext/KoParagDia.cpp
@@ -779,6 +779,22 @@ KoStylePreview::KoStylePreview( const QString& title, const QString& text, QWidg
     m_zoomHandler = new KoTextZoomHandler;
     QFont font = KoGlobal::defaultFont();
     m_textdoc = new KoTextDocument( m_zoomHandler, new KoTextFormatCollection( font, QColor(), KGlobal::locale()->language(), false ) );
+
+    class MyFlow : public KoTextFlow {
+        public:
+            MyFlow(QWidget *parent, KoTextZoomHandler *zoom) {
+                m_parent = parent;
+                m_zoomHandler = zoom;
+            }
+            int availableHeight() const {
+                return m_zoomHandler->pixelToLayoutUnitY(m_parent->height());
+            }
+        private:
+            QWidget *m_parent;
+            KoTextZoomHandler *m_zoomHandler;
+    };
+
+    m_textdoc->setFlow( new MyFlow(this, m_zoomHandler) );
     //m_textdoc->setWidth( KoTextZoomHandler::ptToLayoutUnitPt( 1000 ) );
     KoTextParag * parag = m_textdoc->firstParag();
     parag->insert( 0, text );
