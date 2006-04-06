6ca6c2dfad904d8104886f58e37b0c0a32d89772
Leo Savernik <l.savernik@aon.at>
2006-04-06 19:39:40 +0000

    SVN_SILENT ld didn't like this
    svn path=/branches/koffice/1.5/koffice/; revision=527078

--- lib/kotext/KoParagDia.cpp
+++ lib/kotext/KoParagDia.cpp
@@ -772,6 +772,20 @@ QPen KoBorderPreview::setBorderPen( KoBorder _brd )
 /******************************************************************/
 /* Class: KoStylePreview. Previewing text with style ;)           */
 /******************************************************************/
+class MyFlow : public KoTextFlow {
+    public:
+        MyFlow(QWidget *parent, KoTextZoomHandler *zoom) {
+            m_parent = parent;
+            m_zoomHandler = zoom;
+        }
+        int availableHeight() const {
+            return m_zoomHandler->pixelToLayoutUnitY(m_parent->height());
+        }
+    private:
+        QWidget *m_parent;
+        KoTextZoomHandler *m_zoomHandler;
+};
+
 KoStylePreview::KoStylePreview( const QString& title, const QString& text, QWidget* parent, const char* name )
     : QGroupBox( title, parent, name )
 {
@@ -779,20 +793,6 @@ KoStylePreview::KoStylePreview( const QString& title, const QString& text, QWidg
     m_zoomHandler = new KoTextZoomHandler;
     QFont font = KoGlobal::defaultFont();
     m_textdoc = new KoTextDocument( m_zoomHandler, new KoTextFormatCollection( font, QColor(), KGlobal::locale()->language(), false ) );
-
-    class MyFlow : public KoTextFlow {
-        public:
-            MyFlow(QWidget *parent, KoTextZoomHandler *zoom) {
-                m_parent = parent;
-                m_zoomHandler = zoom;
-            }
-            int availableHeight() const {
-                return m_zoomHandler->pixelToLayoutUnitY(m_parent->height());
-            }
-        private:
-            QWidget *m_parent;
-            KoTextZoomHandler *m_zoomHandler;
-    };
 
     m_textdoc->setFlow( new MyFlow(this, m_zoomHandler) );
     //m_textdoc->setWidth( KoTextZoomHandler::ptToLayoutUnitPt( 1000 ) );
