621776a37d2fd4f70196602a14e67caa5eced844
Sebastian Sauer <mail@dipe.org>
2006-04-13 15:27:39 +0000

    Added initial files for XMLHandler.
    CC_MAIL: bernd@itii.de
    
    
    svn path=/branches/koffice/1.6/koffice/; revision=529494

---
+++ kexi/plugins/macros/lib/xmlhandler.cpp
@@ -0,0 +1,113 @@
+/***************************************************************************
+ * This file is part of the KDE project
+ * copyright (C) 2005 by Sebastian Sauer (mail@dipe.org)
+ * copyright (C) 2006 by Bernd Steindorff (bernd@itii.de)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ * You should have received a copy of the GNU Library General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ ***************************************************************************/
+
+#include "xmlhandler.h"
+
+#include <qdom.h>
+
+using namespace KoMacro;
+
+namespace KoMacro {
+
+	class XMLHandler::Private
+	{
+		public:
+
+			/**
+			* The \a Macro instance this \a XMLHandler
+			* manages.
+			*/
+			Macro::Ptr macro;
+
+			/**
+			* Constructor.
+			*
+			* @param macro The \a Macro instance this
+			* \a XMLHandler manages.
+			*/
+			Private(Macro::Ptr macro)
+				: macro(macro)
+			{
+			}
+
+			/**
+			* Parse an @a Action out of a given node
+			* @return Returns an @a Action, if the node
+			* contains a valid userdefined
+			* @a Action, else returns NULL.
+			*/
+			Action* insertAction(QDomElement element)
+			{
+				Q_UNUSED(element);
+				/*
+				QMap<QString, Action*> actionTypes = Manager::self().actionTypes();
+				Action* action = actionTypes[node.getAttribute("actiontype"));
+				// - subclasses of Action and let them parse
+				// - read properties of a node and fill them automaticly
+				// - factory in Action
+
+				// diff between predefined Action and in a Macro defined Actions
+				// - Action vs. ActionImpl
+				// - copy from Action
+				ActionImpl* ai = action->create();
+
+				action->activate(); // bzw. ai->activate()
+				return ai or action;
+				*/
+				return 0;
+			}
+	};
+}
+
+XMLHandler::XMLHandler(Macro::Ptr macro)
+	: d( new Private(macro) )
+{
+}
+
+XMLHandler::~XMLHandler()
+{
+	delete d;
+}
+
+void XMLHandler::toMacro(QDomElement element)
+{
+	Q_UNUSED(element);
+	/*
+	Macro* macro = new Macro();
+	while (node.nextNode != null) {
+		macro->addChild(insertAction(node.toElement()));
+		node = node.nextSibling();
+	}
+	*/
+}
+
+QDomElement XMLHandler::toXML()
+{
+	/*
+	QDomElement dom = new QDomElement();
+	QValueList<Action::Ptr> children = d->macro->children();
+	QValueList<Action::Ptr>::Iterator childIt = children.begin();
+	for(;childIt != children.end(); childIt++) {
+		QDomElement domChild = new QDomElement();
+		domChild = childIt;
+	}
+	*/
+	return QDomElement();
+}
+
---
+++ kexi/plugins/macros/lib/xmlhandler.h
@@ -0,0 +1,73 @@
+/***************************************************************************
+ * This file is part of the KDE project
+ * copyright (C) 2005 by Sebastian Sauer (mail@dipe.org)
+ * copyright (C) 2006 by Bernd Steindorff (bernd@itii.de)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ * You should have received a copy of the GNU Library General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ ***************************************************************************/
+
+#ifndef KOMACRO_XMLHANDLER_H
+#define KOMACRO_XMLHANDLER_H
+
+//#include <qmap.h>
+//#include <qguardedptr.h>
+//#include <ksharedptr.h>
+//#include <kxmlguiclient.h>
+#include "macro.h"
+
+class QObject;
+class QDomElement;
+
+namespace KoMacro {
+
+	class XMLHandler {
+		public:
+			/**
+			* Constructor to init a @a XMLHandler .
+			* @param macro The @a Macro instance which will
+			* be managed.
+			*/
+			XMLHandler(Macro::Ptr macro);
+
+			/**
+			* Destructor to @a XMLHandler .
+			*/
+			~XMLHandler();
+
+			/**
+			* Reads a given @a QDomElement, extracts given
+			* Actions into the managed Macro-Instance.
+			* @param element The @a QDomElement within
+			* the @a Macro.
+			*/
+			void toMacro(QDomElement element);
+
+
+			/**
+			* Converts the macro to a @a QDomElement.
+			* @return The resulten @a QDomElement from
+			* the @a Macro.
+			*/
+			QDomElement toXML();
+
+
+		private:
+			/// @internal d-pointer class.
+			class Private;
+			/// @internal d-pointer instance.
+			Private* const d;
+	};
+}
+
+#endif
