beadae7d26b205413f48b4dcd8d33efacda26f79
Dag Andersen <danders@get2net.dk>
2006-05-03 11:11:34 +0000

    Update version
    svn path=/branches/koffice/1.5/koffice/; revision=536889

--- kplato/kptaboutdata.h
+++ kplato/kptaboutdata.h
@@ -28,7 +28,7 @@ namespace KPlato
 {
 
 static const char* KPLATO_DESCRIPTION=I18N_NOOP("KPlato - KDE Planning Tool");
-static const char* KPLATO_VERSION="0.5.0";
+static const char* KPLATO_VERSION="0.5.1";
 
 KAboutData * newAboutData()
 {
