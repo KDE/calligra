cdf854efed344ee2b6d276dcc0a74ac80233b33a
Stefan Nikolaus <stefan.nikolaus@kdemail.net>
2006-05-07 11:50:37 +0000

    - add missing copyrights SVN_SILENT
    svn path=/trunk/koffice/; revision=538272

--- kspread/kspread_locale.h
+++ kspread/kspread_locale.h
@@ -1,3 +1,23 @@
+/* This file is part of the KDE project
+   Copyright (C) 2001 Laurent Montel <montel@kde.org>
+             (C) 2000 Torben Weis <weis@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
 #ifndef KSPREAD_LOCALE_H
 #define KSPREAD_LOCALE_H
 
@@ -13,7 +33,7 @@ class Locale : public KLocale
 {
 public:
     Locale();
-    
+
     void load( const QDomElement& element );
     QDomElement save( QDomDocument& doc ) const;
     void defaultSystemConfig();
