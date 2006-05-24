ae43ca4b0a008c1f3e8bed4a9947346e5441d768
Stefan Nikolaus <stefan.nikolaus@kdemail.net>
2006-05-24 11:38:54 +0000

    OpenDocument	Loading 		Fix loading of repeated cells, which don't have an assigned style. BUG: 127899
    svn path=/branches/koffice/1.5/koffice/; revision=544286

--- kspread/kspread_sheet.cc
+++ kspread/kspread_sheet.cc
@@ -6914,13 +6914,10 @@ bool Sheet::loadRowFormat( const QDomElement& row, int &rowIndex, KoOasisLoading
 
 				for ( int newRow = backupRow; newRow < endRow;++newRow )
 				{
-				    if ( targetStyle && (targetStyle->features() != 0 ) )
-				    {
                                     	Cell* target = nonDefaultCell( columnIndex, newRow );
 
 					if (cell != target)
 						target->copyAll( cell );
-				    }
                                 }
 			    }
                     }
