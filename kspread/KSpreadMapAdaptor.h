9d3dd2091018826d80753900a2b112f55a3012f9
Fredrik Edemar <f_edemar@linux.se>
2006-06-14 18:13:49 +0000

    Forgot this in my last commit
    svn path=/trunk/koffice/; revision=551480

---
+++ kspread/KSpreadMapAdaptor.h
@@ -0,0 +1,58 @@
+/* This file is part of the KDE project
+
+   Copyright 1999-2000 Torben Weis <weis@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KSPREAD_MAP_IFACE_H
+#define KSPREAD_MAP_IFACE_H
+
+#include <QList>
+#include <QStringList>
+
+#include <dbus/qdbus.h>
+
+
+namespace KSpread
+{
+class Map;
+
+class MapAdaptor : public QDBusAbstractAdaptor
+{
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", "org.kde.koffice.spreadsheet.map")
+public:
+    MapAdaptor( Map* );
+
+//     virtual bool processDynamic(const DCOPCString &fun, const QByteArray &data,
+// 				DCOPCString& replyType, QByteArray &replyData);
+
+public Q_SLOTS: // METHODS
+    virtual QString sheet( const QString& name );
+    virtual QString sheetByIndex( int index );
+    virtual int sheetCount() const;
+    virtual QStringList sheetNames() const;
+    virtual QStringList sheets();
+    virtual QString insertSheet( const QString& name );
+
+private:
+    Map* m_map;
+};
+
+} // namespace KSpread
+
+#endif
---
+++ kspread/KSpreadViewAdaptor.h
@@ -0,0 +1,149 @@
+/* This file is part of the KDE project
+
+   Copyright (C) 2006 Fredrik Edemar <f_edemar@linux.se>
+   Copyright 2002-2003 Joseph Wenninger <jowenn@kde.org>
+   Copyright 2002 John Dailey <dailey@vt.edu>
+   Copyright 2001-2002 Laurent Montel <montel@kde.org>
+   Copyright 2000 Simon Hausmann <hausmann@kde.org>
+   Copyright 1999 Torben Weis <weis@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KSPREAD_VIEW_IFACE_H
+#define KSPREAD_VIEW_IFACE_H
+
+#include <QColor>
+#include <QRect>
+#include <QString>
+
+#include <dbus/qdbus.h>
+
+namespace KSpread
+{
+class View;
+class CellProxy;
+
+class ViewAdaptor : public QDBusAbstractAdaptor
+{
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", "org.kde.koffice.spreadsheet.view")
+public:
+    ViewAdaptor( View* );
+    ~ViewAdaptor();
+
+public Q_SLOTS: // METHODS
+    virtual QString doc() const;
+    virtual QString map() const;
+    virtual QString sheet() const;
+
+    virtual void changeNbOfRecentFiles(int _nb);
+
+    virtual void hide();
+    virtual void show();
+
+    virtual void setSelection(QRect selection);
+    virtual QRect selection();
+    virtual void find();
+    virtual void replace();
+    virtual void conditional();
+    virtual void validity();
+    virtual void insertSeries();
+    virtual void insertHyperlink();
+    virtual void gotoCell();
+    virtual void changeAngle();
+    virtual void preference();
+    virtual void nextSheet();
+    virtual void previousSheet();
+    virtual bool showSheet(QString sheetName);
+    virtual void sortList();
+    virtual void setAreaName();
+    virtual void showAreaName();
+    virtual void mergeCell();
+    virtual void dissociateCell();
+    virtual void consolidate();
+
+    virtual void deleteColumn();
+    virtual void insertColumn();
+    virtual void deleteRow();
+    virtual void insertRow();
+    virtual void hideRow();
+    virtual void showRow();
+    virtual void hideColumn();
+    virtual void showColumn();
+    virtual void upper();
+    virtual void lower();
+
+    virtual void equalizeColumn();
+    virtual void equalizeRow();
+
+    virtual void clearTextSelection();
+    virtual void clearCommentSelection();
+    virtual void clearValiditySelection();
+    virtual void clearConditionalSelection();
+    virtual void goalSeek();
+    virtual void insertFromDatabase();
+    virtual void insertFromTextfile();
+    virtual void insertFromClipboard();
+    virtual void textToColumns();
+    virtual void copyAsText();
+
+    virtual void setSelectionMoneyFormat( bool b );
+    virtual void setSelectionPrecision( int delta );
+    virtual void setSelectionPercent( bool b );
+    virtual void setSelectionMultiRow( bool enable );
+    virtual void setSelectionSize(int size);
+    virtual void setSelectionUpper();
+    virtual void setSelectionLower();
+    virtual void setSelectionFirstLetterUpper();
+    virtual void setSelectionVerticalText(bool enable);
+    virtual void setSelectionComment(QString comment);
+    virtual void setSelectionAngle(int value);
+    virtual void setSelectionTextColor(QColor txtColor );
+    virtual void setSelectionBgColor(QColor bgColor );
+    virtual void setSelectionBorderColor(QColor bdColor );
+
+    virtual void deleteSelection();
+    virtual void copySelection();
+    virtual void cutSelection();
+
+    virtual void setLeftBorderColor(QColor color);
+    virtual void setTopBorderColor(QColor color);
+    virtual void setRightBorderColor(QColor color);
+    virtual void setBottomBorderColor(QColor color);
+    virtual void setAllBorderColor(QColor color);
+    virtual void setOutlineBorderColor(QColor color);
+    virtual void removeBorder();
+
+    virtual void increaseIndent();
+    virtual void decreaseIndent();
+
+    void subtotals();
+    void sortInc();
+    void sortDec();
+    void layoutDlg();
+    void increaseFontSize();
+    void decreaseFontSize();
+
+
+private:
+    View* m_view;
+    CellProxy* m_proxy;
+};
+
+} // namespace KSpread
+
+#endif
