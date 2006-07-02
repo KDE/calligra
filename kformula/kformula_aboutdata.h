a17f446f00d90dcd9238667e5f1decded9bb0084
Alfredo Beaumont Sainz <alfredo.beaumont@gmail.com>
2006-07-02 19:29:16 +0000

    Added myself as current maintainer
    svn path=/branches/koffice/1.6/koffice/; revision=557258

--- kformula/kformula_aboutdata.h
+++ kformula/kformula_aboutdata.h
@@ -1,6 +1,7 @@
 /* This file is part of the KDE project
    Copyright (C) 2001 Andrea Rizzi <rizzi@kde.org>
 	              Ulrich Kuettler <ulrich.kuettler@mailbox.tu-dresden.de>
+   Copyright (C) 2006 Alfredo Beaumont Sainz <alfredo.beaumont@gmail.com>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -34,8 +35,12 @@ KAboutData * newKFormulaAboutData()
                                              version, description, KAboutData::License_GPL,
                                              "(c) 1998-2006, The KFormula Team", 0,
 					     "http://www.koffice.org/kformula/" );
+    aboutData->addAuthor( "Alfredo Beaumont", 
+                          I18N_NOOP( "current maintainer" ),
+                          "alfredo.beaumont@gmail.com",
+                          "http://www.alfredobeaumont.org" );
     aboutData->addAuthor( "Andrea Rizzi", I18N_NOOP( "original author" ), "rizzi@kde.org" );
-    aboutData->addAuthor( "Ulrich Kuettler", I18N_NOOP( "current maintainer" ), "ulrich.kuettler@gmx.de" );
+    aboutData->addAuthor( "Ulrich Kuettler", I18N_NOOP( "core developer" ), "ulrich.kuettler@gmx.de" );
     aboutData->addAuthor( "Heinrich Kuettler", I18N_NOOP( "core developer" ), "heinrich.kuettler@gmx.de" );
     aboutData->addCredit( "Claus O. Wilke", I18N_NOOP( "for your advice to look at TeX first" ) );
     //aboutData->addCredit( "Tom Deblauwe", I18N_NOOP( "for the sweet icons" ) );
