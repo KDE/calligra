6f50cf222e31b1df4893b91d2769496e352fd186
Alfredo Beaumont Sainz <alfredo.beaumont@gmail.com>
2006-07-25 17:37:49 +0000

    OASIS OpenDocument / MathML: Created a new class to support <mstyle> element. Currently handles attribute reading. Still to develop attribute saving and rendering.
    svn path=/branches/koffice/1.6/koffice/; revision=566282

---
+++ lib/kformula/styleelement.cc
@@ -0,0 +1,162 @@
+/* This file is part of the KDE project
+   Copyright (C) 2006 Alfredo Beaumont Sainz <alfredo.beaumont@gmail.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+*/
+
+#include "basicelement.h"
+#include "styleelement.h"
+
+KFORMULA_NAMESPACE_BEGIN
+
+StyleElement::StyleElement( BasicElement* parent ) : TokenStyleElement( parent ),
+                                                     m_scriptMinSizeType( NoSize ),
+                                                     m_veryVeryThinMathSpaceType( NoSize ),
+                                                     m_veryThinMathSpaceType( NoSize ),
+                                                     m_thinMathSpaceType( NoSize ),
+                                                     m_mediumMathSpaceType( NoSize ),
+                                                     m_thickMathSpaceType( NoSize ),
+                                                     m_veryThickMathSpaceType( NoSize ),
+                                                     m_veryVeryThickMathSpaceType( NoSize ),
+                                                     m_customDisplayStyle( false ),
+                                                     m_customScriptSizeMultiplier( false ),
+                                                     m_customBackground( false )
+{
+}
+
+bool StyleElement::readAttributesFromMathMLDom( const QDomElement& element )
+{
+    if ( !BasicElement::readAttributesFromMathMLDom( element ) ) {
+        return false;
+    }
+    
+    QString scriptlevelStr = element.attribute( "scriptlevel" );
+    if ( ! scriptlevelStr.isNull() ) {
+        bool ok;
+        m_scriptLevel = scriptlevelStr.toUInt( &ok );
+        if ( ! ok ) {
+            kdWarning( DEBUGID ) << "Invalid scriptlevel attribute value: " 
+                                 << scriptlevelStr << endl;
+        }
+        else {
+            m_customScriptLevel = true;
+        }
+    }
+    QString displaystyleStr = element.attribute( "displaystyle" );
+    if ( ! displaystyleStr.isNull() ) {
+        if ( displaystyleStr.lower() == "true" ) {
+            m_displayStyle = true;
+        }
+        else {
+            m_displayStyle = false;
+        }
+        m_customDisplayStyle = true;
+    }
+    QString scriptsizemultiplierStr = element.attribute( "scriptsizemultiplier" );
+    if ( ! scriptsizemultiplierStr.isNull() ) {
+        bool ok;
+        m_scriptSizeMultiplier = scriptsizemultiplierStr.toDouble( &ok );
+        if ( ! ok ) {
+            kdWarning( DEBUGID ) << "Invalid scriptsizemultiplier attribute value: " 
+                                 << scriptsizemultiplierStr << endl;
+        }
+        else {
+            m_customScriptSizeMultiplier = true;
+        }
+    }
+    QString scriptminsizeStr = element.attribute( "scriptminsize" );
+    if ( ! scriptminsizeStr.isNull() ) {
+        readSizeAttribute( scriptminsizeStr, &m_scriptMinSizeType, &m_scriptMinSize );
+    }
+    QString backgroundStr = element.attribute( "background" );
+    if ( ! backgroundStr.isNull() ) {
+        // TODO: tranparent background
+        m_customBackground = true;
+        if ( backgroundStr[0] != '#' ) {
+            m_background = QColor( getHtmlColor( backgroundStr ) );
+        }
+        else {
+            m_background = QColor( backgroundStr );
+        }
+    }
+    QString veryverythinmathspaceStr = element.attribute( "veryverythinmathspace" );
+    if ( ! veryverythinmathspaceStr.isNull() ) {
+        readSizeAttribute( veryverythinmathspaceStr, &m_veryVeryThinMathSpaceType, &m_veryVeryThinMathSpace );
+    }
+    QString verythinmathspaceStr = element.attribute( "verythinmathspace" );
+    if ( ! verythinmathspaceStr.isNull() ) {
+        readSizeAttribute( verythinmathspaceStr, &m_veryThinMathSpaceType, &m_veryThinMathSpace );
+    }
+    QString thinmathspaceStr = element.attribute( "thinmathspace" );
+    if ( ! thinmathspaceStr.isNull() ) {
+        readSizeAttribute( thinmathspaceStr, &m_thinMathSpaceType, &m_thinMathSpace );
+    }
+    QString mediummathspaceStr = element.attribute( "mediummathspace" );
+    if ( ! mediummathspaceStr.isNull() ) {
+        readSizeAttribute( mediummathspaceStr, &m_mediumMathSpaceType, &m_mediumMathSpace );
+    }
+    QString thickmathspaceStr = element.attribute( "thickmathspace" );
+    if ( ! thickmathspaceStr.isNull() ) {
+        readSizeAttribute( thickmathspaceStr, &m_thickMathSpaceType, &m_thickMathSpace );
+    }
+    QString verythickmathspaceStr = element.attribute( "verythickmathspace" );
+    if ( ! verythickmathspaceStr.isNull() ) {
+        readSizeAttribute( verythickmathspaceStr, &m_veryThickMathSpaceType, &m_veryThickMathSpace );
+    }
+    QString veryverythickmathspaceStr = element.attribute( "veryverythickmathspace" );
+    if ( ! veryverythickmathspaceStr.isNull() ) {
+        readSizeAttribute( veryverythickmathspaceStr, &m_veryVeryThickMathSpaceType, &m_veryVeryThickMathSpace );
+    }
+    return inherited::readAttributesFromMathMLDom( element );
+}
+
+void StyleElement::writeMathML( QDomDocument& doc, QDomNode& parent, bool oasisFormat )
+{
+    QDomElement de = doc.createElement( oasisFormat ? "math:mstyle" : "mstyle" );
+    writeMathMLAttributes( de );
+    SequenceElement::writeMathML( doc, de, oasisFormat );
+    parent.appendChild( de );
+}
+
+void StyleElement::writeMathMLAttributes( QDomElement& element )
+{
+    inherited::writeMathMLAttributes( element );
+}
+
+void StyleElement::readSizeAttribute( const QString& str, SizeType* st, double* s )
+{
+    if ( st == 0 || s == 0 ){
+        return;
+    }
+    if ( str == "small" ) {
+        *st = RelativeSize;
+        *s = 0.8; // ### Arbitrary size
+    }
+    else if ( str == "normal" ) {
+        *st = RelativeSize;
+        *s = 1.0;
+    }
+    else if ( str == "big" ) {
+        *st = RelativeSize;
+        *s = 1.2; // ### Arbitrary size
+    }
+    else {
+        *s = getSize( str, st );
+    }
+
+}
+
+KFORMULA_NAMESPACE_END
