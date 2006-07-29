b4c5ea4851a482bf595346ccefe279aef5b2889a
Cyrille Berger <cyb@lepi.org>
2006-07-29 15:51:43 +0000

    make it build
    svn path=/branches/koffice/1.6/koffice/; revision=567622

--- lib/kformula/tokenelement.h
+++ lib/kformula/tokenelement.h
@@ -32,7 +32,7 @@ class TokenElement : public TokenStyleElement {
 public:
     TokenElement( BasicElement* parent = 0 );
 	virtual int buildChildrenFromMathMLDom(QPtrList<BasicElement>& list, QDomNode n);
-
+virtual QString getCharFromEntity( const QString& entity );
 protected:
     void writeMathMLContent( QDomDocument& doc, QDomElement& element, bool oasisFormat = false );
 };
