02cd2bad3dd0337ff103a0fd021215f677b2f685
Nicolas Goutte <nicolasg@snafu.de>
2006-08-17 19:36:33 +0000

    Use i18nc() instead of i18n() when there is a message context.
    svn path=/trunk/koffice/; revision=574010

--- kword/stylenames.cpp
+++ kword/stylenames.cpp
@@ -18,57 +18,57 @@
 */
 
 // This file provides translations for the standard style names
-i18n( "Style name", "Standard" );
-i18n( "Style name", "Head 1" );
-i18n( "Style name", "Head 2" );
-i18n( "Style name", "Head 3" );
-i18n( "Style name", "Enumerated List" );
-i18n( "Style name", "Alphabetical List" );
-i18n( "Style name", "Bullet List" );
-i18n( "Style name", "Contents Title" );
-i18n( "Style name", "Contents Head 1" );
-i18n( "Style name", "Contents Head 2" );
-i18n( "Style name", "Contents Head 3" );
-i18n( "Style name", "Document Title" );
-i18n( "Style name", "Header" );
-i18n( "Style name", "Footer" );
+i18nc( "Style name", "Standard" );
+i18nc( "Style name", "Head 1" );
+i18nc( "Style name", "Head 2" );
+i18nc( "Style name", "Head 3" );
+i18nc( "Style name", "Enumerated List" );
+i18nc( "Style name", "Alphabetical List" );
+i18nc( "Style name", "Bullet List" );
+i18nc( "Style name", "Contents Title" );
+i18nc( "Style name", "Contents Head 1" );
+i18nc( "Style name", "Contents Head 2" );
+i18nc( "Style name", "Contents Head 3" );
+i18nc( "Style name", "Document Title" );
+i18nc( "Style name", "Header" );
+i18nc( "Style name", "Footer" );
 
 // Translations for the standard framestyle names
-i18n( "Style name", "Plain" );
-i18n( "Style name", "Borders 1" );
-i18n( "Style name", "Borders 2" );
-i18n( "Style name", "Borders 3" );
-i18n( "Style name", "Row" );
-i18n( "Style name", "Column" );
-i18n( "Style name", "Light Gray" );
-i18n( "Style name", "Dark Gray" );
-i18n( "Style name", "Black" );
-i18n( "Style name", "Light Blue" );
-i18n( "Style name", "Dark Blue" );
-i18n( "Style name", "Red" );
-i18n( "Style name", "Yellow" );
-i18n( "Style name", "Colorful" );
-i18n( "Style name", "Bluish" );
+i18nc( "Style name", "Plain" );
+i18nc( "Style name", "Borders 1" );
+i18nc( "Style name", "Borders 2" );
+i18nc( "Style name", "Borders 3" );
+i18nc( "Style name", "Row" );
+i18nc( "Style name", "Column" );
+i18nc( "Style name", "Light Gray" );
+i18nc( "Style name", "Dark Gray" );
+i18nc( "Style name", "Black" );
+i18nc( "Style name", "Light Blue" );
+i18nc( "Style name", "Dark Blue" );
+i18nc( "Style name", "Red" );
+i18nc( "Style name", "Yellow" );
+i18nc( "Style name", "Colorful" );
+i18nc( "Style name", "Bluish" );
 
 // Translations for the standard tablestyle names
-i18n( "Style name", "Simple 1" );
-i18n( "Style name", "Simple 2" );
-i18n( "Style name", "Simple 3" );
-i18n( "Style name", "Header 1" );
-i18n( "Style name", "Header 2" );
-i18n( "Style name", "Header 3" );
-i18n( "Style name", "Header 4" );
+i18nc( "Style name", "Simple 1" );
+i18nc( "Style name", "Simple 2" );
+i18nc( "Style name", "Simple 3" );
+i18nc( "Style name", "Header 1" );
+i18nc( "Style name", "Header 2" );
+i18nc( "Style name", "Header 3" );
+i18nc( "Style name", "Header 4" );
 
 // Translations for the standard tabletemplate names
-i18n( "Style name", "Columns 1" );
-i18n( "Style name", "Columns 2" );
-i18n( "Style name", "Grid 1" );
-i18n( "Style name", "Grid 2" );
-i18n( "Style name", "Grid 3" );
-i18n( "Style name", "Gray Heading" );
-i18n( "Style name", "Blue Heading" );
-i18n( "Style name", "Blue Traditional" );
-i18n( "Style name", "Gray Traditional" );
-i18n( "Style name", "Blue Top and Bottom" );
-i18n( "Style name", "Gray Top and Bottom" );
+i18nc( "Style name", "Columns 1" );
+i18nc( "Style name", "Columns 2" );
+i18nc( "Style name", "Grid 1" );
+i18nc( "Style name", "Grid 2" );
+i18nc( "Style name", "Grid 3" );
+i18nc( "Style name", "Gray Heading" );
+i18nc( "Style name", "Blue Heading" );
+i18nc( "Style name", "Blue Traditional" );
+i18nc( "Style name", "Gray Traditional" );
+i18nc( "Style name", "Blue Top and Bottom" );
+i18nc( "Style name", "Gray Top and Bottom" );
 
