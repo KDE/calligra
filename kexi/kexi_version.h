6f7b9f4de537cc1b270d5246672e83a14fb4a386
Jarosław Staniek <staniek@kde.org>
2006-08-31 23:06:38 +0000

    SVN_SILENT KEXI_VERSION_RELEASE++
    svn path=/branches/koffice/1.6/koffice/; revision=579379

--- kexi/kexi_version.h
+++ kexi/kexi_version.h
@@ -35,7 +35,7 @@
 
 #define KEXI_VERSION_MAJOR 1
 #define KEXI_VERSION_MINOR 0
-#define KEXI_VERSION_RELEASE 89
+#define KEXI_VERSION_RELEASE 90
 
 #define KEXI_MAKE_VERSION( a,b,c ) (((a) << 16) | ((b) << 8) | (c))
 
