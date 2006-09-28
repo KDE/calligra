1edcd89fc44fb2caa7eac68800baa87437996405
Jarosław Staniek <staniek@kde.org>
2006-09-28 21:53:11 +0000

    KexiDB 2.0: 'parser' target added
    svn path=/trunk/koffice/; revision=589893

--- kexi/kexidb/parser/generate_parser_code.sh
+++ kexi/kexidb/parser/generate_parser_code.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
+# generates parser and lexer code using bison and flex
 
-# A script for Kexi Developers
-# Run this script to generate parser code using Lex and Bison tools
-
+dir=`dirname $0`
+cd $dir
 lex -osqlscanner.cpp sqlscanner.l
 bison -dv sqlparser.y
-echo '#ifndef _SQLPARSER_H_' > sqlparser.h
-echo '#define _SQLPARSER_H_' >> sqlparser.h
-echo '#include "field.h"' >> sqlparser.h
-echo '#include "parser.h"' >> sqlparser.h
-echo '#include "sqltypes.h"' >> sqlparser.h
-echo '' >> sqlparser.h
-echo 'bool parseData(KexiDB::Parser *p, const char *data);' >> sqlparser.h
+echo '#ifndef _SQLPARSER_H_
+#define _SQLPARSER_H_
+#include "field.h"
+#include "parser.h"
+#include "sqltypes.h"
+
+bool parseData(KexiDB::Parser *p, const char *data);' > sqlparser.h
+
 cat sqlparser.tab.h >> sqlparser.h
 echo '#endif' >> sqlparser.h
+
 cat sqlparser.tab.c > sqlparser.cpp
 echo "const char * const tname(int offset) { return yytname[offset]; }" >> sqlparser.cpp
+
 ./extract_tokens.sh > tokens.cpp
 rm -f sqlparser.tab.h sqlparser.tab.c
