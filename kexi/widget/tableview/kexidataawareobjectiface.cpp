b10cc3b55a147f50f1584dae40f206a085a68120
Jarosław Staniek <staniek@kde.org>
2006-10-09 12:35:05 +0000

    Table View - fixed drawing selection highlighting for the horizontal header immediately after   scrolling the table
    2.0: merged
    
    
    svn path=/branches/koffice/1.6/koffice/; revision=593878

--- kexi/widget/tableview/kexidataawareobjectiface.cpp
+++ kexi/widget/tableview/kexidataawareobjectiface.cpp
@@ -623,8 +623,6 @@ void KexiDataAwareObjectInterface::setCursorPosition(int row, int col/*=-1*/, bo
 //		ensureVisible(columnPos(d->curCol), rowPos(d->curRow) - contentsY(), columnWidth(d->curCol), rh);
 		if (m_verticalHeader && oldRow != m_curRow)
 			m_verticalHeader->setCurrentRow(m_curRow);
-		if (m_horizontalHeader && oldCol != m_curCol)
-			m_horizontalHeader->setSelectedSection(m_curCol);
 
 		if (m_updateEntireRowWhenMovingToOtherRow)
 			updateRow( m_curRow );
@@ -667,6 +665,9 @@ void KexiDataAwareObjectInterface::setCursorPosition(int row, int col/*=-1*/, bo
 
 		//quite clever: ensure the cell is visible:
 		ensureCellVisible(m_curRow, m_curCol);
+
+		if (m_horizontalHeader && oldCol != m_curCol)
+			m_horizontalHeader->setSelectedSection(m_curCol);
 
 		/*emit*/ itemSelected(m_currentItem);
 		/*emit*/ cellSelected(m_curCol, m_curRow);
