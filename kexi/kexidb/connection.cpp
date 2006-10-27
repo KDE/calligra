9d61342c4f9a54cf8750aaa721b4cd047f645618
Jarosław Staniek <staniek@kde.org>
2006-10-27 11:02:07 +0000

    Kexi Utils = multivalidator accepts everything by default   (this avoids blocking data entry e.g. for text fields   that have no specific validator assigned) 2.0: merged
    svn path=/trunk/koffice/; revision=599486

--- kexi/kexidb/connection.cpp
+++ kexi/kexidb/connection.cpp
@@ -2274,7 +2274,7 @@ tristate Connection::querySingleRecordInternal(RowData &data, const QString* sql
 	}
 	if (!cursor->moveFirst() || cursor->eof()) {
 		const tristate result = cursor->error() ? false : cancelled;
-		KexiDBWarn << "Connection::querySingleRecord(): !cursor->moveFirst() || cursor->eof()" << m_sql << endl;
+		KexiDBWarn << "Connection::querySingleRecord(): !cursor->moveFirst() || cursor->eof() m_sql=" << m_sql << endl;
 		setError(cursor);
 		deleteCursor(cursor);
 		return result;
--- kexi/kexiutils/validator.cpp
+++ kexi/kexiutils/validator.cpp
@@ -83,8 +83,6 @@ void MultiValidator::addSubvalidator( QValidator* validator, bool owned )
 
 QValidator::State MultiValidator::validate( QString & input, int & pos ) const
 {
-	if (m_subValidators.isEmpty())
-		return Invalid;
 	State s;
 	foreach3( Q3ValueList<QValidator*>::ConstIterator, it,  m_subValidators ) {
 		s = (*it)->validate(input, pos);
@@ -104,8 +102,6 @@ Validator::Result MultiValidator::internalCheck(
 	const QString &valueName, const QVariant& v, 
 	QString &message, QString &details)
 {
-	if (m_subValidators.isEmpty())
-		return Error;
 	Result r;
 	bool warning = false;
 	foreach3( Q3ValueList<QValidator*>::ConstIterator, it,  m_subValidators ) {
