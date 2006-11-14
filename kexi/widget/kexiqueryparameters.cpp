601eb732904d857f1f1ecd33462767dc8fd5cd19
Jarosław Staniek <staniek@kde.org>
2006-11-14 19:17:55 +0000

    Queries - fixed retrieving parameters of type Text 2.0: merged
    CCMAIL:cberger@cberger.net
    
    svn path=/branches/koffice/1.6/koffice/; revision=604943

--- kexi/widget/kexiqueryparameters.cpp
+++ kexi/widget/kexiqueryparameters.cpp
@@ -114,6 +114,7 @@ QValueList<QVariant> KexiQueryParameters::getParameters(QWidget *parent,
 			values.append( result );
 			break;
 		}
+		case KexiDB::Field::Text:
 		case KexiDB::Field::LongText: {
 			const QString result = KInputDialog::getText( 
 				caption, (*it).message, QString::null, &ok, parent);
--- kexi/widget/tableview/kexitableview.cpp
+++ kexi/widget/tableview/kexitableview.cpp
@@ -1150,7 +1150,7 @@ void KexiTableView::contentsMouseMoveEvent( QMouseEvent *e )
 		if (row > (rows() - 1 + (isInsertingEnabled()?1:0)))
 			row = -1; //no row to paint
 	}
-	kexidbg << " row="<<row<< " col="<<col<<endl;
+//	kexidbg << " row="<<row<< " col="<<col<<endl;
 	//update row highlight if needed
 	if (d->appearance.rowMouseOverHighlightingEnabled) {
 		if (row != d->highlightedRow) {
