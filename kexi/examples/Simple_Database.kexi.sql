3182cbb940308519a1f4fefbd934156dbd5dbda9
Jarosław Staniek <staniek@kde.org>
2006-11-29 21:51:28 +0000

    Examples - update the parameter query: remove joins because there's still one related bug
    svn path=/branches/koffice/1.6/koffice/; revision=609254

--- kexi/examples/Simple_Database.kexi.sql
+++ kexi/examples/Simple_Database.kexi.sql
@@ -633,7 +633,7 @@ INSERT INTO "kexi__objectdata" VALUES(106, '<!DOCTYPE macros>
   </item>
 </macro>
 ', NULL);
-INSERT INTO "kexi__objectdata" VALUES(107, 'SELECT persons.name, persons.surname, persons.age, cars.model, ownership.since FROM persons, ownership, cars WHERE (cars.id = ownership.car AND persons.id = ownership.owner) AND (persons.age > [Enter minimum owner''s age]) ORDER BY persons.age', 'sql');
+INSERT INTO "kexi__objectdata" VALUES(107, 'SELECT name, surname, age FROM persons WHERE persons.age > [Enter minimum person''s age] ORDER BY age', 'sql');
 INSERT INTO "kexi__objectdata" VALUES(107, NULL, 'query_layout');
 CREATE TABLE kexi__objects (o_id INTEGER PRIMARY KEY, o_type UNSIGNED Byte, o_name Text(200), o_caption Text(200), o_desc CLOB);
 INSERT INTO "kexi__objects" VALUES(1, 1, 'persons', 'Persons in our company', NULL);
@@ -646,7 +646,7 @@ INSERT INTO "kexi__objects" VALUES(103, 1, 'ownership', 'Ownership', NULL);
 INSERT INTO "kexi__objects" VALUES(104, 2, 'persons_and_cars', 'Persons and cars', NULL);
 INSERT INTO "kexi__objects" VALUES(105, 3, 'ownership', 'Ownership', NULL);
 INSERT INTO "kexi__objects" VALUES(106, 7, 'open_cars_and_go_to_last', 'Open cars and go to the last row', NULL);
-INSERT INTO "kexi__objects" VALUES(107, 2, 'owners_by_age', 'Owners by age', NULL);
+INSERT INTO "kexi__objects" VALUES(107, 2, 'persons_by_age', 'owners_by_age', NULL);
 CREATE TABLE kexi__parts (p_id INTEGER PRIMARY KEY, p_name Text(200), p_mime Text(200), p_url Text(200));
 INSERT INTO "kexi__parts" VALUES(1, 'Tables', 'kexi/table', 'http://koffice.org/kexi/');
 INSERT INTO "kexi__parts" VALUES(2, 'Queries', 'kexi/query', 'http://koffice.org/kexi/');
