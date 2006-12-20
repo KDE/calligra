4bc14d21d3270726a0210450766eae85228ef53b
Jarosław Staniek <staniek@kde.org>
2006-12-20 15:26:58 +0000

    SVN_SILENT compile
    svn path=/branches/koffice/1.6/koffice/; revision=615187

--- kexi/main/keximainwindowimpl.cpp
+++ kexi/main/keximainwindowimpl.cpp
@@ -994,8 +994,6 @@ void KexiMainWindowImpl::initActions()
 	acat->addAction("window_close", Kexi::GlobalActionCategory | Kexi::WindowActionCategory);
 	acat->setAllObjectTypesSupported("window_close", true);
 
-		KexiPart::TableObjectType, KexiPart::QueryObjectType,
-
 	acat->addAction("window_next", Kexi::GlobalActionCategory);
 
 	acat->addAction("window_previous", Kexi::GlobalActionCategory);
