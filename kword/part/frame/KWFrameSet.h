1a54c222ba4d7e0d2fddce1708e31229e26a21e4
David Faure <faure@kde.org>
2006-12-21 18:33:44 +0000

    Add missing export macro; might fix dashbot, I realize scripting is disabled here.
    svn path=/trunk/koffice/; revision=615490

--- kword/part/frame/KWFrameSet.h
+++ kword/part/frame/KWFrameSet.h
@@ -21,6 +21,7 @@
 #define KWFRAMESET_H
 
 #include "KWord.h"
+#include "kword_export.h"
 
 #include <QObject>
 
@@ -32,7 +33,7 @@ class KWFrame;
  * The FrameSet holds KWFrame objects that actually render the content this object
  * holds to the screen or to the printer.
  */
-class KWFrameSet : public QObject {
+class KWORD_TEST_EXPORT KWFrameSet : public QObject {
     Q_OBJECT
 public:
     /// Constructor.
