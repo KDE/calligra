55c9f2d657f52bbcfc7a34df1e53586dd96338a6
Ariya Hidayat <ariya@kde.org>
2006-12-26 11:52:02 +0000

    valid copyright holders
    svn path=/trunk/koffice/; revision=616660

--- kspread/functions/financial.cpp
+++ kspread/functions/financial.cpp
@@ -1,6 +1,12 @@
 /* This file is part of the KDE project
-   Copyright (C) 1998-2002 The KSpread Team <koffice-devel@kde.org>
+
    Copyright (C) 2005 Tomas Mecir <mecirt@gmail.com>
+   Copyright (C) 2002 Ariya Hidayat <ariya@kde.org>
+   Copyright (C) 2002 Norbert Andres <nandres@web.de>
+   Copyright (C) 1999-2000 Laurent Montel <montel@kde.org>
+   Copyright (C) 1999-2000 Laurent Montel <montel@kde.org>
+   Copyright (C) 1999-2000 Jonathan Singer <jsinger@genome.wi.mit.edu>
+   Copyright (C) 1998-1999 Torben Weis <weis@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
