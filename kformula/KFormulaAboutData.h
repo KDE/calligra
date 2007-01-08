48b5cb79a2a3333c488b7da575cfb91009b20767
David Faure <faure@kde.org>
2007-01-08 14:48:28 +0000

    Use the koffice version string
    svn path=/trunk/koffice/; revision=621270

--- kformula/KFormulaAboutData.h
+++ kformula/KFormulaAboutData.h
@@ -24,10 +24,10 @@
 
 #include <kaboutdata.h>
 #include <klocale.h>
-#include <config.h>
+#include <kofficeversion.h>
 
 static const char* description=I18N_NOOP("KOffice Formula Editor");
-static const char* version=VERSION;
+static const char* version=KOFFICE_VERSION_STRING;
 
 KAboutData * newKFormulaAboutData()
 {
