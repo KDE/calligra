94db6826dcb1fe43f244d98294f441e6e8bc455b
Stefan Nikolaus <stefan.nikolaus@kdemail.net>
2007-01-08 19:25:54 +0000

    PointStorage	A hint, that row-wise filling is faster than column-wise.
    svn path=/trunk/koffice/; revision=621393

--- kspread/PointStorage.h
+++ kspread/PointStorage.h
@@ -46,6 +46,7 @@ namespace KSpread
  * \author Stefan Nikolaus <stefan.nikolaus@kdemail.net>
  * \since 2.0
  *
+ * \note If you fill the storage, do it row-wise. That's more performant.
  * \note For data assigned to rectangular regions use RectStorage.
  */
 template<typename T>
