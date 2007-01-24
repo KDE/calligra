174fec7744937dcf8262c2b1e105c840fde90015
Jarosław Staniek <staniek@kde.org>
2007-01-24 18:50:50 +0000

    Examples - allow to skip $2
    2.0: merged
    
    svn path=/trunk/koffice/; revision=626852

--- kexi/examples/build_kexi_file.sh
+++ kexi/examples/build_kexi_file.sh
@@ -1,22 +1,31 @@
 #!/bin/sh
 
 # Builds a single .kexi file from a .sql file specified as $1.
+# The destination .kexi file is saved with name specified as $2.
+# $2 can be omitted if $1 is of a form "name.kexi.sql" - then
+# the destination file will be "name.kexi".
 # Only .kexi file that is older than .sql file is recreated.
-# ksqlite is needed on the PATH
+# ksqlite is needed on the $PATH.
 
 KEXISQL3PATH=../3rdparty/kexisql3/src/.libs/
 PATH=$PATH:$KEXISQL3PATH
 LD_LIBRARY_PATH=$KEXISQL3PATH:$KEXISQL3PATH
 which ksqlite > /dev/null || exit 1
 
-[ $# -lt 1 ] && echo "Missing .kexi.sql filename." && exit 1
+[ $# -lt 1 ] && echo "Missing .sql filename." && exit 1
 
-kexi_file=`echo $1 | sed -e "s/\.kexi\.sql/\.kexi/"`
-if test -f $kexi_file -a ! $kexi_file -ot $1 ; then
+if [ $# -lt 2 ] ; then
+	kexi_file=`echo $1 | sed -e "s/\.kexi\.sql/\.kexi/"`
+else
+	kexi_file=$2
+fi
+
+if test -f "$kexi_file" -a ! "$kexi_file" -ot "$1" ; then
 	echo "Local $kexi_file is newer than $1 - skipping it"
 	exit 0
 fi
-rm -f $kexi_file
-echo "Creating $kexi_file ... "
-ksqlite $kexi_file < $1 || exit 1
+
+rm -f "$kexi_file"
+echo "Creating \"$kexi_file\" ... "
+ksqlite "$kexi_file" < "$1" || exit 1
 echo "OK"
