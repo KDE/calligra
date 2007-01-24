160d9abfd9cb352ff53c75681b586917972e1ead
Andras Mantia <amantia@kde.org>
2007-01-24 18:20:15 +0000

    Fix builddir != srcdir.
    svn path=/branches/koffice/1.6/koffice/; revision=626840

--- kexi/examples/build_kexi_file.sh
+++ kexi/examples/build_kexi_file.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 # Builds a single .kexi file from a .sql file specified as $1.
+# The otuput kexi file name is specified in $2.
 # Only .kexi file that is older than .sql file is recreated.
 # ksqlite is needed on the PATH
+#
+
 
 KEXISQL3PATH=../3rdparty/kexisql3/src/.libs/
 PATH=$PATH:$KEXISQL3PATH
@@ -11,7 +14,8 @@ which ksqlite > /dev/null || exit 1
 
 [ $# -lt 1 ] && echo "Missing .kexi.sql filename." && exit 1
 
-kexi_file=`echo $1 | sed -e "s/\.kexi\.sql/\.kexi/"`
+#kexi_file=`echo $1 | sed -e "s/\.kexi\.sql/\.kexi/"`
+kexi_file=$2
 if test -f $kexi_file -a ! $kexi_file -ot $1 ; then
 	echo "Local $kexi_file is newer than $1 - skipping it"
 	exit 0
