f765422b70dbcb63d925a335bd064d0c6acf82c3
Sebastian Sauer <mail@dipe.org>
2007-02-04 20:57:27 +0000

    even better. SVN_SILENT
    svn path=/trunk/koffice/; revision=630241

--- kword/plugins/scripting/scripts/sample_progressbar.py
+++ kword/plugins/scripting/scripts/sample_progressbar.py
@@ -2,7 +2,9 @@
 
 import KWord, time
 komainwindow = KWord.shell()
-for i in range(1,101):
-    komainwindow.slotProgress(i)
-    time.sleep(0.1)
-komainwindow.slotProgress(-1)
+try:
+    for i in range(1,101):
+        komainwindow.slotProgress(i)
+        time.sleep(0.1)
+finally:
+    komainwindow.slotProgress(-1)
