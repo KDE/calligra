d41b39ddeda00201fce2d44544f2fc04bd781e84
Sebastian Sauer <mail@dipe.org>
2007-02-04 21:23:24 +0000

    Set also the statusbartext.
    svn path=/trunk/koffice/; revision=630250

--- kword/plugins/scripting/scripts/sample_progressbar.py
+++ kword/plugins/scripting/scripts/sample_progressbar.py
@@ -2,9 +2,11 @@
 
 import KWord, time
 komainwindow = KWord.shell()
+komainwindow.slotSetStatusBarText("This is the sample_progressbar.py script")
 try:
     for i in range(1,101):
         komainwindow.slotProgress(i)
         time.sleep(0.1)
 finally:
+    komainwindow.slotSetStatusBarText("")
     komainwindow.slotProgress(-1)
