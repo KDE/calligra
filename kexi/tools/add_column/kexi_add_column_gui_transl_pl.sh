483d301c008f0acdebe305fb7ee6a3361ec8741e
Jarosław Staniek <staniek@kde.org>
2007-02-07 19:45:49 +0000

    Tools - added GUI version of "Add column" tool (with translation enabled) - the tool is now installed
    2.0: merged
    
    
    svn path=/branches/koffice/1.6/koffice/; revision=631349

---
+++ kexi/tools/add_column/kexi_add_column_gui_transl_pl.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Translation file for kexi_add_column_gui
+
+msg_filters="*.kexi|Projekt Kexi zapisany w pliku
+*.*|Wszystkie pliki"
+msg_select_db_file="Wybierz plik bazy danych"
+msg_enter_table_name="Nazwa tabeli (bez spacji):"
+msg_enter_new_column_name="Nazwa nowej kolumny (bez spacji):"
+msg_enter_new_column_type="Typ nowej kolumny:"
+msg_short_integer="Liczba calkowita krotka"
+msg_integer="Liczba calkowita"
+msg_big_integer="Liczba calkowita wielka"
+msg_yes_no="Tak/Nie"
+msg_date="Data"
+msg_date_time="Data/Czas"
+msg_time="Czas"
+msg_float="Liczba zmiennoprzecinkowa krotka"
+msg_double="Liczba zmiennoprzecinkowa dluga"
+msg_text="Tekst"
+msg_long_text="Dlugi tekst"
+msg_object="Obiekt (obraz)"
+msg_enter_new_column_caption="Tytul nowej kolumny (opcjonalnie):"
