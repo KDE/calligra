f867451cfce59f53b121ee372322ba441b9eb236
Brad Hards <bradh@frogmouth.net>
2007-02-08 03:52:16 +0000

    Add in the start of some unit tests for the information functions. In this case, just VALUE, and it fails.
    Stefan: can you have a look at this, and let me know
    what sort of changes you want to make (or have me make) -
    certainly N() and VALUE() are supposed to have different
    functionality.
    
    CCMAIL: stefan.nikolaus@kdemail.net
    
    svn path=/trunk/koffice/; revision=631450

---
+++ kspread/tests/TestInformationFunctions.cpp
@@ -0,0 +1,101 @@
+/* This file is part of the KDE project
+   Copyright 2007 Brad Hards <bradh@frogmouth.net>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; only
+   version 2 of the License.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "qtest_kde.h"
+
+#include <Formula.h>
+#include <Value.h>
+
+#include "TestInformationFunctions.h"
+
+#include <float.h> // DBL_EPSILON
+#include <math.h>
+
+using namespace KSpread;
+
+
+// because we may need to promote expected value from integer to float
+#define CHECK_EVAL(x,y) { Value z(y); QCOMPARE(evaluate(x,z),(z)); }
+
+Value TestInformationFunctions::evaluate(const QString& formula, Value& ex)
+{
+  Formula f;
+  QString expr = formula;
+  if ( expr[0] != '=' )
+    expr.prepend( '=' );
+  f.setExpression( expr );
+  Value result = f.eval();
+
+  if(result.isFloat() && ex.isInteger())
+    ex = Value(ex.asFloat());
+  if(result.isInteger() && ex.isFloat())
+    result = Value(result.asFloat());
+
+  return result;
+}
+
+namespace QTest
+{
+  template<>
+  char *toString(const Value& value)
+  {
+    QString message;
+    QTextStream ts( &message, QIODevice::WriteOnly );
+    ts << value;
+    return qstrdup(message.toLatin1());
+  }
+}
+
+void TestInformationFunctions::testVALUE()
+{
+    CHECK_EVAL( "VALUE(\"6\")", Value( 6 ) );
+    CHECK_EVAL( "VALUE(\"1E5\")", Value( 100000 ) );
+    CHECK_EVAL( "VALUE(\"200%\")",  Value( 2 ) );
+    CHECK_EVAL( "VALUE(\"1.5\")", Value( 1.5 ) );
+    // Check fractions
+    CHECK_EVAL( "VALUE(\"7 1/4\")", Value( 7.25 ) );
+    CHECK_EVAL( "VALUE(\"0 1/2\")", Value( 0.5 ) );
+    // Check times
+    CHECK_EVAL( "VALUE(\"00:00\")", Value( 0 ) );
+    CHECK_EVAL( "VALUE(\"02:00\")-2/24", Value( 0 ) );
+    // check dates - local dependent
+    CHECK_EVAL( "VALUE(\"5/21/06\")=DATE(2006;5;21)", Value( true ) );
+    CHECK_EVAL( "VALUE(\"1/2/2005\")=DATE(2005;1;2)", Value( true ) );
+}
+
+
+#include <kaboutdata.h>
+#include <kcmdlineargs.h>
+#include <kapplication.h>
+
+#define KSPREAD_TEST(TestObject) \
+int main(int argc, char *argv[]) \
+{ \
+    setenv("LC_ALL", "C", 1); \
+    setenv("KDEHOME", QFile::encodeName( QDir::homePath() + "/.kde-unit-test" ), 1); \
+    KAboutData aboutData( "qttest", "qttest", "version" );  \
+    KCmdLineArgs::init(&aboutData); \
+    KApplication app; \
+    TestObject tc; \
+    return QTest::qExec( &tc, argc, argv ); \
+}
+
+KSPREAD_TEST(TestInformationFunctions)
+
+#include "TestInformationFunctions.moc"
