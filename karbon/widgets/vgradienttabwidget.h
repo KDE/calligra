38b72319c06508842ad853b301a1558d1ad51541
Jan Hambrecht <jaham@gmx.net>
2007-02-08 18:57:26 +0000

    SVN_SILENT fixed copyright notices
    svn path=/trunk/koffice/; revision=631675

--- karbon/widgets/vgradienttabwidget.h
+++ karbon/widgets/vgradienttabwidget.h
@@ -1,7 +1,7 @@
 /* This file is part of the KDE project
-   Copyright (C) 2001, The Karbon Developers
-   Copyright (C) 2002, The Karbon Developers
-   Copyright (C) 2007 Jan Hambrecht <jaham@gmx.net>
+   Copyright (C) 2001-2002 Benoît Vautrin <benoit.vautrin@free.fr>
+   Copyright (C) 2002 Rob Buis <buis@kde.org>
+   Copyright (C) 2006-2007 Jan Hambrecht <jaham@gmx.net>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
