f86f6e761d9ad9515cf1cf1028d60ce72a1bffe3
Stefan Nikolaus <stefan.nikolaus@kdemail.net>
2007-02-18 11:07:38 +0000

    Beautify. SVN_SILENT
    svn path=/trunk/koffice/; revision=634784

--- kspread/Digest.h
+++ kspread/Digest.h
@@ -33,7 +33,7 @@
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301, USA
+   Boston, MA 02110-1301, USA
  *
  *  Contributor(s): Matthias Huetsch <matthias.huetsch@sun.com>
  *
