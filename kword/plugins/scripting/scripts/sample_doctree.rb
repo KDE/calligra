f9ba608e7b067929a84fa099d15244f3d5ed93ea
Sebastian Sauer <mail@dipe.org>
2007-02-22 00:46:56 +0000

    improved the ruby doctree sample. Nice language :)
    svn path=/trunk/koffice/; revision=636125

--- kword/plugins/scripting/scripts/sample_doctree.rb
+++ kword/plugins/scripting/scripts/sample_doctree.rb
@@ -1,23 +1,113 @@
 #!/usr/bin/env kross
 
+# Load the Qt4-QtRuby module.
 begin
     require 'Qt'
 rescue LoadError
     raise "Failed to load the required QtRuby module. Please install QtRuby."
 end
 
+# Load the KWord scripting module.
 begin
     require 'KWord'
 rescue LoadError
+    # If loading failed (cause the script was not executed from within
+    # a running KWord instance) we try to load the Kross module and
+    # ask it to load the KWord module for us.
     require 'Kross'
     KWord = Kross.module('KWord')
 
-    # testcase to fill KWord with some content ;)
-    mytextframeset = KWord.addTextFrameSet("myTextFrame")
-    mytextframe = mytextframeset.addTextFrame()
-    textdoc = mytextframeset.textDocument()
-    textdoc.setHtml("<h1>The Header</h1><p>Some text</p>")
+    # Testcase to fill KWord with some content. You are also able to e.g.
+    # load a document here with "KWord.document().openURL('/path/file.odt')"
+    # (maybe openURL was renamed to openUrl meanwhile ;)
+    mytextframeset1 = KWord.addTextFrameSet("myFirstTextFrame")
+    mytextframeset1.addTextFrame()
+    mytextframeset1.textDocument().setHtml("<h1>First Header</h1><p>Some text</p><p>Some more text with <b>bold</b> and <i>italic</i> and <u>underline</u> to test the stuff.</p>")
+    mytextframeset2 = KWord.addTextFrameSet("mySecondTextFrame")
+    mytextframeset2.addTextFrame()
+    mytextframeset2.textDocument().setHtml("<h1>Second Header</h1><h2>First Sub Header</h2><p>Some text.</p><h2>Second Sub Header</h2><p>Some text.</p>")
 end
+
+#########################################################################
+# Pages - Each item is able to provide different pages
+
+class Page
+    def initialize(label)
+        @label = label
+    end
+    def label()
+        return @label
+    end
+    def createWidget(parent)
+        return nil
+    end
+end
+
+class PropertyPage < Page
+
+    class Property
+        def initialize(name, value)
+            @name = name
+            @value = value
+        end
+        def name
+            return @name.to_s
+        end
+        def value
+            return @value.to_s
+        end
+    end
+
+    def initialize(properties = [])
+        super("Properties")
+        @properties = properties
+    end
+
+    def createWidget(parent)
+        table = Qt::TableWidget.new(parent)
+        table.setColumnCount(2)
+        #table.setHorizontalHeaderLabels( QStringList() << i18n("Name") << i18n("Value") );
+        table.setHorizontalHeaderLabels( ["Name","Value"] )
+        table.verticalHeader().hide()
+        table.alternatingRowColors = true
+        #table.horizontalHeader().resizeSections( Qt::HeaderView::ResizeToContents )
+        table.horizontalHeader().setStretchLastSection(true)
+        #table.setEditTriggers( Qt::AbstractItemView::AllEditTriggers )
+        table.setEditTriggers( Qt::AbstractItemView::NoEditTriggers )
+        #@table.setSortingEnabled(true)
+
+        for property in @properties
+            rows = table.rowCount()
+            table.insertRow(rows)
+            table.setItem(rows, 0, Qt::TableWidgetItem.new( property.name ))
+            table.setItem(rows, 1, Qt::TableWidgetItem.new( property.value ))
+        end
+
+        return table
+    end
+
+end
+
+class TextEditPage < Page
+
+    def initialize(label, text = "")
+        super(label)
+        @text = text
+    end
+
+    def createWidget(parent)
+        edit = Qt::TextEdit.new(parent)
+        if @text
+            edit.plainText = @text.to_s
+        end
+        edit.readOnly = true
+        return edit
+    end
+
+end
+
+#########################################################################
+# Item - Interface class for all items within the TreeModel.
 
 class Item
 
@@ -25,6 +115,7 @@ class Item
         @parentitem = parentitem
         @data = data
         @childitems = []
+        @pages = []
     end
 
     def parent
@@ -46,111 +137,132 @@ class Item
         return 0
     end
 
-    def createEditorWidget(parent)
+    def page(index)
+        return @pages[index]
     end
+
+    def pageCount
+        return @pages.length()
+    end
+
 end
+
+#########################################################################
+# FrameItem - Item for KWord Frame objects.
 
 class FrameItem < Item
 
+    class TextDocumentItem < Item
+        def initialize(frameitem, textdoc)
+            super(frameitem, textdoc)
+            #textdoc.rootFrame()
+            @pages.push(
+                PropertyPage.new( [
+                    PropertyPage::Property.new("width", @data.width()),
+                    PropertyPage::Property.new("height", @data.height()),
+                    PropertyPage::Property.new("pageCount", @data.pageCount()),
+                    PropertyPage::Property.new("isModified", @data.isModified()),
+                ] )
+            )
+            @pages.push( TextEditPage.new("Text", @data.toText()) )
+            @pages.push( TextEditPage.new("HTML", @data.toHtml()) )
+            @pages.push( TextEditPage.new("Style", @data.defaultStyleSheet()) )
+
+            vars = ""
+            for n in @data.variableNames()
+                vars += "#{n}=%s\n" % @data.variableValue(n)
+            end
+            @pages.push( TextEditPage.new("Variables", vars) )
+        end
+        def data(column)
+            return Qt::Variant.new("Document")
+        end
+    end
+
     def initialize(framesetitem, frame)
         super(framesetitem, frame)
+        frameset = frame.frameSet()
+        textdoc = frameset.textDocument()
+        if textdoc != 0
+            @childitems.push( TextDocumentItem.new(self, textdoc) )
+        end
+        @pages.push(
+            PropertyPage.new( [
+                PropertyPage::Property.new("width", @data.width()),
+                PropertyPage::Property.new("shapeId", @data.shapeId()),
+                PropertyPage::Property.new("visible", @data.isVisible()),
+                PropertyPage::Property.new("scaleX", @data.scaleX()),
+                PropertyPage::Property.new("scaleY", @data.scaleY()),
+                PropertyPage::Property.new("rotation", @data.rotation()),
+                PropertyPage::Property.new("shearX", @data.shearX()),
+                PropertyPage::Property.new("shearY", @data.shearY()),
+                PropertyPage::Property.new("width", @data.width()),
+                PropertyPage::Property.new("height", @data.height()),
+                PropertyPage::Property.new("positionX", @data.positionX()),
+                PropertyPage::Property.new("positionY", @data.positionY()),
+                PropertyPage::Property.new("zIndex", @data.zIndex()),
+            ] )
+        )
     end
 
     def data(column)
         return Qt::Variant.new( @data.shapeId().to_s() )
     end
 
-    def createEditorWidget(parent)
-        widget = Qt::Widget.new(parent)
-        layout = Qt::VBoxLayout.new
-        layout.setMargin(0)
-        widget.setLayout(layout)
-
-        visible = @data.isVisible()
-        layout.addWidget( Qt::Label.new("visible: #{visible}") )
-
-        scaleX = @data.scaleX()
-        layout.addWidget( Qt::Label.new("scaleX: #{scaleX}") )
-        scaleY = @data.scaleY()
-        layout.addWidget( Qt::Label.new("scaleY: #{scaleY}") )
-
-        rotation = @data.rotation()
-        layout.addWidget( Qt::Label.new("rotation: #{rotation}") )
-
-        shearX = @data.shearX()
-        layout.addWidget( Qt::Label.new("shearX: #{shearX}") )
-        shearY = @data.shearY()
-        layout.addWidget( Qt::Label.new("shearY: #{shearY}") )
-
-        width = @data.width()
-        layout.addWidget( Qt::Label.new("width: #{width}") )
-        height = @data.height()
-        layout.addWidget( Qt::Label.new("height: #{height}") )
-
-        positionX = @data.positionX()
-        layout.addWidget( Qt::Label.new("positionX: #{positionX}") )
-        positionY = @data.positionY()
-        layout.addWidget( Qt::Label.new("positionY: #{positionY}") )
-        zIndex = @data.zIndex()
-        layout.addWidget( Qt::Label.new("zIndex: #{zIndex}") )
-
-        layout.addStretch(1)
-        return widget
-    end
 end
+
+#########################################################################
+# FramesetItem - Item for KWord FrameSet objects.
 
 class FramesetItem < Item
 
     def initialize(rootitem, frameset)
         super(rootitem, frameset)
         for i in 0..(frameset.frameCount() - 1)
-            frame = frameset.frame(i)
-            @childitems.push( FrameItem.new(self, frame) )
+            @childitems.push( FrameItem.new(self, frameset.frame(i)) )
         end
+        @pages.push(
+            PropertyPage.new( [
+                PropertyPage::Property.new("name", @data.name()),
+                PropertyPage::Property.new("frameCount", @data.frameCount()),
+                PropertyPage::Property.new("isText", @data.textDocument() != 0),
+            ] )
+        )
+
     end
 
     def data(column)
         return Qt::Variant.new( @data.name().to_s() )
     end
 
-    def createEditorWidget(parent)
-        widget = Qt::Widget.new(parent)
-        layout = Qt::VBoxLayout.new
-        layout.setMargin(0)
-        widget.setLayout(layout)
-
-        frameCount = @data.frameCount()
-        layout.addWidget( Qt::Label.new("frameCount: #{frameCount}") )
-
-        isText = @data.textDocument() != nil
-        layout.addWidget( Qt::Label.new("isText: #{isText}") )
-
-        layout.addStretch(1)
-        return widget
-    end
 end
+
+#########################################################################
+# RootItem - Rootitem within the TreeModel.
 
 class RootItem < Item
 
     def initialize()
         super(nil, nil)
         for i in 0..(KWord.frameSetCount() - 1)
-            frameset = KWord.frameSet(i)
-            @childitems.push( FramesetItem.new(self, frameset) )
+            @childitems.push( FramesetItem.new(self, KWord.frameSet(i)) )
         end
     end
 
     def data(column)
-        return Qt::Variant.new()
+        return Qt::Variant.new
     end
 
 end
+
+#########################################################################
+# TreeModel - The model that handles the items.
 
 class TreeModel < Qt::AbstractItemModel
 
     def initialize(parent)
         super(parent)
-        @rootItem = RootItem.new()
+        @rootItem = RootItem.new
     end
 
     def columnCount(parent)
@@ -168,7 +280,7 @@ class TreeModel < Qt::AbstractItemModel
     #    if orientation == Qt::Horizontal && role == Qt::DisplayRole
     #        return Qt::Variant.new(@rootItem.data(section))
     #    end
-    #    return Qt::Variant.new()
+    #    return Qt::Variant.new
     #end
 
     def index(row, column, parent)
@@ -181,7 +293,7 @@ class TreeModel < Qt::AbstractItemModel
         if !childItem.nil?
             return createIndex(row, column, childItem)
         end
-        return Qt::ModelIndex.new()
+        return Qt::ModelIndex.new
     end
 
     def parent(index)
@@ -192,18 +304,18 @@ class TreeModel < Qt::AbstractItemModel
                 return createIndex(parentItem.row(), 0, parentItem)
             end
         end
-        return Qt::ModelIndex.new()
+        return Qt::ModelIndex.new
     end
 
     def data(index, role)
         if !index.valid?
-            return Qt::Variant.new()
+            return Qt::Variant.new
         end
         if role == Qt::DisplayRole
             item = index.internalPointer
             return item.data(index.column)
         end
-        return Qt::Variant.new()
+        return Qt::Variant.new
     end
 
     def rowCount(parent)
@@ -217,45 +329,12 @@ class TreeModel < Qt::AbstractItemModel
 
 end
 
-class Editor < Qt::Widget
-
-    slots 'slotCurrentChanged(const QModelIndex&, const QModelIndex&)'
-
-    def initialize(parent, view)
-        super(parent)
-        @view = view
-
-        layout = Qt::VBoxLayout.new
-        setLayout(layout)
-
-        @label = Qt::Label.new(self)
-        layout.addWidget(@label)
-
-        @widget = Qt::Widget.new(self)
-        layout.addWidget(@widget, 1)
-
-        #layout.addSpacing(10)
-        #layout.addStretch(1)
-    end
-
-    def slotCurrentChanged(current, prev)
-        hide()
-        @widget.hide()
-        @widget.destroy()
-        @widget = Qt::Widget.new(self)
-        layout.addWidget(@widget, 1)
-        if !current.valid?
-            @label.text = ""
-        else
-            @label.text = @view.model().data(current, Qt::DisplayRole)
-            current.internalPointer.createEditorWidget(@widget)
-        end
-        show()
-    end
-
-end
+#########################################################################
+# Dialog - The main dialog window.
 
 class Dialog < Qt::Dialog
+
+    slots 'slotCurrentChanged(const QModelIndex&, const QModelIndex&)'
 
     def initialize()
         super()
@@ -274,19 +353,38 @@ class Dialog < Qt::Dialog
         @view.show()
         splitter.addWidget(@view)
 
-        scrollArea = Qt::ScrollArea.new(self)
-        scrollArea.widgetResizable = true
-        #scrollArea.backgroundRole = Qt::Palette::Dark
-        @editor = Editor.new(scrollArea, @view)
-        scrollArea.widget = @editor
-        splitter.addWidget(scrollArea)
-        scrollArea.resize( Qt::Size.new(260, 200).expandedTo( scrollArea.minimumSizeHint() ) );
+        editorwidget = Qt::Widget.new(self)
+        editorlayout = Qt::VBoxLayout.new
+        editorlayout.setMargin(0)
+        editorwidget.setLayout(editorlayout)
+        #@label = Qt::Label.new(editorwidget)
+        #editorlayout.addWidget(@label)
+        @tab = Qt::TabWidget.new(self)
+        editorlayout.addWidget(@tab)
+        #layout.addSpacing(10)
+        #layout.addStretch(1)
+        splitter.addWidget(editorwidget)
+        editorwidget.resize( Qt::Size.new(360, 400).expandedTo( editorwidget.minimumSizeHint() ) )
 
-        connect(@view.selectionModel, SIGNAL('currentChanged(const QModelIndex&, const QModelIndex&)'), @editor, SLOT('slotCurrentChanged(const QModelIndex&, const QModelIndex&)'))
-        resize( Qt::Size.new(580, 380).expandedTo( minimumSizeHint() ) );
+        connect(@view.selectionModel, SIGNAL('currentChanged(const QModelIndex&, const QModelIndex&)'), self, SLOT('slotCurrentChanged(const QModelIndex&, const QModelIndex&)'))
+        resize( Qt::Size.new(700, 520).expandedTo( minimumSizeHint() ) );
+    end
+
+    def slotCurrentChanged(current, prev)
+        while @tab.count() > 0; @tab.removeTab(0); end
+        if current.valid?
+            #@label.text = @view.model().data(current, Qt::DisplayRole)
+            for i in 0..(current.internalPointer.pageCount() - 1)
+                page = current.internalPointer.page(i)
+                widget = page.createWidget(@tab)
+                if widget != nil
+                    @tab.addTab(widget, page.label)
+                end
+            end
+        end
     end
 
 end
 
-dialog = Dialog.new()
+dialog = Dialog.new
 dialog.exec()
