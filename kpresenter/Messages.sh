4bdd4929873e85574aaa6a5a9549ab1e40f9c79d
Stephan Kulow <coolo@kde.org>
2007-03-05 10:31:25 +0000

    hopefully works better
    svn path=/trunk/koffice/; revision=639548

--- kpresenter/Messages.sh
+++ kpresenter/Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-$EXTRACTRC *.ui *.rc >> rc.cpp || exit 11
-$XGETTEXT */*.cpp *.cpp *.h KPrAboutData.h -o $podir/kpresenter.pot
+$EXTRACTRC `find . -name "*.ui" -o -name "*.rc"` >> rc.cpp || exit 11
+$XGETTEXT */*.cpp *.cpp `find . -name "*.h"` -o $podir/kpresenter.pot
