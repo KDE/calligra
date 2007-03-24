45b503aed8ffa724cf538f7a82f95a49435f2cb6
Dag Andersen <danders@get2net.dk>
2007-03-24 11:07:20 +0000

    Fix crash, and meake the tests pass.
    svn path=/trunk/koffice/; revision=646015

--- kplato/tests/CalendarTester.cpp
+++ kplato/tests/CalendarTester.cpp
@@ -28,7 +28,7 @@ namespace KPlato
 {
 
 void CalendarTester::testSingleDay() {
-    Calendar *t = new Calendar("Test");
+    Calendar t("Test");
     QDate wdate(2006,1,2);
     DateTime before = DateTime(wdate.addDays(-1), QTime());
     DateTime after = DateTime(wdate.addDays(1), QTime());
@@ -38,51 +38,50 @@ void CalendarTester::testSingleDay() {
     DateTime wdt2(wdate, t2);
     CalendarDay *day = new CalendarDay(QDate(2006,1,2), CalendarDay::Working);
     day->addInterval(QPair<QTime, QTime>(t1, t2));
-    t->addDay(day);
-    QVERIFY(t->findDay(wdate) == day);
-    QVERIFY((t->firstAvailableAfter(after, after.addDays(10))).isValid() == false);
-    QVERIFY((t->firstAvailableBefore(before, before.addDays(-10))).isValid() == false);
+    t.addDay(day);
+    QVERIFY(t.findDay(wdate) == day);
     
-    QVERIFY(t->firstAvailableAfter(before,after).toString() == wdt1.toString());
-    QVERIFY(t->firstAvailableBefore(after, before).toString() == wdt2.toString());
+    QVERIFY(t.hasInterval(after, after.addDays(1)) == false);
+    QVERIFY(t.hasInterval(before, before.addDays(-1)) == false);
     
-    QVERIFY(t->hasInterval(before, after));
-    QVERIFY(t->hasInterval(after, before) == false);
+    QVERIFY(t.hasInterval(after, before) == false);
+    QVERIFY(t.hasInterval(before, after));
     
-    QVERIFY(t->hasInterval(after, after.addDays(1)) == false);
-    QVERIFY(t->hasInterval(before, before.addDays(-1)) == false);
+    QVERIFY((t.firstAvailableAfter(after, after.addDays(10))).isValid() == false);
+    QVERIFY((t.firstAvailableBefore(before, before.addDays(-10))).isValid() == false);
+    
+    QCOMPARE(t.firstAvailableAfter(before,after).toString(), wdt1.toString());
+    QCOMPARE(t.firstAvailableBefore(after, before).toString(), wdt2.toString());
     
     Duration e(0, 2, 0);
-    QVERIFY((t->effort(before, after)).toString() == e.toString());
-    delete t;
+    QVERIFY((t.effort(before, after)).toString() == e.toString());
 }
 
 void CalendarTester::testWeekdays() {
-    Calendar *t = new Calendar("Test");
+    Calendar t("Test");
     QDate wdate(2006,1,4); // wednesday
     DateTime before = DateTime(wdate.addDays(-2), QTime());
     DateTime after = DateTime(wdate.addDays(2), QTime());
     QTime t1(8,0,0);
     QTime t2(10,0,0);
     
-    CalendarDay *wd1 = t->weekday(2); // wednesday
+    CalendarDay *wd1 = t.weekday(Qt::Wednesday);
     QVERIFY(wd1 != 0);
     
     wd1->setState(CalendarDay::Working);
     wd1->addInterval(QPair<QTime, QTime>(t1, t2));
 
-    QVERIFY(t->firstAvailableAfter(before, after).toString() == QDateTime(QDate(2006, 1, 4), QTime(8,0,0)).toString());
-/*    QVERIFY((t->firstAvailableBefore(after, before)).toString() == QDateTime(QDate(2006, 1, 4), QTime(10,0,0)).toString());
+    QVERIFY(t.firstAvailableAfter(before, after).toString() == DateTime(QDate(2006, 1, 4), QTime(8,0,0)).toString());
+    QVERIFY((t.firstAvailableBefore(after, before)).toString() == DateTime(QDate(2006, 1, 4), QTime(10,0,0)).toString());
     
-    QVERIFY(t->firstAvailableAfter(after, DateTime(QDate(QDate(2006,1,14)), QTime())).toString() == QDateTime(QDate(2006, 1, 11), QTime(8,0,0)).toString());
-    QVERIFY(t->firstAvailableBefore(before, DateTime(QDate(2005,12,25), QTime())).toString() == QDateTime(QDate(2005, 12, 28), QTime(10,0,0)).toString());*/
-    delete t;
+    QVERIFY(t.firstAvailableAfter(after, DateTime(QDate(QDate(2006,1,14)), QTime())).toString() == DateTime(QDate(2006, 1, 11), QTime(8,0,0)).toString());
+    QVERIFY(t.firstAvailableBefore(before, DateTime(QDate(2005,12,25), QTime())).toString() == DateTime(QDate(2005, 12, 28), QTime(10,0,0)).toString());
 }
 
 void CalendarTester::testCalendarWithParent() {
-/*    Calendar t("Test 3");
     Calendar p("Test 3 parent");
-    t.setParent(&p);
+    Calendar t("Test 3");
+    t.setParentCal(&p);
     QDate wdate(2006,1,2);
     DateTime before = DateTime(wdate.addDays(-1), QTime());
     DateTime after = DateTime(wdate.addDays(1), QTime());
@@ -91,27 +90,30 @@ void CalendarTester::testCalendarWithParent() {
     DateTime wdt1(wdate, t1);
     DateTime wdt2(wdate, t2);
     
-    CalendarDay *day = new CalendarDay(QDate(2006,1,2), CalendarDay::Working);
+    CalendarDay *day = new CalendarDay(wdate, CalendarDay::Working);
     day->addInterval(QPair<QTime, QTime>(t1, t2));
     p.addDay(day);
     QVERIFY(p.findDay(wdate) == day);
     
-    // same tests as in testSingleDay()
-    QVERIFY((t.firstAvailableAfter(after, after.addDays(10))).isValid() == false);
-    QVERIFY((t.firstAvailableBefore(before, before.addDays(-10))).isValid() == false);
-    
-    QVERIFY(t.firstAvailableAfter(before,after).toString() == wdt1.toString());
-    QVERIFY(t.firstAvailableBefore(after, before).toString() == wdt2.toString());
-    
-    QVERIFY(t.hasInterval(before, after));
-    QVERIFY(t.hasInterval(after, before) == false);
-    
+    //same tests as in testSingleDay()
     QVERIFY(t.hasInterval(after, after.addDays(1)) == false);
     QVERIFY(t.hasInterval(before, before.addDays(-1)) == false);
     
+    QVERIFY(t.hasInterval(after, before) == false);
+    QVERIFY(t.hasInterval(before, after));
+    
+    QVERIFY((t.firstAvailableAfter(after, after.addDays(10))).isValid() == false);
+    QVERIFY((t.firstAvailableBefore(before, before.addDays(-10))).isValid() == false);
+    
+    QVERIFY(t.firstAvailableAfter(before, after).isValid());
+    QVERIFY(t.firstAvailableBefore(after, before).isValid());
+    
+    QCOMPARE(t.firstAvailableAfter(before,after).toString(), wdt1.toString());
+    QCOMPARE(t.firstAvailableBefore(after, before).toString(), wdt2.toString());
+    
     Duration e(0, 2, 0);
     QVERIFY((t.effort(before, after)).toString() == e.toString());
-    */
+    
 }
 
 } //namespace KPlato
