14eceb614cb521593a06c3aedcad5b03c19e228b
Sebastian Sauer <mail@dipe.org>
2007-03-28 19:54:59 +0000

    right, thx.
    svn path=/trunk/koffice/; revision=647589

--- kword/part/KWViewModeNormal.cpp
+++ kword/part/KWViewModeNormal.cpp
@@ -145,7 +145,7 @@ QPointF KWViewModeNormal::documentToView( const QPointF & point ) const {
     }
 
     QPointF offsetInPage(point.x(),  + point.y() - page->offsetInDocument());
-    Q_ASSERT( m_pageTops.contains(pageIndex) );
+    Q_ASSERT( pageIndex >= 0 && pageIndex < m_pageTops.count() );
     QPointF translated(x, m_pageTops[pageIndex]);
     return canvas()->viewConverter()->documentToView(translated + offsetInPage);
 }
