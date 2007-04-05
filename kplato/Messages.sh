cd2a30f8258748b5fce3c61939a218a335afc8e7
Script Kiddy <scripty@kde.org>
2007-04-05 12:53:44 +0000

    I suppose that rc.cpp is meant not rp.cpp (as otherwise it is not extracted) (goutte)
    svn path=/trunk/koffice/; revision=650795

--- kplato/Messages.sh
+++ kplato/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-$EXTRACTATTR --attr=Label,Text reports/*.ktf >>rp.cpp || exit 11
+$EXTRACTATTR --attr=Label,Text reports/*.ktf >> rc.cpp || exit 11
 $XGETTEXT rc.cpp *.cc kptaboutdata.h -o $podir/kplato.pot
 
