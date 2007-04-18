fae033b4f4b144013e9a4262059593844bd87b0e
Thomas Zander <zander@kde.org>
2007-04-18 16:00:34 +0000

    Some initial work for this diag
    svn path=/trunk/koffice/; revision=655509

---
+++ shapes/text/dialogs/StyleManager.h
@@ -0,0 +1,36 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2007 Thomas Zander <zander@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef STYLEMANAGER_H
+#define STYLEMANAGER_H
+
+#include "ui_StyleManager.h"
+
+#include <QWidget>
+
+class StyleManager : public QWidget {
+    Q_OBJECT
+public:
+    StyleManager(QWidget *parent = 0);
+
+private:
+    Ui::StyleManager widget;
+};
+
+#endif
