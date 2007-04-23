d2c2f6e22dfbbe98c3a5d126bb49b3c717a8e4f1
Jarosław Staniek <staniek@kde.org>
2007-04-23 14:24:42 +0000

    SVN_SILENT indent
    svn path=/branches/koffice/1.6/koffice/; revision=657195

--- kexi/kexidb/drivers/pqxx/pqxxconnection.cpp
+++ kexi/kexidb/drivers/pqxx/pqxxconnection.cpp
@@ -291,11 +291,11 @@ bool pqxxSqlConnection::drv_executeSQL( const QString& statement )
 		ok = true;
 	}
 	catch(const pqxx::sql_error& sqlerr) {
-			KexiDBDrvDbg << "pqxxSqlConnection::drv_executeSQL: sql_error exception - " << sqlerr.query().c_str() << endl;
-  }
+		KexiDBDrvDbg << "pqxxSqlConnection::drv_executeSQL: sql_error exception - " << sqlerr.query().c_str() << endl;
+	}
 	catch (const pqxx::broken_connection& bcerr) {
-			KexiDBDrvDbg << "pqxxSqlConnection::drv_executeSQL: broken_connection exception" << endl;
-  }
+		KexiDBDrvDbg << "pqxxSqlConnection::drv_executeSQL: broken_connection exception" << endl;
+	}
 	catch (const std::exception &e)
 	{
 		//If an error ocurred then put the error description into _dbError
