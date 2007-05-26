2d383fd44c5b3d9a8a55b99c9a77acf9614e4b14
Alfredo Beaumont Sainz <alfredo.beaumont@gmail.com>
2007-05-26 19:06:54 +0000

    Let ElementFactory create any kind of element, now that all of them build
    svn path=/trunk/koffice/; revision=668558

--- kformula/flake/ElementFactory.cpp
+++ kformula/flake/ElementFactory.cpp
@@ -1,7 +1,7 @@
 /* This file is part of the KDE project
    Copyright (C) 2003 Ulrich Kuettler <ulrich.kuettler@gmx.de>
                  2006 Martin Pfeiffer <hubipete@gmx.net>
-                 2006 Alfredo Beaumont Sainz <alfredo.beaumont@gmail.com>
+                 2006-2007 Alfredo Beaumont Sainz <alfredo.beaumont@gmail.com>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -23,28 +23,28 @@
 #include "kformuladefs.h"
 #include <kdebug.h>
 
-//#include "ActionElement.h"
-//#include "BracketElement.h"
-//#include "EncloseElement.h"
-//#include "ErrorElement.h"
+#include "ActionElement.h"
+#include "BracketElement.h"
+#include "EncloseElement.h"
+#include "ErrorElement.h"
 #include "FractionElement.h"
 #include "GlyphElement.h"
 #include "IdentifierElement.h"
-//#include "MatrixElement.h"
-//#include "MatrixRowElement.h"
-//#include "MatrixEntryElement.h"
-//#include "MultiscriptElement.h"
+#include "MatrixElement.h"
+#include "MatrixRowElement.h"
+#include "MatrixEntryElement.h"
+#include "MultiscriptElement.h"
 #include "NumberElement.h"
 #include "OperatorElement.h"
-//#include "PaddedElement.h"
-//#include "PhantomElement.h"
+#include "PaddedElement.h"
+#include "PhantomElement.h"
 #include "RootElement.h"
 #include "RowElement.h"
 #include "SpaceElement.h"
-//#include "StringElement.h"
-//#include "StyleElement.h"
+#include "StringElement.h"
+#include "StyleElement.h"
 #include "TextElement.h"
-//#include "UnderOverElement.h"
+#include "UnderOverElement.h"
 
 BasicElement* ElementFactory::createElement( const QString& tagName,
                                              BasicElement* parent )
@@ -58,8 +58,8 @@ BasicElement* ElementFactory::createElement( const QString& tagName,
         return new NumberElement( parent );
     else if ( tagName == "mtext" )
         return new TokenElement( parent );
-    //else if ( tagName == "ms" )
-    //        return new StringElement( parent );
+    else if ( tagName == "ms" )
+        return new StringElement( parent );
     else if ( tagName == "mspace" )
         return new SpaceElement( parent );
     else if ( tagName == "mglyph" )
@@ -70,24 +70,24 @@ BasicElement* ElementFactory::createElement( const QString& tagName,
           return new FractionElement( parent );
     else if ( tagName == "msqrt" || tagName == "mroot" )
           return new RootElement( parent );
-    //else if ( tagName == "mstyle" )
-    //      return new StyleElement( parent );
-    //else if ( tagName == "merror" )
-    //      return new ErrorElement( parent );
-    //else if ( tagName == "mpadded" )
-    //      return new PaddedElement( parent );
-    //else if ( tagName == "mphantom" )
-    //      return new PhantomElement( parent );
-    //else if ( tagName == "mfenced" )
-    //      return new BracketElement( parent );
-    //else if ( tagName == "menclose" )
-    //      return new EncloseElement( parent );
-    //else if ( tagName == "msub" || tagName == "msup" || tagName == "msubsup" )
-    //      return new MultiscriptElement( parent );
-    //else if ( tagName == "munder" || tagName == "mover" || tagName == "munderover" )
-    //      return new UnderOverElement( parent );
-    //else if ( tagName == "mmultiscripts" )
-    //      return new MultiscriptElement( parent );
+    else if ( tagName == "mstyle" )
+          return new StyleElement( parent );
+    else if ( tagName == "merror" )
+          return new ErrorElement( parent );
+    else if ( tagName == "mpadded" )
+          return new PaddedElement( parent );
+    else if ( tagName == "mphantom" )
+          return new PhantomElement( parent );
+    else if ( tagName == "mfenced" )
+          return new BracketElement( parent );
+    else if ( tagName == "menclose" )
+          return new EncloseElement( parent );
+    else if ( tagName == "msub" || tagName == "msup" || tagName == "msubsup" )
+          return new MultiscriptElement( parent );
+    else if ( tagName == "munder" || tagName == "mover" || tagName == "munderover" )
+          return new UnderOverElement( parent );
+    else if ( tagName == "mmultiscripts" )
+          return new MultiscriptElement( parent );
 
     kWarning( DEBUGID ) << "Do not know how to create the following element: " << tagName << endl;
     return 0;
