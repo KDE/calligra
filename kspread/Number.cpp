6435f17177aea00e01df5e48f61388baf1003719
Tomas Mecir <mecirt@gmail.com>
2007-06-02 14:15:44 +0000

    Implemented the Number class. It is a wrapper for double, pretty much, GnuMP support shall be here as well.
    The class currently isn't used anywhere, although this will change soon.
    
    
    svn path=/trunk/koffice/; revision=670714

--- kspread/Number.cpp
+++ kspread/Number.cpp
@@ -19,11 +19,282 @@
 
 #include "Number.h"
 
+#include <math.h>
+
 using namespace KSpread;
 
 class Number::Private : public QSharedData {
-  // TODO
+ public:
+  Private () {
+    type = Number::Float;
+    f = 0;
+  }
+
+  Private (const Private &o) : QSharedData (o), type (o.type)
+  {
+    switch (type)
+    {
+      case Number::Float:
+        f = o.f;
+        break;
+    }
+  }
+
+  Number::Type type;
+
+  union
+  {
+    double f;
+  };
+
+  private:
+    void operator=( const Number::Private& o );
 };
 
-// TODO
+// constructors
+Number::Number (int num)
+{
+  d->type = Number::Float;
+  d->f = (double) num;
+}
+
+Number::Number (double num)
+{
+  d->type = Number::Float;
+  d->f = num;
+}
+
+Number::Number (const Number& n)
+  : d (n.d)
+{
+}
+
+// set/get
+Number& Number::operator= (const Number &n)
+{
+  d = n.d;
+  return *this;
+}
+
+
+double Number::asFloat () const
+{
+  return d->f;
+}
+
+// basic operations
+Number Number::operator+ (const Number &n) const
+{
+  return Number (d->f + n.d->f);
+}
+
+Number Number::operator- (const Number &n) const
+{
+  return Number (d->f - n.d->f);
+}
+
+Number Number::operator* (const Number &n) const
+{
+  return Number (d->f * n.d->f);
+}
+
+Number Number::operator/ (const Number &n) const
+{
+  return Number (d->f / n.d->f);
+}
+
+Number Number::operator+ (double n) const
+{
+  return Number (d->f + n);
+}
+
+Number Number::operator- (double n) const
+{
+  return Number (d->f - n);
+}
+
+Number Number::operator* (double n) const
+{
+  return Number (d->f * n);
+}
+
+Number Number::operator/ (double n) const
+{
+  return Number (d->f / n);
+}
+
+void Number::operator+= (const Number &n)
+{
+  d->f += n.d->f;
+}
+
+void Number::operator-= (const Number &n)
+{
+  d->f -= n.d->f;
+}
+
+void Number::operator*= (const Number &n)
+{
+  d->f *= n.d->f;
+}
+
+void Number::operator/= (const Number &n)
+{
+  d->f /= n.d->f;
+}
+
+void Number::operator+= (double n)
+{
+  d->f += n;
+}
+
+void Number::operator-= (double n)
+{
+  d->f -= n;
+}
+
+void Number::operator*= (double n)
+{
+  d->f *= n;
+}
+
+void Number::operator/= (double n)
+{
+  d->f /= n;
+}
+
+// comparison
+bool Number::operator<= (const Number &n) const
+{
+  return (d->f <= n.d->f);
+}
+
+bool Number::operator< (const Number &n) const
+{
+  return (d->f < n.d->f);
+}
+
+bool Number::operator== (const Number &n) const
+{
+  return (d->f == n.d->f);
+}
+
+bool Number::operator<= (double n) const
+{
+  return (d->f <= n);
+}
+
+bool Number::operator< (double n) const
+{
+  return (d->f < n);
+}
+
+bool Number::operator== (double n) const
+{
+  return (d->f == n);
+}
+
+Number Number::abs () const
+{
+  return Number (fabs (d->f));
+}
+
+Number Number::neg () const
+{
+  return Number (-1 * d->f);
+}
+
+Number Number::pow (const Number &exp) const
+{
+  return Number (::pow (d->f, exp.d->f));
+}
+
+Number Number::pow (double exp) const
+{
+  return Number (::pow (d->f, exp));
+}
+
+Number Number::log (double base) const
+{
+  double logbase = log10 (base);
+  return Number (log10 (d->f) / logbase);
+}
+
+Number Number::ln () const
+{
+  return Number (::log (d->f));
+}
+
+// goniometric functions
+Number Number::sin () const
+{
+  return Number (::sin (d->f));
+}
+
+Number Number::cos () const
+{
+  return Number (::cos (d->f));
+}
+
+Number Number::tg () const
+{
+  return Number (::tan (d->f));
+}
+
+Number Number::cotg () const
+{
+  return Number (1 / ::tan (d->f));
+}
+
+Number Number::asin () const
+{
+  return Number (::asin (d->f));
+}
+
+Number Number::acos () const
+{
+  return Number (::acos (d->f));
+}
+
+Number Number::atg () const
+{
+  return Number (::atan (d->f));
+}
+
+Number Number::atan2 (const Number &y, const Number &x)
+{
+  return Number (::atan2 (y.d->f, x.d->f));
+}
+
+
+// hyperbolic functions
+Number Number::sinh () const
+{
+  return Number (::sinh (d->f));
+}
+
+Number Number::cosh () const
+{
+  return Number (::cosh (d->f));
+}
+
+Number Number::tgh () const
+{
+  return Number (::tanh (d->f));
+}
+
+Number Number::asinh () const
+{
+  return Number (::asinh (d->f));
+}
+
+Number Number::acosh () const
+{
+  return Number (::acosh (d->f));
+}
+
+Number Number::atgh () const
+{
+  return Number (::atanh (d->f));
+}
 
--- kspread/Number.h
+++ kspread/Number.h
@@ -24,24 +24,148 @@
 
 #include "kspread_export.h"
 
+#include <complex>
+
+using namespace std;
+
 namespace KSpread {
 
 /**
-The Number class holds a single number. The class will support integers, doubles, complex numbers, and eventually also high-precision numbers from GnuMP.
+The Number class holds a single floating-point number. At the moment, it's just a wrapper for double, but it's going to support GnuMP or something eventually.
 
-The class will be able to format itself into a string, using provided locale settings.
+The class is made so that if high precision is not desired, a "typedef Number double" will revert us back to doubles.
 
-Out-of-class methods for computations will be provided, so that objects of the class may be used just like regular number variables, and so that the caller needs not worry whether the number is an integer, double, complex, or anything else.
+The class will be able to format itself into a string, using provided locale settings. (TODO: how to handle this so that parsing/formatting works even if we typedef this class out?)
+
+Out-of-class methods for computations are provided
 */
 
 class KSPREAD_EXPORT Number {
   public:
-    // TODO
+    enum Type {
+      Float  // GnuMP will be here as well, eventually
+    };
+
+    // constructors
+    Number (int num);
+    Number (double num);
+
+    Number (const Number& n);
+
+    // set/get
+    Number& operator= (const Number &n);
+
+    double asFloat () const;
+
+    // basic operations
+    Number operator+ (const Number &n) const;
+    Number operator- (const Number &n) const;
+    Number operator* (const Number &n) const;
+    Number operator/ (const Number &n) const;
+    Number operator+ (double n) const;
+    Number operator- (double n) const;
+    Number operator* (double n) const;
+    Number operator/ (double n) const;
+
+    void operator+= (const Number &n);
+    void operator-= (const Number &n);
+    void operator*= (const Number &n);
+    void operator/= (const Number &n);
+    void operator+= (double n);
+    void operator-= (double n);
+    void operator*= (double n);
+    void operator/= (double n);
+
+    void operator++ () { return operator+= (1); };
+    void operator-- () { return operator-= (1); };
+
+    // comparison
+    bool operator<= (const Number &n) const;
+    bool operator< (const Number &n) const;
+    bool operator== (const Number &n) const;
+    bool operator!= (const Number &n) const { return (!operator== (n)); };
+    bool operator>= (const Number &n) const { return (!operator< (n)); };
+    bool operator> (const Number &n) const { return (!operator<= (n)); };
+    bool operator<= (double n) const;
+    bool operator< (double n) const;
+    bool operator== (double n) const;
+    bool operator!= (double n) const { return (!operator== (n)); };
+    bool operator>= (double n) const { return (!operator< (n)); };
+    bool operator> (double n) const { return (!operator<= (n)); };
+
+    // absolute value
+    Number abs () const;
+    // negative value
+    Number neg () const;
+    // power
+    Number pow (const Number &exp) const;
+    Number pow (double exp) const;
+    // logarithms
+    Number log (double base) const;
+    Number ln () const;
+
+    // goniometric functions
+    Number sin () const;
+    Number cos () const;
+    Number tg () const;
+    Number cotg () const;
+    Number asin () const;
+    Number acos () const;
+    Number atg () const;
+    static Number atan2 (const Number &y, const Number &x);
+
+    // hyperbolic functions
+    Number sinh () const;
+    Number cosh () const;
+    Number tgh () const;
+    Number asinh () const;
+    Number acosh () const;
+    Number atgh () const;
+
+    // TODO: add more functions, as needed
+
+    // TODO: functions to output the number to a string
+
   private:
     class Private;
     QSharedDataPointer<Private> d;
 
 };  // class Number
+
+// external operators, so that we can do things like 4+a without having to create temporary objects
+// not provided for complex numbers, as we won't be using them often like that
+Number operator+ (double n1, const Number &n2) { return n2 + n1; };
+Number operator- (double n1, const Number &n2) { return (n2 - n1).neg(); };
+Number operator* (double n1, const Number &n2) { return n2 * n1; };
+Number operator/ (double n1, const Number &n2) { return Number(n1) / n2; /* TODO optimise perhaps */ };
+bool operator<= (double n1, const Number &n2) { return (n2 >= n1); };
+bool operator< (double n1, const Number &n2) { return (n2 > n1); };
+bool operator== (double n1, const Number &n2) { return (n2 == n1); };
+bool operator!= (double n1, const Number &n2) { return (n2 != n1); };
+bool operator>= (double n1, const Number &n2) { return (n2 <= n1); };
+bool operator> (double n1, const Number &n2) { return (n2 < n1); };
+
+// external versions of the functions
+Number abs (const Number &n) { return n.abs(); };
+Number neg (const Number &n) { return n.neg(); };
+Number pow (const Number &n, const Number &exp) { return n.pow (exp); };
+Number pow (const Number &n, double exp) { return n.pow (exp); };
+Number log (const Number &n, double base) { return n.log (base); };
+Number ln (const Number &n) { return n.ln (); };
+Number sin (const Number &n) { return n.sin(); };
+Number cos (const Number &n) { return n.cos(); };
+Number tg (const Number &n) { return n.tg(); };
+Number cotg (const Number &n) { return n.cotg(); };
+Number asin (const Number &n) { return n.asin(); };
+Number acos (const Number &n) { return n.acos(); };
+Number atg (const Number &n) { return n.atg(); };
+Number atan2 (const Number &y, const Number &x) { return Number::atan2(y, x); };
+Number sinh (const Number &n) { return n.sinh(); };
+Number cosh (const Number &n) { return n.cosh(); };
+Number tgh (const Number &n) { return n.tg(); };
+Number asinh (const Number &n) { return n.asinh(); };
+Number acosh (const Number &n) { return n.acosh(); };
+Number atgh (const Number &n) { return n.atgh(); };
 
 } // namespace KSpread
 
