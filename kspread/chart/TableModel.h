a6e560d1f97bf9c00050b24606104cffcaa87518
Stefan Nikolaus <stefan.nikolaus@kdemail.net>
2007-06-09 17:07:37 +0000

    Embedding	Chart embedding. 		Add a basic table model for a specific cell region.
    svn path=/trunk/koffice/; revision=673267

---
+++ kspread/chart/TableModel.h
@@ -0,0 +1,59 @@
+/* This file is part of the KDE project
+   Copyright 2007 Stefan Nikolaus <stefan.nikolaus@kdemail.net>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KSPREAD_TABLE_MODEL
+#define KSPREAD_TABLE_MODEL
+
+#include <QAbstractTableModel>
+#include <QSharedDataPointer>
+#include <QVariant>
+
+namespace KSpread
+{
+class Region;
+
+/**
+ * Abstracts the access to the ValueStorage.
+ * Useful for KChart (or other apps, that want to access KSpread's data).
+ */
+class TableModel : public QAbstractTableModel
+{
+    Q_OBJECT
+
+public:
+    TableModel( QObject* parent = 0 );
+    ~TableModel();
+
+    virtual QVariant headerData( int section, Qt::Orientation orientation, int role = Qt::DisplayRole ) const;
+    virtual int rowCount( const QModelIndex& parent = QModelIndex() ) const;
+    virtual int columnCount( const QModelIndex& parent = QModelIndex() ) const;
+    virtual QVariant data( const QModelIndex& index, int role = Qt::DisplayRole ) const;
+
+    void setRegion( const Region& region );
+
+private:
+    class Private;
+    QSharedDataPointer<Private> d;
+};
+
+} // namespace KSpread
+
+Q_DECLARE_TYPEINFO( KSpread::TableModel, Q_MOVABLE_TYPE );
+
+#endif // KSPREAD_TABLE_MODEL
