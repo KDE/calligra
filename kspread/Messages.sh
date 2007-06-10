eca4a08ba0f0359643d00ce0e6cbd18e03a9597f
Stephan Kulow <coolo@kde.org>
2007-06-10 09:41:24 +0000

    you need to be careful when moving sources
    svn path=/trunk/koffice/; revision=673452

--- kspread/Messages.sh
+++ kspread/Messages.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 $EXTRACTRC --tag-group=koffice extensions/*.xml > xml_doc.cpp
 $EXTRACTRC dialogs/*.ui >> rc.cpp
-$XGETTEXT *.cpp AboutData.h dialogs/*.cc dialogs/*.cpp tests/*.cc tests/*.cpp -o $podir/kspread.pot
+$XGETTEXT *.cpp ui/*.h ui/*.cpp dialogs/*.cc dialogs/*.cpp tests/*.cc tests/*.cpp -o $podir/kspread.pot
 rm xml_doc.cpp
 
