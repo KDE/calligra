237f956e642f7930b809ded4a4cf0a139259a56d
Laurent Montel <montel@kde.org>
2007-06-23 09:59:52 +0000

    Fix extract i18n from .h file
    svn path=/trunk/koffice/; revision=679195

--- kplato/Messages.sh
+++ kplato/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTATTR --attr=Label,Text reports/*.ktf >> rc.cpp || exit 11
-$XGETTEXT rc.cpp *.cc kptaboutdata.h -o $podir/kplato.pot
+$XGETTEXT rc.cpp *.cc kptglobal.h kptcalendar.h kptaboutdata.h -o $podir/kplato.pot
 
