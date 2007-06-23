bfc012694ce85d757c81587830a8111031d20efb
Laurent Montel <montel@kde.org>
2007-06-23 10:05:20 +0000

    Fix extract i18n into .h files
    svn path=/trunk/koffice/; revision=679198

--- kspread/Messages.sh
+++ kspread/Messages.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 $EXTRACTRC --tag-group=koffice extensions/*.xml > xml_doc.cpp
 $EXTRACTRC dialogs/*.ui >> rc.cpp
-$XGETTEXT *.cpp chart/*.cpp commands/*.cpp database/*.cpp dialogs/*.cpp functions/*.cpp ui/AboutData.h ui/*.cpp -o $podir/kspread.pot
+$XGETTEXT Object.h plugins/scripting/ScriptingWriter.h *.cpp chart/*.cpp commands/*.cpp database/*.cpp dialogs/*.cpp functions/*.cpp ui/AboutData.h ui/*.cpp -o $podir/kspread.pot
 rm xml_doc.cpp
 
