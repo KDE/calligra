7a88fb6a7a2f9043db1621ab55075fa17231b853
Laurent Montel <montel@kde.org>
2007-08-09 20:32:50 +0000

    there is not .cc files
    svn path=/trunk/koffice/; revision=698317

--- shapes/text/Messages.sh
+++ shapes/text/Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 $EXTRACTRC `find . -name \*.ui` > rc.cpp || exit 11
-$XGETTEXT `find . -name \*.cpp -o -name \*.cc` -o $podir/TextShape.pot
+$XGETTEXT `find . -name \*.cpp ` -o $podir/TextShape.pot
