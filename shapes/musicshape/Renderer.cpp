a355daa84c6f79e052ec5e1d729fb405031041d0
Marijn Kruisselbrink <m.kruisselbrink@student.tue.nl>
2007-08-16 17:28:17 +0000

    SVN_SILENT oops, didn't mean to commit that
    svn path=/trunk/koffice/; revision=700879

--- shapes/musicshape/Renderer.cpp
+++ shapes/musicshape/Renderer.cpp
@@ -34,7 +34,7 @@
 
 using namespace MusicCore;
 
-MusicRenderer::MusicRenderer(MusicStyle* style) : m_style(style), m_debug(true)
+MusicRenderer::MusicRenderer(MusicStyle* style) : m_style(style), m_debug(false)
 {
 }
 
