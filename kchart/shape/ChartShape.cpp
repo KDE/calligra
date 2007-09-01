d1ef9311e6609d24a6f362456a71bf1d006deaf7
Inge Wallin <inge@lysator.liu.se>
2007-09-01 15:39:18 +0000

    Add copyright
    svn path=/trunk/koffice/; revision=707343

--- kchart/shape/ChartShape.cpp
+++ kchart/shape/ChartShape.cpp
@@ -1,5 +1,7 @@
 /* This file is part of the KDE project
+
    Copyright 2007 Stefan Nikolaus <stefan.nikolaus@kdemail.net>
+   Copyright 2007 Inge Wallin     <inge@lysator.liu.se>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
