a48f7f6642e00f008991ebe43fb8df21052ba314
Stefan Nikolaus <stefan.nikolaus@kdemail.net>
2007-09-13 14:39:54 +0000

    The extensions/ dir was renamed to functions/ long ago. Honor that in the message extraction. SVN_SILENT
    svn path=/trunk/koffice/; revision=712082

--- kspread/Messages.sh
+++ kspread/Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-$EXTRACTRC --tag-group=koffice extensions/*.xml > xml_doc.cpp
+$EXTRACTRC --tag-group=koffice functions/*.xml > xml_doc.cpp
 $EXTRACTRC dialogs/*.ui >> rc.cpp
 $XGETTEXT Object.h plugins/scripting/ScriptingWriter.h *.cpp chart/*.cpp commands/*.cpp database/*.cpp dialogs/*.cpp functions/*.cpp ui/AboutData.h ui/*.cpp -o $podir/kspread.pot
 rm xml_doc.cpp
