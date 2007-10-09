790e4cc67da6ffd07c0fdef3f02d811dadf2e31f
Thomas Zander <zander@kde.org>
2007-10-09 08:12:48 +0000

    cleanup
    svn path=/trunk/koffice/; revision=723293

--- kword/part/KWView.cpp
+++ kword/part/KWView.cpp
@@ -53,7 +53,6 @@
 #include <KoZoomAction.h>
 #include <KoToolManager.h>
 #include <KoMainWindow.h>
-#include <KoToolBox.h>
 #include <KoTextShapeData.h>
 #include <KoTextSelectionHandler.h>
 #include <KoInlineObjectRegistry.h>
