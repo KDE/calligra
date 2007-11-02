99f1577347417ee97bde49b5c354cdf02b704aba
Burkhard Lück <lueck@hube-lueck.de>
2007-11-02 12:36:08 +0000

    make some menu strings translatable
    svn path=/trunk/koffice/; revision=731973

--- kword/Messages.sh
+++ kword/Messages.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 $EXTRACTRC --tag-group=koffice expression/*.xml > xml_doc.cpp
-$EXTRACTRC `find mailmerge part plugins -name \*.ui` >> rc.cpp
+$EXTRACTRC `find mailmerge part plugins -name \*.ui -o -name \*.rc` >> rc.cpp
 $XGETTEXT rc.cpp *.cpp `find mailmerge part plugins -name \*.cpp -o -name \*.cpp -o -name \*.h | egrep -v "/old"` -o $podir/kword.pot
 rm xml_doc.cpp
 
