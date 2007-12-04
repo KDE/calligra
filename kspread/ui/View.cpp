31cd3960de29b6cf372405c72e973a6f4734c238
Sebastian Sauer <mail@dipe.org>
2007-12-04 00:12:43 +0000

    added the per-view setup for fullscreen to KoView what means, that each ko-app has it now + add it to the view-menu of kword and kpresenter as well, no idea there if other ko-apps like to have it direct visible in there menu as well. I also reordered the view-menu of kpresenter so that zoom+fullscreen are displayed at the bottom of the menu like at kspread and kword - hope that's ok with you zagge (else please just revert :)
    svn path=/trunk/koffice/; revision=744634

--- kspread/ui/View.cpp
+++ kspread/ui/View.cpp
@@ -532,8 +532,6 @@ void View::Private::initActions()
 
     KActionCollection* ac = view->actionCollection();
 
-  ac->addAction("view_fullscreen", view->shell()->actionCollection()->action("view_fullscreen"));
-
   // -- cell formatting actions --
 
   actions->cellLayout  = new KAction(KIcon( "cell_layout" ), i18n("Cell Format..."), view);
