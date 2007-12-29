f36d77dffa91abf872b983ffa8222a7d5bb43caf
Jan Hambrecht <jaham@gmx.net>
2007-12-29 00:17:09 +0000

    make it possible to set a resource server on the adapter after connecting to the adapters signals to not miss the resources which are already loaded by the server
    svn path=/trunk/koffice/; revision=754061

--- karbon/widgets/vgradienttabwidget.cc
+++ karbon/widgets/vgradienttabwidget.cc
@@ -461,9 +461,10 @@ void VGradientTabWidget::setResourceServer( KoResourceServer<KoAbstractGradient>
 
     if( m_resourceServer )
     {
-        m_resourceAdapter = new KoResourceServerAdapter<KoAbstractGradient>( m_resourceServer );
+        m_resourceAdapter = new KoResourceServerAdapter<KoAbstractGradient>( 0 );
         connect( m_resourceAdapter, SIGNAL(resourceAdded(KoResource*)), 
                  this, SLOT(addResource(KoResource*)));
+        m_resourceAdapter->setResourceServer( m_resourceServer );
     }
 }
 
