72064acf719c65d76bd7fdd1daaed96b13c4c1d3
Jan Hambrecht <jaham@gmx.net>
2008-01-19 17:37:07 +0000

    adapt to directory restructuring
    svn path=/trunk/koffice/; revision=763524

--- karbon/Messages.sh
+++ karbon/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC data/*.rc >> rc.cpp
-$XGETTEXT rc.cpp *.h *.cpp *.cc */*.cc widgets/*.cpp plugins/*/*.cc -o $podir/karbon.pot
+$XGETTEXT rc.cpp *.h *.cpp *.cc */*.cc common/*/*.cpp ui/*/*.cpp plugins/*/*.cpp -o $podir/karbon.pot
 
