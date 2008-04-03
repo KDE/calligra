94f32be9ab64c9655a1244026f9822e80788c65f
Adam Pigg <adam@piggz.co.uk>
2008-04-03 07:42:48 +0000

    Added missing licenses
    svn path=/trunk/koffice/; revision=793209

--- kexi/plugins/reportspgz/backend/common/krdetailsectiondata.h
+++ kexi/plugins/reportspgz/backend/common/krdetailsectiondata.h
@@ -1,14 +1,24 @@
-//
-// C++ Interface: krdetailsectiondata
-//
-// Description:
-//
-//
-// Author: Adam Pigg <adam@piggz.co.uk>, (C) 2008
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/*
+ * Kexi Report Plugin /
+ * OpenRPT report writer and rendering engine
+ * Copyright (C) 2001-2007 by OpenMFG, LLC
+ * Copyright (C) 2007-2008 by Adam Pigg (adam@piggz.co.uk)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * Please contact info@openmfg.com with any questions on this license.
+ */
 #ifndef KRDETAILSECTIONDATA_H
 #define KRDETAILSECTIONDATA_H
 
