e8b6fbcd7bf7d6043ac42b59f1f80f932447daee
Jan Hambrecht <jaham@gmx.net>
2008-05-17 16:38:10 +0000

    SVN_SILENT copyright fixes
    svn path=/trunk/koffice/; revision=808817

--- filters/karbon/svg/svgimport.cc
+++ filters/karbon/svg/svgimport.cc
@@ -1,22 +1,27 @@
 /* This file is part of the KDE project
-   Copyright (C) 2002, 2003, The Karbon Developers
-   Copyright (C) 2007 Jan Hambrecht <jaham@gmx.net>
+ * Copyright (C) 2002-2005,2007 Rob Buis <buis@kde.org>
+ * Copyright (C) 2002-2004 Nicolas Goutte <nicolasg@snafu.de>
+ * Copyright (C) 2005-2006 Tim Beaulen <tbscope@gmail.com>
+ * Copyright (C) 2005-2008 Jan Hambrecht <jaham@gmx.net>
+ * Copyright (C) 2005,2007 Thomas Zander <zander@kde.org>
+ * Copyright (C) 2006-2007 Inge Wallin <inge@lysator.liu.se>
+ * Copyright (C) 2007-2008 Thorsten Zachmann <t.zachmann@zagge.de>
 
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
-*/
+ */
 
 #include "svgimport.h"
 #include "color.h"
--- filters/karbon/svg/svgimport.h
+++ filters/karbon/svg/svgimport.h
@@ -1,22 +1,23 @@
 /* This file is part of the KDE project
-   Copyright (C) 2002, The Karbon Developers
-   Copyright (C) 2007 Jan Hambrecht <jaham@gmx.net>
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Copyright (C) 2002-2003,2005 Rob Buis <buis@kde.org>
+ * Copyright (C) 2005-2006 Tim Beaulen <tbscope@gmail.com>
+ * Copyright (C) 2005,2007-2008 Jan Hambrecht <jaham@gmx.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
-*/
+ */
 
 #ifndef SVGIMPORT_H
 #define SVGIMPORT_H
