42ba91e63cca5cb176f4bbe834168ef6d052a960
David Faure <faure@kde.org>
2008-06-03 08:28:17 +0000

    RELICENSE: GPL v2 -> GPL v2+v3+e.V. as discussed with Werner CCMAIL: trobin@kde.org
    svn path=/trunk/koffice/; revision=816077

--- filters/kword/msword/document.h
+++ filters/kword/msword/document.h
@@ -4,7 +4,9 @@
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
-   License version 2 as published by the Free Software Foundation.
+   version 2 of the License, or (at your option) version 3 or,
+   at the discretion of KDE e.V (which shall act as a proxy as in
+   section 14 of the GPLv3), any later version..
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
