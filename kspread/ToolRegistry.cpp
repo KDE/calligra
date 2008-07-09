e846e3781983572172cbfae63519ff6d76ea9752
Stefan Nikolaus <stefan.nikolaus@kdemail.net>
2008-07-09 17:01:21 +0000

    Plugins		Add a tool registry, that manages cell tool derivatives.
    (r830048 continued; missed these files)
    
    svn path=/trunk/koffice/; revision=830058

---
+++ kspread/ToolRegistry.cpp
@@ -0,0 +1,103 @@
+/* This file is part of the KDE project
+   Copyright 2008 Stefan Nikolaus <stefan.nikolaus@kdemail.net>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "ToolRegistry.h"
+
+#include "DefaultTool.h"
+#include "DefaultToolFactory.h"
+
+#include <KGlobal>
+#include <KPluginInfo>
+#include <KServiceTypeTrader>
+
+#include <KoToolRegistry.h>
+
+using namespace KSpread;
+
+class ToolRegistrySingleton
+{
+public:
+    ToolRegistry instance;
+};
+K_GLOBAL_STATIC(ToolRegistrySingleton, s_singleton)
+
+
+class ToolRegistry::Private
+{
+public:
+};
+
+
+ToolRegistry::ToolRegistry()
+    : d(new Private)
+{
+    // Add the built-in cell tool.
+    KoToolRegistry::instance()->add(new DefaultToolFactory(this, "KSpreadDefaultToolId", i18n("Cell Tool")));
+    // Load the tool plugins.
+    loadTools();
+}
+
+ToolRegistry::~ToolRegistry()
+{
+    delete d;
+}
+
+ToolRegistry* ToolRegistry::instance()
+{
+    return &s_singleton->instance;
+}
+
+void ToolRegistry::loadTools()
+{
+    const QString serviceType = QString::fromLatin1("KSpread/Plugin");
+    const QString query = QString::fromLatin1("([X-KSpread-Version] >= 2) and "
+                                              "([X-KDE-PluginInfo-Category] == 'Tool')");
+    const KService::List offers = KServiceTypeTrader::self()->query(serviceType, query);
+    const KConfigGroup moduleGroup = KGlobal::config()->group("Plugins");
+    const KPluginInfo::List pluginInfos = KPluginInfo::fromServices(offers, moduleGroup);
+    foreach (KPluginInfo pluginInfo, pluginInfos) {
+        KPluginFactory *factory = KPluginLoader(*pluginInfo.service()).factory();
+        if (!factory) {
+            kDebug(36002) << "Unable to create plugin factory for" << pluginInfo.name();
+            continue;
+        }
+        DefaultToolFactory* toolFactory = factory->create<DefaultToolFactory>(this);
+        if (!toolFactory) {
+            kDebug(36002) << "Unable to create tool factory for" << pluginInfo.name();
+            continue;
+        }
+        pluginInfo.load(); // load activation state
+        if (pluginInfo.isPluginEnabled()) {
+            // Tool already registered?
+            if (KoToolRegistry::instance()->contains(toolFactory->id())) {
+                continue;
+            }
+            toolFactory->setIcon(pluginInfo.service()->icon());
+            toolFactory->setToolTip(pluginInfo.service()->comment());
+            KoToolRegistry::instance()->add(toolFactory);
+        } else {
+            // Tool not registered?
+            if (!KoToolRegistry::instance()->contains(toolFactory->id())) {
+                continue;
+            }
+            delete KoToolRegistry::instance()->value(toolFactory->id());
+            KoToolRegistry::instance()->remove(toolFactory->id());
+        }
+    }
+}
---
+++ kspread/ToolRegistry.h
@@ -0,0 +1,61 @@
+/* This file is part of the KDE project
+   Copyright 2008 Stefan Nikolaus <stefan.nikolaus@kdemail.net>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KSPREAD_TOOL_REGISTRY
+#define KSPREAD_TOOL_REGISTRY
+
+#include <QObject>
+
+#include "kspread_export.h"
+
+namespace KSpread
+{
+
+/**
+ * Registry for tools.
+ */
+class KSPREAD_EXPORT ToolRegistry : public QObject
+{
+public:
+    /**
+     * Creates the registry.
+     */
+    ToolRegistry();
+    ~ToolRegistry();
+
+    /**
+     * \return the singleton instance
+     */
+    static ToolRegistry* instance();
+
+    /**
+     * Loads the tools.
+     * Depending on their activation state read from the config,
+     * the tools are added or removed from the registry.
+     */
+    void loadTools();
+
+private:
+    class Private;
+    Private * const d;
+};
+
+} // namespace KSpread
+
+#endif // KSPREAD_TOOL_REGISTRY
