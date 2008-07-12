9944a2e8d4e808fd493191412b9d63255fe3e7eb
Stefan Nikolaus <stefan.nikolaus@kdemail.net>
2008-07-12 15:11:52 +0000

    These ui files also belong to the files to be translated.
    svn path=/trunk/koffice/; revision=831380

--- kspread/Messages.sh
+++ kspread/Messages.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 $EXTRACTRC --tag-group=koffice functions/*.xml > xml_doc.cpp
-$EXTRACTRC dialogs/*.ui >> rc.cpp
+$EXTRACTRC chart/*.ui dialogs/*.ui ui/*.ui >> rc.cpp
 $XGETTEXT Object.h plugins/scripting/ScriptingWriter.h *.cpp chart/*.cpp commands/*.cpp database/*.cpp dialogs/*.cpp functions/*.cpp part/AboutData.h part/*.cpp ui/*.cpp -o $podir/kspread.pot
 rm xml_doc.cpp
 
