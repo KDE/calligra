c19135e27a22b32c28cee4808ad92722f0e5180c
Stephan Kulow <coolo@kde.org>
2008-07-15 11:38:44 +0000

    Object.h does not exist
    svn path=/trunk/koffice/; revision=832756

--- kspread/Messages.sh
+++ kspread/Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 $EXTRACTRC --tag-group=koffice functions/*.xml > xml_doc.cpp
 $EXTRACTRC chart/*.ui dialogs/*.ui  part/dialogs/*.ui ui/*.ui >> rc.cpp
-$XGETTEXT Object.h *.cpp chart/*.cpp commands/*.cpp database/*.cpp dialogs/*.cpp functions/*.cpp part/AboutData.h part/*.cpp part/commands/*.cpp part/dialogs/*.cpp ui/*.cpp -o $podir/kspread.pot
+$XGETTEXT *.cpp chart/*.cpp commands/*.cpp database/*.cpp dialogs/*.cpp functions/*.cpp part/AboutData.h part/*.cpp part/commands/*.cpp part/dialogs/*.cpp ui/*.cpp -o $podir/kspread.pot
 rm xml_doc.cpp
