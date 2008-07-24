b52f5427c6a7f3cec006f929f585d47eeded6bf7
Lorenzo Villani <lvillani@binaryhelix.net>
2008-07-24 13:32:49 +0000

    * restored Read.cpp Read.h
    svn path=/trunk/koffice/; revision=837343

---
+++ kexi/webforms/view/Read.cpp
@@ -0,0 +1,142 @@
+/* This file is part of the KDE project
+
+   (C) Copyright 2008 by Lorenzo Villani <lvillani@binaryhelix.net>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include <QHash>
+#include <QString>
+#include <QTextDocument>
+
+#include <pion/net/HTTPResponseWriter.hpp>
+
+#include <KDebug>
+
+#include <kexidb/cursor.h>
+#include <kexidb/connection.h>
+#include <kexidb/indexschema.h>
+
+#include "model/DataProvider.h"
+#include "TemplateProvider.h"
+
+#include "Read.h"
+
+namespace KexiWebForms {
+namespace View {
+
+    void Read::view(const QHash<QString, QString>& d, pion::net::HTTPResponseWriterPtr writer) {
+        QString requestedTable(d["uri-requested"]);
+
+        QString tableData;
+        KexiDB::TableSchema* tableSchema = gConnection->tableSchema(requestedTable);
+        KexiDB::QuerySchema querySchema(*tableSchema);
+        KexiDB::Cursor* cursor = gConnection->executeQuery(querySchema);
+
+        bool readOnly = (querySchema.connection() && querySchema.connection()->isReadOnly());
+        if (readOnly) {
+            setValue("TABLENAME", requestedTable.append(" (read only)"));
+        } else {
+            setValue("TABLENAME", requestedTable);
+        }
+
+        /* awful */
+        int recordsTotal = 0;
+        while (cursor->moveNext())
+            recordsTotal++;
+        gConnection->deleteCursor(cursor);
+
+        /* even more awful */
+        cursor = gConnection->executeQuery(querySchema);
+
+        if (!cursor) {
+            setValue("ERROR", "Unable to execute the query (" __FILE__ ")");
+        } else if (tableSchema->primaryKey()->fields()->isEmpty()) {
+            setValue("ERROR", "This table has no primary key!");
+        } else {
+            KexiDB::Field* primaryKey = tableSchema->primaryKey()->field(0);
+
+            // Create labels with field name
+            tableData.append("<tr>\t<th scope=\"col\">Record</th>\n");
+            for (uint i = 0; i < cursor->fieldCount(); i++) {
+                tableData.append(QString("\t<th scope=\"col\">%1</th>\n")
+                    .arg(querySchema.field(i)->captionOrName()));
+            }
+            tableData.append("</tr>\n");
+
+
+            // Create labels with fields data
+            int currentRecord = 0;
+            QString totalRecords(QVariant(recordsTotal).toString());
+            while (cursor->moveNext()) {
+                currentRecord++;
+
+                tableData.append(QString("<tr><td>%1 of %2</td>").arg(QVariant(currentRecord).toString())
+                    .arg(totalRecords));
+
+                QString pkeyVal(cursor->value(tableSchema->indexOf(primaryKey)).toString());
+
+                for (uint i = 0; i < cursor->fieldCount(); i++) {
+                    tableData.append("<td>");
+
+                    //
+                    // Use Kexi functions to retrieve and represent the Value
+                    //! @todo use Kexi the same functions for rendering values as Kexi table and form view
+                    //
+                    KexiDB::Field* field = querySchema.field(i);
+                    const KexiDB::Field::Type type = field->type();
+                    QString valueString;
+                    if (type == KexiDB::Field::BLOB) {
+                        valueString = QString("<img src=\"/blob/%1/%2/%3/%4\" alt=\"Image\"/>")
+                            .arg(requestedTable).arg(field->name()).arg(primaryKey->name())
+                            .arg(pkeyVal);
+                    }
+                    else if (field->isTextType()) {
+                        valueString = Qt::escape( cursor->value(i).toString() );
+                    }
+                    else {
+                        valueString = Qt::escape( cursor->value(i).toString() );
+                    }
+                    tableData.append(valueString);
+                    tableData.append("</td>");
+                }
+                // Toolbox
+                if (!readOnly) {
+                    // Edit
+                    tableData.append(QString("<td><a href=\"/update/%1/%2/%3\">"
+                        "<img src=\"/f/toolbox/draw-freehand.png\" alt=\"Edit\"/></a></td>")
+                        .arg(requestedTable).arg(primaryKey->name()).arg(pkeyVal));
+                    // Delete
+                    tableData.append(QString("<td><a href=\"/delete/%1/%2/%3\">"
+                        "<img src=\"/f/toolbox/draw-eraser.png\" alt=\"Edit\"/></a></td>")
+                        .arg(requestedTable).arg(primaryKey->name()).arg(pkeyVal));
+                    // End row
+                }
+                tableData.append("</tr>\n");
+            }
+
+            setValue("TABLEDATA", tableData);
+
+            kDebug() << "Deleting cursor..." << endl;
+            gConnection->deleteCursor(cursor);
+        }
+
+        renderTemplate(m_dict, writer);
+
+    }
+    
+}    
+}
---
+++ kexi/webforms/view/Read.h
@@ -0,0 +1,51 @@
+/* This file is part of the KDE project
+
+   (C) Copyright 2008 by Lorenzo Villani <lvillani@binaryhelix.net>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KEXIWEBFORMS_VIEW_READ_H
+#define KEXIWEBFORMS_VIEW_READ_H
+
+#include <QHash>
+#include <pion/net/HTTPResponseWriter.hpp>
+
+#include "View.h"
+
+namespace KexiWebForms {
+
+    class Controller;
+    
+namespace View {
+
+    /**
+     * @brief WebService handling the read page
+     *
+     * This service lists records in a particular database table
+     */
+    class Read : public View {
+    public:
+        Read(KexiWebForms::Controller& c, const char* name) : View(c, name) {}
+        virtual ~Read() {}
+
+        virtual void view(const QHash<QString, QString>&, pion::net::HTTPResponseWriterPtr);
+    };
+    
+}
+}
+
+#endif /* KEXIWEBFORMS_VIEW_READ_H */
