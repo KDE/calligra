beeed29c492ab2b812595b6b89e8e112b33aaa2e
Lorenzo Villani <lvillani@binaryhelix.net>
2008-08-05 11:31:46 +0000

    * reindent ShutdownManager.hpp
    svn path=/trunk/koffice/; revision=842479

--- kexi/webforms/ShutdownManager.hpp
+++ kexi/webforms/ShutdownManager.hpp
@@ -13,7 +13,7 @@
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/condition.hpp>
 #ifndef PION_WIN32
-  #include <signal.h>
+#include <signal.h>
 #endif
 
 
@@ -22,35 +22,35 @@
 ///
 class ShutdownManager {
 public:
-  // default constructor & destructor
-  ShutdownManager(void) : m_shutdown_now(false) {}
-  ~ShutdownManager() {}
+    // default constructor & destructor
+    ShutdownManager(void) : m_shutdown_now(false) {}
+    ~ShutdownManager() {}
 
-  /// signals the shutdown condition
-  inline void shutdown(void) {
-    boost::mutex::scoped_lock shutdown_lock(m_shutdown_mutex);
-    m_shutdown_now = true;
-    m_shutdown_cond.notify_all();
-  }
+    /// signals the shutdown condition
+    inline void shutdown(void) {
+        boost::mutex::scoped_lock shutdown_lock(m_shutdown_mutex);
+        m_shutdown_now = true;
+        m_shutdown_cond.notify_all();
+    }
   
-  /// blocks until the shutdown condition has been signaled
-  inline void wait(void) {
-    boost::mutex::scoped_lock shutdown_lock(m_shutdown_mutex);
-    while (! m_shutdown_now) {
+    /// blocks until the shutdown condition has been signaled
+    inline void wait(void) {
+        boost::mutex::scoped_lock shutdown_lock(m_shutdown_mutex);
+        while (! m_shutdown_now) {
             KUniqueApplication::processEvents();
-      m_shutdown_cond.wait(shutdown_lock);
+            m_shutdown_cond.wait(shutdown_lock);
         }
-  }
+    }
   
 private:
-  /// true if we should shutdown now
-  bool					m_shutdown_now;
+    /// true if we should shutdown now
+    bool					m_shutdown_now;
   
-  /// used to protect the shutdown condition
-  boost::mutex			m_shutdown_mutex;
+    /// used to protect the shutdown condition
+    boost::mutex			m_shutdown_mutex;
   
-  /// triggered when it is time to shutdown
-  boost::condition		m_shutdown_cond;
+    /// triggered when it is time to shutdown
+    boost::condition		m_shutdown_cond;
 };
 
 /// static shutdown manager instance used to control shutdown of main()
@@ -61,21 +61,21 @@ static ShutdownManager	main_shutdown_manager;
 #ifdef PION_WIN32
 BOOL WINAPI console_ctrl_handler(DWORD ctrl_type)
 {
-  switch(ctrl_type) {
+    switch(ctrl_type) {
     case CTRL_C_EVENT:
     case CTRL_BREAK_EVENT:
     case CTRL_CLOSE_EVENT:
     case CTRL_SHUTDOWN_EVENT:
-      main_shutdown_manager.shutdown();
-      return TRUE;
+        main_shutdown_manager.shutdown();
+        return TRUE;
     default:
-      return FALSE;
-  }
+        return FALSE;
+    }
 }
 #else
 void handle_signal(int sig)
 {
-  main_shutdown_manager.shutdown();
+    main_shutdown_manager.shutdown();
 }
 #endif
 
