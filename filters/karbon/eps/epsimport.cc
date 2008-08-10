5df9fe1eb51b857edbbe6dca3df332f8b8f3ade2
Jan Hambrecht <jaham@gmx.net>
2008-08-10 14:58:01 +0000

    port eps import filter to use pstoedit to convert to svg
    svn path=/trunk/koffice/; revision=844814

--- filters/karbon/eps/epsimport.cc
+++ filters/karbon/eps/epsimport.cc
@@ -17,27 +17,26 @@
  * Boston, MA 02110-1301, USA.
 */
 
-#include <q3cstring.h>
+#include "epsimport.h"
+#include "pscommentlexer.h"
+
 #include <QString>
 #include <QFile>
 
-#include <kgenericfactory.h>
 #include <KoFilter.h>
 #include <KoFilterChain.h>
-#include <krun.h>
-#include <kprocess.h>
-#include <kshell.h>
 
-#include <kdebug.h>
-
-#include "epsimport.h"
-#include "pscommentlexer.h"
+#include <KGenericFactory>
+#include <KRun>
+#include <KProcess>
+#include <KShell>
+#include <KDebug>
 
 typedef KGenericFactory<EpsImport> EpsImportFactory;
 K_EXPORT_COMPONENT_FACTORY( libkarbonepsimport, EpsImportFactory( "kofficefilters" ) )
 
 EpsImport::EpsImport( QObject*parent, const QStringList& )
-	: KoFilter(parent)
+    : KoFilter(parent)
 {
     kDebug() << "###   ###   EPS Import Filter" << endl;
 }
@@ -48,60 +47,81 @@ EpsImport::~EpsImport()
 
 KoFilter::ConversionStatus EpsImport::convert( const QByteArray& from, const QByteArray& to )
 {
-	if(
-		to != "application/illustrator" ||
-		(
-			from != "image/x-eps" &&
-			from != "image/eps" &&
-			from != "application/eps" &&
-			from != "application/x-eps" &&
-			from != "application/postscript" ) ) 
-	{
-		return KoFilter::NotImplemented;
-	}
+    if( from != "image/x-eps" &&
+        from != "image/eps" &&
+        from != "application/eps" &&
+        from != "application/x-eps" &&
+        from != "application/postscript" )
+    {
+        return KoFilter::NotImplemented;
+    }
 
-	// Copy input filename:
-	QString input = m_chain->inputFile();
+    if( to == "image/svg+xml" )
+    {
+        // Copy filenames
+        QString input = m_chain->inputFile();
+        QString output = m_chain->outputFile();
 
+        QString command( "pstoedit -f plot-svg " );
+        command += KShell::quoteArg(input);
+        command += ' ';
+        command += KShell::quoteArg(output);
 
-	// EPS original bounding box
-	int llx = -1, lly = -1, urx = -1, ury = -1;
- 	BoundingBoxExtractor extractor;
+        kDebug() << "command to execute is (%s)" << QFile::encodeName(command).data() ;
 
-	QFile file (input);
+        // Execute it:
+        if( ! system( QFile::encodeName(command) ) )
+            return KoFilter::OK;
+        else
+            return KoFilter::StupidError;
+    }
+    if( to == "application/illustrator" )
+    {
 
-	if ( file.open(QIODevice::ReadOnly) )
-	{
-		extractor.parse (file);
-		llx = extractor.llx();
-		lly = extractor.lly();
-		urx = extractor.urx();
-		ury = extractor.ury();
-    		file.close();
-  	}
-	else
-		qDebug ("file could not be opened");
+        // Copy input filename:
+        QString input = m_chain->inputFile();
 
-	// sed filter
-	QString sedFilter = QString ("sed -e \"s/%%BoundingBox: 0 0 612 792/%%BoundingBox: %1 %2 %3 %4/g\"").
-            arg(llx).arg(lly).arg(urx).arg(ury);
+        // EPS original bounding box
+        int llx = -1, lly = -1, urx = -1, ury = -1;
+        BoundingBoxExtractor extractor;
 
-	// Build ghostscript call to convert ps/eps -> ai:
-	QString command(
-		"gs -q -P- -dBATCH -dNOPAUSE -dSAFER -dPARANOIDSAFER -dNODISPLAY ps2ai.ps ");
-	command += KShell::quoteArg(input);
-	command += " | ";
-	command += sedFilter;
-	command += " > ";
-	command += KShell::quoteArg(m_chain->outputFile());
+        QFile file (input);
 
-	qDebug ("command to execute is (%s)", QFile::encodeName(command).data());
+        if ( file.open(QIODevice::ReadOnly) )
+        {
+            extractor.parse (file);
+            llx = extractor.llx();
+            lly = extractor.lly();
+            urx = extractor.urx();
+            ury = extractor.ury();
+                file.close();
+        }
+        else
+            qDebug ("file could not be opened");
 
-	// Execute it:
-	if( !system( QFile::encodeName(command)) )
-		return KoFilter::OK;
-	else
-		return KoFilter::StupidError;
+        // sed filter
+        QString sedFilter = QString ("sed -e \"s/%%BoundingBox: 0 0 612 792/%%BoundingBox: %1 %2 %3 %4/g\"").
+                arg(llx).arg(lly).arg(urx).arg(ury);
+
+        // Build ghostscript call to convert ps/eps -> ai:
+        QString command(
+            "gs -q -P- -dBATCH -dNOPAUSE -dSAFER -dPARANOIDSAFER -dNODISPLAY ps2ai.ps ");
+        command += KShell::quoteArg(input);
+        command += " | ";
+        command += sedFilter;
+        command += " > ";
+        command += KShell::quoteArg(m_chain->outputFile());
+
+        qDebug ("command to execute is (%s)", QFile::encodeName(command).data());
+
+        // Execute it:
+        if( !system( QFile::encodeName(command)) )
+            return KoFilter::OK;
+        else
+            return KoFilter::StupidError;
+    }
+
+    return KoFilter::NotImplemented;
 }
 
 #include "epsimport.moc"
