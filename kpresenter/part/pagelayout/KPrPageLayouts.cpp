5169cea6509580823d483f5e4ec9d64225b3e7b7
Thorsten Zachmann <t.zachmann@zagge.de>
2008-08-21 04:13:33 +0000

    o implemented the functions
    svn path=/trunk/koffice/; revision=850231

--- kpresenter/part/pagelayout/KPrPageLayouts.cpp
+++ kpresenter/part/pagelayout/KPrPageLayouts.cpp
@@ -19,6 +19,10 @@
 
 #include "KPrPageLayouts.h"
 
+#include <KoOdfLoadingContext.h>
+#include <KoOdfStylesReader.h>
+#include <KoPALoadingContext.h>
+
 #include "KPrPageLayout.h"
 
 KPrPageLayouts::KPrPageLayouts()
@@ -27,18 +31,44 @@ KPrPageLayouts::KPrPageLayouts()
 
 KPrPageLayouts::~KPrPageLayouts()
 {
+    QMap<QString, KPrPageLayout *>::iterator it( m_pageLayouts.begin() );
+    for ( ; it != m_pageLayouts.end(); ++it ) {
+        delete it.value();
+    }
 }
 
 bool KPrPageLayouts::saveOdf( KoPASavingContext & context )
 {
-  return false;
+    QMap<QString, KPrPageLayout *>::iterator it( m_pageLayouts.begin() );
+    for ( ; it != m_pageLayouts.end(); ++it ) {
+        it.value()->saveOdf( context );
+    }
+    return true;
 }
 
 void KPrPageLayouts::loadOdf( KoPALoadingContext &loadingContext )
 {
+    QHash<QString, KoXmlElement*> layouts = loadingContext.odfLoadingContext().stylesReader().presentationPageLayouts();
+
+    QHash<QString, KoXmlElement*>::iterator it( layouts.begin() );
+
+    for ( ; it != layouts.end(); ++it ) {
+        KPrPageLayout * pageLayout = new KPrPageLayout();
+        if ( pageLayout->loadOdf( *( it.value() ), loadingContext ) ) {
+            m_pageLayouts.insert( it.key(), pageLayout );
+        }
+        else {
+            delete pageLayout;
+        }
+    }
 }
 
 KPrPageLayout * KPrPageLayouts::pageLayout( const QString & name )
 {
-  return 0;
+    KPrPageLayout * pageLayout = 0;
+    QMap<QString, KPrPageLayout *>::iterator it( m_pageLayouts.find( name ) );
+    if ( it != m_pageLayouts.end() ) {
+        pageLayout = it.value();
+    }
+    return pageLayout;
 }
