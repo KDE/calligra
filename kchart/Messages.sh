1ba0eb7b01d55b10764012d67f96efca993fbc9c
Burkhard Lück <lueck@hube-lueck.de>
2008-11-18 22:22:31 +0000

    split message catalogs for kformula/FormulaShape and kchart/ChartShape
    svn path=/trunk/koffice/; revision=886277

--- kchart/Messages.sh
+++ kchart/Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 # TODO: extract Qt's tr() messages in kdchart/*
-$XGETTEXT *.cpp kchart_aboutdata.h dialogs/*.cpp commands/*.cpp shape/*.cpp -o $podir/kchart.pot
+$XGETTEXT *.cpp kchart_aboutdata.h dialogs/*.cpp commands/*.cpp -o $podir/kchart.pot
