519caf18b94f20ed8b9b3d8d94b3965af7e571c0
Thomas Zander <zander@kde.org>
2008-12-12 11:27:41 +0000

    Revert "readded copyright 'someone' removed without being allowed to do so" The committer was a bit hasty; nothing improper seems to actually have happened. :)
    svn path=/trunk/koffice/; revision=896048

--- kword/part/commands/KWPageRemoveCommand.h
+++ kword/part/commands/KWPageRemoveCommand.h
@@ -1,7 +1,5 @@
 /* This file is part of the KDE project
  * Copyright (C) 2007-2008 Thomas Zander <zander@kde.org>
- * Copyright (C) 2008 Pierre Ducroquet <pinaraf@pinaraf.info>
- * Copyright (C) 2008 Sebastian Sauer <mail@dipe.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
