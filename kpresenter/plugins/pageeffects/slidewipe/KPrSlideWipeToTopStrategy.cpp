4cf6ab637eae63020f7892c8e61044234a2922e0
Thorsten Zachmann <t.zachmann@zagge.de>
2008-12-14 06:31:19 +0000

    o Fix. The content of the two effects was swapped.
    svn path=/trunk/koffice/; revision=896681

--- kpresenter/plugins/pageeffects/slidewipe/KPrSlideWipeToTopStrategy.cpp
+++ kpresenter/plugins/pageeffects/slidewipe/KPrSlideWipeToTopStrategy.cpp
@@ -41,14 +41,14 @@ void KPrSlideWipeToTopStrategy::paintStep( QPainter &p, int currPos, const KPrPa
 {
     int height = data.m_widget->height();
     int width = data.m_widget->width();
-    QRect rect1( 0, 0, width, height - currPos );
-    QRect rect2( 0, 0, width, currPos );
-    p.drawPixmap( QPoint( 0, currPos ), data.m_oldPage, rect1 );
-    p.drawPixmap( QPoint( 0, 0 ), data.m_newPage, rect2 );
+    QRect rect1( 0, currPos, width, height - currPos );
+    QRect rect2( 0, height - currPos, width, currPos );
+    p.drawPixmap( QPoint( 0, 0 ), data.m_oldPage, rect1 );
+    p.drawPixmap( QPoint( 0, height - currPos ), data.m_newPage, rect2 );
 }
 
 void KPrSlideWipeToTopStrategy::next( const KPrPageEffect::Data &data )
 {
     int lastPos = data.m_timeLine.frameForTime( data.m_lastTime );
-    data.m_widget->update( 0, lastPos, data.m_widget->width(), data.m_widget->height() - lastPos );
+    data.m_widget->update( 0, 0, data.m_widget->width(), data.m_widget->height() - lastPos );
 }
