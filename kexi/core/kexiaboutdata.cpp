9af5321e9fba8bb42039e67183a14bcaecc33b4c
Jarosław Staniek <staniek@kde.org>
2009-01-03 20:22:32 +0000

    About Data - year++ - update info on contribution of OOPL, make it a part of contributors list - fix Cedric's name
    svn path=/trunk/koffice/; revision=905162

--- kexi/core/kexiaboutdata.cpp
+++ kexi/core/kexiaboutdata.cpp
@@ -1,7 +1,7 @@
 /* This file is part of the KDE project
    Copyright (C) 2002, 2003 Lucijan Busch <lucijan@gmx.at>
    Copyright (C) 2002, 2003 Joseph Wenninger <jowenn@kde.org>
-   Copyright (C) 2003-2008 Jarosław Staniek <staniek@kde.org>
+   Copyright (C) 2003-2009 Jarosław Staniek <staniek@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -49,18 +49,17 @@ KAboutData* Kexi::createAboutData()
 #endif
         , ki18n(description),
         KAboutData::License_LGPL_V2,
-        ki18n("(c) 2002-2008, Kexi Team\n"
-              "(c) 2003-2008, OpenOffice Polska LLC\n"),
+        ki18n("(c) 2002-2009, Kexi Team"),
         ki18n("This software is developed by Kexi Team - an international group\n"
-              "of independent developers, with additional assistance and support\n"
-              "from the OpenOffice Polska company.\n\n"
-              "Visit the company Home Page: http://www.openoffice.com.pl"),
+              "of independent developers."),
         "http://www.koffice.org/kexi",
         "submit@bugs.kde.org"
     );
     // authors sorted by last nontrivial contribution date
     aboutData->addAuthor(
-        ki18n("Jarosław Staniek / OpenOffice Polska"), ki18n("Project maintainer & developer, design, KexiDB, commercially supported version, win32 port"), "staniek@kde.org");
+        ki18n("Jarosław Staniek"), ki18n("Project maintainer & developer, design, KexiDB, commercially supported version, MS Windows version"), "staniek@kde.org");
+    aboutData->addAuthor(
+        ki18n("OpenOffice Polska LLC"), ki18n("Sponsoring and support (employer of Jarosław Staniek in 2003-2007)"), "it@openoffice.com.pl");
     aboutData->addAuthor(
         ki18n("Sebastian Sauer"), ki18n("Scripting module (KROSS), Python language bindings, design"), "mail@dipe.org");
     aboutData->addAuthor(
@@ -68,9 +67,9 @@ KAboutData* Kexi::createAboutData()
     aboutData->addAuthor(
         ki18n("Sharan Rao"), ki18n("Sybase/MS SQL Server database drivers, xBase migration plugin, improvements for KexiDB"), "sharanrao@gmail.com");
     aboutData->addAuthor(
-        ki18n("Cedric Pasteur"), ki18n("KexiPropertyEditor and FormDesigner"), "cedric.pasteur@free.fr");
+        ki18n("Cédric Pasteur"), ki18n("First version of Property Editor and Form Designer"), "cedric.pasteur@free.fr");
     aboutData->addAuthor(
-        ki18n("Martin Ellis"), ki18n("Contributions for MySQL and KexiDB, fixes, Migration module, MDB support"), "martin.ellis@kdemail.net");
+        ki18n("Martin Ellis"), ki18n("Contributions for MySQL and KexiDB, fixes, Migration module, MS Access file format support"), "martin.ellis@kdemail.net");
     aboutData->addAuthor(
         ki18n("Christian Nitschkowski"), ki18n("Graphics effects, helper dialogs"), "segfault_ii@web.de");
     aboutData->addAuthor(
