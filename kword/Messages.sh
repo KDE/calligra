a9221d81b4fada7e5100c090894045a303c5a26e
Albert Astals Cid <tsdgeos@terra.es>
2009-01-04 14:32:47 +0000

    Remove the removal of xml_doc.cpp after zander removed its creation CCMAIL: zander@kde.org
    svn path=/trunk/koffice/; revision=905520

--- kword/Messages.sh
+++ kword/Messages.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
 $EXTRACTRC `find part -name \*.ui -o -name \*.rc` >> rc.cpp
 $XGETTEXT rc.cpp *.cpp `find part -name \*.cpp -o -name \*.cpp -o -name \*.h | egrep -v "/old"` -o $podir/kword.pot
-rm xml_doc.cpp
 
