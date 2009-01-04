b8fee79d1205596d76b143d5ad5b0a05e88a1133
Thomas Zander <zander@kde.org>
2009-01-04 01:12:57 +0000

    Fixes: changing page size in print dialog has no effect BUG:132415 Details: Do not show the page size and margins options in the kword print     dialog as they are useless.
    svn path=/trunk/koffice/; revision=905245

--- kword/part/dialogs/KWPrintingDialog.cpp
+++ kword/part/dialogs/KWPrintingDialog.cpp
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
- * Copyright (C) 2007 Thomas Zander <zander@kde.org>
+ * Copyright (C) 2007, 2009 Thomas Zander <zander@kde.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -125,6 +125,14 @@ int KWPrintingDialog::documentFirstPage() const
 int KWPrintingDialog::documentLastPage() const
 {
     return m_document->pageManager()->last().pageNumber();
+}
+
+QAbstractPrintDialog::PrintDialogOptions KWPrintingDialog::printDialogOptions() const
+{
+    return QAbstractPrintDialog::PrintToFile |
+           QAbstractPrintDialog::PrintPageRange |
+           QAbstractPrintDialog::PrintCollateCopies |
+           QAbstractPrintDialog::DontUseSheet;
 }
 
 // options;
