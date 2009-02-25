0399374a102bd6bdf44866b7ee7b5b32074d70c8
Cyrille Berger <cyb@lepi.org>
2009-02-25 22:44:13 +0000

    remove unreleased stuff
    svn path=/tags/koffice/1.9.98.7/koffice/; revision=931910

--- kexi/3rdparty/kexisql3/src/patches/mk_patch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ..
-patches/remove_id.sh
-
-diff -u ../../sqlite-3.2.8.orig/ . | grep -v "^Only in"
--- kexi/3rdparty/kexisql3/src/patches/remove_id.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-for i in *.h *.c ; do cat $i | grep -v "\$Id" > $i.tmp && mv $i.tmp $i ; done
