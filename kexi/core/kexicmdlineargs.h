705650016aa129078c55a5c918efa4aa74374a26
Thomas Zander <zander@kde.org>
2009-03-09 21:35:14 +0000

    Oops. Sorry guys...
    svn path=/trunk/koffice/; revision=937534

--- kexi/core/kexicmdlineargs.h
+++ kexi/core/kexicmdlineargs.h
@@ -92,7 +92,7 @@ static KCmdLineOptions kexi_options()
     .add("skip-startup-dialog", ki18n(
              "Skip displaying startup dialog window.\n"
              "If there is no project name specified to open,\n"
-             "an empty application window will appear."))
+             "empty application window will appear."))
     .add(":", ki18n("Options related to opening objects within a project:"))
     .add("open [<object_type>:]<object_name>", ki18n(
              "\nOpen object of type <object_type>\n"
