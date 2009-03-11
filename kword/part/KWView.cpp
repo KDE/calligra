746aa7c249db1caa0e31ed167293a41f072924e1
Thomas Zander <zander@kde.org>
2009-03-11 21:53:53 +0000

    Fix crash on embedding BUG:181369
    svn path=/trunk/koffice/; revision=938459

--- kword/part/KWView.cpp
+++ kword/part/KWView.cpp
@@ -116,7 +116,8 @@ KWView::KWView(const QString& viewMode, KWDocument* document, QWidget *parent)
     KWStatisticsDocker *docker = dynamic_cast<KWStatisticsDocker *>(createDockWidget(&statisticsFactory));
     if (docker && docker->view() != this) docker->setView(this);
 
-    new KWStatusBar(statusBar(), this);
+    if (statusBar())
+        new KWStatusBar(statusBar(), this);
 
     // the zoom controller needs to be initialized after the status bar gets initialized as 
     // that resulted in bug 180759
