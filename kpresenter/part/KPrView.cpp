f65a1bbb94fb74bc264ea4aaa2177d2bbf6618e2
Alexia Allanic <alexia_allanic@yahoo.fr>
2009-03-14 09:58:27 +0000

    rename ImportSlideShow to importDocument, correct the dialog bug
    svn path=/branches/work/koffice-isi/; revision=939205

--- kpresenter/part/KPrView.cpp
+++ kpresenter/part/KPrView.cpp
@@ -61,6 +61,7 @@ KPrView::KPrView( KPrDocument *document, QWidget *parent )
 
     // Change strings because in KPresenter it's called slides and not pages
     actionCollection()->action("view_masterpages")->setText(i18n("Show Master Slides"));
+    actionCollection()->action("import_document")->setText(i18n("Import Slideshow"));
     actionCollection()->action("page_insertpage")->setText(i18n( "Insert Slide"));
     actionCollection()->action("page_insertpage")->setToolTip(i18n("Insert a new slide after the current one"));
     actionCollection()->action("page_insertpage")->setWhatsThis(i18n("Insert a new slide after the current one"));
