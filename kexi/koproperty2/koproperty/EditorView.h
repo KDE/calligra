aa4a0abaa10ae2b9229218d22d31e26d541e0730
David Faure <faure@kde.org>
2009-03-20 00:31:59 +0000

    --pedantic says EditorView.h:80: error: comma at end of enumerator list
    svn path=/trunk/koffice/; revision=941580

--- kexi/koproperty2/koproperty/EditorView.h
+++ kexi/koproperty2/koproperty/EditorView.h
@@ -77,7 +77,7 @@ public:
         NoOptions = 0,
         PreservePreviousSelection = 1, //!< If used, previously selected editor item
                                        //!< will be kept selected.
-        AlphabeticalOrder = 2, //!< Alphabetical order of properies (the default is insert-order)
+        AlphabeticalOrder = 2 //!< Alphabetical order of properies (the default is insert-order)
     };
     Q_DECLARE_FLAGS(SetOptions, SetOption)
 
