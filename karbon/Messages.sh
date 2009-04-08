fe6d307afdb16e46bfd484055e49ccbefe91d38a
Pino Toscano <pino@kde.org>
2009-04-08 22:18:06 +0000

    don't miss to extract messages from .ui files as well CCMAIL: kde-i18n-doc@kde.org
    svn path=/branches/koffice/2.0/koffice/; revision=951319

--- karbon/Messages.sh
+++ karbon/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-$EXTRACTRC data/*.rc >> rc.cpp
+$EXTRACTRC `find ui -name \*.ui` data/*.rc >> rc.cpp
 $XGETTEXT rc.cpp *.cpp *.cc common/*/*.cpp ui/*/*.cpp plugins/*/*.cpp plugins/*/*/*.cpp ui/*.cpp  ui/*.h -o $podir/karbon.pot
 
