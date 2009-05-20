b9fd0ecc59b8c81b5e26568bcca1ef1ec80f2d76
Cyrille Berger <cberger@cberger.net>
2009-05-20 15:19:05 +0200

    start working on web shape

---
+++ braindump/plugins/webshape/WebShapeFactory.cpp
@@ -0,0 +1,46 @@
+/*
+ *  Copyright (c) 2009 Cyrille Berger <cberger@cberger.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "WebShapeFactory.h"
+
+#include <klocale.h>
+
+#include "WebShape.h"
+
+WebShapeFactory::WebShapeFactory(QObject* parent) 
+   : KoShapeFactory( parent, "FooShape",
+                     i18n("Foo Shape") )
+{
+    setToolTip( i18n("A foo shape") );
+}
+ 
+KoShape* WebShapeFactory::createDefaultShape() const
+{
+    WebShape* fooShape = new WebShape();
+    // set defaults
+    return fooShape;
+}
+ 
+KoShape* WebShapeFactory::createShape(
+                            const KoProperties* params ) const
+{
+    WebShape* fooShape = new WebShape();
+    // use the params
+    return fooShape;
+}
