956b159f9e7cadc63c4ebe0fbec6f21c9f2a0c48
Cyrille Berger <cberger@cberger.net>
2009-05-28 15:00:19 +0200

    duplicate the name

--- braindump/src/Section.cpp
+++ braindump/src/Section.cpp
@@ -26,6 +26,7 @@ Section::Section() : SectionGroup(0), m_sectionContainer(new SectionContainer(th
 }
 
 Section::Section(const Section& _rhs) : SectionGroup(0), m_sectionContainer(new SectionContainer(this)) {
+  setName(_rhs.name());
 }
 
 SectionContainer* Section::sectionContainer() {
