9bf5b7ae042c0468883c3f673d7519c9e9f4f9e2
Jarosław Staniek <staniek@kde.org>
2009-06-17 07:44:43 +0000

    Forms * image box: fix painting semitransparent icon for unbound widgets
    svn path=/trunk/koffice/; revision=982915

--- kexi/plugins/forms/widgets/kexidbimagebox.h
+++ kexi/plugins/forms/widgets/kexidbimagebox.h
@@ -1,6 +1,6 @@
 /* This file is part of the KDE project
    Copyright (C) 2005 Cedric Pasteur <cedric.pasteur@free.fr>
-   Copyright (C) 2004-2007 Jarosław Staniek <staniek@kde.org>
+   Copyright (C) 2004-2009 Jarosław Staniek <staniek@kde.org>
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
