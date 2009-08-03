37e548ea69727ce753189610eda2450605d0a71d
Boudewijn Rempt <boud@valdyas.org>
2009-08-03 18:42:45 +0000

    remove q3 deps from this filter
    svn path=/trunk/koffice/; revision=1006436

--- filters/kspread/html/exportdialog.cc
+++ filters/kspread/html/exportdialog.cc
@@ -19,7 +19,7 @@
 
 #include <QCheckBox>
 #include <QComboBox>
-#include <q3listbox.h>
+#include <QListWidget>
 #include <QRadioButton>
 #include <QSpinBox>
 #include <QTextCodec>
@@ -32,84 +32,93 @@
 #include <exportdialog.h>
 
 ExportDialog::ExportDialog( QWidget *parent )
-  : KDialog( parent ), m_mainwidget( new ExportWidget( this ) )
+        : KDialog( parent ), m_mainwidget( new ExportWidget( this ) )
 {
-  setCaption( i18n("Export Sheet to HTML") );
-  setButtons( Ok|Cancel );
-  setDefaultButton(KDialog::Cancel);
-  kapp->restoreOverrideCursor();
+    setCaption( i18n("Export Sheet to HTML") );
+    setButtons( Ok|Cancel );
+    setDefaultButton(KDialog::Cancel);
+    kapp->restoreOverrideCursor();
 
-  connect( m_mainwidget->mCustomButton, SIGNAL( toggled( bool ) ),
-           m_mainwidget->mCustomURL, SLOT( setEnabled( bool ) ) );
-  connect( m_mainwidget->mSelectAllButton, SIGNAL( clicked() ), SLOT( selectAll() ) );
-  connect( m_mainwidget->mDeselectAllButton, SIGNAL( clicked() ),
-           m_mainwidget->mSheets, SLOT( clearSelection() ) );
+    connect( m_mainwidget->mCustomButton, SIGNAL( toggled( bool ) ),
+             m_mainwidget->mCustomURL, SLOT( setEnabled( bool ) ) );
+    connect( m_mainwidget->mSelectAllButton, SIGNAL( clicked() ), SLOT( selectAll() ) );
+    connect( m_mainwidget->mDeselectAllButton, SIGNAL( clicked() ),
+             m_mainwidget->mSheets, SLOT( clearSelection() ) );
 
-  m_mainwidget->mEncodingBox->addItem( i18n( "Recommended: UTF-8" ) );
-  m_mainwidget->mEncodingBox->addItem( i18n( "Locale (%1)", QString::fromLatin1( KGlobal::locale()->codecForEncoding()->name() ) ) );
+    m_mainwidget->mEncodingBox->addItem( i18n( "Recommended: UTF-8" ) );
+    m_mainwidget->mEncodingBox->addItem( i18n( "Locale (%1)", QString::fromLatin1( KGlobal::locale()->codecForEncoding()->name() ) ) );
 
-  m_mainwidget->mCustomURL->setMode( KFile::ExistingOnly );
+    m_mainwidget->mCustomURL->setMode( KFile::ExistingOnly );
 
-  setMainWidget( m_mainwidget );
+    setMainWidget( m_mainwidget );
 }
 
 void ExportDialog::selectAll()
 {
-  m_mainwidget->mSheets->selectAll( true );
+
+    QListWidget *view = m_mainwidget->mSheets;
+    QAbstractItemModel *model = view->model();
+    QModelIndex topLeft = model->index(0, 0);
+    QModelIndex bottomRight = model->index(model->rowCount() - 1, model->columnCount() - 1);
+    QItemSelection selection(topLeft, bottomRight);
+    view->selectionModel()->select(selection ,QItemSelectionModel::QItemSelectionModel::Select);
 }
 
 ExportDialog::~ExportDialog()
 {
-  kapp->setOverrideCursor(Qt::WaitCursor);
+    kapp->setOverrideCursor(Qt::WaitCursor);
 }
 
 QTextCodec *ExportDialog::encoding() const
 {
-  if( m_mainwidget->mEncodingBox->currentIndex() == 1 ) // locale selected
-    return KGlobal::locale()->codecForEncoding();
+    if( m_mainwidget->mEncodingBox->currentIndex() == 1 ) // locale selected
+        return KGlobal::locale()->codecForEncoding();
 
-  return QTextCodec::codecForName( "utf8" ); // utf8 is default
+    return QTextCodec::codecForName( "utf8" ); // utf8 is default
 }
 
 bool ExportDialog::useBorders() const
 {
-  return m_mainwidget->mUseBorders->isChecked();
+    return m_mainwidget->mUseBorders->isChecked();
 }
 
 bool ExportDialog::separateFiles() const
 {
-  return m_mainwidget->mSeparateFiles->isChecked();
+    return m_mainwidget->mSeparateFiles->isChecked();
 }
 
 KUrl ExportDialog::customStyleURL() const
 {
-  KUrl url = m_mainwidget->mCustomURL->url();
-  if( m_mainwidget->mCustomButton->isChecked() && url.isValid() )
-    return url;
+    KUrl url = m_mainwidget->mCustomURL->url();
+    if( m_mainwidget->mCustomButton->isChecked() && url.isValid() )
+        return url;
 
-  return KUrl();
+    return KUrl();
 }
 
 void ExportDialog::setSheets( const QStringList &list )
 {
-  m_mainwidget->mSheets->insertStringList( list );
-  selectAll();
+    m_mainwidget->mSheets->addItems( list );
+    selectAll();
 }
 
 QStringList ExportDialog::sheets() const
 {
-  QStringList list;
-  for( uint i = 0; i < m_mainwidget->mSheets->count() ; i++ )
-  {
-    if( m_mainwidget->mSheets->isSelected( i ) )
-      list.append( m_mainwidget->mSheets->text( i ) );
-  }
-  return list;
+    QListWidget* view = m_mainwidget->mSheets;
+    QStringList list;
+    for( uint i = 0; i < view->count() ; i++ )
+    {
+        QListWidgetItem* item = view->item(i);
+        if ( item->isSelected() ) {
+            list.append( item->text() );
+        }
+    }
+    return list;
 }
 
 int ExportDialog::pixelsBetweenCells() const
 {
-  return m_mainwidget->mPixelsBetweenCells->value();
+    return m_mainwidget->mPixelsBetweenCells->value();
 }
 
 #include <exportdialog.moc>
