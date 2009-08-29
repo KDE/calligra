0fb313911c44e44bed11286bfd7b22793c062ee8
Burkhard Lück <lueck@hube-lueck.de>
2009-08-29 20:47:03 +0000

    extract strings from ui files too
    svn path=/trunk/koffice/; revision=1017179

--- kplato/libs/Messages.sh
+++ kplato/libs/Messages.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
-$XGETTEXT kernel/*.cpp kernel/*.h models/*.cpp models/*.h ui/*.cpp ui/*.h -o $podir/kplatolibs.pot
+$EXTRACTRC `find . -name \*.ui` >> rc.cpp
+$XGETTEXT *.cpp kernel/*.cpp kernel/*.h models/*.cpp models/*.h ui/*.cpp ui/*.h -o $podir/kplatolibs.pot
