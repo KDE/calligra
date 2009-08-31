fa60fad6f7aaf8d968edaf7178f41c963473e08f
Burkhard Lück <lueck@hube-lueck.de>
2009-08-31 06:36:33 +0000

    extract missing strings from scripts/*.ui files
    svn path=/trunk/koffice/; revision=1017665

--- kspread/plugins/scripting/Messages.sh
+++ kspread/plugins/scripting/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-$EXTRACTRC *.ui *.rc >> rc.cpp || exit 11
+$EXTRACTRC scripts/*.ui *.ui *.rc >> rc.cpp || exit 11
 $EXTRACTATTR --attr=collection,text --attr=collection,comment --attr=script,text --attr=script,comment scripts/*.rc >> rc.cpp || exit 12
 $XGETTEXT ScriptingWriter.h *.cpp -o $podir/krossmodulekspread.pot
