30d5111f51cc7e13d1bf393cec5db8e3125553e6
Burkhard Lück <lueck@hube-lueck.de>
2009-08-31 06:19:10 +0000

    extract these messages too
    svn path=/trunk/koffice/; revision=1017660

--- karbon/plugins/tools/Messages.sh
+++ karbon/plugins/tools/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-$EXTRACTRC ../../ui/widgets/KarbonPatternOptionsWidget.ui >> rc.cpp
-$XGETTEXT rc.cpp *.cpp *.h CalligraphyTool/*.cpp CalligraphyTool/*.h ../../ui/widgets/KarbonPatternOptionsWidget.cpp \
+$EXTRACTRC ../../ui/widgets/KarbonPatternOptionsWidget.ui filterEffectTool/FilterEffectEditWidget.ui >> rc.cpp
+$XGETTEXT rc.cpp *.cpp *.h CalligraphyTool/*.cpp CalligraphyTool/*.h filterEffectTool/*cpp ../../ui/widgets/KarbonPatternOptionsWidget.cpp \
 ../../ui/widgets/KarbonGradientEditWidget.cpp ../../ui/widgets/KarbonStyleButtonBox.cpp  -o $podir/KarbonTools.pot
