e4acff4a3973d6cb639c5019731c8ab66df817d2
Jarosław Staniek <staniek@kde.org>
2009-09-01 22:57:13 +0000

    SVN_SILENT disable messages for relations
    svn path=/trunk/koffice/; revision=1018616

--- kexi/Messages.sh
+++ kexi/Messages.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 rm -f tips.cpp
-EXCLUDE="-path ./doc -o -path ./plugins/importwizard -o -path ./tests -o -path ./scriptingplugins -o -path ./plugins/scripting -o -path ./plugins/reports -o -path ./3rdparty -o -path ./shapes"
+EXCLUDE="-path ./doc -o -path ./plugins/importwizard -o -path ./tests -o -path ./scriptingplugins -o -path ./plugins/scripting -o -path ./plugins/reports -o -path ./plugins/relations -o -path ./3rdparty -o -path ./shapes"
 LIST="data/*.rc `find . \( \( $EXCLUDE \) -prune -o -name \*.ui \) -type f | grep -v -e '/\.'`"
 if test -n "$LIST"; then
 	$EXTRACTRC $LIST >> rc.cpp
