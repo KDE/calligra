90d170e1cc1fa1059bf01c8e5336bdd719b82838
Adam Pigg <adam@piggz.co.uk>
2009-09-22 19:32:51 +0000

    Export not required here
    svn path=/trunk/koffice/; revision=1026896

--- kexi/migration/keximigrate.cpp
+++ kexi/migration/keximigrate.cpp
@@ -618,7 +618,7 @@ QVariant KexiMigrate::value(uint i)
 
 //------------------------
 
-KEXIMIGR_EXPORT DatabaseVersionInfo KexiMigration::version()
+DatabaseVersionInfo KexiMigration::version()
 {
     return KEXI_MIGRATION_VERSION;
 }
