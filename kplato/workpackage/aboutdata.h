c87f689ef69b7ca8968424c639c581ad02648060
Dag Andersen <danders@get2net.dk>
2009-10-24 08:25:19 +0000

    Update to RC1 kplatowork.
    svn path=/trunk/koffice/; revision=1039648

--- kplato/workpackage/aboutdata.h
+++ kplato/workpackage/aboutdata.h
@@ -28,7 +28,7 @@ namespace KPlatoWork
 {
 
 static const char* KPLATOWORK_DESCRIPTION=I18N_NOOP("KPlatoWork - Work Package handler for the KPlato Project Planning Tool");
-static const char* KPLATOWORK_VERSION="0.7.82";
+static const char* KPLATOWORK_VERSION="0.7.91";
 
 KAboutData * newAboutData()
 {
