65e9225081b9bbc789463fbff0890e23dafba0f2
Jarosław Staniek <staniek@kde.org>
2009-10-31 22:38:35 +0000

    Forms *fix recently introduced possible crash (happens when there are no focusable daata-aware widget in the data view)
    svn path=/trunk/koffice/; revision=1043168

--- kexi/plugins/forms/kexiformscrollview.cpp
+++ kexi/plugins/forms/kexiformscrollview.cpp
@@ -103,7 +103,10 @@ void KexiFormScrollView::selectCellInternal()
         if (m_currentItem != m_previousRecord) {
             fillDataItems(*m_currentItem, cursorAtNewRow());
             m_previousRecord = m_currentItem;
-            QWidget *w = dbFormWidget()->orderedDataAwareWidgets()->at(m_curCol);
+            QWidget *w = 0;
+            if (m_curCol >= 0 && m_curCol < dbFormWidget()->orderedDataAwareWidgets()->count()) {
+                w = dbFormWidget()->orderedDataAwareWidgets()->at(m_curCol);
+            }
             if (w) {
                 w->setFocus(); // re-focus, as we could have lost focus, e.g. when navigator button was clicked
                 // select all
