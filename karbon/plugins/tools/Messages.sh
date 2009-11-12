1539038e77d2f96f23716a4755ef7d79a2b3d43e
Albert Astals Cid <tsdgeos@terra.es>
2009-11-12 00:00:52 +0000

    drop KarbonStyleButtonBox.cpp from here
    svn path=/trunk/koffice/; revision=1047744

--- karbon/plugins/tools/Messages.sh
+++ karbon/plugins/tools/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC ../../ui/widgets/KarbonPatternOptionsWidget.ui filterEffectTool/FilterEffectEditWidget.ui >> rc.cpp
 $XGETTEXT rc.cpp *.cpp *.h CalligraphyTool/*.cpp CalligraphyTool/*.h filterEffectTool/*cpp ../../ui/widgets/KarbonPatternOptionsWidget.cpp \
-../../ui/widgets/KarbonGradientEditWidget.cpp ../../ui/widgets/KarbonStyleButtonBox.cpp  -o $podir/KarbonTools.pot
+../../ui/widgets/KarbonGradientEditWidget.cpp -o $podir/KarbonTools.pot
