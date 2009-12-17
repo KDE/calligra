f509af30f3f89a2989862ad1e110399f212e18ff
Inge Wallin <inge@lysator.liu.se>
2009-12-17 13:47:35 +0000

    Make the msword-odf filter handle embedded EMF files in addition to WMF.
    svn path=/trunk/koffice/; revision=1063230

--- filters/kword/msword-odf/graphicshandler.cpp
+++ filters/kword/msword-odf/graphicshandler.cpp
@@ -29,7 +29,11 @@
 #include <kdebug.h>
 #include <kmimetype.h>
 
+
 using namespace wvWare;
+
+#define IMG_BUF_SIZE 2048L
+
 
 KWordPictureHandler::KWordPictureHandler(Document* doc, KoXmlWriter* bodyWriter,
         KoXmlWriter* manifestWriter, KoStore* store, KoGenStyles* mainStyles)
@@ -64,7 +68,6 @@ void KWordPictureHandler::escherData(OLEImageReader& reader, SharedPtr<const Wor
 
     //write picture data to file
     m_store->open(picName);//open picture file
-#define IMG_BUF_SIZE 2048L
     long len = reader.size();
     while (len > 0)  {
         kDebug(30513) << "len = " << len;
@@ -97,7 +100,7 @@ void KWordPictureHandler::escherData(std::vector<wvWare::U8> data, SharedPtr<con
 
     //write picture data to file
     m_store->open(picName);//open picture file
-#define IMG_BUF_SIZE 2048L
+
     long len = data.size();
     int index = 0; //index for reading from vector
     while (len > 0)  {
@@ -139,8 +142,10 @@ void KWordPictureHandler::ODTProcessing(QString* picName, SharedPtr<const Word97
         picName->append(".jpg");
     else if (type == 6)
         picName->append(".png");
-    else if (type == 3 || type == 2) //3 is for Windows metafile, 2 is for Windows enhanced metafile
+    else if (type == 3) 
         picName->append(".wmf");
+    else if (type == 2) 
+        picName->append(".emf");
     else {
         kWarning() << "Unhandled file type (" << type << ") - pictures won't be displayed.";
         return;
@@ -181,32 +186,55 @@ void KWordPictureHandler::ODTProcessing(QString* picName, SharedPtr<const Word97
 
 void KWordPictureHandler::wmfData(OLEImageReader& reader, SharedPtr<const Word97::PICF> picf)
 {
+    wvWare::U8* buf = new wvWare::U8[IMG_BUF_SIZE];
+
     kDebug(30513) << "WMF data found. Size=" << reader.size();
 
     QString picName("Pictures/");
-    ODTProcessing(&picName, picf, 3); //pass 3 in for wmf image
-
-    //write picture data to file
-    m_store->open(picName);//open picture file
-#define IMG_BUF_SIZE 2048L
+    
+    // Read the first bytes of the picture.  We need this to determine
+    // if the contents is a WMF or an EMF.
     long len = reader.size();
+    size_t  n = reader.read(buf, qMin(len, IMG_BUF_SIZE));
+
+    // Pass 2 for emf image and 3 for wmf image
+    // An EMF has the string " EMF" at the start + offset 40.
+    if (len > 44 && buf[40] == ' ' && buf[41] == 'E' && buf[42] == 'M' && buf[43] == 'F') {
+        kDebug(30513) << "Found an EMF file";
+        ODTProcessing(&picName, picf, 2);
+    }
+    else {
+        kDebug(30513) << "Found a WMF file";
+        ODTProcessing(&picName, picf, 3);
+    }
+
+    // Write picture data to file.
+    m_store->open(picName);//open picture file
     while (len > 0)  {
         kDebug(30513) << "len = " << len;
-        wvWare::U8* buf = new wvWare::U8[IMG_BUF_SIZE];
-        size_t n = reader.read(buf, qMin(len, IMG_BUF_SIZE));
-        long n1 = m_store->write((const char*)buf, n);
+
+        // Write the bytes that we have in the buffer.
+        ulong    n1 = m_store->write((const char*)buf, n);
         kDebug(30513) << "n=" << n << ", n1=" << n1 << "; buf contains " << (void*) buf;
         len -= n;
-        delete [] buf;
+
+        // Break if something went wrong with the writing.
+        if (n1 != n)
+            break;
+
+        if (len == 0)
+            break;
+
+        // Read the next batch.
+        size_t  n = reader.read(buf, qMin(len, IMG_BUF_SIZE));
+
         //error checking
-        if ((n == 0 && len != 0) ||  //endless loop
-                (size_t)n1 != n) { //read/wrote different lengths
-            m_store->close(); //close picture file before returning
-            return; //ouch - we're in an endless loop!
-        }
-        //Q_ASSERT( (size_t)n1 == n );
+        if (n == 0 && len != 0)
+            break;
     }
-    Q_ASSERT(len == 0);
+
+    delete [] buf;
+
     m_store->close(); //close picture file
 }
 
