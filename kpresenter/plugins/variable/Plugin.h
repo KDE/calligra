b80878c7505787311187edcaf3c1fcf00147f514
Thomas Zander <zander@kde.org>
2010-01-22 13:47:36 +0000

    fix license of files to follow the format set by the fsf.
    Same license; just different wording.  This solves the issue that
    license checkers don't recognize the license used.
    
    svn path=/trunk/koffice/; revision=1078621

--- kpresenter/plugins/variable/Plugin.h
+++ kpresenter/plugins/variable/Plugin.h
@@ -7,13 +7,13 @@
 *            <amit.5.aggarwal@nokia.com>
 *
 * This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public License
-* version 2.1 or (at your option) any later version as published by
-* the Free Software Foundation.
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
 *
-* This library is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 * Lesser General Public License for more details.
 *
 * You should have received a copy of the GNU Lesser General Public
