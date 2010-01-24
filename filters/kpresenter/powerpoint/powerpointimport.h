7b11e54049d8954058863e3018a9afae5b2c3542
Jos van den Oever <jos@vandenoever.info>
2010-01-24 00:14:25 +0000

    Some more license and copyright additions.
    svn path=/trunk/koffice/; revision=1079287

--- filters/kpresenter/powerpoint/powerpointimport.h
+++ filters/kpresenter/powerpoint/powerpointimport.h
@@ -1,5 +1,6 @@
 /* This file is part of the KDE project
    Copyright (C) 2005 Yolla Indria <yolla.indria@gmail.com>
+   Copyright (C) 2010 KO GmbH <jos.van.den.oever@kogmbh.com>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
