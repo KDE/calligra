KDE/calligra

commit bdb84b1704c782a6d8492cfcad73a4a8a2f28034

Fix crash if named area references an invalid sheet.
svn path=/trunk/koffice/; revision=1080591

kspread/Region.cpp
@@ -93,6 +93,8 @@ Region::Region(const QString& string, const Map* map, Sheet* fallbackSheet)
             return;
         if (!sheet)
             sheet = fallbackSheet;
+        if (!sheet)
+            continue;
 
         int delimiterPos = sRegion.indexOf(':');
         if (delimiterPos > -1) {

commit 28f6acdfa5c42d945b8820476e6a34752d317776

Fix regression introduced with r1080591, the check for the sheet was to early.
svn path=/trunk/koffice/; revision=1085696

kspread/Region.cpp
@@ -93,8 +93,6 @@ Region::Region(const QString& string, const Map* map, Sheet* fallbackSheet)
             return;
         if (!sheet)
             sheet = fallbackSheet;
-        if (!sheet)
-            continue;
 
         int delimiterPos = sRegion.indexOf(':');
         if (delimiterPos > -1) {
@@ -104,21 +102,21 @@ Region::Region(const QString& string, const Map* map, Sheet* fallbackSheet)
 
             if (ul.isValid() && lr.isValid()) {
                 Range* range = createRange(sRegion);
-                range->setSheet(sheet);
+                if(sheet) range->setSheet(sheet);
                 d->cells.append(range);
             } else if (ul.isValid()) {
                 Point* point = createPoint(sRegion.left(delimiterPos));
-                point->setSheet(sheet);
+                if(sheet) point->setSheet(sheet);
                 d->cells.append(point);
             } else { // lr.isValid()
                 Point* point = createPoint(sRegion.right(delimiterPos + 1));
-                point->setSheet(sheet);
+                if(sheet) point->setSheet(sheet);
                 d->cells.append(point);
             }
         } else {
             // single cell
             Point* point = createPoint(sRegion);
-            point->setSheet(sheet);
+            if(sheet) point->setSheet(sheet);
             d->cells.append(point);
         }
     }

kspread/tests/TestRegion.cpp
@@ -90,6 +90,7 @@ void TestRegion::testSheet()
     QCOMPARE(region.name(), QString("Sheet1!A1"));
     QCOMPARE(region.firstSheet(), m_doc->map()->sheet(0));
     region = Region("A1");
+qDebug()<<">>>>>>>>>>>>>>>>>>>>>"<<region.name();
     QCOMPARE(region.name(), QString("A1"));
     QCOMPARE(region.firstSheet(), (Sheet*)0);
     region = Region("A1", m_doc->map(), m_doc->map()->sheet(0));