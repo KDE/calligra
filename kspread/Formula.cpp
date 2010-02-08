11b2d58e85abbc13a212b78e5d8adcb8bf61ed51
Sebastian Sauer <mail@dipe.org>
2010-02-08 12:48:10 +0000

    at least print a warning if we are running into a non-singular cell reference.
    svn path=/trunk/koffice/; revision=1087023

--- kspread/Formula.cpp
+++ kspread/Formula.cpp
@@ -1574,6 +1574,10 @@ Value Formula::evalRecursive(CellIndirection cellIndirections, QHash<Cell, Value
                 // store the reference, so we can use it within functions
                 entry.col1 = entry.col2 = position.x();
                 entry.row1 = entry.row2 = position.y();
+            } else {
+                QStringList l;
+                foreach(QRect r, region.rects()) l << QString("(%1,%2,%3,%4)").arg(r.x()).arg(r.y()).arg(r.width()).arg(r.height());
+                kWarning() << "Unhandled non singular region in Opcode::Cell with rects=" << l.join(",");
             }
             entry.val = val1;
             stack.push(entry);
