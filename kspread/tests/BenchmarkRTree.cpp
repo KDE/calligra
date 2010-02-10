42773e283576805c0ff03b9ab5698ea318d8f60b
Marijn Kruisselbrink <m.kruisselbrink@student.tue.nl>
2010-02-10 15:34:40 +0000

    change RTree benchmark to use QBENCHMARK instead of custom x86-only timekeeping code. I'm not entirely happy with this at the moment, as especially in the removal tests every iteration will take a different time, so the results might not be very meaningfull.
    svn path=/trunk/koffice/; revision=1088350

--- kspread/tests/BenchmarkRTree.cpp
+++ kspread/tests/BenchmarkRTree.cpp
@@ -18,13 +18,9 @@
 */
 #include "BenchmarkRTree.h"
 
-#include <iostream>
-
 #include <QtTest/QtTest>
 
 #include <kdebug.h>
-
-#include "BenchmarkHelper.h"
 
 // #include "rtree.h"
 #include "RTree.h"
@@ -54,8 +50,6 @@ void RTreeBenchmark::cleanup()
 
 void RTreeBenchmark::testInsertionPerformance()
 {
-    cout << "Insertion performance test..." << endl;
-
     // reset tree for this test
     RTree<double> tree;
     m_tree = tree;
@@ -64,65 +58,58 @@ void RTreeBenchmark::testInsertionPerformance()
     const int step_x = 1;
     const int max_y = 1000;
     const int step_y = 1;
-    int counter = 0;
-    Time::tval start = Time::stamp();
-    for (int y = 1; y <= max_y; y += step_y) { // equals row insertion into table
-        for (int x = 1; x <= max_x; x += step_x) { // equals cell insertion into row
-            m_tree.insert(QRect(x, y, step_x, step_y), 42);
-            ++counter;
+
+    QBENCHMARK {
+        for (int y = 1; y <= max_y; y += step_y) { // equals row insertion into table
+            for (int x = 1; x <= max_x; x += step_x) { // equals cell insertion into row
+                m_tree.insert(QRect(x, y, step_x, step_y), 42);
+            }
         }
     }
-    cout << "\t " << qPrintable(Time::printAverage(Time::elapsed(start), counter)) << endl;
 }
 
 void RTreeBenchmark::testRowInsertionPerformance()
 {
-    cout << "Row insertion performance test (5 rows at row 1)..." << endl;
-    Time::tval start = Time::stamp();
-    m_tree.insertRows(1, 5);
-    cout << "\t " << qPrintable(Time::printAverage(Time::elapsed(start), 1)) << endl;
+    QBENCHMARK {
+        m_tree.insertRows(1, 5);
+    }
 }
 
 void RTreeBenchmark::testColumnInsertionPerformance()
 {
-    cout << "Column insertion performance test (5 columns at column 1)..." << endl;
-    Time::tval start = Time::stamp();
-    m_tree.insertColumns(1, 5);
-    cout << "\t " << qPrintable(Time::printAverage(Time::elapsed(start), 1)) << endl;
+    QBENCHMARK {
+        m_tree.insertColumns(1, 5);
+    }
 }
 
 void RTreeBenchmark::testRowDeletionPerformance()
 {
-    cout << "Row deletion performance test (5 rows at row 1)..." << endl;
-    Time::tval start = Time::stamp();
-    m_tree.removeRows(1, 5);
-    cout << "\t " << qPrintable(Time::printAverage(Time::elapsed(start), 1)) << endl;
+    QBENCHMARK {
+        m_tree.removeRows(1, 5);
+    }
 }
 
 void RTreeBenchmark::testColumnDeletionPerformance()
 {
-    cout << "Column deletion performance test (5 columns at column 1)..." << endl;
-    Time::tval start = Time::stamp();
-    m_tree.removeColumns(1, 5);
-    cout << "\t " << qPrintable(Time::printAverage(Time::elapsed(start), 1)) << endl;
-
+    QBENCHMARK {
+        m_tree.removeColumns(1, 5);
+    }
 }
 
 void RTreeBenchmark::testLookupPerformance()
 {
-    cout << "Lookup performance test..." << endl;
-    Time::tval start = Time::stamp();
     int counter = 0;
     const int max_x = 100;
     const int step_x = 1;
     const int max_y = 1000;
     const int step_y = 1;
-    for (int y = 1; y <= max_y; y += step_y) {
-        for (int x = 1; x <= max_x; x += step_x) {
-            if (!m_tree.contains(QPoint(x, y)).isEmpty()) counter++;
+    QBENCHMARK {
+        for (int y = 1; y <= max_y; y += step_y) {
+            for (int x = 1; x <= max_x; x += step_x) {
+                if (!m_tree.contains(QPoint(x, y)).isEmpty()) counter++;
+            }
         }
     }
-    cout << "\t " << qPrintable(Time::printAverage(Time::elapsed(start), counter)) << endl;
 }
 
 QTEST_MAIN(RTreeBenchmark)
