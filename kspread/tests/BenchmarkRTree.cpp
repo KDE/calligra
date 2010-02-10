b2b196e6c1f291743eac5c51d3008d224b22d9d9
Marijn Kruisselbrink <m.kruisselbrink@student.tue.nl>
2010-02-10 15:34:37 +0000

    change RTree benchmark to use QTestList
    svn path=/trunk/koffice/; revision=1088349

--- kspread/tests/BenchmarkRTree.cpp
+++ kspread/tests/BenchmarkRTree.cpp
@@ -16,10 +16,11 @@
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
    MA  02110-1301  USA
 */
+#include "BenchmarkRTree.h"
 
 #include <iostream>
 
-#include <QCoreApplication>
+#include <QtTest/QtTest>
 
 #include <kdebug.h>
 
@@ -31,13 +32,33 @@
 using namespace std;
 using namespace KSpread;
 
-int main(int argc, char** argv)
+void RTreeBenchmark::init()
 {
-    QCoreApplication app(argc, argv);
-
     RTree<double> tree;
+    m_tree = tree;
+    // insert some data in the RTree
+    const int max_x = 100;
+    const int step_x = 1;
+    const int max_y = 1000;
+    const int step_y = 1;
+    for (int y = 1; y <= max_y; y += step_y) { // equals row insertion into table
+        for (int x = 1; x <= max_x; x += step_x) { // equals cell insertion into row
+            m_tree.insert(QRect(x, y, step_x, step_y), 42);
+        }
+    }
+}
 
+void RTreeBenchmark::cleanup()
+{
+}
+
+void RTreeBenchmark::testInsertionPerformance()
+{
     cout << "Insertion performance test..." << endl;
+
+    // reset tree for this test
+    RTree<double> tree;
+    m_tree = tree;
 
     const int max_x = 100;
     const int step_x = 1;
@@ -47,40 +68,64 @@ int main(int argc, char** argv)
     Time::tval start = Time::stamp();
     for (int y = 1; y <= max_y; y += step_y) { // equals row insertion into table
         for (int x = 1; x <= max_x; x += step_x) { // equals cell insertion into row
-            tree.insert(QRect(x, y, step_x, step_y), 42);
+            m_tree.insert(QRect(x, y, step_x, step_y), 42);
             ++counter;
         }
     }
     cout << "\t " << qPrintable(Time::printAverage(Time::elapsed(start), counter)) << endl;
+}
 
+void RTreeBenchmark::testRowInsertionPerformance()
+{
     cout << "Row insertion performance test (5 rows at row 1)..." << endl;
-    start = Time::stamp();
-    tree.insertRows(1, 5);
+    Time::tval start = Time::stamp();
+    m_tree.insertRows(1, 5);
     cout << "\t " << qPrintable(Time::printAverage(Time::elapsed(start), 1)) << endl;
+}
 
+void RTreeBenchmark::testColumnInsertionPerformance()
+{
     cout << "Column insertion performance test (5 columns at column 1)..." << endl;
-    start = Time::stamp();
-    tree.insertColumns(1, 5);
+    Time::tval start = Time::stamp();
+    m_tree.insertColumns(1, 5);
     cout << "\t " << qPrintable(Time::printAverage(Time::elapsed(start), 1)) << endl;
+}
 
+void RTreeBenchmark::testRowDeletionPerformance()
+{
     cout << "Row deletion performance test (5 rows at row 1)..." << endl;
-    start = Time::stamp();
-    tree.removeRows(1, 5);
+    Time::tval start = Time::stamp();
+    m_tree.removeRows(1, 5);
     cout << "\t " << qPrintable(Time::printAverage(Time::elapsed(start), 1)) << endl;
+}
 
+void RTreeBenchmark::testColumnDeletionPerformance()
+{
     cout << "Column deletion performance test (5 columns at column 1)..." << endl;
-    start = Time::stamp();
-    tree.removeColumns(1, 5);
+    Time::tval start = Time::stamp();
+    m_tree.removeColumns(1, 5);
     cout << "\t " << qPrintable(Time::printAverage(Time::elapsed(start), 1)) << endl;
 
+}
+
+void RTreeBenchmark::testLookupPerformance()
+{
     cout << "Lookup performance test..." << endl;
-    start = Time::stamp();
-    counter = 0;
+    Time::tval start = Time::stamp();
+    int counter = 0;
+    const int max_x = 100;
+    const int step_x = 1;
+    const int max_y = 1000;
+    const int step_y = 1;
     for (int y = 1; y <= max_y; y += step_y) {
         for (int x = 1; x <= max_x; x += step_x) {
-            if (!tree.contains(QPoint(x, y)).isEmpty()) counter++;
+            if (!m_tree.contains(QPoint(x, y)).isEmpty()) counter++;
         }
     }
     cout << "\t " << qPrintable(Time::printAverage(Time::elapsed(start), counter)) << endl;
-    return 0;
 }
+
+QTEST_MAIN(RTreeBenchmark)
+
+#include "BenchmarkRTree.moc"
+
