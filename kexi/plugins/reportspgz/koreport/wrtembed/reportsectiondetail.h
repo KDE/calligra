1897688b8c70c89a577316a86f7ebfde77e35c45
Adam Pigg <adam@piggz.co.uk>
2010-02-12 16:54:42 +0000

    Krazy license fixes SVN_SILENT
    svn path=/trunk/koffice/; revision=1089184

--- kexi/plugins/reportspgz/koreport/wrtembed/reportsectiondetail.h
+++ kexi/plugins/reportspgz/koreport/wrtembed/reportsectiondetail.h
@@ -14,9 +14,7 @@
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- * Please contact info@openmfg.com with any questions on this license.
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef REPORTSECTIONDETAIL_H
