276cd80f2137222862339bd9fa30914be75a3169
Jarosław Staniek <staniek@kde.org>
2010-02-24 13:02:53 +0000

    SVN_SILENT copyright
    svn path=/trunk/koffice/; revision=1095514

--- kexi/migration/pqxx/pg_type.h
+++ kexi/migration/pqxx/pg_type.h
@@ -1,15 +1,22 @@
-//
-//
-// C++ Interface: pg_type
-//
-// Description:
-//
-//
-// Author: Adam Pigg <adam@piggz.fsnet.co.uk>, (C) 2003
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/* This file is part of the KDE project
+   Copyright (C) 2003 Adam Pigg <adam@piggz.co.uk>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+*/
+
 //boolean, 'true'/'false'
 #define BOOLOID   16
 
