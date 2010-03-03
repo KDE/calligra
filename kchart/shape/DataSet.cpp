KDE/calligra

commit 1a139aaaa5eec201fc98dce5771853e2c2becbed

Load the explode factor in the DataSet (as it can be specified per DataSet). I need information from other more experienced KChart hackers as how to translate the explode set in the PlotArea and caught in the Axis to be in the ChartProxyModel.
CCMAIL:johannes.simon@gmail.com
CCMAIL:inge@lysator.liu.se


svn path=/trunk/koffice/; revision=1098179

kchart/shape/DataSet.cpp
@@ -103,10 +103,13 @@ public:
     bool penIsSet;
     // Determines whether brush has been set
     bool brushIsSet;
+    bool pieExplodeFactorIsSet;
     QPen pen;
     QBrush brush;
+    int pieExplodeFactor;
     QMap<int, QPen> pens;
     QMap<int, QBrush> brushes;
+    QMap<int, int> pieExplodeFactors;
 
     int num;
 
@@ -561,6 +564,13 @@ void DataSet::setBrush( const QBrush &brush )
         d->kdChartModel->dataSetChanged( this );
 }
 
+void DataSet::setPieExplodeFactor( int factor )
+{
+    d->pieExplodeFactor = factor;
+    if( d->kdChartModel )
+        d->kdChartModel->dataSetChanged( this );
+}
+
 void DataSet::setPen( int section, const QPen &pen )
 {
     d->pens[ section ] = pen;
@@ -573,6 +583,13 @@ void DataSet::setBrush( int section, const QBrush &brush )
     d->brushes[ section ] = brush;
     if ( d->kdChartModel )
         d->kdChartModel->dataSetChanged( this, KDChartModel::BrushDataRole, section );
+}
+
+void DataSet::setPieExplodeFactor( int section, int factor )
+{
+    d->pieExplodeFactors[ section ] = factor;
+    if( d->kdChartModel )
+        d->kdChartModel->dataSetChanged(this, KDChartModel::ExplodeRole, section );
 }
 
 QColor DataSet::color() const
@@ -962,11 +979,6 @@ bool loadBrushAndPen(KoShapeLoadingContext &context, const KoXmlElement &n, QBru
     styleStack.clear();
     odfLoadingContext.fillStyleStack( n, KoXmlNS::chart, "style-name", "chart" );
 
-    //styleStack.setTypeProperties( "chart" );
-    // FIXME: Load Pie explode factors
-    //if ( styleStack.hasProperty( KoXmlNS::chart, "pie-offset" ) )
-    //    setPieExplodeFactor( dataSet, styleStack.property( KoXmlNS::chart, "pie-offset" ).toInt() );
-
     brushLoaded = false;                                                                                                                                                 
     penLoaded = false; 
 
@@ -1028,6 +1040,8 @@ bool DataSet::loadOdf( const KoXmlElement &n,
             setPen( pen );
         if(brushLoaded)
             setBrush( brush );
+        if(styleStack.hasProperty(KoXmlNS::chart, "pie-offset"))
+            setPieExplodeFactor( styleStack.property( KoXmlNS::chart, "pie-offset"  ).toInt() );
     }
 
     if ( n.hasAttributeNS( KoXmlNS::chart, "values-cell-range-address" ) ) {
@@ -1056,8 +1070,18 @@ bool DataSet::loadOdf( const KoXmlElement &n,
             setPen( loadedDataPointCount, pen );
         if(brushLoaded)
             setBrush( loadedDataPointCount, brush );
+
+        //load pie explode factor
+        styleStack.save();
+        styleStack.clear();
+        odfLoadingContext.fillStyleStack(m, KoXmlNS::chart, "style-name", "chart");
+        styleStack.setTypeProperties("chart");
+        if(styleStack.hasProperty( KoXmlNS::chart, "pie-offset"))
+            setPieExplodeFactor( loadedDataPointCount, styleStack.property( KoXmlNS::chart, "pie-offset" ).toInt() );
+        styleStack.restore();
+
         ++loadedDataPointCount;
     }
-    
+
     return true;
 }

kchart/shape/DataSet.h
@@ -104,6 +104,9 @@ public:
     void setColor( const QColor &color );
     void setNumber( int num );
 
+    void setPieExplodeFactor( int factor );
+    void setPieExplodeFactor( int section, int factor );
+
     void setShowMeanValue( bool b );
     void setMeanValuePen( const QPen &pen );
 

kchart/shape/KDChartModel.h
@@ -68,7 +68,8 @@ public:
         CategoryDataRole,
         CustomDataRole,
         BrushDataRole,
-        PenDataRole
+        PenDataRole,
+        ExplodeRole
     };
     
     /**

commit 80b73603021755e372dd6b4109e7b76e8211370d

Fix assert introduced with r1098179
[KCrash Handler]
#12 0xb5d59a63 in qt_assert (assertion=0xb73ce090 "toIndex <= (int)m_stack.count()", file=0xb73ce034 "/home/kdab/src/kde/trunk/koffice/libs/odf/KoStyleStack.cpp", line=74) at /home/kdab/src/qt/src/corelib/global/qglobal.cpp:2011
#13 0xb739ee1a in KoStyleStack::restore (this=0xbf8652bc) at /home/kdab/src/kde/trunk/koffice/libs/odf/KoStyleStack.cpp:74
#14 0xb0ec863d in KChart::DataSet::loadOdf (this=0x92a9508, n=..., context=...) at /home/kdab/src/kde/trunk/koffice/kchart/shape/DataSet.cpp:1081
#15 0xb0ed8118 in KChart::ChartProxyModel::loadOdf (this=0x93d1ae8, element=..., context=...) at /home/kdab/src/kde/trunk/koffice/kchart/shape/ChartProxyModel.cpp:484
#16 0xb0ed3487 in KChart::PlotArea::loadOdf (this=0x94b76b8, plotAreaElement=..., context=...) at /home/kdab/src/kde/trunk/koffice/kchart/shape/PlotArea.cpp:653

svn path=/trunk/koffice/; revision=1098431

kchart/shape/DataSet.cpp
@@ -1073,7 +1073,6 @@ bool DataSet::loadOdf( const KoXmlElement &n,
 
         //load pie explode factor
         styleStack.save();
-        styleStack.clear();
         odfLoadingContext.fillStyleStack(m, KoXmlNS::chart, "style-name", "chart");
         styleStack.setTypeProperties("chart");
         if(styleStack.hasProperty( KoXmlNS::chart, "pie-offset"))