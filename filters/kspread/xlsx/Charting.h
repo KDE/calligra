cdcf1176f7ff8a0b02752b3a240c3ce61820e80f
Sebastian Sauer <mail@dipe.org>
2010-03-10 16:21:47 +0000

    to copy+paste the license-header is not always a good idea. Use the same header that is used in sidewinder and partly in xlsx (e.g. ColorConversion) which are LGPLv2+ rather then LGPLv2.1 SVN_SILENT
    svn path=/trunk/koffice/; revision=1101689

--- filters/kspread/xlsx/Charting.h
+++ filters/kspread/xlsx/Charting.h
@@ -1,25 +1,21 @@
 /*
- * This file is part of Office 2007 Filters for KOffice
+ *  Copyright (c) 2010 Sebastian Sauer <sebsauer@kdab.com>
  *
- * Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- * Contact: Sebastian Sauer sebsauer@kdab.com
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * version 2.1 as published by the Free Software Foundation.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA
- *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
+
 #ifndef CHARTING_H
 #define CHARTING_H
 
