1397e6575cee4099c86a00635edbf7dae570916a
Cyrille Berger <cyb@lepi.org>
2010-03-13 17:46:20 +0000

    update version to beta 1
    svn path=/trunk/koffice/; revision=1102808

--- kplato/kptaboutdata.h
+++ kplato/kptaboutdata.h
@@ -30,7 +30,7 @@ namespace KPlato
 {
 
 static const char* KPLATO_DESCRIPTION=I18N_NOOP("KPlato - KDE Planning Tool");
-static const char* KPLATO_VERSION="0.8.71";
+static const char* KPLATO_VERSION="0.8.81";
 
 KAboutData * newAboutData()
 {
