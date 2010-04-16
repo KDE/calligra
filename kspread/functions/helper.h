25d645abc3cac8c558873e320ff1d6ca15197e05
Sebastian Sauer <mail@dipe.org>
2010-04-16 18:27:09 +0000

    Fix the remaining issues with the 30/360 US calculation.
    see for the testcase that passes now http://plugtest.opendocsociety.org/doku.php?id=scenarios:20100415:yearfrac
    see http://qa.openoffice.org/issues/show_bug.cgi?id=74069 for the matching (still open) for the oocalc bugreport
    
    
    svn path=/branches/work/koffice-ko/; revision=1115527

--- kspread/functions/helper.h
+++ kspread/functions/helper.h
@@ -46,7 +46,7 @@ KSPREAD_EXPORT int daysBetweenDates(QDate const & date1, QDate const & date2, in
 
 // ODF
 KSPREAD_EXPORT int days360(const QDate& _date1, const QDate& _date2, bool european);
-KSPREAD_EXPORT int days360(int day1, int month1, int year1, bool leapYear1, int  day2, int month2, int year2, bool usa);
+KSPREAD_EXPORT int days360(int day1, int month1, int year1, bool leapYear1, int  day2, int month2, int year2, bool leapYear2, bool usa);
 KSPREAD_EXPORT long double yearFrac(const QDate& refDate, const QDate& startDate, const QDate& endDate, int basis);
 KSPREAD_EXPORT long double duration(const QDate& refDate, const QDate& settlement, const QDate& maturity, const long double& coup_, const long double& yield_, const int& freq, const int& basis, const long double& numOfCoups);
 KSPREAD_EXPORT long double pow1p(const long double& x, const long double& y);
