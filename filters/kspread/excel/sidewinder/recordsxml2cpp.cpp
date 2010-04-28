eb8454dd1161e67d4d48488c7318cb4695f7c0ed
Marijn Kruisselbrink <m.kruisselbrink@student.tue.nl>
2010-04-28 21:07:29 +0000

    make sure the generated records.cpp compiles without warnings
    svn path=/trunk/koffice/; revision=1120316

--- filters/kspread/excel/sidewinder/recordsxml2cpp.cpp
+++ filters/kspread/excel/sidewinder/recordsxml2cpp.cpp
@@ -539,7 +539,12 @@ void processRecordForImplementation(QDomElement e, QTextStream& out)
     }
 
     // setData method
-    out << "void " << className << "::setData( unsigned size, const unsigned char* data, const unsigned int* )\n{\n";
+    bool hasFields = !fieldNodes.isEmpty();
+    if (hasFields) {
+        out << "void " << className << "::setData( unsigned size, const unsigned char* data, const unsigned int* )\n{\n";
+    } else {
+        out << "void " << className << "::setData( unsigned, const unsigned char*, const unsigned int* )\n{\n";
+    }
     if (e.elementsByTagName("if").size() > 0 || e.elementsByTagName("array").size() > 0 || containsStrings)
         out << "    unsigned curOffset;\n";
     if (containsStrings) {
