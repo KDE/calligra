0175f12181ad61c89960cf8c81c186e06664a6b3
Laurent Montel <montel@kde.org>
2010-05-09 10:23:27 +0000

    old directory was removed
    svn path=/trunk/koffice/; revision=1124542

--- kpresenter/Messages.sh
+++ kpresenter/Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 $EXTRACTRC `find . -name "*.ui" -o -name "*.rc"` >> rc.cpp || exit 11
-$XGETTEXT `find . -name "*.h" -o -name "*.cpp" | grep -v "/old"` -o $podir/kpresenter.pot
+$XGETTEXT `find . -name "*.h" -o -name "*.cpp" -o $podir/kpresenter.pot
