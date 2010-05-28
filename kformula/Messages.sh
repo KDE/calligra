0b262646670ec6e58e292336b5a3ea52a101a7eb
Laurent Montel <montel@kde.org>
2010-05-28 11:06:02 +0000

    Minor fix
    svn path=/trunk/koffice/; revision=1131544

--- kformula/Messages.sh
+++ kformula/Messages.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
 $PREPARETIPS >> rc.cpp
 $XGETTEXT *.h *.cpp -o $podir/kformula.pot
+rm -f rc.cpp
+
--- kformula/flake/Messages.sh
+++ kformula/flake/Messages.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 $EXTRACTRC *.ui  >> rc.cpp
 $XGETTEXT *.cpp *.h -o $podir/FormulaShape.pot
+rm -f rc.cpp
