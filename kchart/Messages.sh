82da87c2eded7b0eb0ee802fb20d21d6714b5140
Albert Astals Cid <tsdgeos@terra.es>
2010-06-17 22:19:23 +0000

    fix Messages.sh CCMAIL: inge@lysator.liu.se
    svn path=/trunk/koffice/; revision=1139259

--- kchart/Messages.sh
+++ kchart/Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 # TODO: extract Qt's tr() messages in kdchart/*
-$XGETTEXT *.cpp kchart_aboutdata.h dialogs/*.cpp commands/*.cpp -o $podir/kchart.pot
+$XGETTEXT *.cpp kchart_aboutdata.h dialogs/*.cpp -o $podir/kchart.pot
