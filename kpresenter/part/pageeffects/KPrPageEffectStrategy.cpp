84ceccea7993d6243a4e0b9a28689b92eb7d3298
Michael Drueing <michael@drueing.de>
2010-06-20 13:36:21 +0000

    Make KPresenter compile on Win32/MSVC
    svn path=/trunk/koffice/; revision=1140353

--- kpresenter/part/pageeffects/KPrPageEffectStrategy.cpp
+++ kpresenter/part/pageeffects/KPrPageEffectStrategy.cpp
@@ -23,6 +23,7 @@
 #include <QWidget>
 
 #include <KoXmlWriter.h>
+#include <KoXmlReader.h>
 #include <KoGenStyle.h>
 
 KPrPageEffectStrategy::KPrPageEffectStrategy( int subType, const char * smilType, const char *smilSubType, bool reverse, bool graphicsView )
