07a12c8aadb5168f5a55f4d990b44cbc8b41c155
Dag Andersen <danders@get2net.dk>
2010-07-01 11:17:50 +0000

    Don't load plugins in document, will be loaded in view anyway.
    svn path=/trunk/koffice/; revision=1144946

--- kplato/kptpart.cpp
+++ kplato/kptpart.cpp
@@ -79,7 +79,7 @@ Part::Part( QWidget *parentWidget, QObject *parent, bool singleViewMode )
         m_loadingTemplate( false ),
         m_viewlistModified( false )
 {
-    setComponentData( Factory::global() );
+    setComponentData( Factory::global(), false ); // Do not load plugins now (the view will load them)
     setTemplateType( "kplato_template" );
     m_config.setReadWrite( isReadWrite() || !isEmbedded() );
     // Add library translation files
