1707b605c306684c690b065dc462e984d1f62909
Marijn Kruisselbrink <m.kruisselbrink@student.tue.nl>
2010-07-26 08:55:44 +0000

    unbreak TestSelection. Sheet's in a Map have always been owned by the Map, but previously it by chance worked to create sheets on the stack, as long as the sheets went out of scope before the map did.
    svn path=/trunk/koffice/; revision=1154782

--- kspread/tests/TestSelection.cpp
+++ kspread/tests/TestSelection.cpp
@@ -33,13 +33,13 @@ using namespace KSpread;
 void TestSelection::initialize()
 {
     Map map;
-    Sheet sheet1(&map, "Sheet1");
-    map.addSheet(&sheet1);
-    Sheet sheet2(&map, "Sheet2");
-    map.addSheet(&sheet2);
+    Sheet* sheet1 = new Sheet(&map, "Sheet1");
+    map.addSheet(sheet1);
+    Sheet* sheet2 = new Sheet(&map, "Sheet2");
+    map.addSheet(sheet2);
     Canvas canvas(0);
     Selection selection(&canvas);
-    selection.setActiveSheet(&sheet1);
+    selection.setActiveSheet(sheet1);
 
     QVERIFY(!selection.isEmpty());
     QCOMPARE(selection.name(), QString("A1"));
@@ -49,24 +49,24 @@ void TestSelection::initialize()
     QVERIFY(!selection.isEmpty());
     QCOMPARE(selection.name(), QString("Sheet1!A1"));
 
-    selection.initialize(QPoint(2, 4), &sheet2);
+    selection.initialize(QPoint(2, 4), sheet2);
     QCOMPARE(selection.name(), QString("Sheet2!B4"));
 
     selection.initialize(QRect(3, 5, 2, 3));
     QCOMPARE(selection.name(), QString("Sheet1!C5:D7"));
 
-    Region region("A1:A4", &map, &sheet2);
+    Region region("A1:A4", &map, sheet2);
     selection.initialize(region);
     QCOMPARE(selection.name(), QString("Sheet2!A1:A4"));
 
-    region = Region("Sheet1!A2:A4", &map, &sheet2);
+    region = Region("Sheet1!A2:A4", &map, sheet2);
     selection.initialize(region);
     QCOMPARE(selection.name(), QString("Sheet1!A2:A4"));
 
     // reference mode:
     selection.startReferenceSelection();
 
-    region = Region("Sheet1!A2:A4;B3;C5", &map, &sheet2);
+    region = Region("Sheet1!A2:A4;B3;C5", &map, sheet2);
     selection.initialize(region);
     QCOMPARE(selection.name(), QString("A2:A4;Sheet2!B3;Sheet2!C5"));
     QCOMPARE(selection.activeSubRegionName(), QString("A2:A4;Sheet2!B3;Sheet2!C5"));
@@ -79,13 +79,13 @@ void TestSelection::initialize()
 
     selection.setActiveSubRegion(1, 2, 2);
     QCOMPARE(selection.activeSubRegionName(), QString("B2;Sheet2!C5"));
-    selection.initialize(QPoint(3, 3), &sheet2);
+    selection.initialize(QPoint(3, 3), sheet2);
     QCOMPARE(selection.name(), QString("A2:A4;Sheet2!C3"));
     QCOMPARE(selection.activeSubRegionName(), QString("Sheet2!C3"));
 
     selection.setActiveSubRegion(1, 0, 2);
     QCOMPARE(selection.activeSubRegionName(), QString());
-    selection.initialize(QPoint(4, 4), &sheet2);
+    selection.initialize(QPoint(4, 4), sheet2);
     QCOMPARE(selection.name(), QString("A2:A4;Sheet2!D4;Sheet2!C3"));
     QCOMPARE(selection.activeSubRegionName(), QString("Sheet2!D4"));
 
@@ -99,11 +99,11 @@ void TestSelection::initialize()
     selection.clear();
     selection.setActiveSubRegion(0, 0, 0);
     QCOMPARE(selection.activeSubRegionName(), QString());
-    selection.initialize(QPoint(1, 1), &sheet1);
+    selection.initialize(QPoint(1, 1), sheet1);
     QCOMPARE(selection.activeSubRegionName(), QString("A1"));
     selection.setActiveSubRegion(1, 0, 1);
     QCOMPARE(selection.activeSubRegionName(), QString());
-    selection.initialize(QPoint(2, 2), &sheet1);
+    selection.initialize(QPoint(2, 2), sheet1);
     QCOMPARE(selection.name(), QString("A1;B2"));
     QCOMPARE(selection.activeSubRegionName(), QString("B2"));
 }
@@ -111,13 +111,13 @@ void TestSelection::initialize()
 void TestSelection::update()
 {
     Map map;
-    Sheet sheet1(&map, "Sheet1");
-    map.addSheet(&sheet1);
-    Sheet sheet2(&map, "Sheet2");
-    map.addSheet(&sheet2);
+    Sheet *sheet1 = new Sheet(&map, "Sheet1");
+    map.addSheet(sheet1);
+    Sheet *sheet2 = new Sheet(&map, "Sheet2");
+    map.addSheet(sheet2);
     Canvas canvas(0);
     Selection selection(&canvas);
-    selection.setActiveSheet(&sheet1);
+    selection.setActiveSheet(sheet1);
 
     QVERIFY(!selection.isEmpty());
     QCOMPARE(selection.name(), QString("A1"));
@@ -127,15 +127,15 @@ void TestSelection::update()
     // normal mode:
 
     // init with location
-    selection.initialize(QPoint(1, 6), &sheet1);
+    selection.initialize(QPoint(1, 6), sheet1);
     QCOMPARE(selection.name(), QString("Sheet1!A6"));
 
     // init with region
-    selection.initialize(Region("A3", &map, &sheet1));
+    selection.initialize(Region("A3", &map, sheet1));
     QCOMPARE(selection.name(), QString("Sheet1!A3"));
 
     // init with location after init with region
-    selection.initialize(QPoint(1, 5), &sheet1);
+    selection.initialize(QPoint(1, 5), sheet1);
     QCOMPARE(selection.name(), QString("Sheet1!A5"));
 
     // TODO
@@ -143,7 +143,7 @@ void TestSelection::update()
     // reference mode:
     selection.startReferenceSelection();
 
-    Region region("Sheet1!A2:A4;B3;C5", &map, &sheet2);
+    Region region("Sheet1!A2:A4;B3;C5", &map, sheet2);
     selection.initialize(region);
     QCOMPARE(selection.name(), QString("A2:A4;Sheet2!B3;Sheet2!C5"));
 
@@ -181,13 +181,13 @@ void TestSelection::update()
 void TestSelection::extend()
 {
     Map map;
-    Sheet sheet1(&map, "Sheet1");
-    map.addSheet(&sheet1);
-    Sheet sheet2(&map, "Sheet2");
-    map.addSheet(&sheet2);
+    Sheet *sheet1 = new Sheet(&map, "Sheet1");
+    map.addSheet(sheet1);
+    Sheet *sheet2 = new Sheet(&map, "Sheet2");
+    map.addSheet(sheet2);
     Canvas canvas(0);
     Selection selection(&canvas);
-    selection.setActiveSheet(&sheet1);
+    selection.setActiveSheet(sheet1);
 
     QVERIFY(!selection.isEmpty());
     QCOMPARE(selection.name(), QString("A1"));
@@ -198,35 +198,35 @@ void TestSelection::extend()
     // reference mode:
     selection.startReferenceSelection();
 
-    Region region("Sheet1!A2:A4;B3;C5", &map, &sheet2);
+    Region region("Sheet1!A2:A4;B3;C5", &map, sheet2);
     selection.initialize(region);
     QCOMPARE(selection.name(), QString("A2:A4;Sheet2!B3;Sheet2!C5"));
 
     // Prepend new location.
     selection.setActiveSubRegion(0, 0);
     QCOMPARE(selection.activeSubRegionName(), QString());
-    selection.extend(QPoint(1, 1), &sheet2);
+    selection.extend(QPoint(1, 1), sheet2);
     QCOMPARE(selection.name(), QString("Sheet2!A1;A2:A4;Sheet2!B3;Sheet2!C5"));
     QCOMPARE(selection.activeSubRegionName(), QString("Sheet2!A1"));
 
     // Try to prepend new location. Prepending needs length = 0.
     selection.setActiveSubRegion(0, 2); //  results in: 0, 2, 0
     QCOMPARE(selection.activeSubRegionName(), QString("Sheet2!A1;A2:A4"));
-    selection.extend(QPoint(1, 1), &sheet1);
+    selection.extend(QPoint(1, 1), sheet1);
     QCOMPARE(selection.name(), QString("Sheet2!A1;A1;A2:A4;Sheet2!B3;Sheet2!C5"));
     QCOMPARE(selection.activeSubRegionName(), QString("Sheet2!A1;A1;A2:A4"));
 
     // Prepend new range.
     selection.setActiveSubRegion(0, 0);
     QCOMPARE(selection.activeSubRegionName(), QString());
-    selection.extend(QRect(1, 1, 2, 2), &sheet1);
+    selection.extend(QRect(1, 1, 2, 2), sheet1);
     QCOMPARE(selection.name(), QString("A1:B2;Sheet2!A1;A1;A2:A4;Sheet2!B3;Sheet2!C5"));
     QCOMPARE(selection.activeSubRegionName(), QString("A1:B2"));
 
     // Try to prepend new range. Prepending needs length = 0.
     selection.setActiveSubRegion(0, 3); //  results in: 0, 3, 0
     QCOMPARE(selection.activeSubRegionName(), QString("A1:B2;Sheet2!A1;A1"));
-    selection.extend(QRect(1, 2, 2, 2), &sheet2);
+    selection.extend(QRect(1, 2, 2, 2), sheet2);
     QCOMPARE(selection.name(), QString("A1:B2;Sheet2!A2:B3;Sheet2!A1;A1;A2:A4;Sheet2!B3;Sheet2!C5"));
     QCOMPARE(selection.activeSubRegionName(), QString("A1:B2;Sheet2!A2:B3;Sheet2!A1;A1"));
 
@@ -238,28 +238,28 @@ void TestSelection::extend()
     // Append new location.
     selection.setActiveSubRegion(1, 3, 3); // results in: 1, 2, 3
     QCOMPARE(selection.activeSubRegionName(), QString("Sheet2!B3;Sheet2!C5"));
-    selection.extend(QPoint(1, 1), &sheet2);
+    selection.extend(QPoint(1, 1), sheet2);
     QCOMPARE(selection.name(), QString("A2:A4;Sheet2!B3;Sheet2!C5;Sheet2!A1"));
     QCOMPARE(selection.activeSubRegionName(), QString("Sheet2!B3;Sheet2!C5;Sheet2!A1"));
 
     // Append new location.
     selection.setActiveSubRegion(4, 1, 3); // results in: 4, 0, 4
     QCOMPARE(selection.activeSubRegionName(), QString());
-    selection.extend(QPoint(1, 1), &sheet1);
+    selection.extend(QPoint(1, 1), sheet1);
     QCOMPARE(selection.name(), QString("A2:A4;Sheet2!B3;Sheet2!C5;Sheet2!A1;A1"));
     QCOMPARE(selection.activeSubRegionName(), QString("A1"));
 
     // Append new range.
     selection.setActiveSubRegion(5, 0); // results in: 5, 0, 5
     QCOMPARE(selection.activeSubRegionName(), QString());
-    selection.extend(QRect(1, 1, 2, 2), &sheet1);
+    selection.extend(QRect(1, 1, 2, 2), sheet1);
     QCOMPARE(selection.name(), QString("A2:A4;Sheet2!B3;Sheet2!C5;Sheet2!A1;A1;A1:B2"));
     QCOMPARE(selection.activeSubRegionName(), QString("A1:B2"));
 
     // Append new range.
     selection.setActiveSubRegion(5, 1, 6);
     QCOMPARE(selection.activeSubRegionName(), QString("A1:B2"));
-    selection.extend(QRect(1, 2, 2, 2), &sheet2);
+    selection.extend(QRect(1, 2, 2, 2), sheet2);
     QCOMPARE(selection.name(), QString("A2:A4;Sheet2!B3;Sheet2!C5;Sheet2!A1;A1;A1:B2;Sheet2!A2:B3"));
     QCOMPARE(selection.activeSubRegionName(), QString("A1:B2;Sheet2!A2:B3"));
 
@@ -271,14 +271,14 @@ void TestSelection::extend()
     // Insert new location.
     selection.setActiveSubRegion(0, 3, 1);
     QCOMPARE(selection.activeSubRegionName(), QString("A2:A4;Sheet2!B3;Sheet2!C5"));
-    selection.extend(QPoint(1, 1), &sheet2);
+    selection.extend(QPoint(1, 1), sheet2);
     QCOMPARE(selection.name(), QString("A2:A4;Sheet2!B3;Sheet2!A1;Sheet2!C5"));
     QCOMPARE(selection.activeSubRegionName(), QString("A2:A4;Sheet2!B3;Sheet2!A1;Sheet2!C5"));
 
     // Insert new range.
     selection.setActiveSubRegion(1, 3, 3);
     QCOMPARE(selection.activeSubRegionName(), QString("Sheet2!B3;Sheet2!A1;Sheet2!C5"));
-    selection.extend(QRect(1, 1, 2, 2), &sheet1);
+    selection.extend(QRect(1, 1, 2, 2), sheet1);
     QCOMPARE(selection.name(), QString("A2:A4;Sheet2!B3;Sheet2!A1;Sheet2!C5;A1:B2"));
     QCOMPARE(selection.activeSubRegionName(), QString("Sheet2!B3;Sheet2!A1;Sheet2!C5;A1:B2"));
 }
@@ -286,13 +286,13 @@ void TestSelection::extend()
 void TestSelection::activeElement()
 {
     Map map;
-    Sheet sheet1(&map, "Sheet1");
-    map.addSheet(&sheet1);
-    Sheet sheet2(&map, "Sheet2");
-    map.addSheet(&sheet2);
+    Sheet *sheet1 = new Sheet(&map, "Sheet1");
+    map.addSheet(sheet1);
+    Sheet *sheet2 = new Sheet(&map, "Sheet2");
+    map.addSheet(sheet2);
     Canvas canvas(0);
     Selection selection(&canvas);
-    selection.setActiveSheet(&sheet1);
+    selection.setActiveSheet(sheet1);
 
     QVERIFY(!selection.isEmpty());
     QCOMPARE(selection.name(), QString("A1"));
@@ -304,28 +304,28 @@ void TestSelection::activeElement()
     selection.startReferenceSelection();
     QVERIFY(!selection.activeElement());
 
-    selection.initialize(Region("A3:A4;B2;C4:D5", &map, &sheet1));
-    QCOMPARE(selection.name(&sheet1), QString("A3:A4;B2;C4:D5"));
+    selection.initialize(Region("A3:A4;B2;C4:D5", &map, sheet1));
+    QCOMPARE(selection.name(sheet1), QString("A3:A4;B2;C4:D5"));
     QVERIFY(!selection.activeElement());
 
-    selection.extend(QRect(1, 1, 4, 4), &sheet1);
-    QCOMPARE(selection.name(&sheet1), QString("A3:A4;B2;C4:D5;A1:D4"));
+    selection.extend(QRect(1, 1, 4, 4), sheet1);
+    QCOMPARE(selection.name(sheet1), QString("A3:A4;B2;C4:D5;A1:D4"));
     QVERIFY(!selection.activeElement());
 
     selection.update(QPoint(5, 5));
-    QCOMPARE(selection.name(&sheet1), QString("A3:A4;B2;C4:D5;A1:E5"));
+    QCOMPARE(selection.name(sheet1), QString("A3:A4;B2;C4:D5;A1:E5"));
     QVERIFY(!selection.activeElement());
 
-    selection.initialize(QPoint(5, 6), &sheet1);
-    QCOMPARE(selection.name(&sheet1), QString("E6"));
+    selection.initialize(QPoint(5, 6), sheet1);
+    QCOMPARE(selection.name(sheet1), QString("E6"));
     QVERIFY(!selection.activeElement());
 
-    selection.initialize(QRect(1, 1, 2, 1), &sheet1);
-    QCOMPARE(selection.name(&sheet1), QString("A1:B1"));
+    selection.initialize(QRect(1, 1, 2, 1), sheet1);
+    QCOMPARE(selection.name(sheet1), QString("A1:B1"));
     QVERIFY(!selection.activeElement());
 
-    selection.extend(QPoint(1, 3), &sheet1);
-    QCOMPARE(selection.name(&sheet1), QString("A1:B1;A3"));
+    selection.extend(QPoint(1, 3), sheet1);
+    QCOMPARE(selection.name(sheet1), QString("A1:B1;A3"));
     QVERIFY(!selection.activeElement());
     //END Leave the active element unset.
 }
@@ -333,13 +333,13 @@ void TestSelection::activeElement()
 void TestSelection::referenceSelectionMode()
 {
     Map map;
-    Sheet sheet1(&map, "Sheet1");
-    map.addSheet(&sheet1);
-    Sheet sheet2(&map, "Sheet2");
-    map.addSheet(&sheet2);
+    Sheet *sheet1 = new Sheet(&map, "Sheet1");
+    map.addSheet(sheet1);
+    Sheet *sheet2 = new Sheet(&map, "Sheet2");
+    map.addSheet(sheet2);
     Canvas canvas(0);
     Selection selection(&canvas);
-    selection.setActiveSheet(&sheet1);
+    selection.setActiveSheet(sheet1);
 
     QVERIFY(!selection.isEmpty());
     QCOMPARE(selection.name(), QString("A1"));
@@ -348,7 +348,7 @@ void TestSelection::referenceSelectionMode()
     selection.startReferenceSelection();
     QVERIFY(selection.isEmpty());
 
-    selection.initialize(QPoint(2, 2), &sheet2);
+    selection.initialize(QPoint(2, 2), sheet2);
     QCOMPARE(selection.name(), QString("Sheet2!B2"));
 
     selection.extend(QPoint(1, 2));
@@ -360,7 +360,7 @@ void TestSelection::referenceSelectionMode()
     selection.endReferenceSelection();
     QCOMPARE(selection.name(), QString("A1"));
 
-    selection.initialize(QPoint(2, 2), &sheet1);
+    selection.initialize(QPoint(2, 2), sheet1);
     QCOMPARE(selection.name(), QString("B2"));
 
     // quit it again
@@ -374,7 +374,7 @@ void TestSelection::referenceSelectionMode()
     selection.startReferenceSelection();
     QVERIFY(selection.isEmpty());
 
-    selection.initialize(Region("A3", &map, &sheet1));
+    selection.initialize(Region("A3", &map, sheet1));
     QCOMPARE(selection.name(), QString("A3"));
 
     // set the active sub-region beyond the last range
@@ -389,13 +389,13 @@ void TestSelection::referenceSelectionMode()
 void TestSelection::covering()
 {
     Map map;
-    Sheet sheet1(&map, "Sheet1");
-    map.addSheet(&sheet1);
-    Sheet sheet2(&map, "Sheet2");
-    map.addSheet(&sheet2);
+    Sheet *sheet1 = new Sheet(&map, "Sheet1");
+    map.addSheet(sheet1);
+    Sheet *sheet2 = new Sheet(&map, "Sheet2");
+    map.addSheet(sheet2);
     Canvas canvas(0);
     Selection selection(&canvas);
-    selection.setActiveSheet(&sheet1);
+    selection.setActiveSheet(sheet1);
 
     QVERIFY(!selection.isEmpty());
     QCOMPARE(selection.name(), QString("A1"));
@@ -403,40 +403,40 @@ void TestSelection::covering()
     // normal mode:
     // convered ranges get removed
 
-    selection.initialize(Region("A3:A4;B2;C4:D5", &map, &sheet1));
-    QCOMPARE(selection.name(&sheet1), QString("A3:A4;B2;C4:D5"));
+    selection.initialize(Region("A3:A4;B2;C4:D5", &map, sheet1));
+    QCOMPARE(selection.name(sheet1), QString("A3:A4;B2;C4:D5"));
 
-    selection.extend(QRect(1, 1, 4, 4), &sheet1);
-    QCOMPARE(selection.name(&sheet1), QString("C4:D5;A1:D4"));
+    selection.extend(QRect(1, 1, 4, 4), sheet1);
+    QCOMPARE(selection.name(sheet1), QString("C4:D5;A1:D4"));
 
     selection.update(QPoint(5, 5));
-    QCOMPARE(selection.name(&sheet1), QString("A1:E5"));
+    QCOMPARE(selection.name(sheet1), QString("A1:E5"));
 
     // reference mode:
     // covered ranges get ignored
 
     selection.startReferenceSelection();
 
-    selection.initialize(Region("A3:A4;B2;C4:D5", &map, &sheet1));
+    selection.initialize(Region("A3:A4;B2;C4:D5", &map, sheet1));
     QCOMPARE(selection.name(), QString("A3:A4;B2;C4:D5"));
 
-    selection.extend(QRect(1, 1, 4, 4), &sheet1);
+    selection.extend(QRect(1, 1, 4, 4), sheet1);
     QCOMPARE(selection.name(), QString("A3:A4;B2;C4:D5;A1:D4"));
 
     selection.update(QPoint(5, 5));
-    QCOMPARE(selection.name(&sheet1), QString("A3:A4;B2;C4:D5;A1:E5"));
+    QCOMPARE(selection.name(sheet1), QString("A3:A4;B2;C4:D5;A1:E5"));
 }
 
 void TestSelection::splitting()
 {
     Map map;
-    Sheet sheet1(&map, "Sheet1");
-    map.addSheet(&sheet1);
-    Sheet sheet2(&map, "Sheet2");
-    map.addSheet(&sheet2);
+    Sheet *sheet1 = new Sheet(&map, "Sheet1");
+    map.addSheet(sheet1);
+    Sheet *sheet2 = new Sheet(&map, "Sheet2");
+    map.addSheet(sheet2);
     Canvas canvas(0);
     Selection selection(&canvas);
-    selection.setActiveSheet(&sheet1);
+    selection.setActiveSheet(sheet1);
 
     QVERIFY(!selection.isEmpty());
     QCOMPARE(selection.name(), QString("A1"));
@@ -444,21 +444,21 @@ void TestSelection::splitting()
     // normal mode:
     // ranges get split
 
-    selection.initialize(Region("A1:D5", &map, &sheet1));
-    QCOMPARE(selection.name(&sheet1), QString("A1:D5"));
+    selection.initialize(Region("A1:D5", &map, sheet1));
+    QCOMPARE(selection.name(sheet1), QString("A1:D5"));
 
-    selection.extend(QPoint(2, 2), &sheet1);
-    QCOMPARE(selection.name(&sheet1), QString("A3:D5;C2:D2;A2;A1:D1"));
+    selection.extend(QPoint(2, 2), sheet1);
+    QCOMPARE(selection.name(sheet1), QString("A3:D5;C2:D2;A2;A1:D1"));
 
     // reference mode:
     // ranges are not affected
 
     selection.startReferenceSelection();
 
-    selection.initialize(Region("A1:D5", &map, &sheet1));
+    selection.initialize(Region("A1:D5", &map, sheet1));
     QCOMPARE(selection.name(), QString("A1:D5"));
 
-    selection.extend(QPoint(2, 2), &sheet1);
+    selection.extend(QPoint(2, 2), sheet1);
     QCOMPARE(selection.name(), QString("A1:D5;B2"));
 }
 
