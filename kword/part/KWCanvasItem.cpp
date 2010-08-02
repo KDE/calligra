756d5e7d83f278e2e651a3f9b7cc7bb879948197
Thomas Zander <zander@kde.org>
2010-08-02 22:21:46 +0000

    Update copyrights
    when copying code from another class, also copy the original
    copyright owners lines.
    
    svn path=/trunk/koffice/; revision=1158519

--- kword/part/KWCanvasItem.cpp
+++ kword/part/KWCanvasItem.cpp
@@ -1,6 +1,8 @@
 /* This file is part of the KDE project
  *
  * Copyright (C) 2010 Boudewijn Rempt <boud@valdyas.org>
+ * Copyright (C) 2002-2006 David Faure <faure@kde.org>
+ * Copyright (C) 2005-2006 Thomas Zander <zander@kde.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
