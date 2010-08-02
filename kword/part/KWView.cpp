0f33e4d1fafe0f9c40dab31e11bdb798ce74ada1
Thomas Zander <zander@kde.org>
2010-08-02 19:19:26 +0000

    Fix version
    svn path=/branches/koffice/2.2/koffice/; revision=1158420

--- kword/part/KWView.cpp
+++ kword/part/KWView.cpp
@@ -916,7 +916,7 @@ KoPrintJob *KWView::createPrintJob()
 {
     KWPrintingDialog *dia = new KWPrintingDialog(this);
     dia->printer().setResolution(600);
-    dia->printer().setCreator("KWord 2.0");
+    dia->printer().setCreator("KWord 2.2");
     dia->printer().setFullPage(true); // ignore printer margins
     return dia;
 }
