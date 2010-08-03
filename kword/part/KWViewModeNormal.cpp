f9d4d61e9a5a9d6ce458f3f9d63faf3b19107ca1
Thomas Zander <zander@kde.org>
2010-08-03 16:38:41 +0000

    This reverts commit 1158735.
    I find it rather unpleasant that someone is telling me I need a review for
    a commit in the application I wrote and am the maintainer of. That is *not*
    now this works.
    
    svn path=/trunk/koffice/; revision=1158763

--- kword/part/KWViewModeNormal.cpp
+++ kword/part/KWViewModeNormal.cpp
@@ -25,9 +25,10 @@
 
 #include <kdebug.h>
 
+#define GAP 5
+
 KWViewModeNormal::KWViewModeNormal()
     : m_pageSpreadMode(false)
-    , m_gap(5)
 {
 }
 
@@ -98,7 +99,7 @@ QList<KWViewMode::ViewMap> KWViewModeNormal::clipRectToDocument(const QRect &vie
             break;
         if (m_pageSpreadMode) {
             if (page.pageSide() == KWPage::Left)
-                offsetX = page.width() + m_gap;
+                offsetX = page.width() + GAP;
             else
                 offsetX = 0.0;
         }
@@ -130,10 +131,10 @@ void KWViewModeNormal::updatePageCache()
             switch (page.pageSide()) {
             case KWPage::PageSpread:
                 if (last > 0)
-                    top += last + m_gap;
+                    top += last + GAP;
                 m_pageTops.append(top);
                 m_pageTops.append(top);
-                top += page.height() + m_gap;
+                top += page.height() + GAP;
                 width = qMax(width, page.width());
                 halfWidth = 0.0;
                 last = 0.0;
@@ -142,7 +143,7 @@ void KWViewModeNormal::updatePageCache()
             case KWPage::Left:
                 m_pageTops.append(top);
                 last = page.height();
-                halfWidth = page.width() + m_gap;
+                halfWidth = page.width() + GAP;
                 width = qMax(width, halfWidth);
                 bottom = top + last;
                 break;
@@ -153,7 +154,7 @@ void KWViewModeNormal::updatePageCache()
                 width = qMax(width, halfWidth + page.width());
                 halfWidth = 0.0;
                 bottom = top;
-                top += m_gap;
+                top += GAP;
                 break;
             default:
                 Q_ASSERT(false);
@@ -164,13 +165,13 @@ void KWViewModeNormal::updatePageCache()
         qreal top = 0.0;
         foreach (const KWPage &page, m_pageManager->pages()) {
             m_pageTops.append(top);
-            top += page.height() + m_gap;
+            top += page.height() + GAP;
             width = qMax(width, page.width());
         }
         bottom = top;
     }
-    if (bottom > m_gap)
-        bottom -= m_gap; // remove one too many added
+    if (bottom > GAP)
+        bottom -= GAP; // remove one too many added
     m_contents = QSizeF(width, bottom);
 }
 
@@ -227,9 +228,4 @@ QPointF KWViewModeNormal::viewToDocument(const QPointF & point) const
         yOffset -= m_pageTops[pageNumber -1];
 
     return QPointF(xOffset, page.offsetInDocument() + yOffset);
-}
-
-void KWViewModeNormal::setGap(int gap)
-{
-    m_gap = gap;
 }
