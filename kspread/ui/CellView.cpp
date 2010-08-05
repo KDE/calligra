fd1cc2aaad38353b8a9d1c0f3b77236e4068b926
Marijn Kruisselbrink <m.kruisselbrink@student.tue.nl>
2010-08-05 16:47:47 +0000

    don't come into a nearly infinite loop if none of the text is going to fit in the cell BUG: 246826
    svn path=/trunk/koffice/; revision=1159570

--- kspread/ui/CellView.cpp
+++ kspread/ui/CellView.cpp
@@ -1421,7 +1421,7 @@ QString CellView::textDisplaying(const QFontMetricsF& fm, const Cell& cell)
         start = qMin(d->displayText.length(), start);
         // Start out with the whole text, cut one character at a time, and
         // when the text finally fits, return it.
-        for (int i = start; i != 0; i--) {
+        for (int i = start; i >= 0; i--) {
             //Note that numbers are always treated as left-aligned since if we have to cut digits off, they should
             //always be the least significant ones at the end of the string
             if (hAlign == Style::Left || hAlign == Style::HAlignUndefined || isNumeric)
