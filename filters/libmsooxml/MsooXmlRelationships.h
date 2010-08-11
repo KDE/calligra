f875ea35bec46c7035b9d38490b9dada69efe92c
Carlos Licea <carlos_licea@hotmail.com>
2010-08-11 21:37:19 +0000

    Add the tableStyles relationship.
    svn path=/trunk/koffice/; revision=1162384

--- filters/libmsooxml/MsooXmlRelationships.h
+++ filters/libmsooxml/MsooXmlRelationships.h
@@ -56,6 +56,7 @@ private:
 namespace Relationships {
     static const char* const comments = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/comments";
     static const char* const commentAuthors = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/commentAuthors";
+    static const char* const tableStyles = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/tableStyles";
 }
 
 }
