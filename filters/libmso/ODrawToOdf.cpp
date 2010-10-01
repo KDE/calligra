6d604488953e0993406ed694b2eb8042d72682ee
Matus Uzak <matus.uzak@ixonos.com>
2010-10-01 10:19:57 +0000

    libmso: Corrected accidental error breaking the compilation.
    svn path=/trunk/koffice/; revision=1181531

--- filters/libmso/ODrawToOdf.cpp
+++ filters/libmso/ODrawToOdf.cpp
@@ -102,6 +102,7 @@ void ODrawToOdf::processGroup(const MSO::OfficeArtSpgrContainer& o, Writer& out)
 void ODrawToOdf::addGraphicStyleToDrawElement(Writer& out,
                                             const OfficeArtSpContainer& o)
 {
+    KoGenStyle style;
     const OfficeArtDggContainer* drawingGroup = 0;
     const OfficeArtSpContainer* master = 0; 
  
@@ -120,7 +121,6 @@ void ODrawToOdf::addGraphicStyleToDrawElement(Writer& out,
             master = client->getMasterShapeContainer(spid);
         }
     }
-    KoGenStyle style;
     const DrawStyle ds(*drawingGroup, master, &o);
     defineGraphicProperties(style, ds, out.styles);
 
