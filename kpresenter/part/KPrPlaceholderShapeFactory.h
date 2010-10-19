1dd8d778e022468db02efdb363595cbe1117f696
Thorsten Zachmann <t.zachmann@zagge.de>
2010-10-19 07:04:38 +0000

    o added fixed missing from commit
    svn path=/trunk/koffice/; revision=1187379

--- kpresenter/part/KPrPlaceholderShapeFactory.h
+++ kpresenter/part/KPrPlaceholderShapeFactory.h
@@ -31,7 +31,7 @@ public:
     virtual ~KPrPlaceholderShapeFactory();
 
     virtual KoShape *createDefaultShape(KoResourceManager *documentResources = 0) const;
-    virtual bool supports( const KoXmlElement & e ) const;
+    virtual bool supports( const KoXmlElement & e, KoShapeLoadingContext &context ) const;
 };
 
 #endif /* KPRPLACEHOLDERSHAPEFACTORY_H */
