c3bb23bc457abcbf9d64e72623c554faa4ec09ce
Jos van den Oever <jos@vandenoever.info>
2010-10-20 10:23:18 +0000

    Add script to validate ODF.
    svn path=/trunk/koffice/; revision=1187724

---
+++ tools/scripts/validateODF.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python -Qwarnall
+
+import sys, os, tempfile, time, signal, subprocess, re, lxml.etree, zipfile
+
+class odfvalidator:
+	def __init__(self):
+		path = sys.path[0]
+		self.relaxNGValidator = lxml.etree.RelaxNG( \
+				lxml.etree.parse(open(os.path.join(path, \
+				'OpenDocument-v1.2-cd05-schema.rng'), 'r')))
+	# returns error string on error, None otherwise
+	def validate(self, odtpath): 
+		zip = zipfile.ZipFile(odtpath, 'r')
+		err = self.validateFile(zip, 'content.xml',
+				self.relaxNGValidator)
+		if (err):
+			return err
+		err = self.validateFile(zip, 'styles.xml',
+				self.relaxNGValidator)
+		if (err):
+			return err
+		return None
+
+	def validateFile(self, zip, file, validator):
+		try:
+			xml = lxml.etree.XML(zip.read(file));
+		except lxml.etree.XMLSyntaxError as e:
+			return file + ':' + str(e)
+		if not validator.validate(xml):
+			return file + ':' + str(validator.error_log.last_error)
+
+if __name__ == '__main__':
+	validator = odfvalidator()
+	for f in sys.argv[1:]:
+		if os.path.isfile(f):
+			e = validator.validate(f)
+			if e:
+				print str(e)
