b5be7742bbed823a4b74ff7cbd716dbf99ba99fd
Alfredo Beaumont Sainz <alfredo.beaumont@gmail.com>
2010-11-05 07:18:06 +0000

    Disable AnnotationElement for now
    The reason is that with current loading process, we cannot be sure that the
    annotation element is properly handled and results in a valid MathML output,
    since we do not have a proper SemanticsElement to handle this yet.
    
    OOo compatibility is not harmed, which was the main reason for this element
    after all.
    
    CCMAIL: inge@lysator.liu.se
    
    svn path=/trunk/koffice/; revision=1193227

--- kformula/flake/ElementFactory.cpp
+++ kformula/flake/ElementFactory.cpp
@@ -108,9 +108,11 @@ BasicElement* ElementFactory::createElement( const QString& tagName,
           return new UnderOverElement( parent, UnderOver );
     else if ( tagName == "mmultiscripts" )
           return new MultiscriptElement( parent );
+    /*
     else if ( tagName == "annotation" )
           return new AnnotationElement( parent );
-    
+    */
+
     kWarning( DEBUGID ) << "Do not know how to create the following element: " << tagName;
     return new UnknownElement( parent );
 }
@@ -178,8 +180,10 @@ QString ElementFactory::elementName( ElementType type )
             return "mrow";
 	case Formula:
 	    return "math";
+        /*
         case Annotation:
-	    return "annotation";
+            return "annotation";
+        */
         default:
             kWarning( DEBUGID ) << "Invalid element type " << type;
     }
