33b1e91ea605e5af7fc79bb11df1050146eba20c
Marijn Kruisselbrink <mkruisselbrink@kde.org>
2010-12-06 05:29:48 -0800

    disallow name expension in the KoStore, as we don't want our image filesnames to be stored as part[md5sum].png/maindoc.xml

--- filters/kspread/excel/import/ExcelImport.cpp
+++ filters/kspread/excel/import/ExcelImport.cpp
@@ -237,6 +237,7 @@ KoFilter::ConversionStatus ExcelImport::convert(const QByteArray& from, const QB
     QBuffer storeBuffer; // TODO: use temporary file instead
     delete d->storeout;
     d->storeout = KoStore::createStore(&storeBuffer, KoStore::Write);
+    d->storeout->disallowNameExpansion();
 
     // open inputFile
     d->workbook = new Swinder::Workbook(d->storeout);
@@ -327,6 +328,7 @@ KoFilter::ConversionStatus ExcelImport::convert(const QByteArray& from, const QB
     storeBuffer.close();
 
     KoStore *store = KoStore::createStore(&storeBuffer, KoStore::Read);
+    store->disallowNameExpansion();
 
     KoXmlDocument xmlDoc = d->endMemoryXmlWriter(d->shapesXml);
     d->processEmbeddedObjects(xmlDoc.documentElement(), store);
