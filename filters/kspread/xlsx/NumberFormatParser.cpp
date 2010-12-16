f8c469fa1c99b696d9cda3a82bc2eb346e578dec
Mark Kretschmann <kretschmann@kde.org>
2010-12-16 17:16:08 +0100

    Remove unused variable.

--- filters/kspread/xlsx/NumberFormatParser.cpp
+++ filters/kspread/xlsx/NumberFormatParser.cpp
@@ -594,7 +594,6 @@ bool NumberFormatParser::isDateFormat(const QString& numberFormat)
         const bool isLong = i < numberFormat.length() - 1 && numberFormat[ i + 1 ] == c;
         const bool isLonger = isLong && i < numberFormat.length() - 2 && numberFormat[ i + 2 ] == c;
         const bool isLongest = isLonger && i < numberFormat.length() - 3 && numberFormat[ i + 3 ] == c;
-        const bool isWayTooLong = isLongest && i < numberFormat.length() - 4 && numberFormat[ i + 4 ] == c;
 
         switch (c) {
             // condition or color or locale...
