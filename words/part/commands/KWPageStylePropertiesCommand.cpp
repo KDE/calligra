607ab78888be7865414df4cd279580bdb5674b7f
Casper Boemann <cbo@boemann.dk>
2010-12-27 18:56:44 +0100

    fix compile

--- words/part/commands/KWPageStylePropertiesCommand.cpp
+++ words/part/commands/KWPageStylePropertiesCommand.cpp
@@ -135,7 +135,8 @@ KWPageStylePropertiesCommand::KWPageStylePropertiesCommand(KWDocument *document,
 void KWPageStylePropertiesCommand::redo()
 {
     QUndoCommand::redo();
-    m_style.priv()->copyProperties(m_styleAfter.priv());+    m_document->updatePagesForStyle(m_style);
+    m_style.priv()->copyProperties(m_styleAfter.priv());
+    m_document->updatePagesForStyle(m_style);
 #if 0
     m_document->m_frameLayout.createNewFramesForPage(m_page.pageNumber());
     m_document->firePageSetupChanged();
