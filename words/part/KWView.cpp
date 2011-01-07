5b38ed4a4098a72c5fc6c2161c5fd51998f50538
Inge Wallin <inge@lysator.liu.se>
2011-01-07 21:38:29 +0100

    Fix bug 262192: KWord crash when trying to print
    Don't crash when creating the printer dialog.
    
    BUG: 262192

--- words/part/KWView.cpp
+++ words/part/KWView.cpp
@@ -952,8 +952,8 @@ KoPrintJob *KWView::createPrintJob()
 {
     KWPrintingDialog *dia = new KWPrintingDialog(m_document, m_canvas->shapeManager(), this);
     dia->printer().setResolution(600);
-    dia->printer().setCreator(QString("Words %1.%2.$3").arg(KOffice::versionMajor(),
-        KOffice::versionMinor(), KOffice::versionRelease()));
+    dia->printer().setCreator(QString("KWord %1.%2.%3").arg(KOffice::versionMajor())
+                              .arg(KOffice::versionMinor()).arg(KOffice::versionRelease()));
     dia->printer().setFullPage(true); // ignore printer margins
     return dia;
 }
