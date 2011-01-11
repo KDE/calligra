f43393b6fe34b0e2e333c7f89f98929d4ea2d092
Lassi Nieminen <lassniem@gmail.com>
2011-01-11 14:26:51 +0200

    Fixed ooxml filter to be able to use fillRef more often.

--- filters/libmsooxml/MsooXmlCommonReaderDrawingMLImpl.h
+++ filters/libmsooxml/MsooXmlCommonReaderDrawingMLImpl.h
@@ -605,9 +605,11 @@ KoFilter::ConversionStatus MSOOXML_CURRENT_CLASS::read_grpSpPr()
             else if (qualifiedName() == QLatin1String("a:solidFill")) {
                 TRY_READ(solidFill)
                 // We must set the color immediately, otherwise currentColor may be modified by eg. ln
-                m_currentDrawStyle->addProperty("draw:fill", QLatin1String("solid"));
-                m_currentDrawStyle->addProperty("draw:fill-color", m_currentColor.name());
-                m_currentColor = QColor();
+                if (m_currentColor != QColor()) {
+                    m_currentDrawStyle->addProperty("draw:fill", QLatin1String("solid"));
+                    m_currentDrawStyle->addProperty("draw:fill-color", m_currentColor.name());
+                    m_currentColor = QColor();
+                }
             }
             else if ( qualifiedName() == QLatin1String("a:ln") ) {
                 TRY_READ(ln)
@@ -1192,10 +1194,12 @@ KoFilter::ConversionStatus MSOOXML_CURRENT_CLASS::read_spPr()
                 d->textBoxHasContent = true; // We count normal fill and gardient as content
 #endif
                 TRY_READ(solidFill)
-                // We must set the color immediately, otherwise currentColor may be modified by eg. ln
-                m_currentDrawStyle->addProperty("draw:fill", QLatin1String("solid"));
-                m_currentDrawStyle->addProperty("draw:fill-color", m_currentColor.name());
-                m_currentColor = QColor();
+                if (m_currentColor != QColor()) {
+                    // We must set the color immediately, otherwise currentColor may be modified by eg. ln
+                    m_currentDrawStyle->addProperty("draw:fill", QLatin1String("solid"));
+                    m_currentDrawStyle->addProperty("draw:fill-color", m_currentColor.name());
+                    m_currentColor = QColor();
+                }
             }
             else if ( qualifiedName() == QLatin1String("a:ln") ) {
                 TRY_READ(ln)
@@ -3942,7 +3946,9 @@ KoFilter::ConversionStatus MSOOXML_CURRENT_CLASS::read_ln()
         m_currentPen = QPen();
     }
 
-    KoOdfGraphicStyles::saveOdfStrokeStyle(*m_currentDrawStyle, *mainStyles, m_currentPen);
+    if (m_currentPen != QPen()) {
+        KoOdfGraphicStyles::saveOdfStrokeStyle(*m_currentDrawStyle, *mainStyles, m_currentPen);
+    }
 
     READ_EPILOGUE
 }
