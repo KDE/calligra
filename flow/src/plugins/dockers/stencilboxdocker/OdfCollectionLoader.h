81de9d9736515244541e794f3bb5d981d5723e60
Yue Liu <opuspace@gmail.com>
2011-01-29 01:45:17 +0800

    fixed tos for stencil box

--- flow/src/plugins/dockers/stencilboxdocker/OdfCollectionLoader.h
+++ flow/src/plugins/dockers/stencilboxdocker/OdfCollectionLoader.h
@@ -34,6 +34,7 @@ class KoShapeLoadingContext;
 class QTimer;
 class KoShape;
 class KoFilterManager;
+class KoResourceManager;
 
 class OdfCollectionLoader : public QObject
 {
@@ -60,6 +61,7 @@ class OdfCollectionLoader : public QObject
         QTimer* m_loadingTimer;
         KoOdfLoadingContext* m_loadingContext;
         KoShapeLoadingContext* m_shapeLoadingContext;
+        KoResourceManager* m_resourceManager;
         KoXmlElement m_body;
         KoXmlElement m_page;
         KoXmlElement m_shape;
