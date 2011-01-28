c81383510b520d1409a7bd6b33ab152758148a51
Mani Chandrasekar <maninc@gmail.com>
2011-01-28 16:46:07 +0530

    Removing RDF code from f-office. It is alread a plugin

--- tools/f-office/MainWindow.cpp
+++ tools/f-office/MainWindow.cpp
@@ -208,10 +208,6 @@ MainWindow::MainWindow(Splash *aSplash, QWidget *parent)
         m_slideNotesIcon(VIEW_NOTES_PIXMAP),
         m_presentationTool(0),
         m_storeButtonPreview(0)
-#ifdef Q_WS_MAEMO_5
-        ,m_foDocumentRdf(0),
-        m_rdfShortcut(0)
-#endif
 {
     setSplashScreen(aSplash);
     m_digitalSignatureDialog = 0;
@@ -1885,15 +1881,6 @@ void MainWindow::closeDocument()
 {
     KoAbstractApplication::closeDocument();
 
-#ifdef Q_WS_MAEMO_5
-    if (m_foDocumentRdf && documentType() == TextDocument) {
-        disconnect(m_rdfShortcut,SIGNAL(activated()),m_foDocumentRdf,SLOT(highlightRdf()));
-        delete m_rdfShortcut;
-        delete m_foDocumentRdf;
-        m_foDocumentRdf=0;
-        m_rdfShortcut=0;
-    }
-#endif
     if (m_digitalSignatureDialog && documentType() == TextDocument) {
         disconnect(m_ui->actionDigitalSignature,SIGNAL(triggered()),this,SLOT(showDigitalSignatureInfo()));
         QMenuBar* menu = menuBar();
@@ -2018,12 +2005,6 @@ bool MainWindow::doOpenDocument()
 // reimplemented
 void MainWindow::resourceChanged(int key, const QVariant& value)
 {
-#ifdef Q_WS_MAEMO_5
-    if (documentType() == TextDocument && m_ui->actionEdit->isChecked()) {
-        if (m_foDocumentRdf)
-            m_foDocumentRdf->findStatements(*textEditor()->cursor(), 1);
-    }
-#endif
     if (m_presentationTool && m_presentationTool->toolsActivated() && documentType() == PresentationDocument) {
         return;
     }
@@ -3378,26 +3359,6 @@ void MainWindow::showUiBeforeDocumentOpening(bool isNewDocument)
             connect(m_ui->actionDigitalSignature,SIGNAL(triggered()),this,SLOT(showDigitalSignatureInfo()));
         }
    }
-#ifdef Q_WS_MAEMO_5
-   if (documentType() == TextDocument)
-   {
-       /*
-        * intialising the rdf
-        */
-       KoStore::Backend backend = KoStore::Auto;
-       KoStore * store = KoStore::createStore(fileName, KoStore::Read, "", backend);
-
-       m_foDocumentRdf = new FoDocumentRdf(document(), textEditor());
-       if (!m_foDocumentRdf->loadOasis(store)) {
-           delete m_foDocumentRdf;
-           m_foDocumentRdf = 0;
-           return false;
-       }
-       delete store;
-       m_rdfShortcut= new QShortcut(QKeySequence(("Ctrl+R")),this);
-       connect(m_rdfShortcut,SIGNAL(activated()),m_foDocumentRdf,SLOT(highlightRdf()));
-   }
-#endif
 
     raiseWindow();
 }
