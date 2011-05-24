00471968c4b6f2a72dce120f5877782cb3a53881
Jaroslaw Staniek <staniek@kde.org>
2011-05-25 01:52:24 +0200

    apply 90f1aa253ca: [PATCH 08/13] Fix build on ARM architecture

--- kexi/kexiutils/kmessagewidget.cpp
+++ kexi/kexiutils/kmessagewidget.cpp
@@ -227,7 +227,7 @@ void KMessageWidgetPrivate::updateSnapShot()
 
 void KMessageWidgetPrivate::slotTimeLineChanged(qreal value)
 {
-    q->setFixedHeight(qMin(value * 2, 1.) * content->height());
+    q->setFixedHeight(qMin(value * 2, qreal(1.0)) * content->height());
     q->update();
 }
 
