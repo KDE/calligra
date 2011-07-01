e83eeb0f6d4ca4052b73a08af9170e09000b9a9a
Paul Mendez <paulestebanms@gmail.com>
2011-07-01 17:03:27 -0500

    Fix some code style

--- kpresenter/part/KPrCustomSlideShows.h
+++ kpresenter/part/KPrCustomSlideShows.h
@@ -115,7 +115,7 @@ public:
      * If it's 0 it will be prepended to the list, if it's the size it will be appended to the list.
      * Negative numbers can be used to count backwards.
      */
-    void addSlideToAll( KoPAPageBase *page, unsigned int position );
+    void addSlideToAll(KoPAPageBase *page, unsigned int position);
 
     /**
      * @brief Insert a  list of slides to the right of the given position of all the known custom SlideShows.
@@ -131,7 +131,7 @@ public:
      *
      * @param slide slide to be removed
      */
-    void removeSlideFromAll( KoPAPageBase *page );
+    void removeSlideFromAll(KoPAPageBase *page);
 
     /**
      * @brief Deletes all the ocurrencies of a given list of slides from all the known custom SlideShows
