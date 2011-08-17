KDE/calligra

commit d61d0a1a5d09e46edc581f31e067a155410c8942

Remove usage of PRETTY_FUNCTION
DO NOT USE THIS. It breaks compilation on Windows unless you do the
samething as the excel import and redefine it. But don't do that. Just
don't use it.

tools/cstester/visualimagecompare/CSTCompareView.cpp
@@ -91,7 +91,6 @@ bool CSTCompareView::open(const QString &inDir1, const QString &inDir2, const QS
 
 void CSTCompareView::keyPressEvent(QKeyEvent * event)
 {
-    qDebug() << __PRETTY_FUNCTION__ << event->key() << Qt::Key_Down << Qt::Key_Up << m_currentIndex << m_result.size();
     switch (event->key()) {
     case Qt::Key_B:
         if (m_dataIndex > 1) {
@@ -125,7 +124,7 @@ void CSTCompareView::keyPressEvent(QKeyEvent * event)
 int CSTCompareView::updateResult(int index)
 {
     QString result(m_result[index]);
-    qDebug() << __PRETTY_FUNCTION__ << result << index;
+}
 
     m_data = result.split(' ');
     if (m_data.size()) {

tools/cstester/visualimagecompare/CompareView.cpp
@@ -115,7 +115,6 @@ QImage CompareView::difference(const QImage &image1, const QImage &image2)
 
 void CompareView::keyPressEvent(QKeyEvent * event)
 {
-    qDebug() << __PRETTY_FUNCTION__ << event->key();
     switch (event->key()) {
     case Qt::Key_Space:
         m_tabBar->setCurrentIndex((m_tabBar->currentIndex() + 1) % 2);

commit f00fbcf18d00158406227005d439ddcec76db77a

Fix compilation
Commit d61d0a1a5d09e46edc581f31e067a155410c8942 accidentally
removed a closing brace.

tools/cstester/visualimagecompare/CSTCompareView.cpp
@@ -124,7 +124,6 @@ void CSTCompareView::keyPressEvent(QKeyEvent * event)
 int CSTCompareView::updateResult(int index)
 {
     QString result(m_result[index]);
-}
 
     m_data = result.split(' ');
     if (m_data.size()) {