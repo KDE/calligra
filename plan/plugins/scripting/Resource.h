844a7f4363e0ea576b8058d5b45147b08d736abd
Dag Andersen <danders@get2net.dk>
2011-08-23 13:18:38 +0200

    copyright

--- plan/plugins/scripting/Resource.h
+++ plan/plugins/scripting/Resource.h
@@ -1,5 +1,5 @@
 /* This file is part of the Calligra project
- * Copyright (c) 2008 Dag Andersen <danders@get2net>
+ * Copyright (c) 2008, 2011 Dag Andersen <danders@get2net>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
