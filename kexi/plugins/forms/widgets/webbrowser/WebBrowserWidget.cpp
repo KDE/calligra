08bfcbc0583ec44e0dc529fe060156ad70ed368f
Shreya Pandit <shreya@shreyapandit.com>
2011-09-06 08:06:25 +0530

    more feaatures for web widget

---
+++ kexi/plugins/forms/widgets/webbrowser/WebBrowserWidget.cpp
@@ -0,0 +1,216 @@
+/*
+    <This file is part of the KDE project>
+    Copyright (C) 2011  Shreya Pandit <shreya@shreyapandit.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#include "WebBrowserWidget.h"
+#include <QtWebKit>
+#include <QtWebKit/QWebHistory>
+#include <QtGui/QPushButton>
+#include <QtGui/QLabel>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QVBoxLayout>
+#include <QtCore/QUrl>
+#include <QtGui/QAction>
+#include <QTextBrowser>
+#include <QWebPage>
+#include <QtGui/QWidget>
+#include <QtGui/QApplication>
+
+WebBrowserWidget::WebBrowserWidget(QWidget *parent)
+        : QWidget(parent),KexiFormDataItemInterface()
+        ,KFormDesigner::FormWidgetInterface()
+	,m_readOnly(false)
+	,m_urlChanged_enabled(false)
+{
+    setFocusPolicy(Qt::StrongFocus);
+    setMinimumHeight(sizeHint().height());
+    setMinimumWidth(minimumHeight());
+    m_view = new QWebView(this);
+    m_view->load(QUrl("http://www.kde.org"));
+    m_backButton = new QPushButton("Back",this);
+    m_forward= new QPushButton("Forward",this);
+    m_reload=new QPushButton("Reload",this);
+    m_stop=new QPushButton("Stop",this);
+    h_layout = new QHBoxLayout;
+    h_layout->addWidget(m_backButton);
+    h_layout->addWidget(m_forward);
+    h_layout->addWidget(m_reload);
+    h_layout->addWidget(m_stop);
+    h_layout->addStretch();
+    v_layout = new QVBoxLayout();
+    v_layout->addWidget(m_view);
+    v_layout->addLayout(h_layout);
+    setLayout(v_layout);
+  
+    if(!designMode()){
+     m_pbar=new QProgressBar();
+     h_layout->addWidget(m_pbar);
+    }
+    else{
+      m_pbar=0;
+    }
+
+    connect(m_backButton,SIGNAL(clicked()),m_view,SLOT(back()));
+    connect(m_forward,SIGNAL(clicked()),m_view,SLOT(forward()));
+    connect(m_reload,SIGNAL(clicked()),m_view,SLOT(reload()));
+    connect(m_stop,SIGNAL(clicked()),m_view,SLOT(stop()));
+    connect(m_view,SIGNAL(loadProgress(int)),m_pbar,SLOT(setValue(int)));
+    connect(m_view,SIGNAL(loadFinished(bool)),SLOT(hide_bar()));
+  
+};
+
+WebBrowserWidget::~WebBrowserWidget()
+{
+
+}
+
+
+WebBrowserWidget::WebBrowserWidget()  
+{
+}
+
+
+void WebBrowserWidget::setDataSourcePartClass(const QString &ds) {
+        KexiFormDataItemInterface::setDataSourcePartClass(ds);
+    }
+ 
+
+
+void WebBrowserWidget::setDataSource(const QString &ds)
+{
+    KexiFormDataItemInterface::setDataSource(ds);
+    
+}
+
+void WebBrowserWidget::hide_bar()
+{
+    m_pbar->setVisible(false);    
+}
+
+void WebBrowserWidget::setUrl(const QString& url)
+{
+    setUrl(QUrl(url));
+}
+
+void WebBrowserWidget::setUrl(const QUrl& url)
+{
+    m_url=url;
+    m_view->load(m_url);
+}
+
+
+void WebBrowserWidget::updateToolBar()
+{
+    if(m_view->history()->canGoBack()) {
+      m_backButton->setEnabled(true);
+    }
+    if(m_view->history()->canGoForward()) {
+      m_forward->setEnabled(true);
+    }
+}
+
+
+void WebBrowserWidget::setZoomFactor(qreal factor)
+{
+    m_zoomFactor=factor; //! \todo ?
+}
+bool WebBrowserWidget::cursorAtStart()
+{
+    return true; //! \todo ?
+}
+
+bool WebBrowserWidget::cursorAtEnd()
+{
+    return true; //! \todo ?
+}
+
+
+QVariant WebBrowserWidget::value()
+{
+    if (dataSource().isEmpty()) {
+
+      return QVariant();
+    }
+    //db-aware mode
+    
+    return m_url;
+
+
+}
+
+bool WebBrowserWidget::valueIsNull()
+{
+    return (m_url).isEmpty();
+
+}
+void WebBrowserWidget::clear()
+{
+    setUrl("www.google.com");
+}
+
+
+
+void WebBrowserWidget::setInvalidState(const QString& displayText)
+{
+    Q_UNUSED(displayText);
+
+    if (!dataSource().isEmpty()) {
+        m_url.clear();
+    }
+    setReadOnly(true);
+}
+
+void WebBrowserWidget::setValueInternal(const QVariant &add, bool removeOld)
+{
+    Q_UNUSED(add); //compares  
+    Q_UNUSED(removeOld);
+
+    if (isReadOnly())
+        return;
+    m_urlChanged_enabled= false;		
+    
+    if (removeOld) { 			
+	setUrl(add.toString());  
+    }       
+    else{
+        setUrl(m_origValue.toString() + add.toString());
+     	
+    }
+
+    m_urlChanged_enabled = true;
+}
+
+bool WebBrowserWidget::valueIsEmpty()
+{
+    return false;
+}
+
+
+bool WebBrowserWidget::isReadOnly() const
+{
+    return m_readOnly;
+}
+
+
+void  WebBrowserWidget::setReadOnly(bool val)
+{
+    m_readOnly=val;
+}
+
+#include "WebBrowserWidget.moc"
---
+++ kexi/plugins/forms/widgets/webbrowser/WebBrowserWidget.h
@@ -0,0 +1,130 @@
+/* <This file is part of the KDE project>
+    Copyright (C) 2011  Shreya Pandit <shreya@shreyapandit.com>
+
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+*/
+
+
+#ifndef WEBBROWSERWIDGET_H
+#define WEBBROWSERWIDGET_H
+#include <QProgressBar>
+#include <QtGui/QWidget>
+#include <QtGui/QPushButton>
+#include "widgetfactory.h"	
+#include "container.h"
+#include <formeditor/FormWidgetInterface.h>
+#include <plugins/forms/kexiformdataiteminterface.h>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QVBoxLayout>
+#include <QtCore/QUrl>
+#include<QWebView>
+  
+class QLineEdit;
+class QVBoxLayout;
+class QLabel;
+class QAction;
+class QWebHistory;
+class ToolBar;
+class QHBoxLayout;
+class QLabel;
+class QUrl;
+class QProgressBar;
+class KUrlComboBox;
+
+class KEXIFORMUTILS_EXPORT WebBrowserWidget :  public QWidget, 
+					       public KexiFormDataItemInterface,
+					       public KFormDesigner::FormWidgetInterface
+{
+    Q_OBJECT
+    Q_PROPERTY(QString dataSource READ dataSource WRITE setDataSource)
+    Q_PROPERTY(QString dataSourcePartClass READ dataSourcePartClass WRITE setDataSourcePartClass)
+    Q_PROPERTY(QString url READ url WRITE setUrl)
+    Q_PROPERTY(qreal zoomFactor READ zoomFactor WRITE setZoomFactor)
+    Q_PROPERTY(QString title READ title)
+    
+public:
+    WebBrowserWidget();    
+    ~WebBrowserWidget();
+    WebBrowserWidget(QWidget *parent=0);
+
+    inline QString dataSource() const {
+        return KexiFormDataItemInterface::dataSource();
+    }
+    inline QString dataSourcePartClass() const {
+        return KexiFormDataItemInterface::dataSourcePartClass();
+    }
+
+    inline QString url() const {
+	
+	return m_url.toString();
+      
+    }
+    
+    inline QString title() const {
+	
+ 	return m_view->title();
+    }
+
+    inline qreal zoomFactor() const {
+	
+	return m_view->zoomFactor();
+    }
+    
+    virtual QVariant value();
+    virtual void setInvalidState(const QString& displayText);
+    virtual bool valueIsNull();
+    virtual bool valueIsEmpty();
+    virtual bool cursorAtStart();
+    virtual bool cursorAtEnd();
+    virtual void clear();
+    bool isReadOnly() const ;
+    virtual void setReadOnly(bool readOnly);  
+    QWebView* m_view;
+    void updateToolBar(); 
+
+public slots:
+    void setDataSource(const QString &ds);
+    void setDataSourcePartClass(const QString &ds);
+    void setUrl(const QString& url);
+    void setZoomFactor(qreal factor);
+    void hide_bar();
+    
+protected:
+    virtual void setValueInternal(const QVariant& add, bool removeOld); 
+    void setUrl(const QUrl& url);
+    bool m_readOnly;
+    QUrl m_url;
+
+private:
+    QLineEdit* m_lineEdit;
+    QLabel *m_label;
+    QVBoxLayout* v_layout;
+    QWebHistory* m_history;
+    qreal m_zoomFactor; 
+    QProgressBar* m_pbar;
+    bool  m_urlChanged_enabled;
+    QPushButton* m_backButton;
+    QPushButton* m_forward;
+    QHBoxLayout* h_layout;
+    QPushButton* m_reload;  
+    QPushButton* m_stop;  
+  
+};
+
+#endif 
+
+
