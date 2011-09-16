36f681be74b13dbfdb8b14c7516b0fbb48f72543
Boudewijn Rempt <boud@valdyas.org>
2011-09-16 14:25:47 +0200

    constify KoTextWriter a bit

--- libs/kotext/opendocument/KoTextWriter.cpp
+++ libs/kotext/opendocument/KoTextWriter.cpp
@@ -133,9 +133,9 @@ QString KoTextWriter::saveParagraphStyle(const QTextBlockFormat &blockFormat, co
     return generatedName;
 }
 
-void KoTextWriter::write(QTextDocument *document, int from, int to)
+void KoTextWriter::write(const QTextDocument *document, int from, int to)
 {
-    d->document = document;
+    d->document = const_cast<QTextDocument*>(document);
     d->styleManager = KoTextDocument(document).styleManager();
     d->changeTracker = KoTextDocument(document).changeTracker();
 
@@ -150,5 +150,5 @@ void KoTextWriter::write(QTextDocument *document, int from, int to)
     }
 
     QHash<QTextList *, QString> listStyles = d->saveListStyles(block, to);
-    d->writeBlocks(document, from, to, listStyles);
+    d->writeBlocks(d->document, from, to, listStyles);
 }
