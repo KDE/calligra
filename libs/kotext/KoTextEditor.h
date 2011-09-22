2a5bf58823290b7b28fe03d809d078e3c02783f6
Boudewijn Rempt <boud@valdyas.org>
2011-09-22 12:24:04 +0200

    Enable and implement KoTextEditor::setBlockFormat
    It's used in SimpleParagaphWidget::directionChangeReguested

--- libs/kotext/KoTextEditor.h
+++ libs/kotext/KoTextEditor.h
@@ -401,7 +401,7 @@ public slots:
 
 //    void setBlockCharFormat(const QTextCharFormat &format);
 
-//    void setBlockFormat(const QTextBlockFormat &format);
+   void setBlockFormat(const QTextBlockFormat &format);
 
 //    void setCharFormat(const QTextCharFormat &format);
 
