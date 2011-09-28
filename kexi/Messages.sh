df7bace9be0ab25ba85db527d30af196247a1af4
Jaroslaw Staniek <staniek@kde.org>
2011-09-28 23:04:07 +0200

    Kexi, reports: Updated messages retrieving, splitted for plugins

--- kexi/Messages.sh
+++ kexi/Messages.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
-EXCLUDE="-path ./doc -o -path ./plugins/importwizard -o -path ./tests -o -path ./scriptingplugins -o -path ./plugins/scripting -o -path ./plugins/reports -o -path ./plugins/relations -o -path ./3rdparty -o -path ./shapes"
-LIST="data/*.rc `find . \( \( $EXCLUDE \) -prune -o -name \*.ui \) -type f | grep -v -e '/\.'`"
+EXCLUDE="-path ./3rdparty -o -path ./doc -o -path ./examples -o -path ./tests -o -path ./plugins/relations -o -path ./mobile -o -path ./tools -o -path ./webforms -o -path ./plugins/forms/widgets/webbrowser -o -path ./plugins/forms/widgets/mapbrowser"
+LIST="`find . \( \( $EXCLUDE \) -prune -o -name \*.ui \) -type f | grep -v -e '/\.'`"
 if test -n "$LIST"; then
 	$EXTRACTRC $LIST >> rc.cpp
 fi
-LIST=`find . \( \( $EXCLUDE \) -prune -o -name \*.h -o -name \*.cpp \) -type f | grep -v -e '/\.' -e kexidswelcome.cpp`
-if test -n "$LIST"; then \
-	$XGETTEXT $LIST -o $podir/kexi.pot
+LIST=`find . \( \( $EXCLUDE \) -prune -o -name \*.h -o -name \*.cpp \) -type f` #| grep -v -e '/\.' -e kexidswelcome.cpp
+if test -n "$LIST"; then
+	echo $XGETTEXT $LIST -o $podir/kexi.pot
 fi
