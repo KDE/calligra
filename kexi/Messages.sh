ab73d55b6e6f1c308085de6290f09e8c9617cbbf
Jaroslaw Staniek <staniek@kde.org>
2011-09-30 01:59:40 +0200

    Exclude currently unused main/printing/ from translation

--- kexi/Messages.sh
+++ kexi/Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-EXCLUDE="-path ./3rdparty -o -path ./doc -o -path ./examples -o -path ./tests -o -path ./plugins/relations -o -path ./mobile -o -path ./tools -o -path ./webforms -o -path ./plugins/forms/widgets/webbrowser -o -path ./plugins/forms/widgets/mapbrowser"
+EXCLUDE="-path ./3rdparty -o -path ./doc -o -path ./examples -o -path ./tests -o -path ./plugins/relations -o -path ./main/printing -o -path ./mobile -o -path ./tools -o -path ./webforms -o -path ./plugins/forms/widgets/webbrowser -o -path ./plugins/forms/widgets/mapbrowser"
 LIST="`find . \( \( $EXCLUDE \) -prune -o -name \*.ui \) -type f | grep -v -e '/\.'`"
 if test -n "$LIST"; then
 	$EXTRACTRC $LIST >> rc.cpp
@@ -8,3 +8,4 @@ LIST=`find . \( \( $EXCLUDE \) -prune -o -name \*.h -o -name \*.cpp \) -type f` 
 if test -n "$LIST"; then
 	$XGETTEXT $LIST -o $podir/kexi.pot
 fi
+
