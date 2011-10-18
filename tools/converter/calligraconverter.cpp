6d71da0a3103eb3027e89650c81046d770fba98c
Dan Leinir Turthra Jensen <admin@leinir.dk>
2011-10-18 14:25:31 +0100

    Fix whitespace issues by applying kdelibs style using astyle
    This also closes the review for calligraconverter, as it
    addresses the final issues highlighted there (that is, the
    code style of the original code)
    
    REVIEW:102837

--- tools/converter/calligraconverter.cpp
+++ tools/converter/calligraconverter.cpp
@@ -45,75 +45,70 @@
 
 #include <calligraversion.h>
 
-bool convert( const KUrl & uIn, const QString & inputFormat, const KUrl & uOut, const QString & outputFormat, bool batch )
+bool convert(const KUrl &uIn, const QString &inputFormat, const KUrl &uOut, const QString &outputFormat, bool batch)
 {
     KoFilter::ConversionStatus status = KoFilter::OK;
-    if ( outputFormat == "application/pdf" ) {
+    if (outputFormat == "application/pdf") {
         QString error;
         KoDocument *doc = KMimeTypeTrader::self()->createPartInstanceFromQuery< KoDocument >(
-            inputFormat, 0, 0, QString(), QVariantList(), &error );
-        if ( doc ) {
-            doc->setCheckAutoSaveFile( false );
-            doc->setAutoErrorHandlingEnabled( false );
-            if ( doc->openUrl( uIn ) ) {
-                doc->setReadWrite( false );
-                KoPrintJob* printJob = 0;
-                Calligra::Tables::Doc* tdoc = qobject_cast< Calligra::Tables::Doc* >( doc );
-                if ( tdoc ) {
-                    Calligra::Tables::View* tview = new Calligra::Tables::View( 0, tdoc );
-                    tview->setActiveSheet( tdoc->map()->sheet( 0 ) );
+                              inputFormat, 0, 0, QString(), QVariantList(), &error);
+        if (doc) {
+            doc->setCheckAutoSaveFile(false);
+            doc->setAutoErrorHandlingEnabled(false);
+            if (doc->openUrl(uIn)) {
+                doc->setReadWrite(false);
+                KoPrintJob *printJob = 0;
+                Calligra::Tables::Doc *tdoc = qobject_cast< Calligra::Tables::Doc * >(doc);
+                if (tdoc) {
+                    Calligra::Tables::View *tview = new Calligra::Tables::View(0, tdoc);
+                    tview->setActiveSheet(tdoc->map()->sheet(0));
                     printJob = tview->createPdfPrintJob();
-                }
-                else {
-                    KoView* view = doc->createView();
+                } else {
+                    KoView *view = doc->createView();
                     printJob = view->createPdfPrintJob();
                 }
                 // We should now have a print job - but check to make sure
-                if ( printJob ) {
-                    printJob->printer().setOutputFileName( uOut.path() );
-                    printJob->printer().setOutputFormat( QPrinter::PdfFormat );
+                if (printJob) {
+                    printJob->printer().setOutputFileName(uOut.path());
+                    printJob->printer().setOutputFormat(QPrinter::PdfFormat);
                     printJob->startPrinting();
-                }
-                else {
+                } else {
                     status = KoFilter::StupidError;
                     qDebug() << "The document" << uIn.path()
-                    << "of format" << inputFormat
-                    << "was unable to provide a print job for PDF export";
+                             << "of format" << inputFormat
+                             << "was unable to provide a print job for PDF export";
                 }
-            }
-            else {
+            } else {
                 status = KoFilter::InvalidFormat;
                 qDebug() << "The document" << uIn.path()
-                    << "of format" << inputFormat
-                    << "failed to open with the error" << error;
+                         << "of format" << inputFormat
+                         << "failed to open with the error" << error;
             }
             doc->deleteLater();
             doc = 0;
-        }
-        else {
+        } else {
             status = KoFilter::WrongFormat;
         }
-    }
-    else {
-        KoFilterManager* manager = new KoFilterManager( uIn.path() );
-        manager->setBatchMode( batch );
-        QByteArray mime( outputFormat.toLatin1() );
-        status = manager->exportDocument( uOut.path(), mime );
+    } else {
+        KoFilterManager *manager = new KoFilterManager(uIn.path());
+        manager->setBatchMode(batch);
+        QByteArray mime(outputFormat.toLatin1());
+        status = manager->exportDocument(uOut.path(), mime);
         delete manager;
     }
     return status == KoFilter::OK;
 }
 
-int main( int argc, char **argv )
+int main(int argc, char **argv)
 {
-    KAboutData aboutData( "calligraconverter", 0, ki18n("CalligraConverter"), CALLIGRA_VERSION_STRING,
-                          ki18n("Calligra Document Converter"),
-                          KAboutData::License_GPL,
-                          ki18n("(c) 2001-2011 Calligra developers") );
-    aboutData.addAuthor(ki18n("David Faure"),KLocalizedString(), "faure@kde.org");
-    aboutData.addAuthor(ki18n("Nicolas Goutte"),KLocalizedString(), "goutte@kde.org");
-    aboutData.addAuthor(ki18n("Dan Leinir Turthra Jensen"),KLocalizedString(), "admin@leinir.dk");
-    KCmdLineArgs::init( argc, argv, &aboutData);
+    KAboutData aboutData("calligraconverter", 0, ki18n("CalligraConverter"), CALLIGRA_VERSION_STRING,
+                         ki18n("Calligra Document Converter"),
+                         KAboutData::License_GPL,
+                         ki18n("(c) 2001-2011 Calligra developers"));
+    aboutData.addAuthor(ki18n("David Faure"), KLocalizedString(), "faure@kde.org");
+    aboutData.addAuthor(ki18n("Nicolas Goutte"), KLocalizedString(), "goutte@kde.org");
+    aboutData.addAuthor(ki18n("Dan Leinir Turthra Jensen"), KLocalizedString(), "admin@leinir.dk");
+    KCmdLineArgs::init(argc, argv, &aboutData);
 
     KCmdLineOptions options;
     options.add("+in", ki18n("Input file"));
@@ -122,7 +117,7 @@ int main( int argc, char **argv )
     options.add("batch", ki18n("Batch mode: do not show dialogs"));
     options.add("interactive", ki18n("Interactive mode: show dialogs (default)"));
     options.add("mimetype <mime>", ki18n("Mimetype of the output file"));
-    KCmdLineArgs::addCmdLineOptions( options );
+    KCmdLineArgs::addCmdLineOptions(options);
 
     KApplication app;
 
@@ -130,70 +125,58 @@ int main( int argc, char **argv )
     KGlobal::locale()->insertCatalog("calligra");
 
     // Get the command line arguments which we have to parse
-    KCmdLineArgs *args= KCmdLineArgs::parsedArgs();
-    if ( args->count() == 2 )
-    {
-        KUrl uIn = args->url( 0 );
-        KUrl uOut = args->url( 1 );
+    KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+    if (args->count() == 2) {
+        KUrl uIn = args->url(0);
+        KUrl uOut = args->url(1);
 
         // Are we in batch mode or in interactive mode.
         bool batch = args->isSet("batch");
-        if ( args->isSet("interactive") )
+        if (args->isSet("interactive"))
             batch = false;
 
-        if ( args->isSet("backup") )
-        {
+        if (args->isSet("backup")) {
             // Code form koDocument.cc
             KIO::UDSEntry entry;
-            if ( KIO::NetAccess::stat( uOut, entry, 0L ) ) // this file exists => backup
-            {
-                kDebug() <<"Making backup...";;
-                KUrl backup( uOut );
-                backup.setPath( uOut.path() + '~' );
-                KIO::FileCopyJob *job = KIO::file_copy( uOut, backup, -1, KIO::Overwrite | KIO::HideProgressInfo);
+            if (KIO::NetAccess::stat(uOut, entry, 0L)) {   // this file exists => backup
+                kDebug() << "Making backup...";;
+                KUrl backup(uOut);
+                backup.setPath(uOut.path() + '~');
+                KIO::FileCopyJob *job = KIO::file_copy(uOut, backup, -1, KIO::Overwrite | KIO::HideProgressInfo);
                 job->exec();
             }
         }
 
 
-        KMimeType::Ptr inputMimetype = KMimeType::findByUrl( uIn );
-        if ( inputMimetype->name() == KMimeType::defaultMimeType() )
-        {
-            kError() << i18n("Mimetype for input file %1 not found!",uIn.prettyUrl()) << endl;
+        KMimeType::Ptr inputMimetype = KMimeType::findByUrl(uIn);
+        if (inputMimetype->name() == KMimeType::defaultMimeType()) {
+            kError() << i18n("Mimetype for input file %1 not found!", uIn.prettyUrl()) << endl;
             return 1;
         }
         KMimeType::Ptr outputMimetype;
-        if ( args->isSet("mimetype") )
-        {
+        if (args->isSet("mimetype")) {
             QString mime = args->getOption("mimetype");
-            outputMimetype = KMimeType::mimeType( mime );
-            if ( ! outputMimetype )
-            {
-                kError() << i18n("Mimetype not found %1",mime) << endl;
+            outputMimetype = KMimeType::mimeType(mime);
+            if (! outputMimetype) {
+                kError() << i18n("Mimetype not found %1", mime) << endl;
                 return 1;
             }
-        }
-        else
-        {
-            outputMimetype = KMimeType::findByUrl( uOut, 0, false, true /* file doesn't exist */ );
-            if ( outputMimetype->name() == KMimeType::defaultMimeType() )
-            {
+        } else {
+            outputMimetype = KMimeType::findByUrl(uOut, 0, false, true /* file doesn't exist */);
+            if (outputMimetype->name() == KMimeType::defaultMimeType()) {
                 kError() << i18n("Mimetype not found, try using the -mimetype option") << endl;
                 return 1;
             }
         }
 
-        QApplication::setOverrideCursor( Qt::WaitCursor );
-        bool ok = convert( uIn, inputMimetype->name(), uOut, outputMimetype->name(), batch );
-        QTimer::singleShot( 0, &app, SLOT(quit()) );
+        QApplication::setOverrideCursor(Qt::WaitCursor);
+        bool ok = convert(uIn, inputMimetype->name(), uOut, outputMimetype->name(), batch);
+        QTimer::singleShot(0, &app, SLOT(quit()));
         app.exec();
         QApplication::restoreOverrideCursor();
-        if ( ok )
-        {
+        if (ok) {
             return 0;
-        }
-        else
-        {
+        } else {
             kError() << i18n("*** The conversion failed! ***") << endl;
             return 2;
         }
