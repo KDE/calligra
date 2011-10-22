KDE/calligra

commit 78a8b341f6905c6f70dcba24fbd59d331b1f185b

Use the same structure for KoBorder as in KoTableBorderStyle... That way, we will be able to refactor even more and share more code
REVIEW: 102724

filters/libmsooxml/MsooXmlDrawingTableStyleReader.cpp
@@ -907,7 +907,7 @@ KoFilter::ConversionStatus MsooXmlDrawingTableStyleReader::read_Table_ln()
     }
 
     TRY_READ_ATTR_WITHOUT_NS(w) //width
-    m_currentBorder.width = EMU_TO_POINT(w.toDouble());
+    m_currentBorder.innerPen.setWidthF(EMU_TO_POINT(w.toDouble()));
 
     while(!atEnd()) {
         readNext();
@@ -916,12 +916,14 @@ KoFilter::ConversionStatus MsooXmlDrawingTableStyleReader::read_Table_ln()
             if(QUALIFIED_NAME_IS(solidFill)) {
                 TRY_READ(solidFill);
                 m_currentBorder.style = KoBorder::BorderSolid;
-                m_currentBorder.color = m_currentColor;
+                m_currentBorder.innerPen.setColor(m_currentColor);
+                m_currentBorder.outerPen.setColor(m_currentColor);
             }
             else if (QUALIFIED_NAME_IS(prstDash)) {
                 attrs = attributes();
                 //TODO find out how other colors are handled
-                m_currentBorder.color = Qt::black;
+                m_currentBorder.innerPen.setColor(Qt::black);
+                m_currentBorder.outerPen.setColor(Qt::black);
                 TRY_READ_ATTR_WITHOUT_NS(val)
                 //TODO support other dash types. Make it its own function.
                 if (val == "dash") {

filters/words/docx/DocxXmlDocumentReader.cpp
@@ -3783,21 +3783,23 @@ KoBorder::BorderData DocxXmlDocumentReader::getBorderData()
 
     if (color.isEmpty()) {
         QString colorString = QString("#").append(color);
-        borderData.color = QColor(colorString);
+        borderData.innerPen.setColor(QColor(colorString));
+        borderData.outerPen.setColor(QColor(colorString));
     }
 
     // Fallback to theme
-    if (!borderData.color.isValid() && !themeColor.isEmpty()) {
+    if (!borderData.innerPen.color().isValid() && !themeColor.isEmpty()) {
 
         MSOOXML::DrawingMLColorSchemeItemBase *colorItem = 0;
         colorItem = m_context->themes->colorScheme.value(themeColor);
         if (colorItem) {
-            borderData.color = colorItem->value();
+            borderData.innerPen.setColor(colorItem->value());
+            borderData.outerPen.setColor(colorItem->value());
         }
     }
 
     TRY_READ_ATTR(sz)
-    borderData.width = sz.toDouble() / 8.0;
+    borderData.innerPen.setWidthF(sz.toDouble() / 8.0);
 
     return borderData;
 }

words/part/KWCanvasBase.cpp
@@ -224,7 +224,7 @@ void KWCanvasBase::paintBorderSide(QPainter &painter, const KoBorder::BorderData
     // Set up the painter and inner and outer pens.
     QPen pen = painter.pen();
     // Line color
-    pen.setColor(borderData.color);
+    pen.setColor(borderData.outerPen.color());
 
     // Line style
     switch (borderData.style) {
@@ -248,9 +248,9 @@ void KWCanvasBase::paintBorderSide(QPainter &painter, const KoBorder::BorderData
         // are the distances from the center line of the whole
         // border to the centerlines of the outer and inner
         // borders respectively.
-        qreal outerWidth = borderData.width - borderData.innerWidth - borderData.spacing;
-        qreal outerOffset = borderData.width / 2.0 + outerWidth / 2.0;
-        qreal innerOffset = borderData.width / 2.0 - borderData.innerWidth / 2.0;
+        qreal outerWidth = borderData.outerPen.widthF() - borderData.innerPen.widthF() - borderData.spacing;
+        qreal outerOffset = borderData.outerPen.widthF() / 2.0 + outerWidth / 2.0;
+        qreal innerOffset = borderData.outerPen.widthF() / 2.0 - borderData.innerPen.widthF() / 2.0;
 
         QPointF outerOffset2D(-inwardsX * outerOffset, -inwardsY * outerOffset);
         QPointF innerOffset2D(inwardsX * innerOffset, inwardsY * innerOffset);
@@ -261,12 +261,12 @@ void KWCanvasBase::paintBorderSide(QPainter &painter, const KoBorder::BorderData
         painter.drawLine(lineStart + outerOffset2D, lineEnd + outerOffset2D);
 
         // Draw the inner line
-        pen.setWidthF(zoom * borderData.innerWidth);
+        pen.setWidthF(zoom * borderData.innerPen.widthF());
         painter.setPen(pen);
         painter.drawLine(lineStart + innerOffset2D, lineEnd + innerOffset2D);
     }
     else {
-        pen.setWidthF(zoom * borderData.width);
+        pen.setWidthF(zoom * borderData.innerPen.widthF());
         painter.setPen(pen);
         painter.drawLine(lineStart, lineEnd);
     }

filters/libmsooxml/MsooXmlTableStyle.cpp
@@ -178,15 +178,12 @@ void TableStyleConverter::applyBordersStyle(TableStyleProperties* stylePropertie
         }
         else {
             topData = &styleProperties->insideH;
-            if (topData->width == 0) {
+            if (topData->innerPen.widthF() == 0) {
                 topData = &styleProperties->top;
             }
         }
         if (!styleProperties->bordersToEdgesOnly || row == 0) {
-            style->borders()->setTopBorderColor(topData->color);
-            style->borders()->setTopBorderSpacing(topData->spacing);
-            style->borders()->setTopBorderStyle(topData->style);
-            style->borders()->setTopBorderWidth(topData->width);
+            style->borders()->setTopBorderData(*topData);
         }
     }
 
@@ -197,15 +194,12 @@ void TableStyleConverter::applyBordersStyle(TableStyleProperties* stylePropertie
         }
         else {
             bottomData = &styleProperties->insideH;
-            if (bottomData->width == 0) {
+            if (bottomData->innerPen.widthF() == 0) {
                 bottomData = &styleProperties->bottom;
             }
         }
         if (!styleProperties->bordersToEdgesOnly || row == lastRow) {
-            style->borders()->setBottomBorderColor(bottomData->color);
-            style->borders()->setBottomBorderSpacing(bottomData->spacing);
-            style->borders()->setBottomBorderStyle(bottomData->style);
-            style->borders()->setBottomBorderWidth(bottomData->width);
+            style->borders()->setBottomBorderData(*bottomData);
         }
     }
 
@@ -216,15 +210,12 @@ void TableStyleConverter::applyBordersStyle(TableStyleProperties* stylePropertie
         }
         else {
             leftData = &styleProperties->insideV;
-            if (leftData->width == 0) {
+            if (leftData->innerPen.widthF() == 0) {
                 leftData = &styleProperties->left;
             }
         }
         if (!styleProperties->bordersToEdgesOnly || column == 0) {
-            style->borders()->setLeftBorderColor(leftData->color);
-            style->borders()->setLeftBorderSpacing(leftData->spacing);
-            style->borders()->setLeftBorderStyle(leftData->style);
-            style->borders()->setLeftBorderWidth(leftData->width);
+            style->borders()->setLeftBorderData(*leftData);
         }
     }
 
@@ -235,31 +226,22 @@ void TableStyleConverter::applyBordersStyle(TableStyleProperties* stylePropertie
         }
         else {
             rightData = &styleProperties->insideV;
-            if (rightData->width == 0) {
+            if (rightData->innerPen.widthF() == 0) {
                 rightData = &styleProperties->right;
             }
         }
         if (!styleProperties->bordersToEdgesOnly || column == lastColumn) {
-            style->borders()->setRightBorderColor(rightData->color);
-            style->borders()->setRightBorderSpacing(rightData->spacing);
-            style->borders()->setRightBorderStyle(rightData->style);
-            style->borders()->setRightBorderWidth(rightData->width);
+            style->borders()->setRightBorderData(*rightData);
         }
     }
 
     if (setProperties & TableStyleProperties::Tl2brBorder) {
         KoBorder::BorderData* tl2brData = &styleProperties->tl2br;
-        style->borders()->setTlbrBorderColor(tl2brData->color);
-        style->borders()->setTlbrBorderSpacing(tl2brData->spacing);
-        style->borders()->setTlbrBorderStyle(tl2brData->style);
-        style->borders()->setTlbrBorderWidth(tl2brData->width);
+        style->borders()->setTlbrBorderData(*tl2brData);
     }
     if (setProperties & TableStyleProperties::Tr2blBorder) {
         KoBorder::BorderData* tr2blData = &styleProperties->tr2bl;
-        style->borders()->setTrblBorderColor(tr2blData->color);
-        style->borders()->setTrblBorderSpacing(tr2blData->spacing);
-        style->borders()->setTrblBorderStyle(tr2blData->style);
-        style->borders()->setTrblBorderWidth(tr2blData->width);
+        style->borders()->setTrblBorderData(*tr2blData);
     }
 }
 

filters/words/docx/DocxXmlStylesReader.cpp
@@ -537,7 +537,7 @@ KoFilter::ConversionStatus DocxXmlStylesReader::read_tblStylePr()
     if (type == "firstRow") {
         // In docx predefined styles for first row, even though it may define insideV to be 0 and bottom
         // border to have something, it in reality wishes insideV to also contain the bottom data
-        if (m_currentStyleProperties->insideH.width == 0) {
+        if (m_currentStyleProperties->insideH.innerPen.widthF() == 0) {
             m_currentStyleProperties->insideH = m_currentStyleProperties->bottom;
         }
         m_currentStyle->addProperties(MSOOXML::DrawingTableStyle::FirstRow, m_currentStyleProperties);
@@ -558,7 +558,7 @@ KoFilter::ConversionStatus DocxXmlStylesReader::read_tblStylePr()
         m_currentStyle->addProperties(MSOOXML::DrawingTableStyle::Band2Vertical, m_currentStyleProperties);
     }
     else if (type == "firstCol") {
-        if (m_currentStyleProperties->insideV.width == 0) {
+        if (m_currentStyleProperties->insideV.innerPen.widthF() == 0) {
             m_currentStyleProperties->insideV = m_currentStyleProperties->right;
         }
         m_currentStyle->addProperties(MSOOXML::DrawingTableStyle::FirstCol, m_currentStyleProperties);

libs/kotext/KoTextEditor.cpp
@@ -1425,10 +1425,10 @@ void KoTextEditor::insertTable(int rows, int columns)
             QTextTableCell cell = table->cellAt(row, col);
             QTextTableCellFormat format;
             KoTableCellStyle cellStyle;
-            cellStyle.setEdge(KoTableBorderStyle::Top, KoBorder::BorderSolid, 2, QColor(Qt::black));
-            cellStyle.setEdge(KoTableBorderStyle::Left, KoBorder::BorderSolid, 2, QColor(Qt::black));
-            cellStyle.setEdge(KoTableBorderStyle::Bottom, KoBorder::BorderSolid, 2, QColor(Qt::black));
-            cellStyle.setEdge(KoTableBorderStyle::Right, KoBorder::BorderSolid, 2, QColor(Qt::black));
+            cellStyle.setEdge(KoBorder::Top, KoBorder::BorderSolid, 2, QColor(Qt::black));
+            cellStyle.setEdge(KoBorder::Left, KoBorder::BorderSolid, 2, QColor(Qt::black));
+            cellStyle.setEdge(KoBorder::Bottom, KoBorder::BorderSolid, 2, QColor(Qt::black));
+            cellStyle.setEdge(KoBorder::Right, KoBorder::BorderSolid, 2, QColor(Qt::black));
             cellStyle.setPadding(5);
 
             cellStyle.applyStyle(format);

libs/kotext/styles/KoTableBorderStyle.h
@@ -58,23 +58,6 @@ public:
         BottomLeftToTopRightBorderInnerPen, ///< the bottom letf to top right diagonal inner pen
         BottomLeftToTopRightBorderStyle,    ///< the bottom letf to top right borderstyle
     };
-
-    enum Side {
-        Top = 0, ///< References the border at the top of the cell
-        Left,    ///< References the border at the left side of the cell
-        Bottom,  ///< References the border at the bottom of the cell
-        Right,   ///< References the border at the right side of the paragraph
-        TopLeftToBottomRight, ///< References the border from top, left corner to bottom, right corner of cell
-        BottomLeftToTopRight  ///< References the border from bottom, left corner to top, right corner of cell
-    };
-
-    struct Edge {
-        Edge() : innerPen(), outerPen(), spacing(0.0) { }
-
-        QPen innerPen;
-        QPen outerPen;
-        qreal spacing;
-    };
 };
 
 #endif // KOTABLEBORDERSTYLE_H

commit 5492fc6d4a953c5cc1ff434935da83beceaff496

Fix regression introduced in commit 78a8b341
Use the outer pen where it should be used.

filters/libmsooxml/MsooXmlDrawingTableStyleReader.cpp
@@ -907,7 +907,7 @@ KoFilter::ConversionStatus MsooXmlDrawingTableStyleReader::read_Table_ln()
     }
 
     TRY_READ_ATTR_WITHOUT_NS(w) //width
-    m_currentBorder.innerPen.setWidthF(EMU_TO_POINT(w.toDouble()));
+    m_currentBorder.outerPen.setWidthF(EMU_TO_POINT(w.toDouble()));
 
     while(!atEnd()) {
         readNext();

filters/words/docx/DocxXmlDocumentReader.cpp
@@ -3799,7 +3799,7 @@ KoBorder::BorderData DocxXmlDocumentReader::getBorderData()
     }
 
     TRY_READ_ATTR(sz)
-    borderData.innerPen.setWidthF(sz.toDouble() / 8.0);
+    borderData.outerPen.setWidthF(sz.toDouble() / 8.0);
 
     return borderData;
 }

words/part/KWCanvasBase.cpp
@@ -266,7 +266,7 @@ void KWCanvasBase::paintBorderSide(QPainter &painter, const KoBorder::BorderData
         painter.drawLine(lineStart + innerOffset2D, lineEnd + innerOffset2D);
     }
     else {
-        pen.setWidthF(zoom * borderData.innerPen.widthF());
+        pen.setWidthF(zoom * borderData.outerPen.widthF());
         painter.setPen(pen);
         painter.drawLine(lineStart, lineEnd);
     }