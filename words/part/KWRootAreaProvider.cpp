eac18ca9908503f9811c563295a57cb10675d513
Sebastian Sauer <sebastian.sauer@kdab.com>
2011-10-31 14:17:11 +0100

    Be more resistent to buggy docs. A warning is enough.

--- words/part/KWRootAreaProvider.cpp
+++ words/part/KWRootAreaProvider.cpp
@@ -258,9 +258,12 @@ KoTextLayoutRootArea* KWRootAreaProvider::provideNext(KoTextDocumentLayout *docu
         Q_ASSERT(shape);
         //Q_ASSERT_X(pageNumber == pageManager->page(shape).pageNumber(), __FUNCTION__, QString("KWPageManager is out-of-sync, pageNumber=%1 vs pageNumber=%2 with offset=%3 vs offset=%4 on frameSetType=%5").arg(pageNumber).arg(pageManager->page(shape).pageNumber()).arg(shape->absolutePosition().y()).arg(pageManager->page(shape).offsetInDocument()).arg(Words::frameSetTypeName(m_textFrameSet->textFrameSetType())).toLocal8Bit());
         KoTextShapeData *data = qobject_cast<KoTextShapeData*>(shape->userData());
-        Q_ASSERT(data);
-        data->setRootArea(area);
-        area->setAssociatedShape(shape);
+        if (data) {
+            data->setRootArea(area);
+            area->setAssociatedShape(shape);
+        } else {
+            kWarning(32001) << "shape has no KoTextShapeData";
+        }
     }
 
     if (m_textFrameSet->type() != Words::OtherFrameSet && m_textFrameSet->textFrameSetType() != Words::OtherTextFrameSet) {
