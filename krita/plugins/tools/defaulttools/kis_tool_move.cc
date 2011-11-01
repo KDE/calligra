KDE/calligra

commit 0efcc581d1995d480752983fd9708baad44f88e0

Fix crashing when using tools on empty image
When image is empty (has no layers), color picker & move tools caused
crashes when using.

krita/plugins/tools/defaulttools/kis_tool_move.cc
@@ -188,6 +188,11 @@ void KisToolMove::mousePressEvent(KoPointerEvent *event)
 
         if(!node) {
             node = currentNode();
+            if (!node)
+            {
+                m_strokeId.clear();
+                return;
+            }
         }
 
         /**
@@ -237,6 +242,9 @@ void KisToolMove::mousePressEvent(KoPointerEvent *event)
 void KisToolMove::mouseMoveEvent(KoPointerEvent *event)
 {
     if(MOVE_CONDITION(event, KisTool::PAINT_MODE)) {
+        if (!m_strokeId)
+            return;
+        
         QPoint pos = convertToPixelCoord(event).toPoint();
         if ((event->modifiers() & Qt::AltModifier) ||
             (event->modifiers() & Qt::ControlModifier)) {
@@ -258,6 +266,9 @@ void KisToolMove::mouseMoveEvent(KoPointerEvent *event)
 void KisToolMove::mouseReleaseEvent(KoPointerEvent *event)
 {
     if(RELEASE_CONDITION(event, KisTool::PAINT_MODE, Qt::LeftButton)) {
+        if (!m_strokeId)
+            return;
+        
         setMode(KisTool::HOVER_MODE);
 
         QPoint pos = convertToPixelCoord(event).toPoint();

krita/plugins/tools/defaulttools/kis_tool_colorpicker.cc
@@ -88,6 +88,9 @@ void KisToolColorPicker::pickColor(const QPointF& pos)
             m_colorPickerDelayTimer.start(100);
         }
 
+        if (!currentNode())
+            return;
+        
         KisPaintDeviceSP dev = currentNode()->paintDevice();
         if (!dev) return;
 

commit af46622de4275d5584aa833053036d9b5a53ae75

Fix layers moving after trying to move empty image
This fix small bug introduced by commit
0efcc581d1995d480752983fd9708baad44f88e0. After trying to use move tool
on empty image, it became impossible to use it anymore, because mode was
not restored after releasing mouse. Now mode is restored correctly.
Also, remove clearing m_strokeId in mousePressEvent because it is
already cleared after endStroke call in mouseReleaseEvent.

krita/plugins/tools/defaulttools/kis_tool_move.cc
@@ -189,7 +189,6 @@ void KisToolMove::mousePressEvent(KoPointerEvent *event)
         if(!node) {
             node = currentNode();
             if (!node) {
-                m_strokeId.clear();
                 return;
             }
         }
@@ -267,12 +266,12 @@ void KisToolMove::mouseMoveEvent(KoPointerEvent *event)
 void KisToolMove::mouseReleaseEvent(KoPointerEvent *event)
 {
     if(RELEASE_CONDITION(event, KisTool::PAINT_MODE, Qt::LeftButton)) {
+        setMode(KisTool::HOVER_MODE);
+        
         if (!m_strokeId)
         {
             return;
         }
-
-        setMode(KisTool::HOVER_MODE);
 
         QPoint pos = convertToPixelCoord(event).toPoint();
         drag(pos);