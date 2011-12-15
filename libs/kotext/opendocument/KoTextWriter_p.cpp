KDE/calligra

commit e66889d566e11c3b409ed6bcdbce5e37f7164314

Fix saving of paragraphs missed saving the formatting of framents where the block have more properties (non style) than the fragment
Took the opotunity to implement autoStyle method in KoCharacterStyle as
a followup on the style inheritance I did a couple of weeks ago

BUG:260115

libs/kotext/opendocument/KoTextWriter_p.cpp
@@ -537,27 +537,20 @@ QString KoTextWriter::Private::saveCharacterStyle(const QTextCharFormat &charFor
     QString displayName = originalCharStyle->name();
     QString internalName = QString(QUrl::toPercentEncoding(displayName, "", " ")).replace('%', '_');
 
-    KoCharacterStyle charStyle(charFormat);
-    // we'll convert it to a KoCharacterStyle to check for local changes.
-    // we remove that properties given by the paragraphstyle char format, these are not present in the saved style (should it really be the case?)
-    charStyle.removeDuplicates(blockCharFormat);
-    if (charStyle == (*originalCharStyle)) { // This is the real, unmodified character style.
+    KoCharacterStyle *autoStyle = originalCharStyle->autoStyle(charFormat, blockCharFormat);
+
+    if (autoStyle->isEmpty()) { // This is the real, unmodified character style.
         if (originalCharStyle != defaultCharStyle) {
-            if (!charStyle.isEmpty()) {
-                KoGenStyle style(KoGenStyle::ParagraphStyle, "text");
-                originalCharStyle->saveOdf(style);
-                generatedName = context.mainStyles().insert(style, internalName, KoGenStyles::DontAddNumberToName);
-            }
+            KoGenStyle style(KoGenStyle::ParagraphStyle, "text");
+            originalCharStyle->saveOdf(style);
+            generatedName = context.mainStyles().insert(style, internalName, KoGenStyles::DontAddNumberToName);
         }
     } else { // There are manual changes... We'll have to store them then
         KoGenStyle style(KoGenStyle::ParagraphAutoStyle, "text", originalCharStyle != defaultCharStyle ? internalName : "" /*parent*/);
         if (context.isSet(KoShapeSavingContext::AutoStyleInStyleXml))
             style.setAutoStyleInStylesDotXml(true);
-        charStyle.removeDuplicates(*originalCharStyle);
-        if (!charStyle.isEmpty()) {
-            charStyle.saveOdf(style);
-            generatedName = context.mainStyles().insert(style, "T");
-        }
+        autoStyle->saveOdf(style);
+        generatedName = context.mainStyles().insert(style, "T");
     }
 
     return generatedName;

libs/kotext/styles/KoCharacterStyle.cpp
@@ -448,6 +448,16 @@ void KoCharacterStyle::applyStyle(QTextCharFormat &format) const
     }
 }
 
+KoCharacterStyle *KoCharacterStyle::autoStyle(const QTextCharFormat &format, QTextCharFormat blockCharFormat) const
+{
+    applyStyle(blockCharFormat);
+    ensureMinimalProperties(blockCharFormat);
+    KoCharacterStyle *autoStyle = new KoCharacterStyle(blockCharFormat);
+    autoStyle->d->stylesPrivate.removeDuplicates(format.properties());
+
+    return autoStyle;
+}
+
 void KoCharacterStyle::applyStyle(QTextBlock &block) const
 {
     QTextCursor cursor(block);

libs/kotext/styles/KoCharacterStyle.h
@@ -486,6 +486,15 @@ public:
      */
     void removeDuplicates(const QTextCharFormat &other_format);
 
+    /**
+     * Create an autostyle out of the format and baseFormat
+     * @param format the format that is converted to an autostyle.
+     * @param baseFormat the format (typically a blockCharFormat) that is the basis of the format,
+     * but not itself part of the character style inheritance.
+     * @return pointer to autostyle that has this as parent style
+     */
+    KoCharacterStyle *autoStyle(const QTextCharFormat &format, QTextCharFormat blockCharFormat) const;
+ 
     void saveOdf(KoGenStyle &style) const;
 
     /**

commit 48a20afcfeed0af60e4ce6b1f393f5a1b0beeb0f

Fix mem-leak. Don't leak the just created new auto-KoCharacterStyle which is only temporary used to generate a proper auto-style while saving the QTextCharFormat. The mem-leak was introduced in e66889d566e.

libs/kotext/opendocument/KoTextWriter_p.cpp
@@ -553,6 +553,7 @@ QString KoTextWriter::Private::saveCharacterStyle(const QTextCharFormat &charFor
         generatedName = context.mainStyles().insert(style, "T");
     }
 
+    delete autoStyle;
     return generatedName;
 }
 