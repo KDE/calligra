KDE/calligra

commit 63d6ce0b15c34a29e20ee318dbb1380db2ca6a6b

Improve performance while scrolling around the sheet attached to bug 281910. Intel's VTunes shows 3 hotsports. This commit fixes two of them. The theird and yet not fixed case is CellStorage::isPartMerged which does an expensive RTree-lookup. Probably we should cache that info? CCBUG:281910

tables/Cell.cpp
@@ -161,9 +161,9 @@ KLocale* Cell::locale() const
 bool Cell::isDefault() const
 {
     // check each stored attribute
-    if (value() != Value())
+    if (value().isEmpty())
         return false;
-    if (formula() != Formula())
+    if (formula() != Formula::empty())
         return false;
     if (!link().isEmpty())
         return false;

tables/ui/SheetView.cpp
@@ -239,17 +239,13 @@ const CellView& SheetView::cellView(int col, int row)
 #ifdef CALLIGRA_TABLES_MT
     QMutexLocker ml(&d->cacheMutex);
 #endif
-    if (!d->cache.contains(QPoint(col, row))) {
-        CellView *v = createCellView(col, row);
+    CellView *v = d->cache.object(QPoint(col, row));
+    if (!v) {
+        v = createCellView(col, row);
         d->cache.insert(QPoint(col, row), v);
         d->cachedArea += QRect(col, row, 1, 1);
     }
-#ifdef CALLIGRA_TABLES_MT
-    CellView v = *d->cache.object(QPoint(col, row));
-    return v;
-#else
-    return *d->cache.object(QPoint(col, row));
-#endif
+    return *v;
 }
 
 void SheetView::setPaintCellRange(const QRect& rect)

commit c6867c731a2762e376326febc17769d3d288be5b

It's actually the other way around. This fixes a regression introduced with commit 63d6ce0b15.

tables/Cell.cpp
@@ -161,7 +161,7 @@ KLocale* Cell::locale() const
 bool Cell::isDefault() const
 {
     // check each stored attribute
-    if (value().isEmpty())
+    if (!value().isEmpty())
         return false;
     if (formula() != Formula::empty())
         return false;