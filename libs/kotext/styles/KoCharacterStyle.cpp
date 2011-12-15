KDE/calligra

commit e66889d566e11c3b409ed6bcdbce5e37f7164314

Fix saving of paragraphs missed saving the formatting of framents where the block have more properties (non style) than the fragment
Took the opotunity to implement autoStyle method in KoCharacterStyle as
a followup on the style inheritance I did a couple of weeks ago

BUG:260115

libs/kotext/styles/KoCharacterStyle.cpp
@@ -448,6 +448,16 @@ void KoCharacterStyle::applyStyle(QTextCharFormat &format) const
     }
 }
 
+KoCharacterStyle *KoCharacterStyle::autoStyle(const QTextCharFormat &format, QTextCharFormat blockCharFormat) const
+{
+    applyStyle(blockCharFormat);
+    ensureMinimalProperties(blockCharFormat);
+    KoCharacterStyle *autoStyle = new KoCharacterStyle(blockCharFormat);
+    autoStyle->d->stylesPrivate.removeDuplicates(format.properties());
+
+    return autoStyle;
+}
+
 void KoCharacterStyle::applyStyle(QTextBlock &block) const
 {
     QTextCursor cursor(block);

libs/kotext/opendocument/KoTextWriter_p.cpp
@@ -537,27 +537,20 @@ QString KoTextWriter::Private::saveCharacterStyle(const QTextCharFormat &charFor
     QString displayName = originalCharStyle->name();
     QString internalName = QString(QUrl::toPercentEncoding(displayName, "", " ")).replace('%', '_');
 
-    KoCharacterStyle charStyle(charFormat);
-    // we'll convert it to a KoCharacterStyle to check for local changes.
-    // we remove that properties given by the paragraphstyle char format, these are not present in the saved style (should it really be the case?)
-    charStyle.removeDuplicates(blockCharFormat);
-    if (charStyle == (*originalCharStyle)) { // This is the real, unmodified character style.
+    KoCharacterStyle *autoStyle = originalCharStyle->autoStyle(charFormat, blockCharFormat);
+
+    if (autoStyle->isEmpty()) { // This is the real, unmodified character style.
         if (originalCharStyle != defaultCharStyle) {
-            if (!charStyle.isEmpty()) {
-                KoGenStyle style(KoGenStyle::ParagraphStyle, "text");
-                originalCharStyle->saveOdf(style);
-                generatedName = context.mainStyles().insert(style, internalName, KoGenStyles::DontAddNumberToName);
-            }
+            KoGenStyle style(KoGenStyle::ParagraphStyle, "text");
+            originalCharStyle->saveOdf(style);
+            generatedName = context.mainStyles().insert(style, internalName, KoGenStyles::DontAddNumberToName);
         }
     } else { // There are manual changes... We'll have to store them then
         KoGenStyle style(KoGenStyle::ParagraphAutoStyle, "text", originalCharStyle != defaultCharStyle ? internalName : "" /*parent*/);
         if (context.isSet(KoShapeSavingContext::AutoStyleInStyleXml))
             style.setAutoStyleInStylesDotXml(true);
-        charStyle.removeDuplicates(*originalCharStyle);
-        if (!charStyle.isEmpty()) {
-            charStyle.saveOdf(style);
-            generatedName = context.mainStyles().insert(style, "T");
-        }
+        autoStyle->saveOdf(style);
+        generatedName = context.mainStyles().insert(style, "T");
     }
 
     return generatedName;

libs/kotext/styles/KoCharacterStyle.h
@@ -486,6 +486,15 @@ public:
      */
     void removeDuplicates(const QTextCharFormat &other_format);
 
+    /**
+     * Create an autostyle out of the format and baseFormat
+     * @param format the format that is converted to an autostyle.
+     * @param baseFormat the format (typically a blockCharFormat) that is the basis of the format,
+     * but not itself part of the character style inheritance.
+     * @return pointer to autostyle that has this as parent style
+     */
+    KoCharacterStyle *autoStyle(const QTextCharFormat &format, QTextCharFormat blockCharFormat) const;
+ 
     void saveOdf(KoGenStyle &style) const;
 
     /**

commit 07e9f4d24a666bdd4074b56f78402f8b1209d4e8

Fix styles are changed during saving. This is especially fatal if the used KoCharacterStyle is the default KoCharacterStyle in which case we modify the character-style properties in a rather random way just because we try to save the content. The bug was introduced with commit e66889d566e. Verified that bug 260115 the commit fixed is not reopened and things still work as expected. CCBUG:260115

libs/kotext/styles/KoCharacterStyle.cpp
@@ -453,11 +453,11 @@ void KoCharacterStyle::applyStyle(QTextCharFormat &format) const
 
 KoCharacterStyle *KoCharacterStyle::autoStyle(const QTextCharFormat &format, QTextCharFormat blockCharFormat) const
 {
-    applyStyle(blockCharFormat);
-    ensureMinimalProperties(blockCharFormat);
-    KoCharacterStyle *autoStyle = new KoCharacterStyle(blockCharFormat);
-    autoStyle->d->stylesPrivate.removeDuplicates(format.properties());
-
+    KoCharacterStyle *autoStyle = new KoCharacterStyle(format);
+    autoStyle->setParentStyle(const_cast<KoCharacterStyle*>(this));
+    autoStyle->applyStyle(blockCharFormat);
+    autoStyle->ensureMinimalProperties(blockCharFormat);
+    autoStyle->removeDuplicates(*this);
     return autoStyle;
 }
 