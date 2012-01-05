911bda03c72d8bf488166beeb1b3ad36a61842ea
Marijn Kruisselbrink <mkruisselbrink@kde.org>
2012-01-05 22:24:07 +0100

    no dbus on android

--- stage/part/KPrView.cpp
+++ stage/part/KPrView.cpp
@@ -44,7 +44,9 @@
 #include "KPrPage.h"
 #include "KPrMasterPage.h"
 #include "KPrPageApplicationData.h"
+#ifndef Q_OS_ANDROID
 #include "KPrViewAdaptor.h"
+#endif
 #include "KPrViewModePresentation.h"
 #include "KPrViewModeNotes.h"
 #include "KPrViewModeSlidesSorter.h"
@@ -70,7 +72,9 @@ KPrView::KPrView( KPrDocument *document, QWidget *parent )
   , m_normalMode( viewMode() )
   , m_notesMode( new KPrViewModeNotes( this, kopaCanvas() ))
   , m_slidesSorterMode(new KPrViewModeSlidesSorter(this, kopaCanvas()))
+#ifndef Q_OS_ANDROID
   , m_dbus( new KPrViewAdaptor( this ) )
+#endif
 {
     m_normalMode->setName(i18n("Normal"));
     initGUI();
