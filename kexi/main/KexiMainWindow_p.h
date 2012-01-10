1785003ae243032faa768abde39ff56fec2553e0
Dimitrios T. Tanis <dimitrios.tanis@kdemail.net>
2012-01-10 23:58:08 +0200

    Updated context sensitivity in Show/Hide tabs

--- kexi/main/KexiMainWindow_p.h
+++ kexi/main/KexiMainWindow_p.h
@@ -1380,6 +1380,8 @@ protected:
     virtual bool queryExit();
 protected slots:
     void slotCurrentTabIndexChanged(int index);
+signals:
+    void currentTabIndexChanged(int index);
 
 private:
     void setupCentralWidget();
@@ -1434,6 +1436,7 @@ void KexiMainWidget::slotCurrentTabIndexChanged(int index)
     if (m_mainWindow)
         m_mainWindow->activeWindowChanged(cont->window, (KexiWindow*)m_previouslyActiveWindow);
     m_previouslyActiveWindow = cont->window;
+    emit currentTabIndexChanged(index);
 }
 
 //------------------------------------------
