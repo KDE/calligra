813620de7112aba321adbee84d88f7ade4347bd0
Sebastian Sauer <sebastian.sauer@kdab.com>
2012-01-11 19:39:10 +0100

    Improve debug for xmlTk

--- filters/tables/excel/sidewinder/chartsubstreamhandler.cpp
+++ filters/tables/excel/sidewinder/chartsubstreamhandler.cpp
@@ -98,7 +98,7 @@ public:
     virtual void dump(std::ostream& out) const {
         QStringList tokens;
         foreach(XmlTk* t, m_tokens)
-            tokens.append( QString("%1 %2 %3").arg(t->m_xmlTkTag, 0, 16).arg(t->type()).arg(t->value()) );
+            tokens.append( QString("%1(%2)=%3").arg(QString::fromUtf8(xmlTkTagName(XmlTkTags(t->m_xmlTkTag)))).arg(t->type()).arg(t->value()) );
         out << qPrintable(QString("[%1]").arg(tokens.join(", ")));
     }
     virtual void setData(unsigned size, const unsigned char* data, const unsigned int* /*continuePositions*/) {
@@ -1239,14 +1239,14 @@ void ChartSubStreamHandler::handleMsoDrawing(MsoDrawingRecord* record)
 void ChartSubStreamHandler::handleShapePropsStream(ShapePropsStreamRecord* record)
 {
     if (!record) return;
-    DEBUG << "rgb=" << record->rgb().length() << " " << record->rgb() << std::endl;
+    DEBUG << "wObjContext=" << record->wObjContext() << "rgbLength=" << record->rgb().length() << " rgbString=" << record->rgb() << std::endl;
     //TODO
 }
 
 void ChartSubStreamHandler::handleTextPropsStream(TextPropsStreamRecord* record)
 {
     if (!record) return;
-    DEBUG << "rgb=" << record->rgb().length() << " " << record->rgb() << std::endl;
+    DEBUG << "rgbLength=" << record->rgb().length() << " rgbString=" << record->rgb() << std::endl;
     //TODO
 }
 
