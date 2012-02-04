d8d9cb01ea0f7896cdb614508c85e83603ae9023
Sujith Haridasan <sujith.haridasan@kdemail.net>
2012-02-04 16:02:18 +0530

    Hide the left button when user is in the initial page of the spreadsheet and hide the right button when user is in the last page of the spreadsheet.

--- active/src/CASpreadsheetHandler.h
+++ active/src/CASpreadsheetHandler.h
@@ -29,6 +29,9 @@
 class CASpreadsheetHandler : public CAAbstractDocumentHandler
 {
     Q_OBJECT
+    Q_PROPERTY(int currentSheetNumber READ currentSheetNumber NOTIFY currentSheetNumChanged)
+    Q_PROPERTY(int sheetCount READ sheetCount NOTIFY sheetCountChanged)
+
 protected:
     virtual KoDocument* document();
 
@@ -44,6 +47,8 @@ public:
     virtual QString rightToolbarSource() const;
     virtual QString leftToolbarSource() const;
 
+    int currentSheetNumber() const;
+
 public slots:
     void tellZoomControllerToSetDocumentSize(QSize size);
     void updateCanvas();
@@ -51,6 +56,10 @@ public slots:
 
     void nextSheet();
     void previousSheet();
+
+signals:
+    void currentSheetNumChanged();
+    void sheetCountChanged();
 
 private:
     class Private;
