77167e308ded2d63f3c42d11487cf57862d9c528
Dag Andersen <danders@get2net.dk>
2012-02-16 14:26:27 +0100

    Remove dbus connection
    Remove unnecessary dbus registration
    Disable unusable plan adapter

--- plan/kptview.cpp
+++ plan/kptview.cpp
@@ -125,7 +125,7 @@
 
 #include "plansettings.h"
 
-#include "KPtViewAdaptor.h"
+// #include "KPtViewAdaptor.h"
 
 #include <assert.h>
 
@@ -218,8 +218,7 @@ View::View( Part* part, QWidget* parent )
     else
         setXMLFile( "plan.rc" );
 
-    m_dbus = new ViewAdaptor( this );
-    QDBusConnection::sessionBus().registerObject( '/' + objectName(), this );
+//     new ViewAdaptor( this );
 
     m_sp = new QSplitter( this );
     QVBoxLayout *layout = new QVBoxLayout( this );
@@ -447,11 +446,6 @@ void View::hideToolDocker()
             }
         }
     }
-}
-
-ViewAdaptor* View::dbusObject()
-{
-    return m_dbus;
 }
 
 void View::slotCreateTemplate()
