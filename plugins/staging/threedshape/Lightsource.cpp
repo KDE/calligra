4424fbe1f065e38bb72cc340261fbf95616109df
Inge Wallin <inge@lysator.liu.se>
2012-02-21 23:04:43 +0100

    Fix a nasty cut&paste bug and add a comment.

--- plugins/staging/threedshape/Lightsource.cpp
+++ plugins/staging/threedshape/Lightsource.cpp
@@ -69,9 +69,10 @@ void Lightsource::saveOdf(KoXmlWriter &writer)
 
 static QVector3D stringToVector(QString &string)
 {
+    // The string comes into this function in the form "(0 3.5 0.3)".
     QStringList elements = string.mid(1, string.size() - 2).split(' ', QString::SkipEmptyParts);
     if (elements.size() != 3)
         return QVector3D(0, 0, 1);
     else
-        return QVector3D(elements[0].toDouble(), elements[2].toDouble(), elements[2].toDouble());
+        return QVector3D(elements[0].toDouble(), elements[1].toDouble(), elements[2].toDouble());
 }
