5cdecef8bfe76d158c314e8020b2ba47ab1b0cdb
Jaroslaw Staniek <staniek@kde.org>
2012-03-07 12:23:06 +0100

    KexiDB: SQLite Driver: Fixed crashes when compacting database fails

--- kexi/kexidb/drivers/sqlite/sqlitevacuum.cpp
+++ kexi/kexidb/drivers/sqlite/sqlitevacuum.cpp
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 2006 Jarosław Staniek <staniek@kde.org>
+   Copyright (C) 2006-2012 Jarosław Staniek <staniek@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -49,10 +49,14 @@ SQLiteVacuum::SQLiteVacuum(const QString& filePath)
 
 SQLiteVacuum::~SQLiteVacuum()
 {
-    m_dumpProcess->waitForFinished();
-    delete m_dumpProcess;
-    m_sqliteProcess->waitForFinished();
-    delete m_sqliteProcess;
+    if (m_dumpProcess) {
+        m_dumpProcess->waitForFinished();
+        delete m_dumpProcess;
+    }
+    if (m_sqliteProcess) {
+        m_sqliteProcess->waitForFinished();
+        delete m_sqliteProcess;
+    }
     if (m_dlg)
         m_dlg->close();
     delete m_dlg;
