661260efffecbda5139a13fe73a8a7e6364132d5
Boudewijn Rempt <boud@valdyas.org>
2012-03-08 11:59:53 +0100

    don't forget to delete the temporary file ourselves

--- tables/part/View.cpp
+++ tables/part/View.cpp
@@ -1180,6 +1180,9 @@ void View::createTemplate()
 
     Factory::global().dirs()->addResourceType("tables_template",
             "data", "tables/templates/");
+
+    QDir d;
+    d.remove(fileName);
 }
 
 void View::setActiveSheet(Sheet* sheet, bool updateSheet)
