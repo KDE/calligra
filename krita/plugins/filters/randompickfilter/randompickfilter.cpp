6b6d52d484226fa03df788c2f47f95ff3c4f1db9
Boudewijn Rempt <boud@valdyas.org>
2012-04-23 10:28:11 +0200

    use ng iteratorss for the randompick filter

--- krita/plugins/filters/randompickfilter/randompickfilter.cpp
+++ krita/plugins/filters/randompickfilter/randompickfilter.cpp
@@ -43,7 +43,7 @@
 #include <kis_iterators_pixel.h>
 #include <kis_layer.h>
 #include <kis_paint_device.h>
-#include <kis_random_accessor.h>
+#include <kis_random_accessor_ng.h>
 #include <kis_random_generator.h>
 #include <kis_selection.h>
 #include <kis_types.h>
