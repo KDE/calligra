KDE/calligra

commit 8d7f540bc15945acbca36ce990c1027d0749af9a

Fix text:bullet-char MSO filter so bullet-char is exactly one character.

filters/libmsooxml/MsooXmlUtils.cpp
@@ -1765,8 +1765,8 @@ QString Utils::ParagraphBulletProperties::convertToListProperties(KoGenStyles& m
     }
     else {
         out.startElement("text:list-level-style-bullet");
-        if (m_bulletChar == UNUSED) {
-            out.addAttribute("text:bullet-char", "");
+        if (m_bulletChar.length() != 1) {
+            out.addAttribute("text:bullet-char", " ");
         } else {
             out.addAttribute("text:bullet-char", m_bulletChar);
         }

commit bcf244bbfe61a29f82e7ad1c6ee814a081392b41

Workaround lists without bullet
This is a temporary workaround until the correct solution has been implemented.
It generates valid odf and there is no visual regression. The regressions was
made visible by commit 8d7f540bc15945acbca36ce990c1027d0749af9a that fixed the
generated xml.

filters/libmsooxml/MsooXmlUtils.cpp
@@ -1766,7 +1766,11 @@ QString Utils::ParagraphBulletProperties::convertToListProperties(KoGenStyles& m
     else {
         out.startElement("text:list-level-style-bullet");
         if (m_bulletChar.length() != 1) {
-            out.addAttribute("text:bullet-char", " ");
+            // TODO if there is no bullet char this should not be saved as list but as
+            // normal paragraph. Both LO and MSO do export it just as paragraph and no list
+            // until there is a fix available that change that we use a Zero Width Space 
+            // to not generate invalid xml
+            out.addAttribute("text:bullet-char", QChar(0x200B));
         } else {
             out.addAttribute("text:bullet-char", m_bulletChar);
         }