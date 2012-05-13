KDE/calligra

commit ecac81d9a42dc361996fb3562f397777bda4c743

Do not write enpty value for attribute anchor-type.

filters/libmsooxml/MsooXmlVmlReaderImpl.h
@@ -357,7 +357,7 @@ void MSOOXML_CURRENT_CLASS::createFrameStart(FrameStartElement startType)
             }
         }
     }
-    else {
+    else if (!m_currentVMLProperties.anchorType.isEmpty()) {
         body->addAttribute("text:anchor-type", m_currentVMLProperties.anchorType);
     }
     if (!asChar) {

commit 51ceaed363661ce7728ecbf92cd34f27fc4bb77b

Use char as anchor-type when it is empty
This fixes a regression introduced by commit ecac81d9a42dc361996fb3562f397777bda4c743.
This fix brings back the old behaviour while still fixing the bug that was fixed in
the commit.

filters/libmsooxml/MsooXmlVmlReaderImpl.h
@@ -357,8 +357,8 @@ void MSOOXML_CURRENT_CLASS::createFrameStart(FrameStartElement startType)
             }
         }
     }
-    else if (!m_currentVMLProperties.anchorType.isEmpty()) {
-        body->addAttribute("text:anchor-type", m_currentVMLProperties.anchorType);
+    else {
+        body->addAttribute("text:anchor-type", m_currentVMLProperties.anchorType.isEmpty() ? "char": m_currentVMLProperties.anchorType);
     }
     if (!asChar) {
         if (hor_pos.isEmpty() || hor_pos == "absolute") {