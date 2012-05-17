KDE/calligra

commit e9bc977c082450165805578f37fcb5bd21dd8755

Changed: cleanup includes

libs/main/KoToolBox.cpp
@@ -23,25 +23,13 @@
 #include "KoToolBoxLayout_p.h"
 
 #include <KoCanvasController.h>
-#include <KoToolManager.h>
 #include <KoShapeLayer.h>
-#include <KoInteractionTool.h>
-
-#include <KDebug>
-#include <QLayout>
-#include <QMap>
 #include <QButtonGroup>
 #include <QToolButton>
-#include <QHash>
-#include <QPainter>
-#include <QRect>
-#include <QTimer>
-#include <QStyle>
 #include <QStyleOptionFrameV3>
+#include <QPainter>
+#include <QHash>
 #include <QApplication>
-
-#include "math.h"
-#include <KoDockWidgetTitleBar.h>
 
 
 class KoToolBox::Private
@@ -254,7 +242,7 @@ void KoToolBox::resizeEvent(QResizeEvent* event)
 void KoToolBox::setOrientation(Qt::Orientation orientation)
 {
     d->layout->setOrientation(orientation);
-    QTimer::singleShot(0, this, SLOT(update()));
+    QMetaObject::invokeMethod(this, SLOT(update()), Qt::QueuedConnection);
     foreach(Section* section, d->sections) {
         section->setOrientation(orientation);
     }

libs/main/KoToolBoxLayout_p.h
@@ -24,14 +24,11 @@
 #include <KDebug>
 #include <QLayout>
 #include <QMap>
-#include <QButtonGroup>
-#include <QToolButton>
-#include <QHash>
-#include <QPainter>
 #include <QRect>
-#include <QTimer>
+#include <QAbstractButton>
 
 #include "math.h"
+
 
 class SectionLayout : public QLayout
 {

libs/main/KoToolBox_p.h
@@ -24,10 +24,8 @@
 
 #include <KoCanvasObserverBase.h>
 
+#include <QWidget>
 #include <QList>
-#include <QMap>
-#include <QHash>
-#include <QDockWidget>
 
 #include <KoToolManager.h>
 

commit 3a8792a6360820bc4017d14243f5279ab218b295

Fix regression introduced in e9bc977c082450165805578f37fcb5bd21dd8755
The slot when invoked by inwokeMethod is never called

libs/main/KoToolBox.cpp
@@ -30,7 +30,7 @@
 #include <QPainter>
 #include <QHash>
 #include <QApplication>
-
+#include <QTimer>
 
 class KoToolBox::Private
 {
@@ -242,7 +242,7 @@ void KoToolBox::resizeEvent(QResizeEvent* event)
 void KoToolBox::setOrientation(Qt::Orientation orientation)
 {
     d->layout->setOrientation(orientation);
-    QMetaObject::invokeMethod(this, SLOT(update()), Qt::QueuedConnection);
+    QTimer::singleShot(0, this, SLOT(update()));
     foreach(Section* section, d->sections) {
         section->setOrientation(orientation);
     }