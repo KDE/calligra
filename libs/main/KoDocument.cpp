1b5c88a475ebc0228110454de931c988386f15a2
Boudewijn Rempt <boud@valdyas.org>
2012-05-19 12:28:46 +0200

    Remove paintEverything
    It was only used to call paintContent now that there are no subdocuments
    anymore. (and it was this way for a long time already).

--- libs/main/KoDocument.cpp
+++ libs/main/KoDocument.cpp
@@ -707,12 +707,6 @@ KoDocumentRdfBase *KoDocument::documentRdfBase() const
 }
 
 
-void KoDocument::paintEverything(QPainter &painter, const QRect &rect, KoView *view)
-{
-    Q_UNUSED(view);
-    paintContent(painter, rect);
-}
-
 bool KoDocument::isModified() const
 {
     if (KParts::ReadWritePart::isModified()) {
@@ -1067,7 +1061,7 @@ QPixmap KoDocument::generatePreview(const QSize& size)
 
     QPainter p;
     p.begin(&pix);
-    paintEverything(p, rc);
+    paintContent(p, rc);
     p.end();
 
     return pix.scaled(QSize(previewWidth, previewHeight), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
