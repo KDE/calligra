f21445ecbda954bee12d1d115d7d33e47f3b53e8
Adam Pigg <adam@piggz.co.uk>
2012-06-09 14:16:48 +0100

    Added copyright

--- kexi/formeditor/kexiactionselectiondialog_p.h
+++ kexi/formeditor/kexiactionselectiondialog_p.h
@@ -1,5 +1,6 @@
 /* This file is part of the KDE project
    Copyright (C) 2005-2006 Jarosław Staniek <staniek@kde.org>
+   Copyright (C) 2012 Adam Pigg <adam@piggz.co.uk>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
