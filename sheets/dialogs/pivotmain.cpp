bb57d6593da23a683879ba25e21e6972f7469356
Jigar Raisinghani <jigarraisinghani@gmail.com>
2012-06-29 16:52:36 +0530

    Implemented Drag and Drop and improved GUI

--- sheets/dialogs/pivotmain.cpp
+++ sheets/dialogs/pivotmain.cpp
@@ -1,8 +1,28 @@
+/* This file is part of the KDE project
+   Copyright (C) 2012-2013 Jigar Raisinghani <jigarraisinghani@gmail.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+// Local
+
 #include "pivotmain.h"
 #include "ui_pivotmain.h"
 #include "pivotoptions.h"
 #include "pivotfilters.h"
-
 
 PivotMain::PivotMain(QWidget *parent) :
     QDialog(parent),
@@ -11,6 +31,39 @@ PivotMain::PivotMain(QWidget *parent) :
     ui->setupUi(this);
     ui->TotalRows->setChecked(true);
     ui->TotalColumns->setChecked(true);
+
+
+    ui->Labels->setSelectionMode(QAbstractItemView::ExtendedSelection);
+    ui->Labels->setDragEnabled(true);
+    ui->Labels->setDragDropMode(QAbstractItemView::InternalMove);
+    ui->Labels->viewport()->setAcceptDrops(true);
+    ui->Labels->setDropIndicatorShown(true);
+
+    ui->Rows->setSelectionMode(QAbstractItemView::SingleSelection);
+    ui->Rows->setDragEnabled(true);
+    ui->Rows->setDragDropMode(QAbstractItemView::DropOnly);
+    ui->Rows->viewport()->setAcceptDrops(true);
+    ui->Rows->setDropIndicatorShown(true);
+
+    ui->Columns->setSelectionMode(QAbstractItemView::SingleSelection);
+    ui->Columns->setDragEnabled(true);
+    ui->Columns->setDragDropMode(QAbstractItemView::DropOnly);
+    ui->Columns->viewport()->setAcceptDrops(true);
+    ui->Columns->setDropIndicatorShown(true);
+
+    ui->Values->setSelectionMode(QAbstractItemView::SingleSelection);
+    ui->Values->setDragEnabled(true);
+    ui->Values->setDragDropMode(QAbstractItemView::DropOnly);
+    ui->Values->viewport()->setAcceptDrops(true);
+    ui->Values->setDropIndicatorShown(true);
+
+    ui->PageFields->setSelectionMode(QAbstractItemView::SingleSelection);
+    ui->PageFields->setDragEnabled(true);
+    ui->PageFields->setDragDropMode(QAbstractItemView::DropOnly);
+    ui->PageFields->viewport()->setAcceptDrops(true);
+    ui->PageFields->setDropIndicatorShown(true);
+
+
 
 }
 
--- sheets/dialogs/pivotsubtotals.cpp
+++ sheets/dialogs/pivotsubtotals.cpp
@@ -1,3 +1,23 @@
+/* This file is part of the KDE project
+   Copyright (C) 2012-2013 Jigar Raisinghani <jigarraisinghani@gmail.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+// Local
 #include "pivotsubtotals.h"
 #include "ui_pivotsubtotals.h"
 
@@ -6,6 +26,12 @@ PivotSubTotals::PivotSubTotals(QWidget *parent) :
     ui(new Ui::PivotSubTotals)
 {
     ui->setupUi(this);
+    ui->None->setChecked(true);
+    ui->SortBy->setChecked(true);
+    ui->Order->addItem("Ascending");
+    ui->Order->addItem("Descending");
+
+
 }
 
 PivotSubTotals::~PivotSubTotals()
