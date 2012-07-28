9221c8f3601e3fe7446138c3b9f7c4cd88789734
Paul Mendez <paulestebanms@gmail.com>
2012-07-27 21:18:06 -0500

    Add license headers

--- stage/part/tests/TestShapeAnimations.cpp
+++ stage/part/tests/TestShapeAnimations.cpp
@@ -1,5 +1,6 @@
 /* This file is part of the KDE project
  * Copyright ( C ) 2007 Thorsten Zachmann <zachmann@kde.org>
+ * Copyright ( C ) 2012 Paul Mendez <paulestebanms@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
