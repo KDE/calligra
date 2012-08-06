KDE/calligra

commit 558ecaefde97f9314439d0f339932dd1127b52a9

Removed unnecessary statements in totalImageBounds()

krita/ui/canvas/kis_coordinates_converter.cpp
@@ -270,10 +270,7 @@ QRectF KisCoordinatesConverter::totalImageBounds() const
     if(!m_d->image)
         return QRect();
     QRectF boundingRect = QRectF(m_d->image.data()->bounds());
-    if(boundingRect.contains(QRectF(QPointF(0,0), m_d->canvasWidgetSize)))
-        return QRectF(boundingRect | imageToWidget(QRectF(QPointF(0,0), m_d->canvasWidgetSize)));
-    else
-        return boundingRect;
+    return QRectF(boundingRect | imageToWidget(QRectF(QPointF(0,0), m_d->canvasWidgetSize)));
 }
 
 QPointF KisCoordinatesConverter::imageCenterInWidgetPixel() const

commit af6770ed020ac14166a9b5c6845faf19de4a4ae3

Fixed the broken canvas caused by commit 558ecae

krita/ui/canvas/kis_coordinates_converter.cpp
@@ -270,7 +270,10 @@ QRectF KisCoordinatesConverter::totalImageBounds() const
     if(!m_d->image)
         return QRect();
     QRectF boundingRect = QRectF(m_d->image.data()->bounds());
-    return QRectF(boundingRect | imageToWidget(QRectF(QPointF(0,0), m_d->canvasWidgetSize)));
+    if(boundingRect.contains(QRectF(QPointF(0,0), m_d->canvasWidgetSize)))
+        return QRectF(boundingRect | imageToWidget(QRectF(QPointF(0,0), m_d->canvasWidgetSize)));
+    else
+        return boundingRect;
 }
 
 QPointF KisCoordinatesConverter::imageCenterInWidgetPixel() const