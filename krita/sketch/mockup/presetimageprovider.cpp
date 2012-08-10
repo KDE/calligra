bb19f2723f6b5acfc8f86c7aff1b86cb821e143e
Dan Leinir Turthra Jensen <admin@leinir.dk>
2012-08-10 17:21:19 +0100

    Attempt first preset thumbnail implementation (untested as yet)

--- krita/sketch/mockup/presetimageprovider.cpp
+++ krita/sketch/mockup/presetimageprovider.cpp
@@ -1,12 +1,16 @@
 #include "presetimageprovider.h"
 
+#include <KoResourceServerAdapter.h>
 #include <ui/kis_resource_server_provider.h>
+#include <image/brushengine/kis_paintop_preset.h>
 
 class PresetImageProvider::Private {
 public:
     Private()
-        : rserver(0)
-    {}
+    {
+        rserver = KisResourceServerProvider::instance()->paintOpPresetServer();
+    }
+
     KoResourceServer<KisPaintOpPreset> * rserver;
 };
 
@@ -18,5 +22,11 @@ PresetImageProvider::PresetImageProvider() :
 QImage PresetImageProvider::requestImage(const QString &id, QSize *size, const QSize &requestedSize)
 {
     QImage image(requestedSize, QImage::Format_ARGB32);
+    QList<KisPaintOpPreset*> resources = d->rserver->resources();
+    int theID = id.toInt();
+    if(theID >= 0 && theID < resources.count())
+    {
+        image = resources.at(theID)->image().scaled(requestedSize, Qt::KeepAspectRatio);
+    }
     return image;
 }
