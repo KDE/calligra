7aaf3e85355eb275cb3d5bdcdfd79a4426b82524
Dan Leinir Turthra Jensen <admin@leinir.dk>
2012-08-14 14:03:54 +0100

    Make the plugin not crash (don't need this for designer anyway)

--- krita/sketch/mockup/kritasketchqmlextensions.cpp
+++ krita/sketch/mockup/kritasketchqmlextensions.cpp
@@ -1,6 +1,5 @@
 #include "constants.h"
 #include "settings.h"
-#include "presetimageprovider.h"
 #include "kritasketchqmlextensions.h"
 
 #include <qdeclarative.h>
@@ -20,7 +19,6 @@ void KritaSketchQmlExtensions::registerTypes(const char *uri)
 void KritaSketchQmlExtensions::initializeEngine(QDeclarativeEngine *engine, const char *uri)
 {
     Q_UNUSED(uri)
-    engine->addImageProvider(QLatin1String("presetthumb"), new PresetImageProvider);
     engine->rootContext()->setContextProperty( "Constants", new Constants( engine ) );
     engine->rootContext()->setContextProperty( "Settings", new Settings( engine ) );
 }
