KDE/calligra

commit 414b04642080011873a3777c488760f3cb79b66e

Move core parts of KexiDB lib to libcalligradb
Move core parts of KexiDB lib from kexi/kexidb to calligra/libs/db (libcalligradb) for reuse by Word's Biblio module

* GUI- or Kexi-related bits such as validators are still in Kexi
* Only the SQLite3 driver is moved to libcalligradb, others stay within Kexi
** thus, calligra libs depend on the driver, i.e. depend on SQLite3 (this was the case already for Words and Kexi)
** calligra libs depend on ICU as well for proper unicode handling (which is dependency of Qt 5 anyway)
* Predicate library, as a Qt-only component, will replace functionality of the calligradb lib
* sqlitedump tool used for database for interactive database compacting stays in Kexi; if needed could be moved to libs/ too
* a few functions from libkexiutils moved to libcalligradb, so libkexiutils is not dependency
* for simplicity/API stability, namespace stays as KexiDB
* README.PACKAGERS file updated

REVIEW:105464
DIGEST:Kexi's data handling library KexiDB become common component of Calligra.

kexi/widget/relations/KexiRelationsTableContainer_p.cpp
@@ -38,8 +38,8 @@
 #include <kconfig.h>
 #include <kglobalsettings.h>
 
-#include <kexidb/tableschema.h>
-#include <kexidb/utils.h>
+#include <db/tableschema.h>
+#include <db/utils.h>
 #include <kexidragobjects.h>
 #include "KexiRelationsTableContainer_p.h"
 #include "KexiRelationsScrollArea.h"

commit bfe50581851aeb5dc1527f5d7b896604acd2c9d2

Query Designer: Fix: Missing "*" (All Columns) item in Query Designer
It was present in Kexi 2.2 and ealier.

*regression introduced in commit 414b0464208001

BUG:306577

kexi/widget/relations/KexiRelationsTableContainer_p.cpp
@@ -159,7 +159,7 @@ void KexiRelationViewTableContainerHeader::mouseReleaseEvent(QMouseEvent *ev)
 KexiRelationsTableFieldList::KexiRelationsTableFieldList(
     KexiDB::TableOrQuerySchema* tableOrQuerySchema,
     KexiRelationsScrollArea *scrollArea, QWidget *parent)
-        : KexiFieldListView(parent, 0)
+        : KexiFieldListView(parent, ShowAsterisk)
         , m_scrollArea(scrollArea)
 {
     setSchema(tableOrQuerySchema);