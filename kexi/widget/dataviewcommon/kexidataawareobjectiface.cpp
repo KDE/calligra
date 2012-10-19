7b25c36ee826e0e6ad011587c0cde6e97cfb73cd
Jaroslaw Staniek <staniek@kde.org>
2012-10-19 21:02:24 +0200

    No need to check for m_errorMessagePopup != 0
    GIT_SILENT

--- kexi/widget/dataviewcommon/kexidataawareobjectiface.cpp
+++ kexi/widget/dataviewcommon/kexidataawareobjectiface.cpp
@@ -964,26 +964,24 @@ bool KexiDataAwareObjectInterface::acceptEditor()
 //! @todo allow displaying user-defined warning
 //! @todo also use for other error messages
                 delete m_errorMessagePopup;
-                m_errorMessagePopup = 0;
-                if (!m_errorMessagePopup) {
-                    KexiContextMessage msg(
-                        i18nc("Question", "Error: %1?", m_editor->columnInfo()->field->typeName()));
-                    m_errorMessagePopup = new KexiContextMessageWidget(dynamic_cast<QWidget*>(this), 0, 0, msg);
-                    QPoint arrowPos =
+                KexiContextMessage msg(
+                            i18nc("Question", "Error: %1?", m_editor->columnInfo()->field->typeName()));
+                m_errorMessagePopup = new KexiContextMessageWidget(dynamic_cast<QWidget*>(this), 0, 0, msg);
+                QPoint arrowPos =
                         par->mapToGlobal(edit->pos()) + QPoint(12, edit->height() + 6);
-                    if (m_verticalHeader) {
-                        arrowPos += QPoint(m_verticalHeader->width(), horizontalHeaderHeight());
-                    }
-                    m_errorMessagePopup->setMessageType(KMessageWidget::Error);
-                    m_errorMessagePopup->setCalloutPointerDirection(KMessageWidget::Up);
-                    m_errorMessagePopup->setCalloutPointerPosition(arrowPos);
-                    m_errorMessagePopup->setWordWrap(false);
-                    m_errorMessagePopup->setClickClosesMessage(true);
-                    m_errorMessagePopup->resizeToContents();
-                    m_errorMessagePopup->animatedShow();
-                    QObject::connect(m_errorMessagePopup, SIGNAL(animatedHideFinished()),
-                                     dynamic_cast<QWidget*>(m_editor), SLOT(setFocus()));
+                if (m_verticalHeader) {
+                    arrowPos += QPoint(m_verticalHeader->width(), horizontalHeaderHeight());
                 }
+                m_errorMessagePopup->setMessageType(KMessageWidget::Error);
+                m_errorMessagePopup->setCalloutPointerDirection(KMessageWidget::Up);
+                m_errorMessagePopup->setCalloutPointerPosition(arrowPos);
+                m_errorMessagePopup->setWordWrap(false);
+                m_errorMessagePopup->setClickClosesMessage(true);
+                m_errorMessagePopup->resizeToContents();
+                m_errorMessagePopup->animatedShow();
+                QObject::connect(m_errorMessagePopup, SIGNAL(animatedHideFinished()),
+                                 dynamic_cast<QWidget*>(m_editor), SLOT(setFocus()));
+
                 m_editor->setFocus();
             }
         } else if (m_editor->valueIsNull()) {//null value entered
