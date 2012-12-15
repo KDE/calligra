2c78c9db86e1478d5fc9b0e8b6f132f377b4358d
Inge Wallin <inge@lysator.liu.se>
2012-12-15 18:39:14 +0100

    Fix building and using the kdgantt library on windows.
    Squashed commit of the following:
    
    commit ca63cd5a5b277293cda752c4933c19c075762240
    Author: Inge Wallin <inge@lysator.liu.se>
    Date:   Sat Dec 15 17:20:56 2012 +0100
    
        Fix compile error.
    
        I have no clue how this change got here.
    
    commit 7573989801a1cc29bd9b6ac92f9a527fcfe4d66f
    Merge: 5e1ad7b0 23a2d82
    Author: Inge Wallin <inge@lysator.liu.se>
    Date:   Thu Dec 13 00:16:16 2012 +0100
    
        Merge branch 'thirdparty-kdlibs2.6-ingwa' of ssh://git.kde.org/calligra into thirdparty-kdlibs2.6-ingwa
    
    commit 5e1ad7b09e520ed9abd71f646a2066e7654ba11a
    Merge: 90a3b05 db1d453
    Author: Inge Wallin <inge@lysator.liu.se>
    Date:   Wed Dec 12 22:25:48 2012 +0100
    
        Merge branch 'calligra/2.6' into thirdparty-kdlibs2.6-ingwa
    
    commit 23a2d8218236dff029aa5d5562bc57c268893dd4
    Author: Stuart Dickson <stuartmd@kogmbh.com>
    Date:   Wed Dec 12 20:03:04 2012 +0100
    
        Fixes for windows builds
    
    commit 87815b7b281694d7250184ae1485edab056f8aba
    Author: Stuart Dickson <stuartmd@kogmbh.com>
    Date:   Wed Nov 21 01:22:46 2012 +0100
    
        Moving Span::~Span() inline (fix compile on Windows)
    
    commit 1d4d37a80a901bde26eb791202ff0f354ba35715
    Author: Stuart Dickson <stuartmd@kogmbh.com>
    Date:   Mon Dec 10 08:57:43 2012 +0100
    
        Re-enable chartshape plugin
    
    commit 4e6ce398f1aec7eae30c88e430a0ddcfdb76012f
    Author: Stuart Dickson <stuartmd@kogmbh.com>
    Date:   Fri Nov 23 04:50:22 2012 +0100
    
        Reference calligrakdgantt library (instead of kdgantt)
    
    commit 90a3b05e1809fdfb7aa1c59bb4070bb34a27b116
    Author: Inge Wallin <inge@lysator.liu.se>
    Date:   Tue Nov 20 22:26:55 2012 +0100
    
        Make a real dynamic library out of kdgantt.
    
    commit c11c3aebebbfd45c63bd37473e4b1a4ada5c5a8b
    Author: Inge Wallin <inge@lysator.liu.se>
    Date:   Fri Nov 16 00:55:13 2012 +0100
    
        Move the kdgantt library to a special place for 3rd party libraries.

--- plugins/chartshape/kchart_export.h
+++ plugins/chartshape/kchart_export.h
@@ -24,7 +24,10 @@
 #include <kdemacros.h>
 
 #ifndef KCHART_EXPORT
-# if defined(MAKE_KCHART_LIB)
+#  if defined(KDELIBS_STATIC_LIBS)
+   /* No export/import for static libraries */
+#   define KCHART_EXPORT
+# elif defined(MAKE_KCHART_LIB)
    /* We are building this library */ 
 #  define KCHART_EXPORT KDE_EXPORT
 # else
