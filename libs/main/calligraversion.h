3a1233fdc902261b5b71613d519b33f329b627ca
Cyrille Berger <cberger@cberger.net>
2013-01-22 15:41:22 +0100

    update version to 2.5.5

--- libs/main/calligraversion.h
+++ libs/main/calligraversion.h
@@ -33,7 +33,7 @@
 * it contains the version at compile time. See Calligra::versionString() if you need
 * the Calligra version used at runtime.
 */
-#define CALLIGRA_VERSION_STRING "2.5.4"
+#define CALLIGRA_VERSION_STRING "2.5.5"
 
 /**
  * @def CALLIGRA_VERSION_MAJOR
@@ -56,7 +56,7 @@
  * @ingroup CalligraMacros
  * @brief Release version of Calligra, at compile time
  */
-#define CALLIGRA_VERSION_RELEASE 4
+#define CALLIGRA_VERSION_RELEASE 5
 
 /**
  * @def CALLIGRA_ALPHA
