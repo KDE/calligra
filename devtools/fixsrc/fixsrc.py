a78f62d6f55cb0a60ce67706fc880133cfa8bc12
Inge Wallin <inge@lysator.liu.se>
2013-02-25 21:32:47 +0100

    Fix signal and slot normalizing to not remove single & in the parameter list.
    So:
      const Foo &  -->  Foo
      const Foo    -->  Foo
    but:
      Foo &        -->  Foo&

--- devtools/fixsrc/fixsrc.py
+++ devtools/fixsrc/fixsrc.py
@@ -33,28 +33,63 @@ def doNormalize(contents):
     # Warning: This is a very simple parser that will fail on multi-line
     #          cases or cases with more than 2 parenthesis.  But it should
     #          cover the majority of cases.
-    pattern = "(SIGNAL|SLOT)(\\s*\\([^\\(]*\\([^\\)]*\\)\s*\\))"
-    regex = re.compile(pattern)
+    topPattern = "(SIGNAL|SLOT)(\\s*\\([^\\(]*\\([^\\)]*\\)\s*\\))"
+    topRegex = re.compile(topPattern)
+
+    # regex to identify the parameter list
+    #   [0]: Start parenthesis
+    #   [1]: the identifier (function name of the signal or slot)
+    #   [2]: the parameter list
+    #   [3]: end parenthesis
+    # Note: this won't work on function parameters with their own parameter list.
+    paramPattern = "(\s*\\(\s*)([a-zA-z][a-zA-z0-9]*)(\s*\\(\s*[^\\)]*\s*\\))(\s*\\)\s*)"
+    paramRegex = re.compile(paramPattern)
 
     lineNo = 1
     newContents = []
     for line in contents:
-        #print line
+        #print "input: ", line
 
         # replace signal/slot signatures with the normalized equivalent
-        matches = regex.findall(line)
-        #print "matches:", matches
-        for match in matches:
+        signalSlotMacros = topRegex.findall(line)
+        for macro in signalSlotMacros:
             # Parameters to SIGNAL or SLOT
-            param = match[1] #match[0] = SIGNAL or SLOT
-            param2 = param.replace("const", "").replace("&", "").replace(" ", "")
+            signalSlotParam = macro[1] #macro[0] = "SIGNAL" or "SLOT"
 
-            if verbose and param != param2:
-                sys.stderr.write("  Normalizing " + match[0] + " statement on line "
+            params = paramRegex.findall(signalSlotParam)[0]  # There should only be one hit
+            #print "params:", params, len(params)
+
+            functionName  = params[1]
+            paramList = params[2].strip()[1:-1]  # remove the parenthesis
+            #print repr(functionName), repr(paramList)
+
+            # Now we have dug out the parameter list to the function
+            # This sequence does the actual normalizing.
+            functionParams = paramList.split(",")
+            #print functionParams
+            outParamList = []
+            for functionParam in functionParams:
+                s = functionParam.strip()
+                if s[:5] == "const" and s[-1:] == "&":
+                    # remove const-&
+                    outParamList.append(s[5:-1].replace(" ", ""))
+                elif s[:5] == "const":
+                    # remove const without &
+                    outParamList.append(s[5:].replace(" ", ""))
+                elif s[-1:] == "&":
+                    # keep & without const but remove spaces
+                    outParamList.append(s.replace(" ", ""))
+            outParams = string.join(outParamList, ",")
+            signalSlotParam2 = "(" + functionName + "(" + outParams + "))"
+
+            if verbose and signalSlotParam != signalSlotParam2:
+                sys.stderr.write("  Normalizing " + macro[0] + " statement on line "
                                  + str(lineNo) + "\n")
 
-            line = line.replace(param, param2, 1)
+            line = line.replace(signalSlotParam, signalSlotParam2, 1)
+            #print "result: ", line
 
+        # When we exit the loop all the substitutions inside the line are done.
         newContents.append(line)
         lineNo = lineNo + 1
 
