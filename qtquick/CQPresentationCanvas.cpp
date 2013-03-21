24f7265e03d42bd43c3d6e83984284b956f3f42b
Arjen Hiemstra <ahiemstra@heimr.nl>
2013-03-21 14:41:03 +0100

    Remove more obsolete includes

--- qtquick/CQPresentationCanvas.cpp
+++ qtquick/CQPresentationCanvas.cpp
@@ -30,10 +30,6 @@
 #include <KoZoomHandler.h>
 #include <KoPADocument.h>
 #include <KoPACanvasItem.h>
-#include <sheets/part/Doc.h>
-#include <sheets/part/CanvasItem.h>
-#include <KMimeType>
-#include <KMimeTypeTrader>
 #include <KDebug>
 #include <KActionCollection>
 #include <QGraphicsWidget>
