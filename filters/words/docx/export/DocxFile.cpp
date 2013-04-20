a5a277c051cad7474d963728e9bfb557f09ba52e
Inge Wallin <inge@lysator.liu.se>
2013-04-20 19:41:09 +0200

    Remove old unused code from when the file was copied.

--- filters/words/docx/export/DocxFile.cpp
+++ filters/words/docx/export/DocxFile.cpp
@@ -54,6 +54,7 @@ KoFilter::ConversionStatus DocxFile::writeDocx(const QString &fileName,
                                                const OdtTraverserDocxContext &context)
 {
     // Create the store and check if everything went well.
+    // FIXME: Should docxStore be deleted from a finalizer?
     KoStore *docxStore = KoStore::createStore(fileName, KoStore::Write,
                                               appIdentification, KoStore::Auto);
     if (!docxStore || docxStore->bad()) {
@@ -79,30 +80,12 @@ KoFilter::ConversionStatus DocxFile::writeDocx(const QString &fileName,
         return status;
     }
 
-#if 0
-    // Write META-INF/container.xml
-    status = writeMetaInf(docxStore);
-    if (status != KoFilter::OK) {
-        delete docxStore;
-        return status;
-    }
-
-    // Write content.opf
-    status = writeOpf(docxStore, metadata);
-    if (status != KoFilter::OK) {
-        delete docxStore;
-        return status;
-    }
-
-    // Write toc.ncx
-    status = writeNcx(docxStore, metadata);
-    if (status != KoFilter::OK) {
-        delete docxStore;
-        return status;
-    }
-#endif
     // Write contents of added files.
     status = FileCollector::writeFiles(docxStore);
+    if (status != KoFilter::OK) {
+        delete docxStore;
+        return status;
+    }
 
     delete docxStore;
     return status;
@@ -133,23 +116,23 @@ KoFilter::ConversionStatus DocxFile::writeTopLevelRels(KoStore *docxStore)
     writer.addAttribute("Id", "rId1");
     writer.addAttribute("Type", "http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument");
     writer.addAttribute("Target", "word/document.xml");
-    writer.endElement();        // Relationship
+    writer.endElement();  // Relationship
 
     // Doc props core
     writer.startElement("Relationship");
     writer.addAttribute("Id", "rId2");
     writer.addAttribute("Type", "http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties");
     writer.addAttribute("Target", "docProps/core.xml");
-    writer.endElement();        // Relationship
+    writer.endElement();  // Relationship
 
     // Doc props app
     writer.startElement("Relationship");
     writer.addAttribute("Id", "rId3");
     writer.addAttribute("Type", "http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties");
     writer.addAttribute("Target", "docProps/app.xml");
-    writer.endElement();        // Relationship
+    writer.endElement();  // Relationship
 
-    writer.endElement();        // Relationships
+    writer.endElement();  // Relationships
     writer.endDocument();
 
     docxStore->close();
@@ -215,294 +198,3 @@ KoFilter::ConversionStatus DocxFile::writeDocumentRels(KoStore *docxStore)
     return KoFilter::OK;
 }
 
-#if 0
-KoFilter::ConversionStatus DocxFile::writeMetaInf(KoStore *docxStore)
-{
-    // We can hardcode this one.
-    if (!docxStore->open("META-INF/container.xml")) {
-        kDebug(30503) << "Can not to open META-INF/container.xml.";
-        return KoFilter::CreationError;
-    }
-
-    KoStoreDevice metaDevice(docxStore);
-    KoXmlWriter writer(&metaDevice);
-
-    writer.startElement("container");
-    writer.addAttribute("version", "1.0");
-    writer.addAttribute("xmlns", "urn:oasis:names:tc:opendocument:xmlns:container");
-
-    writer.startElement("rootfiles");
-
-    writer.startElement("rootfile");
-    writer.addAttribute("full-path", pathPrefix() + "content.opf");
-    writer.addAttribute("media-type", "application/oebps-package+xml");
-    writer.endElement(); // rootfile
-
-    writer.endElement(); // rootfiles
-    writer.endElement(); // container
-
-    docxStore->close();
-    return KoFilter::OK;
-}
-
-KoFilter::ConversionStatus DocxFile::writeOpf(KoStore *docxStore,
-                                              QHash<QString, QString> &metadata)
-{
-    if (!docxStore->open(pathPrefix() + "content.opf")) {
-        kDebug(30503) << "Can not create content.opf .";
-        return KoFilter::CreationError;
-    }
-
-    KoStoreDevice opfDevice(docxStore);
-    KoXmlWriter writer(&opfDevice);
-
-    //FIXME: Write   <?xml version="1.0"?> -> FIXED
-    writer.startDocument(NULL,NULL,NULL);
-
-    // FIXME: Get the unique identifier
-    writer.startElement("package");
-    writer.addAttribute("version", "2.0");
-    writer.addAttribute("xmlns", "http://www.idpf.org/2007/opf");
-    writer.addAttribute("xmlns:dc", "http://purl.org/dc/elements/1.1/");
-    writer.addAttribute("xmlns:opf", "http://www.idpf.org/2007/opf");
-    writer.addAttribute("unique-identifier", "BookId"); // FIXME: Where to get this?
-
-    // ==== metadata ====
-    writer.startElement("metadata");
-
-    // Required elements are: title, language and identifier
-
-    writer.startElement("dc:title");
-    writer.addTextNode(metadata.value("title"));
-    writer.endElement(); // dc:title
-
-    writer.startElement("meta");
-    writer.addAttribute("name", "cover");
-    writer.addAttribute("content", "cover-image");
-    writer.endElement();
-
-    writer.startElement("dc:language");
-    if (!metadata.value("language").isEmpty())
-        writer.addTextNode(metadata.value("language"));
-    else
-        writer.addTextNode("en");
-    writer.endElement(); // dc:language
-
-    writer.startElement("dc:identifier");
-    writer.addAttribute("id", "BookId");
-    writer.addAttribute("opf:scheme", "ISBN");
-    writer.addTextNode("123456789X");  // FIXME: Where to get this?
-    writer.endElement(); // dc:identifier
-
-    // FIXME: dc:creator and many more (optional)
-
-    writer.endElement(); // metadata
-
-    // ==== manifest ====
-    writer.startElement("manifest");
-
-    // Write manifest entry for each embedded file.
-    foreach(FileInfo *file, files()) {
-        // Since paths are relative from where this file is, remove
-        // the common prefix from the reference.
-        QString relativeFilename = file->m_fileName;
-        if (relativeFilename.startsWith(pathPrefix()))
-            relativeFilename = relativeFilename.right(relativeFilename.size()
-                                                      - pathPrefix().size());
-
-        writer.startElement("item");
-        writer.addAttribute("id", file->m_id);
-        writer.addAttribute("href", relativeFilename);
-        writer.addAttribute("media-type", file->m_mimetype);
-        writer.endElement(); // item
-    }
-
-    // toc.ncx 
-    writer.startElement("item");
-    writer.addAttribute("id", "ncx");
-    writer.addAttribute("href", "toc.ncx");
-    writer.addAttribute("media-type", "application/x-dtbncx+xml");
-    writer.endElement(); // item
-
-    writer.endElement(); // manifest
-
-    // ==== spine ==== 
-    writer.startElement("spine");
-    writer.addAttribute("toc", "ncx");
-
-    // If there is a cover image, it should be the first page for now
-    // maybe we can have an option to lets user choose the cover page
-    // number in future.
-    foreach (FileInfo *file, files()) {
-        if ( file->m_id == "cover") {
-            writer.startElement("itemref");
-            writer.addAttribute("idref", file->m_id);
-            writer.endElement(); // itemref
-        }
-    }
-    foreach(FileInfo *file, files()) {
-        // Since paths are relative from where this file is, remove
-        // the common prefix from the reference.
-        if (file->m_id == "cover") {
-            continue;
-        }
-        QString relativeFilename = file->m_fileName;
-        if (relativeFilename.contains(filePrefix())) {
-            if (relativeFilename.startsWith(pathPrefix())) {
-                relativeFilename = relativeFilename.right(relativeFilename.size() 
-                                                          - pathPrefix().size());
-            }
-            writer.startElement("itemref");
-            writer.addAttribute("idref", file->m_id);
-            writer.endElement(); // itemref
-        }
-    }
-
-    writer.endElement(); // spine
-
-    writer.endElement(); // package
-
-    docxStore->close();
-    return KoFilter::OK;
-}
-
-KoFilter::ConversionStatus DocxFile::writeNcx(KoStore *docxStore,
-                                              QHash<QString, QString> &metadata)
-{
-    if (!docxStore->open(pathPrefix() + "toc.ncx")) {
-        kDebug(30503) << "Can not create toc.ncx.";
-        return KoFilter::CreationError;
-    }
-
-    KoStoreDevice ncxDevice(docxStore);
-    KoXmlWriter writer(&ncxDevice);
-
-//    QByteArray arr =" <!DOCTYPE ncx PUBLIC "-//NISO//DTD ncx 2005-1//EN" "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd">";
-//    ncxDevice.write(arr);
-
-    writer.startElement("ncx");
-    writer.addAttribute("version", "2005-1");
-    writer.addAttribute("xml:lang", "en");
-    writer.addAttribute("xmlns", "http://www.daisy.org/z3986/2005/ncx/");
-
-    writer.startElement("head");
-
-//    arr = "<!-- The following four metadata items are required for all NCX documents,"
-//            "including those conforming to the relaxed constraints of OPS 2.0 -->";
-//    ncxDevice.write(arr);
-
-    // ##### write meta tags ######
-    writer.startElement("meta");
-    writer.addAttribute("name", "dtb:uid");
-    writer.addAttribute("content", "123456789X");
-    writer.endElement();
-
-    writer.startElement("meta");
-    writer.addAttribute("name", "dtb:depth");
-    writer.addAttribute("content", "1"); // FIXME: For now it is 1
-    writer.endElement();
-
-    writer.startElement("meta");
-    writer.addAttribute("name", "dtb:totalPageCount");
-    writer.addAttribute("content", "0"); // must be 0
-    writer.endElement();
-
-    writer.startElement("meta");
-    writer.addAttribute("name", "dtb:maxPageNumber");
-    writer.addAttribute("content", "0"); // must be 0
-    writer.endElement();
-
-    writer.endElement(); //head
-
-
-    // ###### title ########
-    writer.startElement("docTitle");
-    writer.startElement("text");
-    writer.addTextNode(metadata.value("title"));
-    writer.endElement();
-    writer.endElement(); // docTitle
-
-    // ##### docAuthor ######
-    writer.startElement("docAuthor");
-
-    writer.startElement("text");
-    writer.addTextNode(metadata.value("creator"));
-    writer.endElement();
-
-    writer.endElement(); // docAuthor
-
-    // #### navMap #####
-    writer.startElement("navMap");
-
-    int playOrder = 1;
-    foreach (FileInfo *file, files()) {
-        if ( file->m_id == "cover") {
-            writer.startElement("navPoint");
-            writer.addAttribute("id", "navpoint-" + QString::number(playOrder));
-            writer.addAttribute("playOrder", QString::number(playOrder));
-
-            writer.startElement("navLabel");
-
-            writer.startElement("text");
-            writer.addTextNode(file->m_label);
-            writer.endElement();
-
-            writer.endElement(); // navLabel
-
-            writer.startElement("content");
-            QString src = file->m_fileName;
-            src = src.right(src.size() - pathPrefix().size());
-            writer.addAttribute("src", src);
-            writer.endElement(); // content
-
-            writer.endElement(); // navePoint
-            playOrder ++;
-        }
-    }
-
-    foreach(FileInfo *file, files()) {
-
-        if (file->m_label.isEmpty()) {
-            continue;
-        }
-        if (file->m_id == "cover-html") {
-            continue;
-        }
-
-        // Since paths are relative from where this file is, remove
-        // the common prefix from the reference.
-        QString relativeFilename = file->m_fileName;
-        if (relativeFilename.contains(filePrefix())) {
-            if (relativeFilename.startsWith(pathPrefix()))
-                relativeFilename = relativeFilename.right(relativeFilename.size()
-                                                          - pathPrefix().size());
-
-            writer.startElement("navPoint");
-            writer.addAttribute("id", "navpoint-" + QString::number(playOrder));
-            writer.addAttribute("playOrder", QString::number(playOrder));
-
-            writer.startElement("navLabel");
-
-            writer.startElement("text");
-            writer.addTextNode(file->m_label);
-            writer.endElement();
-
-            writer.endElement(); // navLabel
-
-            writer.startElement("content");
-            writer.addAttribute("src", relativeFilename);
-            writer.endElement(); // content
-
-            writer.endElement(); // navePoint
-
-            playOrder++;
-        }
-    }
-
-    writer.endElement(); // navMap
-    writer.endElement(); // ncx
-
-    docxStore->close();
-    return KoFilter::OK;
-}
-#endif
