884c2e681231fe4a926012b2a9f83510b4b598ec
Inge Wallin <inge@lysator.liu.se>
2013-05-12 17:05:25 +0200

    Add the first embryo to the new reader and the backend class.

---
+++ filters/libodfreader/OdtReader.cpp
@@ -0,0 +1,394 @@
+/* This file is part of the KDE project
+
+   Copyright (C) 2012-2013 Inge Wallin            <inge@lysator.liu.se>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+
+// Own
+#include "OdtReader.h"
+
+// Qt
+#include <QStringList>
+#include <QBuffer>
+
+// KDE
+#include <kdebug.h>
+#include <klocalizedstring.h>
+
+// Calligra
+#include <KoStore.h>
+#include <KoXmlStreamReader.h>
+#include <KoXmlNS.h>
+#include <KoXmlWriter.h>  // For copyXmlElement
+#include <KoOdfReadStore.h>
+
+// Reader library
+#include "OdtReaderBackend.h"
+#include "OdfReaderContext.h"
+
+
+OdtReader::OdtReader()
+{
+}
+
+OdtReader::~OdtReader()
+{
+}
+
+
+bool OdtReader::readContent(OdtReaderBackend *backend, OdfReaderContext *context)
+{
+    m_backend = backend;
+    m_context = context;
+
+    if (context->analyzeOdfFile() != KoFilter::OK) {
+        return false;
+    }
+
+    // ----------------------------------------------------------------
+    // Read the body from content.xml
+
+    KoStore *odfStore = m_context->odfStore();
+
+    if (!odfStore->open("content.xml")) {
+        kError(30503) << "Unable to open input file content.xml" << endl;
+        return false;
+    }
+
+    KoXmlStreamReader reader;
+    prepareForOdf(reader);
+
+    reader.setDevice(odfStore->device());
+    bool  foundContent = false;
+    while (!reader.atEnd()) {
+        reader.readNext();
+
+        if (reader.isStartElement() && reader.qualifiedName() == "office:document-content") {
+            foundContent = true;
+            break;
+        }
+    }
+    if (!foundContent) {
+        kError(30503) << "Couldn't find the content in content.xml" << endl;
+    }
+
+    // FIXME: Call backend function for starting the document here.
+
+    // <office:document-content> has the following children in ODF 1.2:
+    //   <office:automatic-styles> 3.15.3
+    //   <office:body> 3.3
+    //   <office:font-face-decls> 3.14
+    //   <office:scripts> 3.12.
+    while (reader.readNextStartElement()) {
+        QString tagName = reader.qualifiedName().toString();
+        
+        if (tagName == "office:automatic-styles") {
+            // We already have the styles in the context.  No need to read them again.
+            reader.skipCurrentElement();
+        }
+        else if (tagName == "office:font-face-decls") {
+            // FIXME: Not yet implemented
+            reader.skipCurrentElement();
+        }
+        else if (tagName == "office:scripts") {
+            // FIXME: Not yet implemented
+            reader.skipCurrentElement();
+        }
+        else if (tagName == "office:body") {
+            // This is the big one.
+            readElementOfficeBody(reader);
+        }
+        else {
+            reader.skipCurrentElement();
+        }
+    }
+
+    // FIXME: Call backend function for ending the document here.
+    odfStore->close();
+
+    return true;
+}
+
+
+#if 0
+// This is a template function for the reader library
+void OdtReader::readElementNamespaceTagname(KoXmlStreamReader &reader)
+{
+    m_backend->elementNamespaceTagname(reader, m_context);
+
+    // <namespace:tagname> has the following children in ODF 1.2:
+    //   FILL IN THE CHILDREN LIKE THIS EXAMPLE (taken from office:document-content):
+    //   <office:automatic-styles> 3.15.3
+    //   <office:body> 3.3
+    //   <office:font-face-decls> 3.14
+    //   <office:scripts> 3.12.
+    while (reader.readNextStartElement()) {
+        QString tagName = reader.qualifiedName().toString();
+        
+        if (tagName == "office:automatic-styles") {
+            // FIXME: NYI
+        }
+        else if (tagName == "office:body") {
+            readElementOfficeBody(reader);
+        }
+        ...  MORE else if () HERE
+        else {
+            reader.skipCurrentElement();
+        }
+    }
+
+    m_backend->elementNamespaceTagname(reader, m_context);
+}
+#endif
+
+
+void OdtReader::readElementOfficeBody(KoXmlStreamReader &reader)
+{
+    m_backend->elementOfficeBody(reader, m_context);
+
+    // <office:body> has the following children in ODF 1.2:
+    //   <office:chart> 3.8,
+    //   <office:database> 12.1
+    //   <office:drawing> 3.5
+    //   <office:image> 3.9
+    //   <office:presentation> 3.6
+    //   <office:spreadsheet> 3.7
+    //   <office:text> 3.4
+    //
+    // Of those only <office:text> is present in a text document (odt).
+    while (reader.readNextStartElement()) {
+        QString tagName = reader.qualifiedName().toString();
+        
+        if (tagName == "office:text") {
+            readElementOfficeText(reader);
+        }
+        else {
+            reader.skipCurrentElement();
+        }
+    }
+
+    m_backend->elementOfficeBody(reader, m_context);
+}
+
+void OdtReader::readElementOfficeText(KoXmlStreamReader &reader)
+{
+    m_backend->elementOfficeText(reader, m_context);
+
+    // <office:text> has the following children in ODF 1.2:
+    //
+    // In addition to the text level tags like <text:p> etc that can
+    // be found in any textbox, table cell, etc, it has the following
+    // text document children:
+    //
+    //   <office:forms> 13.2
+    //   <table:calculation-settings> 9.4.1
+    //   <table:consolidation> 9.7
+    //   <table:content-validations> 9.4.4
+    //   <table:database-ranges> 9.4.14
+    //   <table:data-pilot-tables> 9.6.2
+    //   <table:dde-links> 9.8
+    //   <table:label-ranges> 9.4.10
+    //   <table:named-expressions> 9.4.11
+    //   <text:alphabetical-index-auto-mark-file> 8.8.3
+    //   <text:dde-connection-decls> 14.6.2
+    //   <text:page-sequence> 5.2
+    //   <text:sequence-decls> 7.4.11
+    //   <text:tracked-changes> 5.5.1
+    //   <text:user-field-decls> 7.4.7
+    //   <text:variable-decls> 7.4.2
+    //
+    // FIXME: For now, none of these are handled
+    while (reader.readNextStartElement()) {
+        QString tagName = reader.qualifiedName().toString();
+        
+        if (tagName == "office:forms") {
+            // FIXME: NYI
+            reader.skipCurrentElement();
+            
+        }
+        else if (tagName == "office:body") {
+            // HANDLE CONTENTS HERE
+        }
+        else {
+            readTextLevelElements(reader);
+        }
+    }
+
+    m_backend->elementOfficeText(reader, m_context);
+}
+
+
+// ----------------------------------------------------------------
+//                         Text level functions
+
+
+// This function is a bit special since it doesn't handle a specific
+// element.  Instead it handles the common child elements between a
+// number of text-level elements.
+//
+void OdtReader::readTextLevelElements(KoXmlStreamReader &reader)
+{
+    // We should not call any backend functions here.  That is already
+    // done in the functions that call this one.
+
+    // We define the common elements on the text level as the
+    // following list.  They are the basic text level contents that
+    // can be found in a text box (<draw:text-box>) but also in many
+    // other places like <table:table-cell>, <text:section>,
+    // <office:text>, etc.
+    //
+    // The ones that are not text boxes can also have other children
+    // but these are the ones we have found to be the common ones.
+    //
+    //   <dr3d:scene> 10.5.2
+    //   <draw:a> 10.4.12
+    //   <draw:caption> 10.3.11
+    //   <draw:circle> 10.3.8
+    //   <draw:connector> 10.3.10
+    //   <draw:control> 10.3.13
+    //   <draw:custom-shape> 10.6.1
+    //   <draw:ellipse> 10.3.9
+    //   <draw:frame> 10.4.2
+    //   <draw:g> 10.3.15
+    //   <draw:line> 10.3.3
+    //   <draw:measure> 10.3.12
+    //   <draw:page-thumbnail> 10.3.14
+    //   <draw:path> 10.3.7
+    //   <draw:polygon> 10.3.5
+    //   <draw:polyline> 10.3.4
+    //   <draw:rect> 10.3.2
+    //   <draw:regular-polygon> 10.3.6
+    //   <table:table> 9.1.2
+    //   <text:alphabetical-index> 8.8
+    //   <text:bibliography> 8.9
+    //   <text:change> 5.5.7.4
+    //   <text:change-end> 5.5.7.3
+    //   <text:change-start> 5.5.7.2
+    //   <text:h> 5.1.2
+    //   <text:illustration-index> 8.4
+    //   <text:list> 5.3.1
+    //   <text:numbered-paragraph> 5.3.6
+    //   <text:object-index> 8.6
+    //   <text:p> 5.1.3
+    //   <text:section> 5.4
+    //   <text:soft-page-break> 5.6
+    //   <text:table-index> 8.5
+    //   <text:table-of-content> 8.3
+    //   <text:user-index> 8.7
+
+    QString tagName = reader.qualifiedName().toString();
+        
+    // FIXME: Only paragraphs are handled right now.
+    if (tagName == "text:h") {
+        readElementTextH(reader);
+    }
+    else if (tagName == "text:p") {
+        readElementTextP(reader);
+    }
+    else {
+        readUnknownElement(reader);
+    }
+}
+
+
+void OdtReader::readElementTextH(KoXmlStreamReader &reader)
+{
+    m_backend->elementTextH(reader, m_context);
+
+    readParagraphLevelElements(reader);
+
+    m_backend->elementTextH(reader, m_context);
+}
+
+void OdtReader::readElementTextP(KoXmlStreamReader &reader)
+{
+    m_backend->elementTextP(reader, m_context);
+
+    readParagraphLevelElements(reader);
+
+    m_backend->elementTextP(reader, m_context);
+}
+
+
+// ----------------------------------------------------------------
+//                     Paragraph level functions
+
+
+// This function is a bit special since it doesn't handle a specific
+// element.  Instead it handles the common child elements between a
+// number of paragraph-level elements.
+//
+void OdtReader::readParagraphLevelElements(KoXmlStreamReader &reader)
+{
+    // We should not call any backend functions here.  That is already
+    // done in the functions that call this one.
+
+    if (reader.isCharacters()) {
+        m_backend->characterData(reader, m_context);
+    }
+
+    // We define the common elements on the paragraph level as the
+    // following list.  They are the basic paragraph level contents that
+    // can be found in a paragraph (text:p), heading (text:h), etc
+    //
+
+    QString tagName = reader.qualifiedName().toString();
+        
+    // FIXME: Only very few tags are handled right now.
+    if (tagName == "text:span") {
+        readElementTextSpan(reader);
+    }
+    else if (tagName == "text:s") {
+        readElementTextS(reader);
+    }
+    else {
+        readUnknownElement(reader);
+    }
+}
+
+
+void OdtReader::readElementTextS(KoXmlStreamReader &reader)
+{
+    m_backend->elementTextS(reader, m_context);
+
+    // FIXME: NYI
+    reader.skipCurrentElement();
+
+    m_backend->elementTextS(reader, m_context);
+}
+
+void OdtReader::readElementTextSpan(KoXmlStreamReader &reader)
+{
+    m_backend->elementTextSpan(reader, m_context);
+
+    // FIXME: NYI
+    reader.skipCurrentElement();
+
+    m_backend->elementTextSpan(reader, m_context);
+}
+
+
+// ----------------------------------------------------------------
+//                             Other functions
+
+
+void OdtReader::readUnknownElement(KoXmlStreamReader &reader)
+{
+    // FIXME: NYI
+    reader.skipCurrentElement();
+}
