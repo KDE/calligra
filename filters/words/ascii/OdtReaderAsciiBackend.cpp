c3cb4a6065a25351a14342d8e9dee0f5cfb084d5
Inge Wallin <inge@lysator.liu.se>
2013-05-18 22:10:36 +0200

    Port the ascii filter to the odfreader.
    Still doesn't work perfectly.

---
+++ filters/words/ascii/OdtReaderAsciiBackend.cpp
@@ -0,0 +1,112 @@
+/* This file is part of the KDE project
+
+   Copyright (C) 2013 Inge Wallin            <inge@lysator.liu.se>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+
+// Own
+#include "OdtReaderAsciiBackend.h"
+
+// Calligra
+#include <KoXmlReader.h>
+
+// This filter
+#include "OdfReaderAsciiContext.h"
+
+
+
+// ================================================================
+//                 class OdtReaderAsciiBackend
+
+
+OdtReaderAsciiBackend::OdtReaderAsciiBackend(OdfReaderContext *context)
+    : OdtReaderBackend(context)
+{
+}
+
+OdtReaderAsciiBackend::~OdtReaderAsciiBackend()
+{
+}
+
+
+
+// ----------------------------------------------------------------
+// Text level functions: paragraphs, headings, sections, frames, objects, etc
+
+void OdtReaderAsciiBackend::elementTextH(KoXmlStreamReader &reader, OdfReaderContext *context)
+{
+    Q_UNUSED(reader);
+    Q_UNUSED(context);
+}
+
+void OdtReaderAsciiBackend::elementTextP(KoXmlStreamReader &reader, OdfReaderContext *context)
+{
+    if (!reader.isEndElement())
+        return;
+
+    OdfReaderAsciiContext *asciiContext = dynamic_cast<OdfReaderAsciiContext*>(context);
+    if (!asciiContext) {
+        return;
+    }
+
+    // At the end of a paragraph, output two newlines.
+    asciiContext->outStream << "\n\n";
+}
+
+
+// ----------------------------------------------------------------
+// Paragraph level functions: spans, annotations, notes, text content itself, etc.
+
+void OdtReaderAsciiBackend::elementTextSpan(KoXmlStreamReader &reader, OdfReaderContext *context)
+{
+    Q_UNUSED(reader);
+    Q_UNUSED(context);
+}
+
+void OdtReaderAsciiBackend::elementTextS(KoXmlStreamReader &reader, OdfReaderContext *context)
+{
+    if (!reader.isStartElement())
+        return;
+
+    OdfReaderAsciiContext *asciiContext = dynamic_cast<OdfReaderAsciiContext*>(context);
+    if (!asciiContext) {
+        return;
+    }
+
+#if 0
+    QString dummy = element.attribute("text:c", "1");
+    bool ok;
+    int  numSpaces = dummy.toUInt(&ok);
+    if (!ok) 
+        numSpaces = 1;
+
+    // At the end of a paragraph, output two newlines.
+    asciiContext->outStream << "\n\n";
+#endif
+}
+
+
+void OdtReaderAsciiBackend::characterData(KoXmlStreamReader &reader, OdfReaderContext *context)
+{
+    OdfReaderAsciiContext *asciiContext = dynamic_cast<OdfReaderAsciiContext*>(context);
+    if (!asciiContext) {
+        return;
+    }
+
+    asciiContext->outStream << reader.text().data();
+}
---
+++ filters/words/ascii/OdtReaderAsciiBackend.h
@@ -0,0 +1,70 @@
+/* This file is part of the KDE project
+
+   Copyright (C) 2013 Inge Wallin            <inge@lysator.liu.se>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef ODTREADERASCIIBACKEND_H
+#define ODTREADERASCIIBACKEND_H
+
+// Calligra
+#include <KoXmlStreamReader.h>
+#include <KoFilter.h>
+#include <OdtReaderBackend.h>
+
+
+
+class QByteArray;
+class QSizeF;
+class QStringList;
+class KoStore;
+class OdfReaderContext;
+
+
+class OdtReaderAsciiBackend : public OdtReaderBackend
+{
+ public:
+    explicit OdtReaderAsciiBackend(OdfReaderContext *context);
+    virtual ~OdtReaderAsciiBackend();
+
+    // Called before and after the actual traversal.
+    // FIXME: NYI
+
+    // Called when there is a document level element with children.
+
+    // ----------------------------------------------------------------
+    // ODT document level functions
+    //void elementOfficeBody(KoXmlStreamReader &reader, OdfReaderContext *context);
+    //void elementOfficeText(KoXmlStreamReader &reader, OdfReaderContext *context);
+
+    // ----------------------------------------------------------------
+    // Text level functions: paragraphs, headings, sections, frames, objects, etc
+
+    void elementTextH(KoXmlStreamReader &reader, OdfReaderContext *context);
+    void elementTextP(KoXmlStreamReader &reader, OdfReaderContext *context);
+
+    // ----------------------------------------------------------------
+    // Paragraph level functions: spans, annotations, notes, text content itself, etc.
+
+    void elementTextSpan(KoXmlStreamReader &reader, OdfReaderContext *context);
+    void elementTextS(KoXmlStreamReader &reader, OdfReaderContext *context);
+
+    void characterData(KoXmlStreamReader &reader, OdfReaderContext *context);
+};
+
+
+#endif // ODTREADERASCIIBACKEND_H
