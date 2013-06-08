ae4d4e884d659f9e7a6bc6d2efc10488c75ce2bd
Cyrille Berger <cberger@cberger.net>
2013-06-08 17:43:09 +0200

    update to version 2.6.92 (Beta 3)

--- libs/main/calligraversion.h
+++ libs/main/calligraversion.h
@@ -33,7 +33,7 @@
 * it contains the version at compile time. See Calligra::versionString() if you need
 * the Calligra version used at runtime.
 */
-#define CALLIGRA_VERSION_STRING "2.7 Beta 2"
+#define CALLIGRA_VERSION_STRING "2.7 Beta 3"
 
 /**
  * @def CALLIGRA_VERSION_MAJOR
@@ -56,7 +56,7 @@
  * @ingroup CalligraMacros
  * @brief Release version of Calligra, at compile time
  */
-#define CALLIGRA_VERSION_RELEASE 91
+#define CALLIGRA_VERSION_RELEASE 92
 
 /**
  * @def CALLIGRA_ALPHA
@@ -70,7 +70,7 @@
  * @ingroup CalligraMacros
  * @brief If defined (1..9), indicates at compile time that Calligra is in beta stage
  */
-#define CALLIGRA_BETA 1
+#define CALLIGRA_BETA 3
 
 /**
  * @def CALLIGRA_RC
