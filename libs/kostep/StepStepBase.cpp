ba4c5d01f4f17c1abd6d861d9dc1720b072aac21
Luke De Mouy <lukewolf101010@gmail.com>
2013-06-13 14:59:14 -0700

    Expand StepStepBase.h to being a full base class for Steps

---
+++ libs/kostep/StepStepBase.cpp
@@ -0,0 +1,47 @@
+/*
+    <one line to give the library's name and an idea of what it does.>
+    Copyright (C) 2013  Luke Wolf <Lukewolf101010@gmail.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#include "StepStepBase.h"
+
+StepStepBase::StepStepBase()
+{
+
+}
+
+StepStepBase::StepStepBase(const StepStepBase& other)
+{
+
+}
+
+StepStepBase::~StepStepBase()
+{
+
+}
+
+StepStepBase& StepStepBase::operator=(const StepStepBase& other)
+{
+    return *this;
+}
+
+bool StepStepBase::operator==(const StepStepBase& other) const
+{
+///TODO: return ...;
+}
+
---
+++ libs/kostep/StepStepBase.h
@@ -0,0 +1,73 @@
+/*
+    <one line to give the library's name and an idea of what it does.>
+    Copyright (C) 2013  Luke Wolf <Lukewolf101010@gmail.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#ifndef STEPSTEPBASE_H
+#define STEPSTEPBASE_H
+#include <QtCore/qobject.h>
+#include <QtCore/QString>
+
+
+class StepStepBase : public QObject
+{
+  public:
+    Q_OBJECT
+    
+    //Basic Properties of a step, allows for usage in QML
+    Q_PROPERTY(bool Foreign READ Foreign WRITE setForeign)
+    Q_PROPERTY(int Position READ Position WRITE setPosition)
+    Q_PROPERTY(QString Step READ Step);
+    
+    StepStepBase();
+    StepStepBase(const StepStepBase& other);
+    virtual ~StepStepBase();
+    virtual StepStepBase& operator=(const StepStepBase& other);
+    virtual bool operator==(const StepStepBase& other) const;
+    
+    //String Representation of a Step
+    virtual QString toString();
+    //XML Representation of a Step for use in serialization
+    virtual QString toXML();
+    
+    //The Foreign Property gives indication whether the step came
+    //from a source besides the user, say for instance if the user
+    //is collaboratively editing with another user and thus receiving
+    //steps from them
+    bool Foreign();
+    virtual void setForeign(bool foreign);
+    
+    //The Position Property gives the position in characters from the
+    //beginning of the text  which is then used when doing Operational
+    //Transformation to allow derivative operations
+    int Position();
+    virtual void setPosition(int position);
+    
+    //The Step Property holds the text of the change so say I've got the word
+    //Dog and I make it Dogs, Step would hold an "s"
+    QString Step();    
+    
+    
+private:
+  bool _isForeign;
+  int _position;
+  QString _Step;
+  
+};
+
+#endif // STEPSTEPBASE_H
