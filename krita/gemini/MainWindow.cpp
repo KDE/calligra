1f9441994f79ae0a66e19d10fea68370b838f51d
Dan Leinir Turthra Jensen <admin@leinir.dk>
2013-07-12 10:20:10 +0100

    First Gemini code
    Loosely based on the newly minimized Sketch code, first switch achieved.
    Crashes on exit, timers trigger switches now. Actions up next,
    then reactions to the appropriate signals.

---
+++ krita/gemini/MainWindow.cpp
@@ -0,0 +1,183 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2012 Arjen Hiemstra <ahiemstra@heimr.nl>
+ * Copyright (C) 2012 KO GmbH. Contact: Boudewijn Rempt <boud@kogmbh.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "MainWindow.h"
+
+#include "opengl/kis_opengl.h"
+
+#include <QApplication>
+#include <QResizeEvent>
+#include <QDeclarativeView>
+#include <QDeclarativeContext>
+#include <QDeclarativeEngine>
+#include <QDir>
+#include <QFile>
+#include <QMessageBox>
+#include <QFileInfo>
+#include <QGLWidget>
+
+#include <kcmdlineargs.h>
+#include <kurl.h>
+#include <kstandarddirs.h>
+
+#include <KoColorSpaceRegistry.h>
+#include <KoColorSpace.h>
+#include <KoMainWindow.h>
+
+#include "kis_config.h"
+#include <kis_factory2.h>
+#include <kis_doc2.h>
+
+#include "cpuid.h"
+#include "sketch/SketchDeclarativeView.h"
+#include "sketch/RecentFileManager.h"
+#include "sketch/DocumentManager.h"
+#include <sketch/KisSketchPart.h>
+
+class MainWindow::Private
+{
+public:
+    Private() 
+        : allowClose(true)
+        , sketchView(0)
+        , desktopView(0)
+        , currentView(0)
+    { }
+    bool allowClose;
+    QDeclarativeView* sketchView;
+    KoMainWindow* desktopView;
+    QObject* currentView;
+
+    void initSketchView(QObject* parent)
+    {
+        sketchView = new SketchDeclarativeView();
+        sketchView->engine()->rootContext()->setContextProperty("mainWindow", parent);
+
+        QDir appdir(qApp->applicationDirPath());
+        // for now, the app in bin/ and we still use the env.bat script
+        appdir.cdUp();
+
+        sketchView->engine()->addImportPath(appdir.canonicalPath() + "/lib/calligra/imports");
+        sketchView->engine()->addImportPath(appdir.canonicalPath() + "/lib64/calligra/imports");
+        QString mainqml = appdir.canonicalPath() + "/share/apps/kritagemini/kritagemini.qml";
+
+        Q_ASSERT(QFile::exists(mainqml));
+        if (!QFile::exists(mainqml)) {
+            QMessageBox::warning(0, "No QML found", mainqml + " doesn't exist.");
+        }
+        QFileInfo fi(mainqml);
+
+        sketchView->setSource(QUrl::fromLocalFile(fi.canonicalFilePath()));
+        sketchView->setResizeMode( QDeclarativeView::SizeRootObjectToView );
+    }
+
+    void initDesktopView()
+    {
+        desktopView = new KoMainWindow(KisFactory2::componentData());
+    }
+};
+
+MainWindow::MainWindow(QStringList fileNames, QWidget* parent, Qt::WindowFlags flags )
+    : QMainWindow( parent, flags ), d( new Private )
+{
+    qApp->setActiveWindow( this );
+
+    setWindowTitle(i18n("Krita Gemini"));
+
+    KisConfig cfg;
+    cfg.setCursorStyle(CURSOR_STYLE_NO_CURSOR);
+    cfg.setUseOpenGL(true);
+
+    foreach(QString fileName, fileNames) {
+        DocumentManager::instance()->recentFileManager()->addRecent(fileName);
+    }
+
+    connect(DocumentManager::instance(), SIGNAL(documentChanged()), SLOT(documentChanged()));
+
+    d->initSketchView(this);
+    d->initDesktopView();
+    // Set the initial view to sketch... because reasons.
+    switchToSketch();
+}
+
+void MainWindow::switchToSketch()
+{
+    d->desktopView->hide();
+    d->desktopView->setParent(0);
+    setCentralWidget(d->sketchView);
+    d->sketchView->show();
+    qDebug() << "Switched to sketch view";
+}
+
+void MainWindow::switchToDesktop()
+{
+    d->sketchView->hide();
+    d->sketchView->setParent(0);
+    setCentralWidget(d->desktopView);
+    d->desktopView->show();
+    qDebug() << "Switched to desktop view";
+}
+
+void MainWindow::documentChanged()
+{
+    d->desktopView->setRootDocument(DocumentManager::instance()->document(), DocumentManager::instance()->part());
+    QTimer::singleShot(5000, this, SLOT(switchToDesktop()));
+    QTimer::singleShot(15000, this, SLOT(switchToSketch()));
+}
+
+bool MainWindow::allowClose() const
+{
+    return d->allowClose;
+}
+
+void MainWindow::setAllowClose(bool allow)
+{
+    d->allowClose = allow;
+}
+
+void MainWindow::minimize()
+{
+    setWindowState(windowState() ^ Qt::WindowMinimized);
+}
+
+void MainWindow::closeWindow()
+{
+    //For some reason, close() does not work even if setAllowClose(true) was called just before this method.
+    //So instead just completely quit the application, since we are using a single window anyway.
+    QApplication::exit();
+}
+
+void MainWindow::closeEvent(QCloseEvent* event)
+{
+    if(!d->allowClose) {
+        event->ignore();
+        emit closeRequested();
+    } else {
+        event->accept();
+        d->desktopView->close();
+    }
+}
+
+MainWindow::~MainWindow()
+{
+    delete d;
+}
+
+#include "MainWindow.moc"
---
+++ krita/gemini/MainWindow.h
@@ -0,0 +1,54 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2012 Arjen Hiemstra <ahiemstra@heimr.nl>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+    Q_PROPERTY(bool allowClose READ allowClose WRITE setAllowClose)
+
+public:
+    explicit MainWindow(QStringList fileNames, QWidget* parent = 0, Qt::WindowFlags flags = 0);
+    virtual ~MainWindow();
+
+    bool allowClose() const;
+    void setAllowClose(bool allow);
+
+    virtual void closeEvent(QCloseEvent* event);
+
+public Q_SLOTS:
+    void minimize();
+    void closeWindow();
+
+    void switchToSketch();
+    void switchToDesktop();
+    void documentChanged();
+Q_SIGNALS:
+    void closeRequested();
+
+private:
+    class Private;
+    Private * const d;
+};
+
+#endif // MAINWINDOW_H
