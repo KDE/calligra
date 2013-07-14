3242b33fde86b80a36cabce73bba2947b7a1eee2
Inge Wallin <inge@lysator.liu.se>
2013-07-14 17:07:04 +0200

    Create the wiki export filter
    I did this because Moji is on Ramadan and cannot concentrate :)

---
+++ filters/words/wiki/export/WikiExport.h
@@ -0,0 +1,44 @@
+/* This file is part of the KDE project
+
+   Copyright (C) 2013 Inge Wallin <inge@lysator.liu.se>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+ */
+
+#ifndef WIKIEXPORT_H
+#define WIKIEXPORT_H
+
+#include <KoFilter.h>
+
+#include <QVariantList>
+
+class QByteArray;
+class QTextStream;
+
+
+class WikiExport : public KoFilter
+{
+    Q_OBJECT
+
+public:
+    WikiExport(QObject *parent, const QVariantList &);
+    virtual ~WikiExport();
+    virtual KoFilter::ConversionStatus convert(const QByteArray& from, const QByteArray& to);
+
+private:
+};
+
+#endif /* WIKIEXPORT_H */
