2b5cbee1d3ae0c408e774b625dca60997cba1a4f
Luke Wolf <lukewolf101010@gmail.com>
2013-07-22 23:08:52 -0700

    set up the 4 current basic steps for right now they're essentially stubs but that will solidify better as the standard advances

---
+++ libs/kostep/StepAddTextBlockStep.cpp
@@ -0,0 +1,55 @@
+/*
+ *     kostep -- handles changetracking using operational transformation for calligra
+ *     Copyright (C) 2013  Luke Wolf <Lukewolf101010@gmail.com>
+ *
+ *     This library is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public
+ *     License as published by the Free Software Foundation; either
+ *     version 2.1 of the License, or (at your option) any later version.
+ *
+ *     This library is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *     Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with this library; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "StepAddTextBlockStep.h"
+#include "StepAddTextBlockStep_p.h"
+
+StepAddTextBlockStepPrivate::StepAddTextBlockStepPrivate (StepAddTextBlockStep * q):q (q)
+{
+}
+
+StepAddTextBlockStepPrivate::~StepAddTextBlockStepPrivate ()
+{
+}
+
+StepAddTextBlockStep::StepAddTextBlockStep ():d (new
+   StepAddTextBlockStepPrivate
+   (this))
+{
+
+}
+
+StepAddTextBlockStep::~StepAddTextBlockStep ()
+{
+  delete d;
+
+}
+
+QString
+StepAddTextBlockStep::toString ()
+{
+  return "";
+}
+
+QString
+StepAddTextBlockStep::toXML ()
+{
+  return "";
+
+}
---
+++ libs/kostep/StepDeleteTextBlockStep.cpp
@@ -0,0 +1,61 @@
+/*
+ *     kostep -- handles changetracking using operational transformation for calligra
+ *     Copyright (C) 2013  Luke Wolf <Lukewolf101010@gmail.com>
+ *
+ *     This library is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public
+ *     License as published by the Free Software Foundation; either
+ *     version 2.1 of the License, or (at your option) any later version.
+ *
+ *     This library is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *     Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with this library; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "StepDeleteTextBlockStep.h"
+#include "StepDeleteTextBlockStep_p.h"
+
+StepDeleteTextBlockStepPrivate::StepDeleteTextBlockStepPrivate (StepDeleteTextBlockStep * q):q (q)
+{
+}
+
+StepDeleteTextBlockStepPrivate::~StepDeleteTextBlockStepPrivate ()
+{
+}
+
+StepDeleteTextBlockStep::StepDeleteTextBlockStep ():d (new
+   StepDeleteTextBlockStepPrivate
+   (this))
+{
+
+}
+
+StepDeleteTextBlockStep::
+StepDeleteTextBlockStep (const StepDeleteTextBlockStep & other):
+d (new StepDeleteTextBlockStepPrivate (this))
+{
+
+}
+
+StepDeleteTextBlockStep::~StepDeleteTextBlockStep ()
+{
+  delete d;
+
+}
+
+QString StepDeleteTextBlockStep::toString ()
+{
+  return "";
+
+}
+
+QString StepDeleteTextBlockStep::toXML ()
+{
+  return "";
+
+}
