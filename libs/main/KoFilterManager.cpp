3679c6bfdb79f0a9d9ee5fd4624165bfeffbd3a5
Boudewijn Rempt <boud@valdyas.org>
2013-08-03 21:45:53 +0200

    Move klibloader away

--- libs/main/KoFilterManager.cpp
+++ libs/main/KoFilterManager.cpp
@@ -35,11 +35,10 @@ Boston, MA 02110-1301, USA.
 
 #include <klocale.h>
 #include <kmessagebox.h>
-#include <klibloader.h>
 #include <ksqueezedtextlabel.h>
 #include <kmimetype.h>
 #include <kdebug.h>
-
+#include <klibrary.h>
 #include <queue>
 
 #include <unistd.h>
@@ -504,7 +503,6 @@ bool KoFilterManager::filterAvailable(KoFilterEntry::Ptr entry)
             return false;
         }
 
-        // This code is "borrowed" from klibloader ;)
         QByteArray symname = "check_" + QString(library.objectName()).toLatin1();
         KLibrary::void_function_ptr sym = library.resolveFunction(symname);
         if (!sym) {
@@ -524,7 +522,6 @@ bool KoFilterManager::filterAvailable(KoFilterEntry::Ptr entry)
 void KoFilterManager::importErrorHelper(const QString& mimeType, const bool suppressDialog)
 {
     QString tmp = i18n("Could not import file of type\n%1", mimeType);
-    // ###### FIXME: use KLibLoader::lastErrorMessage() here
     if (!suppressDialog) KMessageBox::error(0, tmp, i18n("Missing Import Filter"));
 }
 
