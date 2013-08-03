c038a2cbabecd8b82d4f63ecdb0eaf72a2626958
Boudewijn Rempt <boud@valdyas.org>
2013-08-03 04:17:09 +0200

    Add setValid() method to make DatabaseDialog build

--- fake/kassistantdialog.h
+++ fake/kassistantdialog.h
@@ -8,6 +8,8 @@ class KAssistantDialog : public KPageDialog
 public:
     KAssistantDialog(QWidget *parent=0, Qt::WFlags flags=0) : KPageDialog(parent) {}
     virtual ~KAssistantDialog() {}
+    void setValid(KPageWidgetItem* page, bool enable) {Q_UNUSED(page); Q_UNUSED(enable);}
+
 };
 
 #endif
