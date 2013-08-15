91939fa2352c7bd5393e715000c23440d14e7c87
Boudewijn Rempt <boud@valdyas.org>
2013-08-15 13:50:08 +0200

    Forgot this one, make build

--- stage/app/main.cpp
+++ stage/app/main.cpp
@@ -36,7 +36,7 @@ extern "C" KDE_EXPORT int kdemain( int argc, char **argv )
     options.add("+[file]", ki18n("File to open"));
     KCmdLineArgs::addCmdLineOptions( options );
 
-    KoApplication app(MIME_TYPE);
+    KoApplication app(STAGE_MIME_TYPE);
 
     if (!app.start())
         return 1;
