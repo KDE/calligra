421ef02434b5d536c76582c03fe1c9e4e65b695d
Boudewijn Rempt <boud@valdyas.org>
2013-08-22 15:35:10 +0200

    Better fix
    15:32:55 < dfaure> boud: another thing: line 31 in the above code will crash for sure ;) qApp doesn't exist yet at
                       that point
    15:33:35 < boud> dfaure: ah, right
    15:33:40 < boud> and that's in the qt4 version as well
    15:34:14 < dfaure> boud: oh it's static
    15:34:18 < dfaure> so no crash, just weird syntax
    15:34:23 < dfaure> this should be QApplication::, instead
    
    :-)

--- words/app/main.cpp
+++ words/app/main.cpp
@@ -25,6 +25,10 @@ extern "C" KDE_EXPORT int kdemain(int argc, char **argv)
 {
     KAboutData * aboutData = newWordsAboutData();
 
+#ifdef Q_WS_X11
+    // the "raster" graphicssystem is way faster then the "native" graphicssystem on x11 with Calligra Words
+    QApplication::setGraphicsSystem( QLatin1String("raster") );
+#endif
 
     KCmdLineArgs::init(argc, argv, aboutData);
 
@@ -33,10 +37,6 @@ extern "C" KDE_EXPORT int kdemain(int argc, char **argv)
     KCmdLineArgs::addCmdLineOptions(options);
 
     KoApplication app;
-#ifdef Q_WS_X11
-    // the "raster" graphicssystem is way faster then the "native" graphicssystem on x11 with Calligra Words
-    qApp->setGraphicsSystem( QLatin1String("raster") );
-#endif
 
     if (!app.start())
         return 1;
