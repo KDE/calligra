71e4faa88458391dc643e34d7db7cbea8cb63d2c
Yue Liu <yue.liu@mail.com>
2013-09-07 03:40:07 -0400

    re-fectored KoFileDialog API

--- sheets/dialogs/CSVDialog.cpp
+++ sheets/dialogs/CSVDialog.cpp
@@ -77,8 +77,8 @@ CSVDialog::CSVDialog(QWidget* parent, Selection* selection, Mode mode)
     } else if (m_mode == File) {
         //setWindowTitle(i18n("Inserting Text File"));
         KoFileDialog dialog;
-        m_filename = dialog.getOpenFileName(parent, i18n("Import CSV Data File"),
-                                                        "", i18n("CSV data files (*.csv)"));
+        m_filename = dialog.getImportFileName(parent, i18n("Import CSV Data File"),
+                                                        "", QStringList(i18n("CSV data files (*.csv)")));
         //cancel action !
         if (m_filename.isEmpty()) {
             enableButton(Ok, false);
