69c6774a065bfb8eed1df45751a6c6fbe0b75295
Boudewijn Rempt <boud@valdyas.org>
2013-10-19 12:49:00 +0200

    Fix copyright attribution

--- libs/flake/tests/TestSnapStrategy.cpp
+++ libs/flake/tests/TestSnapStrategy.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2012 Stuart Dickson <stuartmd@kogmbh.com>
+    Copyright (C) 2012 <hanna.et.scott@gmail.com> 
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
