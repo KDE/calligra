cacf2b9bb0c1e97987d46c26edadc2fea040ee8c
C. Boemann <cbo@boemann.dk>
2013-10-31 12:06:56 +0100

    Writing temporary files directly to / in most caaes is not allowed so writing of temporary files fails And since all images over 90k uses this we had a huge visual regression This should possibly also fix a lot of other issues where we use KTemporaryFile with setting suffix or prefix.

--- fake/ktemporaryfile.h
+++ fake/ktemporaryfile.h
@@ -9,11 +9,11 @@ class KTemporaryFile : public QTemporaryFile
 public:
     void setPrefix(const QString &s) {
         m_prefix = s;
-        setFileTemplate(QDir::tempPath() + m_prefix + QLatin1String("XXXXXX") + m_suffix);
+        setFileTemplate(QDir::tempPath() + '/' + m_prefix + QLatin1String("XXXXXX") + m_suffix);
     }
     void setSuffix(const QString &s) {
         m_suffix = s;
-        setFileTemplate(QDir::tempPath() + m_prefix + QLatin1String("XXXXXX") + m_suffix);
+        setFileTemplate(QDir::tempPath() + '/' + m_prefix + QLatin1String("XXXXXX") + m_suffix);
     }
 private:
     QString m_prefix, m_suffix;
