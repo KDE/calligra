da9aad0535e380b096b71e7bfa98ef2322c0e7b4
Friedrich W. H. Kossebau <kossebau@kde.org>
2013-11-03 03:45:49 +0100

    Resolve hardcoding of contact/event/location mimedata drop in KoRdfSemanticTree into generic solution
    Only issue: some extra logic for "Location" stays hardcoded for now

--- libs/rdf/KoRdfSemanticTree.cpp
+++ libs/rdf/KoRdfSemanticTree.cpp
@@ -19,6 +19,8 @@
 */
 
 #include "KoRdfSemanticTree.h"
+
+#include "KoRdfSemanticItemRegistry.h"
 #include "KoDocumentRdf.h"
 #include "KoRdfSemanticTreeWidgetItem.h"
 #include <kdebug.h>
@@ -30,24 +32,20 @@ class KoRdfSemanticTreePrivate : public QSharedData
 {
 public:
     QTreeWidget *m_tree;
-    QTreeWidgetItem *m_peopleItem;
-    QTreeWidgetItem *m_eventsItem;
-    QTreeWidgetItem *m_locationsItem;
+    QHash<QString, QTreeWidgetItem*> m_treeWidgetItems;
 
     KoRdfSemanticTreePrivate(QTreeWidget *tree)
         {
             m_tree = tree;
             if(m_tree)  {
                 tree->sortItems(1, Qt::DescendingOrder);
-                m_peopleItem = new QTreeWidgetItem(tree);
-                m_peopleItem->setText(0, i18n("People"));
-                tree->expandItem(m_peopleItem);
-                m_eventsItem = new QTreeWidgetItem(tree);
-                m_eventsItem->setText(0, i18n("Events"));
-                tree->expandItem(m_eventsItem);
-                m_locationsItem = new QTreeWidgetItem(tree);
-                m_locationsItem->setText(0, i18n("Locations"));
-                tree->expandItem(m_locationsItem);
+
+                foreach (const QString &semanticClass, KoRdfSemanticItemRegistry::instance()->classNames()) {
+                    QTreeWidgetItem *treeWidgetItem = new QTreeWidgetItem(tree);
+                    treeWidgetItem->setText(0, KoRdfSemanticItemRegistry::instance()->classDisplayName(semanticClass));
+                    tree->expandItem(treeWidgetItem);
+                    m_treeWidgetItems.insert(semanticClass, treeWidgetItem);
+                }
             }
         }
     void update(KoDocumentRdf *rdf, QSharedPointer<Soprano::Model> model);
@@ -108,63 +106,42 @@ void KoRdfSemanticTreePrivate::buildSelectedSet(QTreeWidgetItem *parent, QSet<QS
 
 void KoRdfSemanticTreePrivate::update(KoDocumentRdf *rdf, QSharedPointer<Soprano::Model> model)
 {
-    QSet<QString> selectedPeople;
-    QSet<QString> selectedEvents;
-    QSet<QString> selectedLocations;
+    QHash<QString, QTreeWidgetItem*>::ConstIterator it = m_treeWidgetItems.constBegin();
+    QHash<QString, QTreeWidgetItem*>::ConstIterator end = m_treeWidgetItems.constEnd();
+    for( ; it != end; ++it) {
+        const QString &semanticClass = it.key();
+        QTreeWidgetItem *treeWidgetItem = it.value();
 
-    buildSelectedSet(m_peopleItem, selectedPeople);
-    buildSelectedSet(m_eventsItem, selectedEvents);
-    buildSelectedSet(m_locationsItem, selectedLocations);
-    clear(m_peopleItem);
-    clear(m_eventsItem);
-    clear(m_locationsItem);
-    m_peopleItem->setSelected(false);
-    m_eventsItem->setSelected(false);
-    m_locationsItem->setSelected(false);
+        QSet<QString> selectedItems;
+        buildSelectedSet(treeWidgetItem, selectedItems);
+        clear(treeWidgetItem);
+        treeWidgetItem->setSelected(false);
 
-    // people
-    const QList<hKoRdfSemanticItem> contactSemanticItems = rdf->semanticItems("Contact", model);
-    foreach (hKoRdfSemanticItem semanticItem, contactSemanticItems) {
-        KoRdfSemanticTreeWidgetItem *item = semanticItem->createQTreeWidgetItem(m_peopleItem);
-        if (selectedPeople.contains(item->semanticItem()->name())) {
-            item->setSelected(true);
+        // TODO: for locations this unexplained hack is done, find out why and if really needed
+        if (model && semanticClass == QLatin1String("Location")) {
+            //
+            // grab the lat/long triples from m_model into the passed model.
+            //
+            // geo84
+            // <uri:dan84> <http://xmlns.com/foaf/0.1/based_near> _:genid1
+            // _:genid1 <http://www.w3.org/2003/01/geo/wgs84_pos#lat> "51.47026" (empty)
+            // _:genid1 <http://www.w3.org/2003/01/geo/wgs84_pos#long> "-2.59466" (empty)
+            rdf->expandStatementsSubjectPointsTo(model);
+            //kDebug(30015) << "expanding lists... old.sz:" << model->statementCount();
+            // other geo is an rdf:list, so bring that in too
+            rdf->expandStatementsToIncludeRdfLists(model);
+            //kDebug(30015) << "expanding lists... new.sz:" << model->statementCount();
         }
-    }
-    m_tree->expandItem(m_peopleItem);
 
-    // events
-    const QList<hKoRdfSemanticItem> eventSemanticItems = rdf->semanticItems("Event", model);
-    foreach (hKoRdfSemanticItem semanticItem, eventSemanticItems) {
-        KoRdfSemanticTreeWidgetItem *item = semanticItem->createQTreeWidgetItem(m_eventsItem);
-        if (selectedEvents.contains(item->semanticItem()->name())) {
-            item->setSelected(true);
+        const QList<hKoRdfSemanticItem> contactSemanticItems = rdf->semanticItems(semanticClass, model);
+        foreach (hKoRdfSemanticItem semanticItem, contactSemanticItems) {
+            KoRdfSemanticTreeWidgetItem *item = semanticItem->createQTreeWidgetItem(treeWidgetItem);
+            if (selectedItems.contains(item->semanticItem()->name())) {
+                item->setSelected(true);
+            }
         }
+        m_tree->expandItem(treeWidgetItem);
     }
-    m_tree->expandItem(m_eventsItem);
-
-    // locations
-    if (model) {
-        //
-        // grab the lat/long triples from m_model into the passed model.
-        //
-        // geo84
-        // <uri:dan84> <http://xmlns.com/foaf/0.1/based_near> _:genid1
-        // _:genid1 <http://www.w3.org/2003/01/geo/wgs84_pos#lat> "51.47026" (empty)
-        // _:genid1 <http://www.w3.org/2003/01/geo/wgs84_pos#long> "-2.59466" (empty)
-        rdf->expandStatementsSubjectPointsTo(model);
-        //kDebug(30015) << "expanding lists... old.sz:" << model->statementCount();
-        // other geo is an rdf:list, so bring that in too
-        rdf->expandStatementsToIncludeRdfLists(model);
-        //kDebug(30015) << "expanding lists... new.sz:" << model->statementCount();
-    }
-    const QList<hKoRdfSemanticItem> locationSemanticItems = rdf->semanticItems("Location", model);
-    foreach (hKoRdfSemanticItem semanticItem, locationSemanticItems) {
-        KoRdfSemanticTreeWidgetItem *item = semanticItem->createQTreeWidgetItem(m_locationsItem);
-        if (selectedLocations.contains(item->semanticItem()->name())) {
-            item->setSelected(true);
-        }
-    }
-    m_tree->expandItem(m_locationsItem);
 }
 
 void KoRdfSemanticTree::update(KoDocumentRdf *rdf, QSharedPointer<Soprano::Model> model)
