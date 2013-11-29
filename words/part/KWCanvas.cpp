d42ff971f851dac96367bae8a0bbf45ec960056e
Inge Wallin <inge@lysator.liu.se>
2013-11-30 00:36:03 +0100

    Merge the text-annotation branch.
    This was ok'ed by Boemann.
    
    REVIEW: 111914
    FEATURE: Add support for annotations to Words and Author
    
    Squashed commit of the following:
    
    commit 60814211de89ee9d7a9fca8b2d95c4bb28d009ba
    Merge: 2eeedc8 b25faa3
    Author: Inge Wallin <inge@lysator.liu.se>
    Date:   Sat Feb 16 10:24:45 2013 +0100
    
        Merge branch 'master' into text-annotation
    
        Conflicts:
        	libs/widgets/KoBalloon.h
        	libs/widgets/KoOpacityConfigWidget.h
        	plugins/textshape/AnnotationTextShapeFactory.h
        	plugins/textshape/TextDocumentInspectionPlugin.cpp
        	plugins/textshape/TextDocumentInspectionPlugin.h
        	words/part/KWCanvas.cpp
    
    commit 2eeedc8451cdd71733388bab948cdae320454ca2
    Author: Inge Wallin <inge@lysator.liu.se>
    Date:   Fri Feb 15 19:24:23 2013 +0100
    
        Fix broken build in a hacky way.
    
        This breakage was introduced by my previous patch.  Boemann says that
        she has a better way of doing this anyway so we will just comment the
        offending line away for now.
    
    commit c965e855efb13716fc39c276a2a61b46cbce991c
    Author: Inge Wallin <inge@lysator.liu.se>
    Date:   Fri Feb 15 18:48:02 2013 +0100
    
        Remove a line on advice (more like an order :)) from boemann.
    
    commit 13eeae1f47c0a0e4fcb958c2514d9d8177b39f88
    Author: Inge Wallin <inge@lysator.liu.se>
    Date:   Fri Feb 15 18:29:55 2013 +0100
    
        Some small cleanups, FIXME's and notes for later work.
    
    commit c8becb5d98f2799ccaa68426a8d1a913be06c729
    Author: Inge Wallin <inge@lysator.liu.se>
    Date:   Mon Jan 28 21:35:46 2013 +0100
    
        Make the annotation area work with zoom settings Fit to Page Width
    
    commit 9114e642206208eb95db10d1f74832ef2e1c9ce0
    Author: Inge Wallin <inge@lysator.liu.se>
    Date:   Mon Jan 28 19:58:50 2013 +0100
    
        Make clear that there is a temporary hack in the code that has to be fixed.
    
    commit fa867b2bafa2f30615b6d841e918b54faf233d80
    Author: Mojtaba Shahi Senobari <mojtaba.shahi3000@gmail.com>
    Date:   Thu Jan 24 20:58:57 2013 +0330
    
        Reintroduce the red "annotation area" per boemann's plan.
    
    commit c8b39525e57b4ff26814bd180ec4e0372472effe
    Author: Mojtaba Shahi Senobari <mojtaba.shahi3000@gmail.com>
    Date:   Sun Jan 27 22:57:22 2013 +0330
    
        Implement Annotation::saveOdf, call it from KoAnnotation  and tweak words to doesen't KoShape::saveOdf.
    
    commit 80e04c49b81ae0b24ac4e5bb5a5ceb67d667f60d
    Author: Mojtaba Shahi Senobari <mojtaba.shahi3000@gmail.com>
    Date:   Sun Jan 27 12:57:24 2013 +0330
    
        Load annotation at a shape and show it in document.
    
    commit e346a0277d2815a9fce637eae51417b0f774d636
    Author: Mojtaba Shahi Senobari <mojtaba.shahi3000@gmail.com>
    Date:   Sat Jan 26 19:22:45 2013 +0330
    
        Add an annotation shape with AnnotationTextShape. you can edit text set bold, italic and also have a list.
    
    commit d8753f798c4cf507750b90e0a161313ad48e9cc4
    Author: Mojtaba Shahi Senobari <mojtaba.shahi3000@gmail.com>
    Date:   Sat Jan 26 15:43:42 2013 +0330
    
        Remove all old ui for annotation.

--- words/part/KWCanvas.cpp
+++ words/part/KWCanvas.cpp
@@ -21,6 +21,7 @@
  */
 
 // words includes
+#define  SHOW_ANNOTATIONS 1
 #include "KWCanvas.h"
 #include "KWGui.h"
 #include "KWView.h"
@@ -68,7 +69,7 @@ void KWCanvas::pageSetupChanged()
 void KWCanvas::updateSize()
 {
     resourceManager()->setResource(Words::CurrentPageCount, m_document->pageCount());
-    emit documentSize(m_viewMode->contentsSize());
+    emit documentSize(m_viewMode->contentsSize() + QSize(KWView::AnnotationAreaWidth, 0.0));
 }
 
 void KWCanvas::setDocumentOffset(const QPoint &offset)
@@ -183,7 +184,15 @@ void KWCanvas::paintEvent(QPaintEvent *ev)
 {
     QPainter painter(this);
     painter.eraseRect(ev->rect());
+#ifdef SHOW_ANNOTATIONS
+    QColor color = Qt::red;
+    QRect annotationRect(m_viewMode->contentsSize().width(), 0,
+                         KWView::AnnotationAreaWidth, m_viewMode->contentsSize().height());
+    qDebug()<<"annotation rect "<< annotationRect;
+    painter.fillRect(m_viewMode->documentToView(annotationRect, m_viewConverter), QBrush(color));
+#endif
     paint(painter, ev->rect());
+
     painter.end();
 }
 
