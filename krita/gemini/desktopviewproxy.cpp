947be21cccca5da491249409b0cfa7237c63822d
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-03-26 16:21:44 +0100

    gemini: Start a new instance when opening a file
    This prevents issues where we would get an empty screen on opening an
    image and now more closely matches the new image behaviour.

--- krita/gemini/desktopviewproxy.cpp
+++ krita/gemini/desktopviewproxy.cpp
@@ -120,7 +120,11 @@ void DesktopViewProxy::fileOpen()
                                                            "OpenDocument");
     if (filename.isEmpty()) return;
 
-    DocumentManager::instance()->openDocument(filename, d->isImporting);
+    if(!d->mainWindow->slateMode()) {
+        QProcess::startDetached(qApp->applicationFilePath(), QStringList() << filename);
+    } else {
+        DocumentManager::instance()->openDocument(filename, d->isImporting);
+    }
 }
 
 void DesktopViewProxy::fileSave()
