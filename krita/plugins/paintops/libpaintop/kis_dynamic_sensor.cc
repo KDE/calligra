5522e745bcd1707814637241275a823f1a6bdf9d
Boudewijn Rempt <boud@valdyas.org>
2014-03-31 22:22:19 +0200

    BUG:331592 Fix memory leak when creating strokes
    Use KisShared to handle memory management of the sensor objects that
    get created when making a stroke.

--- krita/plugins/paintops/libpaintop/kis_dynamic_sensor.cc
+++ krita/plugins/paintops/libpaintop/kis_dynamic_sensor.cc
@@ -26,8 +26,6 @@
 #include "sensors/kis_dynamic_sensor_fade.h"
 #include "sensors/kis_dynamic_sensor_fuzzy.h"
 
-static int sensorcount = 0;
-
 KisDynamicSensor::KisDynamicSensor(const KoID& id)
     : m_id(id)
     , m_customCurve(false)
