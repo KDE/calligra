17747f2ee4d1d832cbe60793db50b5bcf272d46a
Dan Leinir Turthra Jensen <admin@leinir.dk>
2014-03-31 15:56:26 +0100

    Don't forget to add the sketch resource dir to gemini as well...

--- krita/gemini/main.cpp
+++ krita/gemini/main.cpp
@@ -105,6 +105,7 @@ int main( int argc, char** argv )
     KApplication app;
     app.setApplicationName("kritagemini");
     KIconLoader::global()->addAppDir("krita");
+    KIconLoader::global()->addAppDir("kritasketch");
 
 #ifdef Q_OS_WIN
     QDir appdir(app.applicationDirPath());
