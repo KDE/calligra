7c37f4dea300c591c0d199444b655bd8ea41f0eb
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-04-11 15:40:32 +0200

    gemini: Use the right working dir for new instances.

--- krita/gemini/desktopviewproxy.cpp
+++ krita/gemini/desktopviewproxy.cpp
@@ -122,7 +122,7 @@ void DesktopViewProxy::fileOpen()
 
     DocumentManager::instance()->recentFileManager()->addRecent(filename);
 
-    QProcess::startDetached(qApp->applicationFilePath(), QStringList() << filename);
+    QProcess::startDetached(qApp->applicationFilePath(), QStringList() << filename, QDir::currentPath());
 }
 
 void DesktopViewProxy::fileSave()
@@ -162,7 +162,7 @@ void DesktopViewProxy::loadExistingAsNew()
 
 void DesktopViewProxy::slotFileOpenRecent(const KUrl& url)
 {
-    QProcess::startDetached(qApp->applicationFilePath(), QStringList() << url.toLocalFile());
+    QProcess::startDetached(qApp->applicationFilePath(), QStringList() << url.toLocalFile(), QDir::currentPath());
 }
 
 #include "desktopviewproxy.moc"
