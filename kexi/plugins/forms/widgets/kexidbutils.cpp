KDE/calligra

commit 900640e6df1f25583f562c5a6bedf9ba9968d762

add d-pointers (part #1 of GCI)
(miration drivers ported by jstaniek)

REVIEW:107610

kexi/plugins/forms/widgets/kexidbutils.cpp
@@ -37,6 +37,24 @@
 #include "../kexiformmanager.h"
 #include <widget/utils/kexicontextmenuutils.h>
 
+
+class KexiDBWidgetContextMenuExtender::Private
+{
+public:
+    Private(KexiDataItemInterface* iface_)
+      : iface(iface_)
+      , contextMenuHasTitle(false)
+    {
+    }
+
+    KexiDataItemInterface* iface;
+    QPointer<QMenu> contextMenu;
+    QPointer<QAction> titleAction;
+    bool contextMenuHasTitle; //!< true if KPopupTitle has been added to the context menu.
+
+};
+
+
 //! Static data for kexi forms
 struct KexiFormStatics
 {
@@ -92,8 +110,7 @@ QPixmap KexiFormUtils::dataSourceRTLTagIcon()
 
 KexiDBWidgetContextMenuExtender::KexiDBWidgetContextMenuExtender(QObject* parent, KexiDataItemInterface* iface)
         : QObject(parent)
-        , m_iface(iface)
-        , m_contextMenuHasTitle(false)
+	, d(new Private(iface))
 {
 }
 
@@ -117,15 +134,15 @@ void KexiDBWidgetContextMenuExtender::createTitle(KMenu *menu)
         return;
 
     QString icon;
-    if (dynamic_cast<QWidget*>(m_iface)) {
+    if (dynamic_cast<QWidget*>(d->iface)) {
         icon = KexiFormManager::self()->library()->iconName(
-                   dynamic_cast<QWidget*>(m_iface)->metaObject()->className());
+                   dynamic_cast<QWidget*>(d->iface)->metaObject()->className());
     }
-    m_contextMenuHasTitle = m_iface->columnInfo() ?
+    d->contextMenuHasTitle = d->iface->columnInfo() ?
         KexiContextMenuUtils::updateTitle(
             menu,
-            m_iface->columnInfo()->captionOrAliasOrName(),
-            KexiDB::simplifiedTypeName(*m_iface->columnInfo()->field), icon)
+            d->iface->columnInfo()->captionOrAliasOrName(),
+            KexiDB::simplifiedTypeName(*d->iface->columnInfo()->field), icon)
         : false;
 
     updatePopupMenuActions(menu);
@@ -133,7 +150,7 @@ void KexiDBWidgetContextMenuExtender::createTitle(KMenu *menu)
 
 void KexiDBWidgetContextMenuExtender::updatePopupMenuActions(QMenu *menu)
 {
-    const bool readOnly = m_iface->isReadOnly();
+    const bool readOnly = d->iface->isReadOnly();
 
     foreach(QAction* action, menu->actions()) {
         const QString text(action->text());

kexi/migration/importwizard.h
@@ -122,43 +122,8 @@ private:
     //! @return source filename selected by user or preselected one (if present)
     QString selectedSourceFileName() const;
 
-    QWidget *m_introPageWidget, *m_srcConnPageWidget, *m_srcDBPageWidget,
-    *m_dstTypePageWidget, *m_dstPageWidget, *m_importTypePageWidget, *m_importingPageWidget, *m_finishPageWidget;
-
-    KPageWidgetItem *m_introPageItem, *m_srcConnPageItem, *m_srcDBPageItem, *m_dstTypePageItem,
-        *m_dstPageItem, *m_importTypePageItem, *m_importingPageItem, *m_finishPageItem;
-
-        
-    QGroupBox *m_importTypeGroupBox;
-    QRadioButton *m_importTypeStructureAndDataCheckBox;
-    QRadioButton *m_importTypeStructureOnlyCheckBox;
-    KexiDBTitlePage* m_dstTitlePageWidget;
-    KPageWidgetItem *m_dstTitlePageItem;
-
-    KComboBox *m_srcTypeCombo;
-    KexiDBDriverComboBox *m_dstServerTypeCombo;
-    KexiPrjTypeSelector *m_dstPrjTypeSelector;
-
-    KexiConnectionSelectorWidget *m_srcConn, *m_dstConn;
-    KLineEdit *m_dstNewDBNameLineEdit;
-    KexiProjectSelectorWidget *m_srcDBName;
-
-    QLabel *m_lblImportingTxt, *m_lblImportingErrTxt, *m_finishLbl;
-    QCheckBox *m_openImportedProjectCheckBox;
-    bool m_fileBasedDstWasPresented : 1;
-    bool m_setupFileBasedSrcNeeded : 1;
-    bool m_importExecuted  :1; //!< used in import()
-    KexiProjectSet* m_prjSet;
-    QProgressBar *m_progressBar;
-    KPushButton* m_importOptionsButton;
-    QMap<QString, QString> *m_args;
-    QString m_predefinedDatabaseName, m_predefinedMimeType;
-    KexiDB::ConnectionData *m_predefinedConnectionData;
-    MigrateManager m_migrateManager; //!< object lives here, so status messages can be globally preserved
-
-    //! Encoding for source db. Currently only used for MDB driver.
-//! @todo Hardcoded. Move to KexiMigrate driver's impl.
-    QString m_sourceDBEncoding;
+    class Private;
+    Private * const d;
 };
 
 }

kexi/migration/keximigrate.h
@@ -90,9 +90,7 @@ public:
     virtual ~KexiMigrate();
 
 //! @todo Remove this! KexiMigrate should be usable for multiple concurrent migrations!
-    KexiMigration::Data* data() const {
-        return m_migrateData;
-    }
+    KexiMigration::Data* data();
 
 //! @todo Remove this! KexiMigrate should be usable for multiple concurrent migrations!
     //! Data Setup.  Requires two connection objects, a name and a bool
@@ -119,7 +117,7 @@ public:
 
     //! Perform an export operation
     bool performExport(Kexi::ObjectStatus* result = 0);
-    
+
     //! Returns true if the migration driver supports progress updates.
     inline bool progressSupported() {
         return drv_progressSupported();
@@ -131,17 +129,21 @@ public:
 #endif
 
 //! @todo This is copied from KexiDB::Driver. One day it will be merged with KexiDB.
-    //! \return property value for \a propeName available for this driver.
+    //! \return property value for \a propertyName available for this driver.
     //! If there's no such property defined for driver, Null QVariant value is returned.
-    virtual QVariant propertyValue(const QByteArray& propName);
+    virtual QVariant propertyValue(const QByteArray& propertyName);
 
 //! @todo This is copied from KexiDB::Driver. One day it will be merged with KexiDB.
-    void setPropertyValue(const QByteArray& propName, const QVariant& value);
+    void setPropertyValue(const QByteArray& propertyName, const QVariant& value);
 
 //! @todo This is copied from KexiDB::Driver. One day it will be merged with KexiDB.
-    //! \return translated property caption for \a propeName.
+    //! \return translated property caption for \a propertyName.
     //! If there's no such property defined for driver, empty string value is returned.
-    QString propertyCaption(const QByteArray& propName) const;
+    QString propertyCaption(const QByteArray& propertyName) const;
+
+    //! @todo This is copied from KexiDB::Driver. One day it will be merged with KexiDB.
+    //! Set translated property caption for \a propertyName.
+    void setPropertyCaption(const QByteArray& propertyName, const QString &caption);
 
 //! @todo This is copied from KexiDB::Driver. One day it will be merged with KexiDB.
     //! \return a list of property names available for this driver.
@@ -152,32 +154,32 @@ public:
 //     and KexiMigrate::versionMinor() are matching.
 //     You can reimplement this but always call KexiMigrate::isValid() implementation. */
 //    virtual bool isValid();
-    
+
     //Extension of existing api to provide generic row access to external data.
     //!Connect to the source data
     bool connectSource();
-    
+
     //! Get table names in source database (driver specific)
     bool tableNames(QStringList& tablenames);
 
     //! Read schema for a given table (driver specific)
     bool readTableSchema(const QString& originalName, KexiDB::TableSchema& tableSchema);
-    
-    //!Position the source dataset at the start of a table    
+
+    //!Position the source dataset at the start of a table
     bool readFromTable(const QString& tableName);
-    
+
     //!Move to the next row
     bool moveNext();
-    
+
     //!Move to the previous row
     bool movePrevious();
-    
+
     //!Move to the next row
     bool moveFirst();
-    
+
     //!Move to the previous row
     bool moveLast();
-    
+
     //!Read the data at the given row/field
     QVariant value(uint i);
 
@@ -246,7 +248,8 @@ protected:
      \a data is resized to appropriate size. cancelled is returned on EOF. */
 //! @todo SQL-dependent!
     virtual tristate drv_fetchRecordFromSQL(const QString& sqlStatement,
-                                            KexiDB::RecordData& data, bool &firstRecord) {
+                                            KexiDB::RecordData& data,
+					    bool &firstRecord) {
         Q_UNUSED(sqlStatement); Q_UNUSED(data); Q_UNUSED(firstRecord);
         return cancelled;
     }
@@ -288,58 +291,40 @@ protected:
     //! Prompt user to select a field type for unrecognized fields
     KexiDB::Field::Type userType(const QString& fname);
 
-    virtual QString drv_escapeIdentifier(const QString& str) const {
-        return m_kexiDBDriver ? m_kexiDBDriver->escapeIdentifier(str) : str;
-    }
+    virtual QString drv_escapeIdentifier(const QString& str) const;
 
-//! @todo Remove this! KexiMigrate should be usable for multiple concurrent migrations!
-    //! Migrate Options
-    KexiMigration::Data* m_migrateData;
 
-//   // Temporary values used during import (set by driver specific methods)
-//   KexiDB::Field* m_f;
-
-    /*! Driver properties dictionary (indexed by name),
-     useful for presenting properties to the user.
-     Set available properties here in driver implementation. */
-    QMap<QByteArray, QVariant> m_properties;
-
-    /*! i18n'd captions for properties. You do not need
-     to set predefined properties' caption in driver implementation
-     -it's done automatically. */
-    QMap<QByteArray, QString> m_propertyCaptions;
-
-    //! KexiDB driver. For instance, it is used for escaping identifiers
-    QPointer<KexiDB::Driver> m_kexiDBDriver;
-    
     //Extended API
     //! Position the source dataset at the start of a table
-    virtual bool drv_readFromTable(const QString & tableName) { Q_UNUSED(tableName); return false; }
-    
+    virtual bool drv_readFromTable(const QString & tableName) {
+      Q_UNUSED(tableName);
+      return false;
+    }
+
     //! Move to the next row
     virtual bool drv_moveNext() { return false; }
-    
+
     //! Move to the previous row
     virtual bool drv_movePrevious() { return false; }
-    
+
     //! Move to the next row
     virtual bool drv_moveFirst() { return false; }
-    
+
     //! Move to the previous row
     virtual bool drv_moveLast() { return false; }
-    
+
     //! Read the data at the given row/field
     virtual QVariant drv_value(uint i) { Q_UNUSED(i); return QVariant(); };
-    
+
+    //! @return Database driver for this migration.
+    KexiDB::Driver *driver();
+
+    //! Sets database driver for this migration.
+    void setDriver(KexiDB::Driver *driver);
+
 private:
-
     //! Create the target database project
-//  KexiProject* createProject(Kexi::ObjectStatus* result);
-
-    //! Table schemas from source DB
-    QList<KexiDB::TableSchema*> m_tableSchemas;
-
-    QList<KexiDB::TableSchema*> m_kexiDBCompatibleTableSchemasToRemoveFromMemoryAfterImport;
+    //  KexiProject* createProject(Kexi::ObjectStatus* result);
 
     /*! Estimate size of migration job
      Calls drv_getTableSize for each table to be copied.
@@ -347,16 +332,8 @@ private:
     */
     bool progressInitialise();
 
-    KexiProject *m_destPrj;
-
-    //! Size of migration job
-    quint64 m_progressTotal;
-
-    //! Amount of migration job complete
-    quint64 m_progressDone;
-
-    //! Don't recalculate progress done until this value is reached.
-    quint64 m_progressNextReport;
+    class Private;
+    Private * const d;
 
     friend class MigrateManager;
 };

kexi/migration/mdb/src/keximdb/mdbmigrate.cpp
@@ -43,12 +43,10 @@ MDBMigrate::MDBMigrate(QObject *parent, const QVariantList &args)
         : KexiMigrate(parent, args)
 {
     /*! @todo invert the sense of values, then remove "Non-" from these strings */
-    m_properties[ isNonUnicodePropId ] = QVariant(true);
-    m_propertyCaptions[ isNonUnicodePropId ] =
-        i18n("Source Database Has Non-Unicode Encoding");
-    m_properties[ nonUnicodePropId ] = QVariant(QString());
-    m_propertyCaptions[ nonUnicodePropId ]
-    = i18n("Source Database Non-Unicode Encoding");
+    setPropertyValue(isNonUnicodePropId, true);
+    setPropertyCaption(isNonUnicodePropId, i18n("Source Database Has Non-Unicode Encoding"));
+    setPropertyValue(nonUnicodePropId, QString());
+    setPropertyCaption(nonUnicodePropId, i18n("Source Database Non-Unicode Encoding"));
 
     initBackend();
 }
@@ -77,7 +75,7 @@ void MDBMigrate::releaseBackend()
 QVariant MDBMigrate::propertyValue(const QByteArray& propName)
 {
     if (propName == isNonUnicodePropId) {
-        m_properties[ isNonUnicodePropId ] = false;
+        setPropertyValue(isNonUnicodePropId, false);
 
         // Costly, but we need this to get this property from file...
         drv_connect();
@@ -90,7 +88,7 @@ QVariant MDBMigrate::propertyValue(const QByteArray& propName)
 bool MDBMigrate::drv_connect()
 {
     kDebug() << "mdb_open:";
-    KexiDB::ConnectionData *data = m_migrateData->source;
+    KexiDB::ConnectionData *data = this->data()->source;
 
     // mdb_open takes a char*, not const char*, hence this nonsense.
     char *filename = qstrdup(QFile::encodeName(data->fileName()));
@@ -103,8 +101,8 @@ bool MDBMigrate::drv_connect()
     }
 
     // Setting source encoding
-    if (!m_properties[ nonUnicodePropId ].toString().isEmpty()) {
-        const QByteArray encoding(m_properties[ nonUnicodePropId ].toByteArray());
+    if (!propertyValue(nonUnicodePropId).toString().isEmpty()) {
+        const QByteArray encoding(propertyValue(nonUnicodePropId).toByteArray());
 
         mdb_set_encoding(m_mdb, encoding.constData());
         kDebug() << "non-unicode encoding set to \""
@@ -113,7 +111,7 @@ bool MDBMigrate::drv_connect()
     }
 
     // Supports setting source encoding
-    m_properties[ isNonUnicodePropId ] = QVariant((bool)IS_JET3(m_mdb));
+    setPropertyValue(isNonUnicodePropId, bool(IS_JET3(m_mdb)));
 
     return true;
 }

kexi/migration/mysql/mysqlmigrate.cpp
@@ -74,7 +74,7 @@ MySQLMigrate::MySQLMigrate(QObject *parent, const QVariantList& args) :
         , m_dataRow(0)
 {
     KexiDB::DriverManager manager;
-    m_kexiDBDriver = manager.driver("mysql");
+    setDriver(manager.driver("mysql"));
 }
 
 /* ************************************************************************** */
@@ -90,9 +90,9 @@ MySQLMigrate::~MySQLMigrate()
 /*! Connect to the db backend */
 bool MySQLMigrate::drv_connect()
 {
-    if (!d->db_connect(*m_migrateData->source))
+    if (!d->db_connect(*data()->source))
         return false;
-    return d->useDatabase(m_migrateData->sourceName);
+    return d->useDatabase(data()->sourceName);
 }
 
 

kexi/migration/pqxx/pqxxmigrate.cpp
@@ -67,7 +67,7 @@ PqxxMigrate::PqxxMigrate(QObject *parent, const QVariantList& args)
     m_row = 0;
     
     KexiDB::DriverManager manager;
-    m_kexiDBDriver = manager.driver("pqxx");
+    setDriver(manager.driver("pqxx"));
 }
 //==================================================================================
 //Destructor
@@ -184,34 +184,34 @@ KexiDB::Field::Type PqxxMigrate::type(int t, const QString& fname)
 //Connect to the db backend
 bool PqxxMigrate::drv_connect()
 {
-    kDebug() << "drv_connect: " << m_migrateData->sourceName;
+    kDebug() << "drv_connect: " << data()->sourceName;
 
     QString conninfo;
     QString socket;
 
     //Setup local/remote connection
-    if (m_migrateData->source->hostName.isEmpty()) {
-        if (m_migrateData->source->fileName().isEmpty()) {
+    if (data()->source->hostName.isEmpty()) {
+        if (data()->source->fileName().isEmpty()) {
             socket = "/tmp/.s.PGSQL.5432";
         } else {
-            socket = m_migrateData->source->fileName();
+            socket = data()->source->fileName();
         }
     } else {
-        conninfo = "host='" + m_migrateData->source->hostName + '\'';
+        conninfo = "host='" + data()->source->hostName + '\'';
     }
 
     //Build up the connection string
-    if (m_migrateData->source->port == 0)
-        m_migrateData->source->port = 5432;
+    if (data()->source->port == 0)
+        data()->source->port = 5432;
 
-    conninfo += QString::fromLatin1(" port='%1'").arg(m_migrateData->source->port);
-    conninfo += QString::fromLatin1(" dbname='%1'").arg(m_migrateData->sourceName);
+    conninfo += QString::fromLatin1(" port='%1'").arg(data()->source->port);
+    conninfo += QString::fromLatin1(" dbname='%1'").arg(data()->sourceName);
 
-    if (!m_migrateData->source->userName.isEmpty())
-        conninfo += QString::fromLatin1(" user='%1'").arg(m_migrateData->source->userName);
+    if (!data()->source->userName.isEmpty())
+        conninfo += QString::fromLatin1(" user='%1'").arg(data()->source->userName);
 
-    if (!m_migrateData->source->password.isEmpty())
-        conninfo += QString::fromLatin1(" password='%1'").arg(m_migrateData->source->password);
+    if (!data()->source->password.isEmpty())
+        conninfo += QString::fromLatin1(" password='%1'").arg(data()->source->password);
 
     try {
         m_conn = new pqxx::connection(conninfo.toLatin1().constData());

commit 3d10179551637223c6c1e7fe0787e0cb86a84c12

fix memory leak introduced in 900640e6df1f25583
GIT_SILENT

kexi/plugins/forms/widgets/kexidbutils.cpp
@@ -116,6 +116,7 @@ KexiDBWidgetContextMenuExtender::KexiDBWidgetContextMenuExtender(QObject* parent
 
 KexiDBWidgetContextMenuExtender::~KexiDBWidgetContextMenuExtender()
 {
+    delete d;
 }
 
 void KexiDBWidgetContextMenuExtender::exec(QMenu *menu, const QPoint &globalPos)