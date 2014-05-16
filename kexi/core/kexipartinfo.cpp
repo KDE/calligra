KDE/calligra

commit decd43c010eb6ee69bbe4e4629d56b373588e88a

Do not crash when Kexi is unable to find plugins; display message and exit
Also try to build sycoca

REVIEW:117759
FIXED-IN:2.9.0

kexi/core/kexipartinfo.cpp
@@ -1,6 +1,6 @@
 /* This file is part of the KDE project
    Copyright (C) 2003 Lucijan Busch <lucijan@kde.org>
-   Copyright (C) 2003-2011 Jarosław Staniek <staniek@kde.org>
+   Copyright (C) 2003-2014 Jarosław Staniek <staniek@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -23,6 +23,7 @@
 
 #include <db/global.h>
 #include <kactioncollection.h>
+#include <KDebug>
 
 using namespace KexiPart;
 
@@ -110,15 +111,7 @@ void KexiNewObjectAction::slotTriggered()
 Info::Info(KService::Ptr ptr)
         : d(new Private(ptr))
 {
-    if (KexiMainWindowIface::global()) {
-        KexiNewObjectAction *act = new KexiNewObjectAction(
-            this,
-            KexiMainWindowIface::global()->actionCollection());
-        
-        if (KexiMainWindowIface::global()->actionCollection()) {
-            KexiMainWindowIface::global()->actionCollection()->addAction(act->objectName(), act);
-        }
-    }
+
 }
 
 Info::Info(const QString &partClass, const QString &itemIconName,
@@ -231,6 +224,20 @@ bool Info::isExecuteSupported() const
 bool Info::isPropertyEditorAlwaysVisibleInDesignMode() const
 {
     return d->isPropertyEditorAlwaysVisibleInDesignMode;
+}
+
+QAction* Info::newObjectAction()
+{
+    if (!KexiMainWindowIface::global() || !KexiMainWindowIface::global()->actionCollection()) {
+        kWarning() << "!KexiMainWindowIface::global()";
+        return 0;
+    }
+    QAction *act = KexiMainWindowIface::global()->actionCollection()->action(KexiPart::nameForCreateAction(*this));
+    if (!act) {
+        KexiNewObjectAction *act = new KexiNewObjectAction(this, KexiMainWindowIface::global()->actionCollection());
+        KexiMainWindowIface::global()->actionCollection()->addAction(act->objectName(), act);
+    }
+    return act;
 }
 
 //--------------

kexi/core/kexipartinfo.h
@@ -1,6 +1,6 @@
 /* This file is part of the KDE project
    Copyright (C) 2003 Lucijan Busch <lucijan@kde.org>
-   Copyright (C) 2003-2011 Jarosław Staniek <staniek@kde.org>
+   Copyright (C) 2003-2014 Jarosław Staniek <staniek@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -24,6 +24,7 @@
 #include "kexi.h"
 #include "kexipartmanager.h"
 
+class QAction;
 class KexiProject;
 class KexiWindow;
 
@@ -130,6 +131,11 @@ public:
      * False by default. It is set to true e.g. for table part.
      */
     bool isPropertyEditorAlwaysVisibleInDesignMode() const;
+
+    /**
+     * \return "New object" action for this part.
+     */
+    QAction* newObjectAction();
 
 protected:
     /**

kexi/core/kexipart.cpp
@@ -137,8 +137,7 @@ void Part::createGUIClients()//KexiMainWindow *win)
         d->guiClient = new GUIClient(this, false, "part");
 
         //default actions for part's gui client:
-        QAction* act = KexiMainWindowIface::global()->actionCollection()->action(
-                            KexiPart::nameForCreateAction(*info()));
+        QAction* act = info()->newObjectAction();
         // - update action's tooltip and "what's this"
         QString tip(toolTip());
         if (!tip.isEmpty()) {

kexi/core/kexipartmanager.cpp
@@ -1,6 +1,6 @@
 /* This file is part of the KDE project
    Copyright (C) 2003 Lucijan Busch <lucijan@kde.org>
-   Copyright (C) 2003-2009 Jarosław Staniek <staniek@kde.org>
+   Copyright (C) 2003-2014 Jarosław Staniek <staniek@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -74,6 +74,12 @@ Manager::~Manager()
     delete d;
 }
 
+static QString appIncorrectlyInstalledMessage()
+{
+    return i18nc("@info", "<application>%1</application> could have been incorrectly installed or started. The application will be closed.",
+                 KGlobal::mainComponent().aboutData()->programName());
+}
+
 bool Manager::lookup()
 {
 //! @todo Allow refreshing!!!! (will need calling removeClient() by Part objects)
@@ -87,9 +93,9 @@ bool Manager::lookup()
 
     if (!KServiceType::serviceType("Kexi/Handler")) {
         kWarning() << "No 'Kexi/Handler' service type installed! Aborting.";
-        setError(i18nc("@info", "No <resource>%1</resource> service type installed. "
-                       "Check your <application>%2</application> installation. The application will be closed.",
-                       QLatin1String("Kexi/Handler"), KGlobal::mainComponent().aboutData()->programName()));
+        m_serverErrorMsg = i18nc("@info", "No <resource>%1</resource> service type installed.",
+                                 QLatin1String("Kexi/Handler"));
+        setError(appIncorrectlyInstalledMessage());
         return false;
     }
     KService::List tlist = KServiceTypeTrader::self()->query("Kexi/Handler",
@@ -97,10 +103,10 @@ bool Manager::lookup()
 
     KConfigGroup cg(KGlobal::config()->group("Parts"));
     if (qApp && !cg.hasKey("Order")) {
-        setError(i18nc("@info",
-                       "Missing or invalid default <application>%1</application> configuration (no <resource>%2</resource> key). "
-                       "Check your <application>%1</application> installation. The application will be closed.",
-                       KGlobal::mainComponent().aboutData()->programName(), QLatin1String("Parts/Order")));
+        m_serverErrorMsg = i18nc("@info",
+                                 "Missing or invalid default application configuration. No <resource>%1</resource> key.",
+                                 QLatin1String("Parts/Order"));
+        setError(appIncorrectlyInstalledMessage());
         return false;
     }
     const QStringList sl_order = cg.readEntry("Order").split(',');  //we'll set parts in defined order

kexi/main/KexiMainWindow.cpp
@@ -1390,7 +1390,7 @@ void KexiMainWindow::updateReadOnlyState()
     KexiPart::PartInfoList *plist = Kexi::partManager().infoList();
     if (plist) {
         foreach(KexiPart::Info *info, *plist) {
-            QAction *a = ac->action(KexiPart::nameForCreateAction(*info));
+            QAction *a = info->newObjectAction();
             if (a)
                 a->setEnabled(!readOnly);
         }

kexi/main/startup/KexiStartup.cpp
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 2003-2013 Jarosław Staniek <staniek@kde.org>
+   Copyright (C) 2003-2014 Jarosław Staniek <staniek@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -19,6 +19,7 @@
 
 #include "KexiStartup.h"
 
+#include "kexi.h"
 #include "kexiproject.h"
 #include "kexiprojectdata.h"
 #include "kexiprojectset.h"
@@ -28,6 +29,7 @@
 #include <db/utils.h>
 #include <db/driver.h>
 #include <db/drivermanager.h>
+#include <core/kexipartmanager.h>
 #include <widget/KexiConnectionSelectorWidget.h>
 #include <widget/KexiProjectSelectorWidget.h>
 #include <kexidbconnectionwidget.h>
@@ -44,6 +46,10 @@
 #include <ktextedit.h>
 #include <kuser.h>
 #include <KProgressDialog>
+#include <KBuildSycocaProgressDialog>
+#include <KProcess>
+#include <KStandardDirs>
+#include <KSycoca>
 
 #include <unistd.h>
 
@@ -692,6 +698,12 @@ tristate KexiStartupHandler::init(int /*argc*/, char ** /*argv*/)
 
     //------
 
+    KexiPart::PartInfoList *partInfoList = Kexi::partManager().infoList();
+    if (!partInfoList || partInfoList->isEmpty()) {
+        showNoPluginsMessageAndTryToRebuildSyCoCa();
+        return false;
+    }
+
     if (!KexiStartupData::projectData()) {
         cdata = KexiDB::ConnectionData(); //clear
 
@@ -1043,6 +1055,17 @@ void KexiStartupHandler::slotSaveShortcutFileChanges()
         KMessageBox::sorry(0, i18n("Failed saving connection data to\n\"%1\" file.",
                            QDir::convertSeparators(fileName)));
     }
+}
+
+void KexiStartupHandler::showNoPluginsMessageAndTryToRebuildSyCoCa()
+{
+    KexiGUIMessageHandler msgh;
+    msgh.showErrorMessage(&Kexi::partManager());
+    KexiStartupData::setProjectData(0);
+    KProcess* proc = new KProcess(&Kexi::partManager());
+    QObject::connect(proc, SIGNAL(finished()), proc, SLOT(deleteLater()));
+    (*proc) << KStandardDirs::findExe(KBUILDSYCOCA_EXENAME);
+    proc->start();
 }
 
 #include "KexiStartup.moc"

kexi/main/startup/KexiStartup.h
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 2003-2013 Jarosław Staniek <staniek@kde.org>
+   Copyright (C) 2003-2014 Jarosław Staniek <staniek@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -118,6 +118,9 @@ protected slots:
 
 protected:
     bool getAutoopenObjects(KCmdLineArgs *args, const QByteArray &action_name);
+
+    //! No plugins: show message and try to rebuild the system configuration
+    void showNoPluginsMessageAndTryToRebuildSyCoCa();
 
     class Private;
     Private * const d;

commit 07bb425715ad75f8e66aebd073df3eed26a27441

Properly populate Create toolbar with icons
This regression has been introduced by commit decd43c010eb6ee "Do not
crash when Kexi is unable to find plugins" (REVIEW:117759).
Look at the bug in Info::newObjectAction() and become fan of thorough
reviews :)

REVIEW:118027

kexi/core/kexipartinfo.cpp
@@ -86,11 +86,20 @@ Info::Private::Private()
 
 //------------------------------
 
+/*! \return "create" KAction's name for part defined by \a info.
+ The result is like "tablepart_create". */
+static QString nameForCreateAction(const Info& info)
+{
+    return info.objectName() + "part_create";
+}
+
+//------------------------------
+
 KexiNewObjectAction::KexiNewObjectAction(Info* info, QObject *parent)
     : KAction(KIcon(info->createItemIconName()), info->instanceCaption() + "...", parent)
     , m_info(info)
 {
-    setObjectName(KexiPart::nameForCreateAction(*m_info));
+    setObjectName(nameForCreateAction(*m_info));
     // default tooltip and what's this
     setToolTip(i18n("Create new object of type \"%1\"",
                 m_info->instanceCaption().toLower()));
@@ -232,19 +241,12 @@ QAction* Info::newObjectAction()
         kWarning();
         return 0;
     }
-    QAction *act = KexiMainWindowIface::global()->actionCollection()->action(KexiPart::nameForCreateAction(*this));
+    QAction *act = KexiMainWindowIface::global()->actionCollection()->action(nameForCreateAction(*this));
     if (!act) {
-        KexiNewObjectAction *act = new KexiNewObjectAction(this, KexiMainWindowIface::global()->actionCollection());
+        act = new KexiNewObjectAction(this, KexiMainWindowIface::global()->actionCollection());
         KexiMainWindowIface::global()->actionCollection()->addAction(act->objectName(), act);
     }
     return act;
-}
-
-//--------------
-
-QString KexiPart::nameForCreateAction(const Info& info)
-{
-    return info.objectName() + "part_create";
 }
 
 #include "kexipartinfo_p.moc"

kexi/core/kexipartinfo.h
@@ -177,11 +177,6 @@ private:
     Private * const d;
 };
 
-/*! \return "create" KAction's name for part defined by \a info.
- The result is like "tablepart_create". Used in Part::createGUIClients()
- and KexiBrowser. */
-KEXICORE_EXPORT QString nameForCreateAction(const Info& info);
-
 }
 
 #endif

kexi/main/KexiMainWindow_p.h
@@ -1229,8 +1229,7 @@ void KexiTabbedToolBar::setupCreateWidgetToolbar()
     KexiPart::PartInfoList *plist = Kexi::partManager().infoList(); //this list is properly sorted
     if (plist) {
         foreach(KexiPart::Info *info, *plist) {
-            QAction* a = d->ac->action(
-                            KexiPart::nameForCreateAction(*info));
+            QAction* a = info->newObjectAction();
             if (a) {
                 d->createWidgetToolBar->addAction(a);
             } else {