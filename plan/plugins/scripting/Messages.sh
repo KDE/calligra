52a9ed8b1b67ea84877f9ef7425ffb2b38b1c8cb
Alexander Potashev <aspotashev@gmail.com>
2014-06-01 21:35:52 +0400

    Remove commented out old code

--- plan/plugins/scripting/Messages.sh
+++ plan/plugins/scripting/Messages.sh
@@ -4,9 +4,6 @@ source ../../../calligra_xgettext.sh
 $EXTRACTRC *.ui *.rc >> rc.cpp || exit 11
 $EXTRACTATTR --attr=collection,text --attr=collection,comment --attr=script,text --attr=script,comment scripts/*.rc >> rc.cpp || exit 11
 
-#$XGETTEXT -kkundo2_i18nc:1c,2 -kkundo2_i18ncp:1c,2,3 --force-po *.cpp -o $podir/krossmoduleplan.pot
-#$XGETTEXT -kkundo2_i18nc:1c,2 -kkundo2_i18ncp:1c,2,3 --language=Python --join scripts/*.py -o $podir/krossmoduleplan.pot
-
 POT_CPP=$(mktemp $podir/_cpp_XXXXXXXX.pot)
 POT_PY=$(mktemp $podir/_py_XXXXXXXX.pot)
 calligra_xgettext *.cpp > "${POT_CPP}"
