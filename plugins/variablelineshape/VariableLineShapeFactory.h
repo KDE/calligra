ef309f4fafb8bf00fcf2185b9b67a823c0be4f2e
Wenchao Li <manchiu.lee.9@gmail.com>
2014-06-12 13:57:56 +0800

    On branch plugin-gsoc-variable-path-wenchao  Changes to be committed: 	modified:   plugins/CMakeLists.txt 	new file:   plugins/variablelineshape/CMakeLists.txt 	new file:   plugins/variablelineshape/VariableLineShape.cpp 	new file:   plugins/variablelineshape/VariableLineShape.h 	new file:   plugins/variablelineshape/VariableLineShapeFactory.cpp 	new file:   plugins/variablelineshape/VariableLineShapeFactory.h 	new file:   plugins/variablelineshape/VariableLineShapePlugin.cpp 	new file:   plugins/variablelineshape/VariableLineShapePlugin.h 	new file:   plugins/variablelineshape/calligra_shape_variableline.desktop 	new file:   plugins/variablelineshape/pics/CMakeLists.txt 	new file:   plugins/variablelineshape/pics/ox22-action-variableline-shape.png

---
+++ plugins/variablelineshape/VariableLineShapeFactory.h
@@ -0,0 +1,40 @@
+/*
+ * <one line to give the library's name and an idea of what it does.>
+ * Copyright (C) 2014  <copyright holder> <email>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef VARIABLELINESHAPEFACTORY_H
+#define VARIABLELINESHAPEFACTORY_H
+
+#include "KoShapeFactoryBase.h"
+
+class KoShape;
+
+/// Factory for path shapes
+class VariableLineShapeFactory : public KoShapeFactoryBase
+{
+public:
+    /// constructor
+    VariableLineShapeFactory();
+    ~VariableLineShapeFactory() {}
+    
+    virtual KoShape *createDefaultShape(KoDocumentResourceManager *documentResources = 0) const;
+    virtual bool supports(const KoXmlElement &e, KoShapeLoadingContext &context) const;
+};
+
+#endif // VARIABLELINESHAPEFACTORY_H
