f36d0e76dc8f739e0f42162970e177e6a2ca53f6
Harshita Mistry <harshimistry@gmail.com>
2014-06-24 02:41:36 -0400

    cmake files for java compiling

--- kexi/migration/odb/odbmigrate.cpp
+++ kexi/migration/odb/odbmigrate.cpp
@@ -5,15 +5,18 @@
 #include <QVariant>
 #include <QDateTime>
 #include <QList>
+#include <QString>
+#include <QStringList>
 
 #include <KDebug>
 #include <KLocale>
+#include <KStandardDirs>
 
 #include <kexiutils/identifier.h>
 
 using namespace KexiMigration;
-//K_EXPORT_KEXIMIGRATE_DRIVER(OdbMigrate, "odb")
-
+K_EXPORT_KEXIMIGRATE_DRIVER(OdbMigrate, "odb")
+JNIEnv *env;
 
 OdbMigrate::OdbMigrate(QObject *parent, const QVariantList &args)
         : KexiMigrate(parent, args)
@@ -30,36 +33,59 @@ OdbMigrate::~OdbMigrate()
 
 JNIEnv* create_vm(JavaVM ** jvm) {
 
-    JNIEnv *env;
     JavaVMInitArgs vm_args;
     JavaVMOption options;
-    options.optionString = "-Djava.class.path=/home/dhruv/kde4/src/calligra/kexi/migration/odb/reader:/home/dhruv/Downloads/hsqldb-2.3.2/hsqldb/lib/hsqldb.jar"; //Path to the java source code
 
+    QString odbreader_path=KStandardDirs::locate("lib","OdbReader.jar");
+    QString hsqldb_path=KStandardDirs::locate("lib","hsqldb.jar");
+    kDebug() << odbreader_path;
+    kDebug() << hsqldb_path;
+
+    options.optionString = QString("-Djava.class.path="+odbreader_path+":"+hsqldb_path).toAscii().data();
     vm_args.version = JNI_VERSION_1_6; //JDK version. This indicates version 1.6
     vm_args.nOptions = 1;
     vm_args.options = &options;
     vm_args.ignoreUnrecognized = 0;
 
     int ret = JNI_CreateJavaVM(jvm, (void**)&env, &vm_args);
-    if(ret < 0)
-        
-        kDebug() << "\nUnable to Launch JVM\n";
+    if(ret != JNI_OK)
+        kDebug() << "Unable to Launch JVM";
     return env;
 }
 
 
 bool OdbMigrate::drv_connect()
 {
-    JNIEnv *env;
     jclass clsH = env->FindClass("OdbReader");
     if(clsH) 
-      kDebug() << "found class\n";
-    else return 0;
+        kDebug() << "found class";
+    else 
+        return 0;
     jobject object = env->NewObject(clsH, NULL);
 
    
-    kDebug() << "object constructed\n";
+    kDebug() << "object constructed";
     return true;
 }
 
+bool OdbMigrate::drv_readTableSchema(
+    const QString& originalName, KexiDB::TableSchema& tableSchema)
+{
+    return false;
+}
 
+bool OdbMigrate::drv_disconnect()
+{
+    return false;
+}
+
+bool OdbMigrate::drv_tableNames(QStringList& tableNames)
+{
+    return false;
+}
+
+bool OdbMigrate::drv_copyTable(const QString& srcTable, KexiDB::Connection *destConn,
+                                 KexiDB::TableSchema* dstTable)
+{
+    return false;
+}
