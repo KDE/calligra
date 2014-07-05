398a1134f26cd4014798c950152ac47d5947fadc
Friedrich W. H. Kossebau <kossebau@kde.org>
2014-07-05 20:58:37 +0200

    Fix Next and Last button in Page selector of Report data view to be disabled on last page
    REVIEW: 119120
    
    Thanks jarosław for review

--- kexi/plugins/reports/kexireportview.cpp
+++ kexi/plugins/reports/kexireportview.cpp
@@ -71,6 +71,7 @@ KexiReportView::KexiReportView(QWidget *parent)
     layout()->addWidget(m_pageSelector);
     m_pageSelector->setRecordCount(0);
     m_pageSelector->setInsertingButtonVisible(false);
+    m_pageSelector->setInsertingEnabled(false);
     m_pageSelector->setLabelText(i18n("Page"));
     m_pageSelector->setButtonToolTipText(KexiRecordNavigator::ButtonFirst, i18n("Go to first page"));
     m_pageSelector->setButtonWhatsThisText(KexiRecordNavigator::ButtonFirst, i18n("Goes to first page"));
