fe6bc332a586507747fa894e0c2219897f8f758d
Boudewijn Rempt <boud@valdyas.org>
2014-07-13 13:22:53 +0200

    Only build the pyqt plugin if python, sip and pyqt are found

--- krita/plugins/extensions/pyqt/src/plugins/hello/hello.py
+++ krita/plugins/extensions/pyqt/src/plugins/hello/hello.py
@@ -1,5 +1,5 @@
-import sys
 from PyQt4.QtGui import *
 
-app = QApplication(sys.argv)
-QMessageBox.information(QWidget(), "Test", "Hello World")
+def hello():
+    QMessageBox.information(QWidget(), "Test", "Hello World")
+
