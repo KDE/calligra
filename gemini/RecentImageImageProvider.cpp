bfd6d4e67b27a1ed38c243f4a31138ccb5a3ad7c
Dan Leinir Turthra Jensen <admin@leinir.dk>
2014-07-31 12:44:06 +0100

    gemini: A bit of work on the file thumbnailer
    There is something which causes severe crashing and the like, so
    for now we just use embedded thumbnails (which now work for pptx as well)

--- gemini/RecentImageImageProvider.cpp
+++ gemini/RecentImageImageProvider.cpp
@@ -20,9 +20,12 @@
 #include <QFile>
 #include <QImage>
 #include <QImageReader>
+#include <QPainter>
 
 #include <KoStore.h>
 #include <KoDocument.h>
+#include <KoDocumentEntry.h>
+#include <KoPart.h>
 #include <KMimeTypeTrader>
 #include <KMimeType>
 #include <kio/previewjob.h>
@@ -56,18 +59,67 @@ QImage RecentImageImageProvider::requestImage(const QString &id, QSize *size, co
         // try to use any embedded thumbnail
         KoStore *store = KoStore::createStore(id, KoStore::Read);
 
+        bool thumbnailFound = false;
         if (store &&
-                (store->open(QLatin1String("Thumbnails/thumbnail.png")) ||
-                    store->open(QLatin1String("preview.png")))) {
+            // ODF thumbnail?
+            (store->open(QLatin1String("Thumbnails/thumbnail.png")) ||
+            // old KOffice/Calligra thumbnail?
+            store->open(QLatin1String("preview.png")) ||
+            // OOXML?
+            store->open(QLatin1String("docProps/thumbnail.jpeg")))) {
             // Hooray! No long delay for the user...
             const QByteArray thumbnailData = store->read(store->size());
 
-            if (thumbnail.loadFromData(thumbnailData) &&
-                    (thumbnail.width() >= width || thumbnail.height() >= height)) {
+            QImage thumbnailImage;
+            if (thumbnailImage.loadFromData(thumbnailData) ){//&&
+                //thumbnailImage.width() >= width && thumbnailImage.height() >= height) {
+                // put a white background behind the thumbnail
+                // as lots of old(?) OOo files have thumbnails with transparent background
+                thumbnail = QImage(thumbnailImage.size(), QImage::Format_RGB32);
+                thumbnail.fill(QColor(Qt::white).rgb());
+                QPainter p(&thumbnail);
+                p.drawImage(QPoint(0, 0), thumbnailImage);
+                p.end();
                 thumbnail = thumbnail.scaled(sz, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+                delete store;
+                thumbnailFound = true;
             }
         }
-        delete store;
+
+         if(!thumbnailFound) {
+//            qDebug() << "Thumbnail not found embedded in" << id;
+//             // load document and render the thumbnail ourselves
+//             const QString mimetype = KMimeType::findByPath(id)->name();
+//             QString error;
+//             KoDocumentEntry documentEntry = KoDocumentEntry::queryByMimeType(mimetype);
+//             KoPart* part = documentEntry.createKoPart(&error);
+// 
+//             if(part) {
+//                 KoDocument* document = part->document();
+// 
+//                 // prepare the document object
+//                 document->setCheckAutoSaveFile(false);
+//                 document->setAutoErrorHandlingEnabled(false); // don't show message boxes
+// 
+//                 // load the document content
+//                 KUrl url;
+//                 url.setPath(id);
+//                 if (document->openUrl(url)) {
+//                     while(document->isLoading()) {
+//                         qApp->processEvents();
+//                     }
+// 
+//                     // render the page on a bigger pixmap and use smoothScale,
+//                     // looks better than directly scaling with the QPainter (malte)
+//                     const bool usePassedSize = (width > sz.width() && height > sz.height());
+//                     const QSize size = usePassedSize ? QSize(width, height) : sz;
+//                     thumbnail = document->generatePreview(size).toImage();
+// 
+//                     document->closeUrl();
+//                 }
+//                 delete document;
+//             }
+         }
     }
     return thumbnail;
 }
