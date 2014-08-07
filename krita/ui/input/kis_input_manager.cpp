0651623ea6b7ebefce449259bc40b515b9c1f658
Boudewijn Rempt <boud@valdyas.org>
2014-08-07 15:54:22 +0200

    Comment out the setFocus call in KisInputManager
    This probably will give other problems, but it prevents Krita from rapidly
    switching between two image views.

--- krita/ui/input/kis_input_manager.cpp
+++ krita/ui/input/kis_input_manager.cpp
@@ -595,7 +595,8 @@ bool KisInputManager::eventFilter(QObject* object, QEvent* event)
         //Make sure the input actions know we are active.
         KisAbstractInputAction::setInputManager(this);
         //Ensure we have focus so we get key events.
-        d->canvas->canvasWidget()->setFocus();
+        // XXX: KOMVC Commented out because it kept setting focus to its canvas, even when we were selecting another one
+        //d->canvas->canvasWidget()->setFocus();
         stop_ignore_cursor_events();
         touch_stop_block_press_events();
 
