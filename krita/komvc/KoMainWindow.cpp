ba43ebb8317e3408b9ce8c8dbdf6873d89b0952f
Boudewijn Rempt <boud@valdyas.org>
2014-08-23 13:59:15 +0200

    Port the mimetype fix to KoMvc, too
    See http://commits.kde.org/calligra/fcfa63f8fe37f97bdb61f5c8d507d51765615844

--- krita/komvc/KoMainWindow.cpp
+++ krita/komvc/KoMainWindow.cpp
@@ -920,8 +920,18 @@ bool KoMainWindow::saveDocument(KoDocument *document, bool saveas, bool silent, 
         dialog.setMimeTypeFilters(mimeFilter);
         KUrl newURL = dialog.url();
 
+        if (newURL.isLocalFile()) {
+            QString fn = newURL.fileName();
+            if (QFileInfo(fn).completeSuffix().isEmpty()) {
+                KMimeType::Ptr mime = KMimeType::mimeType(_native_format);
+                fn.append(mime->mainExtension());
+            }
+            newURL = KUrl(fn);
+        }
+
         QByteArray outputFormat = _native_format;
-        if (!specialOutputFlag) {
+
+        if (!specialOutputFlag && mimeFilter.contains(dialog.selectedMimeType())) {
             KMimeType::Ptr mime = KMimeType::findByUrl(newURL);
             QString outputFormatString = mime->name();
             outputFormat = outputFormatString.toLatin1();
