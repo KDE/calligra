a1d08f052a0e90595afe7687a77fa42e5c727a42
Jaroslaw Staniek <staniek@kde.org>
2014-08-26 15:20:21 +0200

    Define translation file per Kexi (db/migration) driver
    +make the managers to load the right i18n catalog

---
+++ kexi/kexidb/drivers/sybase/Messages.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+cd ../../..
+source ../calligra_xgettext.sh
+
+potfile=kexisybasedriver
+find_exclude $potfile
+
+LIST="`find . \( $EXCLUDE \) -prune -o \( -name \*.ui \) -type f -print \
+| egrep "kexidb/drivers/sybase/|migration/sybase/" \
+| grep -v -e '/\.'`"
+if test -n "$LIST"; then
+    $EXTRACTRC $LIST >> rc.cpp
+fi
+
+# Exclude files containing "#warning noi18n"
+LIST=`find . \( $EXCLUDE \) -prune -o \( -name \*.h -o -name \*.cpp -o -name \*.cc -o -name \*.hxx -o -name \*.cxx \) -type f -print \
+| egrep "kexidb/drivers/sybase/|migration/sybase/" \
+| sort | while read f ; do \
+    if ! grep -q '^#warning noi18n ' $f ; then echo $f; fi \
+done \
+`
+if test -n "$LIST"; then
+    calligra_xgettext $LIST > $podir/$potfile.pot 2> /dev/null
+fi
+rm -f rc.cpp
