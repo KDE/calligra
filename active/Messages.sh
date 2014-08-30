36505349a9d40c2855ae962584ff8cbe4796e258
Yuri Chornoivan <yurchor@ukr.net>
2014-08-30 10:49:38 +0300

    Make scripty log cleaner

--- active/Messages.sh
+++ active/Messages.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
 source ../calligra_xgettext.sh
 
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
 calligra_xgettext calligraactive.pot `find . -name \*.qml -o -name \*.cpp -o -name \*.h`
 rm -f rc.cpp
