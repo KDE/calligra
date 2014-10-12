4c0a28cea216465ddf3a4a917dd1974a2c3ec286
Dan Leinir Turthra Jensen <admin@leinir.dk>
2014-10-12 12:37:13 +0100

    gemini: Don't clear in the dtor, just delete the contents
    This achieves the intention, without causing redrawing etc, which
    causes crashes in some circumstances... (i.e., on app exit)

--- gemini/cloud/dropbox/libs/listmodel.cc
+++ gemini/cloud/dropbox/libs/listmodel.cc
@@ -52,7 +52,7 @@ QVariant ListModel::data(const QModelIndex &index, int role) const
 
 ListModel::~ListModel() {
   delete m_prototype;
-  //clear();
+  qDeleteAll(m_list);
 }
 
 void ListModel::appendRow(ListItem *item)
