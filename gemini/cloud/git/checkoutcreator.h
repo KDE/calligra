00229e1587257e58647df1fbc1e5ccd33dc1eca2
Dan Leinir Turthra Jensen <admin@leinir.dk>
2014-10-28 12:27:22 +0000

    gemini: Add clone functionality to the git plugin

--- gemini/cloud/git/checkoutcreator.h
+++ gemini/cloud/git/checkoutcreator.h
@@ -34,6 +34,21 @@ public:
     Q_INVOKABLE QString getFile(QString caption, QString filter) const;
     Q_INVOKABLE QString getDir() const;
     Q_INVOKABLE bool isGitDir(QString directory) const;
+    /**
+     * Create a clone of the repository at url, in the local directory localPath, using
+     * the credentials as represented by credentials (same format as used by the cloud 
+     * accounts system). The user visible name is used to generate a reasonable clone dir
+     * name if one isn't easily guessable from the clone url.
+     * @param userVisibleName The user visible name of the account
+     * @param url             The clone URL (for example git@server.org:someplace/repositoryname.git
+     * @param localPath       The directory into which the clone will be made
+     * @param credentials     A QObject with properties set for the appropriate credentials type
+     * @return The local directory name in which the clone was eventually created, or empty if it failed
+     */
+    Q_INVOKABLE QString createClone(QString userVisibleName, QString url, QString localPath, QObject* credentials) const;
+
+Q_SIGNALS:
+    void cloneProgress(int progress);
 
 private:
     class Private;
