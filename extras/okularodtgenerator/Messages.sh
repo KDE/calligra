a93c5d77295cc390e28acb80ed9a802d7a430534
Dan Leinir Turthra Jensen <admin@leinir.dk>
2014-10-29 12:40:07 +0000

    Fix some merging leftovers

--- extras/okularodtgenerator/Messages.sh
+++ extras/okularodtgenerator/Messages.sh
@@ -1,8 +1,4 @@
 #! /bin/sh
 source ../../calligra_xgettext.sh
 
-<<<<<<< HEAD
-calligra_xgettext *.cpp > $podir/okularGenerator_odt.pot
-=======
 calligra_xgettext okularGenerator_odt.pot *.cpp
->>>>>>> origin/master
