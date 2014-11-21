453290259c8b0b74aceb8f5b76ce1b8dd25c1ac8
Dan Leinir Turthra Jensen <admin@leinir.dk>
2014-11-21 11:00:16 +0000

    Post-merge license header fixing

--- gemini/cloud/dropbox/libs/json.cc
+++ gemini/cloud/dropbox/libs/json.cc
@@ -1,3 +1,24 @@
+/*
+
+    Copyright 2010 Eeli Reilin <eeli@nilier.org>
+    Copyright 2010 Mikko Ahonen <mikko.j.ahonen@jyu.fi>
+
+    This package is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This package is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this package; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+*/
+
 /**
  * \file json.h
  *
