8b3e978c8d9013c701da22ff809a8fa9dae0f130
Friedrich W. H. Kossebau <kossebau@kde.org>
2015-01-03 17:11:41 +0100

    Let include guard match class/file name

--- gemini/TouchDeclarativeView.h
+++ gemini/TouchDeclarativeView.h
@@ -15,8 +15,9 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-#ifndef SKETCHDECLARATIVEVIEW_H
-#define SKETCHDECLARATIVEVIEW_H
+
+#ifndef TOUCHDECLARATIVEVIEW_H
+#define TOUCHDECLARATIVEVIEW_H
 
 #include <QDeclarativeView>
 #include <QPointer>
