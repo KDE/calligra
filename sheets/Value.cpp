d4aab7429e9d1bd961d45202c10a73b20e1877c6
Friedrich W. H. Kossebau <kossebau@kde.org>
2015-04-03 17:35:59 +0200

    qRound -> ::round & qAbs -> ::fabsl for long double

--- sheets/Value.cpp
+++ sheets/Value.cpp
@@ -514,7 +514,7 @@ QDateTime Value::asDateTime(const CalculationSettings* settings) const
     QDateTime datetime(settings->referenceDate(), QTime(), Qt::UTC);
 
     const int days = asInteger();
-    const int msecs = qRound((numToDouble(asFloat() - double(days))) * 86400000.0);      // 24*60*60*1000
+    const int msecs = ::round((numToDouble(asFloat() - double(days))) * 86400000.0);      // 24*60*60*1000
     datetime = datetime.addDays(days);
     datetime = datetime.addMSecs(msecs);
 
@@ -539,7 +539,7 @@ QTime Value::asTime(const CalculationSettings* settings) const
     QTime dt;
 
     const int days = asInteger();
-    const int msecs = qRound(numToDouble(asFloat() - double(days)) * 86400000.0);      // 24*60*60*1000
+    const int msecs = ::round(numToDouble(asFloat() - double(days)) * 86400000.0);      // 24*60*60*1000
     dt = dt.addMSecs(msecs);
 
     return dt;
