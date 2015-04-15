9c747eae8ac34b34a21ce083d8edd8fb4eb8d5d8
Dmitry Kazakov <dimula73@gmail.com>
2015-04-15 11:02:18 +0300

    Fix compilation of Karbon

--- karbon/plugins/tools/filterEffectTool/FilterResourceServerProvider.cpp
+++ karbon/plugins/tools/filterEffectTool/FilterResourceServerProvider.cpp
@@ -39,7 +39,7 @@ FilterResourceServerProvider::FilterResourceServerProvider()
 {
     KGlobal::mainComponent().dirs()->addResourceType("ko_effects", "data", "karbon/effects/");
 
-    m_filterEffectServer = new KoResourceServer<FilterEffectResource>("ko_effects", "*.svg");
+    m_filterEffectServer = new KoResourceServerSimpleConstruction<FilterEffectResource>("ko_effects", "*.svg");
     if (!QFileInfo(m_filterEffectServer->saveLocation()).exists()) {
         QDir().mkpath(m_filterEffectServer->saveLocation());
     }
