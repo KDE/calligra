bd4ab9a91ad288f7be54472d12181c6ba005e4b0
Roman Shtemberko <shtemberko@gmail.com>
2015-04-22 23:37:57 +0200

    Remove "dirty" flag when saving-as modified Kexi object
    BUG:344653
    REVIEW:123357

--- kexi/core/KexiWindow.cpp
+++ kexi/core/KexiWindow.cpp
@@ -804,6 +804,8 @@ tristate KexiWindow::storeDataAs(KexiPart::Item *item, KexiView::StoreNewDataOpt
         if (!project->createIdForPart(*part()->info()))
             return false;
     }
+    // clear 'dirty' for old window
+    setDirty(false);
     // for now this Window has new item assigned
     d->item = item;
 
