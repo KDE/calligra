189cd9c18909afe508b62af72f7504446b151190
Jaroslaw Staniek <staniek@kde.org>
2015-06-17 22:47:14 +0200

    Kexi: use QProxyStyle instead of KexiUtils::StyleProxy

--- kexi/formeditor/form.cpp
+++ kexi/formeditor/form.cpp
@@ -298,7 +298,8 @@ void Form::createToplevel(QWidget *container, FormWidget *formWidget, const QByt
     delete d->designModeStyle;
     d->designModeStyle = 0;
     if (d->mode == DesignMode) {
-        d->designModeStyle = new DesignModeStyle(d->topTree->widget()->style(), this);
+        d->designModeStyle = new DesignModeStyle(d->topTree->widget()->style());
+        d->designModeStyle->setParent(this);
         d->topTree->widget()->setStyle(d->designModeStyle);
     }
 }
@@ -364,6 +365,7 @@ void Form::setMode(Mode mode)
     d->mode = mode;
     if (d->mode == DesignMode) {
         d->designModeStyle = new DesignModeStyle(d->widget->style());
+        d->designModeStyle->setParent(this);
         d->widget->setStyle(d->designModeStyle);
         return;
     }
