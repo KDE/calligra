20740dbf4f40fa306f0b5e0e5bd4e95aadada606
Jaroslaw Staniek <staniek@kde.org>
2015-06-17 22:37:50 +0200

    Kexi: More safety: use KDb::intToFieldTypeGroup() and KDb::intToFieldType()

--- kexi/migration/AlterSchemaWidget.cpp
+++ kexi/migration/AlterSchemaWidget.cpp
@@ -115,7 +115,7 @@ void AlterSchemaWidget::tableClicked(const QModelIndex& idx)
         m_columnType->setCurrentIndex(m_types.indexOf(m_schema->field(m_selectedColumn)->typeName()));
 
         //Only set the pkey check enabled if the field type is integer
-        m_columnPKey->setEnabled(KDbField::isIntegerType(KDbField::Type(m_columnType->itemData(m_types.indexOf(m_schema->field(m_selectedColumn)->typeName())).toInt())));
+        m_columnPKey->setEnabled(KDbField::isIntegerType(KDb::intToFieldType(m_columnType->itemData(m_types.indexOf(m_schema->field(m_selectedColumn)->typeName())).toInt())));
 
         m_columnPKey->setChecked(m_schema->field(m_selectedColumn)->isPrimaryKey());
     }
@@ -123,13 +123,13 @@ void AlterSchemaWidget::tableClicked(const QModelIndex& idx)
 
 void AlterSchemaWidget::typeActivated(int typ)
 {
-    m_schema->field(m_selectedColumn)->setType(KDbField::Type(m_columnType->itemData(typ).toInt()));
+    m_schema->field(m_selectedColumn)->setType(KDb::intToFieldType(m_columnType->itemData(typ).toInt()));
 
     //Only set the pkey check enabled if the field type is integer
-    m_columnPKey->setEnabled(KDbField::isIntegerType(KDbField::Type(m_columnType->itemData(typ).toInt())));
+    m_columnPKey->setEnabled(KDbField::isIntegerType(KDb::intToFieldType(m_columnType->itemData(typ).toInt())));
 
     //If the field type is not integer, then the field cannot be a pkey
-    if (!KDbField::isIntegerType(KDbField::Type(m_columnType->itemData(typ).toInt()))) {
+    if (!KDbField::isIntegerType(KDb::intToFieldType(m_columnType->itemData(typ).toInt()))) {
         m_schema->field(m_selectedColumn)->setPrimaryKey(false);
     }
 }
