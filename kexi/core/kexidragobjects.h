ed91e5ff6873c0aa93fe46e75e323e41a6792fc2
Jaroslaw Staniek <staniek@kde.org>
2015-06-24 16:39:33 +0200

    KexiDragObjects: update for Qt5 API

--- kexi/core/kexidragobjects.h
+++ kexi/core/kexidragobjects.h
@@ -38,7 +38,7 @@ public:
      can be decoded as "kexi/fields" data. If decoding of "kexi/field"
      type is supported, decoding of "kexi/fields" is always supported.
      */
-    static bool canDecode(QMimeSource* e);
+    static bool canDecode(QDropEvent* e);
 
     /*! Decodes data of multiple-field drag ("kexi/fields" mime type) coming with event \a e.
      Sets \a sourceMimeType, \a sourceName and \a fields. Also works with "kexi/field" data.
