cc0c40fe25e180df12705b612488b41830f0a16d
Jaroslaw Staniek <staniek@kde.org>
2015-07-15 17:29:22 +0200

    Kexi: add a todo for showWarningContinueMessage

--- kexi/core/kexiguimsghandler.cpp
+++ kexi/core/kexiguimsghandler.cpp
@@ -212,6 +212,7 @@ void KexiGUIMessageHandler::showWarningContinueMessage(const QString &title,
     }
     if (!KMessageBox::shouldBeShownContinue(dontShowAgainName))
         return;
+    //! @todo what about the result?
     KMessageBox::warningContinueCancel(parentWidget(),
                                        title + (details.isEmpty() ? QString() : (QString("\n") + details)),
                                        QString(),
