5956a9e6c55f815f19e68556582e67908e6f1201
Wolthera van Hovell <griffinvalley@gmail.com>
2015-07-20 15:17:19 +0200

    Adding tilt-cursor to other brushengines, removing colour cursor entry

--- krita/plugins/paintops/libpaintop/kis_brush_based_paintop_settings.cpp
+++ krita/plugins/paintops/libpaintop/kis_brush_based_paintop_settings.cpp
@@ -73,10 +73,13 @@ QPainterPath KisBrushBasedPaintOpSettings::brushOutlineImpl(const KisPaintInform
         QPainterPath realOutline = brush->outline();
         QPainterPath tiltLine;
         QLineF tiltAngle(realOutline.boundingRect().center(), realOutline.boundingRect().topLeft());
-        tiltAngle.setLength(50 * (1 - info.tiltElevation(info, 60.0, 60.0, true)));
-        tiltAngle.setAngle(360.0 - fmod(KisPaintInformation::tiltDirection(info, true) * 360.0 + 270.0, 360.0));
+        tiltAngle.setLength(qMax(finalScale, 50.0) * (1 - info.tiltElevation(info, 60.0, 60.0, true)));
+        tiltAngle.setAngle((360.0 - fmod(KisPaintInformation::tiltDirection(info, true) * 360.0 + 270.0, 360.0))-2.0);
         tiltLine.moveTo(tiltAngle.p1());
         tiltLine.lineTo(tiltAngle.p2());
+        tiltAngle.setAngle((360.0 - fmod(KisPaintInformation::tiltDirection(info, true) * 360.0 + 270.0, 360.0))+2.0);
+        tiltLine.lineTo(tiltAngle.p2());
+        tiltLine.lineTo(tiltAngle.p1());
 
         if (mode == CursorIsCircleOutline || mode == CursorTiltOutline ||
             (forceOutline && mode == CursorNoOutline)) {
--- krita/ui/dialogs/kis_dlg_preferences.cc
+++ krita/ui/dialogs/kis_dlg_preferences.cc
@@ -103,7 +103,6 @@ GeneralTab::GeneralTab(QWidget *_parent, const char *_name)
     m_cmbOutlineShape->addItem(i18n("Circle Outline"));
     m_cmbOutlineShape->addItem(i18n("Preview Outline"));
     m_cmbOutlineShape->addItem(i18n("Tilt Outline"));
-    m_cmbOutlineShape->addItem(i18n("Color Outline"));
 
     m_cmbCursorShape->setCurrentIndex(cfg.newCursorStyle());
     m_cmbOutlineShape->setCurrentIndex(cfg.newOutlineStyle());
