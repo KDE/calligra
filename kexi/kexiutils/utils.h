KDE/calligra

commit 8fd9eeef0678f8e34b95fd5e6ace70c52b520583

Kexi API style: Use pointers for IN-OUT parameters instead of references

kexi/kexiutils/utils.h
@@ -383,7 +383,7 @@ KEXIUTILS_EXPORT void setMargins(QLayout *layout, int value);
  void myNonRecursiveFunctionEnabled() {
     if (!m_myNonRecursiveFunctionEnabled)
         return;
-    kexiUtils::BoolBlocker guard(m_myNonRecursiveFunctionEnabled, false);
+    kexiUtils::BoolBlocker guard(&m_myNonRecursiveFunctionEnabled, false);
     // function's body guarded against recursion...
  }
  @endcode
@@ -391,11 +391,11 @@ KEXIUTILS_EXPORT void setMargins(QLayout *layout, int value);
 class KEXIUTILS_EXPORT BoolBlocker
 {
 public:
-    inline BoolBlocker(bool& var, bool tempValue)
-     : v(var), origValue(var) { var = tempValue; }
-    inline ~BoolBlocker() { v = origValue; }
+    inline BoolBlocker(bool *var, bool tempValue)
+     : v(var), origValue(var) { *var = tempValue; }
+    inline ~BoolBlocker() { *v = origValue; }
 private:
-    bool& v;
+    bool *v;
     bool origValue;
 };
 

commit 6da4c38e81b53bf96aa7c0e1494fcaf84442fa91

Fix regression in KexiUtils::BoolBlocker - caused dropping orig values
Introduced in 8fd9eeef0678f8e34b95fd5e6 (Jun 9)

kexi/kexiutils/utils.h
@@ -447,7 +447,7 @@ KEXIUTILS_EXPORT void setMargins(QLayout *layout, int value);
  void myNonRecursiveFunctionEnabled() {
     if (!m_myNonRecursiveFunctionEnabled)
         return;
-    kexiUtils::BoolBlocker guard(&m_myNonRecursiveFunctionEnabled, false);
+    KexiUtils::BoolBlocker guard(&m_myNonRecursiveFunctionEnabled, false);
     // function's body guarded against recursion...
  }
  @endcode
@@ -456,7 +456,7 @@ class KEXIUTILS_EXPORT BoolBlocker
 {
 public:
     inline BoolBlocker(bool *var, bool tempValue)
-     : v(var), origValue(var) { *var = tempValue; }
+     : v(var), origValue(*var) { *var = tempValue; }
     inline ~BoolBlocker() { *v = origValue; }
 private:
     bool *v;