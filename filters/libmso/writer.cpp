c236a802358b643445cac9356cf42fa926ef7519
Jaroslaw Staniek <staniek@kde.org>
2015-09-04 09:12:18 +0200

    Formatting handled by the Client (since a2aa539a6) so format() and mm() not needed
    Summary: This removes warning about unused code
    
    Reviewers: kossebau
    
    Differential Revision: https://phabricator.kde.org/D323

--- filters/libmso/writer.cpp
+++ filters/libmso/writer.cpp
@@ -22,23 +22,6 @@
 #include "KoGenStyles.h"
 #include <QRegExp>
 
-namespace
-{
-QString format(double v)
-{
-    static const QString f("%1");
-    static const QString e("");
-    static const QRegExp r("\\.?0+$");
-    return f.arg(v, 0, 'f').replace(r, e);
-}
-
-QString mm(double v)
-{
-    static const QString mm("mm");
-    return format(v) + mm;
-}
-}//namespace
-
 Writer::Writer(KoXmlWriter& xmlWriter, KoGenStyles& kostyles,
                bool stylesxml_)
         : xOffset(0),
