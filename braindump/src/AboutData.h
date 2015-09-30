a54cb73d95994174ecd71e471d811e335834b7cf
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2015-10-01 00:25:00 +0530

    Update authors data for braindump

--- braindump/src/AboutData.h
+++ braindump/src/AboutData.h
@@ -30,6 +30,8 @@ inline KAboutData newBrainDumpAboutData()
 {
     KAboutData aboutData("braindump", ki18n("Braindump").toString(), BRAINDUMP_VERSION, ki18n(BRAINDUMP_DESCRIPTION).toString(),
                          KAboutLicense::LGPL, ki18n("(c) 2009, 2010, 2011, 2012, 2013 Cyrille Berger").toString());
+    aboutData.addAuthor(ki18n("Somsubhra Bairi").toString(), ki18n("Maintainer").toString(), "somsubhra.bairi@gmail.com", "http://somsubhra.com");
+    aboutData.addAuthor(ki18n("Cyrille Berger").toString(), ki18n("Former Maintainer").toString(), "cberger@cberger.net");
     return aboutData;
 }
 
