dca3b5118985e3f9976c828ee1063aaf405510be
Friedrich W. H. Kossebau <kossebau@kde.org>
2015-10-06 01:32:43 +0200

    Port KGlobalSettings::graphicEffectsLevel -> QStyle::SH_Widget_Animate

--- stage/part/KPrViewModeSlidesSorter.cpp
+++ stage/part/KPrViewModeSlidesSorter.cpp
@@ -599,7 +599,7 @@ void KPrViewModeSlidesSorter::customShowChanged(int showNumber)
 
     //Decide show or hide Custom Slide Shows View
     if (panelVisible != m_editCustomSlideShow) {
-        const bool animate = KGlobalSettings::graphicEffectsLevel() & KGlobalSettings::SimpleAnimationEffects;
+        const bool animate = m_centralWidget->style()->styleHint(QStyle::SH_Widget_Animate);
         const int duration = animate ? 250 : 1;
         QPropertyAnimation *animation = new QPropertyAnimation(m_customSlideShowView, "maximumHeight");
 
