bd31679dfd7fd0610e2e4f700f8b4a46918ba2be
Dmitry Kazakov <dimula73@gmail.com>
2015-11-05 14:32:34 +0300

    Avoid race conditions in the destruction of KisBusyProgressIndicator
    BUG:353638

--- krita/image/kis_node.cpp
+++ krita/image/kis_node.cpp
@@ -203,6 +203,7 @@ KisNode::KisNode(const KisNode & rhs)
 KisNode::~KisNode()
 {
     if (m_d->busyProgressIndicator) {
+        m_d->busyProgressIndicator->endUpdatesBeforeDestroying();
         m_d->busyProgressIndicator->deleteLater();
     }
 
