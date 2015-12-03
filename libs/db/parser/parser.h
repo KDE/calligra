d35b104c8344554e344c3779d7d9bbe762b534fe
Jaroslaw Staniek <staniek@kde.org>
2015-12-03 01:02:40 +0100

    CalligraDB: Remove unused Parser::isReservedKeyword declaration

--- libs/db/parser/parser.h
+++ libs/db/parser/parser.h
@@ -221,13 +221,6 @@ public:
      */
     void setError(const ParserError &err);
 
-    /**
-     * \return true if the \param str is an reserved
-     * keyword (see tokens.cpp for a list of reserved
-     * keywords).
-     */
-    bool isReservedKeyword(const QByteArray& str);
-
 protected:
     void init();
 
@@ -239,4 +232,3 @@ protected:
 }
 
 #endif
-
