ac1b749a6a5835018483657aab358e31a1a9183c
Pino Toscano <pino@kde.org>
2016-04-08 22:20:12 +0200

    i18n: do not extract ui messages in 'tests' subdirs

--- libs/Messages.sh
+++ libs/Messages.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 source ../calligra_xgettext.sh
 
-LIST=`find . \( -name \*.ui -o -name \*.rc \) -type f -print | grep -v -e '/\.'`
+LIST=`find . \( -name \*.ui -o -name \*.rc \) -type f -print | grep -v '/tests/' | grep -v -e '/\.'`
 $EXTRACTRC $LIST >> rc.cpp
 LIST=`find . \( -name \*.cpp -o -name \*.cc -o -name \*.h \) -type f -print | grep -v '/tests/' | grep -v -e '/\.'`
 calligra_xgettext calligra.pot $LIST
