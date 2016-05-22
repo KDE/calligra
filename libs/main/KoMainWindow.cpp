463b49ee91714b6f375a5cc32995dfc82b700549
Friedrich W. H. Kossebau <kossebau@kde.org>
2016-05-22 20:23:29 +0200

    Remove call of deprecated KMainWindow::initialGeometrySet(), always false

--- libs/main/KoMainWindow.cpp
+++ libs/main/KoMainWindow.cpp
@@ -371,44 +371,41 @@ KoMainWindow::KoMainWindow(const QByteArray &nativeMimeType, const KoComponentDa
     createMainwindowGUI();
     d->mainWindowGuiIsBuilt = true;
 
-    // if the user didn's specify the geometry on the command line (does anyone do that still?),
     // we first figure out some good default size and restore the x,y position. See bug 285804Z.
     KConfigGroup cfg( KSharedConfig::openConfig(), "MainWindow");
-    if (!initialGeometrySet()) {
-        QByteArray geom = QByteArray::fromBase64(cfg.readEntry("ko_geometry", QByteArray()));
-        if (!restoreGeometry(geom)) {
-            const int scnum = QApplication::desktop()->screenNumber(parentWidget());
-            QRect desk = QApplication::desktop()->availableGeometry(scnum);
-            // if the desktop is virtual then use virtual screen size
-            if (QApplication::desktop()->isVirtualDesktop()) {
-                desk = QApplication::desktop()->availableGeometry(QApplication::desktop()->screen());
-                desk = QApplication::desktop()->availableGeometry(QApplication::desktop()->screen(scnum));
-            }
-
-            quint32 x = desk.x();
-            quint32 y = desk.y();
-            quint32 w = 0;
-            quint32 h = 0;
-
-            // Default size -- maximize on small screens, something useful on big screens
-            const int deskWidth = desk.width();
-            if (deskWidth > 1024) {
-                // a nice width, and slightly less than total available
-                // height to componensate for the window decs
-                w = (deskWidth / 3) * 2;
-                h = (desk.height() / 3) * 2;
-            }
-            else {
-                w = desk.width();
-                h = desk.height();
-            }
-
-            x += (desk.width() - w) / 2;
-            y += (desk.height() - h) / 2;
-
-            move(x,y);
-            setGeometry(geometry().x(), geometry().y(), w, h);
+    QByteArray geom = QByteArray::fromBase64(cfg.readEntry("ko_geometry", QByteArray()));
+    if (!restoreGeometry(geom)) {
+        const int scnum = QApplication::desktop()->screenNumber(parentWidget());
+        QRect desk = QApplication::desktop()->availableGeometry(scnum);
+        // if the desktop is virtual then use virtual screen size
+        if (QApplication::desktop()->isVirtualDesktop()) {
+            desk = QApplication::desktop()->availableGeometry(QApplication::desktop()->screen());
+            desk = QApplication::desktop()->availableGeometry(QApplication::desktop()->screen(scnum));
         }
+
+        quint32 x = desk.x();
+        quint32 y = desk.y();
+        quint32 w = 0;
+        quint32 h = 0;
+
+        // Default size -- maximize on small screens, something useful on big screens
+        const int deskWidth = desk.width();
+        if (deskWidth > 1024) {
+            // a nice width, and slightly less than total available
+            // height to componensate for the window decs
+            w = (deskWidth / 3) * 2;
+            h = (desk.height() / 3) * 2;
+        }
+        else {
+            w = desk.width();
+            h = desk.height();
+        }
+
+        x += (desk.width() - w) / 2;
+        y += (desk.height() - h) / 2;
+
+        move(x,y);
+        setGeometry(geometry().x(), geometry().y(), w, h);
     }
     restoreState(QByteArray::fromBase64(cfg.readEntry("ko_windowstate", QByteArray())));
 
