KDE/krita

Merge in Krita-testing-wolthera
Make a clean build for best results!

Squashed commit of the following:

commit fdc03dbfd47d8071303bd4c58394d70b58ab1f4e
Author: Wolthera van Hövell <griffinvalley@gmail.com>
Date:   Sun Jan 31 15:16:37 2016 +0100

    Cleanup

    This introduces a little bug in the trc again, but I suposse this works for now.

commit c42e5c93cd9f33dc92927100c5aa47c5d8814b8b
Merge: 4b5806d c2706b3
Author: Wolthera van Hövell <griffinvalley@gmail.com>
Date:   Sun Jan 31 12:45:40 2016 +0100

    Merge branch 'master' into krita-testing-wolthera

commit 4b5806d06ce7c7ad06245489355f4e0f5573f0b7
Merge: 5f4ae5e 97a2851
Author: Wolthera van Hövell <griffinvalley@gmail.com>
Date:   Sat Jan 30 17:25:57 2016 +0100

    Merge branch 'master' into krita-testing-wolthera

commit 5f4ae5e01ec4596172908502fff048ff10ff41a2
Author: Wolthera van Hövell <griffinvalley@gmail.com>
Date:   Sat Jan 30 16:04:59 2016 +0100

    Make the trc curve widget work for cmyk

commit 95ba47ebe0b1a5394621ef82c7e280746b9c42d4
Author: Wolthera van Hövell <griffinvalley@gmail.com>
Date:   Sat Jan 30 16:04:16 2016 +0100

    Fix build

commit a814301c23ff75b88808038729455805ce1d7005
Merge: 2089d33 1a0554c
Author: Wolthera van Hövell <griffinvalley@gmail.com>
Date:   Fri Jan 29 08:41:00 2016 +0100

    Merge branch 'master' into krita-testing-wolthera

commit 2089d335a7f5b928edf5d3245268bf2e49d2ac46
Merge: 597780c 96d2211
Author: Wolthera van Hövell <griffinvalley@gmail.com>
Date:   Fri Jan 29 08:37:28 2016 +0100

    Merge branch 'krita-testing-wolthera' of git://anongit.kde.org/krita into krita-testing-wolthera

commit 96d22115b4006cf0be51d9525c9c4bd43c879bcd
Merge: 952bbc6 7d1d542
Author: Wolthera van Hovell tot Westerflier <griffinvalley@gmail.com>
Date:   Sat Jan 23 12:05:09 2016 +0100

    Merge branch 'master' into krita-testing-wolthera

commit 597780c56793abb5d7e140aae1708bb37226b9f3
Merge: 952bbc6 7167c6b
Author: Wolthera van Hövell <griffinvalley@gmail.com>
Date:   Thu Jan 14 20:

libs/pigment/KoColorConversions.h
@@ -68,6 +68,17 @@ KRITAPIGMENT_EXPORT void RGBToHCI(const qreal r, const qreal g, const qreal b, q
 KRITAPIGMENT_EXPORT void HCYToRGB(const qreal h, const qreal s, const qreal y, qreal *red, qreal *green, qreal *blue,  qreal R=0.299, qreal G=0.587, qreal B=0.114);
 KRITAPIGMENT_EXPORT void RGBToHCY(const qreal r, const qreal g, const qreal b, qreal *h, qreal *c, qreal *y,  qreal R=0.299, qreal G=0.587, qreal B=0.114);
 
+KRITAPIGMENT_EXPORT void RGBToYUV( qreal r, qreal g, qreal b, qreal *y, qreal *cb, qreal *cr, qreal R=0.299, qreal G=0.587, qreal B=0.114);
+KRITAPIGMENT_EXPORT void YUVToRGB(const qreal y, const qreal cb, const qreal cr, qreal *r, qreal *g, qreal *b, qreal R=0.299, qreal G=0.587, qreal B=0.114);
+
+KRITAPIGMENT_EXPORT void LabToLCH(const qreal l, const qreal a, const qreal b, qreal *L, qreal *C, qreal *H);
+KRITAPIGMENT_EXPORT void LCHToLab(const qreal L, const qreal C, const qreal H, qreal *l, qreal *a, qreal *b);
+
+KRITAPIGMENT_EXPORT void XYZToxyY(const qreal X, const qreal Y, const qreal Z, qreal *x, qreal *y, qreal *yY);
+KRITAPIGMENT_EXPORT void xyYToXYZ(const qreal x, const qreal y, const qreal yY, qreal *X, qreal *Y, qreal *Z);
+
+KRITAPIGMENT_EXPORT void CMYToCMYK(qreal *c, qreal *m, qreal *y, qreal *k);
+KRITAPIGMENT_EXPORT void CMYKToCMY(qreal *c, qreal *m, qreal *y, qreal *k);
 
 #endif // _KO_COLORCONVERSIONS_H_
 

KDE/calligra

libs/pigment/KoColorConversions.h
@@ -68,6 +68,17 @@ PIGMENTCMS_EXPORT void RGBToHCI(const qreal r, const qreal g, const qreal b, qre
 PIGMENTCMS_EXPORT void HCYToRGB(const qreal h, const qreal s, const qreal y, qreal *red, qreal *green, qreal *blue,  qreal R=0.299, qreal G=0.587, qreal B=0.114);
 PIGMENTCMS_EXPORT void RGBToHCY(const qreal r, const qreal g, const qreal b, qreal *h, qreal *c, qreal *y,  qreal R=0.299, qreal G=0.587, qreal B=0.114);
 
+PIGMENTCMS_EXPORT void RGBToYUV( qreal r, qreal g, qreal b, qreal *y, qreal *cb, qreal *cr, qreal R=0.299, qreal G=0.587, qreal B=0.114);
+PIGMENTCMS_EXPORT void YUVToRGB(const qreal y, const qreal cb, const qreal cr, qreal *r, qreal *g, qreal *b, qreal R=0.299, qreal G=0.587, qreal B=0.114);
+
+PIGMENTCMS_EXPORT void LabToLCH(const qreal l, const qreal a, const qreal b, qreal *L, qreal *C, qreal *H);
+PIGMENTCMS_EXPORT void LCHToLab(const qreal L, const qreal C, const qreal H, qreal *l, qreal *a, qreal *b);
+
+PIGMENTCMS_EXPORT void XYZToxyY(const qreal X, const qreal Y, const qreal Z, qreal *x, qreal *y, qreal *yY);
+PIGMENTCMS_EXPORT void xyYToXYZ(const qreal x, const qreal y, const qreal yY, qreal *X, qreal *Y, qreal *Z);
+
+PIGMENTCMS_EXPORT void CMYToCMYK(qreal *c, qreal *m, qreal *y, qreal *k);
+PIGMENTCMS_EXPORT void CMYKToCMY(qreal *c, qreal *m, qreal *y, qreal *k);
 
 #endif // _KO_COLORCONVERSIONS_H_
 