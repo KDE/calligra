b76f87b3ac0894a652f223447de0ea859b57d587
Jos van den Oever <jos@vandenoever.info>
2017-07-24 14:40:54 +0200

    Fix warning about static non-POD QString

--- libs/odf/tests/kodomtest.cpp
+++ libs/odf/tests/kodomtest.cpp
@@ -23,8 +23,12 @@
 
 #include <QTest>
 
-static QString const KoXmlNS_office("urn:oasis:names:tc:opendocument:xmlns:office:1.0");
-static QString const KoXmlNS_text("urn:oasis:names:tc:opendocument:xmlns:text:1.0");
+static QString const KoXmlNS_office() {
+    return QStringLiteral("urn:oasis:names:tc:opendocument:xmlns:office:1.0");
+}
+static QString const KoXmlNS_text() {
+    return QStringLiteral("urn:oasis:names:tc:opendocument:xmlns:text:1.0");
+}
 
 //static void debugElemNS( const QDomElement& elem )
 //{
@@ -36,9 +40,9 @@ void KoDomTest::initTestCase()
 {
     const QByteArray xml = QByteArray("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
                                       "<o:document-content xmlns:o=\"")
-                           + KoXmlNS_office.toUtf8()
-                           + "\" xmlns=\"" + KoXmlNS_text.toUtf8()
-                           + "\" xmlns:text=\"" + KoXmlNS_text.toUtf8()
+                           + KoXmlNS_office().toUtf8()
+                           + "\" xmlns=\"" + KoXmlNS_text().toUtf8()
+                           + "\" xmlns:text=\"" + KoXmlNS_text().toUtf8()
                            + "\">\n"
                            "<o:body><p text:style-name=\"L1\">foobar</p><p>2nd</p></o:body><o:styles></o:styles>\n"
                            "</o:document-content>\n";
@@ -55,15 +59,15 @@ void KoDomTest::testQDom()
     QCOMPARE(docElem.tagName(), QString("document-content"));
     QCOMPARE(docElem.localName(), QString("document-content"));
     QCOMPARE(docElem.prefix(), QString("o"));
-    QCOMPARE(docElem.namespaceURI(), KoXmlNS_office);
+    QCOMPARE(docElem.namespaceURI(), KoXmlNS_office());
 
-    KoXmlElement elem = KoXml::namedItemNS(docElem, KoXmlNS_office.toUtf8(), "body");
+    KoXmlElement elem = KoXml::namedItemNS(docElem, KoXmlNS_office().toUtf8(), "body");
 
     //debugElemNS( elem );
     QCOMPARE(elem.tagName(), QString("body"));
     QCOMPARE(elem.localName(), QString("body"));
     QCOMPARE(elem.prefix(), QString("o"));
-    QCOMPARE(elem.namespaceURI(), KoXmlNS_office);
+    QCOMPARE(elem.namespaceURI(), KoXmlNS_office());
 
     KoXmlNode n = elem.firstChild();
     for (; !n.isNull() ; n = n.nextSibling()) {
@@ -74,47 +78,47 @@ void KoDomTest::testQDom()
         QCOMPARE(e.tagName(), QString("p"));
         QCOMPARE(e.localName(), QString("p"));
         QVERIFY(e.prefix().isEmpty());
-        QCOMPARE(e.namespaceURI(), KoXmlNS_text);
+        QCOMPARE(e.namespaceURI(), KoXmlNS_text());
     }
 }
 
 
 void KoDomTest::testKoDom()
 {
-    KoXmlElement docElem = KoXml::namedItemNS(m_doc, KoXmlNS_office.toUtf8(), "document-content");
+    KoXmlElement docElem = KoXml::namedItemNS(m_doc, KoXmlNS_office().toUtf8(), "document-content");
     QCOMPARE(docElem.isNull(), false);
     QCOMPARE(docElem.localName(), QString("document-content"));
-    QCOMPARE(docElem.namespaceURI(), KoXmlNS_office);
+    QCOMPARE(docElem.namespaceURI(), KoXmlNS_office());
 
-    KoXmlElement body = KoXml::namedItemNS(docElem, KoXmlNS_office.toUtf8(), "body");
+    KoXmlElement body = KoXml::namedItemNS(docElem, KoXmlNS_office().toUtf8(), "body");
     QCOMPARE(body.isNull(), false);
     QCOMPARE(body.localName(), QString("body"));
-    QCOMPARE(body.namespaceURI(), KoXmlNS_office);
+    QCOMPARE(body.namespaceURI(), KoXmlNS_office());
 
-    KoXmlElement p = KoXml::namedItemNS(body, KoXmlNS_text.toUtf8(), "p");
+    KoXmlElement p = KoXml::namedItemNS(body, KoXmlNS_text().toUtf8(), "p");
     QCOMPARE(p.isNull(), false);
     QCOMPARE(p.localName(), QString("p"));
-    QCOMPARE(p.namespaceURI(), KoXmlNS_text);
+    QCOMPARE(p.namespaceURI(), KoXmlNS_text());
 
-    const KoXmlElement officeStyle = KoXml::namedItemNS(docElem, KoXmlNS_office.toUtf8(), "styles");
+    const KoXmlElement officeStyle = KoXml::namedItemNS(docElem, KoXmlNS_office().toUtf8(), "styles");
     QCOMPARE(officeStyle.isNull(), false);
 
     // Look for a non-existing element
-    KoXmlElement notexist = KoXml::namedItemNS(body, KoXmlNS_text.toUtf8(), "notexist");
+    KoXmlElement notexist = KoXml::namedItemNS(body, KoXmlNS_text().toUtf8(), "notexist");
     QVERIFY(notexist.isNull());
 
     int count = 0;
     KoXmlElement elem;
     forEachElement(elem, body) {
         QCOMPARE(elem.localName(), QString("p"));
-        QCOMPARE(elem.namespaceURI(), KoXmlNS_text);
+        QCOMPARE(elem.namespaceURI(), KoXmlNS_text());
         ++count;
     }
     QCOMPARE(count, 2);
 
     // Attributes
     // ### Qt bug: it doesn't work if using style-name instead of text:style-name in the XML
-    const QString styleName = p.attributeNS(KoXmlNS_text, "style-name", QString());
+    const QString styleName = p.attributeNS(KoXmlNS_text(), "style-name", QString());
     // qDebug( "%s", qPrintable( styleName ) );
     QCOMPARE(styleName, QString("L1"));
 }
