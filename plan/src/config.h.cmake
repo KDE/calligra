82b45995e017bebe038b6b63434b371683ede56e
Dag Andersen <danders@get2net.dk>
2017-12-01 13:24:07 +0100

    Plan: Move sources into src

--- plan/src/config.h.cmake
+++ plan/src/config.h.cmake
@@ -16,17 +16,6 @@
   #endif
 #endif
 
-#ifdef __APPLE__
-# ifdef __BIG_ENDIAN__
-#  define WORDS_BIGENDIAN 1
-# else
-#  undef WORDS_BIGENDIAN
-# endif
-#else
-/* Define to 1 if your processor stores words with the most significant byte
-   first (like Motorola and SPARC, unlike Intel and VAX). */
-#cmakedefine WORDS_BIGENDIAN ${CMAKE_WORDS_BIGENDIAN}
-#endif
 
 /* Number of bits in a file offset, on hosts where this is settable. */
 #define _FILE_OFFSET_BITS 64
