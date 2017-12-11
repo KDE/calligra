2b50b4d16a2ca0de9156522931ace1ea09e3317b
Dag Andersen <danders@get2net.dk>
2017-12-11 15:41:43 +0100

    Plan: Do not connect unknown slots

--- plan/src/libs/ui/kptperteditor.cpp
+++ plan/src/libs/ui/kptperteditor.cpp
@@ -68,8 +68,9 @@ PertEditor::PertEditor(KoPart *part, KoDocument *doc, QWidget *parent)
 
     connect( this, SIGNAL(executeCommand(KUndo2Command*)), doc, SLOT(addCommand(KUndo2Command*)) );
 
-    connect(this, SIGNAL(expandAll()), m_tasktree, SLOT(slotExpand()));
-    connect(this, SIGNAL(collapseAll()), m_tasktree, SLOT(slotCollapse()));
+// TODO: need to use TreeViewBase here
+//     connect(this, SIGNAL(expandAll()), m_tasktree, SLOT(slotExpand()));
+//     connect(this, SIGNAL(collapseAll()), m_tasktree, SLOT(slotCollapse()));
 }
 
 void PertEditor::slotCurrentTaskChanged( QTreeWidgetItem *curr, QTreeWidgetItem *prev )
