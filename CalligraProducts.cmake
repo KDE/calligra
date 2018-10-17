e6cde3a226ae1d27d51567d921e816217efb934d
Dag Andersen <danders@get2net.dk>
2018-10-17 13:24:24 +0200

    Build stage

--- CalligraProducts.cmake
+++ CalligraProducts.cmake
@@ -91,7 +91,7 @@ calligra_define_product(PART_COMPONENTS "QtQuick2 Plugin that provides Calligra 
 
 # apps
 calligra_define_product(APP_WORDS "Words app (for Desktop)"  REQUIRES PART_WORDS)
-calligra_define_product(APP_STAGE "Stage app (for Desktop)"  UNMAINTAINED   REQUIRES PART_STAGE)
+calligra_define_product(APP_STAGE "Stage app (for Desktop)"   REQUIRES PART_STAGE)
 calligra_define_product(APP_SHEETS "Sheets app (for Desktop)" REQUIRES PART_SHEETS)
 calligra_define_product(APP_KARBON "Karbon app (for Desktop)"  REQUIRES LIB_CALLIGRA LIB_KOMAIN)
 calligra_define_product(APP_FLOW "Flow app (for Desktop)" REQUIRES LIB_CALLIGRA LIB_KOMAIN LIB_KOPAGEAPP)
