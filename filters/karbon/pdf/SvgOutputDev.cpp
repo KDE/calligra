KDE/calligra

commit a615718222f8ad3bba4b88fad640382e517f7dd9

Fix build with poppler>=0.69
Remove no longer available Object::memCheck usage (which didn't do anything anyway since DEBUG_MEM was not set); add some const's

Differential Revision: https://phabricator.kde.org/D16388

filters/karbon/pdf/SvgOutputDev.cpp
@@ -212,7 +212,7 @@ QString SvgOutputDev::convertMatrix(const QMatrix &matrix)
            .arg(matrix.dx()) .arg(matrix.dy());
 }
 
-QString SvgOutputDev::convertMatrix(double * matrix)
+QString SvgOutputDev::convertMatrix(const double * matrix)
 {
     return QString("matrix(%1 %2 %3 %4 %5 %6)")
            .arg(matrix[0]).arg(matrix[1])
@@ -402,7 +402,7 @@ void SvgOutputDev::drawString(GfxState * state, GooString * s)
 
     QString str;
 
-    char * p = s->getCString();
+    const char * p = s->getCString();
     int len = s->getLength();
     CharCode code;
     Unicode *u = nullptr;
@@ -429,7 +429,7 @@ void SvgOutputDev::drawString(GfxState * state, GooString * s)
     double x = state->getCurX();
     double y = state->getCurY();
 
-    double * ctm = state->getCTM();
+    const double * ctm = state->getCTM();
     QMatrix transform(ctm[0], ctm[1], ctm[2], ctm[3], ctm[4], ctm[5]);
 
     QMatrix mirror;
@@ -522,7 +522,7 @@ void SvgOutputDev::drawImage(GfxState *state, Object */*ref*/, Stream *str,
         return;
     }
 
-    double * ctm = state->getCTM();
+    const double * ctm = state->getCTM();
     QMatrix m;
     m.setMatrix(ctm[0] / width, ctm[1] / width, -ctm[2] / height, -ctm[3] / height, ctm[2] + ctm[4], ctm[3] + ctm[5]);
 

filters/karbon/pdf/PdfImport.cpp
@@ -102,9 +102,6 @@ KoFilter::ConversionStatus PdfImport::convert(const QByteArray& from, const QByt
     delete globalParams;
     globalParams = 0;
 
-    // check for memory leaks
-    Object::memCheck(stderr);
-
     return KoFilter::OK;
 }
 

filters/karbon/pdf/SvgOutputDev.h
@@ -84,7 +84,7 @@ public:
 private:
     QString convertPath(GfxPath *path);
     QString convertMatrix(const QMatrix &matrix);
-    QString convertMatrix(double * matrix);
+    QString convertMatrix(const double * matrix);
     QString printFill();
     QString printStroke();
 

commit 3e832a0950eae7f7084fe742fb9c647c95778943

Fix build with poppler<0.64
Commit a615718222f8ad3bba4b88fad640382e517f7dd9 broke build with poppler<0.64, where getNextChar doesn't accept a const char first argument.

Differential Revision: https://phabricator.kde.org/D16406

filters/karbon/pdf/CMakeLists.txt
@@ -1,3 +1,6 @@
+if(Poppler_VERSION VERSION_LESS "0.64.0")
+    add_definitions("-DHAVE_POPPLER_PRE_0_64")
+endif()
 
 set(pdf2svg_PART_SRCS PdfImportDebug.cpp PdfImport.cpp SvgOutputDev.cpp )
 

filters/karbon/pdf/SvgOutputDev.cpp
@@ -402,7 +402,11 @@ void SvgOutputDev::drawString(GfxState * state, GooString * s)
 
     QString str;
 
+#ifdef HAVE_POPPLER_PRE_0_64
+    char * p = s->getCString();
+#else
     const char * p = s->getCString();
+#endif
     int len = s->getLength();
     CharCode code;
     Unicode *u = nullptr;