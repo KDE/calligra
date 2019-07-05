49a51c3a7e10f7df31f868e4e4610817b015f9b7
Pino Toscano <pino@kde.org>
2019-07-05 11:14:47 +0200

    i18n: use rc.cpp for extra sources
    Use rc.cpp as only dummy source for non-C strings, so repack-pot is
    happier.

--- sheets/Messages.sh
+++ sheets/Messages.sh
@@ -8,8 +8,8 @@ potfilename=calligrasheets.pot
 
 source ../kundo2_aware_xgettext.sh
 
-$EXTRACTRC --tag-group=koffice functions/*.xml > xml_doc.cpp
+$EXTRACTRC --tag-group=koffice functions/*.xml >> rc.cpp
 $EXTRACTRC chart/*.ui dialogs/*.ui  part/dialogs/*.ui *.kcfg *.rc >> rc.cpp
 $EXTRACTATTR --attr=optionWidget,name ui/CellToolOptionWidgets.xml >> rc.cpp
 kundo2_aware_xgettext $potfilename *.cpp chart/*.cpp commands/*.cpp database/*.cpp dialogs/*.cpp functions/*.cpp part/AboutData.h part/*.cpp part/commands/*.cpp part/dialogs/*.cpp ui/*.cpp
-rm -f xml_doc.cpp rc.cpp
+rm -f rc.cpp
