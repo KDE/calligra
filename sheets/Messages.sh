b4c7c3c64d59220f7c14ad4c50ddedbdbc359d23
Yuri Chornoivan <yurchor@ukr.net>
2022-05-21 09:48:25 +0300

    Add missing files to the Sheets Messages.sh

--- sheets/Messages.sh
+++ sheets/Messages.sh
@@ -9,7 +9,7 @@ potfilename=calligrasheets.pot
 source ../kundo2_aware_xgettext.sh
 
 $EXTRACTRC --tag-group=koffice functions/*.xml >> rc.cpp
-$EXTRACTRC part/chart/*.ui part/dialogs/*.ui part/*.kcfg part/*.rc >> rc.cpp
+$EXTRACTRC part/chart/*.ui part/dialogs/*.ui ui/dialogs/*.ui part/*.kcfg part/*.rc >> rc.cpp
 $EXTRACTATTR --attr=optionWidget,name ui/CellToolOptionWidgets.xml >> rc.cpp
-kundo2_aware_xgettext $potfilename *.cpp engine/*.cpp core/*.cpp part/chart/*.cpp part/commands/*.cpp part/dialogs/*.cpp functions/*.cpp part/AboutData.h part/*.cpp part/commands/*.cpp part/dialogs/*.cpp ui/*.cpp
+kundo2_aware_xgettext $potfilename *.cpp engine/*.cpp core/*.cpp core/ksp/*.cpp part/chart/*.cpp part/commands/*.cpp part/dialogs/*.cpp functions/*.cpp part/AboutData.h part/*.cpp part/commands/*.cpp part/dialogs/*.cpp ui/*.cpp ui/commands/*.cpp ui/dialogs/*.cpp ui/strategy/*.cpp
 rm -f rc.cpp
