16e456706c5e929295bd60138e8f972883067e02
Carl Schwan <carl@carlschwan.eu>
2024-04-27 16:54:16 +0200

    Ensure Windows.h is included before any other windows includes

--- gemini/MainWindow.cpp
+++ gemini/MainWindow.cpp
@@ -72,6 +72,7 @@
 
 #ifdef Q_OS_WIN
 // Slate mode/docked detection stuff
+#include <Windows.h>
 #include <Shellapi.h>
 #define SM_CONVERTIBLESLATEMODE 0x2003
 #define SM_SYSTEMDOCKED         0x2004
