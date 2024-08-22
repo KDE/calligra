fb7113c37489d2a94672e229b29e832c7ae30510
Carl Schwan <carl@carlschwan.eu>
2024-08-22 15:19:49 +0200

    Rewrite FindLibVisio.cmake
    Based on FindCamberra from ECM

--- cmake/modules/FindLibVisio.cmake
+++ cmake/modules/FindLibVisio.cmake
@@ -1,30 +1,45 @@
-# - Try to find LibVisio
-# Once done this will define
-#
-#  LIBVISIO_FOUND       - libvisio is available
-#  LIBVISIO_INCLUDE_DIRS - include directory, e.g. /usr/include
-#  LIBVISIO_LIBRARIES   - the libraries needed to use LibVisio
-#  LIBVISIO_DEFINITIONS - Compiler switches required for using LibVisio
-#
-# SPDX-FileCopyrightText: 2013 Yue Liu <yue.liu@mail.com>
-# Redistribution and use is allowed according to the terms of the BSD license.
+# SPDX-FileCopyrightText: 2024 Carl Schwan <carl@carlschwan.eu>
+# SPDX-License-Identifier: BSD-3-Clause
 
-include(LibFindMacros)
-libfind_package(LIBWPD LibWpd)
-libfind_package(LIBWPG LibWpg)
-libfind_pkg_check_modules(LIBVISIO_PKGCONF libvisio-0.1)
+find_package(PkgConfig QUIET)
+pkg_check_modules(PKG_LibVisio QUIET libvisio-0.1 IMPORTED_TARGET)
 
-find_path(LIBVISIO_INCLUDE_DIR
-    NAMES libvisio/libvisio.h
-    HINTS ${LIBVISIO_PKGCONF_INCLUDE_DIRS} ${LIBVISIO_PKGCONF_INCLUDEDIR}
-    PATH_SUFFIXES libvisio-0.1
-)
+set(LibVisio_VERSION ${PKG_LibVisio_VERSION})
+set(LibVisio_PREFIX ${PKG_LibVisio_PREFIX})
 
-find_library(LIBVISIO_LIBRARY
+find_library(LibVisio_LIBRARIES
     NAMES visio visio-0.1
-    HINTS ${LIBVISIO_PKGCONF_LIBRARY_DIRS} ${LIBVISIO_PKGCONF_LIBDIR}
+    HINTS ${PKG_LibVisio_LIBRARY_DIRS}
 )
 
-set(LIBVISIO_PROCESS_LIBS LIBVISIO_LIBRARY LIBWPD_LIBRARIES LIBWPG_LIBRARIES)
-set(LIBVISIO_PROCESS_INCLUDES LIBVISIO_INCLUDE_DIR LIBWPD_INCLUDE_DIRS LIBWPG_INCLUDE_DIRS)
-libfind_process(LIBVISIO)
+find_path(LibVisio_INCLUDE_DIRS
+    NAMES libvisio/libvisio.h
+    HINTS ${PKG_LibVisio_INCLUDE_DIRS}
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LibVisio
+    FOUND_VAR LibVisio_FOUND
+    REQUIRED_VARS LibVisio_PREFIX LibVisio_INCLUDE_DIRS
+    VERSION_VAR LibVisio_VERSION
+)
+
+if(LibVisio_FOUND AND NOT TARGET LibVisio::LibVisio)
+    add_library(LibVisio::LibVisio UNKNOWN IMPORTED)
+    set_target_properties(LibVisio::LibVisio PROPERTIES
+        IMPORTED_LOCATION "${LibVisio_LIBRARIES}"
+        INTERFACE_COMPILE_OPTIONS "${PKG_LibVisio_CFLAGS}"
+        INTERFACE_INCLUDE_DIRECTORIES "${LibVisio_INCLUDE_DIRS}"
+    )
+    if (TARGET PkgConfig::PKG_LibVisio)
+        target_link_libraries(LibVisio::LibVisio INTERFACE PkgConfig::PKG_LibVisio)
+    endif()
+endif()
+
+mark_as_advanced(LibVisio_LIBRARIES LibVisio_INCLUDE_DIRS LibVisio_VERSION)
+
+include(FeatureSummary)
+set_package_properties(LibVisio PROPERTIES
+    URL "https://wiki.documentfoundation.org/DLP/Libraries/libvisio"
+    DESCRIPTION "libvisio is a library and a set of tools for reading and converting MS Visio diagrams."
+)
