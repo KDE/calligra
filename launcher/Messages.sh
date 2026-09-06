da112b0b02169fe376b861cdd49d4b3f15f66482
Carl Schwan <carl@carlschwan.eu>
2026-09-06 11:37:20 +0200

    launcher: Fix message extraction

--- launcher/Messages.sh
+++ launcher/Messages.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
 # SPDX-FileCopyrightText: 2020 Tobias Fella <fella@posteo.de>
 # SPDX-License-Identifier: CC0-1.0
+potfilename=calligralauncher.pot
 $XGETTEXT `find -name \*.cpp -o -name \*.qml` -o $podir/calligralauncher.pot
